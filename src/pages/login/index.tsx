73651086e5b387a194342739b8b7b6c9c14b8507
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-25 10:34:30 +0300

    Add Antd Card component for login page.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Layout, Typography, Form, Input, Button } from "antd";
+import { Row, Col, Layout, Card, Typography, Form, Input, Button } from "antd";
 export interface ILoginForm {
     username: string;
     password: string;
@@ -24,37 +24,39 @@ export const LoginPage: React.FC<ILoginPageProps> = ({ onSubmit }) => {
                     height: "100vh",
                 }}
             >
-                <Col span={6}>
-                    <Title level={2} style={{ textAlign: "center" }}>
-                        Login
-                    </Title>
-                    <Form
-                        className="ant-form-vertical"
-                        form={form}
-                        name="control-hooks"
-                        onFinish={onSubmit}
-                    >
-                        <Form.Item
-                            name="username"
-                            label="Username"
-                            rules={[{ required: true }]}
+                <Col xl={6} lg={8} md={12} sm={18} xs={22}>
+                    <Card>
+                        <Title level={2} style={{ textAlign: "center" }}>
+                            Login
+                        </Title>
+                        <Form
+                            className="ant-form-vertical"
+                            form={form}
+                            name="control-hooks"
+                            onFinish={onSubmit}
                         >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            label="Password"
-                            rules={[{ required: true }]}
-                        >
-                            <Input type="password" />
-                        </Form.Item>
+                            <Form.Item
+                                name="username"
+                                label="Username"
+                                rules={[{ required: true }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                name="password"
+                                label="Password"
+                                rules={[{ required: true }]}
+                            >
+                                <Input type="password" />
+                            </Form.Item>
 
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit">
-                                Login
-                            </Button>
-                        </Form.Item>
-                    </Form>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Login
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Card>
                 </Col>
             </Row>
         </Layout>
