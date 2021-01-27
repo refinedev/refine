336d700cdb049a57e94d862cd6f59f64bba9d034
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-27 15:53:28 +0300

    Removed redux store.

--- src/containers/admin/index.tsx
+++ src/containers/admin/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -14,10 +13,9 @@ import {
     LoginPage,
     ListPage,
     CreatePage,
-    ShowPage,
     EditPage,
+    ShowPage,
 } from "@pages";
-import { store } from "@redux/store";
 
 export interface AdminProps {
     authProvider: IAuthContext;
@@ -42,47 +40,45 @@ export const Admin: React.FC<AdminProps> = ({
                 <DataContextProvider {...dataProvider}>
                     <ResourceContextProvider resources={resources}>
                         <Router>
-                            <Provider store={store}>
-                                <Switch>
-                                    <Route exact path="/login">
-                                        <LoginPage />
+                            <Switch>
+                                <Route exact path="/login">
+                                    <LoginPage />
+                                </Route>
+                                <Auth>
+                                    <Route exact path="/">
+                                        <DashboardPage />
                                     </Route>
-                                    <Auth>
-                                        <Route exact path="/">
-                                            <DashboardPage />
-                                        </Route>
-                                        <Route
-                                            exact
-                                            path="/resources/:resourceName"
-                                        >
-                                            <ListPage />
-                                        </Route>
-                                        <Route
-                                            exact
-                                            path="/resources/:resourceName/create"
-                                        >
-                                            <CreatePage />
-                                        </Route>
-                                        <Route
-                                            exact
-                                            path="/resources/:resourceName/edit/:id"
-                                        >
-                                            <EditPage />
-                                        </Route>
-                                        <Route
-                                            exact
-                                            path="/resources/:resourceName/show/:id"
-                                        >
-                                            <ShowPage />
-                                        </Route>
-                                    </Auth>
-                                </Switch>
-                            </Provider>
+                                    <Route
+                                        exact
+                                        path="/resources/:resourceName"
+                                    >
+                                        <ListPage />
+                                    </Route>
+                                    <Route
+                                        exact
+                                        path="/resources/:resourceName/create"
+                                    >
+                                        <CreatePage />
+                                    </Route>
+                                    <Route
+                                        exact
+                                        path="/resources/:resourceName/edit/:id"
+                                    >
+                                        <EditPage />
+                                    </Route>
+                                    <Route
+                                        exact
+                                        path="/resources/:resourceName/show/:id"
+                                    >
+                                        <ShowPage />
+                                    </Route>
+                                </Auth>
+                            </Switch>
                         </Router>
                     </ResourceContextProvider>
                 </DataContextProvider>
             </AuthContextProvider>
-            <ReactQueryDevtools initialIsOpen={false} />
+            <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
         </QueryClientProvider>
     );
 };
--- src/containers/auth/index.tsx
+++ src/containers/auth/index.tsx
@@ -1,25 +1,23 @@
 import React, { useContext } from "react";
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
-import { UserActions } from "@actions";
 import { Layout } from "@components";
 import { AuthContext, IAuthContext } from "@contexts/auth";
 
 export const Auth: React.FC = ({ children }) => {
-    const { checkAuth, userIdentity } = useContext<IAuthContext>(AuthContext);
+    const { checkAuth } = useContext<IAuthContext>(AuthContext);
     const history = useHistory();
 
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
     // check auth
     checkAuth({}).catch(() => history.push("/login"));
 
     // set user identity
-    userIdentity &&
-        userIdentity().then((data: any) => {
-            dispatch(UserActions.setIdentity(data));
-        });
+    // userIdentity &&
+    //     userIdentity().then((data: any) => {
+    //         dispatch(UserActions.setIdentity(data));
+    //     });
 
     return <Layout>{children}</Layout>;
 };
