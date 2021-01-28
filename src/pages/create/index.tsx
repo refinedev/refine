3d2546783bb887e31c164c8ee51d23fdb2690846
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-28 18:07:42 +0300

    Removed unuse pages.

--- src/pages/create/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export const CreatePage: React.FC = () => {
-    const { resourceName } = useParams<Record<string, string>>();
-
-    return <span>create {resourceName}</span>;
-};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,2 @@
 export { DashboardPage } from "./dashboard";
 export { LoginPage } from "./login";
-export { ListPage } from "./list";
-export { CreatePage } from "./create";
-export { ShowPage } from "./show";
-export { EditPage } from "./edit";
