12fc322b73baf2d3393ab5bb09259a75f3abf411
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-28 14:44:40 +0300

    Add example using column render prop.

--- example/src/post.tsx
+++ example/src/post.tsx
@@ -6,7 +6,17 @@ export const PostList = (props: any) => {
         <List {...props}>
             <Column key="id" title="ID" dataIndex="id" />
             <Column key="title" title="Title" dataIndex="title" />
-            <Column key="slug" title="Slug" dataIndex="slug" />
+            <Column
+                key="slug"
+                title="Slug"
+                dataIndex="slug"
+                render={(text: any, record: any) => (
+                    <div>
+                        <p>{text}</p>
+                        <p>{record.title}</p>
+                    </div>
+                )}
+            />
         </List>
     );
 };
