d7ac10f6acf04d04a55a7ffab72b557b8da782e5
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-02-09 12:38:25 +0300

    Add useAuthenticated hook.

--- src/hooks/auth/index.ts
+++ src/hooks/auth/index.ts
@@ -2,3 +2,4 @@ export { usePermissions } from "./usePermissions";
 export { useGetIdentity } from "./useGetIdentity";
 export { useLogout } from "./useLogout";
 export { useLogin } from "./useLogin";
+export { useAuthenticated } from "./useAuthenticated";
