b4fc0122faaa60953763065a4bcd73e820bce833
Ali Emir Sen <senaliemir@gmail.com>
2021-02-09 11:36:42 +0300

    useCreate hook

---
+++ src/hooks/data/useCreate.ts
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { useMutation, UseMutationResult } from "react-query";
+
+import { DataContext } from "@contexts/data";
+import { CreateResponse, IDataContext } from "@interfaces";
+
+type UseCreateReturnType = UseMutationResult<
+    CreateResponse,
+    unknown,
+    {
+        values: string;
+    },
+    unknown
+>;
+
+export const useCreate = (resource: string): UseCreateReturnType => {
+    const { create } = useContext<IDataContext>(DataContext);
+
+    if (!resource) {
+        throw new Error("'resource' is required for useCreate hook.");
+    }
+
+    const mutation = useMutation(({ values }: { values: string }) =>
+        create(resource, values),
+    );
+
+    return mutation;
+};
