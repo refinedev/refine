c9892e265d3af93dc468a5ec2002f0bc8637d0d7
Ali Emir Sen <senaliemir@gmail.com>
2021-02-09 11:29:06 +0300

    useOne hook

---
+++ src/hooks/data/useOne.ts
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { QueryObserverResult, useQuery } from "react-query";
+
+import { DataContext } from "@contexts/data";
+import { GetOneResponse, IDataContext } from "@interfaces";
+
+export const useOne = (
+    resource: string,
+    id: string,
+): QueryObserverResult<GetOneResponse, unknown> => {
+    const { getOne } = useContext<IDataContext>(DataContext);
+
+    if (!resource || !id) {
+        throw new Error(
+            `Resource and ID is required. You provided resource: ${resource}, id: ${id}`,
+        );
+    }
+
+    const queryResponse = useQuery<GetOneResponse>(
+        `resource/getOne/${resource}`,
+        () => getOne(resource, id),
+    );
+
+    return queryResponse;
+};
