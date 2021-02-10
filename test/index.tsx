c67b5a860f8aaf1f1538e09cd02b3525c2ca6b7c
Ali Emir Sen <senaliemir@gmail.com>
2021-02-10 17:12:16 +0300

    add testwrapper helper for testing library

---
+++ test/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { AuthContextProvider } from "@contexts/auth";
+import { DataContextProvider } from "@contexts/data";
+import { ResourceContextProvider } from "@contexts/resource";
+import { IDataContext, IAuthContext } from "@interfaces";
+
+const queryClient = new QueryClient();
+
+interface ITestWrapperProps {
+    authProvider?: IAuthContext;
+    dataProvider?: IDataContext;
+    resources: string[];
+    children?: React.ReactNode;
+}
+
+export const TestWrapper: (props: ITestWrapperProps) => React.FC = ({
+    authProvider,
+    dataProvider,
+    resources,
+}) => {
+    return ({ children }) => {
+        const withResource = (
+            <ResourceContextProvider resources={resources}>
+                {children}
+            </ResourceContextProvider>
+        );
+        const withData = dataProvider ? (
+            <DataContextProvider {...dataProvider}>
+                {withResource}
+            </DataContextProvider>
+        ) : (
+            withResource
+        );
+        const withAuth = authProvider ? (
+            <AuthContextProvider {...authProvider}>
+                {withData}
+            </AuthContextProvider>
+        ) : (
+            withData
+        );
+
+        return (
+            <QueryClientProvider client={queryClient}>
+                {withAuth}
+            </QueryClientProvider>
+        );
+    };
+};
+
+// re-export everything
+export * from "@testing-library/react";
