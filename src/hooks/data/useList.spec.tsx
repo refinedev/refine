1419f09f1acd9dbd953bd9bb421bf372ba872e1c
Ali Emir Sen <senaliemir@gmail.com>
2021-02-10 17:24:23 +0300

    update mock server usage

--- src/hooks/data/useList.spec.tsx
+++ src/hooks/data/useList.spec.tsx
@@ -1,30 +1,14 @@
 import { renderHook } from "@testing-library/react-hooks";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
 
-import JsonServer from "@dataProviders/jsonServer";
-import { TestWrapper } from "@test";
+import { MockJSONServer, TestWrapper } from "@test";
 
 import { useList } from "./useList";
-import { posts } from "./mocks";
-
-const server = setupServer(
-    rest.get("https://readmin-fake-rest.pankod.com/posts", (_req, res, ctx) => {
-        return res(ctx.json(posts), ctx.set("x-total-count", "2"));
-    }),
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
 
 describe("useList Hook", () => {
     it("with rest json server", async () => {
         const { result, waitFor } = renderHook(() => useList("posts"), {
             wrapper: TestWrapper({
-                dataProvider: JsonServer(
-                    "https://readmin-fake-rest.pankod.com",
-                ),
+                dataProvider: MockJSONServer,
                 resources: ["posts"],
             }),
         });
