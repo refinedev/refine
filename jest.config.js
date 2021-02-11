77bcf1bdfaa496517f418ba6d4b5d8e985bc2bf2
Ali Emir Sen <senaliemir@gmail.com>
2021-02-11 10:10:44 +0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,11 @@
 const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const { compilerOptions } = require("../tsconfig.json");
+const { compilerOptions } = require("./tsconfig.json");
 
 const paths = compilerOptions.paths ? compilerOptions.paths : {};
 
 module.exports = {
     preset: "ts-jest",
     rootDir: "./",
-    globals: {
-        "ts-jest": {
-            tsconfig: "tsconfig.test.json",
-        },
-    },
     setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
     testPathIgnorePatterns: [
         "<rootDir>/node_modules/",
