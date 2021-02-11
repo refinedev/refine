7e56ed63a92c810ef083721376669299ecd831ca
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-02-11 16:01:49 +0300

    Fix resource props (#77)
    * Fixed resourceName prop.
    
    * Fixed eslint warning.
    
    * Fixed build errors.

--- src/components/table/components/column/index.tsx
+++ src/components/table/components/column/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Table } from "antd";
 import { ColumnProps as AntdColumnProps } from "antd/lib/table";
 
-export type ColumnProps = AntdColumnProps<any> & {};
+export type ColumnProps = AntdColumnProps<{
+    id: string | number;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any;
+}> & {};
 
 export const Column: React.FC<ColumnProps> = ({ ...rest }) => {
     return <Table.Column {...rest} />;
