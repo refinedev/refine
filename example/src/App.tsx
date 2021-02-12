f2ade3c59d72f83d2124ad6cc124fb0f97d90e34
Muharrem <mkocadere@gmail.com>
2021-02-12 12:45:52 +0300

    add icon, label example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Admin, Resource, AuthProvider, JsonServer } from 'readmin';
+import { Admin, Resource, AuthProvider, JsonServer, UserOutlined } from 'readmin';
 
 import { PostCreate, PostList, PostEdit } from './components/pages/post'
 import { CategoryList, CategoryCreate } from './components/pages/category';
@@ -50,6 +50,7 @@ function App() {
         create={PostCreate}
         edit={PostEdit}
         canDelete
+        options={{label: "My Posts"}}
       />
       <Resource
         name="categories"
@@ -57,7 +58,7 @@ function App() {
         create={CategoryCreate}
         canDelete
       />
-      <Resource name="users" list={UserList} />
+      <Resource name="users" list={UserList} icon={<UserOutlined/>} />
       <Resource
         name="tags"
         list={TagList}
