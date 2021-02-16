93be2241d1e04fc32ed8767e008796d69e142ea2
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-02-16 17:20:41 +0300

    Fixed indentsize on example app.

--- example/src/components/pages/user.tsx
+++ example/src/components/pages/user.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { List, Table, TextField } from 'readmin';
+import * as React from "react";
+import { List, Table, TextField } from "readmin";
 
 export const UserList = (props: any) => {
-  return (
-    <List {...props}>
-      <Table>
-        <TextField source="id" title="ID" />
-        <TextField source="firstName" title="First Name" />
-        <TextField source="lastName" title="Last Name" />
-        <TextField source="status" title="Status" />
-      </Table>
-    </List>
-  );
+    return (
+        <List {...props}>
+            <Table>
+                <TextField source="id" title="ID" />
+                <TextField source="firstName" title="First Name" />
+                <TextField source="lastName" title="Last Name" />
+                <TextField source="status" title="Status" />
+            </Table>
+        </List>
+    );
 };
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root"),
 );
