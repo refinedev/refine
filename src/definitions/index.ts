96c522afe5422924e40e82031a01ce457bbc140f
mhrrmk <mkocadere@gmail.com>
2021-02-17 12:13:48 +0300

    Add empty prop to List. Add OptionalComponent. (#113)
    * add OptionalComponent
    
    * add empty prop to List. add empty default view for list.
    
    * fix naming

--- src/definitions/index.ts
+++
@@ -1,6 +0,0 @@
-import { BaseFieldProps } from "@interfaces";
-
-export const fieldContent = ({ record, source }: BaseFieldProps) => {
-    const stringSource = source.toString();
-    return record ? record?.[stringSource] : stringSource;
-};
