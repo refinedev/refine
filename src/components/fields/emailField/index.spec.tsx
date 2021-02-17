7034b792c34d7795183cd695c51bcf631ae33314
Muharrem <mkocadere@gmail.com>
2021-02-17 16:34:49 +0300

    add FunctionField

--- src/components/fields/emailField/index.spec.tsx
+++ src/components/fields/emailField/index.spec.tsx
@@ -6,7 +6,10 @@ import { render } from "@test";
 describe("EmailField", () => {
     it("renders email with mailto href", () => {
         const { getByText } = render(
-            <EmailField source="email" record={{ email: "test@test.com" }} />,
+            <EmailField
+                source="email"
+                record={{ id: 1, email: "test@test.com" }}
+            />,
         );
 
         expect(getByText("test@test.com")).toHaveProperty("href", "mailto:");
