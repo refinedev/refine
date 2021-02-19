a8a758890f2153611766ab1e2779a2cdda6061d0
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-02-19 09:29:25 +0300

    fix lint

--- src/containers/admin/index.spec.tsx
+++ src/containers/admin/index.spec.tsx
@@ -1,39 +1,35 @@
 import React from "react";
 
 import { ReadyPage } from "@pages";
-import { Resource } from "@containers";
 
-import {
-    render,
-    MockJSONServer,
-} from "@test";
+import { render, MockJSONServer } from "@test";
 
 import { Admin } from "./index";
 
 const mockAuthProvider = {
     login: (params: any) => {
-        if (params.username === 'admin') {
-            localStorage.setItem('username', params.username);
+        if (params.username === "admin") {
+            localStorage.setItem("username", params.username);
             return Promise.resolve();
         }
 
         return Promise.reject();
     },
     logout: () => {
-        localStorage.removeItem('username');
+        localStorage.removeItem("username");
         return Promise.resolve();
     },
     checkError: () => Promise.resolve(),
     checkAuth: () =>
-        localStorage.getItem('username') ? Promise.resolve() : Promise.reject(),
-    getPermissions: () => Promise.resolve(['admin']),
+        localStorage.getItem("username") ? Promise.resolve() : Promise.reject(),
+    getPermissions: () => Promise.resolve(["admin"]),
     getUserIdentity: () =>
         Promise.resolve({
             id: 1,
-            fullName: 'Jane Doe',
+            fullName: "Jane Doe",
             avatar:
-                'https://unsplash.com/photos/IWLOvomUmWU/download?force=true&w=640'
-        })
+                "https://unsplash.com/photos/IWLOvomUmWU/download?force=true&w=640",
+        }),
 };
 
 describe("Admin Container", () => {
@@ -42,7 +38,7 @@ describe("Admin Container", () => {
             <Admin
                 authProvider={mockAuthProvider}
                 dataProvider={MockJSONServer}
-            />
+            />,
         );
         expect(ReadyPage).toBeTruthy();
     });
