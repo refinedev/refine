3b6c75fe56c9880d07b4c813d07a14ed27cf32a8
Necati Özmen <necati@pankod.com>
2021-02-19 14:30:06 +0300

    Add date field (#121)
    * Add date field folder
    
    * Complete datefield component
    
    * Add component tests
    
    * Fix test
    
    * Remove console log
    
    * Update src/components/fields/dateField/index.tsx
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- src/components/fields/index.ts
+++ src/components/fields/index.ts
@@ -3,4 +3,5 @@ export { TextField } from "./textField";
 export { TagField } from "./tagField";
 export { EmailField } from "./emailField";
 export { ImageField } from "./imageField";
+export { DateField } from "./dateField";
 export { FileField } from "./fileField";
