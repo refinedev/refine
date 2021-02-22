59a0708ba6a8fa5ed4d0a281c32b10fd175a4996
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-02-22 15:12:07 +0300

    Add required type on renderFieldRecord method.

--- src/definitions/index.tsx
+++ src/definitions/index.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { get } from "lodash";
+import get from "lodash/get";
 
 import { BaseFieldProps } from "@interfaces";
+
+type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
+    T,
+    Exclude<keyof T, Keys>
+> &
+    {
+        [K in Keys]-?: Required<Pick<T, K>> &
+            Partial<Record<Exclude<Keys, K>, undefined>>;
+    }[Keys];
 
 export const renderFieldRecord = ({
     value,
     record,
     renderRecordKey,
-}: BaseFieldProps): string => {
+}: RequireOnlyOne<BaseFieldProps, "value" | "record">): string => {
     if (record && renderRecordKey) {
         const recordValue = get(record, renderRecordKey);
-        if (recordValue) {
-            return recordValue;
-        } else {
+
+        if (!recordValue) {
             throw new Error("undefined record or renderRecordKey value");
         }
+
+        return recordValue;
     }
 
     return value;
--- src/interfaces/field.ts
+++ src/interfaces/field.ts
@@ -3,6 +3,6 @@ import { BaseRecord } from "@contexts/data/IDataContext";
 export interface BaseFieldProps {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     value: any;
-    record?: BaseRecord;
+    record: BaseRecord;
     renderRecordKey?: string;
 }
