28a9c0682b2867caee7c283985a06ee4e106e220
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-02-22 14:57:16 +0300

    updated tests

--- src/components/fields/booleanField/index.spec.tsx
+++ src/components/fields/booleanField/index.spec.tsx
@@ -1,28 +1,80 @@
 import React from "react";
 
-import { CheckSquareOutlined, CloseOutlined } from "@ant-design/icons";
+import { CheckSquareOutlined } from "@ant-design/icons";
 
-import { render } from "@test";
+import { render, fireEvent } from "@test";
 import { BooleanField } from "./";
 
 describe("BooleanField", () => {
-    it("renders boolean field value with correct icon", () => {
-        // const { getByTestId } = render(
-        //     <BooleanField value={true} data-testid="test-boolean-field" />,
-        // );
-        // const icon = getByTestId("test-boolean-field").children[0].children[0];
-        // expect(icon.classList.contains("anticon-check")).toBe(true);
+    describe("BooleanField with default props values", () => {
+        it("renders boolean field value with correct tooltip text", async () => {
+            const baseDom = render(
+                <div data-testid="default-field">
+                    <BooleanField value={true} />
+                </div>,
+            );
+
+            fireEvent.mouseOver(
+                baseDom.getByTestId("default-field").children[0],
+            );
+
+            expect(await baseDom.findByText("true")).toBeInTheDocument();
+        });
+
+        it("renders boolean field value with correct icon", () => {
+            const baseDom = render(
+                <div data-testid="default-field">
+                    <BooleanField value={true} />
+                </div>,
+            );
+
+            const booleanField = baseDom.getByTestId("default-field")
+                .children[0];
+
+            expect(
+                booleanField.children[0].classList.contains("anticon-check"),
+            ).toBe(true);
+        });
     });
 
-    it("renders value with prop for custom icon", () => {
-        // const { getByTestId } = render(
-        //     <BooleanField
-        //         value={true}
-        //         data-testid="test-boolean-field"
-        //         TrueIcon={<CheckSquareOutlined />}
-        //     />,
-        // );
-        // const icon = getByTestId("test-boolean-field").children[0].children[0];
-        // expect(icon.classList.contains("anticon-check-square")).toBe(true);
+    describe("BooleanField with custom props values", () => {
+        it("should use prop for custom text", async () => {
+            const baseDom = render(
+                <div data-testid="custom-field">
+                    <BooleanField
+                        value={true}
+                        valueLabelTrue="test"
+                        TrueIcon={<CheckSquareOutlined />}
+                    />
+                </div>,
+            );
+
+            const booleanField = baseDom.getByTestId("custom-field")
+                .children[0];
+            fireEvent.mouseOver(booleanField);
+
+            expect(await baseDom.findByText("test")).toBeInTheDocument();
+        });
+
+        it("renders value with prop for custom icon", () => {
+            const baseDom = render(
+                <div data-testid="custom-field">
+                    <BooleanField
+                        value={true}
+                        valueLabelTrue="test"
+                        TrueIcon={<CheckSquareOutlined />}
+                    />
+                </div>,
+            );
+
+            const booleanField = baseDom.getByTestId("custom-field")
+                .children[0];
+
+            expect(
+                booleanField.children[0].classList.contains(
+                    "anticon-check-square",
+                ),
+            ).toBe(true);
+        });
     });
 });
