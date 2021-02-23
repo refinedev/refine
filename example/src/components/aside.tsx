0226f484172c27ac2a572e84a33d4facdb34ad6e
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-02-23 23:57:41 +0300

    delete card style

--- example/src/components/aside.tsx
+++ example/src/components/aside.tsx
@@ -3,12 +3,7 @@ import { Card, CardProps } from "antd";
 
 export const Aside: React.FC<CardProps> = (props: any) => {
     return (
-        <Card
-            {...props}
-            title="Post details"
-            extra={<a href="#">More</a>}
-            style={{ width: 300 }}
-        >
+        <Card {...props} title="Post details" extra={<a href="#">More</a>}>
             <p>Posts will only be published once an editor approves them</p>
         </Card>
     );
