5086fbf1968a0d8be6cfc95d2d6141373b918527
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-02-23 16:53:26 +0300

    update list with aside prop

--- src/components/crud/list/index.tsx
+++ src/components/crud/list/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Button, Card } from "antd";
+import { Button, Card, Row, Col } from "antd";
 import { PlusSquareOutlined } from "@ant-design/icons";
 import humanizeString from "humanize-string";
 
 import { TableProps } from "@components/table";
+
 export interface ListProps {
     resourceName: string;
     canCreate?: boolean;
     canEdit?: boolean;
     canDelete?: boolean;
+    aside?: React.FC | false;
 }
 
 export const List: React.FC<ListProps> = ({
@@ -17,6 +19,7 @@ export const List: React.FC<ListProps> = ({
     canCreate,
     canEdit,
     canDelete,
+    aside,
     children,
 }) => {
     const history = useHistory();
@@ -33,24 +36,32 @@ export const List: React.FC<ListProps> = ({
     });
 
     return (
-        <Card
-            bodyStyle={{ padding: 0 }}
-            title={humanizeString(resourceName)}
-            extra={
-                canCreate && (
-                    <Button
-                        onClick={(): void =>
-                            history.push(`/resources/${resourceName}/create`)
-                        }
-                        type="default"
-                        icon={<PlusSquareOutlined />}
-                    >
-                        Create
-                    </Button>
-                )
-            }
-        >
-            {childrenWithProps}
-        </Card>
+        <Row>
+            <Col flex={1}>
+                <Card
+                    bodyStyle={{ padding: 0, flex: 1 }}
+                    title={humanizeString(resourceName)}
+                    extra={
+                        canCreate && (
+                            <Button
+                                onClick={(): void =>
+                                    history.push(
+                                        `/resources/${resourceName}/create`,
+                                    )
+                                }
+                                type="default"
+                                icon={<PlusSquareOutlined />}
+                            >
+                                Create
+                            </Button>
+                        )
+                    }
+                >
+                    {childrenWithProps}
+                </Card>
+            </Col>
+
+            {aside && <Col style={{ width: 200 }}>{aside}</Col>}
+        </Row>
     );
 };
