bd0df09c949572bed53c2cf2d89934cbebdade4a
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-02-24 16:44:16 +0300

    Add type on normalizeFile response.

--- src/definitions/upload.ts
+++ src/definitions/upload.ts
@@ -14,12 +14,12 @@ export const normalizeFile = (event: Event) => {
 
     return fileList.map((item) => {
         let { url } = item;
-        const { uid, name, response } = item;
+        const { uid, name, response, type } = item;
 
         if (response) {
             url = response.fileUrl;
         }
 
-        return { uid, name, url };
+        return { uid, name, url, type };
     });
 };
