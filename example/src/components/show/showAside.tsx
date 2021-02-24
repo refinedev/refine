92d53f0f97095d64d99a65bd23b335ffaa10b5a8
Muharrem <mkocadere@gmail.com>
2021-02-24 15:22:50 +0300

    ifx prettier

--- example/src/components/show/showAside.tsx
+++ example/src/components/show/showAside.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Card } from "antd";
 
 export const ShowAside: React.FC<{ record: any }> = ({ record }) => {
- 
     return (
         <Card
             style={{
