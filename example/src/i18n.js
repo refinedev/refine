68c54b70ee5fa201a65a9678c6072f8f7bc4654b
necatiozmen <necatiozmn@gmail.com>
2021-02-24 12:27:34 +0300

    wrap app with i18nextProvider

--- example/src/i18n.js
+++ example/src/i18n.js
@@ -6,7 +6,7 @@ import { initReactI18next } from "react-i18next";
 const resources = {
     en: {
         translation: {
-            "Welcome to React": "Welcome to React and react-i18next",
+            "Welcome to React": "Welcome to React and react-i18next 222",
         },
     },
     fr: {
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <I18nextProvider i18n={i18n}>
+            <App />
+        </I18nextProvider>
     </React.StrictMode>,
     document.getElementById("root"),
 );
