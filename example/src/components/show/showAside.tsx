c87cea1800c03c5ab26a3dd5bf46651635718cc9
mhrrmk <mkocadere@gmail.com>
2021-02-24 14:44:02 +0300

    Update example/src/components/show/showAside.tsx
    Co-authored-by: Necati Özmen <necati@pankod.com>

--- example/src/components/show/showAside.tsx
+++ example/src/components/show/showAside.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Card } from "antd";
 
 export const ShowAside: React.FC<{ record: any }> = ({ record }) => {
-    console.log("record: ", record);
+ 
     return (
         <Card
             style={{
