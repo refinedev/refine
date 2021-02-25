1100a1f184d11a62b1d66e09a095685b1ac27513
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-02-25 11:53:24 +0300

    add TextAreaProps

--- src/components/input/markdownInput/index.tsx
+++ src/components/input/markdownInput/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Input } from "antd";
+import { TextAreaProps } from "antd/lib/input";
 
-export const MarkdownInput: React.FC = ({ ...rest }) => {
+export const MarkdownInput: React.FC<TextAreaProps> = ({ ...rest }) => {
     return <Input.TextArea {...rest} />;
 };
