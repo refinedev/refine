557212d4edd552700abb6eac69d3a6323941496d
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-02-25 23:07:51 +0300

    update test name

--- src/components/fields/markdownField/index.spec.tsx
+++ src/components/fields/markdownField/index.spec.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import { render } from "@test";
 import { MarkdownField } from "./";
 
-describe("BooleanField", () => {
+describe("MarkdownField", () => {
     it("renders markDown text with correct value", () => {
         const { getByText, container } = render(
             <div data-testid="custom-field">
-                <MarkdownField value={`**BooleanField Test**`} />
+                <MarkdownField value={`**MarkdownField Test**`} />
             </div>,
         );
 
         expect(container.querySelector("strong")).toBeTruthy();
-        getByText("BooleanField Test");
+        getByText("MarkdownField Test");
     });
 });
