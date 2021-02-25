a5785fe0bacf0f968fe742853e4e62f3981c89e1
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-02-25 12:18:53 +0300

    Fixed example proxy.

--- example/src/setupProxy.js
+++ example/src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const createProxyMiddleware = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
