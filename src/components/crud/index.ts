a0aea31fc68e82fadaeb80c0006d275950bdfed2
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-02-26 14:12:54 +0300

    delete form

--- src/components/crud/index.ts
+++ src/components/crud/index.ts
@@ -1,5 +1,4 @@
 export { List } from "./list";
 export { Create } from "./create";
 export { Edit } from "./edit";
-export { Form } from "./form";
 export { Show, ShowSimple, ShowTab, Tab } from "./show";
