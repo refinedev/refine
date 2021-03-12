ef76b5d7a6305a083205becb568a80f4ef5110bb
necatiozmen <necatiozmn@gmail.com>
2021-03-12 15:54:23 +0300

    Update useCreate

--- src/hooks/data/useCreate.spec.tsx
+++ src/hooks/data/useCreate.spec.tsx
@@ -6,16 +6,27 @@ import { useCreate } from "./useCreate";
 
 describe("useCreate Hook", () => {
     it("with rest json server", async () => {
-        const { result, waitForNextUpdate } = renderHook(() => useCreate(), {
-            wrapper: TestWrapper({
-                dataProvider: MockJSONServer,
-                resources: [{ name: "posts" }],
-            }),
-        });
+        const { result, waitForNextUpdate, waitFor } = renderHook(
+            () => useCreate(),
+            {
+                wrapper: TestWrapper({
+                    dataProvider: MockJSONServer,
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
 
         result.current.mutate({ resource: "posts", values: { id: 1 } });
+
         await waitForNextUpdate();
 
-        expect(result.current.isLoading).toBeTruthy();
+        await waitFor(() => {
+            return result.current.isSuccess;
+        });
+
+        const { status, data } = result.current;
+
+        expect(status).toBe("success");
+        expect(data?.data.slug).toBe("ut-ad-et");
     });
 });
