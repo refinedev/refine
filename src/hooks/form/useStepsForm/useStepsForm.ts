5916e5fb5cbf72df7e97036015cba7f0a093e821
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-03-16 15:51:23 +0300

    add submit with onFinish to useStepsForm

--- src/hooks/form/useStepsForm/useStepsForm.ts
+++ src/hooks/form/useStepsForm/useStepsForm.ts
@@ -1,19 +1,23 @@
 import { useStepsForm as useStepsFormSF } from "sunflower-antd";
 
+import { Form } from "antd";
+
 import { useForm } from "@hooks";
+import { useEditForm } from "../useEditForm";
 
 type useStepsProps = {};
 
 export const useStepsForm = () => {
-    const { form, formProps } = useForm({});
+    const form = useForm({});
 
-    const stepsPropsSunflower = useStepsFormSF({ form, submit: () => {} });
+    const stepsPropsSunflower = useStepsFormSF({
+        form: form.form,
+        submit: (values) => {
+            form.formProps?.onFinish(values as any);
+        },
+    });
 
     return {
         ...stepsPropsSunflower,
-        formProps: {
-            ...stepsPropsSunflower.formProps,
-            onFinish: formProps?.onFinish,
-        },
     };
 };
