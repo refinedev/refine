d6a4a3b324d7483a01344c70a0e7e0f8124087f5
necatiozmen <necatiozmn@gmail.com>
2021-03-17 17:52:27 +0300

    Add useModalForm

---
+++ src/hooks/form/useModalForm/useModalForm.ts
@@ -0,0 +1,34 @@
+import { useModalForm as useModalFormSF } from "sunflower-antd";
+import { useForm } from "@hooks";
+
+type useModalFormProps = {
+    action: "show" | "edit" | "create";
+};
+// export const useModalForm = (action) => {
+export const useModalForm = ({ action }: useModalFormProps) => {
+    const { form, formProps, setEditId, isLoading, saveButtonProps } = useForm({
+        action,
+    });
+
+    const sunflowerUseModal = useModalFormSF({
+        form: form,
+        submit: (values) => formProps?.onFinish(values as any),
+    });
+
+    console.log("form", form);
+
+    return {
+        ...sunflowerUseModal,
+        show: (id: string | number) => {
+            setEditId && setEditId(id);
+            sunflowerUseModal.show();
+        },
+        formProps: {
+            ...sunflowerUseModal.formProps,
+            onValuesChange: formProps?.onValuesChange,
+            onKeyUp: formProps?.onKeyUp,
+        },
+        isLoading: isLoading,
+        saveButtonProps: saveButtonProps,
+    };
+};
