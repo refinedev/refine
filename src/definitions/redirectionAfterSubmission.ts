2de55545af572cd53044b168f07e27d6c5a1d762
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-03-18 09:51:17 +0300

    add useCallback for handleSubmitWithRedirect

--- src/definitions/redirectionAfterSubmission.ts
+++ src/definitions/redirectionAfterSubmission.ts
@@ -1,31 +1,31 @@
+import { useCallback } from "react";
 import { useHistory } from "react-router-dom";
 
 export const redirectionAfterSubmission = () => {
     const history = useHistory();
 
-    const handleSubmitWithRedirect = ({
-        redirect,
-        resource,
-        idFromRoute,
-    }: any) => {
-        if (redirect) {
-            if (resource.canShow && redirect === "show") {
-                return history.push(
-                    `/resources/${resource.route}/show/${idFromRoute}`,
-                );
-            }
+    const handleSubmitWithRedirect = useCallback(
+        ({ redirect, resource, idFromRoute }: any) => {
+            if (redirect) {
+                if (resource.canShow && redirect === "show") {
+                    return history.push(
+                        `/resources/${resource.route}/show/${idFromRoute}`,
+                    );
+                }
 
-            if (resource.canEdit && redirect === "edit") {
-                return history.push(
-                    `/resources/${resource.route}/edit/${idFromRoute}`,
-                );
-            }
+                if (resource.canEdit && redirect === "edit") {
+                    return history.push(
+                        `/resources/${resource.route}/edit/${idFromRoute}`,
+                    );
+                }
 
-            return history.push(`/resources/${resource.route}`);
-        } else {
-            return;
-        }
-    };
+                return history.push(`/resources/${resource.route}`);
+            } else {
+                return;
+            }
+        },
+        [],
+    );
 
     return handleSubmitWithRedirect;
 };
