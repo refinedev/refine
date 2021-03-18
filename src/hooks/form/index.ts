b47241dba9f4e10731012b6bc21057b91d992d90
Muharrem Kocadere <mkocadere@gmail.com>
2021-03-18 09:44:34 +0300

    fix import

--- src/hooks/form/index.ts
+++ src/hooks/form/index.ts
@@ -1,6 +1,4 @@
 export { useForm } from "./useForm";
 export * from "./useCreateForm";
 export * from "./useEditForm";
-export { useCreateForm, useCreateFormProps } from "./useCreateForm";
-export { useEditForm, useEditFormProps } from "./useEditForm";
 export { useStepsForm } from "./useStepsForm";
