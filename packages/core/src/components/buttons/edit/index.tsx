47fd416ae5adefb9da4ae1b25edd21c250c03847
Omer Faruk APLAK <omeraplak@gmail.com>
2021-03-25 14:25:13 +0300

    Squashed commit of the following:
    commit 4c88f19034dba59517dee0770d47986a5aa08d31
    Author: Omer Faruk APLAK <omeraplak@gmail.com>
    Date:   Thu Mar 25 10:47:41 2021 +0300
    
        rename button folders xButton to x

---
+++ packages/core/src/components/buttons/edit/index.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from "react";
+import { Button, ButtonProps } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import { useHistory, useParams } from "react-router-dom";
+
+import { useTranslate } from "@hooks";
+import { ResourceRouterParams } from "@interfaces";
+
+type EditButtonProps = ButtonProps & {
+    resourceName?: string;
+    recordItemId?: string | number;
+};
+
+export const EditButton: FC<EditButtonProps> = ({
+    resourceName: propResourceName,
+    recordItemId,
+    ...rest
+}) => {
+    const history = useHistory();
+    const translate = useTranslate();
+
+    const {
+        resource: routeResourceName,
+        id: idFromRoute,
+    } = useParams<ResourceRouterParams>();
+
+    const resourceName = propResourceName ?? routeResourceName;
+
+    return (
+        <Button
+            onClick={(): void => {
+                history.push(
+                    `/resources/${resourceName}/edit/${
+                        recordItemId ?? idFromRoute
+                    }`,
+                );
+            }}
+            type="default"
+            icon={<EditOutlined />}
+            {...rest}
+        >
+            {translate("common:buttons.edit", "Edit")}
+        </Button>
+    );
+};
---
+++ packages/core/src/components/buttons/export/index.tsx
@@ -0,0 +1,114 @@
+import React, { FC, useContext, useState, MouseEventHandler } from "react";
+import { Button, ButtonProps } from "antd";
+import { ExportOutlined } from "@ant-design/icons";
+import { useParams } from "react-router-dom";
+import { CSVDownload } from "react-csv";
+
+import { useTranslate } from "@hooks";
+import {
+    ResourceRouterParams,
+    Sort,
+    Filters,
+    IDataContext,
+    BaseRecord,
+} from "@interfaces";
+import { DataContext } from "@contexts/data";
+import { CSVDownloadProps } from "./csvDownload.interface";
+
+type ExportButtonProps = ButtonProps & {
+    resourceName?: string;
+    sorter?: Sort;
+    filters?: Filters;
+    maxItemCount?: number;
+    pageSize?: number;
+    mapData?(value: BaseRecord, index: number, array: BaseRecord[]): BaseRecord;
+    csvProps?: CSVDownloadProps;
+};
+
+export const ExportButton: FC<ExportButtonProps> = ({
+    resourceName,
+    sorter,
+    filters,
+    maxItemCount,
+    pageSize = 20,
+    mapData,
+    csvProps,
+    ...rest
+}) => {
+    const translate = useTranslate();
+
+    const [loading, setLoading] = useState(false);
+    const [fileReady, setFileReady] = useState(false);
+    const [exportData, setExportData] = useState<BaseRecord[]>([]);
+
+    let { resource } = useParams<ResourceRouterParams>();
+
+    if (resourceName) {
+        resource = resourceName;
+    }
+
+    const { getList } = useContext<IDataContext>(DataContext);
+
+    const fetchData = async () => {
+        setLoading(true);
+        setFileReady(false);
+        setExportData([]);
+
+        const rawData: BaseRecord[] = [];
+
+        let current = 1;
+        let preparingData = true;
+        while (preparingData) {
+            const { data } = await getList(resource, {
+                filters,
+                sort: sorter,
+                pagination: {
+                    current,
+                    pageSize,
+                },
+            });
+
+            current++;
+
+            if (data.length > 0) {
+                rawData.push(...data);
+
+                if (maxItemCount && rawData.length >= maxItemCount) {
+                    rawData.slice(0, maxItemCount);
+                    preparingData = false;
+                }
+
+                continue;
+            }
+
+            preparingData = false;
+        }
+
+        setExportData(mapData ? rawData.map(mapData) : rawData);
+        setFileReady(true);
+        setLoading(false);
+    };
+
+    const downloadFile = () => {
+        if (fileReady) {
+            return <CSVDownload {...csvProps} data={exportData} />;
+        }
+
+        return;
+    };
+
+    return (
+        <>
+            {downloadFile()}
+            <Button
+                onClick={fetchData}
+                type="default"
+                icon={<ExportOutlined />}
+                loading={loading}
+                {...rest}
+            >
+                {translate("common:buttons.export", "Export")}
+            </Button>
+        </>
+    );
+};
