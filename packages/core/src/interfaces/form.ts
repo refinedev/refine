569a99b9c5288f07a9f78558936926a64c178b98
Muharrem Kocadere <mkocadere@gmail.com>
2021-03-26 16:24:36 +0300

    add sunflower form type

--- packages/core/src/interfaces/form.ts
+++ packages/core/src/interfaces/form.ts
@@ -1,6 +1,10 @@
-import { useForm } from "sunflower-antd";
+import { useForm, useStepsForm } from "sunflower-antd";
 import { FormInstance } from "antd";
 
 export type FormSF = Omit<ReturnType<typeof useForm>, "form"> & {
+    form: FormInstance;
+};
+
+export type StepsFormSF = Omit<ReturnType<typeof useStepsForm>, "form"> & {
     form: FormInstance;
 };
