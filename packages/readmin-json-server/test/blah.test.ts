cc8c1dddc83c1cb8840f466d13b410c69d9a6546
Omer Faruk APLAK <omeraplak@gmail.com>
2021-03-29 18:13:05 +0300

    prettier fix example test

--- packages/readmin-json-server/test/blah.test.ts
+++ packages/readmin-json-server/test/blah.test.ts
@@ -1,7 +1,7 @@
-import { sum } from '../src';
+import { sum } from "../src";
 
-describe('blah', () => {
-    it('works', () => {
+describe("blah", () => {
+    it("works", () => {
         expect(sum(1, 1)).toEqual(2);
     });
 });
