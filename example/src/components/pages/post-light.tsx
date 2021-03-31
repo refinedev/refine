780c829b474bb1959544e6c75ae9e89470103b82
necatiozmen <necatiozmn@gmail.com>
2021-03-31 17:56:28 +0300

    Add cloneButton to example

--- example/src/components/pages/post-light.tsx
+++ example/src/components/pages/post-light.tsx
@@ -20,6 +20,7 @@ import {
     useModalForm,
     ExportButton,
     CreateButton,
+    CloneButton,
 } from "readmin";
 
 export const PostLightList = (props: any) => {
@@ -62,7 +63,7 @@ export const PostLightList = (props: any) => {
                     };
                 }}
             />
-            <CreateButton />
+            <CreateButton onClick={() => createModalShow()} />
         </Space>
     );
 
@@ -134,6 +135,12 @@ export const PostLightList = (props: any) => {
                                 />
                                 <ShowButton
                                     size="small"
+                                    recordItemId={record.id}
+                                />
+                                <CloneButton
+                                    onClick={() => {
+                                        createModalShow(record.id);
+                                    }}
                                     recordItemId={record.id}
                                 />
                             </Space>
