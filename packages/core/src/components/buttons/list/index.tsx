144c2827ee046adf605354da462b29c3d4ae5b65
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-04-05 15:05:10 +0300

    missing translates completed

--- packages/core/src/components/buttons/list/index.tsx
+++ packages/core/src/components/buttons/list/index.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
 import { Button, ButtonProps } from "antd";
 import { BarsOutlined } from "@ant-design/icons";
-import { useHistory, useParams, useRouteMatch } from "react-router-dom";
-
-import { MatchRoute, ResourceRouterParams } from "../../../interfaces";
+import { useHistory, useParams } from "react-router-dom";
 import humanizeString from "humanize-string";
+
 import { useTranslate } from "@hooks";
+import { ResourceRouterParams } from "../../../interfaces";
 
 type ListButtonProps = ButtonProps & {
     resourceName?: string;
