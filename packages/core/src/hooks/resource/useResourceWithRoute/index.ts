7fe45f81bce546d93e1c50b7e2f5aa03bbee2bdc
Burcu Karagöz <burcukaragoz1515@gmail.com>
2021-04-07 10:31:38 +0300

    minor fix useCallback

--- packages/core/src/hooks/resource/useResourceWithRoute/index.ts
+++ packages/core/src/hooks/resource/useResourceWithRoute/index.ts
@@ -4,14 +4,17 @@ import { ResourceContext } from "@contexts/resource";
 export const useResourceWithRoute = () => {
     const { resources } = useContext(ResourceContext);
 
-    const resourceWithRoute = useCallback((route: string) => {
-        const resource = resources.find((p) => p.route === route);
+    const resourceWithRoute = useCallback(
+        (route: string) => {
+            const resource = resources.find((p) => p.route === route);
 
-        if (!resource) {
-            throw new Error(`'${route}' not found on resources."`);
-        }
-        return resource;
-    }, []);
+            if (!resource) {
+                throw new Error(`'${route}' not found on resources."`);
+            }
+            return resource;
+        },
+        [resources],
+    );
 
     return resourceWithRoute;
 };
