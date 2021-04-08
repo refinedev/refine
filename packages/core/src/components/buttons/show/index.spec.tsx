1769e19a745e7bc7c3f9a82084c52ff6d0ebc127
burcu karagoz <burcukaragoz1515@gmail.com>
2021-04-08 11:56:47 +0300

    add show button tests

---
+++ packages/core/src/components/buttons/show/index.spec.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { fireEvent, render, TestWrapper } from "@test";
+import { ShowButton } from "./";
+
+describe("Show Button", () => {
+    const show = jest.fn();
+
+    it("should render button successfuly", () => {
+        const showButton = render(<ShowButton onClick={() => show()} />, {
+            wrapper: TestWrapper({
+                resources: [{ name: "posts" }],
+            }),
+        });
+        const { container, getByText } = showButton;
+
+        expect(container).toBeTruthy();
+
+        getByText("Show");
+    });
+
+    it("should render called function successfully if click the button", () => {
+        const showButton = render(<ShowButton onClick={() => show()} />, {
+            wrapper: TestWrapper({
+                resources: [{ name: "posts" }],
+            }),
+        });
+        const { getByText } = showButton;
+
+        fireEvent.click(getByText("Show"));
+
+        expect(show).toHaveBeenCalledTimes(1);
+    });
+});
