78b371391e5f2fe8cd2b8c706da3ab46dcb30b70
burcu karagoz <burcukaragoz1515@gmail.com>
2021-04-08 12:07:28 +0300

    add delete button tests

---
+++ packages/core/src/components/buttons/delete/index.spec.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { fireEvent, render, TestWrapper } from "@test";
+import { DeleteButton } from "./";
+
+describe("Delete Button", () => {
+    const deleteFunc = jest.fn();
+
+    it("should render button successfuly", () => {
+        const deleteButton = render(
+            <DeleteButton onClick={() => deleteFunc()} />,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+        const { container, getByText } = deleteButton;
+
+        expect(container).toBeTruthy();
+
+        getByText("Delete");
+    });
+
+    it("should render called function successfully if click the button", () => {
+        const deleteButton = render(
+            <DeleteButton onClick={() => deleteFunc()} />,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+        const { getByText } = deleteButton;
+
+        fireEvent.click(getByText("Delete"));
+
+        expect(deleteFunc).toHaveBeenCalledTimes(1);
+    });
+
+    it("should render Popconfirm successfuly", () => {
+        const deleteButton = render(
+            <DeleteButton onClick={() => deleteFunc()} />,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+        const { getByText, getAllByText } = deleteButton;
+
+        fireEvent.click(getByText("Delete"));
+
+        getByText("Are you sure?");
+        getByText("Cancel");
+        getAllByText("Delete");
+    });
+});
