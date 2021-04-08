0f515591e9d85887e688bb1912209e75e4bb124a
burcu karagoz <burcukaragoz1515@gmail.com>
2021-04-08 12:24:54 +0300

    add export button tests

---
+++ packages/core/src/components/buttons/export/index.spec.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { fireEvent, render, TestWrapper } from "@test";
+import { ExportButton } from "./";
+
+describe("Export Button", () => {
+    const fetchData = jest.fn();
+
+    it("should render button successfuly", () => {
+        const exportButton = render(
+            <ExportButton onClick={() => fetchData()} />,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+        const { container, getByText } = exportButton;
+
+        expect(container).toBeTruthy();
+
+        getByText("Export");
+    });
+
+    it("should render called function successfully if click the button", () => {
+        const exportButton = render(
+            <ExportButton onClick={() => fetchData()} />,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+        const { getByText } = exportButton;
+
+        fireEvent.click(getByText("Export"));
+
+        expect(fetchData).toHaveBeenCalledTimes(1);
+    });
+});
