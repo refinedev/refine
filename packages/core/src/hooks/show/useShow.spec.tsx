73bb0623b08415e854c42363d37618e26b88496f
Salih <salihozdemir94@gmail.com>
2021-04-14 16:22:49 +0300

    added useShow test

---
+++ packages/core/src/hooks/show/useShow.spec.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { renderHook } from "@testing-library/react-hooks";
+import { Route } from "react-router-dom";
+
+import { MockJSONServer, TestWrapper } from "@test";
+import { posts } from "@test/dataMocks";
+
+import { useShow } from "./useShow";
+
+const Wrapper = TestWrapper({
+    dataProvider: MockJSONServer,
+    resources: [{ name: "posts", route: "posts" }],
+    routerInitialEntries: ["/resources/posts/show/1"],
+});
+
+const WrapperWithRoute: React.FC = ({ children }) => (
+    <Wrapper>
+        <Route path="/resources/:resource/:action/:id">{children}</Route>
+    </Wrapper>
+);
+describe("useEditForm Hook", () => {
+    it("should fetch data with use-query params succesfully", async () => {
+        const { result, waitFor } = renderHook(() => useShow({}), {
+            wrapper: WrapperWithRoute,
+        });
+
+        await waitFor(() => {
+            return !result.current.queryResult.data;
+        });
+
+        expect(result.current.queryResult.data?.data.id).toEqual(posts[0].id);
+    });
+});
