0978c838a15660a905d5063d4d0d185588d31b8c
Muharrem Kocadere <mkocadere@gmail.com>
2021-04-15 17:40:19 +0300

    remove unnecessary files.
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>

--- adminayna/src/components/pages/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import { useTranslate } from "readmin";
-
-export const DashboardPage: React.FC = () => {
-    const translate = useTranslate();
-
-    return <div>{translate("common:resources.dashboard.title")}</div>;
-};
