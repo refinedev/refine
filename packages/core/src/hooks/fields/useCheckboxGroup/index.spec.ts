7702fbeef4f89d9bd7cb111f2a9f03c47ffbd7b2
Salih <salihozdemir94@gmail.com>
2021-04-15 13:32:36 +0300

    useCheckboxGroup hook test added

---
+++ packages/core/src/hooks/fields/useCheckboxGroup/index.spec.ts
@@ -0,0 +1,30 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import { MockJSONServer, TestWrapper } from "@test";
+
+import { useCheckboxGroup } from "./";
+
+describe("useCheckboxGroup Hook", () => {
+    it("default", async () => {
+        const { result, waitFor } = renderHook(
+            () =>
+                useCheckboxGroup({
+                    resource: "posts",
+                }),
+            {
+                wrapper: TestWrapper({
+                    dataProvider: MockJSONServer,
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+
+        await waitFor(() => {
+            return !result.current.value;
+        });
+
+        const { options } = result.current;
+
+        expect(options).toHaveLength(2);
+    });
+});
