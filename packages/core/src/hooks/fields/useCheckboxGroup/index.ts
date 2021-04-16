058bc9de6d89f8f55bb3415bfdc11c9986ff20d2
Salih <salihozdemir94@gmail.com>
2021-04-16 12:08:29 +0300

    add defaultValue prop to useCheckboxGroup

--- packages/core/src/hooks/fields/useCheckboxGroup/index.ts
+++ packages/core/src/hooks/fields/useCheckboxGroup/index.ts
@@ -1,7 +1,7 @@
 import React from "react";
 import { CheckboxGroupProps } from "antd/lib/checkbox";
 
-import { useList } from "@hooks";
+import { useList, useMany } from "@hooks";
 import { Sort, Option } from "../../../interfaces";
 
 export type useCheckboxGroupProps = {
@@ -9,6 +9,7 @@ export type useCheckboxGroupProps = {
     optionLabel?: string;
     optionValue?: string;
     sort?: Sort;
+    defaultValue?: string | string[];
 };
 
 export const useCheckboxGroup = ({
@@ -16,8 +17,28 @@ export const useCheckboxGroup = ({
     sort,
     optionLabel = "title",
     optionValue = "id",
+    ...rest
 }: useCheckboxGroupProps): CheckboxGroupProps => {
     const [options, setOptions] = React.useState<Option[]>([]);
+
+    let { defaultValue = [] } = rest;
+
+    if (!Array.isArray(defaultValue)) {
+        defaultValue = [defaultValue];
+    }
+
+    useMany(resource, defaultValue, {
+        enabled: defaultValue.length > 0,
+        onSuccess: (data) => {
+            setOptions((current) => [
+                ...current,
+                ...data.data.map((item) => ({
+                    label: item[optionLabel],
+                    value: item[optionValue],
+                })),
+            ]);
+        },
+    });
 
     useList(
         resource,
