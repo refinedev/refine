ad7a1359ce8a3c66509b8cd26bd0394f017b3be1
Umut Özdemir <umutozdemir997@gmail.com>
2021-04-22 16:30:29 +0300

    fix error in tag component test file

--- packages/core/src/components/fields/tag/index.spec.tsx
+++ packages/core/src/components/fields/tag/index.spec.tsx
@@ -5,7 +5,7 @@ import { TagField } from "./";
 import { render } from "@test";
 describe("TagField", () => {
     it("renders boolean values correctly", () => {
-        const { getByText } = render(<TagField value={123} />);
+        const { getByText } = render(<TagField value={true} />);
 
         getByText("true");
     });
