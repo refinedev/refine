f132a5b7503d08bdc5bf19d1dc22cd08008824f8
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-04-27 12:55:46 +0300

    Add base tests.

---
+++ packages/strapi-auth/test/login/index.mock.ts
@@ -0,0 +1,1 @@
+import nock from "nock";
---
+++ packages/strapi-auth/test/login/index.spec.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// import AuthProvider from "../../src/index";
+// import "./index.mock";
+
+axios.defaults.adapter = require("axios/lib/adapters/http");
+
+describe("login", () => {
+    it("correct response", async () => {
+        // TODO: Add tests.
+    });
+});
