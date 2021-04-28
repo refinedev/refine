380affd28f67872a215743dac59ac7087eefe1bc
Salih Özdemir <salihozdemir94@gmail.com>
2021-04-28 18:52:46 +0300

    extract css file (#396)
    * extract css file
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    * remove unneccesary style file
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>

--- packages/core/src/components/antd/datePicker.tsx
+++ packages/core/src/components/antd/datePicker.tsx
@@ -5,7 +5,6 @@ import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
 import generatePicker, {
     PickerProps,
 } from "antd/es/date-picker/generatePicker";
-import "antd/es/date-picker/style/index";
 
 const DayJSDatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
 
