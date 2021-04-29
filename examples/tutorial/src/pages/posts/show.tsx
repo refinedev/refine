24e06a11bb92be200406b1040796b6d35ec90448
Muharrem Kocadere <mkocadere@gmail.com>
2021-04-29 15:03:13 +0300

    add category to show

--- examples/tutorial/src/pages/posts/show.tsx
+++ examples/tutorial/src/pages/posts/show.tsx
@@ -1,4 +1,4 @@
-import { Show, useShow, Typography, Tag } from "@pankod/refine";
+import { Show, useShow, Typography, Tag, useOne } from "@pankod/refine";
 
 const { Title, Text } = Typography;
 
@@ -7,6 +7,10 @@ export const PostShow = () => {
     const { data, isLoading } = queryResult;
     const record = data?.data;
 
+    const {data: categoryData} = useOne("categories", record?.category.id?.toString() ?? "", {
+        enabled: !!record?.category.id
+    })
+
     return (
         <Show isLoading={isLoading}>
             <Title level={5}>Title</Title>
@@ -14,6 +18,9 @@ export const PostShow = () => {
 
             <Title level={5}>Status</Title>
             <Tag>{record?.status}</Tag>
+
+            <Title level={5}>Category</Title>
+            <Text>{categoryData?.data.title}</Text>
         </Show>
     );
 };
