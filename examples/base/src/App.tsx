c17f51d2b5bec53dcba2e2da8e3cec3f559a64ec
Salih Özdemir <salihozdemir94@gmail.com>
2021-04-30 09:37:24 +0300

    Example boilerplate (#399)
    * create draft for example task
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>
    
    * add return type to useTable and add default pagination props
    
    * refactor resource pages
    
    * fix default pagination props
    
    * add return type
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>
    
    * refactor base example
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>
    
    * refactor base example
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>
    
    * fix useTable return type
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>
    
    * fix interface
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>
    
    * add markdownfield
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>
    
    Co-authored-by: Umut Özdemir <umutozdemir997@gmail.com>

---
+++ examples/base/src/App.tsx
@@ -0,0 +1,22 @@
+import { Admin, Resource } from "@pankod/refine";
+import dataProvider from "@pankod/refine-json-server";
+
+import { PostList, PostCreate, PostEdit, PostShow } from "./pages/posts";
+
+const API_URL = "https://refine-fake-rest.pankod.com";
+
+const App = () => {
+    return (
+        <Admin dataProvider={dataProvider(API_URL)}>
+            <Resource
+                name="posts"
+                list={PostList}
+                create={PostCreate}
+                edit={PostEdit}
+                show={PostShow}
+            />
+        </Admin>
+    );
+};
+
+export default App;
