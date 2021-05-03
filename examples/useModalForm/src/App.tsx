6669ddb7ea33e5a7e04525a7b8add2221db3542c
Salih <salihozdemir94@gmail.com>
2021-05-03 15:59:38 +0300

    absolute paths

--- examples/useModalForm/src/App.tsx
+++ examples/useModalForm/src/App.tsx
@@ -2,7 +2,7 @@ import { Admin, Resource } from "@pankod/refine";
 import dataProvider from "@pankod/refine-json-server";
 import "@pankod/refine/dist/styles.min.css";
 
-import { PostList } from "./pages/posts";
+import { PostList } from "pages/posts";
 
 const API_URL = "https://refine-fake-rest.pankod.com";
 
