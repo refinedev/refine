20c1b247cfc51b507203f750d30d70764844057e
Salih <salihozdemir94@gmail.com>
2021-05-03 12:36:13 +0300

    remove unused files

--- examples/useModalForm/src/App.tsx
+++ examples/useModalForm/src/App.tsx
@@ -2,20 +2,14 @@ import { Admin, Resource } from "@pankod/refine";
 import dataProvider from "@pankod/refine-json-server";
 import "@pankod/refine/dist/styles.min.css";
 
-import { PostList, PostCreate, PostEdit, PostShow } from "./pages/posts";
+import { PostList } from "./pages/posts";
 
 const API_URL = "https://refine-fake-rest.pankod.com";
 
 const App = () => {
     return (
         <Admin dataProvider={dataProvider(API_URL)}>
-            <Resource
-                name="posts"
-                list={PostList}
-                create={PostCreate}
-                edit={PostEdit}
-                show={PostShow}
-            />
+            <Resource name="posts" list={PostList} />
         </Admin>
     );
 };
