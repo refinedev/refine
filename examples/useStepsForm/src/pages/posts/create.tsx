dd6b39a6380880ab83051cbfbdafc682ced73585
Umut Özdemir <umutozdemir997@gmail.com>
2021-05-03 14:34:20 +0300

    add example base files

---
+++ examples/useStepsForm/src/pages/posts/create.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+    Create,
+    Form,
+    Input,
+    IResourceComponentsProps,
+    Select,
+    useForm,
+    useSelect,
+} from "@pankod/refine";
+
+import ReactMarkdown from "react-markdown";
+import ReactMde from "react-mde";
+
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+import { IPost, ICategory } from "../../interfaces";
+
+export const PostCreate = (props: IResourceComponentsProps) => {
+    const { formProps, saveButtonProps } = useForm<IPost>();
+
+    const { selectProps: categorySelectProps } = useSelect<ICategory>({
+        resource: "categories",
+    });
+
+    const [selectedTab, setSelectedTab] = React.useState<"write" | "preview">(
+        "write",
+    );
+
+    return (
+        <Create {...props} saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Category"
+                    name={["category", "id"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Select
+                        showSearch
+                        filterOption={false}
+                        {...categorySelectProps}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Status"
+                    name="status"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Select
+                        options={[
+                            {
+                                label: "Published",
+                                value: "published",
+                            },
+                            {
+                                label: "Draft",
+                                value: "draft",
+                            },
+                        ]}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Content"
+                    name="content"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <ReactMde
+                        selectedTab={selectedTab}
+                        onTabChange={setSelectedTab}
+                        generateMarkdownPreview={(markdown) =>
+                            Promise.resolve(
+                                <ReactMarkdown>{markdown}</ReactMarkdown>,
+                            )
+                        }
+                    />
+                </Form.Item>
+            </Form>
+        </Create>
+    );
+};
