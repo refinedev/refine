7727cde2ce9984a2460221826f80b7c5c1a03739
Salih <salihozdemir94@gmail.com>
2021-05-05 11:35:37 +0300

    add baseUrl to base example

--- examples/base/src/App.tsx
+++ examples/base/src/App.tsx
@@ -2,7 +2,7 @@ import { Admin, Resource } from "@pankod/refine";
 import dataProvider from "@pankod/refine-json-server";
 import "@pankod/refine/dist/styles.min.css";
 
-import { PostList, PostCreate, PostEdit, PostShow } from "./pages/posts";
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
 
 const API_URL = "https://refine-fake-rest.pankod.com";
 
