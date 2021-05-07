7030ef286baaa235779fb3809153f22b5c02d629
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-05-07 16:53:22 +0300

    Add basic login for auth0.

--- examples/auth0/src/App.tsx
+++ examples/auth0/src/App.tsx
@@ -1,22 +1,50 @@
 import { Admin, Resource } from "@pankod/refine";
 import dataProvider from "@pankod/refine-json-server";
 import "@pankod/refine/dist/styles.min.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
 
 const API_URL = "https://refine-fake-rest.pankod.com";
 
 const App = () => {
+    const {
+        loginWithRedirect,
+        isLoading,
+        isAuthenticated,
+        user,
+        logout,
+    } = useAuth0();
+
+    console.log("isLoading", isLoading);
+    console.log("isAuthenticated", isAuthenticated);
+    console.log("user", user);
+
+    if (isLoading) {
+        return <span>loding....</span>;
+    }
+
     return (
-        <Admin dataProvider={dataProvider(API_URL)}>
-            <Resource
-                name="posts"
-                list={PostList}
-                create={PostCreate}
-                edit={PostEdit}
-                show={PostShow}
-            />
-        </Admin>
+        <>
+            {isAuthenticated ? (
+                <>
+                    {JSON.stringify(user)}
+                    <button onClick={() => logout()}>logout</button>
+                </>
+            ) : (
+                <button onClick={() => loginWithRedirect()}>login</button>
+            )}
+
+            <Admin dataProvider={dataProvider(API_URL)}>
+                <Resource
+                    name="posts"
+                    list={PostList}
+                    create={PostCreate}
+                    edit={PostEdit}
+                    show={PostShow}
+                />
+            </Admin>
+        </>
     );
 };
 
