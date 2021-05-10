35f379cc109107972200d53b6d8fb2be33caae24
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-05-10 14:43:06 +0300

    Add authProvider.

--- examples/auth0/src/App.tsx
+++ examples/auth0/src/App.tsx
@@ -1,41 +1,52 @@
-import { Admin, Resource } from "@pankod/refine";
+import { Admin, Resource, AuthProvider, Spin } from "@pankod/refine";
 import dataProvider from "@pankod/refine-json-server";
 import "@pankod/refine/dist/styles.min.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import { Login } from "pages/login";
 
 const API_URL = "https://refine-fake-rest.pankod.com";
 
 const App = () => {
-    const {
-        loginWithRedirect,
-        isLoading,
-        isAuthenticated,
-        user,
-        logout,
-    } = useAuth0();
+    const { isLoading, isAuthenticated, user, logout } = useAuth0();
 
     console.log("isLoading", isLoading);
     console.log("isAuthenticated", isAuthenticated);
     console.log("user", user);
 
     if (isLoading) {
-        return <span>loding....</span>;
+        return <span>loading...</span>;
     }
+
+    const authProvider: AuthProvider = {
+        login: async () => {
+            return Promise.resolve();
+        },
+        logout: async () => {
+            logout();
+
+            return Promise.resolve();
+        },
+        checkError: () => Promise.resolve(),
+        checkAuth: async () => {
+            if (isAuthenticated) {
+                return Promise.resolve();
+            }
+
+            return Promise.reject();
+        },
+        getPermissions: () => Promise.resolve(),
+        getUserIdentity: async () => user,
+    };
 
     return (
         <>
-            {isAuthenticated ? (
-                <>
-                    {JSON.stringify(user)}
-                    <button onClick={() => logout()}>logout</button>
-                </>
-            ) : (
-                <button onClick={() => loginWithRedirect()}>login</button>
-            )}
-
-            <Admin dataProvider={dataProvider(API_URL)}>
+            <Admin
+                LoginPage={Login}
+                authProvider={authProvider}
+                dataProvider={dataProvider(API_URL)}
+            >
                 <Resource
                     name="posts"
                     list={PostList}
