efd3b47a390f5c128babcac8527537943fe6df9e
Salih <salihozdemir94@gmail.com>
2021-05-11 18:10:48 +0300

    update and refactor default login page

--- packages/core/src/components/pages/login/index.tsx
+++ packages/core/src/components/pages/login/index.tsx
@@ -1,22 +1,34 @@
 import React, { useContext } from "react";
-import { Row, Col, Layout, Card, Typography, Form, Input, Button } from "antd";
+import {
+    Row,
+    Col,
+    Layout,
+    Card,
+    Typography,
+    Form,
+    Input,
+    Button,
+    Checkbox,
+} from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import "./login.css";
 
-import { useNavigation, useNotification, useTranslate } from "@hooks";
+import { useNavigation, useNotification } from "@hooks";
 import { AuthContext } from "@contexts/auth";
 import { IAuthContext } from "../../../interfaces";
+
+const { Title, Text } = Typography;
 
 export interface ILoginForm {
     username: string;
     password: string;
+    remember: boolean;
 }
 
 export const LoginPage: React.FC = () => {
-    const { Title } = Typography;
-
     const [form] = Form.useForm();
     const { push } = useNavigation();
     const notification = useNotification();
-    const translate = useTranslate();
 
     const { login } = useContext<IAuthContext>(AuthContext);
 
@@ -31,21 +43,23 @@ export const LoginPage: React.FC = () => {
             });
     };
 
+    const CardTitle = (
+        <Title level={2} style={{ textAlign: "center" }}>
+            Refine
+        </Title>
+    );
+
     return (
-        <Layout>
+        <Layout className="login-background">
             <Row
                 justify="center"
+                align="middle"
                 style={{
-                    display: "flex",
-                    alignContent: "center",
                     height: "100vh",
                 }}
             >
-                <Col xl={6} lg={8} md={12} sm={18} xs={22}>
-                    <Card>
-                        <Title level={2} style={{ textAlign: "center" }}>
-                            {translate("common:pages.login.title", "Login")}
-                        </Title>
+                <Col xs={22}>
+                    <Card className="login-card" title={CardTitle}>
                         <Form
                             className="ant-form-vertical"
                             form={form}
@@ -54,38 +68,61 @@ export const LoginPage: React.FC = () => {
                         >
                             <Form.Item
                                 name="username"
-                                label={translate(
-                                    "common:pages.login.username",
-                                    "Username",
-                                )}
+                                label="Username"
                                 rules={[{ required: true }]}
                             >
-                                <Input />
+                                <Input
+                                    prefix={
+                                        <UserOutlined
+                                            style={{ color: "#00000040" }}
+                                        />
+                                    }
+                                />
                             </Form.Item>
                             <Form.Item
                                 name="password"
-                                label={translate(
-                                    "common:pages.login.password",
-                                    "Password",
-                                )}
+                                label="Username"
                                 rules={[{ required: true }]}
                             >
-                                <Input type="password" />
+                                <Input
+                                    type="password"
+                                    prefix={
+                                        <LockOutlined
+                                            style={{ color: "#00000040" }}
+                                        />
+                                    }
+                                />
                             </Form.Item>
+                            <Form.Item>
+                                <Form.Item
+                                    name="remember"
+                                    valuePropName="checked"
+                                    noStyle
+                                >
+                                    <Checkbox>Remember me</Checkbox>
+                                </Form.Item>
 
+                                <a style={{ float: "right" }} href="">
+                                    Forgot password?
+                                </a>
+                            </Form.Item>
                             <Form.Item>
                                 <Button
                                     type="primary"
                                     size="large"
                                     htmlType="submit"
+                                    block
                                 >
-                                    {translate(
-                                        "common:pages.login.title",
-                                        "Login",
-                                    )}
+                                    Login
                                 </Button>
                             </Form.Item>
                         </Form>
+                        <div className="signup-section">
+                            <Text>
+                                Still no account? Please go to{" "}
+                                <a href="">Sign up</a>
+                            </Text>
+                        </div>
                     </Card>
                 </Col>
             </Row>
