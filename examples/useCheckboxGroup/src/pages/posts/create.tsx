a771ab1b0b5ca70041e675facbd56049d9f60836
Salih Özdemir <salihozdemir94@gmail.com>
2021-05-17 17:48:15 +0300

    useCheckboxGroup example and doc (#443)
    * clone base project
    
    * add base doc files
    
    * update the hook
    
    * create example
    
    * fix some bugs
    
    * remove form item status
    
    * addition docs
    
    * remove defaultValue props
    
    * fix doc about defaultqueryResult
    
    * fix tests
    
    * remove unnecessary lines
    
    * fix meta texts
    
    * fix form item names
    
    * remove unused line
    
    * the doc transform like useForm doc
    
    * resource section move into the options section

---
+++ examples/useCheckboxGroup/src/pages/posts/create.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import {
+    Create,
+    Form,
+    Input,
+    IResourceComponentsProps,
+    useForm,
+    useCheckboxGroup,
+    Checkbox,
+} from "@pankod/refine";
+
+import ReactMarkdown from "react-markdown";
+import ReactMde from "react-mde";
+
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+import { IPost, ITag } from "interfaces";
+
+export const PostCreate = (props: IResourceComponentsProps) => {
+    const { formProps, saveButtonProps } = useForm<IPost>();
+
+    const {
+        checkboxGroupProps: tagsCheckboxGroupProps,
+    } = useCheckboxGroup<ITag>({
+        resource: "tags",
+        sort: {
+            field: "title",
+            order: "ascend",
+        },
+    });
+
+    const [selectedTab, setSelectedTab] = useState<"write" | "preview">(
+        "write",
+    );
+
+    return (
+        <Create {...props} saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Tag"
+                    name="tags"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Checkbox.Group {...tagsCheckboxGroupProps} />
+                </Form.Item>
+                <Form.Item
+                    label="Content"
+                    name="content"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <ReactMde
+                        selectedTab={selectedTab}
+                        onTabChange={setSelectedTab}
+                        generateMarkdownPreview={(markdown) =>
+                            Promise.resolve(
+                                <ReactMarkdown>{markdown}</ReactMarkdown>,
+                            )
+                        }
+                    />
+                </Form.Item>
+            </Form>
+        </Create>
+    );
+};
---
+++ examples/useCheckboxGroup/src/pages/posts/edit.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import {
+    Edit,
+    Form,
+    Input,
+    IResourceComponentsProps,
+    useForm,
+    useCheckboxGroup,
+    Checkbox,
+} from "@pankod/refine";
+
+import ReactMarkdown from "react-markdown";
+import ReactMde from "react-mde";
+
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+import { IPost, ITag } from "interfaces";
+
+export const PostEdit = (props: IResourceComponentsProps) => {
+    const { formProps, saveButtonProps } = useForm<IPost>();
+
+    const {
+        checkboxGroupProps: tagsCheckboxGroupProps,
+    } = useCheckboxGroup<ITag>({
+        resource: "tags",
+    });
+
+    const [selectedTab, setSelectedTab] = useState<"write" | "preview">(
+        "write",
+    );
+
+    return (
+        <Edit {...props} saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Tags"
+                    name="tags"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Checkbox.Group {...tagsCheckboxGroupProps} />
+                </Form.Item>
+                <Form.Item
+                    label="Content"
+                    name="content"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <ReactMde
+                        selectedTab={selectedTab}
+                        onTabChange={setSelectedTab}
+                        generateMarkdownPreview={(markdown) =>
+                            Promise.resolve(
+                                <ReactMarkdown>{markdown}</ReactMarkdown>,
+                            )
+                        }
+                    />
+                </Form.Item>
+            </Form>
+        </Edit>
+    );
+};
