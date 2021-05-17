825d8b7ea1286d631df36a781302f14f24d77e13
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-05-17 15:25:06 +0300

    Add CrudFilter interfaces.

--- packages/core/src/contexts/data/IDataContext.ts
+++ packages/core/src/contexts/data/IDataContext.ts
@@ -14,6 +14,48 @@ export interface Search {
     value?: string;
 }
 
+// Filters are used as a suffix of a field name:
+
+// | Filter              | Description                    |
+// | ------------------- | ------------------------------ |
+// | No suffix or `eq`   | Equal                          |
+// | ne                  | Not equal                      |
+// | lt                  | Less than                      |
+// | gt                  | Greater than                   |
+// | lte                 | Less than or equal to          |
+// | gte                 | Greater than or equal to       |
+// | in                  | Included in an array           |
+// | nin                 | Not included in an array       |
+// | contains            | Contains                       |
+// | ncontains           | Doesn't contain                |
+// | containss           | Contains, case sensitive       |
+// | ncontainss          | Doesn't contain, case sensitive|
+// | null                | Is null or not null            |
+
+export type CrudOperators =
+    | "eq"
+    | "ne"
+    | "lt"
+    | "gt"
+    | "lte"
+    | "gte"
+    | "in"
+    | "nin"
+    | "contains"
+    | "ncontains"
+    | "containss"
+    | "ncontainss"
+    | "null";
+
+export type CrudFilter = {
+    field: string;
+    operator: CrudOperators;
+    value: any;
+};
+
+export type CrudFilters = CrudFilter[];
+
+// @deprecated -> CrudFilter
 export type Filters = Record<string, (string | number | boolean)[] | null>;
 
 export interface GetListResponse<RecordType = BaseRecord> {
@@ -60,7 +102,7 @@ export interface IDataContext {
             pagination?: Pagination;
             search?: Search;
             sort?: Sort;
-            filters?: Filters;
+            filters?: CrudFilter[];
         },
     ) => Promise<GetListResponse<RecordType>>;
     getMany: <RecordType extends BaseRecord = BaseRecord>(
