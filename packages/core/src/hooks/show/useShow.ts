6bb5c613c253344ed029299ef0ead35b4d1e25ec
Salih <salihozdemir94@gmail.com>
2021-05-18 16:06:48 +0300

    refactoring * removed unnecasary lines

--- packages/core/src/hooks/show/useShow.ts
+++ packages/core/src/hooks/show/useShow.ts
@@ -1,39 +1,34 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { QueryObserverResult } from "react-query";
 
-import { useResourceWithRoute, useOne } from "@hooks";
+import { useOne } from "@hooks";
 
 import {
     ResourceRouterParams,
     BaseRecord,
     GetOneResponse,
 } from "../../interfaces";
-import { useState } from "react";
 
 export type useShowProps = {
-    resourceName?: string;
+    resource?: string;
     id?: string;
 };
 
 export const useShow = <RecordType extends BaseRecord = BaseRecord>({
-    resourceName,
+    resource: resourceName,
     id,
 }: useShowProps = {}) => {
+    const [showId, setShowId] = useState<string>();
     const {
         resource: routeResourceName,
         id: idFromRoute,
     } = useParams<ResourceRouterParams>();
 
-    const [showId, setShowId] = useState<string>();
+    const resource = resourceName ?? routeResourceName;
+    const resourceId = showId ?? id ?? idFromRoute;
 
-    const resourceWithRoute = useResourceWithRoute();
-
-    const resource = resourceWithRoute(routeResourceName);
-
-    const name = resourceName ?? resource.name;
-    const resourceId = id ?? showId ?? idFromRoute;
-
-    const queryResult = useOne<RecordType>(name, resourceId, {
+    const queryResult = useOne<RecordType>(resource, resourceId, {
         enabled: !!resourceId,
     });
 
