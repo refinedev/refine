a0bae946881fb5344d30370d172dc0c479e22444
Salih <salihozdemir94@gmail.com>
2021-05-20 10:56:11 +0300

    example get more understandable

--- examples/customPages/src/pages/post-review/post-review.tsx
+++ examples/customPages/src/pages/post-review/post-review.tsx
@@ -1,115 +1,76 @@
-import { useState } from "react";
 import {
     Typography,
     Button,
-    Card,
-    Icons,
-    AntdList,
+    Show,
     useList,
     MarkdownField,
-    useMany,
+    useOne,
     useUpdate,
-    Select,
 } from "@pankod/refine";
 
 import { IPost, ICategory } from "interfaces";
 
-const { LeftOutlined, RightOutlined } = Icons;
 const { Title, Text } = Typography;
-const { Option } = Select;
 
 export const PostReview = () => {
-    const [page, setPage] = useState<number>(1);
-    const [pageSize, setPageSize] = useState<number>(1);
-
     const { data, isLoading } = useList<IPost>("posts", {
         filters: { status: ["draft"] },
-        pagination: { pageSize, current: page },
+        pagination: { pageSize: 1, current: 1 },
     });
 
-    const record = data?.data;
+    const record = data?.data[0];
 
-    const categoryIds = data?.data?.map((item) => item.category.id) ?? [];
-    const { data: categoryData } = useMany<ICategory>(
+    const { data: categoryData } = useOne<ICategory>(
         "categories",
-        categoryIds,
+        record!?.category.id,
         {
-            enabled: categoryIds.length > 0,
+            enabled: !!record,
         },
     );
 
     const mutationResult = useUpdate<IPost>("posts");
 
-    const { mutate } = mutationResult;
+    const { mutate, isLoading: mutateIsLoading } = mutationResult;
 
-    const handleUpdate = (item: IPost) => {
-        mutate({ id: item.id, values: { ...item, status: "published" } });
+    const handleUpdate = (item: IPost, status: string) => {
+        mutate({ id: item.id, values: { ...item, status } });
     };
 
     return (
         <div>
             {record && (
-                <AntdList
-                    itemLayout="vertical"
-                    loading={isLoading}
-                    dataSource={record}
-                    loadMore={
-                        <div
-                            style={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                            }}
+                <Show
+                    title="Review Posts"
+                    resource="posts"
+                    isLoading={isLoading}
+                    actionButtons={[
+                        <Button
+                            key="reject"
+                            danger
+                            disabled={mutateIsLoading}
+                            onClick={() => handleUpdate(record, "rejected")}
                         >
-                            <Button onClick={() => setPage((prev) => prev - 1)}>
-                                <LeftOutlined />
-                                Previous Page
-                            </Button>
-                            <Select
-                                defaultValue={1}
-                                onChange={(value) => setPageSize(value)}
-                            >
-                                <Option value={1}>1 / page</Option>
-                                <Option value={2}>2 / page</Option>
-                                <Option value={3}>3 / page</Option>
-                            </Select>
-                            <Button onClick={() => setPage((prev) => prev + 1)}>
-                                Next Page
-                                <RightOutlined />
-                            </Button>
-                        </div>
-                    }
-                    renderItem={(item) => (
-                        <AntdList.Item>
-                            <Card
-                                style={{ height: "100%" }}
-                                actions={[
-                                    <Button
-                                        key="approve"
-                                        type="primary"
-                                        onClick={() => handleUpdate(item)}
-                                    >
-                                        Approve
-                                    </Button>,
-                                ]}
-                            >
-                                <Title level={5}>Status</Title>
-                                <Text mark>{item.status}</Text>
-                                <Title level={5}>Title</Title>
-                                <Text>{item.title}</Text>
-                                <Title level={5}>Category</Title>
-                                <Text>
-                                    {
-                                        categoryData?.data?.find(
-                                            (c) => c.id == item.category.id,
-                                        )?.title
-                                    }
-                                </Text>
-                                <Title level={5}>Content</Title>
-                                <MarkdownField value={item.content} />
-                            </Card>
-                        </AntdList.Item>
-                    )}
-                />
+                            Reject
+                        </Button>,
+                        <Button
+                            key="approve"
+                            type="primary"
+                            disabled={mutateIsLoading}
+                            onClick={() => handleUpdate(record, "published")}
+                        >
+                            Approve
+                        </Button>,
+                    ]}
+                >
+                    <Title level={5}>Status</Title>
+                    <Text mark>{record.status}</Text>
+                    <Title level={5}>Title</Title>
+                    <Text>{record.title}</Text>
+                    <Title level={5}>Category</Title>
+                    <Text>{categoryData?.data.title}</Text>
+                    <Title level={5}>Content</Title>
+                    <MarkdownField value={record.content} />
+                </Show>
             )}
         </div>
     );
