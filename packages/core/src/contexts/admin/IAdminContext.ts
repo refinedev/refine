537d7db3c4315314a4159abb2ae9c5dba00475ac
Ömer Faruk APLAK <omer@pankod.com>
2021-05-21 15:22:25 +0300

    Update IAdminContext.ts

--- packages/core/src/contexts/admin/IAdminContext.ts
+++ packages/core/src/contexts/admin/IAdminContext.ts
@@ -1,6 +1,5 @@
-import { SiderProps } from "../../interfaces";
 import React from "react";
-import { MutationMode, TitleProps, LayoutProps } from "../../interfaces";
+import { MutationMode, TitleProps, LayoutProps, SiderProps } from "../../interfaces";
 
 export interface IAdminContext {
     hasDashboard: boolean;
