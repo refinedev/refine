b54479a8b3fa2e461077ce86a3ccd74b43728314
Umut Özdemir <umutozdemir997@gmail.com>
2021-05-21 15:18:12 +0300

    add customMenu example

---
+++ examples/customMenu/src/CustomMenu.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+    AntdLayout,
+    Menu,
+    Icons,
+    Link,
+    useNavigation,
+    useTranslate,
+    useMenu,
+    useLogout,
+    useTitle,
+} from "@pankod/refine";
+
+export const CustomMenu: React.FC = () => {
+    const [collapsed, setCollapsed] = React.useState(false);
+    const logout = useLogout();
+    const Title = useTitle();
+    const { push } = useNavigation();
+    const translate = useTranslate();
+    const { menuItems, selectedKey } = useMenu();
+
+    return (
+        <AntdLayout.Sider
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(collapsed: boolean): void => setCollapsed(collapsed)}
+        >
+            <Title collapsed={collapsed} />
+            <Menu
+                theme="dark"
+                defaultSelectedKeys={["dashboard"]}
+                selectedKeys={[selectedKey]}
+                mode="inline"
+            >
+                {menuItems.map(({ icon, route, label }) => (
+                    <Menu.Item key={route} icon={icon}>
+                        <Link to={route}>{label}</Link>
+                    </Menu.Item>
+                ))}
+
+                {logout && (
+                    <Menu.Item
+                        onClick={() => {
+                            logout().then(() => push("/login"));
+                        }}
+                        key="logout"
+                        icon={<Icons.LogoutOutlined />}
+                    >
+                        {translate("common:buttons.logout", "Logout")}
+                    </Menu.Item>
+                )}
+            </Menu>
+        </AntdLayout.Sider>
+    );
+};
