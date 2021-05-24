312956dc204370cc548311ff6cd33cb2929d1a8e
Salih <salihozdemir94@gmail.com>
2021-05-24 10:31:43 +0300

    create of i18n instance

---
+++ examples/translate/src/i18n.ts
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-xhr-backend";
+
+i18n.use(Backend)
+    .use(initReactI18next)
+    .init({
+        lng: "en",
+        interpolation: {
+            escapeValue: false,
+        },
+        backend: {
+            loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+        fallbackLng: ["en", "tr"],
+    });
+
+export default i18n;
