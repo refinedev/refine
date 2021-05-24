5c8d22c96a3e2bab2f0ef3cf12449e06ac1133d7
Umut Özdemir <umutozdemir997@gmail.com>
2021-05-24 09:32:52 +0300

    Custom off layout area (#474)
    * add offLayoutArea example
    
    * remove unneccesary stuff

---
+++ examples/offLayoutArea/src/pages/posts/list.tsx
@@ -0,0 +1,23 @@
+import {
+    List,
+    Table,
+    useTable,
+    IResourceComponentsProps,
+} from "@pankod/refine";
+
+import { IPost } from "interfaces";
+
+export const PostList = (props: IResourceComponentsProps) => {
+    const { tableProps } = useTable<IPost>({
+        initialPageSize: 50,
+    });
+
+    return (
+        <List {...props}>
+            <Table {...tableProps} key="id">
+                <Table.Column key="id" dataIndex="id" title="ID" />
+                <Table.Column key="title" dataIndex="title" title="Title" />
+            </Table>
+        </List>
+    );
+};
