39efe3f2fa3b53f730b2fb383bc58109119a7a13
Salih <salihozdemir94@gmail.com>
2021-05-24 11:21:30 +0300

    refine default header change to null component

--- packages/core/src/components/layoutWrapper/components/header/index.tsx
+++ packages/core/src/components/layoutWrapper/components/header/index.tsx
@@ -1,10 +1,3 @@
-import React from "react";
-import { Layout } from "antd";
-
 export const Header = () => {
-    return (
-        <Layout.Header style={{ padding: 0, backgroundColor: "#FFF" }}>
-            <div>Default Header</div>
-        </Layout.Header>
-    );
+    return null;
 };
