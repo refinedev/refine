3ef0ffa27b72ea031be3cc0205feb17c6b8acde3
Salih <salihozdemir94@gmail.com>
2021-05-24 11:03:20 +0300

    create i118nProvider and pass Admin as props

--- examples/translate/src/App.tsx
+++ examples/translate/src/App.tsx
@@ -1,14 +1,24 @@
 import { Admin, Resource } from "@pankod/refine";
 import dataProvider from "@pankod/refine-json-server";
+import { useTranslation } from "react-i18next";
 import "@pankod/refine/dist/styles.min.css";
+import "i18n";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
 
 const API_URL = "https://refine-fake-rest.pankod.com";
 
 const App = () => {
+    const { t, i18n } = useTranslation(["common", "resources"]);
+
+    const i18nProvider = {
+        translate: (key: string, params: object) => t(key, params),
+        changeLocale: (lang: string) => i18n.changeLanguage(lang),
+        getLocale: () => i18n.language,
+    };
+
     return (
-        <Admin dataProvider={dataProvider(API_URL)}>
+        <Admin dataProvider={dataProvider(API_URL)} i18nProvider={i18nProvider}>
             <Resource
                 name="posts"
                 list={PostList}
