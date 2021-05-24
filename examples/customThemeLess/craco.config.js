71c964de046f0395d969b1ad372b73edcaddbf02
Umut Özdemir <umutozdemir997@gmail.com>
2021-05-24 16:51:33 +0300

    remove modifyVars from craco.config.js

--- examples/customThemeLess/craco.config.js
+++ examples/customThemeLess/craco.config.js
@@ -7,7 +7,6 @@ module.exports = {
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
-                        modifyVars: { "@primary-color": "#1DA57A" },
                         javascriptEnabled: true,
                     },
                 },
