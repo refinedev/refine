a647d2e50af2e27771733541831e6b1461a68200
Muharrem Kocadere <mkocadere@gmail.com>
2021-05-25 12:31:12 +0300

    init useList doc

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -28,6 +28,7 @@ module.exports = {
                         "hooks/data/useUpdateMany",
                         "hooks/data/useDelete",
                         "hooks/data/useDeleteMany",
+                        "hooks/data/useList",
                     ],
                 },
                 "useModalForm",
