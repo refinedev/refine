09b253178955907863c4fda9a700f8113ed7a28b
Salih <salihozdemir94@gmail.com>
2021-05-27 15:53:37 +0300

    handling error messages

--- packages/core/src/components/pages/error/index.tsx
+++ packages/core/src/components/pages/error/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Result, Typography, Tooltip, Space } from "antd";
-import { InfoCircleOutlined } from "@ant-design/icons";
+import { Button, Result, Typography, Space, Alert } from "antd";
 import { useParams } from "react-router-dom";
 
 import { useNavigation, useTranslate, useResourceWithRoute } from "@hooks";
@@ -9,7 +8,7 @@ import { ResourceErrorRouterParams } from "../../../interfaces";
 const { Text } = Typography;
 
 export const ErrorComponent: React.FC = () => {
-    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>();
     const { push } = useNavigation();
     const translate = useTranslate();
     const actionTypes = ["edit", "create", "show"];
@@ -21,19 +20,32 @@ export const ErrorComponent: React.FC = () => {
         if (params.resource) {
             try {
                 const resourceFromRoute = resource(params.resource);
-                if (params.action) {
-                    if (actionTypes.includes(params.action)) {
-                        if (!resourceFromRoute[params.action]) {
-                            setErrorMessage(
-                                `You may have forgotten to add the "${params.action}" component to "${params.resource}".`,
-                            );
-                        }
-                    } else {
-                        setErrorMessage(`page not found`);
-                    }
+                if (
+                    params.action &&
+                    actionTypes.includes(params.action) &&
+                    !resourceFromRoute[params.action]
+                ) {
+                    setErrorMessage(
+                        translate(
+                            "pages.error.infoText",
+                            {
+                                action: params.action.toUpperCase(),
+                                resource: params.resource.toUpperCase(),
+                            },
+                            `You may have forgotten to add the "${params.action.toUpperCase()}" component to "${params.resource.toUpperCase()}" resource.`,
+                        ),
+                    );
                 }
             } catch (error) {
-                setErrorMessage(`${params.resource} resource not found`);
+                setErrorMessage(
+                    translate(
+                        "pages.error.resourceNotFound",
+                        {
+                            resource: params.resource.toUpperCase(),
+                        },
+                        `Are you sure you have created the "${params.resource.toUpperCase()}" resource.`,
+                    ),
+                );
             }
         }
     }, [params]);
@@ -43,17 +55,24 @@ export const ErrorComponent: React.FC = () => {
             status="404"
             title="404"
             extra={
-                <Space direction="vertical">
+                <Space direction="vertical" size="large">
                     <Text>
-                        <Space>
-                            Sorry, the page you visited does not exist.
-                            <Tooltip title={errorMessage}>
-                                <InfoCircleOutlined />
-                            </Tooltip>
-                        </Space>
+                        {translate(
+                            "pages.error.404text",
+                            "Sorry, the page you visited does not exist.",
+                        )}
                     </Text>
+                    {errorMessage && (
+                        <Alert
+                            message="Informational Notes"
+                            description={errorMessage}
+                            type="info"
+                            showIcon
+                            style={{ textAlign: "left" }}
+                        />
+                    )}
                     <Button type="primary" onClick={() => push("/")}>
-                        {translate("backHome", "Back Home")}
+                        {translate("pages.error.backHome", "Back Home")}
                     </Button>
                 </Space>
             }
