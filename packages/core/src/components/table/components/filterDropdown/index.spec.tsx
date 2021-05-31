d17acb563b24afa3a8893ebccb3ba86fbbb40071
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-05-31 16:01:16 +0300

    Add base component test.

---
+++ packages/core/src/components/table/components/filterDropdown/index.spec.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import {
+    render,
+    fireEvent,
+} from "@test";
+import {
+    FilterDropdown,
+    FilterDropdownProps,
+} from "./";
+
+describe("FilterDropdown", () => {
+    const confirm = jest.fn();
+    const clearFilters = jest.fn();
+    const setSelectedKeys = jest.fn();
+
+    const props: FilterDropdownProps = {
+        confirm,
+        selectedKeys: [],
+        setSelectedKeys,
+        visible: false,
+        prefixCls:
+            "",
+        filters: [],
+        clearFilters,
+    };
+
+    it("should render called confirm function successfully if click the filter button", async () => {
+        const {
+            getByText,
+        } = render(
+            <FilterDropdown
+                {...props}
+            />,
+        );
+
+        fireEvent.click(
+            getByText(
+                "Filter",
+            ),
+        );
+
+        expect(
+            confirm,
+        ).toHaveBeenCalledTimes(
+            1,
+        );
+    });
+});
