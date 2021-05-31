b295664b1fa9c811fa81496fc5a8794c3826e142
Salih Özdemir <salihozdemir94@gmail.com>
2021-05-31 16:42:51 +0300

    Not found page (#491)
    * change component path
    
    Co-authored-by: Aydın Akyol<aydınakyol11@gmail.com>
    
    * create resource error type
    
    * [wip] handling error messages
    
    * handling error messages
    
    * add new translations
    
    * add handling not found page to unauthorized routes
    
    * rename translation keys
    
    * change display with i icon
    
    * fix re-import same page
    
    * fix test errors
    
    * add new tests and improve test coverage
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -10,7 +10,6 @@ export * from "./containers";
 export * from "./notification";
 
 export { LayoutWrapper } from "./layoutWrapper";
-export { ErrorComponent } from "./layoutWrapper/components";
 export { Authenticated } from "./authenticated";
 export { default as Icon } from "@ant-design/icons";
 export { Icons };
