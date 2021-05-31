dc5d7614e970fca66e810fa0598347bef1312d2d
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-05-31 15:18:16 +0300

    Add routeProvider tests.

--- packages/core/src/components/layoutWrapper/components/title/index.tsx
+++ packages/core/src/components/layoutWrapper/components/title/index.tsx
@@ -3,21 +3,19 @@ import { Link } from "react-router-dom";
 import { TitleProps } from "src/interfaces";
 
 export const Title: React.FC<TitleProps> = () => (
-    <Link to="/">
-        <a
-            href="/"
-            style={{
-                display: "flex",
-                width: "100%",
-                textAlign: "center",
-                color: "rgb(255, 255, 255)",
-                fontSize: "16px",
-                height: "60px",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            refine
-        </a>
+    <Link
+        to="/"
+        style={{
+            display: "flex",
+            width: "100%",
+            textAlign: "center",
+            color: "rgb(255, 255, 255)",
+            fontSize: "16px",
+            height: "60px",
+            justifyContent: "center",
+            alignItems: "center",
+        }}
+    >
+        refine
     </Link>
 );
