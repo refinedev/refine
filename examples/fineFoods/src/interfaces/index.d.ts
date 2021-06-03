8d29ad6da4b65f927678d4440bbf51bc3775d2c5
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-06-03 17:40:52 +0300

    Add interfaces.

--- examples/fineFoods/src/interfaces/index.d.ts
+++ examples/fineFoods/src/interfaces/index.d.ts
@@ -1,6 +1,46 @@
-export interface IPost {
+export interface IOrderStatus {
     id: string;
-    title: string;
-    content: string;
-    created_at: string;
+    text: string;
+}
+
+export interface IAddress {
+    text: string;
+    coordinate: [string, string];
+}
+
+export interface IFile {
+    name: string;
+    percent: number;
+    size: number;
+    status: "error" | "success" | "done" | "uploading" | "removed";
+    type: string;
+    uid: string;
+    url: string;
+}
+
+export interface IEvent {
+    date: string;
+    name: string;
+}
+
+export interface ICourier {
+    id: string;
+    name: string;
+    surname: string;
+    gender: string;
+    gsm: string;
+    createdAt: string;
+    isActive: boolean;
+    avatar: IFile[];
+}
+export interface IOrder {
+    id: string;
+    userId: string;
+    createdAt: string;
+    productIds: string[];
+    status: IOrderStatus;
+    adress: IAddress;
+    storeId: string;
+    courier: {};
+    events: IEvent[];
 }
