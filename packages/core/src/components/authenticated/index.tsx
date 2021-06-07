63647ab9b7bf1078cef3de814c25dd6da0b944d7
necatiozmen <necatiozmn@gmail.com>
2021-06-07 16:58:45 +0300

    Refacator authenticated

--- packages/core/src/components/authenticated/index.tsx
+++ packages/core/src/components/authenticated/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import { useAuthenticated } from "@hooks";
 
-export type AuthenticatedProps = {};
+export type AuthenticatedProps = { fallback?: any };
 
-export const Authenticated: React.FC<AuthenticatedProps> = ({ children }) => {
+export const Authenticated: React.FC<AuthenticatedProps> = ({
+    children,
+    fallback,
+}) => {
     const { isSuccess, isLoading, isError } = useAuthenticated();
 
     if (isLoading || isError) {
-        return null;
+        return fallback || null;
     }
     if (isSuccess) {
         return <>{children}</>;
