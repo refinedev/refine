c168d8f80109c23dcb9fd4ae97c9443e22edc5db
necatiozmen <necatiozmn@gmail.com>
2021-06-08 14:05:41 +0300

    Remove error component

--- packages/core/src/components/authenticated/index.tsx
+++ packages/core/src/components/authenticated/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 
-import { useAuthenticated, useNotification } from "@hooks";
-import { Button, Result, Typography, Space, Tooltip } from "antd";
-import { useParams } from "react-router-dom";
-import { InfoCircleOutlined } from "@ant-design/icons";
-const { Text } = Typography;
+import { useAuthenticated } from "@hooks";
+
 export type AuthenticatedProps = {
     fallback?: React.ReactNode;
     loading?: React.ReactNode;
@@ -17,32 +14,11 @@ export const Authenticated: React.FC<AuthenticatedProps> = ({
 }) => {
     const { isSuccess, isLoading, isError } = useAuthenticated();
 
-    const error = (
-        <Result
-            status="404"
-            extra={
-                <Space direction="vertical" size="large">
-                    <Space>
-                        <Text>sasda</Text>
-                        <Tooltip title={"dasdsa"}>
-                            <InfoCircleOutlined data-testid="error-component-tooltip" />
-                        </Tooltip>
-                    </Space>
-                    <Button type="primary">home</Button>
-                </Space>
-            }
-        />
-    );
-
     if (isLoading) {
         return <>{loading}</> || null;
     }
     if (isError) {
-        return (
-            <>
-                {fallback} || {error}
-            </>
-        );
+        return <>{fallback}</> || null;
     }
     if (isSuccess) {
         return <>{children}</>;
