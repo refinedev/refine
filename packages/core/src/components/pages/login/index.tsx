ccde8536b3b7927017fe6eab6ae913e5865e1ace
necatiozmen <necatiozmn@gmail.com>
2021-06-10 12:11:11 +0300

    Refactor useLogin Co-authored-by: mhrrmk <mkocadere@gmail.com>

--- packages/core/src/components/pages/login/index.tsx
+++ packages/core/src/components/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import {
     Row,
     Col,
@@ -20,12 +20,9 @@ import {
 
 import logo from "./refine.svg";
 
-import { useNavigation, useNotification, useTranslate } from "@hooks";
-import { AuthContext } from "@contexts/auth";
-import { IAuthContext } from "../../../interfaces";
+import { useLogin, useTranslate } from "@hooks";
 
 const { Text } = Typography;
-
 export interface ILoginForm {
     username: string;
     password: string;
@@ -34,21 +31,14 @@ export interface ILoginForm {
 
 export const LoginPage: React.FC = () => {
     const [form] = Form.useForm();
-    const { push } = useNavigation();
-    const notification = useNotification();
     const translate = useTranslate();
 
-    const { login } = useContext<IAuthContext>(AuthContext);
+    const { mutate: login, data } = useLogin();
+
+    console.log("data", data);
 
     const onSubmit = (values: ILoginForm) => {
-        login(values)
-            .then(() => push("/"))
-            .catch(() => {
-                notification.error({
-                    message: "Login Error",
-                    description: "Invalid username or password",
-                });
-            });
+        login(values);
     };
 
     const CardTitle = (
--- packages/core/src/hooks/auth/useLogin/index.ts
+++ packages/core/src/hooks/auth/useLogin/index.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import { useMutation, UseMutationResult } from "react-query";
 
 import { AuthContext } from "@contexts/auth";
 import { useNotification } from "@hooks";
@@ -6,29 +7,23 @@ import { useNavigation } from "@hooks/navigation";
 
 import { IAuthContext } from "../../../interfaces";
 
-export const useLogin = (): ((params: any) => Promise<any>) => {
+export const useLogin = (): UseMutationResult<any, Error, any, unknown> => {
     const { push } = useNavigation();
-    const authContext = React.useContext<IAuthContext>(AuthContext);
+    const { login: loginFromContext } =
+        React.useContext<IAuthContext>(AuthContext);
     const notification = useNotification();
 
-    const login = React.useCallback(
-        (params: any) =>
-            authContext
-                .login(params)
-                .then((response) => {
-                    push("/");
-                    return Promise.resolve(response);
-                })
-                .catch((error: Error) => {
-                    notification.error({
-                        message: error.name || "Login Error",
-                        description: error.message || "Invalid credentials",
-                    });
+    const queryResponse = useMutation("useLogin", loginFromContext, {
+        onSuccess: () => {
+            push("/");
+        },
+        onError: (error: Error) => {
+            notification.error({
+                message: error?.name || "Login Error",
+                description: error?.message || "Invalid credentials",
+            });
+        },
+    });
 
-                    return Promise.reject(error);
-                }),
-        [push, authContext, notification],
-    );
-
-    return login;
+    return queryResponse;
 };
