aea5036dfbb7ba52b8c65465ec817f8ea5182ef1
Ömer Faruk APLAK <omer@pankod.com>
2021-06-11 19:31:21 +0300

    refactor charts data

--- examples/fineFoods/src/components/dailyRevenue/index.tsx
+++ examples/fineFoods/src/components/dailyRevenue/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import {
     Typography,
     useApiUrl,
@@ -14,7 +14,6 @@ import { ISalesChart } from "interfaces";
 import styles from "./styles";
 
 export const DailyRevenue: React.FC = () => {
-    const [total, setTotal] = useState(0);
     const API_URL = useApiUrl();
 
     const [dateRange, setDateRange] = useState<[Dayjs, Dayjs]>([
@@ -33,40 +32,42 @@ export const DailyRevenue: React.FC = () => {
         query,
     });
 
-    useEffect(() => {
-        if (data) {
-            setTotal(
-                data.data.reduce((acc, item) => {
-                    if (item.title === "Order Amount") {
-                        return acc + item.value;
-                    }
-                    return acc;
-                }, 0),
-            );
-        }
-    }, [data]);
+    const total = useMemo(
+        () =>
+            data?.data.reduce((acc, item) => {
+                if (item.title === "Order Amount") {
+                    return acc + item.value;
+                }
+                return acc;
+            }, 0) || 0,
+        [data],
+    );
 
     const { Title } = Typography;
     const { RangePicker } = DatePicker;
 
-    const config: ColumnConfig = {
-        data: data?.data || [],
-        loading: isLoading,
-        isGroup: true,
-        xField: "date",
-        yField: "value",
-        seriesField: "title",
-        tooltip: {
-            title: (date) => dayjs(date).format("LL"),
-        },
-        xAxis: {
-            label: {
-                formatter: () => {
-                    return null;
+    const config = useMemo(() => {
+        const config: ColumnConfig = {
+            data: data?.data || [],
+            loading: isLoading,
+            isGroup: true,
+            xField: "date",
+            yField: "value",
+            seriesField: "title",
+            tooltip: {
+                title: (date) => dayjs(date).format("LL"),
+            },
+            xAxis: {
+                label: {
+                    formatter: () => {
+                        return null;
+                    },
                 },
             },
-        },
-    };
+        };
+
+        return config;
+    }, [data]);
 
     return (
         <>
