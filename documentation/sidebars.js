a85581cabbf3c92204d6956a8b42d7489bb868b9
Aydın Akyol <aydinakyol11@gmail.com>
2021-06-14 12:30:40 +0300

    boolean field doc init

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -105,6 +105,11 @@ module.exports = {
                         },
                     ],
                 },
+                {
+                    type: "category",
+                    label: "Fields",
+                    items: ["guides-and-concepts/fields/boolean/boolean"],
+                },
                 "guides-and-concepts/multipart-upload",
                 "guides-and-concepts/uploading-base64",
                 "guides-and-concepts/auth0",
