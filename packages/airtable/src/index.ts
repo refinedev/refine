bdf24f34cec1ada2dabc9fa86f1bffda85bff851
Ömer Faruk APLAK <omer@pankod.com>
2021-06-15 14:30:40 +0300

    init projects

---
+++ packages/airtable/src/index.ts
@@ -0,0 +1,252 @@
+import axios, { AxiosInstance } from "axios";
+import { stringify } from "query-string";
+import {
+    DataProvider,
+    HttpError,
+    CrudOperators,
+    CrudFilters,
+} from "@pankod/refine";
+import { CrudSorting } from "@pankod/refine/dist/interfaces";
+
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const customError: HttpError = {
+            ...error,
+            message: error.response?.data?.message,
+            statusCode: error.response?.status,
+        };
+
+        return Promise.reject(customError);
+    },
+);
+
+const mapOperator = (operator: CrudOperators): string => {
+    switch (operator) {
+        case "ne":
+        case "gte":
+        case "lte":
+            return `_${operator}`;
+        case "contains":
+            return "_like";
+    }
+
+    return ""; // default "eq"
+};
+
+const generateSort = (sort?: CrudSorting) => {
+    let _sort = ["id"]; // default sorting field
+    let _order = ["desc"]; // default sorting
+
+    if (sort) {
+        _sort = [];
+        _order = [];
+
+        sort.map((item) => {
+            _sort.push(item.field);
+            _order.push(item.order);
+        });
+    }
+
+    return {
+        _sort,
+        _order,
+    };
+};
+
+const generateFilter = (filters?: CrudFilters) => {
+    const queryFilters: { [key: string]: string } = {};
+    if (filters) {
+        filters.map(({ field, operator, value }) => {
+            const mappedOperator = mapOperator(operator);
+            queryFilters[`${field}${mappedOperator}`] = value;
+        });
+    }
+
+    return queryFilters;
+};
+
+const Airtable = (
+    apiUrl: string,
+    httpClient: AxiosInstance = axiosInstance,
+): DataProvider => ({
+    getList: async (resource, params) => {
+        const url = `${apiUrl}/${resource}`;
+
+        // pagination
+        const current = params.pagination?.current || 1;
+        const pageSize = params.pagination?.pageSize || 10;
+
+        const { _sort, _order } = generateSort(params.sort);
+
+        const queryFilters = generateFilter(params.filters);
+
+        const query = {
+            _start: (current - 1) * pageSize,
+            _end: current * pageSize,
+            _sort: _sort.join(","),
+            _order: _order.join(","),
+        };
+
+        const { data, headers } = await httpClient.get(
+            `${url}?${stringify(query)}&${stringify(queryFilters)}`,
+        );
+
+        const total = +headers["x-total-count"];
+
+        return {
+            data,
+            total,
+        };
+    },
+
+    getMany: async (resource, ids) => {
+        const { data } = await httpClient.get(
+            `${apiUrl}/${resource}?${stringify({ id: ids })}`,
+        );
+
+        return {
+            data,
+        };
+    },
+
+    create: async (resource, params) => {
+        const url = `${apiUrl}/${resource}`;
+
+        const { data } = await httpClient.post(url, params);
+
+        return {
+            data,
+        };
+    },
+
+    createMany: async (resource, params) => {
+        const response = await Promise.all(
+            params.map(async (param) => {
+                const { data } = await httpClient.post(
+                    `${apiUrl}/${resource}`,
+                    param,
+                );
+                return data;
+            }),
+        );
+
+        return { data: response };
+    },
+
+    update: async (resource, id, params) => {
+        const url = `${apiUrl}/${resource}/${id}`;
+
+        const { data } = await httpClient.patch(url, params);
+
+        return {
+            data,
+        };
+    },
+
+    updateMany: async (resource, ids, params) => {
+        const response = await Promise.all(
+            ids.map(async (id) => {
+                const { data } = await httpClient.patch(
+                    `${apiUrl}/${resource}/${id}`,
+                    params,
+                );
+                return data;
+            }),
+        );
+
+        return { data: response };
+    },
+
+    getOne: async (resource, id) => {
+        const url = `${apiUrl}/${resource}/${id}`;
+
+        const { data } = await httpClient.get(url);
+
+        return {
+            data,
+        };
+    },
+
+    deleteOne: async (resource, id) => {
+        const url = `${apiUrl}/${resource}/${id}`;
+
+        const { data } = await httpClient.delete(url);
+
+        return {
+            data,
+        };
+    },
+
+    deleteMany: async (resource, ids) => {
+        const response = await Promise.all(
+            ids.map(async (id) => {
+                const { data } = await httpClient.delete(
+                    `${apiUrl}/${resource}/${id}`,
+                );
+                return data;
+            }),
+        );
+        return { data: response };
+    },
+
+    getApiUrl: () => {
+        return apiUrl;
+    },
+
+    custom: async (url, method, params = {}) => {
+        const { filters, sort, payload, query, headers } = params;
+
+        let requestUrl = `${url}?`;
+
+        if (sort) {
+            const { _sort, _order } = generateSort(sort);
+            const sortQuery = {
+                _sort: _sort.join(","),
+                _order: _order.join(","),
+            };
+            requestUrl = `${requestUrl}&${stringify(sortQuery)}`;
+        }
+
+        if (filters) {
+            const filterQuery = generateFilter(filters);
+            requestUrl = `${requestUrl}&${stringify(filterQuery)}`;
+        }
+
+        if (query) {
+            requestUrl = `${requestUrl}&${stringify(query)}`;
+        }
+
+        if (headers) {
+            httpClient.defaults.headers = {
+                ...httpClient.defaults.headers,
+                ...headers,
+            };
+        }
+
+        let axiosResponse;
+        switch (method) {
+            case "put":
+            case "post":
+            case "patch":
+                axiosResponse = await httpClient[method](url, payload);
+                break;
+            case "delete":
+                axiosResponse = await httpClient.delete(url);
+                break;
+            default:
+                axiosResponse = await httpClient.get(requestUrl);
+                break;
+        }
+
+        const { data } = axiosResponse;
+
+        return Promise.resolve({ data });
+    },
+});
+
+export default Airtable;
---
+++ packages/airtable/test/updateMany/index.spec.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+import JsonServer from "../../src/index";
+import "./index.mock";
+
+axios.defaults.adapter = require("axios/lib/adapters/http");
+
+describe("updateMany", () => {
+    it("correct response", async () => {
+        const { data } = await JsonServer(
+            "https://api.nestjsx-crud.refine.dev",
+            axios,
+        ).updateMany("posts", ["f1d6e030-4d70-44d4-98dd-8786f197c640"], {
+            title: "updated-title-1",
+        });
+
+        expect(data[0]["title"]).toBe("updated-title-1");
+    });
+});
