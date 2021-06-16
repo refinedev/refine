cd97a9f8defc924f43215a38dd3c171a4ed1d1c8
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-06-16 15:12:30 +0300

    Updated order show.

--- examples/fineFoods/src/pages/orders/show.tsx
+++ examples/fineFoods/src/pages/orders/show.tsx
@@ -24,138 +24,142 @@ export const OrderShow: React.FC<IResourceComponentsProps> = (props) => {
     const { data, isLoading } = queryResult;
     const record = data?.data;
 
+    const renderContent = () => {
+        return (
+            <Row gutter={[16, 16]}>
+                <Col md={8}>
+                    <Title underline level={3}>
+                        Order
+                    </Title>
+                    <Title level={5}>Order Number</Title>
+                    <Text>{record.orderNumber}</Text>
+                    <Title level={5}>Status</Title>
+                    <OrderStatus status={record.status.text} />
+                    <Title level={5}>Amount</Title>
+                    <NumberField
+                        options={{
+                            currency: "USD",
+                            style: "currency",
+                            notation: "compact",
+                        }}
+                        value={record.amount}
+                    />
+                    <Title level={5}>Created At</Title>
+                    <DateField value={record.createdAt} format="LLL" />
+                    <Title level={5}>Address</Title>
+                    <Text>{record.adress.text}</Text>
+                    <Title underline level={3}>
+                        Order History
+                    </Title>
+                    <Timeline style={{ marginTop: 20 }}>
+                        {record.events.map((event, index) => (
+                            <Timeline.Item key={index}>
+                                <>
+                                    {event.name}
+                                    <small style={{ marginLeft: 10 }}>
+                                        ({dayjs(event.date).format("LLL")})
+                                    </small>
+                                </>
+                            </Timeline.Item>
+                        ))}
+                    </Timeline>
+                    ,
+                </Col>
+
+                <Col md={8}>
+                    <Title underline level={3}>
+                        Products
+                    </Title>
+
+                    <AntdList
+                        itemLayout="horizontal"
+                        dataSource={record.products}
+                        renderItem={(item) => (
+                            <AntdList.Item>
+                                <AntdList.Item.Meta
+                                    avatar={
+                                        <Avatar
+                                            src={item.images[0].url}
+                                            size="large"
+                                            shape="square"
+                                        />
+                                    }
+                                    title={item.name}
+                                    description={
+                                        <NumberField
+                                            options={{
+                                                currency: "USD",
+                                                style: "currency",
+                                                notation: "compact",
+                                            }}
+                                            value={item.price}
+                                        />
+                                    }
+                                />
+                            </AntdList.Item>
+                        )}
+                    />
+                </Col>
+
+                <Col md={4}>
+                    <Title underline level={3}>
+                        User
+                    </Title>
+
+                    <Title level={5}>ID</Title>
+                    <Text>{record.user.id}</Text>
+
+                    <Title level={5}>Full Name</Title>
+                    <Text>{record.user.fullName}</Text>
+
+                    <Title level={5}>Gender</Title>
+                    <Text>{record.user.gender}</Text>
+
+                    <Title level={5}>Active</Title>
+                    <Text>
+                        {record.user.isActive ? (
+                            <Icons.CheckCircleOutlined
+                                style={{ fontSize: 22, color: "#52C41A" }}
+                            />
+                        ) : (
+                            <Icons.CloseOutlined
+                                style={{ fontSize: 22, color: "#F5222D" }}
+                            />
+                        )}
+                    </Text>
+                </Col>
+
+                <Col md={4}>
+                    <Title underline level={3}>
+                        Store
+                    </Title>
+
+                    <Title level={5}>ID</Title>
+                    <Text>{record.store.id}</Text>
+
+                    <Title level={5}>Name</Title>
+                    <Text>{record.store.title}</Text>
+
+                    <Title level={5}>Active</Title>
+                    <Text>
+                        {record.store.isActive ? (
+                            <Icons.CheckCircleOutlined
+                                style={{ fontSize: 22, color: "#52C41A" }}
+                            />
+                        ) : (
+                            <Icons.CloseOutlined
+                                style={{ fontSize: 22, color: "#F5222D" }}
+                            />
+                        )}
+                    </Text>
+                </Col>
+            </Row>
+        );
+    };
+
     return (
         <Show {...props} title="Order Detail" isLoading={isLoading}>
-            {record && (
-                <Row gutter={[16, 16]}>
-                    <Col md={8}>
-                        <Title underline level={3}>
-                            Order
-                        </Title>
-                        <Title level={5}>Order Number</Title>
-                        <Text>{record.orderNumber}</Text>
-                        <Title level={5}>Status</Title>
-                        <OrderStatus status={record.status.text} />
-                        <Title level={5}>Amount</Title>
-                        <NumberField
-                            options={{
-                                currency: "USD",
-                                style: "currency",
-                                notation: "compact",
-                            }}
-                            value={record.amount}
-                        />
-                        <Title level={5}>Created At</Title>
-                        <DateField value={record.createdAt} format="LLL" />
-                        <Title level={5}>Address</Title>
-                        <Text>{record.adress.text}</Text>
-                        <Title underline level={3}>
-                            Order History
-                        </Title>
-                        <Timeline style={{ marginTop: 20 }}>
-                            {record.events.map((event, index) => (
-                                <Timeline.Item key={index}>
-                                    <>
-                                        {event.name}
-                                        <small style={{ marginLeft: 10 }}>
-                                            ({dayjs(event.date).format("LLL")})
-                                        </small>
-                                    </>
-                                </Timeline.Item>
-                            ))}
-                        </Timeline>
-                        ,
-                    </Col>
-
-                    <Col md={8}>
-                        <Title underline level={3}>
-                            Products
-                        </Title>
-
-                        <AntdList
-                            itemLayout="horizontal"
-                            dataSource={record.products}
-                            renderItem={(item) => (
-                                <AntdList.Item>
-                                    <AntdList.Item.Meta
-                                        avatar={
-                                            <Avatar
-                                                src={item.images[0].url}
-                                                size="large"
-                                                shape="square"
-                                            />
-                                        }
-                                        title={item.name}
-                                        description={
-                                            <NumberField
-                                                options={{
-                                                    currency: "USD",
-                                                    style: "currency",
-                                                    notation: "compact",
-                                                }}
-                                                value={item.price}
-                                            />
-                                        }
-                                    />
-                                </AntdList.Item>
-                            )}
-                        />
-                    </Col>
-
-                    <Col md={4}>
-                        <Title underline level={3}>
-                            User
-                        </Title>
-
-                        <Title level={5}>ID</Title>
-                        <Text>{record.user.id}</Text>
-
-                        <Title level={5}>Full Name</Title>
-                        <Text>{record.user.fullName}</Text>
-
-                        <Title level={5}>Gender</Title>
-                        <Text>{record.user.gender}</Text>
-
-                        <Title level={5}>Active</Title>
-                        <Text>
-                            {record.user.isActive ? (
-                                <Icons.CheckCircleOutlined
-                                    style={{ fontSize: 22, color: "#52C41A" }}
-                                />
-                            ) : (
-                                <Icons.CloseOutlined
-                                    style={{ fontSize: 22, color: "#F5222D" }}
-                                />
-                            )}
-                        </Text>
-                    </Col>
-
-                    <Col md={4}>
-                        <Title underline level={3}>
-                            Store
-                        </Title>
-
-                        <Title level={5}>ID</Title>
-                        <Text>{record.store.id}</Text>
-
-                        <Title level={5}>Name</Title>
-                        <Text>{record.store.title}</Text>
-
-                        <Title level={5}>Active</Title>
-                        <Text>
-                            {record.store.isActive ? (
-                                <Icons.CheckCircleOutlined
-                                    style={{ fontSize: 22, color: "#52C41A" }}
-                                />
-                            ) : (
-                                <Icons.CloseOutlined
-                                    style={{ fontSize: 22, color: "#F5222D" }}
-                                />
-                            )}
-                        </Text>
-                    </Col>
-                </Row>
-            )}
+            {record && renderContent()}
         </Show>
     );
 };
