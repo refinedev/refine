fb9adebfb9085512f4860e5c220d5817561d6725
Salih Özdemir <salihozdemir94@gmail.com>
2021-06-17 17:25:40 +0300

    useDeleteMany example fix and add codesandbox embed code (#638)
    * fix example code
    
    * add codesandbox embed code

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -149,7 +149,6 @@ module.exports = {
                         "guides-and-concepts/fields/date",
                         "guides-and-concepts/fields/email",
                         "guides-and-concepts/fields/markdown",
-                        "guides-and-concepts/fields/number",
                         "guides-and-concepts/fields/tag",
                         "guides-and-concepts/fields/text",
                     ],
@@ -162,6 +161,11 @@ module.exports = {
                 "guides-and-concepts/csv-import",
                 "guides-and-concepts/csv-export",
             ],
+        },
+        {
+            type: "category",
+            label: "Examples",
+            items: ["examples/useDeleteMany"],
         },
         {
             type: "doc",
