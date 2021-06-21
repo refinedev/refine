9a7f03856c1c7c26f4ac3c40740df0c7782ebeb6
Salih <salihozdemir94@gmail.com>
2021-06-21 15:37:45 +0300

    fix example code

--- examples/strapi/src/App.tsx
+++ examples/strapi/src/App.tsx
@@ -3,7 +3,7 @@ import { DataProvider, AuthHelper } from "@pankod/refine-strapi";
 import axios from "axios";
 import "@pankod/refine/dist/styles.min.css";
 
-import { PostList, PostCreate, PostEdit } from "./pages/posts";
+import { PostList, PostCreate, PostEdit } from "pages/posts";
 import { TOKEN_KEY } from "./constants";
 
 export const API_URL = "/api";
