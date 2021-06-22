7bf5e872472abbd6c1fd0b22dd4a841890931c86
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-06-22 11:47:41 +0300

    Fixed List page.

--- examples/nestjsxCrud/src/App.tsx
+++ examples/nestjsxCrud/src/App.tsx
@@ -1,10 +1,10 @@
 import { Refine, Resource } from "@pankod/refine";
-import dataProvider from "@pankod/refine-json-server";
+import dataProvider from "@pankod/refine-nestjsx-crud";
 import "@pankod/refine/dist/styles.min.css";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
 
-const API_URL = "https://api.fake-rest.refine.dev";
+const API_URL = "https://api.nestjsx-crud.refine.dev";
 
 const App: React.FC = () => {
     return (
--- examples/nestjsxCrud/src/pages/posts/list.tsx
+++ examples/nestjsxCrud/src/pages/posts/list.tsx
@@ -1,55 +1,33 @@
 import {
     List,
     Table,
-    TextField,
     useTable,
     IResourceComponentsProps,
     Space,
     EditButton,
     ShowButton,
-    useMany,
 } from "@pankod/refine";
 
-import { IPost, ICategory } from "interfaces";
+import { IPost } from "interfaces";
 
-export const PostList: React.FC<IResourceComponentsProps> = (props) => {
+export const PostList: React.FC<IResourceComponentsProps> = () => {
     const { tableProps } = useTable<IPost>();
 
-    const categoryIds =
-        tableProps?.dataSource?.map((item) => item.category.id) ?? [];
-    const { data, isLoading } = useMany<ICategory>("categories", categoryIds, {
-        enabled: categoryIds.length > 0,
-    });
-
     return (
-        <List {...props}>
+        <List>
             <Table {...tableProps} key="id">
                 <Table.Column key="id" dataIndex="id" title="ID" />
                 <Table.Column key="title" dataIndex="title" title="Title" />
                 <Table.Column
-                    dataIndex={["category", "id"]}
+                    dataIndex={["category", "title"]}
                     key="category.id"
                     title="Category"
-                    render={(value) => {
-                        if (isLoading) {
-                            return <TextField value="Loading..." />;
-                        }
-
-                        return (
-                            <TextField
-                                value={
-                                    data?.data.find((item) => item.id === value)
-                                        ?.title
-                                }
-                            />
-                        );
-                    }}
                 />
                 <Table.Column<IPost>
                     title="Actions"
                     dataIndex="actions"
                     key="actions"
-                    render={(_value, record) => (
+                    render={(_, record) => (
                         <Space>
                             <EditButton size="small" recordItemId={record.id} />
                             <ShowButton size="small" recordItemId={record.id} />
