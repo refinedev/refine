ec8956df3e7a16b42e22f6f8ea20955cf0edc082
Aydın <aydinakyol11@gmail.com>
2021-06-23 14:25:30 +0300

    use simple list example and doc (#660)
    * use simple list example and doc
    
    * Update documentation/docs/guides-and-concepts/hooks/useSimpleList.md
    
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>
    
    * Update examples/useSimpleList/tsconfig.json
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    * Update documentation/docs/guides-and-concepts/hooks/useSimpleList.md
    
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>
    
    * Update examples/useSimpleList/public/manifest.json
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    * Update examples/useSimpleList/public/index.html
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    * Update examples/useSimpleList/src/pages/posts/list.tsx
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>

---
+++ examples/useSimpleList/src/interfaces/index.d.ts
@@ -0,0 +1,12 @@
+export interface ICategory {
+    id: string;
+    title: string;
+}
+
+export interface IPost {
+    id: string;
+    title: string;
+    content: string;
+    hit: number;
+    category: ICategory;
+}
