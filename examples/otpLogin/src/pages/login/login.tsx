aad97c7d47a9070a99016dbea739ae4770e1c1a9
Salih <salihozdemir94@gmail.com>
2021-06-25 10:27:56 +0300

    refactoring login page

--- examples/otpLogin/src/pages/login/login.tsx
+++ examples/otpLogin/src/pages/login/login.tsx
@@ -14,33 +14,27 @@ import "./styles.css";
 import { useLogin } from "@pankod/refine";
 
 const { PhoneOutlined, NumberOutlined } = Icons;
+
 export interface ILoginForm {
     gsmNumber: string;
     code: string;
 }
 
 export const Login: React.FC = () => {
-    const [current, setCurrent] = useState<number>(0);
+    const [current, setCurrent] = useState<"gsmNumber" | "code">("gsmNumber");
+    const [gsmNumber, setGsmNumber] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
-    const [form] = Form.useForm();
 
     const { mutate: login } = useLogin<ILoginForm>();
 
-    const onSubmit = async (values: ILoginForm) => {
-        login(values);
+    const onGsmFormSubmit = (values: Pick<ILoginForm, "gsmNumber">) => {
+        setLoading(true);
+        setGsmNumber(values.gsmNumber);
+        setTimeout(() => setCurrent("code"), 1000);
     };
 
-    const handleSend = () => {
-        setLoading(true);
-
-        form.validateFields(["gsmNumber"])
-            .then(() =>
-                setTimeout(() => {
-                    setCurrent(current + 1);
-                    setLoading(false);
-                }, 1000),
-            )
-            .catch(() => setLoading(false));
+    const onCodeFormSubmit = async (values: Pick<ILoginForm, "code">) => {
+        login({ gsmNumber, code: values.code });
     };
 
     const CardTitle = (
@@ -63,81 +57,83 @@ export const Login: React.FC = () => {
             >
                 <Col xs={22}>
                     <Card className="login-card" title={CardTitle}>
-                        <Form
-                            layout="vertical"
-                            form={form}
-                            requiredMark={false}
-                            onFinish={onSubmit}
-                        >
-                            <Form.Item
-                                hidden={current === 1}
-                                name="gsmNumber"
-                                label="Gsm Number"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "Gsm number is required",
-                                    },
-                                ]}
+                        {current === "gsmNumber" && (
+                            <Form
+                                layout="vertical"
+                                requiredMark={false}
+                                onFinish={onGsmFormSubmit}
                             >
-                                <Input
-                                    prefix={
-                                        <PhoneOutlined
-                                            style={{ color: "#00000040" }}
-                                        />
-                                    }
-                                    maxLength={10}
-                                    placeholder="(___)___-____"
-                                />
-                            </Form.Item>
-                            <Form.Item
-                                className="login-buttons"
-                                hidden={current === 1}
-                            >
-                                <Button
-                                    type="primary"
-                                    size="large"
-                                    block
-                                    onClick={handleSend}
-                                    loading={loading}
+                                <Form.Item
+                                    name="gsmNumber"
+                                    label="Gsm Number"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "Gsm number is required",
+                                        },
+                                    ]}
                                 >
-                                    Send
-                                </Button>
-                            </Form.Item>
-                            <Form.Item
-                                hidden={current === 0}
-                                name="code"
-                                label="Code"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "Code is required",
-                                    },
-                                ]}
+                                    <Input
+                                        prefix={
+                                            <PhoneOutlined
+                                                style={{ color: "#00000040" }}
+                                            />
+                                        }
+                                        maxLength={10}
+                                        placeholder="(___)___-____"
+                                    />
+                                </Form.Item>
+                                <Form.Item className="login-buttons">
+                                    <Button
+                                        type="primary"
+                                        size="large"
+                                        htmlType="submit"
+                                        block
+                                        loading={loading}
+                                    >
+                                        Send
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        )}
+                        {current === "code" && (
+                            <Form
+                                layout="vertical"
+                                requiredMark={false}
+                                onFinish={onCodeFormSubmit}
                             >
-                                <Input
-                                    type="password"
-                                    prefix={
-                                        <NumberOutlined
-                                            style={{ color: "#00000040" }}
-                                        />
-                                    }
-                                />
-                            </Form.Item>
-                            <Form.Item
-                                className="login-buttons"
-                                hidden={current === 0}
-                            >
-                                <Button
-                                    type="primary"
-                                    size="large"
-                                    htmlType="submit"
-                                    block
+                                <Form.Item
+                                    name="code"
+                                    label="Code"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "Code is required",
+                                        },
+                                    ]}
                                 >
-                                    Login
-                                </Button>
-                            </Form.Item>
-                        </Form>
+                                    <Input
+                                        type="password"
+                                        maxLength={4}
+                                        prefix={
+                                            <NumberOutlined
+                                                style={{ color: "#00000040" }}
+                                            />
+                                        }
+                                    />
+                                </Form.Item>
+                                <Form.Item className="login-buttons">
+                                    <Button
+                                        type="primary"
+                                        size="large"
+                                        htmlType="submit"
+                                        block
+                                    >
+                                        Login
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        )}
                     </Card>
                 </Col>
             </Row>
