8ba0b1402d55e65b65c788dbe9110989054ff863
Salih <salihozdemir94@gmail.com>
2021-06-28 12:22:54 +0300

    if user has name and avatar prop header'll render

--- packages/core/src/components/layoutWrapper/components/header/index.tsx
+++ packages/core/src/components/layoutWrapper/components/header/index.tsx
@@ -1,3 +1,26 @@
-export const Header = () => {
-    return null;
+import React from "react";
+import { Layout, Typography, Avatar } from "antd";
+
+import { useGetIdentity } from "@hooks";
+
+export const Header: React.FC = () => {
+    const { data: user } = useGetIdentity();
+
+    const shouldRenderHeader = user && user.name && user.avatar;
+
+    return shouldRenderHeader ? (
+        <Layout.Header
+            style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                padding: "0px 24px 0px 24px",
+                height: "48px",
+                backgroundColor: "#FFF",
+            }}
+        >
+            <Typography.Text strong>{user.name}</Typography.Text>
+            <Avatar src={user.avatar} style={{ marginLeft: "16px" }} />
+        </Layout.Header>
+    ) : null;
 };
