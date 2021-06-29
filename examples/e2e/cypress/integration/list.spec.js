6835f94a1bce5ef3560dcf3617dd271b7268b0cf
Muharrem Kocadere <mkocadere@gmail.com>
2021-06-29 12:42:11 +0300

    add test for query when fetching new data from a different page.
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>

--- examples/e2e/cypress/integration/list.spec.js
+++ examples/e2e/cypress/integration/list.spec.js
@@ -7,21 +7,23 @@ describe("list page", () => {
         beforeEach(() => {
             cy.get("ul.ant-pagination").as("pagination");
         });
+
         it("has an item for page 1", () => {
             cy.get("@pagination")
                 .find('li[title="1"].ant-pagination-item-1')
                 .as("item-1");
+        });
+        it("must fetch data with true query", () => {
+            cy.intercept("GET", "/posts*").as("getPosts");
 
             cy.get("@pagination")
                 .find('li[title="2"].ant-pagination-item-2')
                 .as("item-2");
 
-            cy.intercept("GET", "**/posts/*").as("getPosts");
-
             cy.get("@item-2").click();
 
-            cy.wait("@getPosts").then((data) => {
-                console.log({ data });
+            cy.wait("@getPosts").should(({ request }) => {
+                expect(request.url).contain("_end=20&_start=10&");
             });
         });
     });
