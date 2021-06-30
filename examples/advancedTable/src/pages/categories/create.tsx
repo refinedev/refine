b680d2a78aa31536421e618dc08d1d8b3c93947b
Salih Özdemir <salihozdemir94@gmail.com>
2021-06-30 13:27:31 +0300

    Advanced table example (#713)
    * copy of base project
    
    * add expandable row
    
    * add editableRow
    
    * add useDeleteMany
    
    * Add responsibility
    
    * refactoring views
    
    * add category resource
    
    * add missing post pages
    
    * refactoring nested table example
    
    * remove unnecesary import
    
    * add embed code

---
+++ examples/advancedTable/src/pages/categories/create.tsx
@@ -0,0 +1,35 @@
+import {
+    Checkbox,
+    Create,
+    Form,
+    Input,
+    IResourceComponentsProps,
+    useForm,
+} from "@pankod/refine";
+
+import { ICategory } from "interfaces";
+
+export const CategoryCreate: React.FC<IResourceComponentsProps> = () => {
+    const { formProps, saveButtonProps } = useForm<ICategory>();
+
+    return (
+        <Create saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Active" name="active" valuePropName="checked">
+                    <Checkbox>Active</Checkbox>
+                </Form.Item>
+            </Form>
+        </Create>
+    );
+};
---
+++ examples/advancedTable/src/pages/categories/list.tsx
@@ -0,0 +1,93 @@
+import {
+    List,
+    Table,
+    useTable,
+    IResourceComponentsProps,
+    Space,
+    EditButton,
+    ShowButton,
+    BooleanField,
+} from "@pankod/refine";
+
+import { ICategory, IPost } from "interfaces";
+
+export const CategoryList: React.FC<IResourceComponentsProps> = () => {
+    const { tableProps } = useTable<ICategory>();
+
+    return (
+        <List>
+            <Table
+                expandable={{
+                    expandedRowRender,
+                }}
+                {...tableProps}
+                rowKey="id"
+                scroll={{ x: 400 }}
+            >
+                <Table.Column dataIndex="id" title="ID" />
+                <Table.Column dataIndex="title" title="Title" />
+                <Table.Column
+                    dataIndex="active"
+                    title="Active"
+                    align="center"
+                    render={(value) => {
+                        return <BooleanField value={value} />;
+                    }}
+                />
+                <Table.Column<ICategory>
+                    title="Actions"
+                    dataIndex="actions"
+                    render={(_, record) => (
+                        <Space>
+                            <EditButton size="small" recordItemId={record.id} />
+                            <ShowButton size="small" recordItemId={record.id} />
+                        </Space>
+                    )}
+                />
+            </Table>
+        </List>
+    );
+};
+
+const CategoryPostsTable: React.FC<{ record: ICategory }> = ({ record }) => {
+    const { tableProps: postTableProps } = useTable<IPost>({
+        resource: "posts",
+        permanentFilter: [
+            {
+                field: "category.id",
+                operator: "eq",
+                value: record.id,
+            },
+        ],
+    });
+
+    return (
+        <Table {...postTableProps} rowKey="id">
+            <Table.Column dataIndex="id" title="ID" />
+            <Table.Column dataIndex="title" title="Title" />
+            <Table.Column dataIndex="status" title="Status" align="center" />
+            <Table.Column<IPost>
+                title="Actions"
+                dataIndex="actions"
+                render={(_, record) => (
+                    <Space>
+                        <EditButton
+                            size="small"
+                            resourceName="posts"
+                            recordItemId={record.id}
+                        />
+                        <ShowButton
+                            size="small"
+                            resourceName="posts"
+                            recordItemId={record.id}
+                        />
+                    </Space>
+                )}
+            />
+        </Table>
+    );
+};
+
+const expandedRowRender = (record: ICategory) => {
+    return <CategoryPostsTable record={record} />;
+};
