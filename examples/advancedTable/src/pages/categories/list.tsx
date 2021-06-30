409fe4128094bb98caf4137f8610071f7e9cc6df
Salih <salihozdemir94@gmail.com>
2021-06-30 10:58:40 +0300

    refactoring nested table example

--- examples/advancedTable/src/pages/categories/list.tsx
+++ examples/advancedTable/src/pages/categories/list.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import {
     List,
     Table,
@@ -14,59 +13,12 @@ import { ICategory, IPost } from "interfaces";
 
 export const CategoryList: React.FC<IResourceComponentsProps> = () => {
     const { tableProps } = useTable<ICategory>();
-    const [selectedRowId, setSelectedRowId] = useState<string>();
-
-    const { tableProps: postTableProps } = useTable<IPost>({
-        resource: "posts",
-        permanentFilter: [
-            {
-                field: "category.id",
-                operator: "eq",
-                value: selectedRowId,
-            },
-        ],
-    });
-
-    const expandedRowRender = () => {
-        return (
-            <Table {...postTableProps} rowKey="id">
-                <Table.Column dataIndex="id" title="ID" />
-                <Table.Column dataIndex="title" title="Title" />
-                <Table.Column
-                    dataIndex="status"
-                    title="Status"
-                    align="center"
-                />
-                <Table.Column<IPost>
-                    title="Actions"
-                    dataIndex="actions"
-                    render={(_, record) => (
-                        <Space>
-                            <EditButton
-                                size="small"
-                                resourceName="posts"
-                                recordItemId={record.id}
-                            />
-                            <ShowButton
-                                size="small"
-                                resourceName="posts"
-                                recordItemId={record.id}
-                            />
-                        </Space>
-                    )}
-                />
-            </Table>
-        );
-    };
 
     return (
         <List>
             <Table
                 expandable={{
                     expandedRowRender,
-                    expandedRowKeys: [selectedRowId as string],
-                    onExpand: (expandable, record) =>
-                        setSelectedRowId(expandable ? record.id : ""),
                 }}
                 {...tableProps}
                 rowKey="id"
@@ -95,4 +47,47 @@ export const CategoryList: React.FC<IResourceComponentsProps> = () => {
             </Table>
         </List>
     );
+};
+
+const CategoryPostsTable: React.FC<{ record: ICategory }> = ({ record }) => {
+    const { tableProps: postTableProps } = useTable<IPost>({
+        resource: "posts",
+        permanentFilter: [
+            {
+                field: "category.id",
+                operator: "eq",
+                value: record.id,
+            },
+        ],
+    });
+
+    return (
+        <Table {...postTableProps} rowKey="id">
+            <Table.Column dataIndex="id" title="ID" />
+            <Table.Column dataIndex="title" title="Title" />
+            <Table.Column dataIndex="status" title="Status" align="center" />
+            <Table.Column<IPost>
+                title="Actions"
+                dataIndex="actions"
+                render={(_, record) => (
+                    <Space>
+                        <EditButton
+                            size="small"
+                            resourceName="posts"
+                            recordItemId={record.id}
+                        />
+                        <ShowButton
+                            size="small"
+                            resourceName="posts"
+                            recordItemId={record.id}
+                        />
+                    </Space>
+                )}
+            />
+        </Table>
+    );
+};
+
+const expandedRowRender = (record: ICategory) => {
+    return <CategoryPostsTable record={record} />;
 };
