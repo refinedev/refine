f7ded3c8db75ff06a0bf8c001baca5e10453b07a
Necati Özmen <necatiozmn@gmail.com>
2021-07-06 15:54:22 +0300

    Update examples/e2e/src/pages/posts/list.tsx
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- examples/e2e/src/pages/posts/list.tsx
+++ examples/e2e/src/pages/posts/list.tsx
@@ -22,7 +22,7 @@ export const PostList: React.FC<IResourceComponentsProps> = (props) => {
     });
 
     return (
-        <List {...props}>
+        <List>
             <Table {...tableProps} key="id">
                 <Table.Column key="id" dataIndex="id" title="ID" />
                 <Table.Column key="title" dataIndex="title" title="Title" />
