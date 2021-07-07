4f4e8eacc94c31777a5e15980c9a43434e1b09de
Umut Özdemir <umutozdemir997@gmail.com>
2021-07-07 16:35:25 +0300

    Update examples/customValidation/src/pages/posts/list.tsx
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>

--- examples/customValidation/src/pages/posts/list.tsx
+++ examples/customValidation/src/pages/posts/list.tsx
@@ -12,7 +12,7 @@ import {
 
 import { IPost, ICategory } from "interfaces";
 
-export const PostList: React.FC<IResourceComponentsProps> = (props) => {
+export const PostList: React.FC = (props) => {
     const { tableProps } = useTable<IPost>();
 
     const categoryIds =
