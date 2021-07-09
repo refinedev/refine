588c375353bf3470dbc694cc68225b2859bd4200
necatiozmen <necatiozmn@gmail.com>
2021-07-09 13:54:57 +0300

    Add checkbox to list Co-authored-by: mhrrmk <mkocadere@gmail.com>

--- examples/base/src/pages/posts/list.tsx
+++ examples/base/src/pages/posts/list.tsx
@@ -8,12 +8,21 @@ import {
     EditButton,
     ShowButton,
     useMany,
+    FilterDropdown,
+    Typography,
+    Checkbox,
+    useCheckboxGroup,
+    useSelect,
+    Select,
+    Radio,
+    TagField,
 } from "@pankod/refine";
 
 import { IPost, ICategory } from "interfaces";
+const { Title } = Typography;
 
 export const PostList: React.FC<IResourceComponentsProps> = () => {
-    const { tableProps } = useTable<IPost>();
+    const { tableProps, filters } = useTable<IPost>();
 
     const categoryIds =
         tableProps?.dataSource?.map((item) => item.category.id) ?? [];
@@ -21,11 +30,32 @@ export const PostList: React.FC<IResourceComponentsProps> = () => {
         enabled: categoryIds.length > 0,
     });
 
+    const { checkboxGroupProps } = useCheckboxGroup<IPost>({
+        resource: "categories",
+    });
+    const { selectProps: categorySelectProps } = useSelect<ICategory>({
+        resource: "categories",
+        optionLabel: "title",
+        optionValue: "id",
+    });
+
     return (
         <List>
             <Table {...tableProps} rowKey="id">
                 <Table.Column dataIndex="id" title="ID" />
-                <Table.Column dataIndex="title" title="Title" />
+                <Table.Column
+                    dataIndex="title"
+                    title="Title"
+                    filterDropdown={(props) => (
+                        <FilterDropdown {...props}>
+                            <Title level={5}>Rol Seçin</Title>
+                            <Checkbox.Group
+                                style={{ width: 200 }}
+                                {...checkboxGroupProps}
+                            />
+                        </FilterDropdown>
+                    )}
+                />
                 <Table.Column
                     dataIndex={["category", "id"]}
                     title="Category"
@@ -43,6 +73,39 @@ export const PostList: React.FC<IResourceComponentsProps> = () => {
                             />
                         );
                     }}
+                    filterDropdown={(props) => (
+                        <FilterDropdown test {...props}>
+                            <Select
+                                style={{ minWidth: 200 }}
+                                mode="multiple"
+                                placeholder="Select Category"
+                                {...categorySelectProps}
+                            />
+                        </FilterDropdown>
+                    )}
+                    /*  filterDropdown={(props) => (
+                        <FilterDropdown {...props}>
+                            <Title level={5}>Rol Seçin</Title>
+                            <Checkbox.Group
+                                style={{ width: 200 }}
+                                {...checkboxGroupProps}
+                            />
+                        </FilterDropdown>
+                    )} */
+                />
+                <Table.Column
+                    dataIndex="status"
+                    title="Status"
+                    render={(value: string) => <TagField value={value} />}
+                    filterDropdown={(props: any) => (
+                        <FilterDropdown {...props}>
+                            <Radio.Group>
+                                <Radio value="published">Published</Radio>
+                                <Radio value="draft">Draft</Radio>
+                                <Radio value="rejected">Rejected</Radio>
+                            </Radio.Group>
+                        </FilterDropdown>
+                    )}
                 />
                 <Table.Column<IPost>
                     title="Actions"
