48cef5ab8407ee33665993dbe7df2e58b465ed24
Salih Özdemir <salihozdemir94@gmail.com>
2021-07-27 08:30:38 +0300

    add tsdoc for hooks of fields (#863)
    * add useCheckboxGroup tsdoc
    
    * add useRadioGroup tsdoc
    
    * add useSelect tsdoc
    
    * add internal tsdocs
    
    * add typeParam to fields hook

--- packages/core/src/hooks/fields/useCheckboxGroup/index.ts
+++ packages/core/src/hooks/fields/useCheckboxGroup/index.ts
@@ -26,6 +26,14 @@ export type UseCheckboxGroupReturnType<TData extends BaseRecord = BaseRecord> =
         queryResult: QueryObserverResult<GetListResponse<TData>>;
     };
 
+/**
+ * `useCheckboxGroup` hook allows you to manage an Ant Design {@link https://ant.design/components/checkbox/#components-checkbox-demo-group Checkbox.Group} component when records in a resource needs to be used as checkbox options.
+ *
+ * @see {@link https://refine.dev/docs/api-references/hooks/field/useCheckboxGroup} for more details.
+ *
+ * @typeParam TData - Result data of the query extends {@link https://refine.dev/docs/api-references/interfaceReferences#baserecord `BaseRecord`}
+ *
+ */
 export const useCheckboxGroup = <TData extends BaseRecord = BaseRecord>({
     resource,
     sort,
