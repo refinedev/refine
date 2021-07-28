1085971521e609fce5b93fa080f1a1eacad2c38f
Umut Özdemir <umutozdemir997@gmail.com>
2021-07-28 11:33:23 +0300

    add tsdoc for <CloneButton>

--- packages/core/src/components/buttons/clone/index.tsx
+++ packages/core/src/components/buttons/clone/index.tsx
@@ -7,10 +7,23 @@ import { useNavigation, useTranslate } from "@hooks";
 import { ResourceRouterParams } from "../../../interfaces";
 
 type CloneButtonProps = ButtonProps & {
+    /**
+     * Determines which resource to use for redirection
+     * @default Resource name that it reads from route
+     */
     resourceName?: string;
+    /**
+     * Adds `id` to the end of the URL
+     * @default Record id that it reads from route
+     */
     recordItemId?: string;
 };
 
+/**
+ * `<CloneButton>` uses Ant Design's {@link https://ant.design/components/button/ `<Button> component`}.
+ * It uses the {@link https://refine.dev/docs/api-references/hooks/navigation/useNavigation#clone clone} method from {@link https://refine.dev/docs/api-references/hooks/navigation/useNavigation useNavigation} under the hood.
+ * It can be useful when redirecting the app to the create page with the record id route of {@link https://refine.dev/docs/api-references/components/resource `<Resource>`}.
+ */
 export const CloneButton: FC<CloneButtonProps> = ({
     resourceName,
     recordItemId,
