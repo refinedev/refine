0bd9e1d2d50613c7fcc4dd6001648a899257ff09
Salih <salihozdemir94@gmail.com>
2021-07-29 16:16:46 +0300

    fix useLogin types

--- packages/core/src/hooks/auth/useLogin/index.ts
+++ packages/core/src/hooks/auth/useLogin/index.ts
@@ -8,19 +8,24 @@ import { useNavigation } from "@hooks/navigation";
 
 import { IAuthContext } from "../../../interfaces";
 
+export type UseLoginReturnType<TData, TVariables = {}> = UseMutationResult<
+    TData,
+    unknown,
+    TVariables,
+    unknown
+>;
 /**
  * `useLogin` calls `login` method from {@link https://refine.dev/docs/api-references/providers/auth-provider `authProvider`} under the hood.
  *
  * @see {@link https://refine.dev/docs/api-references/hooks/auth/useLogin} for more details.
  *
- * @typeParam TVariables - Values for mutation function
+ * @typeParam TData - Result data of the query
+ * @typeParam TVariables - Values for mutation function. default `{}`
  *
  */
-export const useLogin = <TVariables = any>(): UseMutationResult<
-    any,
-    Error,
-    TVariables,
-    unknown
+export const useLogin = <TData, TVariables = {}>(): UseLoginReturnType<
+    TData,
+    TVariables
 > => {
     const { replace } = useNavigation();
     const { login: loginFromContext } =
@@ -29,14 +34,14 @@ export const useLogin = <TVariables = any>(): UseMutationResult<
     const location = useLocation<{ from: string }>();
     const { from } = location.state || { from: { pathname: "/" } };
 
-    const queryResponse = useMutation<any, Error, TVariables, unknown>(
+    const queryResponse = useMutation<TData, unknown, TVariables, unknown>(
         "useLogin",
         loginFromContext,
         {
             onSuccess: () => {
                 replace(from);
             },
-            onError: (error: Error) => {
+            onError: (error: any) => {
                 notification.error({
                     message: error?.name || "Login Error",
                     description: error?.message || "Invalid credentials",
