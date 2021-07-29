609f48eb3b0068bab0cd3ef456950c519576d15c
Salih <salihozdemir94@gmail.com>
2021-07-29 16:17:18 +0300

    fix wrong link

--- packages/core/src/hooks/auth/useLogout/index.ts
+++ packages/core/src/hooks/auth/useLogout/index.ts
@@ -13,7 +13,7 @@ import { useNavigation } from "@hooks/navigation";
 /**
  * `useLogout` calls the `logout` method from the {@link https://refine.dev/docs/api-references/providers/auth-provider `authProvider`} under the hood.
  *
- * @see {@link https://refine.dev/docs/api-references/hooks/auth/useLogin} for more details.
+ * @see {@link https://refine.dev/docs/api-references/hooks/auth/useLogout} for more details.
  *
  */
 export const useLogout = (): UseMutationResult<
