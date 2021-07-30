2d2b39fe284144c7b057cbf36d5d3a47543a3ecb
Umut Özdemir <umutozdemir997@gmail.com>
2021-07-30 12:33:07 +0300

    add TSDoc for  components/fields (#904)
    * add TSDoc for  <BooleanField>
    
    * add TSDocs for <DateField>
    
    * add TSDoc for <EmailField>
    
    * add TSDoc for <FileField>
    
    * add TSDocs for <ImageField>
    
    * add TSDoc for <MarkdownField>
    
    * add TSDoc for <NumberField>
    
    * add TSDoc for <TagField>
    
    * add TSDoc for <TextField>
    
    * add TSDoc for <LinkField>

--- packages/core/src/components/fields/file/index.tsx
+++ packages/core/src/components/fields/file/index.tsx
@@ -8,6 +8,11 @@ export type FileFieldProps = LinkProps & {
     src: string;
 };
 
+/**
+ * This field is used to display files and uses {@link https://ant.design/components/typography `<Typography.Link>`} from Ant Design.
+ *
+ * @see {@link https://refine.dev/docs/api-references/components/fields/file} for more details.
+ */
 export const FileField: React.FC<FileFieldProps> = ({
     title,
     src,
