e436a8d97a3e8476486b89fbc60e8203a5d49c74
necatiozmen <necatiozmn@gmail.com>
2021-08-03 10:42:22 +0300

    Finish order page Co-authored-by: mhrrmk <mkocadere@gmail.com>

--- examples/fineFoods/src/components/orderStatus/index.tsx
+++ examples/fineFoods/src/components/orderStatus/index.tsx
@@ -14,13 +14,13 @@ export const OrderStatus: React.FC<OrderStatusProps> = ({ status }) => {
             color = "#F39800";
             break;
         case "ready":
-            color = "#B033AB";
+            color = "#36CFC9";
             break;
         case "on the way":
-            color = "#1890FF";
+            color = "#67BE23";
             break;
         case "delivered":
-            color = "#52C41A";
+            color = "#1890FF";
             break;
         case "cancelled":
             color = "#F5222D";
@@ -30,9 +30,8 @@ export const OrderStatus: React.FC<OrderStatusProps> = ({ status }) => {
     const { Text } = Typography;
 
     return (
-        <Text style={styles.container}>
-            <Text style={{ ...styles.icon, backgroundColor: color }}></Text>
+        <div style={{ ...styles.container, backgroundColor: color }}>
             <Text style={styles.text}>{status}</Text>
-        </Text>
+        </div>
     );
 };
