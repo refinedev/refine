c05eda5564e1c8b8c0ad9def04c620556c067b8f
Umut Özdemir <umutozdemir997@gmail.com>
2021-08-04 13:47:47 +0300

    finalize reviews list page

--- examples/fineFoods/src/pages/reviews/list.tsx
+++ examples/fineFoods/src/pages/reviews/list.tsx
@@ -5,6 +5,7 @@ import {
     useTable,
     useTranslate,
     useUpdateMany,
+    useNavigation,
     useUpdate,
     IResourceComponentsProps,
     Icons,
@@ -12,6 +13,7 @@ import {
     Button,
     Avatar,
     Rate,
+    Typography,
 } from "@pankod/refine";
 
 import { IReview } from "interfaces";
@@ -20,6 +22,9 @@ export const ReviewsList: React.FC<IResourceComponentsProps> = () => {
     const [selectedRowKeys, setSelectedRowKeys] = React.useState<React.Key[]>(
         [],
     );
+
+    const t = useTranslate();
+    const { show } = useNavigation();
 
     const { tableProps } = useTable<IReview>({
         permanentFilter: [
@@ -72,19 +77,23 @@ export const ReviewsList: React.FC<IResourceComponentsProps> = () => {
         });
     };
 
-    const t = useTranslate();
     const hasSelected = selectedRowKeys.length > 0;
 
     return (
         <List
-            title={"Reviews"}
+            title={t("reviews:title")}
             pageHeaderProps={{
                 subTitle: hasSelected && (
-                    <Space>
+                    <Space style={{ gap: 0, marginLeft: "1em" }}>
                         <Button
                             type="text"
                             onClick={() => updateSelectedItems("approved")}
                             loading={updateManyIsLoading}
+                            icon={
+                                <Icons.CheckCircleOutlined
+                                    style={{ color: "green" }}
+                                />
+                            }
                         >
                             Accept All
                         </Button>
@@ -92,7 +101,11 @@ export const ReviewsList: React.FC<IResourceComponentsProps> = () => {
                             type="text"
                             onClick={() => updateSelectedItems("rejected")}
                             loading={updateManyIsLoading}
-                            danger
+                            icon={
+                                <Icons.CloseCircleOutlined
+                                    style={{ color: "red" }}
+                                />
+                            }
                         >
                             Reject All
                         </Button>
@@ -111,18 +124,61 @@ export const ReviewsList: React.FC<IResourceComponentsProps> = () => {
                 <Table.Column
                     dataIndex={["user", "avatar"]}
                     render={(value) => <Avatar size={74} src={value[0].url} />}
+                    width={105}
                 />
-                <Table.Column dataIndex={["user", "fullName"]} title={"User"} />
-                <Table.Column dataIndex={["order", "id"]} title={"Order ID"} />
+                <Table.Column
+                    dataIndex={["user", "fullName"]}
+                    title={t("reviews:fields:user")}
+                />
+                <Table.Column
+                    dataIndex={["order", "id"]}
+                    title={t("reviews:fields:orderId")}
+                    render={(value) => (
+                        <Button
+                            onClick={() => {
+                                show("reviews", value);
+                            }}
+                            type="text"
+                        >
+                            #{value}
+                        </Button>
+                    )}
+                />
                 <Table.Column
                     width={250}
                     dataIndex="comment"
-                    title={"Review"}
+                    title={t("reviews:fields:review")}
                 />
                 <Table.Column
                     dataIndex="star"
-                    title={"Rating"}
-                    render={(value) => <Rate disabled value={value} />}
+                    title={t("reviews:fields:rating")}
+                    align="center"
+                    render={(value) => (
+                        <Space
+                            direction="vertical"
+                            style={{
+                                rowGap: 0,
+                            }}
+                        >
+                            <Typography.Text
+                                style={{
+                                    fontSize: 31,
+                                    fontWeight: 800,
+                                }}
+                            >
+                                {value}
+                            </Typography.Text>
+                            <Rate
+                                character={
+                                    <Icons.StarOutlined
+                                        style={{ color: "#FA8C16" }}
+                                    />
+                                }
+                                disabled
+                                value={value}
+                            />
+                        </Space>
+                    )}
                 />
                 <Table.Column<IReview>
                     title={t("common:table.actions")}
@@ -132,13 +188,13 @@ export const ReviewsList: React.FC<IResourceComponentsProps> = () => {
                             <Button
                                 danger
                                 icon={<Icons.CloseCircleOutlined />}
-                                onClick={() => handleUpdate(record, "approved")}
+                                onClick={() => handleUpdate(record, "rejected")}
                             >
                                 Reject
                             </Button>
                             <Button
                                 icon={<Icons.CheckCircleOutlined />}
-                                onClick={() => handleUpdate(record, "rejected")}
+                                onClick={() => handleUpdate(record, "approved")}
                                 type="primary"
                             >
                                 Accept
