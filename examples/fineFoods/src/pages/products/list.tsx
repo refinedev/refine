4f1643b14565b424d76d37314666038f52a497d3
Salih <salihozdemir94@gmail.com>
2021-08-09 16:48:02 +0300

    update product list page

--- examples/fineFoods/src/pages/products/list.tsx
+++ examples/fineFoods/src/pages/products/list.tsx
@@ -1,92 +1,169 @@
 import {
-    List,
-    Table,
-    useTable,
     useTranslate,
     IResourceComponentsProps,
-    DateField,
-    Space,
-    ShowButton,
-    NumberField,
-    BooleanField,
-    EditButton,
+    useSimpleList,
+    CrudFilters,
+    Row,
+    AntdList,
+    Col,
+    CreateButton,
+    Form,
+    Input,
+    Icons,
+    Typography,
+    useDrawerForm,
 } from "@pankod/refine";
+
+const { Text } = Typography;
+const { SearchOutlined } = Icons;
+
+import {
+    ProductItem,
+    ProductCategoryFilter,
+    CreateProduct,
+    EditProduct,
+} from "components/product";
 
 import { IProduct } from "interfaces";
 
 export const ProductList: React.FC<IResourceComponentsProps> = () => {
-    const { tableProps } = useTable<IProduct>({
-        initialSorter: [
-            {
-                field: "id",
-                order: "desc",
-            },
-        ],
-    });
-
     const t = useTranslate();
 
+    const { listProps, searchFormProps } = useSimpleList<
+        IProduct,
+        { name: string; categories: string[] }
+    >({
+        resource: "products",
+        pagination: { pageSize: 12 },
+        onSearch: ({ name, categories }) => {
+            const productFilters: CrudFilters = [];
+
+            if (categories.length > 0) {
+                productFilters.push({
+                    field: "category.id",
+                    operator: "in",
+                    value: categories,
+                });
+            }
+
+            if (name) {
+                productFilters.push({
+                    field: "name",
+                    operator: "contains",
+                    value: name,
+                });
+            }
+
+            return productFilters;
+        },
+    });
+
+    const {
+        drawerProps: createDrawerProps,
+        formProps: createFormProps,
+        saveButtonProps: createSaveButtonProps,
+        show: createShow,
+    } = useDrawerForm<IProduct>({
+        action: "create",
+        resource: "products",
+        redirect: false,
+    });
+
+    const {
+        drawerProps: editDrawerProps,
+        formProps: editFormProps,
+        saveButtonProps: editSaveButtonProps,
+        show: editShow,
+    } = useDrawerForm<IProduct>({
+        action: "edit",
+        resource: "products",
+        redirect: false,
+    });
+
     return (
-        <List title={t("products:title")}>
-            <Table {...tableProps} rowKey="id">
-                <Table.Column
-                    key="id"
-                    dataIndex="id"
-                    title={t("products:fields.id")}
-                />
-                <Table.Column
-                    key="name"
-                    dataIndex="name"
-                    title={t("products:fields.name")}
-                />
-                <Table.Column
-                    align="right"
-                    key="price"
-                    dataIndex="price"
-                    title={t("products:fields.price")}
-                    render={(value) => {
-                        return (
-                            <NumberField
-                                options={{
-                                    currency: "USD",
-                                    style: "currency",
-                                    notation: "compact",
-                                }}
-                                value={value}
-                            />
-                        );
-                    }}
-                />
-                <Table.Column
-                    key="category"
-                    dataIndex={["category", "title"]}
-                    title={t("products:fields.category")}
-                />
-                <Table.Column
-                    key="isActive"
-                    dataIndex="isActive"
-                    title={t("products:fields.isActive")}
-                    render={(value) => <BooleanField value={value} />}
-                />
-                <Table.Column
-                    key="createdAt"
-                    dataIndex="createdAt"
-                    title={t("products:fields.createdAt")}
-                    render={(value) => <DateField value={value} format="LLL" />}
-                    sorter
-                />
-                <Table.Column<IProduct>
-                    title={t("common:table.actions")}
-                    dataIndex="actions"
-                    key="actions"
-                    render={(_value, record) => (
-                        <Space>
-                            <EditButton size="small" recordItemId={record.id} />
-                            <ShowButton size="small" recordItemId={record.id} />
-                        </Space>
-                    )}
-                />
-            </Table>
-        </List>
+        <>
+            <Form
+                {...searchFormProps}
+                onValuesChange={() => searchFormProps.form?.submit()}
+            >
+                <Row
+                    gutter={[16, 16]}
+                    style={{ background: "#fff", padding: "16px 24px" }}
+                >
+                    <Col xs={24} sm={18}>
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "space-between",
+                                flexWrap: "wrap",
+                                marginBottom: "16px",
+                            }}
+                        >
+                            <Text style={{ fontSize: "24px" }} strong>
+                                {t("stores:storeProducts")}
+                            </Text>
+                            <Form.Item name="name" noStyle>
+                                <Input
+                                    style={{ width: "400px" }}
+                                    placeholder={t("stores:productSearch")}
+                                    suffix={<SearchOutlined />}
+                                />
+                            </Form.Item>
+                            <CreateButton onClick={() => createShow()}>
+                                {t("stores:buttons.addProduct")}
+                            </CreateButton>
+                        </div>
+                        <AntdList
+                            grid={{
+                                gutter: 8,
+                                xs: 1,
+                                sm: 1,
+                                md: 2,
+                                lg: 3,
+                                xl: 4,
+                                xxl: 4,
+                            }}
+                            style={{
+                                height: "100%",
+                                overflow: "auto",
+                                paddingRight: "4px",
+                            }}
+                            {...listProps}
+                            renderItem={(item) => (
+                                <ProductItem item={item} editShow={editShow} />
+                            )}
+                        />
+                    </Col>
+                    <Col xs={0} sm={6}>
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                height: "40px",
+                                marginBottom: "16px",
+                            }}
+                        >
+                            <Text style={{ fontWeight: 500 }}>
+                                {t("stores:tagFilterDescription")}
+                            </Text>
+                        </div>
+                        <Form.Item name="categories">
+                            <ProductCategoryFilter />
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </Form>
+            <CreateProduct
+                drawerProps={createDrawerProps}
+                formProps={createFormProps}
+                saveButtonProps={createSaveButtonProps}
+            />
+            <EditProduct
+                drawerProps={editDrawerProps}
+                formProps={editFormProps}
+                saveButtonProps={editSaveButtonProps}
+            />
+        </>
     );
 };
