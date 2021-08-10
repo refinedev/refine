9c18b1f7c5e79a0b0ea3d9e6776e43934ce40ebd
Salih Özdemir <salihozdemir94@gmail.com>
2021-08-10 16:23:19 +0300

    Product Pages (#959)
    * pages split into pieces
    
    * split components to storeProducts
    
    * delete unnecesary files
    
    * change classes to inline style
    
    * update product list page
    
    * add responsive view to store product modal
    
    * fix translate keys
    
    * add gap to flex container
    
    * fix translate key
    
    * fix type props names
    
    * add HttpError type
    
    * Merge branch 'master' of github.com:pankod/refine into products-finefoods

---
+++ examples/fineFoods/src/components/store/storeProducts.tsx
@@ -0,0 +1,221 @@
+import {
+    Typography,
+    Row,
+    Col,
+    AntdList,
+    useSimpleList,
+    useTranslate,
+    Input,
+    CreateButton,
+    Form,
+    CrudFilters,
+    useUpdate,
+    Icons,
+    useDrawerForm,
+    Modal,
+    ModalProps,
+    HttpError,
+} from "@pankod/refine";
+
+const { Text } = Typography;
+const { SearchOutlined } = Icons;
+
+import { IStore, IProduct } from "interfaces";
+import {
+    CreateProduct,
+    EditProduct,
+    ProductItem,
+    ProductCategoryFilter,
+} from "components/product";
+
+type StoreProductsProps = {
+    record: IStore;
+    modalProps: ModalProps;
+};
+
+export const StoreProducts: React.FC<StoreProductsProps> = ({
+    record,
+    modalProps,
+}) => {
+    const t = useTranslate();
+
+    const { listProps, searchFormProps, queryResult } = useSimpleList<
+        IProduct,
+        HttpError,
+        { name: string; categories: string[] }
+    >({
+        resource: "products",
+        pagination: { pageSize: 9 },
+        onSearch: ({ name, categories }) => {
+            const productFilters: CrudFilters = [];
+
+            if (categories.length > 0) {
+                productFilters.push({
+                    field: "category.id",
+                    operator: "in",
+                    value: categories,
+                });
+            }
+
+            if (name) {
+                productFilters.push({
+                    field: "name",
+                    operator: "contains",
+                    value: name,
+                });
+            }
+
+            return productFilters;
+        },
+    });
+    const { data: productData } = queryResult;
+
+    const mergedData = productData?.data.map((product) => ({
+        ...record?.products.find(
+            (storeProduct) => storeProduct.id === product.id,
+        ),
+        ...product,
+    }));
+
+    const { mutate } = useUpdate<IStore>();
+
+    const updateStock = (changedValue: number, clickedProduct: IProduct) => {
+        const shopProduct = record.products.find(
+            (p) => p.id === clickedProduct.id,
+        );
+
+        if (shopProduct) {
+            shopProduct.stock = changedValue;
+
+            mutate({
+                id: record.id,
+                resource: "stores",
+                values: {
+                    products: record.products,
+                },
+                successNotification: false,
+                mutationMode: "optimistic",
+            });
+        }
+    };
+
+    const {
+        drawerProps: createDrawerProps,
+        formProps: createFormProps,
+        saveButtonProps: createSaveButtonProps,
+        show: createShow,
+    } = useDrawerForm<IProduct>({
+        action: "create",
+        resource: "products",
+        redirect: false,
+    });
+
+    const {
+        drawerProps: editDrawerProps,
+        formProps: editFormProps,
+        saveButtonProps: editSaveButtonProps,
+        show: editShow,
+    } = useDrawerForm<IProduct>({
+        action: "edit",
+        resource: "products",
+        redirect: false,
+    });
+
+    return (
+        <>
+            <Modal
+                {...modalProps}
+                width={1000}
+                footer={null}
+                bodyStyle={{ minHeight: "650px" }}
+            >
+                <Form
+                    {...searchFormProps}
+                    onValuesChange={() => searchFormProps.form?.submit()}
+                >
+                    <Row gutter={[16, 16]}>
+                        <Col xs={24} sm={18}>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "space-between",
+                                    flexWrap: "wrap",
+                                    gap: "8px",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                <Text style={{ fontSize: "24px" }} strong>
+                                    {t("stores:storeProducts")}
+                                </Text>
+                                <Form.Item name="name" noStyle>
+                                    <Input
+                                        style={{ width: "300px" }}
+                                        placeholder={t("stores:productSearch")}
+                                        suffix={<SearchOutlined />}
+                                    />
+                                </Form.Item>
+                                <CreateButton onClick={() => createShow()}>
+                                    {t("stores:buttons.addProduct")}
+                                </CreateButton>
+                            </div>
+                            <AntdList
+                                grid={{
+                                    gutter: 8,
+                                    xs: 1,
+                                    sm: 1,
+                                    md: 2,
+                                    lg: 3,
+                                    xl: 3,
+                                    xxl: 3,
+                                }}
+                                style={{
+                                    height: "100%",
+                                    maxHeight: "548px",
+                                    overflow: "auto",
+                                    paddingRight: "4px",
+                                }}
+                                {...listProps}
+                                dataSource={mergedData}
+                                renderItem={(item) => (
+                                    <ProductItem
+                                        item={item}
+                                        updateStock={updateStock}
+                                        editShow={editShow}
+                                    />
+                                )}
+                            />
+                        </Col>
+                        <Col xs={0} sm={6}>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    height: "40px",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                <Text style={{ fontWeight: 500 }}>
+                                    {t("stores:tagFilterDescription")}
+                                </Text>
+                            </div>
+                            <Form.Item name="categories">
+                                <ProductCategoryFilter />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+            </Modal>
+            <CreateProduct
+                drawerProps={createDrawerProps}
+                formProps={createFormProps}
+                saveButtonProps={createSaveButtonProps}
+            />
+            <EditProduct
+                drawerProps={editDrawerProps}
+                formProps={editFormProps}
+                saveButtonProps={editSaveButtonProps}
+            />
+        </>
+    );
+};
--- examples/fineFoods/src/pages/stores/index.ts
+++ examples/fineFoods/src/pages/stores/index.ts
@@ -1,6 +1,3 @@
 export * from "./list";
 export * from "./edit";
 export * from "./create";
-export * from "./storeProducts";
-export * from "./createProduct";
-export * from "./editProduct";
