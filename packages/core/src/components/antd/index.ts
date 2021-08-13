5d59e1413ee160c6bd0a8987baf9a39c717a9f23
Ömer Faruk APLAK <omer@pankod.com>
2021-08-13 19:25:25 +0300

    fix datePicker, calendar and timePicker exports

--- packages/core/src/components/antd/index.ts
+++ packages/core/src/components/antd/index.ts
@@ -1,4 +1,4 @@
 export * from "./antd";
-export * from "./timePicker";
-export * from "./datePicker";
-export * from "./calendar";
+export { default as DatePicker } from "./datePicker";
+export { default as Calendar } from "./calendar";
+export { default as TimePicker } from "./timePicker";
