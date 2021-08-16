b8d4c03b38810641d7da57399b1037ee2ff19ce6
Salih <salihozdemir94@gmail.com>
2021-08-16 15:44:41 +0300

    general development for landing page

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -1,21 +1,8 @@
 import React from "react";
-import clsx from "clsx";
 import Layout from "@theme/Layout";
-import CodeBlock from "@theme/CodeBlock";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
 
-import { features } from "../features";
-import styles from "./styles.module.css";
-
-function Badges() {
-    return <div className={styles.topBadges}></div>;
-}
-function Feature({ imageUrl, title, url }) {
-    const imgUrl = useBaseUrl(imageUrl);
-    return <div> </div>;
-}
+import { Hero } from "../components";
 
 function Home() {
     const context = useDocusaurusContext();
@@ -25,47 +12,8 @@ function Home() {
             title={`Hello from ${siteConfig.title}`}
             description="Description will go into a meta tag in <head />"
         >
-            <header className={clsx("hero hero--primary", styles.heroBanner)}>
-                <div className="container">
-                    <div className={styles.logoContainer}>
-                        <img
-                            className={clsx(
-                                styles.heroBannerLogo,
-                                "margin-vert--md",
-                            )}
-                            src="img/refine_logo.png"
-                            alt="logo-icon"
-                        />
-                    </div>
-                    <p className={clsx(styles.heroSubtitle, "hero__subtitle")}>
-                        {siteConfig.tagline}
-                    </p>
-                    <Badges />
-                    <div className={styles.buttons}>
-                        <Link
-                            className={clsx(
-                                "button button--outline button--secondary button--lg",
-                                styles.getStarted,
-                            )}
-                            to={useBaseUrl("docs/")}
-                        >
-                            Get Started
-                        </Link>
-                    </div>
-                </div>
-            </header>
             <main>
-                {(features ?? []) && features.length > 0 && (
-                    <section className={styles.featuresContainer}>
-                        <div className="container">
-                            <div className="row">
-                                {features.map((props, idx) => (
-                                    <Feature key={idx} {...props} />
-                                ))}
-                            </div>
-                        </div>
-                    </section>
-                )}
+                <Hero />
             </main>
         </Layout>
     );
