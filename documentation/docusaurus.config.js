65271e3a1c3cdbd2cb8d49c979dca14b0380dda2
Salih Özdemir <salihozdemir94@gmail.com>
2021-08-17 13:27:04 +0300

    Docs header and landing page beginning  (#1002)
    * header updated
    
    * general development for landing page
    
    * hide footer
    
    * fix minor bug in css
    
    * update mobil appearance

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -38,60 +38,86 @@ const siteConfig = {
             indexName: "refine",
         },
         navbar: {
-            title: "",
             logo: {
-                alt: "refine Logo",
+                alt: "refine",
                 src: "img/refine_logo.png",
             },
             items: [
-                { to: "docs", label: "Docs", position: "right" },
+                {
+                    to: "docs/guides-and-concepts/upload/multipart-upload",
+                    label: "Guides",
+                    position: "left",
+                    className: "header-first-nav-item",
+                },
+                { to: "docs", label: "Tutorial", position: "left" },
+                {
+                    to: "/docs/api-references/providers/auth-provider",
+                    label: "API",
+                    position: "left",
+                },
+                {
+                    to: "docs/getting-started/overview",
+                    label: "Quickstart",
+                    position: "left",
+                },
                 {
                     href: "https://github.com/pankod/refine",
-                    label: "GitHub",
                     position: "right",
+                    className: "header-icon-link header-github-link",
+                },
+                {
+                    href: "https://discord.gg/qkhjCsJFrp",
+                    position: "right",
+                    className: "header-icon-link header-discord-link",
+                },
+                {
+                    href: "https://twitter.com/PankodDev",
+                    position: "right",
+                    className:
+                        "header-icon-link header-twitter-link header-last-nav-item",
                 },
             ],
         },
-        footer: {
-            style: "dark",
-            links: [
-                {
-                    title: "Docs",
-                    items: [],
-                },
-                {
-                    title: "Community",
-                    items: [
-                        {
-                            label: "Twitter",
-                            href: "https://twitter.com/PankodDev",
-                        },
-                    ],
-                },
-                {
-                    title: "More",
-                    items: [
-                        {
-                            label: "GitHub",
-                            href: "https://github.com/pankod/refine",
-                        },
-                        {
-                            html: `
-                  <a href="https://github.com/pankod/refine" target="_blank" rel="noreferrer noopener" aria-label="Star this project on GitHub">
-                    <img src="https://img.shields.io/github/stars/pankod/refine?logo=reverbnation&logoColor=white" alt="github-stars" />
-                  </a>
-                `,
-                        },
-                    ],
-                },
-            ],
-            logo: {
-                alt: "Pankod Logo",
-                src: "img/pankod_footer_logo.png",
-                href: "https://github.com/pankod",
-            },
-            copyright: `Copyright © ${new Date().getFullYear()} Pankod, Inc.`,
-        },
+        // footer: {
+        //     style: "dark",
+        //     links: [
+        //         {
+        //             title: "Docs",
+        //             items: [],
+        //         },
+        //         {
+        //             title: "Community",
+        //             items: [
+        //                 {
+        //                     label: "Twitter",
+        //                     href: "https://twitter.com/PankodDev",
+        //                 },
+        //             ],
+        //         },
+        //         {
+        //             title: "More",
+        //             items: [
+        //                 {
+        //                     label: "GitHub",
+        //                     href: "https://github.com/pankod/refine",
+        //                 },
+        //                 {
+        //                     html: `
+        //           <a href="https://github.com/pankod/refine" target="_blank" rel="noreferrer noopener" aria-label="Star this project on GitHub">
+        //             <img src="https://img.shields.io/github/stars/pankod/refine?logo=reverbnation&logoColor=white" alt="github-stars" />
+        //           </a>
+        //         `,
+        //                 },
+        //             ],
+        //         },
+        //     ],
+        //     logo: {
+        //         alt: "Pankod Logo",
+        //         src: "img/pankod_footer_logo.png",
+        //         href: "https://github.com/pankod",
+        //     },
+        //     copyright: `Copyright © ${new Date().getFullYear()} Pankod, Inc.`,
+        // },
     },
 };
 
