b31d3f5dfb4d83f3a4de1c0eaa8bac88440693da
Umut Özdemir <umutozdemir997@gmail.com>
2021-08-17 14:06:27 +0300

    fix orderStatuses problem

--- examples/fineFoods/src/components/orderStatus/index.tsx
+++ examples/fineFoods/src/components/orderStatus/index.tsx
@@ -1,10 +1,19 @@
-import { Tag } from "@pankod/refine";
+import { Tag, useTranslate } from "@pankod/refine";
 
 type OrderStatusProps = {
     status: "Pending" | "Ready" | "On The Way" | "Delivered" | "Cancelled";
 };
 
+const orderStatusTranslationKeys: Record<OrderStatusProps["status"], string> = {
+    Pending: "pending",
+    Ready: "ready",
+    "On The Way": "on the way",
+    Delivered: "delivered",
+    Cancelled: "cancelled",
+};
+
 export const OrderStatus: React.FC<OrderStatusProps> = ({ status }) => {
+    const t = useTranslate();
     let color;
 
     switch (status) {
@@ -25,5 +34,9 @@ export const OrderStatus: React.FC<OrderStatusProps> = ({ status }) => {
             break;
     }
 
-    return <Tag color={color}>{status}</Tag>;
+    return (
+        <Tag color={color}>
+            {t(`enum.orderStatuses.${orderStatusTranslationKeys[status]}`)}
+        </Tag>
+    );
 };
