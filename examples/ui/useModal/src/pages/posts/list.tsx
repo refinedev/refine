1021f428af9c687bb0245dc65b602e7afdc4890d
Umut Özdemir <umutozdemir997@gmail.com>
2021-08-20 12:04:30 +0300

    useModal docs (#1020)
    * add useModal doc
    
    * add entry sentence
    
    * refine the first paragraph and add the looks of useModal hook
    
    * add dummy useModal example
    
    * update modal ok button callback
    
    * add usage section
    
    * add api section
    
    * add live codesandbox to end of doc
    
    * add example doc
    
    * Update documentation/docs/api-references/hooks/ui/useModal.md
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    * Update documentation/docs/api-references/hooks/ui/useModal.md
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    * Update documentation/docs/api-references/hooks/ui/useModal.md
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>

---
+++ examples/ui/useModal/src/pages/posts/list.tsx
@@ -0,0 +1,35 @@
+import {
+    List,
+    Table,
+    useTable,
+    useModal,
+    Modal,
+    Button,
+    IResourceComponentsProps,
+} from "@pankod/refine";
+
+import { IPost } from "interfaces";
+
+export const PostList: React.FC<IResourceComponentsProps> = () => {
+    const { tableProps } = useTable<IPost>();
+    const { modalProps, show, close } = useModal();
+
+    return (
+        <>
+            <List
+                pageHeaderProps={{
+                    extra: <Button onClick={show}>Show Dummy Modal</Button>,
+                }}
+            >
+                <Table {...tableProps} rowKey="id">
+                    <Table.Column dataIndex="id" title="ID" />
+                    <Table.Column dataIndex="title" title="Title" />
+                    <Table.Column dataIndex="content" title="Content" />
+                </Table>
+            </List>
+            <Modal onOk={close} {...modalProps}>
+                Dummy Modal Content
+            </Modal>
+        </>
+    );
+};
