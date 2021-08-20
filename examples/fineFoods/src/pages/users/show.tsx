0df31ca2bfa8afe5085bbb11ab88539b793c554b
Umut Özdemir <umutozdemir997@gmail.com>
2021-08-20 15:52:14 +0300

    add user show page

--- examples/fineFoods/src/pages/users/show.tsx
+++ examples/fineFoods/src/pages/users/show.tsx
@@ -1,52 +1,218 @@
 import {
     useShow,
-    Show,
     Typography,
-    BooleanField,
-    DateField,
+    useTable,
     Avatar,
+    HttpError,
+    Row,
+    Col,
+    Card,
+    Space,
+    Icons,
     IResourceComponentsProps,
     useTranslate,
+    List,
+    Table,
+    Grid,
+    TextField,
+    getDefaultSortOrder,
+    NumberField,
+    Popover,
+    DateField,
 } from "@pankod/refine";
+import { OrderStatus } from "components";
 
-import { IUser } from "interfaces";
+import { IUser, IOrder, IOrderFilterVariables } from "interfaces";
 
-const { Title, Text } = Typography;
+const { useBreakpoint } = Grid;
 
 export const UserShow: React.FC<IResourceComponentsProps> = () => {
     const t = useTranslate();
+    const { xl } = useBreakpoint();
     const { queryResult } = useShow<IUser>();
-    const { data, isFetching } = queryResult;
-    const record = data?.data;
 
-    const renderContent = () => {
-        return (
-            <>
-                <Title level={5}>{t("users.fields.id")}</Title>
-                <Text>{record?.id}</Text>
-                <Title level={5}>{t("users.fields.firstName")}</Title>
-                <Text>{record?.firstName}</Text>
-                <Title level={5}>{t("users.fields.lastName")}</Title>
-                <Text>{record?.lastName}</Text>
-                <Title level={5}>{t("users.fields.gender")}</Title>
-                <Text>{record?.gender}</Text>
-                <Title level={5}>{t("users.fields.gsm")}</Title>
-                <Text>{record?.gsm}</Text>
-                <Title level={5}>{t("users.fields.avatar.label")}</Title>
-                <Text>
-                    <Avatar src={record?.avatar[0].url} />
-                </Text>
-                <Title level={5}>{t("users.fields.isActive")}</Title>
-                <Text>
-                    <BooleanField value={record?.isActive} />
-                </Text>
-                <Title level={5}>{t("users.fields.createdAt")}</Title>
-                <Text>
-                    <DateField value={record?.createdAt || ""} format="LLL" />
-                </Text>
-            </>
-        );
-    };
+    const { data } = queryResult;
+    const user = data?.data;
 
-    return <Show isLoading={isFetching}>{record && renderContent()}</Show>;
+    const { tableProps, sorter } = useTable<
+        IOrder,
+        HttpError,
+        IOrderFilterVariables
+    >({
+        resource: "orders",
+        initialSorter: [
+            {
+                field: "createdAt",
+                order: "desc",
+            },
+        ],
+        permanentFilter: [
+            {
+                field: "user.id",
+                operator: "eq",
+                value: user?.id,
+            },
+        ],
+        initialPageSize: 4,
+        queryOptions: {
+            enabled: user !== undefined,
+        },
+    });
+
+    return (
+        <>
+            <Row gutter={[16, 16]}>
+                <Col xl={6} lg={24} xs={24}>
+                    <Card bordered={false} style={{ height: "100%" }}>
+                        <Space
+                            direction="vertical"
+                            style={{ width: "100%", height: "100%" }}
+                        >
+                            <Space
+                                direction="vertical"
+                                style={{
+                                    textAlign: "center",
+                                    width: "100%",
+                                }}
+                            >
+                                <Avatar
+                                    size={120}
+                                    src={user?.avatar?.[0].url}
+                                ></Avatar>
+                                <Typography.Title level={3}>
+                                    {user?.firstName} {user?.lastName}
+                                </Typography.Title>
+                            </Space>
+                            <Space
+                                direction="vertical"
+                                style={{
+                                    width: "100%",
+                                    textAlign: xl ? "unset" : "center",
+                                }}
+                            >
+                                <Typography.Text>
+                                    <Icons.EnvironmentOutlined /> {user?.gender}
+                                </Typography.Text>
+                                <Typography.Text>
+                                    <Icons.PhoneOutlined /> {user?.gsm}
+                                </Typography.Text>
+                                <Typography.Text>
+                                    <Icons.MailOutlined /> {user?.createdAt}
+                                </Typography.Text>
+                                <Typography.Text>
+                                    <Icons.BankOutlined /> {user?.isActive}
+                                </Typography.Text>
+                            </Space>
+                        </Space>
+                    </Card>
+                </Col>
+                <Col xl={18} xs={24}>
+                    <List
+                        title={t("orders.orders")}
+                        pageHeaderProps={{
+                            extra: <></>,
+                        }}
+                    >
+                        <Table
+                            {...tableProps}
+                            scroll={{
+                                x: true,
+                            }}
+                            rowKey="id"
+                        >
+                            <Table.Column
+                                key="orderNumber"
+                                dataIndex="orderNumber"
+                                title={t("orders.fields.orderNumber")}
+                                render={(value) => <TextField value={value} />}
+                            />
+                            <Table.Column
+                                key="status.text"
+                                dataIndex={["status", "text"]}
+                                title={t("orders.fields.status")}
+                                render={(value) => {
+                                    return <OrderStatus status={value} />;
+                                }}
+                                defaultSortOrder={getDefaultSortOrder(
+                                    "status",
+                                    sorter,
+                                )}
+                                sorter
+                            />
+                            <Table.Column
+                                align="right"
+                                key="amount"
+                                dataIndex="amount"
+                                title={t("orders.fields.amount")}
+                                defaultSortOrder={getDefaultSortOrder(
+                                    "amount",
+                                    sorter,
+                                )}
+                                sorter
+                                render={(value) => {
+                                    return (
+                                        <NumberField
+                                            options={{
+                                                currency: "USD",
+                                                style: "currency",
+                                                notation: "compact",
+                                            }}
+                                            value={value}
+                                        />
+                                    );
+                                }}
+                            />
+                            <Table.Column
+                                key="store.id"
+                                dataIndex={["store", "title"]}
+                                title={t("orders.fields.store")}
+                            />
+                            <Table.Column
+                                key="user.fullName"
+                                dataIndex={["user", "fullName"]}
+                                title={t("orders.fields.user")}
+                            />
+                            <Table.Column<IOrder>
+                                key="products"
+                                dataIndex="products"
+                                title={t("orders.fields.products")}
+                                render={(_, record) => (
+                                    <Popover
+                                        content={
+                                            <ul>
+                                                {record.products.map(
+                                                    (product) => (
+                                                        <li key={product.id}>
+                                                            {product.name}
+                                                        </li>
+                                                    ),
+                                                )}
+                                            </ul>
+                                        }
+                                        title="Products"
+                                        trigger="hover"
+                                    >
+                                        {`${record.products.length} Items`}
+                                    </Popover>
+                                )}
+                            />
+                            <Table.Column
+                                key="createdAt"
+                                dataIndex="createdAt"
+                                title={t("orders.fields.createdAt")}
+                                render={(value) => (
+                                    <DateField value={value} format="LLL" />
+                                )}
+                                sorter
+                                defaultSortOrder={getDefaultSortOrder(
+                                    "createdAt",
+                                    sorter,
+                                )}
+                            />
+                        </Table>
+                    </List>
+                </Col>
+            </Row>
+        </>
+    );
 };
