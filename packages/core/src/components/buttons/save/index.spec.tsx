4670e9bfe3a0f73b0d6d32aa15d6c3161e67d810
Salih <salihozdemir94@gmail.com>
2021-08-23 12:39:49 +0300

    save button add hideText props and tests

--- packages/core/src/components/buttons/save/index.spec.tsx
+++ packages/core/src/components/buttons/save/index.spec.tsx
@@ -7,25 +7,56 @@ describe("Save Button", () => {
     const save = jest.fn();
 
     it("should render button successfuly", () => {
-        const saveButton = render(<SaveButton onClick={() => save()} />, {
-            wrapper: TestWrapper({
-                resources: [{ name: "posts" }],
-            }),
-        });
-        const { container, getByText } = saveButton;
+        const { container, getByText } = render(
+            <SaveButton onClick={() => save()} />,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
 
         expect(container).toBeTruthy();
 
         getByText("Save");
     });
 
+    it("should render text by children", () => {
+        const { container, getByText } = render(
+            <SaveButton>refine</SaveButton>,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+
+        expect(container).toBeTruthy();
+
+        getByText("refine");
+    });
+
+    it("should render without text show only icon", () => {
+        const { container, queryByText } = render(
+            <SaveButton hideText>refine</SaveButton>,
+            {
+                wrapper: TestWrapper({
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+
+        expect(container).toBeTruthy();
+
+        expect(queryByText("Save")).not.toBeInTheDocument();
+    });
+
     it("should render called function successfully if click the button", () => {
-        const saveButton = render(<SaveButton onClick={() => save()} />, {
+        const { getByText } = render(<SaveButton onClick={() => save()} />, {
             wrapper: TestWrapper({
                 resources: [{ name: "posts" }],
             }),
         });
-        const { getByText } = saveButton;
 
         fireEvent.click(getByText("Save"));
 
