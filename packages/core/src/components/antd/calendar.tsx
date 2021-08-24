e6914ab08c5cef04017f138d64ca50c632b4b83a
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-08-24 10:10:39 +0300

    Fix calendar style import.

--- packages/core/src/components/antd/calendar.tsx
+++ packages/core/src/components/antd/calendar.tsx
@@ -1,7 +1,7 @@
 import { Dayjs } from "dayjs";
 import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
 import generateCalendar from "antd/es/calendar/generateCalendar";
-import "antd/es/calendar/style";
+import "antd/es/calendar/style/index";
 
 const Calendar = generateCalendar<Dayjs>(dayjsGenerateConfig);
 
