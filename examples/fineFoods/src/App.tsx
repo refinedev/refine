7d408b20d2bcd6a74d7265d76fb1c37f8a348cca
Umut Özdemir <umutozdemir997@gmail.com>
2021-08-24 13:07:04 +0300

    add german language in dayjs and antd localizations

--- examples/fineFoods/src/App.tsx
+++ examples/fineFoods/src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { Refine, Resource, Icons, Icon } from "@pankod/refine";
-import "styles/antd.less";
 import jsonServerDataProvider from "@pankod/refine-simple-rest";
+import de_DE from "@pankod/refine/node_modules/antd/lib/locale/de_DE";
 import { authProvider } from "authProvider";
+import dayjs from "dayjs";
+
+import "styles/antd.less";
+import "dayjs/locale/de";
+
 import { DashboardPage } from "./pages/dashboard";
 import { LoginPage } from "./pages/login";
 import { OrderList, OrderShow } from "./pages/orders";
@@ -32,6 +38,16 @@ const App: React.FC = () => {
         getLocale: () => i18n.language,
     };
 
+    const locale = i18nProvider.getLocale();
+
+    useEffect(() => {
+        if (i18nProvider.getLocale() === "de") {
+            dayjs.locale("de");
+        } else {
+            dayjs.locale("en");
+        }
+    }, [locale]);
+
     return (
         <Refine
             dataProvider={dataProvider}
@@ -43,6 +59,9 @@ const App: React.FC = () => {
             LoginPage={LoginPage}
             syncWithLocation
             warnWhenUnsavedChanges
+            configProviderProps={{
+                locale: locale === "de" ? de_DE : undefined,
+            }}
         >
             <Resource
                 name="orders"
