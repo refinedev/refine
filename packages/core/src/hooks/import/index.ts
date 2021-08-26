8769e647ae4762b33e143309123a742df1cd4b87
Umut Özdemir <umutozdemir997@gmail.com>
2021-08-26 17:07:38 +0300

    override notifications and add progress to notifications

--- packages/core/src/hooks/import/index.ts
+++ packages/core/src/hooks/import/index.ts
@@ -1,6 +1,13 @@
-import { ButtonProps, UploadProps } from "antd";
+import { useEffect, useState } from "react";
+import { ButtonProps, notification, UploadProps } from "antd";
 import { UploadChangeParam } from "antd/lib/upload";
-import { useCreate, useCreateMany, useResourceWithRoute } from "@hooks";
+import {
+    useCreate,
+    useTranslate,
+    useCreateMany,
+    useResourceWithRoute,
+    useListResourceQueries,
+} from "@hooks";
 import { useParams } from "react-router-dom";
 import {
     MapDataFn,
@@ -14,13 +21,15 @@ import { importCSVMapper } from "@definitions";
 import chunk from "lodash/chunk";
 import { UseCreateReturnType } from "@hooks/data/useCreate";
 import { UseCreateManyReturnType } from "@hooks/data/useCreateMany";
+import pluralize from "pluralize";
+import { useQueryClient } from "react-query";
 
 type ImportOptions<TItem, TVariables = any> = {
     resourceName?: string;
     mapData?: MapDataFn<TItem, TVariables>;
     paparseOptions?: ParseConfig;
     batchSize?: number | null;
-} & SuccessErrorNotification;
+};
 
 /**
  * `useImport` hook allows you to handle your csv import logic easily.
@@ -43,8 +52,6 @@ export const useImport = <
     mapData = (item) => item as unknown as TVariables,
     paparseOptions,
     batchSize,
-    successNotification,
-    errorNotification,
 }: ImportOptions<TItem, TVariables> = {}): {
     uploadProps: UploadProps;
     buttonProps: ButtonProps;
@@ -52,54 +59,139 @@ export const useImport = <
         | UseCreateReturnType<TData, TError, TVariables>
         | UseCreateManyReturnType<TData, TError, TVariables>;
 } => {
+    const [processedAmount, setProcessedAmount] = useState<number>(0);
+    const [totalAmount, setTotalAmount] = useState<number>(0);
+
     const resourceWithRoute = useResourceWithRoute();
+    const getListQueries = useListResourceQueries();
+    const queryClient = useQueryClient();
+    const t = useTranslate();
     const { resource: routeResourceName } = useParams<ResourceRouterParams>();
-
     let { name: resource } = resourceWithRoute(routeResourceName);
+    const resourceSingular = pluralize.singular(resource);
 
-    const createManyMutationResult = useCreateMany<TData, TError, TVariables>();
-    const createMutationResult = useCreate<TData, TError, TVariables>();
+    const createMany = useCreateMany<TData, TError, TVariables>();
+    const create = useCreate<TData, TError, TVariables>();
 
     let mutationResult;
 
     if (batchSize === 1) {
-        mutationResult = createMutationResult;
+        mutationResult = create;
     } else {
-        mutationResult = createManyMutationResult;
+        mutationResult = createMany;
     }
 
     if (resourceName) {
         resource = resourceName;
     }
 
+    useEffect(() => {
+        if (totalAmount > 0) {
+            notification.open({
+                // description: t(
+                //     "notifications.createSuccess",
+                //     {
+                //         resource: t(`${resource}.${resource}`, totalAmount),
+                //     },
+                //     `Successfully created ${resourceSingular}`,
+                // ),
+                description: totalAmount,
+                // message: t("notifications.success", "Success"),
+                message: processedAmount,
+                type: "success",
+                key: `${resourceName}-import`,
+            });
+
+            if (processedAmount >= totalAmount) {
+                setTotalAmount(0);
+                setProcessedAmount(0);
+            }
+        }
+    }, [totalAmount, processedAmount]);
+
     const handleChange = ({ file }: UploadChangeParam) => {
         parse(file as unknown as File, {
             complete: ({ data }: { data: unknown[][] }) => {
                 const values = importCSVMapper(data, mapData);
 
+                setTotalAmount(values.length);
+
                 if (batchSize === null) {
-                    createManyMutationResult.mutate({
-                        resource,
-                        values,
-                        successNotification,
-                        errorNotification,
-                    });
+                    createMany.mutateAsync(
+                        {
+                            resource,
+                            values,
+                            successNotification: false,
+                            errorNotification: false,
+                        },
+                        {
+                            onSuccess: (_, { resource }) => {
+                                console.log(
+                                    "useImport onSuccess batchSize = null",
+                                );
+                                setProcessedAmount(totalAmount);
+                            },
+                        },
+                    );
                 } else if (batchSize === 1) {
-                    values.forEach((value) => {
-                        createMutationResult.mutate({
-                            resource,
-                            values: value,
-                        });
-                    });
+                    Promise.all(
+                        values
+                            .map((value) => {
+                                return create.mutateAsync(
+                                    {
+                                        resource,
+                                        values: value,
+                                        successNotification: false,
+                                        errorNotification: false,
+                                    },
+                                    {
+                                        onSuccess: (_, { resource }) => {
+                                            console.log(
+                                                "useImport onSuccess batchSize = 1",
+                                            );
+                                        },
+                                    },
+                                );
+                            })
+                            .map((mutation) =>
+                                mutation.then(() => {
+                                    setProcessedAmount(
+                                        (currentAmount) => currentAmount + 1,
+                                    );
+                                }),
+                            ),
+                    );
                 } else {
-                    chunk(values, batchSize).forEach((batch) => {
-                        createManyMutationResult.mutate({
-                            resource,
-                            values: batch,
-                            successNotification,
-                            errorNotification,
-                        });
-                    });
+                    Promise.all(
+                        chunk(values, batchSize)
+                            .map((batch) => ({
+                                mutation: createMany.mutateAsync(
+                                    {
+                                        resource,
+                                        values: batch,
+                                        successNotification: false,
+                                        errorNotification: false,
+                                    },
+                                    {
+                                        onSuccess: (_, { resource }) => {
+                                            console.log(
+                                                "useImport onSuccess batchSize > 1",
+                                            );
+                                        },
+                                    },
+                                ),
+                                currentBatchLength: batch.length,
+                            }))
+                            .map(({ mutation, currentBatchLength }) =>
+                                mutation.then(() => {
+                                    setProcessedAmount(
+                                        (currentAmount) =>
+                                            currentAmount +
+                                            (currentBatchLength ?? 0),
+                                    );
+                                }),
+                            ),
+                    );
                 }
             },
             ...paparseOptions,
