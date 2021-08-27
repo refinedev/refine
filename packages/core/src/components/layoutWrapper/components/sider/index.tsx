6b926175c5ed8ede40fe842d26393d039b214fc2
Aydın Akyol <aydinakyol11@gmail.com>
2021-08-27 14:00:34 +0300

    fixes

--- packages/core/src/components/layoutWrapper/components/sider/index.tsx
+++ packages/core/src/components/layoutWrapper/components/sider/index.tsx
@@ -33,7 +33,9 @@ export const Sider: React.FC = () => {
             collapsedWidth={isMobile ? 0 : 80}
             breakpoint="lg"
             style={isMobile ? antLayoutSiderMobile : antLayoutSider}
-            className={`refine-sider ${!collapsed && "refine-sider-collapsed"}`}
+            className={`refine-sider ${
+                !collapsed ? "refine-sider-collapsed" : ""
+            }`}
         >
             <Title collapsed={collapsed} />
             <Menu
