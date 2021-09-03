c1aeb27ecd6674806c48a20492837ce2f41c041c
Salih <salihozdemir94@gmail.com>
2021-09-03 12:34:57 +0300

    duplicate base example

---
+++ examples/dataProvider/supabase/src/App.tsx
@@ -0,0 +1,26 @@
+import { Refine, Resource } from "@pankod/refine";
+import {SupabaseDataProvider, createClient} from "@pankod/refine-supabase";
+import "@pankod/refine/dist/styles.min.css";
+
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+
+const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU'
+
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+const App: React.FC = () => {
+    return (
+        <Refine dataProvider={SupabaseDataProvider(supabaseClient)}>
+            <Resource
+                name="posts"
+                list={PostList}
+                // create={PostCreate}
+                // edit={PostEdit}
+                // show={PostShow}
+            />
+        </Refine>
+    );
+};
+
+export default App;
