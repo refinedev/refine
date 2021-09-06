867abca17a7a1f410d29c501d207581fba61ff00
Salih <salihozdemir94@gmail.com>
2021-09-06 12:20:50 +0300

    add getList test

--- packages/supabase/test/getList/index.spec.ts
+++ packages/supabase/test/getList/index.spec.ts
@@ -1,36 +1,31 @@
-import nock from "nock";
-// import nock from "nock";
-
-import dataProvider from "../../src/index";
+import { dataProvider } from "../../src/index";
+import supabaseClient from "../supabaseClient";
 import "./index.mock";
 
 describe("getList", () => {
     it("correct response", async () => {
-        const response = await dataProvider(
-            "keywoytODSr6xAqfg",
-            "appKYl1H4k9g73sBT",
-        ).getList("posts", {});
+        const response = await dataProvider(supabaseClient).getList(
+            "posts",
+            {},
+        );
 
-        expect(response.data[0]["id"]).toBe("rec9GbXLzd6dxn4Il");
-        expect(response.data[0]["title"]).toBe("Hello World 3!");
+        expect(response.data[0]["id"]).toBe(2);
+        expect(response.data[0]["title"]).toBe("test title");
         expect(response.total).toBe(2);
     });
 
     it("correct sorting response", async () => {
-        const response = await dataProvider(
-            "keywoytODSr6xAqfg",
-            "appKYl1H4k9g73sBT",
-        ).getList("posts", {
+        const response = await dataProvider(supabaseClient).getList("posts", {
             sort: [
                 {
                     field: "title",
-                    order: "desc",
+                    order: "asc",
                 },
             ],
         });
 
-        expect(response.data[0]["id"]).toBe("recLKRioqifTrPUIz");
-        expect(response.data[0]["title"]).toBe("Hello World!");
+        expect(response.data[0]["id"]).toBe(3);
+        expect(response.data[0]["title"]).toBe("What a library");
         expect(response.total).toBe(2);
     });
 });
