753995644b6c04f4a2e6146af90f2c8370b7d440
Salih <salihozdemir94@gmail.com>
2021-09-10 14:22:16 +0300

    add getList function

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,13 +1,29 @@
-import { CrudSorting, DataProvider } from "@pankod/refine";
+import { CrudFilters, CrudSorting, DataProvider } from "@pankod/refine";
 import { GraphQLClient } from "graphql-request";
 import * as gql from "gql-query-builder";
 
 const genereteSort = (sort?: CrudSorting) => {
-    const sortQuery = sort?.map((i) => {
-        return `${i.field}:${i.order}`;
-    });
+    if (sort && sort.length > 0) {
+        const sortQuery = sort.map((i) => {
+            return `${i.field}:${i.order}`;
+        });
 
-    return sortQuery?.join();
+        return sortQuery.join();
+    }
+
+    return [];
+};
+
+const generateFilter = (filters?: CrudFilters) => {
+    const queryFilters: { [key: string]: string } = {};
+
+    if (filters) {
+        filters.map(({ field, operator, value }) => {
+            queryFilters[`${field}_${operator}`] = value;
+        });
+    }
+
+    return queryFilters;
 };
 
 const dataProvider = (client: GraphQLClient): DataProvider => {
@@ -19,12 +35,17 @@ const dataProvider = (client: GraphQLClient): DataProvider => {
             const pageSize = pagination?.pageSize || 10;
 
             const sortBy = genereteSort(sort);
+            const filterBy = generateFilter(filters);
 
+            const operation = metaData?.operation ?? resource;
             const { query, variables } = gql.query({
-                operation: metaData?.operation ?? resource,
+                operation,
                 variables: {
                     ...metaData?.variables,
                     sort: sortBy,
+                    where: { value: filterBy, type: "JSON" },
+                    start: (current - 1) * pageSize,
+                    limit: current * pageSize,
                 },
                 ...metaData,
             });
@@ -32,8 +53,8 @@ const dataProvider = (client: GraphQLClient): DataProvider => {
             const response = await client.request(query, variables);
 
             return {
-                data: response[metaData?.operation ?? resource],
-                total: 3,
+                data: response[operation],
+                total: 10,
             };
         },
 
