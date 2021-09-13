06722340df3f6b66f6f1c2a78322b33e3d481721
Ömer Faruk APLAK <omer@pankod.com>
2021-09-13 11:56:02 +0300

    update api key to const

--- examples/dataProvider/altogic/src/App.tsx
+++ examples/dataProvider/altogic/src/App.tsx
@@ -7,10 +7,12 @@ import "@pankod/refine/dist/styles.min.css";
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
 
 const API_URL = "https://dev001.na-dev-engine.altogic.com";
+const YOUR_SECRET_API_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZJZCI6IjYxMzczZGVkMjQ5NWMzMDAxOTliZTAxNiIsImtleUlkIjoiNjEzNzNlMzYyNDk1YzMwMDE5OWJlMDJkIiwiaWF0IjoxNjMxMDEwMzU4LCJleHAiOjI0OTUwMTAzNTh9.2fL28Bzd97mqfAvcsTrYj1mZ_hqf3WRnr2DOtV3lsc0";
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.headers = {
-    Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZJZCI6IjYxMzczZGVkMjQ5NWMzMDAxOTliZTAxNiIsImtleUlkIjoiNjEzNzNlMzYyNDk1YzMwMDE5OWJlMDJkIiwiaWF0IjoxNjMxMDEwMzU4LCJleHAiOjI0OTUwMTAzNTh9.2fL28Bzd97mqfAvcsTrYj1mZ_hqf3WRnr2DOtV3lsc0`,
+    Authorization: YOUR_SECRET_API_KEY,
 };
 
 axiosInstance.interceptors.response.use(
