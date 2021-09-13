97548a069df5eaeca77a81009ce8c8b273ca49dd
Ömer Faruk APLAK <omer@pankod.com>
2021-09-13 11:55:15 +0300

    Update packages/core/src/definitions/upload/index.ts
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>

--- packages/core/src/definitions/upload/index.ts
+++ packages/core/src/definitions/upload/index.ts
@@ -3,7 +3,6 @@ import { UploadFile, UploadChangeParam } from "antd/lib/upload/interface";
 export const getValueFromEvent = (event: UploadChangeParam): UploadFile[] => {
     const { fileList } = event;
 
-    console.log(event);
 
     return [...fileList];
 };
