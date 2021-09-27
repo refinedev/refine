9070710f1011d475c66946aedcb75c5bd159bd06
Muharrem Kocadere <mkocadere@gmail.com>
2021-09-27 17:25:57 +0300

    implement Prompt component for nextjs-router to handle warnWhenUnsavedChanges

--- packages/nextjs-router/src/index.ts
+++ packages/nextjs-router/src/index.ts
@@ -1,8 +1,11 @@
 import { useRouter } from "next/router";
+import type {} from "next/router";
 import Link from "next/link";
 
 import qs from "qs";
-const RouterProvider = () => ({
+import React, { useEffect } from "react";
+
+const RouterProvider: IRouterProvider = () => ({
     useHistory: () => {
         const router = useRouter();
         const { push, replace, back } = router as any;
@@ -38,9 +41,45 @@ const RouterProvider = () => ({
     BrowserRouter: () => null,
     Switch: () => null,
     Route: () => null,
-    Prompt: () => null,
+    Prompt: ({ message, when, setWarnWhen }) => {
+        const router = useRouter();
+
+        useEffect(() => {
+            const routeChangeStart = () => {
+                if (when) {
+                    const allowTransition = window.confirm(message);
+                    if (allowTransition) {
+                        setWarnWhen?.(false);
+                    } else {
+                        router.events.emit("routeChangeError");
+                        throw "Abort route change";
+                    }
+                }
+            };
+            router.events.on("routeChangeStart", routeChangeStart);
+
+            return () =>
+                router.events.off("routeChangeStart", routeChangeStart);
+        }, [when]);
+        return null;
+    },
     Link,
     Redirect: () => null,
 });
+
+type IRouterProvider = () => {
+    useHistory: any;
+    useLocation: any;
+    useParams: any;
+    BrowserRouter: () => void;
+    Switch: () => void;
+    Route: () => void;
+    Prompt: React.FC<{
+        when: boolean;
+        message: string;
+        setWarnWhen?: (warnWhen: boolean) => void;
+    }>;
+    Redirect: () => void;
+};
 
 export default RouterProvider;
