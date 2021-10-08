6aa52708202c43cf5efb55da02c8a3f6b15df20d
Roger Jenkins <rjenkins@zyeyz.com>
2021-10-08 12:40:35 -0600

    Modify strapi-graphql dataProvider to remove reliance on the Strapi REST interface and return both count and totalCount

--- packages/strapi-graphql/src/index.ts
+++ packages/strapi-graphql/src/index.ts
@@ -42,7 +42,21 @@ const dataProvider = (client: GraphQLClient): DataProvider => {
 
             const operation = metaData?.operation ?? camelResource;
 
-            const { query, variables } = gql.query({
+            const operationConnection = operation+"Connection";
+
+            const { query, variables } = gql.query([
+              {
+                operation:operationConnection,
+                variables: {
+                    ...metaData?.variables,
+                    sort: sortBy,
+                    where: { value: filterBy, type: "JSON" },
+                    start: (current - 1) * pageSize,
+                    limit: pageSize,
+                },
+                fields: [{aggregate:["count", "totalCount"]}],
+              },
+              {
                 operation,
                 variables: {
                     ...metaData?.variables,
@@ -52,21 +66,16 @@ const dataProvider = (client: GraphQLClient): DataProvider => {
                     limit: pageSize,
                 },
                 fields: metaData?.fields,
-            });
-
-            const domain = new URL(client["url"]);
-            const restApiUrl = domain.origin;
+            }]);
 
             const response = await Promise.all([
                 client.request(query, variables),
-                fetch(
-                    `${restApiUrl}/${resource}/count?${stringify(filterBy)}`,
-                ).then((res) => res.json()),
             ]);
 
             return {
                 data: response[0][operation],
-                total: response[1],
+                count: response[0][operationConnection].aggregate.count,
+                total: response[0][operationConnection].aggregate.totalCount,
             };
         },
 
