880c95746f3268244031d42b4a0ba0d17ce59d95
Umut Özdemir <umutozdemir997@gmail.com>
2021-10-19 16:13:35 +0300

    update hasura package

---
+++ packages/hasura/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: "ts-jest",
+    rootDir: "./",
+    name: "refine-hasura",
+    displayName: "refine-hasura",
+    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.js"],
+};
