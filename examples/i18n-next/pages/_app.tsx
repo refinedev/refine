fda19a0d0403b4acc99ce0e585cb832618626d40
Muharrem Kocadere <mkocadere@gmail.com>
2021-10-25 13:13:59 +0300

    add refine i18n proivder

--- examples/i18n-next/pages/_app.tsx
+++ examples/i18n-next/pages/_app.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { AppProps } from "next/app";
 import "@styles/global.css";
 
+import { appWithTranslation, useTranslation } from "next-i18next";
+
 import { Refine } from "@pankod/refine";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-nextjs-router";
@@ -14,11 +16,19 @@ import "@pankod/refine/dist/styles.min.css";
 const API_URL = "https://api.fake-rest.refine.dev";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    const { t, i18n } = useTranslation();
+    const i18nProvider = {
+        translate: (key: string, params: object) => t(key, params),
+        changeLocale: (lang: string) => i18n.changeLanguage(lang),
+        getLocale: () => i18n.language,
+    };
+
     return (
         <Refine
             routerProvider={routerProvider}
             authProvider={authProvider}
             dataProvider={dataProvider(API_URL)}
+            i18nProvider={i18nProvider}
             resources={[
                 { name: "users" },
                 {
@@ -37,4 +47,4 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
