df73d5ac87e73f4848de4eb2159e7626cfd879bd
Ömer Faruk APLAK <omer@pankod.com>
2021-10-27 14:29:10 +0300

    update nextjs examples

--- examples/i18n/nextjs/pages/login/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useRefineContext } from "@pankod/refine";
-
-const Login = () => {
-    const { LoginPage } = useRefineContext();
-    return LoginPage ? <LoginPage /> : null;
-};
-export default Login;
