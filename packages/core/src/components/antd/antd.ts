24519c4977d9f32f65fa326162648877699f31ba
necatiozmen <necatiozmn@gmail.com>
2021-11-02 17:04:13 +0300

    Refactor antd exports file

--- packages/core/src/components/antd/antd.ts
+++ packages/core/src/components/antd/antd.ts
@@ -1,165 +1,146 @@
-export type { AffixProps } from "antd/lib/affix";
-export { default as Affix } from "antd/lib/affix";
-
-export type { AnchorProps, AnchorLinkProps } from "antd/lib/anchor";
-export { default as Anchor } from "antd/lib/anchor";
-
-export type { AutoCompleteProps } from "antd/lib/auto-complete";
-export { default as AutoComplete } from "antd/lib/auto-complete";
-
-export type { AlertProps } from "antd/lib/alert";
-export { default as Alert } from "antd/lib/alert";
-
-export type { AvatarProps } from "antd/lib/avatar";
-export { default as Avatar } from "antd/lib/avatar";
-
-export type { BackTopProps } from "antd/lib/back-top";
-export { default as BackTop } from "antd/lib/back-top";
-
-export type { BadgeProps } from "antd/lib/badge";
-export { default as Badge } from "antd/lib/badge";
-
-export type { BreadcrumbProps, BreadcrumbItemProps } from "antd/lib/breadcrumb";
-export { default as Breadcrumb } from "antd/lib/breadcrumb";
-
-export type { ButtonProps } from "antd/lib/button";
-export { default as Button } from "antd/lib/button";
-
-export type { CalendarProps } from "antd/lib/calendar";
-
-export type { CardProps } from "antd/lib/card";
-export { default as Card } from "antd/lib/card";
-
-export type { CollapseProps, CollapsePanelProps } from "antd/lib/collapse";
-export { default as Collapse } from "antd/lib/collapse";
-
-export type { CarouselProps } from "antd/lib/carousel";
-export { default as Carousel } from "antd/lib/carousel";
-
-export type { CascaderProps } from "antd/lib/cascader";
-export { default as Cascader } from "antd/lib/cascader";
-
-export type { CheckboxProps, CheckboxOptionType } from "antd/lib/checkbox";
-export { default as Checkbox } from "antd/lib/checkbox";
-
-export type { ColProps } from "antd/lib/col";
-export { default as Col } from "antd/lib/col";
-
-export type { CommentProps } from "antd/lib/comment";
-export { default as Comment } from "antd/lib/comment";
-
-export { default as ConfigProvider } from "antd/lib/config-provider";
-
-export type { DatePickerProps } from "antd/lib/date-picker";
-
-export type { DescriptionsProps } from "antd/lib/descriptions";
-export { default as Descriptions } from "antd/lib/descriptions";
-
-export type { DividerProps } from "antd/lib/divider";
-export { default as Divider } from "antd/lib/divider";
-
-export type { DropDownProps } from "antd/lib/dropdown";
-export { default as Dropdown } from "antd/lib/dropdown";
-
-export type { DrawerProps } from "antd/lib/drawer";
-export { default as Drawer } from "antd/lib/drawer";
-
-export type { EmptyProps } from "antd/lib/empty";
-export { default as Empty } from "antd/lib/empty";
-
-export type { FormInstance, FormProps, FormItemProps } from "antd/lib/form";
-export { default as Form } from "antd/lib/form";
-
-export { default as Grid } from "antd/lib/grid";
-
-export type { InputProps } from "antd/lib/input";
-export { default as Input } from "antd/lib/input";
-
-export type { ImageProps } from "antd/lib/image";
-export { default as Image } from "antd/lib/image";
-
-export type { InputNumberProps } from "antd/lib/input-number";
-export { default as InputNumber } from "antd/lib/input-number";
-
-export type { LayoutProps } from "antd/lib/layout";
-export { default as AntdLayout } from "antd/lib/layout";
-
-export type { ListProps } from "antd/lib/list";
-export { default as AntdList } from "antd/lib/list";
-
-export type { ArgsProps as MessageArgsProps } from "antd/lib/message";
-export { default as message } from "antd/lib/message";
+export {
+    Affix,
+    Anchor,
+    AutoComplete,
+    Alert,
+    Avatar,
+    BackTop,
+    Badge,
+    Breadcrumb,
+    Button,
+    Card,
+    Collapse,
+    ConfigProvider,
+    Carousel,
+    Cascader,
+    Checkbox,
+    Col,
+    Comment,
+    Descriptions,
+    Divider,
+    Dropdown,
+    Drawer,
+    Empty,
+    Form,
+    Grid,
+    Input,
+    Image,
+    InputNumber,
+    message,
+    Layout as AntdLayout,
+    List as AntdList,
+    Menu,
+    Mentions,
+    Modal,
+    Statistic,
+    notification,
+    PageHeader,
+    Pagination,
+    Popconfirm,
+    Popover,
+    Progress,
+    Radio,
+    Rate,
+    Result,
+    Row,
+    Select,
+    Skeleton,
+    Slider,
+    Space,
+    Spin,
+    Steps,
+    Switch,
+    Table,
+    Transfer,
+    Tree,
+    TreeSelect,
+    Tabs,
+    Tag,
+    Timeline,
+    Tooltip,
+    Typography,
+    Upload,
+    version,
+} from "antd";
 
 export type {
+    AffixProps,
+    AnchorProps,
+    AnchorLinkProps,
+    AutoCompleteProps,
+    AlertProps,
+    AvatarProps,
+    BackTopProps,
+    BadgeProps,
+    BreadcrumbProps,
+    BreadcrumbItemProps,
+    ButtonProps,
+    CardProps,
+    CalendarProps,
+    CollapseProps,
+    CollapsePanelProps,
+    CarouselProps,
+    CascaderProps,
+    CheckboxProps,
+    CheckboxOptionType,
+    ColProps,
+    CommentProps,
+    DatePickerProps,
+    DescriptionsProps,
+    DividerProps,
+    DropDownProps,
+    DrawerProps,
+    EmptyProps,
+    FormInstance,
+    FormItemProps,
+    FormProps,
+    InputProps,
+    ImageProps,
+    InputNumberProps,
+    LayoutProps,
+    ListProps,
     MenuProps,
     MenuTheme,
     SubMenuProps,
     MenuItemProps,
-} from "antd/lib/menu";
-export { default as Menu } from "antd/lib/menu";
-
-export type { MentionProps } from "antd/lib/mentions";
-export { default as Mentions } from "antd/lib/mentions";
-
-export type { ModalProps, ModalFuncProps } from "antd/lib/modal";
-export { default as Modal } from "antd/lib/modal";
-
-export type { StatisticProps } from "antd/lib/statistic";
-export { default as Statistic } from "antd/lib/statistic";
-
-export { default as notification } from "antd/lib/notification";
-
-export type { PageHeaderProps } from "antd/lib/page-header";
-export { default as PageHeader } from "antd/lib/page-header";
-
-export type { PaginationProps } from "antd/lib/pagination";
-export { default as Pagination } from "antd/lib/pagination";
-
-export type { PopconfirmProps } from "antd/lib/popconfirm";
-export { default as Popconfirm } from "antd/lib/popconfirm";
-
-export type { PopoverProps } from "antd/lib/popover";
-export { default as Popover } from "antd/lib/popover";
-
-export type { ProgressProps } from "antd/lib/progress";
-export { default as Progress } from "antd/lib/progress";
-
-export type {
+    ModalProps,
+    ModalFuncProps,
+    StatisticProps,
+    PageHeaderProps,
+    PaginationProps,
+    PopconfirmProps,
+    PopoverProps,
+    ProgressProps,
     RadioProps,
     RadioChangeEvent,
     RadioGroupProps,
-} from "antd/lib/radio";
-export { default as Radio } from "antd/lib/radio";
+    RateProps,
+    ResultProps,
+    RowProps,
+    SelectProps,
+    SkeletonProps,
+    SliderSingleProps,
+    SpaceProps,
+    SpinProps,
+    StepProps,
+    StepsProps,
+    SwitchProps,
+    TransferProps,
+    TreeSelectProps,
+    TabsProps,
+    TabPaneProps,
+    TagProps,
+    TagType,
+    TimelineProps,
+    TimelineItemProps,
+    TooltipProps,
+    TypographyProps,
+    UploadProps,
+} from "antd";
 
-export type { RateProps } from "antd/lib/rate";
-export { default as Rate } from "antd/lib/rate";
+export type { ArgsProps as MessageArgsProps } from "antd/lib/message";
 
-export type { ResultProps } from "antd/lib/result";
-export { default as Result } from "antd/lib/result";
-
-export type { RowProps } from "antd/lib/row";
-export { default as Row } from "antd/lib/row";
-
-export type { SelectProps } from "antd/lib/select";
-export { default as Select } from "antd/lib/select";
-
-export type { SkeletonProps } from "antd/lib/skeleton";
-export { default as Skeleton } from "antd/lib/skeleton";
-
-export type { SliderSingleProps } from "antd/lib/slider";
-export { default as Slider } from "antd/lib/slider";
-
-export type { SpaceProps } from "antd/lib/space";
-export { default as Space } from "antd/lib/space";
-
-export type { SpinProps } from "antd/lib/spin";
-export { default as Spin } from "antd/lib/spin";
-
-export type { StepProps, StepsProps } from "antd/lib/steps";
-export { default as Steps } from "antd/lib/steps";
-
-export type { SwitchProps } from "antd/lib/switch";
-export { default as Switch } from "antd/lib/switch";
+export type { MentionProps } from "antd";
 
 export type {
     TableProps,
@@ -168,38 +149,10 @@ export type {
     ColumnType as TableColumnType,
     ColumnProps as TableColumnProps,
 } from "antd/lib/table";
-export { Table } from "antd";
-
-export type { TransferProps } from "antd/lib/transfer";
-export { default as Transfer } from "antd/lib/transfer";
 
 export type {
     TreeProps,
     AntTreeNodeProps as TreeNodeProps,
 } from "antd/lib/tree";
-export { default as Tree } from "antd/lib/tree";
 
-export type { TreeSelectProps } from "antd/lib/tree-select";
-export { default as TreeSelect } from "antd/lib/tree-select";
-
-export type { TabsProps, TabPaneProps } from "antd/lib/tabs";
-export { default as Tabs } from "antd/lib/tabs";
-
-export type { TagProps, TagType } from "antd/lib/tag";
-export { default as Tag } from "antd/lib/tag";
-
-export type { TimelineProps, TimelineItemProps } from "antd/lib/timeline";
-export { default as Timeline } from "antd/lib/timeline";
-
-export type { TooltipProps } from "antd/lib/tooltip";
-export { default as Tooltip } from "antd/lib/tooltip";
-
-export type { TypographyProps } from "antd/lib/typography";
-export { default as Typography } from "antd/lib/typography";
 export type { TitleProps } from "antd/lib/typography/Title";
-
-export type { UploadProps } from "antd/lib/upload";
-
-export { default as Upload } from "antd/lib/upload";
-
-export { default as version } from "antd/lib/version";
