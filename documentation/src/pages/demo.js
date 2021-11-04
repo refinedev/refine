3639dc305b2402fedf4c723bd50b7e9f9f518a8f
Salih <salihozdemir94@gmail.com>
2021-11-04 12:31:08 +0300

    add mobile view

--- documentation/src/pages/demo.js
+++ documentation/src/pages/demo.js
@@ -8,20 +8,57 @@ import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 const Hello = () => {
     return (
         <Layout title="One framework for all">
-            <div className="main-title-container">
-                <h1>
-                    One framework <br /> for all
-                </h1>
+            <div className="desktop-view">
+                <div className="main-title-container">
+                    <h1>
+                        One framework <br /> for all
+                    </h1>
+                </div>
+                <SplitPane
+                    defaultSize="50%"
+                    minSize={375}
+                    maxSize={-375}
+                    split="vertical"
+                    style={{ height: "auto" }}
+                >
+                    <div className="pane">
+                        <Link
+                            className="link-button"
+                            to="https://alpha.client.example.refine.dev"
+                        >
+                            Public facing app (client)
+                            <IoIosArrowForward />
+                        </Link>
+                        <div className="toggle toggle-left">
+                            <IoIosArrowBack />
+                        </div>
+                        <iframe
+                            src="https://alpha.client.example.refine.dev"
+                            width="100%"
+                            height="100%"
+                        ></iframe>
+                    </div>
+                    <div className="pane">
+                        <Link
+                            className="link-button"
+                            to="https://alpha.example.refine.dev"
+                        >
+                            Admin panel
+                            <IoIosArrowForward />
+                        </Link>
+                        <div className="toggle toggle-right">
+                            <IoIosArrowForward />
+                        </div>
+                        <iframe
+                            src="https://alpha.example.refine.dev"
+                            width="100%"
+                            height="100%"
+                        ></iframe>
+                    </div>
+                </SplitPane>
             </div>
-            <SplitPane
-                defaultSize="50%"
-                minSize={375}
-                maxSize={-375}
-                onResizerClick
-                split="vertical"
-                style={{ height: "auto" }}
-            >
-                <div className="pane">
+            <div className="mobile-view">
+                <div className="mobile-pane client-pane">
                     <Link
                         className="link-button"
                         to="https://alpha.client.example.refine.dev"
@@ -29,16 +66,9 @@ const Hello = () => {
                         Public facing app (client)
                         <IoIosArrowForward />
                     </Link>
-                    <div className="toggle toggle-left">
-                        <IoIosArrowBack />
-                    </div>
-                    <iframe
-                        src="https://alpha.client.example.refine.dev"
-                        width="100%"
-                        height="100%"
-                    ></iframe>
+                    <img src="/demo/nextjs-client.png" />
                 </div>
-                <div className="pane">
+                <div className="mobile-pane admin-pane">
                     <Link
                         className="link-button"
                         to="https://alpha.example.refine.dev"
@@ -46,16 +76,9 @@ const Hello = () => {
                         Admin panel
                         <IoIosArrowForward />
                     </Link>
-                    <div className="toggle toggle-right">
-                        <IoIosArrowForward />
-                    </div>
-                    <iframe
-                        src="https://alpha.example.refine.dev"
-                        width="100%"
-                        height="100%"
-                    ></iframe>
+                    <img src="/demo/react-admin.png" />
                 </div>
-            </SplitPane>
+            </div>
         </Layout>
     );
 };
