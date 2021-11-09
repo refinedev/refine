f130fe11b1aca9888e4183fa75d52eb3c516277a
Umut Özdemir <umutozdemir997@gmail.com>
2021-11-09 10:19:55 +0300

    refactor

--- packages/codemod/src/transformations/refine1-to-refine2.ts
+++ packages/codemod/src/transformations/refine1-to-refine2.ts
@@ -1,13 +1,118 @@
-import { API, FileInfo } from "jscodeshift";
+import { API, JSCodeshift, Collection, FileInfo } from "jscodeshift";
 
 export const parser = "tsx";
 
-export default function transformer(file: FileInfo, api: API): string {
-    const j = api.jscodeshift;
-    const source = j(file.source);
+function addRouterProvider(j: JSCodeshift, root: Collection<any>) {
+    const routerProviderImports = root.find(j.ImportDeclaration, {
+        source: {
+            value: "@pankod/refine-react-router",
+        },
+    });
 
-    // Check if Refine component exists
-    const rootTag = source.find(j.JSXElement, {
+    if (routerProviderImports.length === 0) {
+        // Import route provider
+        root.find(j.ImportDeclaration, {
+            source: {
+                value: "@pankod/refine",
+            },
+        }).insertAfter(
+            j.importDeclaration(
+                [j.importDefaultSpecifier(j.identifier("routerProvider"))],
+                j.literal("@pankod/refine-react-router"),
+            ),
+        );
+    } else {
+        console.log(
+            "WARNING: A router provider from @pankod/refine-react-router is already imported. This tool will not make any migration for router provider.",
+        );
+        return;
+    }
+
+    const refineRoots = root.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                name: "Refine",
+            },
+        },
+    });
+
+    refineRoots.replaceWith((path) => {
+        const openingElement = path.node.openingElement;
+
+        let routesExpression: any;
+
+        const routesAttributeIndex = openingElement.attributes?.findIndex(
+            (attribute) =>
+                attribute.type === "JSXAttribute" &&
+                attribute.name.type === "JSXIdentifier" &&
+                attribute.name.name === "routes",
+        );
+
+        const routerProviderAttributeIndex =
+            openingElement.attributes?.findIndex(
+                (attribute) =>
+                    attribute.type === "JSXAttribute" &&
+                    attribute.name.type === "JSXIdentifier" &&
+                    attribute.name.name === "routerProvider",
+            );
+
+        if (routerProviderAttributeIndex > -1) {
+            console.log(
+                "WARNING: There is already a `routerProvider` attribute on Refine component. This tool will not make any migration for router provider.",
+            );
+            return;
+        }
+
+        if (routesAttributeIndex > -1) {
+            const routesAttribute =
+                openingElement.attributes?.[routesAttributeIndex];
+
+            if (routesAttribute?.type === "JSXAttribute") {
+                const attributeValue = routesAttribute.value;
+
+                if (attributeValue?.type === "JSXExpressionContainer") {
+                    routesExpression = attributeValue.expression;
+                }
+            }
+        }
+
+        if (routesExpression) {
+            openingElement.attributes?.push(
+                j.jsxAttribute(
+                    j.jsxIdentifier("routerProvider"),
+                    j.jsxExpressionContainer(
+                        j.objectExpression([
+                            j.spreadElement(j.identifier("routerProvider")),
+                            j.property(
+                                "init",
+                                j.identifier("routes"),
+                                routesExpression,
+                            ),
+                        ]),
+                    ),
+                ),
+            );
+
+            if (routesAttributeIndex > -1) {
+                openingElement.attributes?.splice(routesAttributeIndex, 1);
+            }
+        } else {
+            openingElement.attributes?.push(
+                j.jsxAttribute(
+                    j.jsxIdentifier("routerProvider"),
+                    j.jsxExpressionContainer(j.identifier("routerProvider")),
+                ),
+            );
+        }
+
+        return path.node;
+    });
+}
+
+const moveResources = (j: JSCodeshift, root: Collection<any>) => {
+    const newResources: { [key: string]: any }[] = [];
+
+    const resourceElements = root.find(j.JSXElement, {
         openingElement: {
             name: {
                 name: "Resource",
@@ -15,121 +120,40 @@ export default function transformer(file: FileInfo, api: API): string {
         },
     });
 
-    if (rootTag.length > 0) {
+    const rootElements = root.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                name: "Refine",
+            },
+        },
+    });
+
+    const resourceImportSpecifiers = root.find(j.ImportSpecifier, {
+        imported: {
+            name: "Resource",
+        },
+    });
+
+    if (resourceElements.length === 0) {
         return;
     }
 
-    // Import route provider
-    source
-        .find(j.ImportDeclaration, {
-            source: {
-                value: "@pankod/refine",
-            },
-        })
-        .insertAfter(
-            j.importDeclaration(
-                [j.importDefaultSpecifier(j.identifier("routerProvider"))],
-                j.literal("@pankod/refine-react-router"),
-            ),
-        );
-
-    // Add routerProvider attribute
-    source
-        .find(j.JSXElement, {
-            openingElement: {
-                name: {
-                    name: "Refine",
-                },
-            },
-        })
-        .replaceWith((path) => {
-            const openingElement = path.node.openingElement;
-
-            let routesExpression: any;
-
-            const routesAttributeIndex = openingElement.attributes?.findIndex(
-                (attribute) =>
-                    attribute.type === "JSXAttribute" &&
-                    attribute.name.type === "JSXIdentifier" &&
-                    attribute.name.name === "routes",
-            );
-
-            if (
-                routesAttributeIndex !== undefined &&
-                routesAttributeIndex > -1
-            ) {
-                const routesAttribute =
-                    openingElement.attributes?.[routesAttributeIndex];
-
-                if (routesAttribute?.type === "JSXAttribute") {
-                    const attributeValue = routesAttribute.value;
-
-                    if (attributeValue?.type === "JSXExpressionContainer") {
-                        routesExpression = attributeValue.expression;
-                    }
-                }
-            }
-
-            if (routesExpression) {
-                openingElement.attributes?.push(
-                    j.jsxAttribute(
-                        j.jsxIdentifier("routerProvider"),
-                        j.jsxExpressionContainer(
-                            j.objectExpression([
-                                j.spreadElement(j.identifier("routerProvider")),
-                                j.property(
-                                    "init",
-                                    j.identifier("routes"),
-                                    routesExpression,
-                                ),
-                            ]),
-                        ),
-                    ),
-                );
-
-                if (routesAttributeIndex !== undefined) {
-                    openingElement.attributes?.splice(routesAttributeIndex, 1);
-                }
-            } else {
-                openingElement.attributes?.push(
-                    j.jsxAttribute(
-                        j.jsxIdentifier("routerProvider"),
-                        j.jsxExpressionContainer(
-                            j.identifier("routerProvider"),
-                        ),
-                    ),
-                );
-            }
-
-            return path.node;
-        });
-
-    const newResources: { [key: string]: any }[] = [];
-
     // Get resources data
-    source
-        .find(j.JSXElement, {
-            openingElement: {
-                name: {
-                    name: "Resource",
-                },
-            },
-        })
-        .forEach((resources) => {
-            const newResource: { [key: string]: any } = {};
+    resourceElements.forEach((resources) => {
+        const newResource: { [key: string]: any } = {};
 
-            resources.node.openingElement.attributes?.forEach((resource) => {
-                if (
-                    resource.type === "JSXAttribute" &&
-                    resource.name.type === "JSXIdentifier" &&
-                    resource.value
-                ) {
-                    newResource[resource.name.name] = resource.value;
-                }
-            });
-
-            newResources.push(newResource);
+        resources.node.openingElement.attributes?.forEach((resource) => {
+            if (
+                resource.type === "JSXAttribute" &&
+                resource.name.type === "JSXIdentifier" &&
+                resource.value
+            ) {
+                newResource[resource.name.name] = resource.value;
+            }
         });
+
+        newResources.push(newResource);
+    });
 
     // Construct a resources attribute with the resources data
     const newAttributes = j.jsxAttribute(
@@ -157,58 +181,61 @@ export default function transformer(file: FileInfo, api: API): string {
     );
 
     // Add resources attribute
-    source
-        .find(j.JSXElement, {
-            openingElement: {
-                name: {
-                    name: "Refine",
-                },
-            },
-        })
-        .replaceWith((path) => {
-            const openingElement = path.node.openingElement;
-            openingElement.attributes?.push(newAttributes);
+    rootElements.replaceWith((path) => {
+        const attributes = path.node.openingElement.attributes;
+
+        const resourcesAttributeIndex = attributes.findIndex(
+            (attribute) =>
+                attribute.type === "JSXAttribute" &&
+                attribute.name.name === "resources",
+        );
+
+        if (resourcesAttributeIndex !== -1) {
+            console.log(
+                "WARNING: There is already a 'resources' attribute on Refine component. This tool will not touch it.",
+            );
 
             return path.node;
-        });
+        }
 
-    // Remove resources
-    source
-        .find(j.JSXElement, {
-            openingElement: {
-                name: {
-                    name: "Resource",
-                },
-            },
-        })
-        .remove();
+        attributes?.push(newAttributes);
 
-    // Remove import of Resource component
-    source
-        .find(j.ImportSpecifier, {
-            imported: {
-                name: "Resource",
-            },
-        })
-        .remove();
+        return path.node;
+    });
+
+    resourceElements.remove();
+
+    resourceImportSpecifiers.remove();
 
     // Clear the body of Refine component
-    source
-        .find(j.JSXElement, {
-            openingElement: {
-                name: {
-                    name: "Refine",
-                },
+    rootElements.replaceWith((path) => {
+        const openingElement = path.node.openingElement;
+
+        path.node.closingElement = null;
+        openingElement.selfClosing = true;
+
+        return path.node;
+    });
+};
+
+export default function transformer(file: FileInfo, api: API): string {
+    const j = api.jscodeshift;
+    const source = j(file.source);
+
+    const rootElement = source.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                name: "Refine",
             },
-        })
-        .replaceWith((path) => {
-            const openingElement = path.node.openingElement;
+        },
+    });
 
-            path.node.closingElement = null;
-            openingElement.selfClosing = true;
+    if (rootElement.length === 0) {
+        return;
+    }
 
-            return path.node;
-        });
+    addRouterProvider(j, source);
+    moveResources(j, source);
 
     return source.toSource();
 }
