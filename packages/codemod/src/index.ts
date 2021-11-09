98a480dc2270ef1530c3f211cb42044542a9dc17
Umut Özdemir <umutozdemir997@gmail.com>
2021-11-09 16:49:28 +0300

    Add codemod package (#1222)
    * add codemod
    
    * remove unrelated types
    
    * wip: handle custom routes
    
    * finalize routerProvider migration
    
    * get cli to work
    
    * refactor
    
    * use vercel license because of forked cli
    
    * add codemod cli forked from nextjs's codemod
    
    * add refine1-to-refine2 codemod 🎉
    
    * remove example files
    
    * update codemod package dependencies
    
    * update codemod deps
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

---
+++ packages/codemod/src/index.ts
@@ -0,0 +1,216 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+// Based on https://github.com/vercel/next.js/blob/canary/packages/next-codemod/bin/cli.ts
+// and https://github.com/reactjs/react-codemod/blob/dd8671c9a470a2c342b221ec903c574cf31e9f57/bin/cli.js
+// @pankod/refine-codemod name-of-transform optional/path/to/src [...options]
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+import globby from "globby";
+import inquirer from "inquirer";
+import meow from "meow";
+import path from "path";
+import execa from "execa";
+import chalk from "chalk";
+import isGitClean from "is-git-clean";
+
+export const jscodeshiftExecutable = require.resolve(".bin/jscodeshift");
+export const transformerDirectory = path.join(
+    __dirname,
+    "../",
+    "dist/transformations",
+);
+
+const transformsWithPostTransform = ["refine1-to-refine2"];
+
+export function checkGitStatus(force) {
+    let clean = false;
+    let errorMessage = "Unable to determine if git directory is clean";
+    try {
+        clean = isGitClean.sync(process.cwd());
+        errorMessage = "Git directory is not clean";
+    } catch (err) {
+        if (
+            err &&
+            err.stderr &&
+            err.stderr.indexOf("Not a git repository") >= 0
+        ) {
+            clean = true;
+        }
+    }
+
+    if (!clean) {
+        if (force) {
+            console.log(`WARNING: ${errorMessage}. Forcibly continuing.`);
+        } else {
+            console.log("Thank you for using @pankod/refine-codemod!");
+            console.log(
+                chalk.yellow(
+                    "\nBut before we continue, please stash or commit your git changes.",
+                ),
+            );
+            console.log(
+                "\nYou may use the --force flag to override this safety check.",
+            );
+            process.exit(1);
+        }
+    }
+}
+
+export function runTransform({ files, flags, transformer }) {
+    const transformerPath = path.join(
+        transformerDirectory,
+        `${transformer}.js`,
+    );
+
+    let args = [];
+
+    const { dry, print, runInBand } = flags;
+
+    if (dry) {
+        args.push("--dry");
+    }
+    if (print) {
+        args.push("--print");
+    }
+    if (runInBand) {
+        args.push("--run-in-band");
+    }
+
+    args.push("--verbose=2");
+
+    args.push("--ignore-pattern=**/node_modules/**");
+
+    args.push("--extensions=tsx,ts,jsx,js");
+    args.push("--parser=tsx");
+
+    args = args.concat(["--transform", transformerPath]);
+
+    if (flags.jscodeshift) {
+        args = args.concat(flags.jscodeshift);
+    }
+
+    args = args.concat(files);
+
+    console.log(`Executing command: jscodeshift ${args.join(" ")}`);
+
+    const result = execa.sync(jscodeshiftExecutable, args, {
+        stdio: "inherit",
+        stripFinalNewline: false,
+    });
+
+    if (result.failed) {
+        throw new Error(`jscodeshift exited with code ${result.exitCode}`);
+    }
+}
+
+const TRANSFORMER_INQUIRER_CHOICES = [
+    {
+        name: "refine1-to-refine2: Transform from refine 1.x.x to at least 2.0.0",
+        value: "refine1-to-refine2",
+    },
+];
+
+function expandFilePathsIfNeeded(filesBeforeExpansion) {
+    const shouldExpandFiles = filesBeforeExpansion.some((file) =>
+        file.includes("*"),
+    );
+    return shouldExpandFiles
+        ? globby.sync(filesBeforeExpansion)
+        : filesBeforeExpansion;
+}
+
+export async function run(): Promise<void> {
+    const cli = meow({
+        description: "Codemods for updating refine apps.",
+        help: `
+    Usage
+      $ npx @pankod/refine-codemod <transform> <path> <...options>
+        path         Files or directory to transform. Can be a glob like pages/**.js
+    Options
+      --force            Bypass Git safety checks and forcibly run codemods
+      --dry              Dry run (no changes are made to files)
+      --print            Print transformed files to your terminal
+      --jscodeshift  (Advanced) Pass options directly to jscodeshift
+    `,
+        flags: {
+            boolean: ["force", "dry", "print", "help"],
+            string: ["_"],
+            alias: {
+                h: "help",
+            },
+        },
+    } as meow.Options<meow.AnyFlags>);
+
+    if (!cli.flags.dry) {
+        checkGitStatus(cli.flags.force);
+    }
+
+    if (
+        cli.input[0] &&
+        !TRANSFORMER_INQUIRER_CHOICES.find((x) => x.value === cli.input[0])
+    ) {
+        console.error("Invalid transform choice, pick one of:");
+        console.error(
+            TRANSFORMER_INQUIRER_CHOICES.map((x) => "- " + x.value).join("\n"),
+        );
+        process.exit(1);
+    }
+
+    const { files, transformer } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "files",
+            message:
+                "On which files or directory should the codemods be applied?",
+            when: !cli.input[1],
+            default: ".",
+            filter: (files) => files.trim(),
+        },
+        {
+            type: "list",
+            name: "transformer",
+            message: "Which transform would you like to apply?",
+            when: !cli.input[0],
+            pageSize: TRANSFORMER_INQUIRER_CHOICES.length,
+            choices: TRANSFORMER_INQUIRER_CHOICES,
+        },
+    ]);
+
+    const filesBeforeExpansion = cli.input[1] || files;
+    const filesExpanded = expandFilePathsIfNeeded([filesBeforeExpansion]);
+
+    const selectedTransformer = cli.input[0] || transformer;
+
+    if (!filesExpanded.length) {
+        console.log(
+            `No files found matching ${filesBeforeExpansion.join(" ")}`,
+        );
+        return null;
+    }
+
+    await runTransform({
+        files: filesExpanded,
+        flags: cli.flags,
+        transformer: selectedTransformer,
+    });
+
+    if (transformsWithPostTransform.includes(selectedTransformer)) {
+        const transformerPath = path.join(
+            transformerDirectory,
+            `${selectedTransformer}.js`,
+        );
+
+        await require(transformerPath).postTransform(filesExpanded, cli.flags);
+    }
+}
+
+run();
---
+++ packages/codemod/src/transformations/refine1-to-refine2.ts
@@ -0,0 +1,307 @@
+import { API, JSCodeshift, Collection, FileInfo } from "jscodeshift";
+import fs from "fs";
+import path from "path";
+import { install } from "../helpers";
+import checkPackageLock from "../helpers/checkPackageLock";
+
+export const parser = "tsx";
+
+function addRouterProvider(j: JSCodeshift, root: Collection<any>) {
+    const routerProviderImports = root.find(j.ImportDeclaration, {
+        source: {
+            value: "@pankod/refine-react-router",
+        },
+    });
+
+    if (routerProviderImports.length === 0) {
+        // Import route provider
+        root.find(j.ImportDeclaration, {
+            source: {
+                value: "@pankod/refine",
+            },
+        }).insertAfter(
+            j.importDeclaration(
+                [j.importDefaultSpecifier(j.identifier("routerProvider"))],
+                j.literal("@pankod/refine-react-router"),
+            ),
+        );
+    } else {
+        console.log(
+            "WARNING: A router provider from @pankod/refine-react-router is already imported. This tool will not make any migration for router provider.",
+        );
+        return;
+    }
+
+    const refineRoots = root.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                name: "Refine",
+            },
+        },
+    });
+
+    refineRoots.replaceWith((path) => {
+        const openingElement = path.node.openingElement;
+
+        let routesExpression: any;
+
+        const routesAttributeIndex = openingElement.attributes?.findIndex(
+            (attribute) =>
+                attribute.type === "JSXAttribute" &&
+                attribute.name.type === "JSXIdentifier" &&
+                attribute.name.name === "routes",
+        );
+
+        const routerProviderAttributeIndex =
+            openingElement.attributes?.findIndex(
+                (attribute) =>
+                    attribute.type === "JSXAttribute" &&
+                    attribute.name.type === "JSXIdentifier" &&
+                    attribute.name.name === "routerProvider",
+            );
+
+        if (routerProviderAttributeIndex > -1) {
+            console.log(
+                "WARNING: There is already a `routerProvider` attribute on Refine component. This tool will not make any migration for router provider.",
+            );
+            return;
+        }
+
+        if (routesAttributeIndex > -1) {
+            const routesAttribute =
+                openingElement.attributes?.[routesAttributeIndex];
+
+            if (routesAttribute?.type === "JSXAttribute") {
+                const attributeValue = routesAttribute.value;
+
+                if (attributeValue?.type === "JSXExpressionContainer") {
+                    routesExpression = attributeValue.expression;
+                }
+            }
+        }
+
+        if (routesExpression) {
+            openingElement.attributes?.push(
+                j.jsxAttribute(
+                    j.jsxIdentifier("routerProvider"),
+                    j.jsxExpressionContainer(
+                        j.objectExpression([
+                            j.spreadElement(j.identifier("routerProvider")),
+                            j.property(
+                                "init",
+                                j.identifier("routes"),
+                                routesExpression,
+                            ),
+                        ]),
+                    ),
+                ),
+            );
+
+            if (routesAttributeIndex > -1) {
+                openingElement.attributes?.splice(routesAttributeIndex, 1);
+            }
+        } else {
+            openingElement.attributes?.push(
+                j.jsxAttribute(
+                    j.jsxIdentifier("routerProvider"),
+                    j.jsxExpressionContainer(j.identifier("routerProvider")),
+                ),
+            );
+        }
+
+        return path.node;
+    });
+}
+
+const moveResources = (j: JSCodeshift, root: Collection<any>) => {
+    const newResources: { [key: string]: any }[] = [];
+
+    const resourceElements = root.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                name: "Resource",
+            },
+        },
+    });
+
+    const rootElements = root.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                name: "Refine",
+            },
+        },
+    });
+
+    const resourceImportSpecifiers = root.find(j.ImportSpecifier, {
+        imported: {
+            name: "Resource",
+        },
+    });
+
+    if (resourceElements.length === 0) {
+        return;
+    }
+
+    // Get resources data
+    resourceElements.forEach((resources) => {
+        const newResource: { [key: string]: any } = {};
+
+        resources.node.openingElement.attributes?.forEach((resource) => {
+            if (
+                resource.type === "JSXAttribute" &&
+                resource.name.type === "JSXIdentifier" &&
+                resource.value
+            ) {
+                newResource[resource.name.name] = resource.value;
+            }
+        });
+
+        newResources.push(newResource);
+    });
+
+    // Construct a resources attribute with the resources data
+    const newAttributes = j.jsxAttribute(
+        j.jsxIdentifier("resources"),
+        j.jsxExpressionContainer(
+            j.arrayExpression(
+                newResources.map((resource) => {
+                    const newValue = j.objectExpression(
+                        Object.entries(resource).map(([key, value]) => {
+                            const valueToPut = value.expression
+                                ? value.expression
+                                : value;
+                            return j.property(
+                                "init",
+                                j.identifier(key),
+                                valueToPut as any,
+                            );
+                        }),
+                    );
+
+                    return newValue;
+                }),
+            ),
+        ),
+    );
+
+    // Add resources attribute
+    rootElements.replaceWith((path) => {
+        const attributes = path.node.openingElement.attributes;
+
+        const resourcesAttributeIndex = attributes.findIndex(
+            (attribute) =>
+                attribute.type === "JSXAttribute" &&
+                attribute.name.name === "resources",
+        );
+
+        if (resourcesAttributeIndex !== -1) {
+            console.log(
+                "WARNING: There is already a 'resources' attribute on Refine component. This tool will not touch it.",
+            );
+
+            return path.node;
+        }
+
+        attributes?.push(newAttributes);
+
+        return path.node;
+    });
+
+    resourceElements.remove();
+    resourceImportSpecifiers.remove();
+
+    // Clear the body of Refine component
+    rootElements.replaceWith((path) => {
+        const openingElement = path.node.openingElement;
+
+        path.node.closingElement = null;
+        openingElement.selfClosing = true;
+
+        return path.node;
+    });
+};
+
+const packagesToUpdate = [
+    "@pankod/refine-airtable",
+    "@pankod/refine-altogic",
+    "@pankod/refine-graphql",
+    "@pankod/refine-hasura",
+    "@pankod/refine-nestjsx-crud",
+    "@pankod/refine-nextjs-router",
+    "@pankod/refine-react-router",
+    "@pankod/refine-simple-rest",
+    "@pankod/refine-strapi",
+    "@pankod/refine-strapi-graphql",
+    "@pankod/refine-supabase",
+];
+
+export async function postTransform(files: any, flags: any) {
+    const rootDir = path.join(process.cwd(), files[0]);
+    const packageJsonPath = path.join(rootDir, "package.json");
+    const useYarn = checkPackageLock(rootDir) === "yarn.lock";
+    let packageJsonData;
+
+    try {
+        packageJsonData = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+    } catch (err) {
+        console.error(
+            `Error: failed to load package.json from ${packageJsonPath}, ensure provided directory is root.`,
+        );
+    }
+
+    const dependenciesToInstall: Array<{
+        name: string;
+        version: string;
+    }> = [
+        {
+            name: "@pankod/refine",
+            version: "2.x.x",
+        },
+        {
+            name: "@pankod/refine-react-router",
+            version: "2.x.x",
+        },
+    ];
+
+    for (const key of Object.keys(packageJsonData.dependencies)) {
+        if (packagesToUpdate.includes(key)) {
+            dependenciesToInstall.push({
+                name: key,
+                version: "2.x.x",
+            });
+        }
+    }
+
+    if (!flags.dry) {
+        await install(
+            rootDir,
+            dependenciesToInstall.map((dep) => `${dep.name}@${dep.version}`),
+            {
+                useYarn,
+                isOnline: true,
+            },
+        );
+    }
+}
+
+export default function transformer(file: FileInfo, api: API): string {
+    const j = api.jscodeshift;
+    const source = j(file.source);
+
+    const rootElement = source.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                name: "Refine",
+            },
+        },
+    });
+
+    if (rootElement.length === 0) {
+        return;
+    }
+
+    addRouterProvider(j, source);
+    moveResources(j, source);
+
+    return source.toSource();
+}
