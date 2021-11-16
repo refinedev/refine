c47eead71ef51b23a26e5a0a98b8cf92dffc42cd
Salih <salihozdemir94@gmail.com>
2021-11-16 13:02:55 +0300

    add queryOptions params to useCan

--- packages/core/src/hooks/accessControl/useCan.ts
+++ packages/core/src/hooks/accessControl/useCan.ts
@@ -1,16 +1,20 @@
 import { useContext } from "react";
-import { useQuery, UseQueryResult } from "react-query";
+import { useQuery, UseQueryResult, UseQueryOptions } from "react-query";
 
 import { AccessControlContext } from "@contexts/accessControl";
 import { CanParams } from "../../interfaces";
 
-export const useCan = (params: CanParams): UseQueryResult<boolean> => {
+export const useCan = (
+    params: CanParams,
+    queryOptions?: UseQueryOptions<boolean>,
+): UseQueryResult<boolean> => {
     const { can } = useContext(AccessControlContext);
 
     const queryResponse = useQuery<boolean>(
         ["useCan", { ...params }],
         () => can(params),
         {
+            ...queryOptions,
             retry: false,
         },
     );
