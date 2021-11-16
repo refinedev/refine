98cfd96712c034a4e13ce5c6076fc425eeffc450
mlheknci <mmlhekinci@gmail.com>
2021-11-16 15:28:30 +0300

    feedback panel blog added

---
+++ examples/blog/refeedback/src/App.tsx
@@ -0,0 +1,30 @@
+import { Refine, Resource } from "@pankod/refine";
+
+import "@pankod/refine/dist/styles.min.css";
+import { DataProvider } from "@pankod/refine-strapi";
+import strapiAuthProvider from "authProvider";
+import { Header, Layout, OffLayoutArea } from "components";
+
+import { FeedbackList } from "./pages/FeedbackList";
+
+import "styles/global.css";
+
+function App() {
+    const API_URL = "http://localhost:1337";
+
+    const { authProvider, axiosInstance } = strapiAuthProvider(API_URL);
+    const dataProvider = DataProvider(API_URL, axiosInstance);
+    return (
+        <Refine
+            dataProvider={dataProvider}
+            authProvider={authProvider}
+            Header={Header}
+            Layout={Layout}
+            OffLayoutArea={OffLayoutArea}
+        >
+            <Resource name="feedbacks" list={FeedbackList} />
+        </Refine>
+    );
+}
+
+export default App;
