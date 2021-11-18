3c09fa35f3f449c40443cbb01e073b0c3be5e00c
Melih Ekinci <mmlhekinci@gmail.com>
2021-11-18 10:19:26 +0300

    manage hackathons blog added (#1256)
    * manage hackathons blog added
    
    * blog edited
    
    * codesandbox link added
    
    * links fixed

---
+++ examples/blog/hackathonize/src/pages/criterias/edit.tsx
@@ -0,0 +1,26 @@
+import { Edit, Form, Input, Select, useForm, useSelect } from "@pankod/refine";
+
+import { CriteriaType, HackathonType } from "interfaces";
+
+export const CriteriasEdit: React.FC = () => {
+    const { formProps, saveButtonProps, queryResult } = useForm<CriteriaType>();
+
+    const { selectProps: hackathonSelectProps } = useSelect<HackathonType>({
+        resource: "hackathons",
+        defaultValue: queryResult?.data?.data?.hackathon_id,
+        optionLabel: "name",
+    });
+
+    return (
+        <Edit saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item label="Name" name="name">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Hackathon" name="hackathon_id">
+                    <Select {...hackathonSelectProps} />
+                </Form.Item>
+            </Form>
+        </Edit>
+    );
+};
---
+++ examples/blog/hackathonize/src/utility/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@pankod/refine-supabase";
+
+const SUPABASE_URL = "https://grqowunuiqfxzvcurivl.supabase.co";
+const SUPABASE_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzA3NjQ3MiwiZXhwIjoxOTQ4NjUyNDcyfQ.YpDFpBI8LAj3_5oxCMCxkWKdL5weoEe4Gh0wQwIudic";
+
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
