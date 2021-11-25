f4784a31089edbf468b1be4d6f8e02b07b8933d4
Umut Özdemir <umutozdemir997@gmail.com>
2021-11-25 16:04:40 +0300

    add pubnub example

---
+++ examples/pubnub/src/App.tsx
@@ -0,0 +1,32 @@
+import { Refine } from "@pankod/refine";
+import dataProvider from "@pankod/refine-simple-rest";
+import { liveProvider } from "@pankod/refine-pubnub";
+import routerProvider from "@pankod/refine-react-router";
+import "@pankod/refine/dist/styles.min.css";
+
+import { pubnubClient } from "utility";
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+const App: React.FC = () => {
+    return (
+        <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider(API_URL)}
+            liveProvider={liveProvider(pubnubClient)}
+            resources={[
+                {
+                    name: "posts",
+                    list: PostList,
+                    create: PostCreate,
+                    edit: PostEdit,
+                    show: PostShow,
+                    canDelete: true,
+                },
+            ]}
+        />
+    );
+};
+
+export default App;
---
+++ examples/pubnub/src/utility/pubnubClient.ts
@@ -0,0 +1,6 @@
+import PubNub from "pubnub";
+
+export const pubnubClient = new PubNub({
+    publishKey: "pub-c-16480a48-d534-4929-bcaa-0f98e54c7fd0",
+    subscribeKey: "sub-c-06614b42-3e0c-11ec-8182-fea14ba1eb2b",
+});
