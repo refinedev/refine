6cc0e39881d097153306eb020bb654b9e51c650d
Muharrem Kocadere <mkocadere@gmail.com>
2021-11-25 17:56:11 +0300

    update casbin-next example

--- examples/accessControl/casbin-next/pages/_app.tsx
+++ examples/accessControl/casbin-next/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppProps } from "next/app";
 
 import { Refine } from "@pankod/refine";
@@ -6,7 +6,15 @@ import routerProvider from "@pankod/refine-nextjs-router";
 import dataProvider from "@pankod/refine-simple-rest";
 import { newEnforcer, newModel, MemoryAdapter } from "casbin.js";
 
+import { Header } from "@components/header";
 import { PostList, PostCreate, PostEdit, PostShow } from "@components/posts";
+import {
+    CategoryList,
+    CategoryCreate,
+    CategoryEdit,
+    CategoryShow,
+} from "@components/categories";
+import { UserList, UserCreate, UserEdit, UserShow } from "@components/users";
 
 import "@pankod/refine/dist/styles.min.css";
 
@@ -17,27 +25,41 @@ export const model = newModel(`
 r = sub, obj, act
 
 [policy_definition]
-p = sub, obj, act
+p = sub, obj, act, eft
 
 [role_definition]
 g = _, _
 
 [policy_effect]
-e = some(where (p.eft == allow))
+e = some(where (p.eft == allow)) && !some(where (p.eft == deny))
 
 [matchers]
 m = g(r.sub, p.sub) && keyMatch(r.obj, p.obj) && regexMatch(r.act, p.act)
 `);
 
 export const adapter = new MemoryAdapter(`
-p, user, posts, (list)|(delete)|(create)|(show)
+p, admin, posts, (list)|(create)
+p, admin, posts/*, (edit)|(show)|(delete)
+p, admin, posts/*, field
 
-p, user, posts/10, delete
-p, user, posts/*, edit
+p, admin, users, (list)|(create)
+p, admin, users/*, (edit)|(show)|(delete)
+
+p, admin, categories, (list)|(create)
+p, admin, categories/*, (edit)|(show)|(delete)
+
+p, editor, posts, (list)|(create)
+p, editor, posts/*, (edit)|(show)
+p, editor, posts/hit, field, deny
+
+p, editor, categories, list
 
 `);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    // const [role, setRole] = useState("admin");
+    const role = "editor";
+
     return (
         <Refine
             routerProvider={routerProvider}
@@ -46,16 +68,30 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
                 can: async ({ action, params, resource }) => {
                     const enforcer = await newEnforcer(model, adapter);
                     if (
-                        (action === "delete" || action === "edit") &&
-                        !!params
+                        action === "delete" ||
+                        action === "edit" ||
+                        action === "show"
                     ) {
-                        return enforcer.enforce(
-                            "user",
-                            `${resource}/${params.id}`,
-                            action,
-                        );
+                        return Promise.resolve({
+                            can: await enforcer.enforce(
+                                role,
+                                `${resource}/${params.id}`,
+                                action,
+                            ),
+                        });
                     }
-                    return enforcer.enforce("user", `${resource}`, action);
+                    if (action === "field") {
+                        return Promise.resolve({
+                            can: await enforcer.enforce(
+                                role,
+                                `${resource}/${params.field}`,
+                                action,
+                            ),
+                        });
+                    }
+                    return Promise.resolve({
+                        can: await enforcer.enforce(role, resource, action),
+                    });
                 },
             }}
             resources={[
@@ -66,7 +102,22 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
                     edit: PostEdit,
                     show: PostShow,
                 },
+                {
+                    name: "users",
+                    list: UserList,
+                    create: UserCreate,
+                    edit: UserEdit,
+                    show: UserShow,
+                },
+                {
+                    name: "categories",
+                    list: CategoryList,
+                    create: CategoryCreate,
+                    edit: CategoryEdit,
+                    show: CategoryShow,
+                },
             ]}
+            // Header={() => <Header role={role} setRole={setRole} />}
         >
             <Component {...pageProps} />
         </Refine>
