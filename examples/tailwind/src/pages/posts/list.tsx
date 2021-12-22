8d5ff60629ee174e7f2a9e9226cc8627eeb8da74
Salih <salihozdemir94@gmail.com>
2021-12-22 18:00:29 +0300

    initial commit for react table example
    Co-authored-by: mhrrmk <mkocadere@gmail.com>

--- examples/tailwind/src/pages/posts/list.tsx
+++ examples/tailwind/src/pages/posts/list.tsx
@@ -1,28 +1,78 @@
+/* eslint-disable react/jsx-key */
+import React from "react";
 import {
     List,
-    Table,
-    TextField,
-    useTable,
-    IResourceComponentsProps,
-    Space,
-    EditButton,
-    ShowButton,
-    useMany,
-    FilterDropdown,
+    useTable as useTableCore,
     useSelect,
-    Select,
-    Radio,
-    TagField,
+    useMany,
 } from "@pankod/refine-core";
+import { useTable, usePagination, Column } from "react-table";
 
 import { IPost, ICategory } from "interfaces";
 
-export const PostList: React.FC<IResourceComponentsProps> = () => {
-    const { tableProps } = useTable<IPost>();
+const columns: Array<Column> = [
+    {
+        Header: "ID",
+        accessor: "id",
+    },
+    {
+        Header: "Title",
+        accessor: "title",
+    },
+    {
+        Header: "Category",
+        accessor: "category.id",
+    },
+    {
+        Header: "Status",
+        accessor: "status",
+    },
+    {
+        Header: "Actions",
+    },
+];
 
-    const categoryIds =
-        tableProps?.dataSource?.map((item) => item.category.id) ?? [];
-    const { data, isLoading } = useMany<ICategory>({
+export const PostList: React.FC = () => {
+    const {
+        tableQueryResult,
+        current,
+        setCurrent,
+        pageSize: pageSizeCore,
+        setPageSize: setPageSizeCore,
+    } = useTableCore<IPost>({
+        syncWithLocation: true,
+    });
+
+    const { data } = tableQueryResult;
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = useTable(
+        {
+            columns,
+            data: tableQueryResult.data?.data || [],
+            initialState: { pageIndex: current - 1, pageSize: pageSizeCore },
+            pageCount: (data?.total || 0) / pageSizeCore,
+            manualPagination: true,
+        },
+        usePagination,
+    );
+
+    const categoryIds = data?.data.map((item) => item.category.id) ?? [];
+    const { data: categoryData, isLoading } = useMany<ICategory>({
         resource: "categories",
         ids: categoryIds,
         queryOptions: {
@@ -30,15 +80,131 @@ export const PostList: React.FC<IResourceComponentsProps> = () => {
         },
     });
 
-    const { selectProps: categorySelectProps } = useSelect<ICategory>({
-        resource: "categories",
-        optionLabel: "title",
-        optionValue: "id",
-    });
+    // const { selectProps: categorySelectProps } = useSelect<ICategory>({
+    //     resource: "categories",
+    //     optionLabel: "title",
+    //     optionValue: "id",
+    // });
 
     return (
         <List>
-            <Table {...tableProps} rowKey="id">
+            <table {...getTableProps()} className="min-w-full">
+                <thead className="bg-white border-b">
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th
+                                    {...column.getHeaderProps()}
+                                    className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                >
+                                    {column.render("Header")}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map((row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr
+                                {...row.getRowProps()}
+                                className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+                            >
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td
+                                            {...cell.getCellProps()}
+                                            className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                        >
+                                            {cell.render("Cell")}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+
+            <div className="mt-4">
+                <button
+                    onClick={() => {
+                        gotoPage(0);
+                        setCurrent(1);
+                    }}
+                    disabled={!canPreviousPage}
+                    className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out"
+                >
+                    {"<<"}
+                </button>{" "}
+                <button
+                    onClick={() => {
+                        previousPage();
+                        setCurrent((prev) => prev - 1);
+                    }}
+                    disabled={!canPreviousPage}
+                    className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out"
+                >
+                    {"<"}
+                </button>{" "}
+                <button
+                    onClick={() => {
+                        nextPage();
+                        setCurrent((prev) => prev + 1);
+                    }}
+                    disabled={!canNextPage}
+                    className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out"
+                >
+                    {">"}
+                </button>{" "}
+                <button
+                    onClick={() => {
+                        gotoPage(pageCount - 1);
+                        setCurrent(pageCount);
+                    }}
+                    disabled={!canNextPage}
+                    className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out"
+                >
+                    {">>"}
+                </button>{" "}
+                <span>
+                    Page{" "}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{" "}
+                </span>
+                <span>
+                    | Go to page:{" "}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={(e) => {
+                            const page = e.target.value
+                                ? Number(e.target.value) - 1
+                                : 0;
+                            gotoPage(page);
+                            setCurrent(page);
+                        }}
+                        className="w-16 px-2 py-1 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    />
+                </span>{" "}
+                <select
+                    value={pageSize}
+                    onChange={(e) => {
+                        setPageSize(Number(e.target.value));
+                        setPageSizeCore(Number(e.target.value));
+                    }}
+                    className="px-2 py-1 text-sm font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                >
+                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            {/* <Table {...tableProps} rowKey="id">
                 <Table.Column dataIndex="id" title="ID" />
                 <Table.Column dataIndex="title" title="Title" />
                 <Table.Column
@@ -101,7 +267,7 @@ export const PostList: React.FC<IResourceComponentsProps> = () => {
                         </Space>
                     )}
                 />
-            </Table>
+            </Table> */}
         </List>
     );
 };
