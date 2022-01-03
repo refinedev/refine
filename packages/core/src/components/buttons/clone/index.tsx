5df5d2600b903be1373e0db85909da4c7ae5a60a
Ömer Faruk APLAK <omer@pankod.com>
2022-01-03 15:35:37 +0300

    fix buttons resourceName prop issue (#1401)

--- packages/core/src/components/buttons/clone/index.tsx
+++ packages/core/src/components/buttons/clone/index.tsx
@@ -51,7 +51,7 @@ export const CloneButton: FC<CloneButtonProps> = ({
     const id = recordItemId ?? idFromRoute;
 
     const onButtonClick = () => {
-        clone(routeResourceName, id);
+        clone(resourceName, id);
     };
 
     const { data } = useCan({
