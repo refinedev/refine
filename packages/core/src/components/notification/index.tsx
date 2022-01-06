31540e138f1676e54c94c3b7633e933f8c7cccb0
Salih <salihozdemir94@gmail.com>
2022-01-06 14:41:01 +0300

    remove antd design dependecy

--- packages/core/src/components/notification/index.tsx
+++ packages/core/src/components/notification/index.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect } from "react";
-import { Button } from "antd";
-import { UndoOutlined } from "@ant-design/icons";
 
 import { ActionTypes } from "@contexts/notification";
 import { useCancelNotification, useNotification, useTranslate } from "@hooks";
 import { INotification } from "../../interfaces";
 
-import { NotificationProgress } from "./components";
 import { userFriendlySecond } from "@definitions/helpers";
 
 export const Notification: React.FC<{
@@ -15,7 +12,7 @@ export const Notification: React.FC<{
     const translate = useTranslate();
 
     const { notificationDispatch } = useCancelNotification();
-    const { open, close } = useNotification();
+    const { open } = useNotification();
 
     const cancelNotification = () => {
         notifications.forEach((notificationItem: INotification) => {
@@ -23,52 +20,6 @@ export const Notification: React.FC<{
                 if (notificationItem.seconds === 0) {
                     notificationItem.doMutation();
                 }
-                const description = (
-                    <div
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "space-between",
-                            marginTop: "-7px",
-                        }}
-                    >
-                        <NotificationProgress
-                            dispatch={notificationDispatch}
-                            notificationItem={notificationItem}
-                        />
-                        <span style={{ marginLeft: 8, width: "100%" }}>
-                            {translate(
-                                "notifications.undoable",
-                                {
-                                    seconds: userFriendlySecond(
-                                        notificationItem.seconds,
-                                    ),
-                                },
-                                `You have ${userFriendlySecond(
-                                    notificationItem.seconds,
-                                )} seconds to undo`,
-                            )}
-                        </span>
-                        <Button
-                            style={{ flexShrink: 0 }}
-                            onClick={() => {
-                                notificationDispatch({
-                                    type: ActionTypes.REMOVE,
-                                    payload: {
-                                        id: notificationItem.id,
-                                        resource: notificationItem.resource,
-                                    },
-                                });
-                                notificationItem.cancelMutation();
-                                close(
-                                    `${notificationItem.id}-${notificationItem.resource}-notification`,
-                                );
-                            }}
-                            disabled={notificationItem.seconds === 0}
-                            icon={<UndoOutlined />}
-                        ></Button>
-                    </div>
-                );
 
                 open({
                     key: `${notificationItem.id}-${notificationItem.resource}-notification`,
