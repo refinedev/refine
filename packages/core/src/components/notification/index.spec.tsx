417f69a552b0b6b98bb3e5019ec4b4d6ee6cfda1
Salih <salihozdemir94@gmail.com>
2022-01-06 17:42:15 +0300

    update notification tests in core

--- packages/core/src/components/notification/index.spec.tsx
+++ packages/core/src/components/notification/index.spec.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import { render, fireEvent } from "@test";
+import { render, TestWrapper } from "@test";
 
 import { Notification } from "./index";
-import { userFriendlySecond } from "@definitions";
 
 const doMutation = jest.fn();
 const cancelMutation = jest.fn();
+
+const openMock = jest.fn();
+const closeMock = jest.fn();
 
 const mockNotification = [
     {
@@ -21,42 +23,23 @@ const mockNotification = [
 ];
 
 describe("Cancel Notification", () => {
-    it("should render cancel notification successfuly", async () => {
-        const { getByText } = render(
-            <Notification notifications={mockNotification} />,
-        );
+    it("should trigger notification open function", async () => {
+        render(<Notification notifications={mockNotification} />, {
+            wrapper: TestWrapper({
+                notificationProvider: {
+                    open: openMock,
+                    close: closeMock,
+                },
+            }),
+        });
 
-        const formattedSeconds = userFriendlySecond(
-            mockNotification[0].seconds,
-        );
-
-        getByText(`You have ${formattedSeconds} seconds to undo`);
-    });
-
-    it("should render Progress successfuly", async () => {
-        const { getByRole } = render(
-            <Notification notifications={mockNotification} />,
-        );
-
-        const icon = getByRole("alert");
-        expect(
-            icon.children[1].children[0].children[0].classList.contains(
-                "ant-progress-circle",
-            ),
-        );
-    });
-
-    it("should click and render undo button successfuly", async () => {
-        const { container } = render(
-            <Notification notifications={mockNotification} />,
-        );
-
-        const button = container.querySelector("button");
-        expect(button).toBeDefined();
-        if (button) {
-            fireEvent.click(button);
-            expect(cancelMutation).toBeCalledTimes(1);
-        }
+        expect(openMock).toBeCalledWith({
+            cancelMutation: cancelMutation,
+            key: "1-posts-notification",
+            message: "You have 5 seconds to undo",
+            type: "progress",
+            undoableTimeout: 5,
+        });
     });
 
     it("should call doMutation on seconds zero", async () => {
--- packages/core/src/hooks/notification/useHandleNotification/index.spec.tsx
+++ packages/core/src/hooks/notification/useHandleNotification/index.spec.tsx
@@ -2,14 +2,22 @@ import { renderHook } from "@testing-library/react-hooks";
 import { TestWrapper } from "@test";
 
 import { useHandleNotification } from "./";
+import { OpenNotificationParams } from "src/interfaces";
+
+const dummyNotification: OpenNotificationParams = {
+    key: "notification",
+    type: "success",
+    message: "test",
+    description: "i am here!!",
+};
+
+const openMock = jest.fn();
+const closeMock = jest.fn();
 
 describe("useHandleNotification Hook", () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
-
-    const openMock = jest.fn();
-    const closeMock = jest.fn();
 
     const { result } = renderHook(() => useHandleNotification(), {
         wrapper: TestWrapper({
@@ -21,10 +29,6 @@ describe("useHandleNotification Hook", () => {
     });
 
     it("should call notification open when notification passed", async () => {
-        const dummyNotification = {
-            message: "test",
-            description: "i am here!!",
-        };
         result.current(dummyNotification);
 
         expect(openMock).toHaveBeenCalledTimes(1);
@@ -38,9 +42,7 @@ describe("useHandleNotification Hook", () => {
     });
 
     it("should not call notification open when notification passed to false and passed a fallback notification", async () => {
-        result.current(false, {
-            message: "test",
-        });
+        result.current(false, dummyNotification);
 
         expect(openMock).toHaveBeenCalledTimes(0);
     });
@@ -48,26 +50,28 @@ describe("useHandleNotification Hook", () => {
     it("should work first notification when passed notification with fallback notification ", async () => {
         result.current(
             {
+                ...dummyNotification,
                 message: "i am here!",
             },
-            {
-                message: "i am here too!",
-            },
+            dummyNotification,
         );
 
         expect(openMock).toHaveBeenCalledTimes(1);
         expect(openMock).toHaveBeenCalledWith({
+            ...dummyNotification,
             message: "i am here!",
         });
     });
 
     it("should work fallback notification when not passed notification and passed fallback notification ", async () => {
         result.current(undefined, {
+            ...dummyNotification,
             message: "i am here too!",
         });
 
         expect(openMock).toHaveBeenCalledTimes(1);
         expect(openMock).toHaveBeenCalledWith({
+            ...dummyNotification,
             message: "i am here too!",
         });
     });
