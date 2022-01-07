7797cc45bffb0d2ec55ee3f30118a983dd096ccf
Salih <salihozdemir94@gmail.com>
2022-01-07 16:14:22 +0300

    add antd design notification tests

---
+++ packages/antd/src/components/undoableNotification/index.spec.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { fireEvent, render } from "@test";
+
+import { UndoableNotification, UndoableNotificationProps } from ".";
+
+const cancelMutation = jest.fn();
+
+const mockNotification: UndoableNotificationProps = {
+    notificationKey: "notificationKey",
+    message: "Test Notification Message",
+    undoableTimeout: 3,
+    cancelMutation,
+};
+
+describe("Cancel Notification", () => {
+    it("should render undo notification message successfuly", async () => {
+        const { getByText } = render(
+            <UndoableNotification {...mockNotification} />,
+        );
+
+        getByText("Test Notification Message");
+    });
+
+    it("should click and render undo button successfuly", async () => {
+        const { container } = render(
+            <UndoableNotification {...mockNotification} />,
+        );
+
+        const button = container.querySelector("button");
+        expect(button).toBeDefined();
+        if (button) {
+            fireEvent.click(button);
+            expect(cancelMutation).toBeCalledTimes(1);
+        }
+    });
+
+    it("should render Progress successfuly", async () => {
+        const { container } = render(
+            <UndoableNotification {...mockNotification} />,
+        );
+
+        expect(
+            container.getElementsByClassName("ant-progress-circle").length,
+        ).toBeDefined();
+    });
+});
