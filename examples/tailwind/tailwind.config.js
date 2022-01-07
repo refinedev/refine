13b893209278edde9ea8756c8181d065cf8303f9
Ömer Faruk APLAK <omer@pankod.com>
2022-01-07 10:27:18 +0300

    tailwind example upgrade to tailwind@3

--- examples/tailwind/tailwind.config.js
+++ examples/tailwind/tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
-    },
-    variants: {
-        extend: {
-            opacity: ["disabled"],
-            cursor: ["disabled"],
-        },
     },
     plugins: [],
 };
