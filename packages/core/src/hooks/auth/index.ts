1dc37c6ce6e01663e051af06bbdeebd0f70d8d35
Salih <salihozdemir94@gmail.com>
2022-01-10 16:15:27 +0300

    create internal useIsExistAuthentication hook

--- packages/core/src/hooks/auth/index.ts
+++ packages/core/src/hooks/auth/index.ts
@@ -5,3 +5,4 @@ export { useLogin } from "./useLogin";
 export { useAuthenticated } from "./useAuthenticated";
 export { useCheckError } from "./useCheckError";
 export { useIsAuthenticated } from "./useIsAuthenticated";
+export { useIsExistAuthentication } from "./useIsExistAuthentication";
