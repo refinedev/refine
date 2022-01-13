74dbe5b2d6902a12611ad5e3c354a47815d3ac9d
Salih <salihozdemir94@gmail.com>
2022-01-13 10:49:29 +0300

    remove tsdx config file

--- packages/core/tsdx.config.js
+++
@@ -1,9 +0,0 @@
-const images = require("@rollup/plugin-image");
-
-// TODO: remove this file
-module.exports = {
-    rollup(config) {
-        config.plugins.unshift(images());
-        return config;
-    },
-};
