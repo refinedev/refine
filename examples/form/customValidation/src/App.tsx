b851dcf85d2ae516abd3f7581bbe0d84c226e166
mlheknci <mmlhekinci@gmail.com>
2022-01-20 10:57:37 +0300

    refine-custom-validation-example ugrade to Refine 3

--- examples/form/customValidation/src/App.tsx
+++ examples/form/customValidation/src/App.tsx
@@ -1,8 +1,13 @@
-import { Refine } from "@pankod/refine";
+import { Refine } from "@pankod/refine-core";
+import {
+    notificationProvider,
+    Layout,
+    ErrorComponent,
+} from "@pankod/refine-antd";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router";
 
-import "@pankod/refine/dist/styles.min.css";
+import "@pankod/refine-antd/dist/styles.min.css";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
 
@@ -22,6 +27,9 @@ const App: React.FC = () => {
                     show: PostShow,
                 },
             ]}
+            notificationProvider={notificationProvider}
+            Layout={Layout}
+            catchAll={<ErrorComponent />}
         />
     );
 };
