3972f223fb7ad26add55256f5ad2f3695ca69902
Ömer Faruk APLAK <omer@pankod.com>
2022-01-21 15:21:27 +0300

    update Title

--- examples/ably/src/App.tsx
+++ examples/ably/src/App.tsx
@@ -1,5 +1,5 @@
 import { Refine } from "@pankod/refine-core";
-import { notificationProvider, Layout } from "@pankod/refine-antd";
+import { notificationProvider, Layout, Title } from "@pankod/refine-antd";
 import dataProvider from "@pankod/refine-simple-rest";
 import { liveProvider } from "@pankod/refine-ably";
 import routerProvider from "@pankod/refine-react-router";
@@ -42,6 +42,7 @@ const App: React.FC = () => {
             ]}
             liveMode="auto"
             Sider={CustomSider}
+            Title={Title}
             notificationProvider={notificationProvider}
             Layout={Layout}
         />
