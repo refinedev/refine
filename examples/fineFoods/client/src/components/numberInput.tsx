94ec03c97f6d505875b0ddb144d5856c4edf290e
Salih <salihozdemir94@gmail.com>
2022-01-21 10:18:28 +0300

    set min value for number input

--- examples/fineFoods/client/src/components/numberInput.tsx
+++ examples/fineFoods/client/src/components/numberInput.tsx
@@ -15,18 +15,36 @@ export const NumberInput: React.FC<NumberInputProps> = ({
                 className="pl-2 py-2 border w-16 rounded-md h-full focus:outline-primary focus:border-primary transition-colors"
                 value={value}
                 onChange={(event) => {
-                    setValue(Number(event.target.value));
+                    const parseNumber = Number(event.target.value);
+
+                    if (parseNumber >= 0) {
+                        setValue(Number(event.target.value));
+                    }
                 }}
             />
             <button className="border border-t-0 border-r-0 absolute right-0.5 top-0 h-1/2 hover:scale-95 transition-all active:bg-gray-50">
                 <ChevronUpIcon
-                    onClick={() => setValue((prev) => prev + 1)}
+                    onClick={() =>
+                        setValue((prev) => {
+                            if (prev >= 0) {
+                                return prev + 1;
+                            }
+                            return 0;
+                        })
+                    }
                     className="text-primary w-4 h-full"
                 />
             </button>
             <button className="border border-b-0 border-r-0 absolute right-0.5 bottom-0 h-1/2 hover:scale-95 transition-all active:bg-gray-50">
                 <ChevronDownIcon
-                    onClick={() => setValue((prev) => prev - 1)}
+                    onClick={() =>
+                        setValue((prev) => {
+                            if (prev > 0) {
+                                return prev - 1;
+                            }
+                            return 0;
+                        })
+                    }
                     className="text-primary w-4 h-full"
                 />
             </button>
