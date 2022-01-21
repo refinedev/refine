a60efd2501b783e28ed6f5c5c9fa1b949e12614b
mlheknci <mmlhekinci@gmail.com>
2022-01-21 11:00:55 +0300

    strapi-multi-tenancy sider title issue fix

--- examples/multi-tenancy/strapi/src/components/sider/index.tsx
+++ examples/multi-tenancy/strapi/src/components/sider/index.tsx
@@ -24,7 +24,7 @@ export const CustomSider: React.FC = () => {
             onCollapse={(collapsed: boolean): void => setCollapsed(collapsed)}
             style={isMobile ? antLayoutSiderMobile : antLayoutSider}
         >
-            <Title collapsed={collapsed} />
+            {Title && <Title collapsed={collapsed} />}
             <Menu
                 selectedKeys={[selectedKey]}
                 mode="inline"
