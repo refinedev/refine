cd16d28baf3d115aaa61c870f70ababa4b5bc9ea
Melih Ekinci <mmlhekinci@gmail.com>
2022-01-28 13:54:19 +0300

    refine overview description update (#1487)
    * refine overview description update
    
    * headless intro description added
    
    * description update

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -14,7 +14,7 @@ import {
 function Home() {
     return (
         <Layout
-            title={`refine | A React-based framework for building data-intensive applications in no time!`}
+            title={`refine | A React-based headless framework for building data-intensive applications in no time!`}
             description="Refine offers lots of out-of-the box functionality for rapid development, without compromising extreme customizability. It ships with Ant Design System, an enterprise-level UI toolkit."
         >
             <main>
