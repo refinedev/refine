43652fcc46ce99bcbee439130149b13648f2fb1f
Aydın Akyol <aydinakyol11@gmail.com>
2022-01-28 12:10:08 +0300

    default table

--- examples/tailwind/src/App.tsx
+++ examples/tailwind/src/App.tsx
@@ -1,112 +1,27 @@
 import { Refine } from "@pankod/refine-core";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router";
-import { ToastContainer } from "react-toastify";
-import { ConfigProvider } from "@pankod/refine-antd";
-
-import {
-    Layout,
-    Header,
-    Sider,
-    Title,
-    ErrorComponent,
-    ReadyPage,
-    LoginPage,
-} from "@pankod/refine-antd";
-
-import "@pankod/refine-antd/dist/styles.min.css";
-import "react-toastify/dist/ReactToastify.css";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
-import { notificationProvider } from "notificationProvider";
 
 const API_URL = "https://api.fake-rest.refine.dev";
-const mockUsers = [
-    {
-        username: "admin",
-        roles: ["admin"],
-    },
-    {
-        username: "editor",
-        roles: ["editor"],
-    },
-];
 
 const App: React.FC = () => {
     return (
-        <ConfigProvider componentSize="large">
-            <Refine
-                routerProvider={routerProvider}
-                dataProvider={dataProvider(API_URL)}
-                authProvider={{
-                    login: ({ username, password, remember }) => {
-                        // Suppose we actually send a request to the back end here.
-                        const user = mockUsers.find(
-                            (item) => item.username === username,
-                        );
-
-                        if (user) {
-                            localStorage.setItem("auth", JSON.stringify(user));
-                            return Promise.resolve();
-                        }
-
-                        return Promise.reject();
-                    },
-                    logout: () => {
-                        localStorage.removeItem("auth");
-                        return Promise.resolve();
-                    },
-                    checkError: (error) => {
-                        if (error && error.statusCode === 401) {
-                            return Promise.reject();
-                        }
-
-                        return Promise.resolve();
-                    },
-                    checkAuth: () =>
-                        localStorage.getItem("auth")
-                            ? Promise.resolve()
-                            : Promise.reject(),
-                    getPermissions: () => {
-                        const auth = localStorage.getItem("auth");
-                        if (auth) {
-                            const parsedUser = JSON.parse(auth);
-                            return Promise.resolve(parsedUser.roles);
-                        }
-                        return Promise.reject();
-                    },
-                    getUserIdentity: () => {
-                        const auth = localStorage.getItem("auth");
-                        if (auth) {
-                            const parsedUser = JSON.parse(auth);
-                            return Promise.resolve(parsedUser.username);
-                        }
-                        return Promise.reject();
-                    },
-                }}
-                notificationProvider={notificationProvider}
-                resources={[
-                    {
-                        name: "posts",
-                        list: PostList,
-                        create: PostCreate,
-                        edit: PostEdit,
-                        show: PostShow,
-                        canDelete: true,
-                    },
-                ]}
-                warnWhenUnsavedChanges={true}
-                mutationMode="undoable"
-                Layout={Layout}
-                Header={Header}
-                Title={Title}
-                Sider={Sider}
-                // catchAll={<ErrorComponent />}
-                // ReadyPage={ReadyPage}
-                // LoginPage={LoginPage}
-            />
-            <ToastContainer />
-        </ConfigProvider>
+        <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider(API_URL)}
+            resources={[
+                {
+                    name: "posts",
+                    list: PostList,
+                    create: PostCreate,
+                    edit: PostEdit,
+                    show: PostShow,
+                    canDelete: true,
+                },
+            ]}
+        />
     );
 };
 
--- examples/tailwind/src/pages/posts/list.tsx
+++ examples/tailwind/src/pages/posts/list.tsx
@@ -8,16 +8,8 @@ import {
     useSortBy,
     usePagination,
 } from "@pankod/refine-react-table";
-import {
-    DeleteButton,
-    EditButton,
-    List,
-    ShowButton,
-    CreateButton,
-} from "@pankod/refine-antd";
 
 import { IPost, ICategory } from "interfaces";
-import { Space } from "@pankod/refine-antd";
 
 export const PostList: React.FC = () => {
     const columns: Array<Column> = React.useMemo(
@@ -57,22 +49,10 @@ export const PostList: React.FC = () => {
                 accessor: "id",
                 //eslint-disable-next-line react/display-name
                 Cell: ({ value }) => (
-                    <div className="flex gap-2">
-                        <EditButton
-                            hideText
-                            size="small"
-                            recordItemId={value}
-                        />
-                        <ShowButton
-                            hideText
-                            size="small"
-                            recordItemId={value}
-                        />
-                        <DeleteButton
-                            hideText
-                            size="small"
-                            recordItemId={value}
-                        />
+                    <div>
+                        <button type="button">edit</button>
+                        <button type="button">show</button>
+                        <button type="button">delete</button>
                     </div>
                 ),
             },
@@ -123,197 +103,113 @@ export const PostList: React.FC = () => {
     });
 
     return (
-        <List
-            pageHeaderProps={{
-                extra: (
-                    <Space>
-                        <input
-                            type="file"
-                            onChange={(event) => {
-                                console.log({ event: event.target.files });
-                                if (event.target.files) {
-                                    handleChange({
-                                        file: event.target.files[0],
-                                    });
-                                }
-                            }}
-                        />
-                        <CreateButton />
-                    </Space>
-                ),
-            }}
-        >
-            <div className="flex">
-                <div className="w-1/4 pr-8">
-                    <label
-                        htmlFor="title"
-                        className="inline-block mb-2 text-gray-700 font-bold"
-                    >
-                        Title
-                    </label>
-                    <input
-                        type="text"
-                        value={
-                            filters.find((filter) => filter.id === "title")
-                                ?.value
-                        }
-                        onChange={(event) =>
-                            setFilter("title", event.target.value)
-                        }
-                        className="w-full px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none"
-                    />
-                    <label
-                        htmlFor="Category"
-                        className="inline-block my-2 text-gray-700 font-bold"
-                    >
-                        Category
-                    </label>
-                    <select
-                        id="category"
-                        className="appearance-none w-full px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none"
-                        aria-label="Category select"
-                        onChange={(event) => {
-                            setFilter("category.id", event.target.value);
-                        }}
-                        value={
-                            filters.find(
-                                (filter) => filter.id === "category.id",
-                            )?.value
-                        }
-                    >
-                        <option value={[]}>All Categories</option>
-                        {options?.map((category) => (
-                            <option
-                                key={category.value}
-                                value={category.value || undefined}
-                            >
-                                {category.label}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div className="w-3/4 overflow-x-auto">
-                    <table {...getTableProps()}>
-                        <thead className="bg-white border-b ">
-                            {headerGroups.map((headerGroup) => (
-                                <tr {...headerGroup.getHeaderGroupProps()}>
-                                    {headerGroup.headers.map((column) => (
-                                        <th
-                                            {...column.getHeaderProps(
-                                                column.getSortByToggleProps(),
-                                            )}
-                                            className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                                        >
-                                            {column.render("Header")}
-                                            <span>
-                                                {column.isSorted
-                                                    ? column.isSortedDesc
-                                                        ? " 🔽"
-                                                        : " 🔼"
-                                                    : ""}
-                                            </span>
-                                        </th>
-                                    ))}
-                                </tr>
+        <div>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th
+                                    {...column.getHeaderProps(
+                                        column.getSortByToggleProps(),
+                                    )}
+                                >
+                                    {column.render("Header")}
+                                    <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? " 🔽"
+                                                : " 🔼"
+                                            : ""}
+                                    </span>
+                                </th>
                             ))}
-                        </thead>
-                        <tbody {...getTableBodyProps()}>
-                            {page.map((row) => {
-                                prepareRow(row);
-                                return (
-                                    <tr
-                                        {...row.getRowProps()}
-                                        className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
-                                    >
-                                        {row.cells.map((cell) => {
-                                            return (
-                                                <td
-                                                    {...cell.getCellProps()}
-                                                    className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                                >
-                                                    {cell.render("Cell")}
-                                                </td>
-                                            );
-                                        })}
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render("Cell")}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
 
-                    <div className="mt-4">
-                        <button
-                            onClick={() => {
-                                gotoPage(0);
-                            }}
-                            disabled={!canPreviousPage}
-                            className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out disabled:opacity-60 disabled:cursor-default"
-                        >
-                            {"<<"}
-                        </button>{" "}
-                        <button
-                            onClick={() => {
-                                previousPage();
-                            }}
-                            disabled={!canPreviousPage}
-                            className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out disabled:opacity-60 disabled:cursor-default"
-                        >
-                            {"<"}
-                        </button>{" "}
-                        <button
-                            onClick={() => {
-                                nextPage();
-                            }}
-                            disabled={!canNextPage}
-                            className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out disabled:opacity-60 disabled:cursor-default"
-                        >
-                            {">"}
-                        </button>{" "}
-                        <button
-                            onClick={() => {
-                                gotoPage(pageCount - 1);
-                            }}
-                            disabled={!canNextPage}
-                            className="px-2.5 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-0 active:bg-gray-300 transition duration-150 ease-in-out disabled:opacity-60 disabled:cursor-default"
-                        >
-                            {">>"}
-                        </button>{" "}
-                        <span>
-                            Page{" "}
-                            <strong>
-                                {pageIndex + 1} of {pageOptions.length}
-                            </strong>{" "}
-                        </span>
-                        <span>
-                            | Go to page:{" "}
-                            <input
-                                type="number"
-                                defaultValue={pageIndex + 1}
-                                onChange={(e) => {
-                                    const page = e.target.value
-                                        ? Number(e.target.value) - 1
-                                        : 0;
-                                    gotoPage(page);
-                                }}
-                                className="w-16 px-2 py-1 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                            />
-                        </span>{" "}
-                        <select
-                            value={pageSize}
-                            onChange={(e) => {
-                                setPageSize(Number(e.target.value));
-                            }}
-                            className="px-2 py-1 text-sm font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        >
-                            {[10, 20, 30, 40, 50].map((pageSize) => (
-                                <option key={pageSize} value={pageSize}>
-                                    Show {pageSize}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                </div>
+            <div>
+                <button
+                    onClick={() => {
+                        gotoPage(0);
+                    }}
+                    disabled={!canPreviousPage}
+                >
+                    {"<<"}
+                </button>{" "}
+                <button
+                    onClick={() => {
+                        previousPage();
+                    }}
+                    disabled={!canPreviousPage}
+                >
+                    {"<"}
+                </button>{" "}
+                <button
+                    onClick={() => {
+                        nextPage();
+                    }}
+                    disabled={!canNextPage}
+                >
+                    {">"}
+                </button>{" "}
+                <button
+                    onClick={() => {
+                        gotoPage(pageCount - 1);
+                    }}
+                    disabled={!canNextPage}
+                >
+                    {">>"}
+                </button>{" "}
+                <span>
+                    Page{" "}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{" "}
+                </span>
+                <span>
+                    | Go to page:{" "}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={(e) => {
+                            const page = e.target.value
+                                ? Number(e.target.value) - 1
+                                : 0;
+                            gotoPage(page);
+                        }}
+                    />
+                </span>{" "}
+                <select
+                    value={pageSize}
+                    onChange={(e) => {
+                        setPageSize(Number(e.target.value));
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
             </div>
-        </List>
+        </div>
     );
 };
