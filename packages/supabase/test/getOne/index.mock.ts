a472a7b76f9e6ad771ce1cd2353ebcbcdc4855c7
Ömer Faruk APLAK <omer@pankod.com>
2022-01-28 16:03:52 +0300

    add supabase data provider metadata select unit tests

--- packages/supabase/test/getOne/index.mock.ts
+++ packages/supabase/test/getOne/index.mock.ts
@@ -47,3 +47,53 @@ nock("https://iwdfzvfqbtokqetmbmbp.supabase.co:443", {
             "kong/2.2.1",
         ],
     );
+
+nock("https://iwdfzvfqbtokqetmbmbp.supabase.co:443", {
+    encodedQueryParams: true,
+})
+    .get("/rest/v1/posts")
+    .query({ select: "title", id: "eq.3" })
+    .reply(
+        200,
+        [
+            "1f8b08000000000000038bae562ac92cc94955b252f2c92f4acd55f02c282ecd5530d23553aa8d050032f8af081d000000",
+        ],
+        [
+            "Date",
+            "Fri, 28 Jan 2022 13:00:12 GMT",
+            "Content-Type",
+            "application/json; charset=utf-8",
+            "Transfer-Encoding",
+            "chunked",
+            "Connection",
+            "close",
+            "CF-Ray",
+            "6d4a67027f4492d1-IST",
+            "Access-Control-Allow-Origin",
+            "*",
+            "Content-Encoding",
+            "gzip",
+            "Content-Location",
+            "/posts?id=eq.3&select=title",
+            "Content-Range",
+            "0-0/*",
+            "Vary",
+            "Accept-Encoding, Origin",
+            "Via",
+            "kong/2.2.1",
+            "CF-Cache-Status",
+            "DYNAMIC",
+            "Content-Profile",
+            "public",
+            "Expect-CT",
+            'max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"',
+            "X-Kong-Proxy-Latency",
+            "0",
+            "X-Kong-Upstream-Latency",
+            "3",
+            "Server",
+            "cloudflare",
+            "alt-svc",
+            'h3=":443"; ma=86400, h3-29=":443"; ma=86400',
+        ],
+    );
