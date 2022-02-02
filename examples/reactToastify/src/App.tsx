f573644c11cad5d4312062a11cea0e1356223fff
Salih Özdemir <salihozdemir94@gmail.com>
2022-02-02 17:59:58 +0300

    Update Notification Provider Doc (#1509)
    * add open and close method sections
    
    * create base headless example
    
    * create react toastify example
    
    * add toastify css and toastify container
    
    * add codesandbox embed code

---
+++ examples/reactToastify/src/App.tsx
@@ -0,0 +1,36 @@
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-react-router";
+import dataProvider from "@pankod/refine-simple-rest";
+import { ToastContainer } from "react-toastify";
+
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import { notificationProvider } from "providers/notificationProvider";
+import { PostList, PostCreate, PostEdit } from "pages/posts";
+
+const App: React.FC = () => {
+    return (
+        <Refine
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            routerProvider={routerProvider}
+            notificationProvider={notificationProvider}
+            resources={[
+                {
+                    name: "posts",
+                    list: PostList,
+                    create: PostCreate,
+                    edit: PostEdit,
+                },
+            ]}
+            Layout={({ children }) => (
+                <div>
+                    {children}
+                    <ToastContainer />
+                </div>
+            )}
+        />
+    );
+};
+
+export default App;
