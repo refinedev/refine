076d9ce5d2b9854386a0f2eb3b2da1497366a849
Salih <salihozdemir94@gmail.com>
2022-02-03 17:20:53 +0300

    design list page

--- examples/tutorial/headless/src/components/layout.tsx
+++ examples/tutorial/headless/src/components/layout.tsx
@@ -5,37 +5,39 @@ export const Layout: React.FC = ({ children }) => {
     const { list } = useNavigation();
 
     return (
-        <div className="flex min-h-screen gap-2 bg-slate-50">
-            <div className="w-40 border-r bg-slate-700 py-2 text-slate-200 shadow-md">
-                <ul>
-                    {resources.map(({ name }) => (
-                        <li key={name}>
-                            <a
-                                className="flex cursor-pointer items-center gap-1 rounded-sm px-2 py-1 capitalize transition duration-300 ease-in-out hover:bg-slate-600 hover:text-slate-100"
-                                onClick={() => list(name)}
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
+        <div className="flex min-h-screen flex-col">
+            <div className="mb-2 bg-gray-100/75 py-2 shadow">
+                <div className="container mx-auto">
+                    <ul>
+                        {resources.map(({ name }) => (
+                            <li key={name} className="float-left">
+                                <a
+                                    className="flex cursor-pointer items-center gap-1 rounded-sm px-2 py-1 capitalize decoration-indigo-500 decoration-2 underline-offset-1 transition duration-300 ease-in-out hover:underline"
+                                    onClick={() => list(name)}
                                 >
-                                    <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-                                    <polyline points="2 17 12 22 22 17"></polyline>
-                                    <polyline points="2 12 12 17 22 12"></polyline>
-                                </svg>
-                                <span>{name}</span>
-                            </a>
-                        </li>
-                    ))}
-                </ul>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    >
+                                        <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                                        <polyline points="2 17 12 22 22 17"></polyline>
+                                        <polyline points="2 12 12 17 22 12"></polyline>
+                                    </svg>
+                                    <span>{name}</span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-            <div className="w-full">{children}</div>
+            <div className="bg-white">{children}</div>
         </div>
     );
 };
--- examples/tutorial/headless/src/pages/posts/list.tsx
+++ examples/tutorial/headless/src/pages/posts/list.tsx
@@ -12,7 +12,7 @@ import { useNavigation } from "@pankod/refine-core";
 import { IPost } from "interfaces";
 
 export const PostList: React.FC = () => {
-    const { edit, create } = useNavigation();
+    const { edit, create, show } = useNavigation();
 
     const columns: Array<Column> = React.useMemo(
         () => [
@@ -43,7 +43,46 @@ export const PostList: React.FC = () => {
                 accessor: "id",
                 // eslint-disable-next-line react/display-name
                 Cell: ({ value }) => (
-                    <button onClick={() => edit("posts", value)}>Edit</button>
+                    <div className="flex gap-2">
+                        <button
+                            className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+                            onClick={() => edit("posts", value)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <path d="M12 20h9"></path>
+                                <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+                            </svg>
+                        </button>
+                        <button
+                            className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+                            onClick={() => show("posts", value)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                <circle cx="12" cy="12" r="3"></circle>
+                            </svg>
+                        </button>
+                    </div>
                 ),
             },
         ],
@@ -69,22 +108,50 @@ export const PostList: React.FC = () => {
     } = useTable<IPost>({ columns }, useFilters, useSortBy, usePagination);
 
     return (
-        <>
-            <div>
-                <button onClick={() => create("posts")}>Create Post</button>
-                <br />
-                <br />
-                <label htmlFor="title">Title: </label>
-                <input
-                    id="title"
-                    type="text"
-                    value={
-                        filters.find((filter) => filter.id === "title")?.value
-                    }
-                    onChange={(event) => setFilter("title", event.target.value)}
-                />
+        <div className="container mx-auto pb-4">
+            <div className="mb-3 mt-1 flex items-center justify-between">
+                <h1 className="text-2xl font-bold">Posts List</h1>
+                <div>
+                    <label className="mr-1" htmlFor="title">
+                        Title:
+                    </label>
+                    <input
+                        id="title"
+                        type="text"
+                        className="rounded border border-gray-200 p-1 text-gray-700"
+                        placeholder="Filter by title"
+                        value={
+                            filters.find((filter) => filter.id === "title")
+                                ?.value
+                        }
+                        onChange={(event) =>
+                            setFilter("title", event.target.value)
+                        }
+                    />
+                </div>
+                <button
+                    className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+                    onClick={() => create("posts")}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                    <span>Create Post</span>
+                </button>
             </div>
-            <table {...getTableProps()}>
+
+            <table className="w-full border" {...getTableProps()}>
                 <thead>
                     {headerGroups.map((headerGroup) => (
                         <tr {...headerGroup.getHeaderGroupProps()}>
@@ -93,6 +160,7 @@ export const PostList: React.FC = () => {
                                     {...column.getHeaderProps(
                                         column.getSortByToggleProps(),
                                     )}
+                                    className="border-b bg-gray-100/75 p-4 pl-2 text-left font-semibold"
                                 >
                                     {column.render("Header")}
                                     <span>
@@ -111,10 +179,16 @@ export const PostList: React.FC = () => {
                     {page.map((row) => {
                         prepareRow(row);
                         return (
-                            <tr {...row.getRowProps()}>
+                            <tr
+                                {...row.getRowProps()}
+                                className="transition hover:bg-gray-50/50"
+                            >
                                 {row.cells.map((cell) => {
                                     return (
-                                        <td {...cell.getCellProps()}>
+                                        <td
+                                            {...cell.getCellProps()}
+                                            className="border-b border-slate-100 p-3 pl-2"
+                                        >
                                             {cell.render("Cell")}
                                         </td>
                                     );
@@ -125,33 +199,95 @@ export const PostList: React.FC = () => {
                 </tbody>
             </table>
 
-            <div className="pagination">
-                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                    {"<<"}
-                </button>
-                <button
-                    onClick={() => previousPage()}
-                    disabled={!canPreviousPage}
-                >
-                    {"<"}
-                </button>
-                <button onClick={() => nextPage()} disabled={!canNextPage}>
-                    {">"}
-                </button>
-                <button
-                    onClick={() => gotoPage(pageCount - 1)}
-                    disabled={!canNextPage}
-                >
-                    {">>"}
-                </button>
+            <div className="mt-2 flex items-center justify-end gap-4">
+                <div className="flex gap-1">
+                    <button
+                        onClick={() => gotoPage(0)}
+                        disabled={!canPreviousPage}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <polyline points="11 17 6 12 11 7"></polyline>
+                            <polyline points="18 17 13 12 18 7"></polyline>
+                        </svg>
+                    </button>
+                    <button
+                        onClick={() => previousPage()}
+                        disabled={!canPreviousPage}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <polyline points="15 18 9 12 15 6"></polyline>
+                        </svg>
+                    </button>
+                    <button
+                        onClick={() => nextPage()}
+                        disabled={!canNextPage}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                    </button>
+                    <button
+                        onClick={() => gotoPage(pageCount - 1)}
+                        disabled={!canNextPage}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <polyline points="13 17 18 12 13 7"></polyline>
+                            <polyline points="6 17 11 12 6 7"></polyline>
+                        </svg>
+                    </button>
+                </div>
                 <span>
-                    Page
+                    Page{" "}
                     <strong>
                         {pageIndex + 1} of {pageOptions.length}
                     </strong>
                 </span>
                 <span>
-                    | Go to page:
+                    Go to page:
                     <input
                         type="number"
                         defaultValue={pageIndex + 1}
@@ -161,14 +297,15 @@ export const PostList: React.FC = () => {
                                 : 0;
                             gotoPage(page);
                         }}
-                        style={{ width: "100px" }}
+                        className="w-12 rounded border border-gray-200 p-1 text-gray-700"
                     />
-                </span>{" "}
+                </span>
                 <select
                     value={pageSize}
                     onChange={(e) => {
                         setPageSize(Number(e.target.value));
                     }}
+                    className="rounded border border-gray-200 p-1 text-gray-700"
                 >
                     {[10, 20, 30, 40, 50].map((pageSize) => (
                         <option key={pageSize} value={pageSize}>
@@ -177,6 +314,6 @@ export const PostList: React.FC = () => {
                     ))}
                 </select>
             </div>
-        </>
+        </div>
     );
 };
