83eefc3f06f7ed2f1c70fad67e9b4f4d74a32278
Salih <salihozdemir94@gmail.com>
2022-02-04 12:24:33 +0300

    add refine logo

--- examples/tutorial/headless/src/components/layout.tsx
+++ examples/tutorial/headless/src/components/layout.tsx
@@ -1,4 +1,7 @@
 import { useResource, useNavigation } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-react-router";
+
+const { Link } = routerProvider;
 
 export const Layout: React.FC = ({ children }) => {
     const { resources } = useResource();
@@ -7,24 +10,29 @@ export const Layout: React.FC = ({ children }) => {
     return (
         <div className="flex min-h-screen flex-col">
             <div className="mb-2 border-b py-2">
-                <div className="container mx-auto flex">
-                    <img
-                        src="https://refine.dev/img/refine_logo.png"
-                        alt="Logo"
-                    />
-                    <ul>
-                        {resources.map(({ name, icon }) => (
-                            <li key={name} className="float-left">
-                                <a
-                                    className="flex cursor-pointer items-center gap-1 rounded-sm px-2 py-1 capitalize decoration-indigo-500 decoration-2 underline-offset-1 transition duration-300 ease-in-out hover:underline"
-                                    onClick={() => list(name)}
-                                >
-                                    {icon}
-                                    <span>{name}</span>
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
+                <div className="container mx-auto">
+                    <div className="flex items-center gap-2">
+                        <Link to="/">
+                            <img
+                                className="w-32"
+                                src="https://refine.dev/img/refine_logo.png"
+                                alt="Logo"
+                            />
+                        </Link>
+                        <ul>
+                            {resources.map(({ name, icon }) => (
+                                <li key={name} className="float-left">
+                                    <a
+                                        className="flex cursor-pointer items-center gap-1 rounded-sm px-2 py-1 capitalize decoration-indigo-500 decoration-2 underline-offset-1 transition duration-300 ease-in-out hover:underline"
+                                        onClick={() => list(name)}
+                                    >
+                                        {icon}
+                                        <span>{name}</span>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className="bg-white">{children}</div>
