350e3d7ee385ff55b3c3838be43764263c415ccf
ozkalai <melihozkalayy@gmail.com>
2022-02-10 15:50:35 +0300

    useLogout hook useMutation onSuccess refactor

--- packages/core/src/hooks/auth/useLogout/index.ts
+++ packages/core/src/hooks/auth/useLogout/index.ts
@@ -2,11 +2,7 @@ import React from "react";
 import { useMutation, UseMutationResult } from "react-query";
 
 import { AuthContext } from "@contexts/auth";
-import {
-    IAuthContext,
-    TLogoutVariables,
-    TLogoutData,
-} from "../../../interfaces";
+import { IAuthContext, TLogoutData } from "../../../interfaces";
 import { useNavigation, useNotification } from "@hooks";
 
 /**
@@ -15,10 +11,10 @@ import { useNavigation, useNotification } from "@hooks";
  * @see {@link https://refine.dev/docs/core/hooks/auth/useLogout} for more details.
  *
  */
-export const useLogout = (): UseMutationResult<
+export const useLogout = <TVariables = {}>(): UseMutationResult<
     TLogoutData,
     Error,
-    TLogoutVariables,
+    TVariables,
     unknown
 > => {
     const { push } = useNavigation();
@@ -26,33 +22,30 @@ export const useLogout = (): UseMutationResult<
         React.useContext<IAuthContext>(AuthContext);
     const { open } = useNotification();
 
-    const queryResponse = useMutation<
-        TLogoutData,
-        Error,
-        TLogoutVariables,
-        unknown
-    >("useLogout", logoutFromContext, {
-        onSuccess: (redirectPathFromAuth, { redirectPath } = {}) => {
-            if (redirectPathFromAuth !== false) {
-                if (redirectPath) {
-                    push(redirectPath);
-                } else if (redirectPathFromAuth) {
-                    push(redirectPathFromAuth);
-                } else {
-                    push("/login");
+    const queryResponse = useMutation<TLogoutData, Error, TVariables, unknown>(
+        "useLogout",
+        logoutFromContext,
+        {
+            onSuccess: (redirectPathFromAuth) => {
+                if (redirectPathFromAuth !== false) {
+                    if (redirectPathFromAuth) {
+                        push(redirectPathFromAuth);
+                    } else {
+                        push("/login");
+                    }
                 }
-            }
+            },
+            onError: (error: Error) => {
+                open({
+                    key: "useLogout-error",
+                    type: "error",
+                    message: error?.name || "Logout Error",
+                    description:
+                        error?.message || "Something went wrong during logout",
+                });
+            },
         },
-        onError: (error: Error) => {
-            open({
-                key: "useLogout-error",
-                type: "error",
-                message: error?.name || "Logout Error",
-                description:
-                    error?.message || "Something went wrong during logout",
-            });
-        },
-    });
+    );
 
     return queryResponse;
 };
