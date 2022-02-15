6c55e4cb13c409cdbe30a20c0b9bd523a1c4ceab
Melih Ekinci <mmlhekinci@gmail.com>
2022-02-15 17:10:19 +0300

    refine-ecommerce-blog (#1549)
    * refine-ecommerce-blog
    
    * Optimised images with calibre/image-actions
    
    * strapi image data updated
    
    * fix conflicts
    
    * Optimised images with calibre/image-actions
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * source code added to blog examples
    
    * update conclusion
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Update documentation/blog/2022-02-14-refine-ecommerce-blog.md
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * unnecessary codes deleted
    
    * unnecessary codes deleted from example
    
    * devtool config postion changed
    
    Co-authored-by: Pankod Community Bot <53254888+pankod-bot@users.noreply.github.com>
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

---
+++ examples/blog/ecommerce/pages/_app.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-nextjs-router";
+import { ChakraProvider } from "@chakra-ui/react";
+import { DataProvider } from "@pankod/refine-strapi-v4";
+
+import { API_URL } from "src/constants";
+import { Layout } from "src/components";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    const dataProvider = DataProvider(API_URL + "/api");
+
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://app.snipcart.com" />
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.snipcart.com/themes/v3.0.16/default/snipcart.css"
+                />
+                <script
+                    async
+                    src="https://cdn.snipcart.com/themes/v3.0.16/default/snipcart.js"
+                />
+            </Head>
+            <Refine
+                routerProvider={routerProvider}
+                dataProvider={dataProvider}
+                resources={[{ name: "products" }]}
+                Layout={Layout}
+                reactQueryDevtoolConfig={{
+                    position: "bottom-left",
+                }}
+            >
+                <ChakraProvider>
+                    <Component {...pageProps} />
+                </ChakraProvider>
+            </Refine>
+            <div
+                hidden
+                id="snipcart"
+                data-api-key="YWEzNDc3NGQtOGI3ZS00MzI2LTliYmUtM2M4NDg0ZTg2OTE0NjM3ODAwODQ0NzIzNDc0NTIw"
+            />
+        </>
+    );
+}
+
+export default MyApp;
---
+++ examples/blog/ecommerce/src/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+    id: string;
+    title: string;
+    description: string;
+    image: any;
+}
+
+export interface IStore {
+    id: string;
+    title: string;
+}
