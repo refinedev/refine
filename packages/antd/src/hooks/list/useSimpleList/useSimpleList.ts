11965d628a8a09f656330a45c043e38247688df6
ozkalai <melihozkalayy@gmail.com>
2022-02-15 11:43:05 +0300

    removed unused and fixed onFinish

--- packages/antd/src/hooks/list/useSimpleList/useSimpleList.ts
+++ packages/antd/src/hooks/list/useSimpleList/useSimpleList.ts
@@ -1,16 +1,10 @@
-import { useEffect, useState } from "react";
 import { QueryObserverResult, UseQueryOptions } from "react-query";
 import { ListProps } from "antd/lib/list";
 import { FormProps } from "antd/lib/form";
 import { useForm } from "antd/lib/form/Form";
 
 import {
-    useResourceWithRoute,
-    useList,
     useSyncWithLocation,
-    useNavigation,
-    useRouterContext,
-    ResourceRouterParams,
     BaseRecord,
     CrudFilters,
     CrudSorting,
@@ -19,13 +13,10 @@ import {
     HttpError,
     MetaDataQuery,
     LiveModeProps,
-    parseTableParams,
-    stringifyTableParams,
-    unionFilters,
     useTable as useTableCore,
 } from "@pankod/refine-core";
 
-export type useSimpleListProps<TData, TError, TSearchVariables = unknown> =
+export type useSimpleListProps<TData, TError, TSearchVariables> =
     ListProps<TData> & {
         permanentFilter?: CrudFilters;
         syncWithLocation?: boolean;
@@ -42,11 +33,10 @@ export type useSimpleListProps<TData, TError, TSearchVariables = unknown> =
 
 export type useSimpleListReturnType<
     TData extends BaseRecord = BaseRecord,
-    TError extends HttpError = HttpError,
     TSearchVariables = unknown,
 > = {
     listProps: ListProps<TData>;
-    queryResult: QueryObserverResult<GetListResponse<TData>, TError>;
+    queryResult: QueryObserverResult<GetListResponse<TData>>;
     searchFormProps: FormProps<TSearchVariables>;
     filters: CrudFilters;
 };
@@ -85,17 +75,17 @@ export const useSimpleList = <
     TData,
     TError,
     TSearchVariables
-> = {}): useSimpleListReturnType<TData, TError, TSearchVariables> => {
-    const { useLocation, useParams } = useRouterContext();
-
+> = {}): useSimpleListReturnType<TData, TSearchVariables> => {
+    const { syncWithLocation: syncWithLocationContext } = useSyncWithLocation();
+    const syncWithLocation = syncWithLocationProp ?? syncWithLocationContext;
     const {
         filters,
-        sorter,
         current,
         pageSize,
         setFilters,
         setCurrent,
         setPageSize,
+        tableQueryResult: queryResult,
     } = useTableCore({
         resource: resourceFromProp,
         initialSorter,
@@ -108,84 +98,12 @@ export const useSimpleList = <
         onLiveEvent,
         liveParams,
         metaData,
+        syncWithLocation,
         ...listProps,
     });
 
-    const { resource: routeResourceName } = useParams<ResourceRouterParams>();
-
-    const { push } = useNavigation();
-
-    const { search } = useLocation();
-
-    const { syncWithLocation: syncWithLocationContext } = useSyncWithLocation();
-    let syncWithLocation = syncWithLocationProp ?? syncWithLocationContext;
-
     const [form] = useForm<TSearchVariables>();
 
-    const resourceWithRoute = useResourceWithRoute();
-    const resource = resourceWithRoute(resourceFromProp ?? routeResourceName);
-
-    // disable syncWithLocation for custom resource tables
-    if (resourceFromProp) {
-        syncWithLocation = false;
-    }
-
-    let defaultPageSize = 10;
-    let defaultCurrent = 1;
-    let defaultSorter = initialSorter;
-    let defaultFilter = initialFilter;
-
-    if (listProps.pagination && listProps.pagination.pageSize) {
-        defaultPageSize = listProps.pagination.pageSize;
-    }
-
-    if (listProps.pagination && listProps.pagination.current) {
-        defaultCurrent = listProps.pagination.current;
-    }
-
-    if (syncWithLocation) {
-        const { parsedCurrent, parsedPageSize, parsedSorter, parsedFilters } =
-            parseTableParams(search);
-
-        defaultCurrent = parsedCurrent || defaultCurrent;
-        defaultPageSize = parsedPageSize || defaultPageSize;
-        defaultSorter = parsedSorter.length ? parsedSorter : defaultSorter;
-        defaultFilter = parsedFilters.length ? parsedFilters : defaultFilter;
-    }
-
-    useEffect(() => {
-        if (syncWithLocation) {
-            const stringifyParams = stringifyTableParams({
-                pagination: {
-                    current,
-                    pageSize,
-                },
-                sorter,
-                filters,
-            });
-
-            return push(`/${resource.route}?${stringifyParams}`);
-        }
-    }, [syncWithLocation, current, pageSize, sorter, filters]);
-
-    const queryResult = useList<TData, TError>({
-        resource: resource.name,
-        config: {
-            pagination: {
-                current,
-                pageSize,
-            },
-            filters: unionFilters(permanentFilter, [], filters),
-            sort: sorter,
-        },
-        queryOptions,
-        successNotification,
-        errorNotification,
-        liveMode,
-        onLiveEvent,
-        liveParams,
-        metaData,
-    });
     const { data, isFetching } = queryResult;
 
     const onChange = (page: number, pageSize?: number): void => {
@@ -197,7 +115,7 @@ export const useSimpleList = <
         if (onSearch) {
             const searchFilters = await onSearch(values);
             setCurrent(1);
-            return setFilters(filters);
+            return setFilters(searchFilters);
         }
     };
 
