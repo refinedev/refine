345e3b29e3a7cf1fb07c2d56fbabc25d435ac9ec
ozkalai <melihozkalayy@gmail.com>
2022-02-16 19:18:01 +0300

    Prompt

--- packages/react-router-v6/src/prompt.tsx
+++ packages/react-router-v6/src/prompt.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useContext } from "react";
+import { useLocation } from "react-location";
 import { UNSAFE_NavigationContext } from "react-router-dom";
-import { PromptProps } from "@pankod/refine-core";
+import type { History } from "history";
+
+import type { PromptProps } from "@pankod/refine-core";
 
 export const Prompt: React.FC<PromptProps> = ({
     message,
-    setWarnWhen,
     when,
+    setWarnWhen,
 }) => {
+    const location = useLocation();
+    const navigator = useContext(UNSAFE_NavigationContext).navigator as History;
+
+    useEffect(() => {
+        if (!when) return;
+
+        const unblock = location.history.block((transition: any) => {
+            if (window.confirm(message)) {
+                setWarnWhen?.(false);
+                unblock();
+                transition.retry();
+            } else {
+                location.current.pathname = window.location.pathname;
+            }
+        });
+        return unblock;
+    }, [navigator, setWarnWhen, when]);
+
     return null;
 };
