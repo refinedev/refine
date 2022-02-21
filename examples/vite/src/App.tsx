8dc3ec2abc2f782ee63dcd0646d167f277a10dc8
Salih <salihozdemir94@gmail.com>
2022-02-21 18:16:13 +0300

    improve vite example

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -8,17 +8,24 @@ import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router";
 import "@pankod/refine-antd/dist/styles.min.css";
 
+import { PostList, PostCreate, PostEdit, PostShow } from "./pages/posts";
+
 const App: React.FC = () => {
     return (
         <Refine
             routerProvider={routerProvider}
-            dataProvider={dataProvider("API_URL")}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
             notificationProvider={notificationProvider}
             Layout={Layout}
             catchAll={<ErrorComponent />}
             resources={[
                 {
                     name: "posts",
+                    list: PostList,
+                    create: PostCreate,
+                    edit: PostEdit,
+                    show: PostShow,
+                    canDelete: true,
                 },
             ]}
         />
