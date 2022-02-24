67dcff8865ba15cdaaa642b7caf4d933f023db73
Salih <salihozdemir94@gmail.com>
2022-02-24 16:23:55 +0300

    create authProvider

--- examples/dataProvider/nhost/src/App.tsx
+++ examples/dataProvider/nhost/src/App.tsx
@@ -1,12 +1,14 @@
-import { Refine } from "@pankod/refine-core";
+import { AuthProvider, Refine } from "@pankod/refine-core";
 import {
     notificationProvider,
     Layout,
+    LoginPage,
     ErrorComponent,
 } from "@pankod/refine-antd";
 import routerProvider from "@pankod/refine-react-router";
 import dataProvider from "@pankod/refine-nhost";
 import { NhostClient } from "@nhost/nhost-js";
+import { NhostAuthProvider } from "@nhost/react-auth";
 import { NhostApolloProvider } from "@nhost/react-apollo";
 
 import "@pankod/refine-antd/dist/styles.min.css";
@@ -22,32 +24,93 @@ const nhost = new NhostClient({
     backendUrl: "https://oxhhlmqsjahbyedrmvll.nhost.run",
 });
 
+const authProvider: AuthProvider = {
+    login: async ({ username, password }) => {
+        const { error } = await nhost.auth.signIn({
+            email: username,
+            password,
+        });
+
+        if (error) {
+            return Promise.reject(error);
+        }
+
+        return Promise.resolve();
+    },
+    logout: async () => {
+        const { error } = await nhost.auth.signOut();
+        if (error) {
+            return Promise.reject(error);
+        }
+
+        return Promise.resolve("/");
+    },
+    checkError: (error) => {
+        if (error.status === 401) {
+            return nhost.auth.refreshSession();
+        }
+        return Promise.reject();
+    },
+    checkAuth: async () => {
+        const isAuthenticated = await nhost.auth.isAuthenticatedAsync();
+        if (isAuthenticated) {
+            return Promise.resolve();
+        }
+
+        return Promise.reject();
+    },
+    getPermissions: () => {
+        const user = nhost.auth.getUser();
+        if (user) {
+            return Promise.resolve(user.roles);
+        }
+
+        return Promise.resolve([]);
+    },
+    getUserIdentity: () => {
+        const user = nhost.auth.getUser();
+        if (user) {
+            return Promise.resolve({
+                ...user,
+                name: user.displayName,
+                avatar: user.avatarUrl,
+            });
+        }
+
+        return Promise.resolve(null);
+    },
+};
+
 const App: React.FC = () => {
     return (
-        <NhostApolloProvider nhost={nhost}>
-            <Refine
-                routerProvider={routerProvider}
-                dataProvider={dataProvider(nhost)}
-                resources={[
-                    {
-                        name: "posts",
-                        list: PostList,
-                        create: PostCreate,
-                        edit: PostEdit,
-                        show: PostShow,
-                    },
-                    {
-                        name: "categories",
-                        list: CategoriesList,
-                        create: CategoriesCreate,
-                        edit: CategoriesEdit,
-                    },
-                ]}
-                notificationProvider={notificationProvider}
-                Layout={Layout}
-                catchAll={<ErrorComponent />}
-            />
-        </NhostApolloProvider>
+        <NhostAuthProvider nhost={nhost}>
+            <NhostApolloProvider nhost={nhost}>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(nhost)}
+                    authProvider={authProvider}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: PostList,
+                            create: PostCreate,
+                            edit: PostEdit,
+                            show: PostShow,
+                        },
+                        {
+                            name: "categories",
+                            list: CategoriesList,
+                            create: CategoriesCreate,
+                            edit: CategoriesEdit,
+                        },
+                    ]}
+                    notificationProvider={notificationProvider}
+                    Layout={Layout}
+                    LoginPage={LoginPage}
+                    catchAll={<ErrorComponent />}
+                />
+            </NhostApolloProvider>
+        </NhostAuthProvider>
     );
 };
 
