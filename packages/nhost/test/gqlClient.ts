0dd2ccfea44fd444459d637697977733fd641c0a
Salih <salihozdemir94@gmail.com>
2022-02-25 11:32:46 +0300

    create nhost client for test

--- packages/nhost/test/gqlClient.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-const API_URL = "https://flowing-mammal-24.hasura.app/v1/graphql";
-
-const client = new GraphQLClient(API_URL);
-
-client.setHeader("x-hasura-role", "public");
-
-export default client;
