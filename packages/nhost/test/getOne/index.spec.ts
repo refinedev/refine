01635f1d3afe59e7cb9d62e1ae205cc344512b26
Salih <salihozdemir94@gmail.com>
2022-02-25 11:33:11 +0300

    add getOne test

--- packages/nhost/test/getOne/index.spec.ts
+++ packages/nhost/test/getOne/index.spec.ts
@@ -1,24 +1,29 @@
 import dataProvider from "../../src/index";
-import client from "../gqlClient";
+import nhost from "../nhost";
 import "./index.mock";
 
 describe("useOne", () => {
+    beforeAll(async () => {
+        await nhost.auth.signIn({
+            email: "salih@pankod.com",
+            password: "refine-nhost",
+        });
+    });
+
     it("correct response with metaData", async () => {
-        const { data } = await dataProvider(client).getOne({
+        const { data } = await dataProvider(nhost).getOne({
             resource: "posts",
-            id: "cddd4ced-651d-4039-abe0-2a9dffbc8c82",
+            id: "72fab741-2352-49cb-8b31-06ae4be2f1d1",
             metaData: {
                 fields: ["id", "title", "content", { category: ["id"] }],
             },
         });
 
-        expect(data["id"]).toEqual("cddd4ced-651d-4039-abe0-2a9dffbc8c82");
-        expect(data["title"]).toEqual("Focus group Human Sleek");
-        expect(data["content"]).toEqual(
-            "Quia nihil dolores et accusantium labore eum.",
-        );
+        expect(data["id"]).toEqual("72fab741-2352-49cb-8b31-06ae4be2f1d1");
+        expect(data["title"]).toEqual("Refine Nhost Demo Title");
+        expect(data["content"]).toEqual("Hello Test");
         expect(data["category"].id).toEqual(
-            "317cea5e-fef3-4858-8043-4496e5c7f5ab",
+            "73c14cb4-a58c-471d-9410-fc97ea6dac66",
         );
     });
 });
