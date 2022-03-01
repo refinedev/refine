6d2b690417691dda6a062ee7f14871b256f7ba71
Ömer Faruk APLAK <omer@pankod.com>
2022-03-01 18:14:23 +0300

    remove lodashOptimizeImports plugin

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "tsup";
-import { lodashOptimizeImports } from "@optimize-lodash/esbuild-plugin";
 
 export default defineConfig({
     entry: ["src/index.tsx"],
     splitting: false,
     sourcemap: true,
     clean: false,
-    esbuildPlugins: [lodashOptimizeImports({})],
     external: ["react", "react-dom"],
 });
