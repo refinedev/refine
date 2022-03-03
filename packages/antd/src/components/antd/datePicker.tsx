30e8c52b07a178cf6c2a88225d2cbb37ca6b5252
Ömer Faruk APLAK <omer@pankod.com>
2022-03-03 11:36:20 +0300

    fix esbuild issues

--- packages/antd/src/components/antd/datePicker.tsx
+++ packages/antd/src/components/antd/datePicker.tsx
@@ -1,6 +1,6 @@
 import { Dayjs } from "dayjs";
-import dayjsGenerateConfig from "rc-picker/es/generate/dayjs.js";
-import generatePicker from "antd/es/date-picker/generatePicker/index.js";
+import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
+import generatePicker from "antd/lib/date-picker/generatePicker/index";
 // import "antd/es/date-picker/style/index.css";
 
 const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
