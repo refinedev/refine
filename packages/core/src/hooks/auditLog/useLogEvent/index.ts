0f137f856923eb26f473a5b2e82d8ce2ac685bc4
Salih <salihozdemir94@gmail.com>
2022-03-09 10:51:19 +0300

    add permission control to useLogEvent hook

--- packages/core/src/hooks/auditLog/useLogEvent/index.ts
+++ packages/core/src/hooks/auditLog/useLogEvent/index.ts
@@ -1,10 +1,31 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 
 import { AuditLogContext } from "@contexts/auditLog";
-import { IAuditLogContext } from "../../../interfaces";
+import { ResourceContext } from "@contexts/resource";
+import { AuditLogEvent } from "../../../interfaces";
 
-export const useLogEvent = () => {
-    const liveContext = useContext<IAuditLogContext>(AuditLogContext);
+export const useLogEvent = (): ((params: AuditLogEvent) => void) => {
+    const liveContext = useContext(AuditLogContext);
+    const { resources } = useContext(ResourceContext);
 
-    return liveContext?.logEvent;
+    const logEvent = useCallback(
+        (params: AuditLogEvent) => {
+            const auditLogPermissions = resources.find(
+                (p) => p.name === params.resource,
+            )?.auditLogPermissions;
+
+            if (auditLogPermissions) {
+                const shouldAuditLog = auditLogPermissions.find(
+                    (i) => i === params.action || i === "*",
+                );
+
+                if (shouldAuditLog) {
+                    liveContext?.logEvent(params);
+                }
+            }
+        },
+        [resources],
+    );
+
+    return logEvent;
 };
