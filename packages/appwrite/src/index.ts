03d87fbbade0096e9d58a1c12d3c99d43e00d953
Simon Trockel <mail@simon-trockel.de>
2022-03-16 10:58:32 +0100

    Update Appwrite to v7

--- packages/appwrite/src/index.ts
+++ packages/appwrite/src/index.ts
@@ -105,7 +105,7 @@ export const dataProvider = (appwriteClient: Appwrite): DataProvider => {
             const appwriteFilters = getAppwriteFilters(filters);
             const { orderField, orderType } = getAppwriteSorting(sort);
 
-            const { sum: total, documents: data } =
+            const { total: total, documents: data } =
                 await appwriteClient.database.listDocuments<any>(
                     resource,
                     appwriteFilters,
