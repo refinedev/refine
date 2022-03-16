16511b6a124e765a63ea8d1bb763d760b6248d13
Salih <salihozdemir94@gmail.com>
2022-03-16 16:03:18 +0300

    initial commit for @refine/audit-log

---
+++ packages/audit-log/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: "ts-jest",
+    rootDir: "./",
+    name: "audit-log",
+    displayName: "audit-log",
+    testEnvironment: "jsdom",
+};
---
+++ packages/audit-log/src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AuditLogList: React.FC = () => {
+    return <div>List All Activities!</div>;
+};
