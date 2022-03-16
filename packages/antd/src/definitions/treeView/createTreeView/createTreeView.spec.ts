e0330703944afa85a9f626a9fd10dc5275bf2828
ozkalai <melihozkalayy@gmail.com>
2022-03-16 16:54:57 +0300

    createTreeView helper with Unit test

---
+++ packages/antd/src/definitions/treeView/createTreeView/createTreeView.spec.ts
@@ -0,0 +1,39 @@
+import { createTreeView } from "./createTreeView";
+import { IMenuItem, ITreeMenu } from "../../../interfaces";
+
+const mockLocation: IMenuItem[] = [
+    {
+        name: "posts",
+        key: "1",
+        route: "posts",
+    },
+    { name: "categories", key: "2", route: "categories" },
+    { name: "active", key: "3", route: "posts/active", parentName: "posts" },
+];
+const expectedMockLocation: ITreeMenu[] = [
+    {
+        name: "posts",
+        key: "1",
+        route: "posts",
+        children: [
+            {
+                name: "active",
+                key: "3",
+                route: "posts/active",
+                parentName: "posts",
+                children: [],
+            },
+        ],
+    },
+    { name: "categories", key: "2", route: "categories", children: [] },
+];
+
+describe("createTreeView", () => {
+    const tree: ITreeMenu[] = createTreeView(mockLocation);
+    it("should return an tree which has two member", async () => {
+        expect(tree.length).toBe(2);
+    });
+    it("should be equal expectedMockLocation", async () => {
+        expect(tree).toEqual(expectedMockLocation);
+    });
+});
