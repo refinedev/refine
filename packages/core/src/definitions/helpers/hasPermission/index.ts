5367bdaf02d77bbffd7f08086aa4c3da194bdc9a
Salih <salihozdemir94@gmail.com>
2022-03-23 15:14:01 +0300

    update hasPermission fucntion with undefined params

--- packages/core/src/definitions/helpers/hasPermission/index.ts
+++ packages/core/src/definitions/helpers/hasPermission/index.ts
@@ -1,6 +1,9 @@
 export const hasPermission = (
-    permissions: string[],
-    action: string,
+    permissions: string[] | undefined,
+    action: string | undefined,
 ): boolean => {
+    if (!permissions || !action) {
+        return false;
+    }
     return !!permissions.find((i) => i === action || i === "*");
 };
