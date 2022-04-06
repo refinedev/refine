27319355d0cb19b703e78635eed93489868d25d6
ozkalai <melihozkalayy@gmail.com>
2022-04-06 18:11:06 +0300

    useResource hook refactor

--- packages/core/src/hooks/resource/useResource/index.spec.tsx
+++ packages/core/src/hooks/resource/useResource/index.spec.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+
+import { Route } from "react-router-dom";
+
 import { renderHook } from "@testing-library/react-hooks";
 
 import { MockJSONServer, TestWrapper } from "@test";
@@ -45,5 +49,117 @@ describe("useResource Hook", () => {
                 }),
             ]),
         );
+    });
+});
+
+describe("useResource Hook without prop", () => {
+    const Wrapper = TestWrapper({
+        routerInitialEntries: ["/posts/edit/1"],
+    });
+
+    const WrapperWith: React.FC = ({ children }) => (
+        <Wrapper>
+            <Route path="/:resource/:action/:id">{children}</Route>
+        </Wrapper>
+    );
+
+    it("should return posts as resourceName cause of inital resource", async () => {
+        const { result } = renderHook(() => useResource(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.resourceName).toBe("posts");
+    });
+
+    it("should return resource which route is custom route", async () => {
+        const Wrapper = TestWrapper({
+            resources: [
+                {
+                    name: "posts",
+                    options: { route: "custom-route-posts" },
+                },
+            ],
+            routerInitialEntries: ["/posts"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Route path="/:resource">{children}</Route>
+            </Wrapper>
+        );
+        const { result } = renderHook(() => useResource(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.resource.options?.route).toBe(
+            "custom-route-posts",
+        );
+    });
+});
+
+describe("useResource Hook with resourceName:propResourceName prop", () => {
+    it("should return propResourceName as resourceName", async () => {
+        const Wrapper = TestWrapper({
+            routerInitialEntries: ["/posts"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Route path="/:resource">{children}</Route>
+            </Wrapper>
+        );
+        const { result } = renderHook(
+            () => useResource({ resourceName: "categories" }),
+            {
+                wrapper: WrapperWith,
+            },
+        );
+
+        expect(result.current.resourceName).toBe("categories");
+    });
+});
+
+describe("useResource Hook with resourceNameOrRouteName prop", () => {
+    it("should return propResourceName as resourceName", async () => {
+        const Wrapper = TestWrapper({
+            resources: [{ name: "refine-makes" }],
+            routerInitialEntries: ["/posts"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Route path="/:resource">{children}</Route>
+            </Wrapper>
+        );
+        const { result } = renderHook(
+            () => useResource({ resourceNameOrRouteName: "refine-makes" }),
+            {
+                wrapper: WrapperWith,
+            },
+        );
+
+        expect(result.current.resourceName).toBe("refine-makes");
+    });
+});
+
+describe("useResource Hook with recordItemId prop", () => {
+    it("should return true id ", async () => {
+        const Wrapper = TestWrapper({
+            routerInitialEntries: ["/posts"],
+        });
+
+        const WrappeWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Route path="/:resource">{children}</Route>
+            </Wrapper>
+        );
+        const { result } = renderHook(
+            () => useResource({ recordItemId: "1" }),
+            {
+                wrapper: WrappeWith,
+            },
+        );
+
+        expect(result.current.id).toBe("1");
     });
 });
