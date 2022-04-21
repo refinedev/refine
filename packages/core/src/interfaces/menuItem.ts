f0dbcb5dbbcd01c61896263d42b07b95a2808969
ozkalai <melihozkalayy@gmail.com>
2022-04-21 11:08:07 +0300

    IResourceItem import path fix

--- packages/core/src/interfaces/menuItem.ts
+++ packages/core/src/interfaces/menuItem.ts
@@ -1,4 +1,4 @@
-import { IResourceItem } from "src";
+import { IResourceItem } from "src/contexts/resource/IResourceContext";
 
 export type ITreeMenu = IResourceItem & {
     children: ITreeMenu[];
