b54f1891b6150c7dafb72ac2f12f320097fcc3ff
mlhekinci <mmlhekinci@gmail.com>
2022-04-21 12:00:07 +0300

    article delete, comment article and delete comment processes done

--- examples/real-world-example/src/pages/article/index.tsx
+++ examples/real-world-example/src/pages/article/index.tsx
@@ -1,5 +1,11 @@
-import { useOne, useGetIdentity } from "@pankod/refine-core";
+import {
+    useOne,
+    useGetIdentity,
+    useDelete,
+    useNavigation,
+} from "@pankod/refine-core";
 import router from "@pankod/refine-react-router-v6";
+import { useForm } from "@pankod/refine-react-hook-form";
 
 import { IArticle } from "interfaces";
 import dayjs from "dayjs";
@@ -9,6 +15,19 @@ const { useParams } = router;
 export const ArticlePage: React.FC = () => {
     const params = useParams();
     const { data: user } = useGetIdentity();
+    const { mutate } = useDelete();
+    const { push } = useNavigation();
+
+    const {
+        register,
+        handleSubmit,
+        refineCore: { onFinish },
+    } = useForm({
+        refineCoreProps: {
+            resource: `articles/${params.slug}/comments`,
+            redirect: false,
+        },
+    });
 
     const { data: item } = useOne<IArticle>({
         resource: "articles",
@@ -21,8 +40,6 @@ export const ArticlePage: React.FC = () => {
         },
     });
 
-    console.log(user);
-
     const { data: commentData } = useOne({
         resource: "articles",
         id: params.slug,
@@ -34,6 +51,18 @@ export const ArticlePage: React.FC = () => {
             enabled: !!params.slug,
         },
     });
+
+    const handleSubmitComment = (data: {}) => {
+        onFinish({ comment: data });
+    };
+
+    const deleteArticle = async () => {
+        await mutate({
+            resource: `articles`,
+            id: params.slug,
+        });
+        push("/");
+    };
 
     return (
         <div className="article-page">
@@ -55,18 +84,41 @@ export const ArticlePage: React.FC = () => {
                                 )}
                             </span>
                         </div>
-                        <button className="btn btn-sm btn-outline-secondary">
-                            <i className="ion-plus-round"></i>
-                            &nbsp; Follow {item?.data.author.username}{" "}
-                        </button>
-                        &nbsp;&nbsp;
-                        <button className="btn btn-sm btn-outline-primary">
-                            <i className="ion-heart"></i>
-                            &nbsp; Favorite Post{" "}
-                            <span className="counter">
-                                {item?.data.favoritesCount}
-                            </span>
-                        </button>
+
+                        {user &&
+                        user?.username === item?.data.author.username ? (
+                            <>
+                                <button className="btn btn-outline-secondary btn-sm">
+                                    <i className="ion-edit"></i>
+                                    &nbsp; Edit Article
+                                </button>
+                                &nbsp;
+                                <button
+                                    className="btn btn-outline-danger btn-sm"
+                                    onClick={() => {
+                                        deleteArticle();
+                                    }}
+                                >
+                                    <i className="ion-trash-a"></i>
+                                    &nbsp; Delete Article
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <button className="btn btn-sm btn-outline-secondary">
+                                    <i className="ion-plus-round"></i>
+                                    &nbsp; Follow {item?.data.author.username}
+                                </button>
+                                &nbsp;
+                                <button className="btn btn-sm btn-outline-primary">
+                                    <i className="ion-heart"></i>
+                                    &nbsp; Favorite Post{" "}
+                                    <span className="counter">
+                                        {item?.data.favoritesCount}
+                                    </span>
+                                </button>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
@@ -102,28 +154,60 @@ export const ArticlePage: React.FC = () => {
                             <a href="" className="author">
                                 {item?.data.author.username}
                             </a>
-                            <span className="date">January 20th</span>
-                        </div>
-                        <button className="btn btn-sm btn-outline-secondary">
-                            <i className="ion-plus-round"></i>
-                            &nbsp; Follow {item?.data.author.username}
-                        </button>
-                        &nbsp;
-                        <button className="btn btn-sm btn-outline-primary">
-                            <i className="ion-heart"></i>
-                            &nbsp; Favorite Post{" "}
-                            <span className="counter">
-                                {item?.data.favoritesCount}
+
+                            <span className="date">
+                                {dayjs(item?.data.createdAt).format(
+                                    "MMM DD, YYYY",
+                                )}
                             </span>
-                        </button>
+                        </div>
+
+                        {user &&
+                        user?.username === item?.data.author.username ? (
+                            <>
+                                <button className="btn btn-outline-secondary btn-sm">
+                                    <i className="ion-edit"></i>
+                                    &nbsp; Edit Article
+                                </button>
+                                &nbsp;
+                                <button
+                                    className="btn btn-outline-danger btn-sm"
+                                    onClick={() => {
+                                        deleteArticle();
+                                    }}
+                                >
+                                    <i className="ion-trash-a"></i>
+                                    &nbsp; Delete Article
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <button className="btn btn-sm btn-outline-secondary">
+                                    <i className="ion-plus-round"></i>
+                                    &nbsp; Follow {item?.data.author.username}
+                                </button>
+                                &nbsp;
+                                <button className="btn btn-sm btn-outline-primary">
+                                    <i className="ion-heart"></i>
+                                    &nbsp; Favorite Post{" "}
+                                    <span className="counter">
+                                        {item?.data.favoritesCount}
+                                    </span>
+                                </button>
+                            </>
+                        )}
                     </div>
                 </div>
 
                 <div className="row">
                     <div className="col-xs-12 col-md-8 offset-md-2">
-                        <form className="card comment-form">
+                        <form
+                            onSubmit={handleSubmit(handleSubmitComment)}
+                            className="card comment-form"
+                        >
                             <div className="card-block">
                                 <textarea
+                                    {...register("body")}
                                     className="form-control"
                                     placeholder="Write a comment..."
                                     rows={3}
@@ -169,6 +253,20 @@ export const ArticlePage: React.FC = () => {
                                             <span className="date-posted">
                                                 {item?.createdAt}
                                             </span>
+                                            {item.author.username ===
+                                                user?.username && (
+                                                <span className="mod-options">
+                                                    <i
+                                                        className="ion-trash-a"
+                                                        onClick={() => {
+                                                            mutate({
+                                                                resource: `articles/${params.slug}/comments`,
+                                                                id: item.id,
+                                                            });
+                                                        }}
+                                                    />
+                                                </span>
+                                            )}
                                         </div>
                                     </>
                                 );
