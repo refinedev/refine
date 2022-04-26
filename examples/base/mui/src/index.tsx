7708d3b0afd150b8aa0c287a8ce86afb4770f96e
ozkalai <melihozkalayy@gmail.com>
2022-04-26 19:31:16 +0300

    App wrapped with StylesProvider for override css

--- examples/base/mui/src/index.tsx
+++ examples/base/mui/src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { StylesProvider } from "@material-ui/styles";
+import "@pankod/refine-mui/dist/styles.min.css";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <StylesProvider injectFirst>
+            <App />
+        </StylesProvider>
     </React.StrictMode>,
     document.getElementById("root"),
 );
