1565fb7f943d94395fcc3fc727a2a36b46937a48
Salih <salihozdemir94@gmail.com>
2022-04-27 12:24:34 +0300

    add i18n provider

--- examples/fineFoods/admin/mui/src/App.tsx
+++ examples/fineFoods/admin/mui/src/App.tsx
@@ -7,19 +7,28 @@ import {
 } from "@pankod/refine-mui";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
+import { useTranslation } from "react-i18next";
 
 import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 
 import { authProvider } from "authProvider";
 import { OrderList, OrderShow } from "pages/orders";
 
-const API_URL = "https://api.fake-rest.refine.dev";
 const App: React.FC = () => {
+    const { t, i18n } = useTranslation();
+
+    const i18nProvider = {
+        translate: (key: string, params: object) => t(key, params),
+        changeLocale: (lang: string) => i18n.changeLanguage(lang),
+        getLocale: () => i18n.language,
+    };
+
     return (
         <Refine
             routerProvider={routerProvider}
-            dataProvider={dataProvider(API_URL)}
+            dataProvider={dataProvider("https://api.finefoods.refine.dev")}
             authProvider={authProvider}
+            i18nProvider={i18nProvider}
             ReadyPage={ReadyPage}
             Layout={Layout}
             LoginPage={LoginPage}
