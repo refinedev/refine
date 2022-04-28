267632ec6e5b8ee5a85b4e734263ca081879c80e
Salih Özdemir <salihozdemir94@gmail.com>
2022-04-28 15:42:30 +0300

    Add Material UI finefoods example (#1759)
    * create base mui fine-foods app
    
    * add i18n provider
    
    * Mui fine foods login (#1762)
    
    * images
    
    * App wrapped with ThemeProvider
    
    * Login page
    
    * body margin
    
    * Montserrat linked
    
    * Login page fix
    
    Co-authored-by: Melih Özkalay <75166276+ozkalai@users.noreply.github.com>
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

---
+++ examples/fineFoods/admin/mui/src/App.tsx
@@ -0,0 +1,64 @@
+import { useMemo } from "react";
+import { Refine } from "@pankod/refine-core";
+import {
+    Layout,
+    ErrorComponent,
+    ReadyPage,
+    ThemeProvider,
+    useMediaQuery,
+    DarkTheme,
+    LightTheme,
+} from "@pankod/refine-mui";
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { useTranslation } from "react-i18next";
+
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
+
+import { authProvider } from "authProvider";
+import { OrderList, OrderShow } from "pages/orders";
+import { LoginPage } from "pages/login";
+
+const App: React.FC = () => {
+    const { t, i18n } = useTranslation();
+
+    const i18nProvider = {
+        translate: (key: string, params: object) => t(key, params),
+        changeLocale: (lang: string) => i18n.changeLanguage(lang),
+        getLocale: () => i18n.language,
+    };
+
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+    const theme = useMemo(
+        () => (prefersDarkMode ? DarkTheme : LightTheme),
+        [prefersDarkMode],
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Refine
+                routerProvider={routerProvider}
+                dataProvider={dataProvider("https://api.finefoods.refine.dev")}
+                authProvider={authProvider}
+                i18nProvider={i18nProvider}
+                ReadyPage={ReadyPage}
+                Layout={Layout}
+                LoginPage={LoginPage}
+                catchAll={<ErrorComponent />}
+                syncWithLocation
+                warnWhenUnsavedChanges
+                resources={[
+                    {
+                        name: "orders",
+                        list: OrderList,
+                        show: OrderShow,
+                        icon: <AddShoppingCartOutlinedIcon />,
+                    },
+                ]}
+            />
+        </ThemeProvider>
+    );
+};
+
+export default App;
---
+++ examples/fineFoods/admin/mui/src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "i18n";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <React.Suspense fallback="loading">
+            <App />
+        </React.Suspense>
+    </React.StrictMode>,
+    document.getElementById("root"),
+);
