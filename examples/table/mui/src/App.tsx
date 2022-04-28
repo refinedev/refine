418c7dfdc9c4753a7e941a56a585dc94c1db46c2
Aydın Akyol <aydinakyol11@gmail.com>
2022-04-28 12:04:15 +0300

    remove auth provider

--- examples/table/mui/src/App.tsx
+++ examples/table/mui/src/App.tsx
@@ -8,7 +8,6 @@ import {
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
 
-import { authProvider } from "authProvider";
 import { BasicDataGrid } from "pages/dataGrid";
 import { PostList } from "pages/table";
 
@@ -16,7 +15,6 @@ const API_URL = "https://api.fake-rest.refine.dev";
 const App: React.FC = () => {
     return (
         <Refine
-            authProvider={authProvider}
             routerProvider={routerProvider}
             dataProvider={dataProvider(API_URL)}
             ReadyPage={ReadyPage}
