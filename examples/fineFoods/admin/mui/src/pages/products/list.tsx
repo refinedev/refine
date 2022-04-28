3f17da3b15e4cfcf0bb287a65a74a42596c66368
ozkalai <melihozkalayy@gmail.com>
2022-04-28 14:18:29 +0300

    product list responsive behavior

--- examples/fineFoods/admin/mui/src/pages/products/list.tsx
+++ examples/fineFoods/admin/mui/src/pages/products/list.tsx
@@ -20,28 +20,23 @@ export const ProductList: React.FC<IResourceComponentsProps> = () => {
 
     return (
         <>
-            <Grid
-                container
-                spacing={{ xs: 2, md: 3 }}
-                columns={{ xs: 4, sm: 8, md: 12 }}
-            >
-                {tableQueryResult.data &&
-                    tableQueryResult.data.data.map((i: IProduct) => (
-                        <Grid item xs={2} sm={3} md={3} key={i.id}>
-                            <ProductItem
-                                name={i.name}
-                                key={i.id}
-                                images={i.images}
-                                price={i.price}
-                                id={i.id}
-                                description={i.description}
-                                isActive={i.isActive}
-                                createdAt={i.createdAt}
-                                category={i.category}
-                                stock={i.stock}
-                            />
-                        </Grid>
-                    ))}
+            <Grid container spacing={{ xs: 2, md: 3 }}>
+                {tableQueryResult.data?.data.map((i: IProduct) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={i.id}>
+                        <ProductItem
+                            name={i.name}
+                            key={i.id}
+                            images={i.images}
+                            price={i.price}
+                            id={i.id}
+                            description={i.description}
+                            isActive={i.isActive}
+                            createdAt={i.createdAt}
+                            category={i.category}
+                            stock={i.stock}
+                        />
+                    </Grid>
+                ))}
             </Grid>
             <button onClick={() => setPageSize((prev) => prev + 10)}>
                 load more
