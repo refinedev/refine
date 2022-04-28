d7a55d29db09e746f5d62b4ddaabb3025d13e812
ozkalai <melihozkalayy@gmail.com>
2022-04-28 16:06:53 +0300

    products list page layout

--- examples/fineFoods/admin/mui/src/pages/products/list.tsx
+++ examples/fineFoods/admin/mui/src/pages/products/list.tsx
@@ -3,7 +3,18 @@ import {
     IResourceComponentsProps,
     useTable,
 } from "@pankod/refine-core";
-import { Grid } from "@pankod/refine-mui";
+import {
+    Grid,
+    Paper,
+    Typography,
+    Box,
+    CreateButton,
+    InputBase,
+    IconButton,
+    Stack,
+} from "@pankod/refine-mui";
+
+import SearchIcon from "@mui/icons-material/Search";
 
 import { ProductItem } from "components";
 
@@ -16,31 +27,93 @@ export const ProductList: React.FC<IResourceComponentsProps> = () => {
         resource: "products",
     });
 
-    console.log("table", tableQueryResult);
-
     return (
-        <>
-            <Grid container spacing={{ xs: 2, md: 3 }}>
-                {tableQueryResult.data?.data.map((i: IProduct) => (
-                    <Grid item xs={12} sm={6} md={4} lg={3} key={i.id}>
-                        <ProductItem
-                            name={i.name}
-                            key={i.id}
-                            images={i.images}
-                            price={i.price}
-                            id={i.id}
-                            description={i.description}
-                            isActive={i.isActive}
-                            createdAt={i.createdAt}
-                            category={i.category}
-                            stock={i.stock}
-                        />
+        <Box component="div" sx={{ backgroundColor: "common.white" }}>
+            <Grid container columns={16}>
+                <Grid item xs={12}>
+                    <Stack
+                        display="flex"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        flexWrap="wrap"
+                        padding={1}
+                        direction="row"
+                    >
+                        <Typography
+                            sx={{
+                                fontWeight: 800,
+                                fontSize: "24px",
+                                lineHeight: "32px",
+                                color: "#626262",
+                                marginBottom: "5px",
+                            }}
+                        >
+                            Product
+                        </Typography>
+                        <Paper
+                            component="form"
+                            sx={{
+                                p: "2px 4px",
+                                display: "flex",
+                                alignItems: "center",
+                                width: 400,
+                                boxShadow: "none",
+                                border: "1px solid #e0e0e0",
+                                marginBottom: "5px",
+                            }}
+                        >
+                            <InputBase
+                                sx={{ ml: 1, flex: 1 }}
+                                placeholder="Product Search"
+                                inputProps={{
+                                    "aria-label": "product search",
+                                }}
+                            />
+                            <IconButton
+                                type="submit"
+                                sx={{ p: "10px" }}
+                                aria-label="search"
+                            >
+                                <SearchIcon />
+                            </IconButton>
+                        </Paper>
+                        <CreateButton
+                            variant="contained"
+                            sx={{ marginBottom: "5px" }}
+                        >
+                            ADD PRODUCT
+                        </CreateButton>
+                    </Stack>
+                    <Grid container>
+                        {tableQueryResult.data?.data.map((i: IProduct) => (
+                            <Grid
+                                item
+                                xs={12}
+                                md={4}
+                                lg={3}
+                                key={i.id}
+                                sx={{ padding: "8px" }}
+                            >
+                                <ProductItem
+                                    name={i.name}
+                                    key={i.id}
+                                    images={i.images}
+                                    price={i.price}
+                                    id={i.id}
+                                    description={i.description}
+                                    isActive={i.isActive}
+                                    createdAt={i.createdAt}
+                                    category={i.category}
+                                    stock={i.stock}
+                                />
+                            </Grid>
+                        ))}
                     </Grid>
-                ))}
+                </Grid>
+                <Grid item xs={4}>
+                    fillter wrapper
+                </Grid>
             </Grid>
-            <button onClick={() => setPageSize((prev) => prev + 10)}>
-                load more
-            </button>
-        </>
+        </Box>
     );
 };
