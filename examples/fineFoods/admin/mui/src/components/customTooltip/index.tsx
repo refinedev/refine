ae299c5a21fdc5b9ef9404936c841c3140b302d4
Salih Özdemir <salihozdemir94@gmail.com>
2022-05-09 12:39:08 +0300

    Mui finefoods order resource (#1763)
    * create base mui fine-foods app
    
    * add i18n provider
    
    * create order list page
    
    * add custom tooltip and fix table appereance
    
    * update filter form
    
    * add google-map-react package
    
    * add env variable
    
    * create order components
    
    * add custom icons
    
    * add order show page
    
    * fix order filters selects
    
    * make multiple filtering for order statuses
    
    * Fix setWarnWhen on react-hook-form package (#1783)
    
    * v3.16.3
    
    * fix order search form warnWhenUnsavedChanges
    
    * update orders filters
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

---
+++ examples/fineFoods/admin/mui/src/components/customTooltip/index.tsx
@@ -0,0 +1,16 @@
+import { styled } from "@mui/material/styles";
+import { Tooltip, TooltipProps, tooltipClasses } from "@pankod/refine-mui";
+
+export const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: theme.palette.common.white,
+        color: "rgba(0, 0, 0, 0.87)",
+        boxShadow: theme.shadows[1],
+        fontSize: 12,
+    },
+    [`& .${tooltipClasses.arrow}`]: {
+        color: theme.palette.common.white,
+    },
+}));
