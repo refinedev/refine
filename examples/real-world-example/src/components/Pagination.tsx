66f69f0053e6119de79a48a345d837437492c85c
Ömer Faruk APLAK <omer@pankod.com>
2022-05-11 11:20:47 +0300

    Add "pageCount" to the useTable hook

--- examples/real-world-example/src/components/Pagination.tsx
+++ examples/real-world-example/src/components/Pagination.tsx
@@ -1,42 +1,38 @@
 type PaginationProps = {
-    total: number | undefined;
-    pageSize: number;
+    total: number;
     current: number;
     setCurrent: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export const Pagination: React.FC<PaginationProps> = ({
     total,
-    pageSize,
     current,
     setCurrent,
 }) => {
     return (
         <nav>
             <ul className="pagination">
-                {[...Array(total ? Math.ceil(total / pageSize) : 1)].map(
-                    (_, index) => {
-                        return (
-                            <li
-                                key={index}
-                                className={`page-item${
-                                    current !== index + 1 ? "" : " active"
-                                }`}
-                            >
-                                {total && total / pageSize > 1 && (
-                                    <button
-                                        className="page-link"
-                                        onClick={() => {
-                                            setCurrent(index + 1);
-                                        }}
-                                    >
-                                        {index + 1}
-                                    </button>
-                                )}
-                            </li>
-                        );
-                    },
-                )}
+                {[...Array(total)].map((_, index) => {
+                    return (
+                        <li
+                            key={index}
+                            className={`page-item${
+                                current !== index + 1 ? "" : " active"
+                            }`}
+                        >
+                            {total > 1 && (
+                                <button
+                                    className="page-link"
+                                    onClick={() => {
+                                        setCurrent(index + 1);
+                                    }}
+                                >
+                                    {index + 1}
+                                </button>
+                            )}
+                        </li>
+                    );
+                })}
             </ul>
         </nav>
     );
--- packages/core/src/hooks/useTable/index.ts
+++ packages/core/src/hooks/useTable/index.ts
@@ -57,6 +57,7 @@ export type useTableReturnType<TData extends BaseRecord = BaseRecord> = {
     setCurrent: ReactSetState<useTableReturnType["current"]>;
     pageSize: number;
     setPageSize: ReactSetState<useTableReturnType["pageSize"]>;
+    pageCount: number;
 };
 
 /**
@@ -177,6 +178,8 @@ export const useTable = <
         setSorter(() => unionSorters(permanentSorter, newSorter));
     };
 
+    const pageCount = Math.ceil((queryResult.data?.total ?? 0) / pageSize);
+
     return {
         tableQueryResult: queryResult,
         sorter,
@@ -187,5 +190,6 @@ export const useTable = <
         setCurrent,
         pageSize,
         setPageSize,
+        pageCount,
     };
 };
