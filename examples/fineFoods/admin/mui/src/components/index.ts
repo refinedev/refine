40bce2e09110aca28ca3013a0abd878f78b39613
Melih Özkalay <75166276+ozkalai@users.noreply.github.com>
2022-05-13 10:06:38 +0300

    Mui fine foods products (#1764)
    * create base mui fine-foods app
    
    * add i18n provider
    
    * interface
    
    * products resource added
    
    * product item component
    
    * products list
    
    * product item
    
    * product list responsive behavior
    
    * product item style fix
    
    * products list page layout
    
    * removed paper background from palette
    
    * pagination and search on product list
    
    * CategoryFilter component initial
    
    * CategoryFilter component added
    
    * CategoryFilter component
    
    * filters handled on List
    
    * fix x-index
    
    * list page
    
    * categories passed as prop
    
    * Create component with export
    
    * removed unused file
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    
    * list page
    
    * fix crud edit title
    
    * product components export
    
    * categoryFilter component
    
    * edit component
    
    * product item component
    
    * edit product export
    
    * pageCount fix
    
    * fix ProductItemProps
    
    * fix comments
    
    * Update examples/fineFoods/admin/mui/src/components/product/create.tsx
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    Co-authored-by: Salih <salihozdemir94@gmail.com>
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- examples/fineFoods/admin/mui/src/components/index.ts
+++ examples/fineFoods/admin/mui/src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./orderStatus";
 export * from "./customTooltip";
 export * from "./courierInfoBox";
 export * from "./mapMarker";
+export * from "./product";
---
+++ examples/fineFoods/admin/mui/src/components/product/edit.tsx
@@ -0,0 +1,368 @@
+import React from "react";
+import axios from "axios";
+
+import { useTranslate, useApiUrl } from "@pankod/refine-core";
+
+import {
+    FieldValues,
+    UseFormRegister,
+    UseFormHandleSubmit,
+    UseFormSetValue,
+    UseFormWatch,
+    Controller,
+    Control,
+} from "@pankod/refine-react-hook-form";
+
+import {
+    Drawer,
+    FormControlLabel,
+    Input,
+    Radio,
+    RadioGroup,
+    TextField,
+    Avatar,
+    Typography,
+    FormLabel,
+    Stack,
+    useTheme,
+    Box,
+    IconButton,
+    FormControl,
+    useAutocomplete,
+    OutlinedInput,
+    InputAdornment,
+    FormHelperText,
+    Autocomplete,
+    Edit,
+    SaveButtonProps,
+} from "@pankod/refine-mui";
+
+import CloseIcon from "@mui/icons-material/Close";
+
+import { ICategory } from "interfaces";
+
+type EditProductProps = {
+    visible: boolean;
+    close: () => void;
+    register: UseFormRegister<FieldValues>;
+    handleSubmit: UseFormHandleSubmit<FieldValues>;
+    setValue: UseFormSetValue<FieldValues>;
+    errors: { [x: string]: any };
+    watch: UseFormWatch<FieldValues>;
+    onFinish: (values: FieldValues) => Promise<void>;
+    control: Control<FieldValues>;
+    saveButtonProps: SaveButtonProps;
+};
+
+export const EditProduct: React.FC<EditProductProps> = ({
+    visible,
+    close,
+    register,
+    handleSubmit,
+    setValue,
+    errors,
+    watch,
+    onFinish,
+    control,
+    saveButtonProps,
+}) => {
+    const t = useTranslate();
+
+    const theme = useTheme();
+
+    const apiUrl = useApiUrl();
+
+    const { autocompleteProps } = useAutocomplete<ICategory>({
+        resource: "categories",
+    });
+
+    const imageInput = watch("images");
+
+    const onChangeHandler = async (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        const formData = new FormData();
+
+        const target = event.target;
+        const file: File = (target.files as FileList)[0];
+
+        formData.append("file", file);
+
+        const res = await axios.post<{ url: string }>(
+            `${apiUrl}/media/upload`,
+            formData,
+            {
+                withCredentials: false,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+            },
+        );
+
+        const { name, size, type, lastModified } = file;
+
+        const imagePaylod = [
+            {
+                name,
+                size,
+                type,
+                lastModified,
+                url: res.data.url,
+            },
+        ];
+
+        setValue("images", imagePaylod, { shouldValidate: true });
+    };
+
+    return (
+        <Drawer
+            sx={{
+                "& .MuiDrawer-paper": {
+                    [theme.breakpoints.down("sm")]: {
+                        width: " 100%",
+                    },
+                    width: "500px",
+                },
+            }}
+            open={visible}
+            onClose={close}
+            anchor="right"
+        >
+            <Edit
+                saveButtonProps={saveButtonProps}
+                cardHeaderProps={{
+                    avatar: (
+                        <IconButton
+                            onClick={() => close()}
+                            sx={{ width: "30px", height: "30px", mb: "5px" }}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    ),
+                    action: null,
+                }}
+                cardProps={{ sx: { overflowY: "scroll", height: "100vh" } }}
+            >
+                <Stack>
+                    <Box
+                        paddingX="50px"
+                        justifyContent="center"
+                        alignItems="center"
+                        marginBottom="50px"
+                    >
+                        <form onSubmit={handleSubmit(onFinish)}>
+                            <FormControl sx={{ width: "100%" }}>
+                                <FormLabel required>
+                                    {t("products.fields.images.label")}
+                                </FormLabel>
+                                <Stack
+                                    display="flex"
+                                    alignItems="center"
+                                    border="1px dashed  "
+                                    borderColor="primary.main"
+                                    borderRadius="5px"
+                                    padding="10px"
+                                    marginTop="5px"
+                                    sx={{
+                                        backgroundColor: "#fafafa",
+                                    }}
+                                >
+                                    <label htmlFor="images-input">
+                                        <Input
+                                            id="images-input"
+                                            type="file"
+                                            sx={{
+                                                display: "none",
+                                            }}
+                                            onChange={onChangeHandler}
+                                        />
+                                        <input
+                                            id="file"
+                                            {...register("images", {
+                                                required: "Image ",
+                                            })}
+                                            type="hidden"
+                                        />
+                                        <Avatar
+                                            sx={{
+                                                cursor: "pointer",
+                                                width: "200px",
+                                                height: "200px",
+                                            }}
+                                            src={
+                                                imageInput && imageInput[0].url
+                                            }
+                                            alt="Store Location"
+                                        />
+                                    </label>
+                                    <Typography
+                                        sx={{
+                                            fontWeight: 800,
+                                            fontSize: "16px",
+                                            marginTop: "8px",
+                                        }}
+                                    >
+                                        {t(
+                                            "products.fields.images.description",
+                                        )}
+                                    </Typography>
+                                    <Typography style={{ fontSize: "12px" }}>
+                                        {t("products.fields.images.validation")}
+                                    </Typography>
+                                </Stack>
+                                {errors.images && (
+                                    <FormHelperText error>
+                                        {errors.images.message}
+                                    </FormHelperText>
+                                )}
+                            </FormControl>
+                            <Stack gap="10px" marginTop="10px">
+                                <FormControl>
+                                    <FormLabel required>
+                                        {t("products.fields.name")}
+                                    </FormLabel>
+                                    <OutlinedInput
+                                        id="name"
+                                        {...register("name", {
+                                            required: "Name required",
+                                        })}
+                                        style={{ height: "40px" }}
+                                    />
+                                    {errors.name && (
+                                        <FormHelperText error>
+                                            {errors.name.message}
+                                        </FormHelperText>
+                                    )}
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel required>
+                                        {t("products.fields.description")}
+                                    </FormLabel>
+                                    <OutlinedInput
+                                        id="description"
+                                        {...register("description", {
+                                            required: "Description required",
+                                        })}
+                                        multiline
+                                        maxRows={5}
+                                    />
+                                    {errors.description && (
+                                        <FormHelperText error>
+                                            {errors.description.message}
+                                        </FormHelperText>
+                                    )}
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel required>
+                                        {t("products.fields.price")}
+                                    </FormLabel>
+                                    <OutlinedInput
+                                        id="price"
+                                        {...register("price", {
+                                            required: "Price required",
+                                        })}
+                                        style={{
+                                            width: "150px",
+                                            height: "40px",
+                                        }}
+                                        startAdornment={
+                                            <InputAdornment position="start">
+                                                $
+                                            </InputAdornment>
+                                        }
+                                    />
+                                    {errors.price && (
+                                        <FormHelperText error>
+                                            {errors.price.message}
+                                        </FormHelperText>
+                                    )}
+                                </FormControl>
+                                <FormControl>
+                                    <Controller
+                                        control={control}
+                                        name="category"
+                                        rules={{
+                                            required: "Category required",
+                                        }}
+                                        render={({ field }) => (
+                                            <Autocomplete
+                                                {...autocompleteProps}
+                                                {...field}
+                                                onChange={(_, value) => {
+                                                    field.onChange(value);
+                                                }}
+                                                getOptionLabel={(item) => {
+                                                    return item.title
+                                                        ? item.title
+                                                        : "";
+                                                }}
+                                                isOptionEqualToValue={(
+                                                    option,
+                                                    value,
+                                                ) =>
+                                                    value === undefined ||
+                                                    option.id === value.id
+                                                }
+                                                renderInput={(params) => (
+                                                    <TextField
+                                                        {...params}
+                                                        label="Category"
+                                                        variant="outlined"
+                                                        error={!!errors.item}
+                                                        required
+                                                    />
+                                                )}
+                                            />
+                                        )}
+                                    />
+                                    {errors.category && (
+                                        <FormHelperText error>
+                                            {errors.category.message}
+                                        </FormHelperText>
+                                    )}
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel
+                                        sx={{ marginTop: "10px" }}
+                                        required
+                                    >
+                                        {t("products.fields.isActive")}
+                                    </FormLabel>
+                                    <Controller
+                                        control={control}
+                                        name="isActive"
+                                        defaultValue={""}
+                                        rules={{ required: true }}
+                                        render={({ field }) => (
+                                            <RadioGroup
+                                                id="isActive"
+                                                {...field}
+                                                row
+                                            >
+                                                <FormControlLabel
+                                                    value={true}
+                                                    control={<Radio />}
+                                                    label={t("status.enable")}
+                                                />
+                                                <FormControlLabel
+                                                    value={false}
+                                                    control={<Radio />}
+                                                    label={t("status.disable")}
+                                                />
+                                            </RadioGroup>
+                                        )}
+                                    />
+                                    {errors.isActive && (
+                                        <FormHelperText error>
+                                            {errors.isActive.message}
+                                        </FormHelperText>
+                                    )}
+                                </FormControl>
+                            </Stack>
+                        </form>
+                    </Box>
+                </Stack>
+            </Edit>
+        </Drawer>
+    );
+};
