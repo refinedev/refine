1c1f0c228c89b86a40d952171665cfdcfaa958ee
ozkalai <melihozkalayy@gmail.com>
2022-05-20 20:36:36 +0300

    ColorModeContext.Provider

--- examples/fineFoods/admin/mui/src/App.tsx
+++ examples/fineFoods/admin/mui/src/App.tsx
@@ -1,10 +1,9 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Refine } from "@pankod/refine-core";
 import {
     ErrorComponent,
     ReadyPage,
     ThemeProvider,
-    useMediaQuery,
     DarkTheme,
     LightTheme,
     notificationProviderHandle,
@@ -28,86 +27,103 @@ import { ReviewsList } from "pages/reviews";
 import { LoginPage } from "pages/login";
 import { StoreList, StoreEdit, StoreCreate } from "pages/stores";
 import { ProductList } from "pages/products";
-
 import { Layout } from "components/layout";
+import { ColorModeContext } from "contexts";
 
 const App: React.FC = () => {
-    const { t, i18n } = useTranslation();
+    const colorModeFromLocalStorage = localStorage.getItem("colorMode");
 
+    const [mode, setMode] = useState(colorModeFromLocalStorage || "light");
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) =>
+                    prevMode === "light" ? "dark" : "light",
+                );
+            },
+            mode,
+        }),
+        [mode],
+    );
+
+    useEffect(() => {
+        localStorage.setItem("colorMode", mode);
+    }, [mode]);
+
+    const { t, i18n } = useTranslation();
     const i18nProvider = {
         translate: (key: string, params: object) => t(key, params),
         changeLocale: (lang: string) => i18n.changeLanguage(lang),
         getLocale: () => i18n.language,
     };
 
-    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-
-    const theme = useMemo(
-        () => (prefersDarkMode ? DarkTheme : LightTheme),
-        [prefersDarkMode],
-    );
     const notificationProvider = notificationProviderHandle();
 
     return (
-        <ThemeProvider theme={theme}>
-            <Refine
-                routerProvider={routerProvider}
-                dataProvider={dataProvider("https://api.finefoods.refine.dev")}
-                authProvider={authProvider}
-                i18nProvider={i18nProvider}
-                DashboardPage={DashboardPage}
-                ReadyPage={ReadyPage}
-                Layout={Layout}
-                LoginPage={LoginPage}
-                catchAll={<ErrorComponent />}
-                syncWithLocation
-                warnWhenUnsavedChanges
-                notificationProvider={notificationProvider}
-                resources={[
-                    {
-                        name: "orders",
-                        list: OrderList,
-                        show: OrderShow,
-                        icon: <AddShoppingCartOutlined />,
-                    },
-                    {
-                        name: "reviews",
-                        list: ReviewsList,
-                        icon: <StarBorderOutlined />,
-                    },
-                    {
-                        name: "stores",
-                        list: StoreList,
-                        edit: StoreEdit,
-                        create: StoreCreate,
-                        icon: <StoreOutlined />,
-                    },
-                    {
-                        name: "users",
-                        list: UserList,
-                        show: UserShow,
-                        icon: <PeopleOutlineOutlined />,
-                    },
-                    {
-                        name: "products",
-                        list: ProductList,
-                        icon: <LocalPizzaOutlined />,
-                    },
-                    {
-                        parentName: "products",
-                        name: "Add Product",
-                        list: ProductList,
-                        icon: <LocalPizzaOutlined />,
-                    },
-                    {
-                        parentName: "products",
-                        name: "Sell Product",
-                        list: ProductList,
-                        icon: <LocalPizzaOutlined />,
-                    },
-                ]}
-            />
-        </ThemeProvider>
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={mode === "dark" ? DarkTheme : LightTheme}>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(
+                        "https://api.finefoods.refine.dev",
+                    )}
+                    authProvider={authProvider}
+                    i18nProvider={i18nProvider}
+                    DashboardPage={DashboardPage}
+                    ReadyPage={ReadyPage}
+                    Layout={Layout}
+                    LoginPage={LoginPage}
+                    catchAll={<ErrorComponent />}
+                    syncWithLocation
+                    warnWhenUnsavedChanges
+                    notificationProvider={notificationProvider}
+                    resources={[
+                        {
+                            name: "orders",
+                            list: OrderList,
+                            show: OrderShow,
+                            icon: <AddShoppingCartOutlined />,
+                        },
+                        {
+                            name: "reviews",
+                            list: ReviewsList,
+                            icon: <StarBorderOutlined />,
+                        },
+                        {
+                            name: "stores",
+                            list: StoreList,
+                            edit: StoreEdit,
+                            create: StoreCreate,
+                            icon: <StoreOutlined />,
+                        },
+                        {
+                            name: "users",
+                            list: UserList,
+                            show: UserShow,
+                            icon: <PeopleOutlineOutlined />,
+                        },
+                        {
+                            name: "products",
+                            list: ProductList,
+                            icon: <LocalPizzaOutlined />,
+                        },
+                        {
+                            parentName: "products",
+                            name: "Add Product",
+                            list: ProductList,
+                            icon: <LocalPizzaOutlined />,
+                        },
+                        {
+                            parentName: "products",
+                            name: "Sell Product",
+                            list: ProductList,
+                            icon: <LocalPizzaOutlined />,
+                        },
+                    ]}
+                />
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     );
 };
 
