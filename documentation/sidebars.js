c974b8e187486b0d25ba5fd608183a44039daf32
Salih <salihozdemir94@gmail.com>
2022-05-24 18:06:13 +0300

    add some sections for mui data grid doc

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -295,6 +295,18 @@ module.exports = {
                         },
                     ],
                 },
+                {
+                    type: "category",
+                    label: "Material Design",
+                    collapsed: false,
+                    items: [
+                        {
+                            type: "category",
+                            label: "Hooks",
+                            items: ["ui-frameworks/mui/hooks/useDataGrid"],
+                        },
+                    ],
+                },
             ],
         },
         {
