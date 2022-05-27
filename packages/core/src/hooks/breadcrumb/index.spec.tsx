5b3cc77978cb3d95752dac4482a253ed23e60558
Salih <salihozdemir94@gmail.com>
2022-05-27 14:44:28 +0300

    add useBreadcrumb hook tests

---
+++ packages/core/src/hooks/breadcrumb/index.spec.tsx
@@ -0,0 +1,192 @@
+import React from "react";
+import { renderHook } from "@testing-library/react-hooks";
+import { Route, Routes } from "react-router-dom";
+
+import { TestWrapper } from "@test";
+
+import { useBreadcrumb } from ".";
+
+const dummyIcon = <div>icon</div>;
+const DummyResourcePage = () => <div>resource page</div>;
+
+describe("useBreadcrumb Hook", () => {
+    it("Should only return `label` without `icon` and `to`", async () => {
+        const Wrapper = TestWrapper({
+            resources: [
+                {
+                    name: "users",
+                    route: "users",
+                },
+            ],
+            routerInitialEntries: ["/users"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Routes>
+                    <Route path="/:resource" element={children} />
+                </Routes>
+            </Wrapper>
+        );
+
+        const { result } = renderHook(() => useBreadcrumb(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.breadcrumbs).toEqual([{ label: "Users" }]);
+    });
+
+    it("Should return `label` and `icon` without `to`", async () => {
+        const Wrapper = TestWrapper({
+            resources: [
+                {
+                    name: "users",
+                    route: "users",
+                    icon: dummyIcon,
+                },
+            ],
+            routerInitialEntries: ["/users"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Routes>
+                    <Route path="/:resource" element={children} />
+                </Routes>
+            </Wrapper>
+        );
+
+        const { result } = renderHook(() => useBreadcrumb(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.breadcrumbs).toEqual([
+            { icon: <div>icon</div>, label: "Users" },
+        ]);
+    });
+
+    it("if resource has `list` resource page should successfully return `to`", async () => {
+        const Wrapper = TestWrapper({
+            resources: [
+                {
+                    name: "users",
+                    route: "users",
+                    list: DummyResourcePage,
+                },
+            ],
+            routerInitialEntries: ["/users"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Routes>
+                    <Route path="/:resource" element={children} />
+                </Routes>
+            </Wrapper>
+        );
+
+        const { result } = renderHook(() => useBreadcrumb(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.breadcrumbs).toEqual([
+            { label: "Users", to: "/users" },
+        ]);
+    });
+
+    it("if the resource has a label, the breadcrumb label should be equal to that label", async () => {
+        const Wrapper = TestWrapper({
+            resources: [
+                {
+                    name: "users",
+                    label: "Hello",
+                    route: "users",
+                },
+            ],
+            routerInitialEntries: ["/users"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Routes>
+                    <Route path="/:resource" element={children} />
+                </Routes>
+            </Wrapper>
+        );
+
+        const { result } = renderHook(() => useBreadcrumb(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.breadcrumbs).toEqual([{ label: "Hello" }]);
+    });
+
+    it("if the user is on the resource action page, the action name should be last in the breadcrumbs", async () => {
+        const Wrapper = TestWrapper({
+            resources: [
+                {
+                    name: "users",
+                    route: "users",
+                    icon: dummyIcon,
+                    list: DummyResourcePage,
+                    create: DummyResourcePage,
+                },
+            ],
+            routerInitialEntries: ["/users/create"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Routes>
+                    <Route path="/:resource/:action" element={children} />
+                </Routes>
+            </Wrapper>
+        );
+
+        const { result } = renderHook(() => useBreadcrumb(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.breadcrumbs).toEqual([
+            { icon: <div>icon</div>, label: "Users", to: "/users" },
+            { label: "Create" },
+        ]);
+    });
+
+    it("if resources has nested resources, `parentName` should come in breadcrumbs", async () => {
+        const Wrapper = TestWrapper({
+            resources: [
+                {
+                    name: "cms",
+                },
+                {
+                    parentName: "cms",
+                    name: "users",
+                    route: "cms/users",
+                    icon: dummyIcon,
+                    list: DummyResourcePage,
+                    create: DummyResourcePage,
+                },
+            ],
+            routerInitialEntries: ["/users/create"],
+        });
+
+        const WrapperWith: React.FC = ({ children }) => (
+            <Wrapper>
+                <Routes>
+                    <Route path="/:resource/:action" element={children} />
+                </Routes>
+            </Wrapper>
+        );
+
+        const { result } = renderHook(() => useBreadcrumb(), {
+            wrapper: WrapperWith,
+        });
+
+        expect(result.current.breadcrumbs).toEqual([
+            { label: "Cms" },
+            { icon: <div>icon</div>, label: "Users", to: "/cms/users" },
+            { label: "Create" },
+        ]);
+    });
+});
