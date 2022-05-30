d245850de1c0519ddab528e0dc0be893b3254e47
Salih <salihozdemir94@gmail.com>
2022-05-30 12:43:22 +0300

    create breadcrumb component

---
+++ packages/antd/src/components/breadcrumb/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+    useBreadcrumb,
+    useRefineContext,
+    useRouterContext,
+} from "@pankod/refine-core";
+import { Breadcrumb as AntdBreadcrumb, BreadcrumbProps } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+
+export type BreadcrumbComponentProps = {
+    breadcrumbProps?: BreadcrumbProps;
+    showHome?: boolean;
+    hideIcons?: boolean;
+};
+
+export const BreadcrumbComponent: React.FC<BreadcrumbComponentProps> = ({
+    breadcrumbProps,
+    showHome = true,
+    hideIcons = false,
+}) => {
+    const { breadcrumbs } = useBreadcrumb();
+    const { Link } = useRouterContext();
+    const { hasDashboard } = useRefineContext();
+
+    return (
+        <AntdBreadcrumb {...breadcrumbProps}>
+            {showHome && hasDashboard && (
+                <AntdBreadcrumb.Item>
+                    <Link to="/" href="/">
+                        <HomeOutlined />
+                    </Link>
+                </AntdBreadcrumb.Item>
+            )}
+            {breadcrumbs.map(({ label, icon, href }) => {
+                return (
+                    <AntdBreadcrumb.Item key={label}>
+                        {!hideIcons && icon}
+                        {href ? (
+                            <Link to={href} href={href}>
+                                {label}
+                            </Link>
+                        ) : (
+                            label
+                        )}
+                    </AntdBreadcrumb.Item>
+                );
+            })}
+        </AntdBreadcrumb>
+    );
+};
