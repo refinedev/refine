e43e9e4e3f08a710ab5a0e7326669a6bff7d2692
ozkalai <melihozkalayy@gmail.com>
2022-05-30 14:19:28 +0300

    header component mobile fix

--- examples/fineFoods/admin/mui/src/components/header/index.tsx
+++ examples/fineFoods/admin/mui/src/components/header/index.tsx
@@ -134,7 +134,7 @@ export const Header: React.FC = () => {
 
     return (
         <AppBar color="default" position="sticky" elevation={1}>
-            <Toolbar>
+            <Toolbar sx={{ padding: { xs: 1, md: 3 } }}>
                 <Stack
                     direction="row"
                     width="100%"
