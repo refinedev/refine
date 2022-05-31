58e0331d14fcf8b22b2771e19485446d9f8fe706
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-05-31 12:08:34 +0300

    refactor(cloud-component): use useContext instance of useResource hook

--- packages/core/src/components/cloud/index.tsx
+++ packages/core/src/components/cloud/index.tsx
@@ -2,39 +2,36 @@ import React, { useContext, useEffect } from "react";
 import { IResource } from "@pankod/refine-sdk";
 
 import { CloudContext } from "@contexts/cloud";
-import { ResourceContext } from "@contexts/resource";
+import { useResource } from "@hooks/resource";
 
 export const Cloud: React.FC<{}> = () => {
     const cloud = useContext(CloudContext);
-    const resourceContext = useContext(ResourceContext);
+    const { resources: contextResources } = useResource();
 
     useEffect(() => {
         if (cloud && cloud.sdk) {
             const { sdk } = cloud;
 
-            const resources: IResource[] = resourceContext.resources.map(
-                (i) => {
-                    const { name, route, list, create, edit, show, canDelete } =
-                        i;
+            const resources: IResource[] = contextResources.map((i) => {
+                const { name, route, list, create, edit, show, canDelete } = i;
 
-                    return {
-                        name: name,
-                        key: route!,
-                        hasList: !!list,
-                        hasCreate: !!create,
-                        hasEdit: !!edit,
-                        hasDelete: !!canDelete,
-                        hasShow: !!show,
-                    };
-                },
-            );
+                return {
+                    name: name,
+                    key: route!,
+                    hasList: !!list,
+                    hasCreate: !!create,
+                    hasEdit: !!edit,
+                    hasDelete: !!canDelete,
+                    hasShow: !!show,
+                };
+            });
 
             // send cloud sdk
             sdk.draftResource.create({
                 resources,
             });
         }
-    }, [resourceContext.resources]);
+    }, [contextResources]);
 
     return null;
 };
