11c4a9e42d57c71d755546608536a6c802d26fa2
Aydın Akyol <aydinakyol11@gmail.com>
2022-06-01 17:19:02 +0300

    refactor(index): add snackbar provider

--- examples/tutorial/mui/src/index.tsx
+++ examples/tutorial/mui/src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { SnackbarProvider } from "notistack";
+
 import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <SnackbarProvider>
+            <App />
+        </SnackbarProvider>
     </React.StrictMode>,
     document.getElementById("root"),
 );
