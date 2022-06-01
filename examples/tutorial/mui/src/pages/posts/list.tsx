31c2b6e957e9e865a0aa000f29cca2667c685199
Aydın Akyol <aydinakyol11@gmail.com>
2022-06-01 17:25:14 +0300

    refactor(list-page): add created at column and filter mode to DataGrid

--- examples/tutorial/mui/src/pages/posts/list.tsx
+++ examples/tutorial/mui/src/pages/posts/list.tsx
@@ -6,66 +6,98 @@ import {
     EditButton,
     ShowButton,
     DeleteButton,
+    TagField,
+    DateField,
     List,
     Stack,
 } from "@pankod/refine-mui";
 
-const columns: GridColumns = [
-    { field: "title", headerName: "Title", flex: 1, minWidth: 350 },
-    {
-        field: "category.id",
-        headerName: "Category",
-        type: "number",
-        minWidth: 350,
-        valueGetter: (params) => {
-            const { data } = useOne({
-                resource: "categories",
-                id: params.row.category.id,
-            });
-            return data?.data.title;
-        },
-    },
-    { field: "status", headerName: "Status", minWidth: 150 },
-    {
-        headerName: "Actions",
-        field: "actions",
-        minWidth: 250,
-        // eslint-disable-next-line react/display-name
-        renderCell: (params) => {
-            return (
-                <Stack direction="row" spacing={1}>
-                    <EditButton
-                        hideText
-                        size="small"
-                        variant="outlined"
-                        recordItemId={params.row.id}
-                    />
-                    <ShowButton
-                        hideText
-                        size="small"
-                        variant="outlined"
-                        recordItemId={params.row.id}
-                    />
-                    <DeleteButton
-                        hideText
-                        size="small"
-                        variant="outlined"
-                        recordItemId={params.row.id}
-                    />
-                </Stack>
-            );
-        },
-    },
-];
+import { IPost, ICategory } from "interfaces";
 
 export const PostList: React.FC = () => {
-    const { dataGridProps } = useDataGrid({
+    const columns: GridColumns = [
+        { field: "title", headerName: "Title", flex: 1, minWidth: 350 },
+        {
+            field: "category.id",
+            headerName: "Category",
+            type: "number",
+            minWidth: 350,
+            valueGetter: (params) => {
+                const { data } = useOne<ICategory>({
+                    resource: "categories",
+                    id: params.row.category.id,
+                });
+                return data?.data.title;
+            },
+        },
+        {
+            field: "status",
+            headerName: "Status",
+            minWidth: 150,
+            // eslint-disable-next-line react/display-name
+            renderCell: (params) => <TagField value={params.row.status} />,
+        },
+        {
+            field: "createdAt",
+            headerName: "CreatedAt",
+            minWidth: 150,
+            // eslint-disable-next-line react/display-name
+            renderCell: (params) => (
+                <DateField format="LLL" value={params.row.createdAt} />
+            ),
+        },
+        {
+            headerName: "Actions",
+            field: "actions",
+            minWidth: 250,
+            // eslint-disable-next-line react/display-name
+            renderCell: (params) => {
+                return (
+                    <Stack direction="row" spacing={1}>
+                        <EditButton
+                            hideText
+                            size="small"
+                            variant="outlined"
+                            recordItemId={params.row.id}
+                        />
+                        <ShowButton
+                            hideText
+                            size="small"
+                            variant="outlined"
+                            recordItemId={params.row.id}
+                        />
+                        <DeleteButton
+                            hideText
+                            size="small"
+                            variant="outlined"
+                            recordItemId={params.row.id}
+                        />
+                    </Stack>
+                );
+            },
+        },
+    ];
+
+    const { dataGridProps } = useDataGrid<IPost>({
         columns,
     });
 
+    const {
+        filterMode,
+        filterModel,
+        onFilterModelChange,
+        ...restDataGridProps
+    } = dataGridProps;
+
     return (
         <List>
-            <DataGrid {...dataGridProps} autoHeight />
+            <DataGrid
+                {...restDataGridProps}
+                filterMode={filterMode}
+                filterModel={filterModel}
+                onFilterModelChange={onFilterModelChange}
+                autoHeight
+            />
         </List>
     );
 };
