20a023fb52fd5f4074c8521fce3d88a505b04248
Ali Emir Sen <senaliemir@gmail.com>
2022-06-02 11:25:28 +0300

    refactor: flip auth provider interface with context

--- packages/core/src/contexts/auth/IAuthContext.ts
+++ packages/core/src/contexts/auth/IAuthContext.ts
@@ -1,23 +1,16 @@
 export type TLogoutData = void | false | string;
 export type TLoginData = void | false | string;
 
-export interface IAuthContext {
-    login?: (params: any) => Promise<TLoginData>;
-    logout?: (params: any) => Promise<TLogoutData>;
-    checkAuth?: (params?: any) => Promise<void>;
-    checkError?: (error: any) => Promise<void>;
-    getPermissions?: (params?: any) => Promise<any>;
+export interface AuthProvider {
+    login: (params: any) => Promise<TLoginData>;
+    logout: (params: any) => Promise<TLogoutData>;
+    checkAuth: (params?: any) => Promise<void>;
+    checkError: (error: any) => Promise<void>;
+    getPermissions: (params?: any) => Promise<any>;
     getUserIdentity?: () => Promise<any>;
+}
+
+export interface IAuthContext extends Partial<AuthProvider> {
     isProvided?: boolean;
     isAuthenticated?: boolean;
 }
-
-export type AuthProvider = Partial<
-    Omit<IAuthContext, "isProvided" | "isAuthenticated">
-> &
-    Required<
-        Pick<
-            IAuthContext,
-            "login" | "logout" | "checkAuth" | "checkError" | "getPermissions"
-        >
-    >;
