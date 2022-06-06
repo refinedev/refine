89d068a2328144a7cb129aa66648d1754e56b6fd
Salih <salihozdemir94@gmail.com>
2022-06-06 12:02:58 +0300

    feat: duplicate useForm example for useModalForm example

---
+++ examples/reactHookForm/useModalForm/src/pages/posts/list.tsx
@@ -0,0 +1,45 @@
+import { useTable, useNavigation } from "@pankod/refine-core";
+
+import { IPost } from "interfaces";
+
+export const PostList: React.FC = () => {
+    const { tableQueryResult } = useTable<IPost>({
+        initialSorter: [
+            {
+                field: "id",
+                order: "desc",
+            },
+        ],
+    });
+    const { edit, create } = useNavigation();
+
+    return (
+        <div>
+            <button onClick={() => create("posts")}>Create Post</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableQueryResult.data?.data.map((post) => (
+                        <tr key={post.id}>
+                            <td>{post.id}</td>
+                            <td>{post.title}</td>
+                            <td>
+                                <button onClick={() => edit("posts", post.id)}>
+                                    Edit
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
