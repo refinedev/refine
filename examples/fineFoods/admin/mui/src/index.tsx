8447f526f2db3bd9071da2c0fa33c49ff28c4382
Ali Emir Sen <senaliemir@gmail.com>
2022-06-08 11:25:40 +0300

    refactor: update mui-fine-foods notistack with lazy initialization

--- examples/fineFoods/admin/mui/src/index.tsx
+++ examples/fineFoods/admin/mui/src/index.tsx
@@ -1,38 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { SnackbarProvider } from "notistack";
 import "i18n";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { styled } from "@pankod/refine-mui";
 
 import App from "./App";
-import { ColorModeContextProvider } from "contexts";
 dayjs.extend(relativeTime);
-
-const StyledSnackbarProvider = styled(SnackbarProvider)`
-    &.SnackbarItem-contentRoot {
-        background-color: ${(props) => props.theme.palette.background.default};
-        color: ${(props) => props.theme.palette.primary.main};
-    }
-    &.SnackbarItem-variantSuccess {
-        background-color: ${(props) => props.theme.palette.primary.main};
-        color: ${(props) => props.theme.palette.primary.contrastText};
-    }
-    &.SnackbarItem-variantError {
-        background-color: ${(props) => props.theme.palette.error.main};
-        color: ${(props) => props.theme.palette.common.white};
-    }
-`;
 
 ReactDOM.render(
     <React.StrictMode>
         <React.Suspense fallback="loading">
-            <ColorModeContextProvider>
-                <StyledSnackbarProvider>
-                    <App />
-                </StyledSnackbarProvider>
-            </ColorModeContextProvider>
+            <App />
         </React.Suspense>
     </React.StrictMode>,
     document.getElementById("root"),
