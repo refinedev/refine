c0bb39412aaeb839e15614eaaf8ed9657c95a750
Salih <salihozdemir94@gmail.com>
2022-06-08 17:29:28 +0300

    feat: add theme to the example

--- examples/field/useSelect/mui/src/App.tsx
+++ examples/field/useSelect/mui/src/App.tsx
@@ -4,30 +4,34 @@ import {
     LoginPage,
     ErrorComponent,
     ReadyPage,
+    LightTheme,
 } from "@pankod/refine-mui";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
 
 import { PostsList, PostCreate, PostEdit } from "pages/posts";
+import { ThemeProvider } from "@mui/system";
 
 const App: React.FC = () => {
     return (
-        <Refine
-            routerProvider={routerProvider}
-            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-            ReadyPage={ReadyPage}
-            Layout={Layout}
-            LoginPage={LoginPage}
-            catchAll={<ErrorComponent />}
-            resources={[
-                {
-                    name: "posts",
-                    list: PostsList,
-                    create: PostCreate,
-                    edit: PostEdit,
-                },
-            ]}
-        />
+        <ThemeProvider theme={LightTheme}>
+            <Refine
+                routerProvider={routerProvider}
+                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                ReadyPage={ReadyPage}
+                Layout={Layout}
+                LoginPage={LoginPage}
+                catchAll={<ErrorComponent />}
+                resources={[
+                    {
+                        name: "posts",
+                        list: PostsList,
+                        create: PostCreate,
+                        edit: PostEdit,
+                    },
+                ]}
+            />
+        </ThemeProvider>
     );
 };
 
