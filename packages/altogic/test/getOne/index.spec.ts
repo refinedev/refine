ba95defeeab22b6bf2f461f50943e9b198d05970
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-06-08 17:12:04 +0300

    refactor: set header for new axios version

--- packages/altogic/test/getOne/index.spec.ts
+++ packages/altogic/test/getOne/index.spec.ts
@@ -9,9 +9,7 @@ const YOUR_SECRET_API_KEY =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZJZCI6IjYxMzczZGVkMjQ5NWMzMDAxOTliZTAxNiIsImtleUlkIjoiNjEzNzNlMzYyNDk1YzMwMDE5OWJlMDJkIiwiaWF0IjoxNjMxMDEwMzU4LCJleHAiOjI0OTUwMTAzNTh9.2fL28Bzd97mqfAvcsTrYj1mZ_hqf3WRnr2DOtV3lsc0";
 
 const axiosInstance = axios.create();
-axiosInstance.defaults.headers = {
-    Authorization: YOUR_SECRET_API_KEY,
-};
+axiosInstance.defaults.headers.common["Authorization"] = YOUR_SECRET_API_KEY;
 
 describe("getOne", () => {
     it("correct response", async () => {
