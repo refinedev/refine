ca05f866f084b4204be22a4f72d6c92b2587d761
Salih <salihozdemir94@gmail.com>
2022-06-09 13:27:43 +0300

    feat: add notificationProvider

--- examples/field/useSelect/mui/src/App.tsx
+++ examples/field/useSelect/mui/src/App.tsx
@@ -6,6 +6,8 @@ import {
     ReadyPage,
     LightTheme,
     ThemeProvider,
+    notificationProviderHandle,
+    SnackbarProvider,
 } from "@pankod/refine-mui";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
@@ -15,22 +17,27 @@ import { PostsList, PostCreate, PostEdit } from "pages/posts";
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={LightTheme}>
-            <Refine
-                routerProvider={routerProvider}
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-                ReadyPage={ReadyPage}
-                Layout={Layout}
-                LoginPage={LoginPage}
-                catchAll={<ErrorComponent />}
-                resources={[
-                    {
-                        name: "posts",
-                        list: PostsList,
-                        create: PostCreate,
-                        edit: PostEdit,
-                    },
-                ]}
-            />
+            <SnackbarProvider>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(
+                        "https://api.fake-rest.refine.dev",
+                    )}
+                    notificationProvider={notificationProviderHandle}
+                    ReadyPage={ReadyPage}
+                    Layout={Layout}
+                    LoginPage={LoginPage}
+                    catchAll={<ErrorComponent />}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: PostsList,
+                            create: PostCreate,
+                            edit: PostEdit,
+                        },
+                    ]}
+                />
+            </SnackbarProvider>
         </ThemeProvider>
     );
 };
