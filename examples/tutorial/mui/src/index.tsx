1a16f91eec92693c418f63bc8134f72fab83ee0f
Aydın Akyol <aydinakyol11@gmail.com>
2022-06-09 15:19:07 +0300

    fix: remove notification

--- examples/tutorial/mui/src/index.tsx
+++ examples/tutorial/mui/src/index.tsx
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { SnackbarProvider } from "notistack";
-import { styled } from "@pankod/refine-mui";
 
 import App from "./App";
 
-const StyledSnackbarProvider = styled(SnackbarProvider)`
-    &.SnackbarItem-contentRoot {
-        background-color: ${(props) => props.theme.palette.background.default};
-        color: ${(props) => props.theme.palette.primary.main};
-    }
-    &.SnackbarItem-variantSuccess {
-        background-color: ${(props) => props.theme.palette.primary.main};
-        color: ${(props) => props.theme.palette.primary.contrastText};
-    }
-    &.SnackbarItem-variantError {
-        background-color: ${(props) => props.theme.palette.error.main};
-        color: ${(props) => props.theme.palette.common.white};
-    }
-`;
-
 ReactDOM.render(
     <React.StrictMode>
-        <StyledSnackbarProvider>
-            <App />
-        </StyledSnackbarProvider>
+        <App />
     </React.StrictMode>,
     document.getElementById("root"),
 );
