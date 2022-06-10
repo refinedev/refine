773c2cff243ce24f6b716041f7ca64fd2938d547
Ali Emir Sen <senaliemir@gmail.com>
2022-06-10 17:20:52 +0300

    refactor: replace greys with dark background color

--- packages/mui/src/components/layout/index.tsx
+++ packages/mui/src/components/layout/index.tsx
@@ -33,10 +33,7 @@ export const Layout: React.FC<LayoutProps> = ({
                     sx={{
                         p: { xs: 1, md: 2, lg: 3 },
                         flexGrow: 1,
-                        bgcolor: (theme) =>
-                            theme.palette.mode === "light"
-                                ? theme.palette.grey[100]
-                                : theme.palette.grey[900],
+                        bgcolor: (theme) => theme.palette.background.default,
                     }}
                 >
                     {children}
