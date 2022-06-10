91061635327f7b0d73ba81cb99cb0ce7c2394bee
Salih <salihozdemir94@gmail.com>
2022-06-10 10:45:50 +0300

    feat: add category column to the List page

--- examples/form/mui/useStepsForm/src/pages/posts/list.tsx
+++ examples/form/mui/useStepsForm/src/pages/posts/list.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useOne } from "@pankod/refine-core";
 import {
     useDataGrid,
     DataGrid,
@@ -6,50 +8,66 @@ import {
     EditButton,
 } from "@pankod/refine-mui";
 
-import { IPost } from "interfaces";
-
-const columns: GridColumns = [
-    {
-        field: "id",
-        headerName: "ID",
-        type: "number",
-    },
-    { field: "title", headerName: "Title", minWidth: 500, flex: 1 },
-    { field: "status", headerName: "Status", minWidth: 120, flex: 0.5 },
-    {
-        field: "actions",
-        headerName: "Actions",
-        // eslint-disable-next-line react/display-name
-        renderCell: ({ row }) => (
-            <EditButton
-                svgIconProps={{ fontSize: "small" }}
-                variant="outlined"
-                size="small"
-                hideText
-                recordItemId={row.id}
-                sx={{
-                    minWidth: "24px",
-                }}
-            />
-        ),
-        align: "center",
-        headerAlign: "center",
-        minWidth: 80,
-    },
-];
+import { ICategory, IPost } from "interfaces";
 
 export const PostsList: React.FC = () => {
+    const columns = React.useMemo<GridColumns<IPost>>(
+        () => [
+            {
+                field: "id",
+                headerName: "ID",
+                type: "number",
+                width: 50,
+            },
+            { field: "title", headerName: "Title", minWidth: 400, flex: 1 },
+            {
+                field: "category.id",
+                headerName: "Category",
+                type: "number",
+                headerAlign: "left",
+                align: "left",
+                minWidth: 250,
+                flex: 0.5,
+                valueGetter: ({ row }) => {
+                    const { data } = useOne<ICategory>({
+                        resource: "categories",
+                        id: row.category.id,
+                    });
+                    return data?.data.title;
+                },
+            },
+            { field: "status", headerName: "Status", minWidth: 120, flex: 0.3 },
+            {
+                field: "actions",
+                headerName: "Actions",
+                // eslint-disable-next-line react/display-name
+                renderCell: ({ row }) => (
+                    <EditButton
+                        svgIconProps={{ fontSize: "small" }}
+                        variant="outlined"
+                        size="small"
+                        hideText
+                        recordItemId={row.id}
+                        sx={{
+                            minWidth: "24px",
+                        }}
+                    />
+                ),
+                align: "center",
+                headerAlign: "center",
+                minWidth: 80,
+            },
+        ],
+        [],
+    );
+
     const { dataGridProps } = useDataGrid<IPost>({
         columns,
     });
 
     return (
         <List>
-            <DataGrid
-                {...dataGridProps}
-                autoHeight
-                rowsPerPageOptions={[10, 20, 30, 50, 100]}
-            />
+            <DataGrid {...dataGridProps} autoHeight />
         </List>
     );
 };
