e4810d9cd70833bb74f11c9effe6de3ce8e33104
Salih <salihozdemir94@gmail.com>
2022-06-14 14:48:58 +0300

    docs: update demo page

--- documentation/src/pages/demo.js
+++ documentation/src/pages/demo.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import SplitPane from "react-split-pane";
+import clsx from "clsx";
 
 import { IoMdOpen, IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
+const iframeURLS = {
+    antd: "https://example.admin.refine.dev",
+    mui: "https://example.admin.refine.dev",
+};
+
 const Hello = () => {
+    const [activeKeyPane1, setActiveKeyPane1] = useState("headless");
+    const [activeKeyPane2, setActiveKeyPane2] = useState("antd");
+
     return (
         <Layout title="One framework for all">
             <div className="desktop-view">
@@ -22,16 +31,29 @@ const Hello = () => {
                     style={{ height: "auto" }}
                 >
                     <div className="pane">
-                        <Link
-                            className="link-button"
-                            to="https://example.refine.dev"
-                        >
-                            Refine Headless <br />
-                            Refine Core + Next.js (SSR) + Tailwind CSS
-                            <IoMdOpen />
-                        </Link>
                         <div className="toggle toggle-left">
                             <IoIosArrowBack />
+                        </div>
+                        <div className="pane-header pane-header-left">
+                            <h2>Customer Facing App</h2>
+                            <p>
+                                Refine Headless &gt; Refine Core + Next.js (SSR)
+                                + Tailwind CSS
+                            </p>
+                        </div>
+                        <div className="tabs">
+                            <button
+                                onClick={() => setActiveKeyPane1("headless")}
+                                className={clsx(
+                                    "tab-button",
+                                    activeKeyPane1 === "headless" && "active",
+                                )}
+                            >
+                                Headless
+                                <Link to="https://example.refine.dev">
+                                    <IoMdOpen />
+                                </Link>
+                            </button>
                         </div>
                         <iframe
                             src="https://example.refine.dev"
@@ -40,20 +62,41 @@ const Hello = () => {
                         ></iframe>
                     </div>
                     <div className="pane">
-                        <Link
-                            className="link-button"
-                            to="https://example.admin.refine.dev"
-                        >
-                            Refine Antd
-                            <br />
-                            Refine Core + Refine Ant Design
-                            <IoMdOpen />
-                        </Link>
                         <div className="toggle toggle-right">
                             <IoIosArrowForward />
                         </div>
+                        <div className="pane-header pane-header-right">
+                            <h2>Admin Panel</h2>
+                            <p>Refine Core + Antd Design / Material UI</p>
+                        </div>
+                        <div className="tabs">
+                            <button
+                                onClick={() => setActiveKeyPane2("antd")}
+                                className={clsx(
+                                    "tab-button",
+                                    activeKeyPane2 === "antd" && "active",
+                                )}
+                            >
+                                Antd Design
+                                <Link to="https://example.admin.refine.dev">
+                                    <IoMdOpen />
+                                </Link>
+                            </button>
+                            <button
+                                onClick={() => setActiveKeyPane2("mui")}
+                                className={clsx(
+                                    "tab-button",
+                                    activeKeyPane2 === "mui" && "active",
+                                )}
+                            >
+                                Material UI
+                                <Link to="https://example.admin.refine.dev">
+                                    <IoMdOpen />
+                                </Link>
+                            </button>
+                        </div>
                         <iframe
-                            src="https://example.admin.refine.dev"
+                            src={iframeURLS[activeKeyPane2]}
                             width="100%"
                             height="100%"
                         ></iframe>
