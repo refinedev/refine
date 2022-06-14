770bab01b62f154ce0e4e4dabc6ad3021d014f9d
Ali Emir Sen <senaliemir@gmail.com>
2022-06-14 16:08:17 +0300

    feat: extend `useMenu` hook for `refine-antd`

--- packages/antd/src/components/layout/sider/index.tsx
+++ packages/antd/src/components/layout/sider/index.tsx
@@ -1,14 +1,11 @@
-import React, { ComponentProps, useEffect, useState } from "react";
+import React, { ComponentProps, useState } from "react";
 import { Layout, Menu, Grid } from "antd";
-import { LogoutOutlined, UnorderedListOutlined } from "@ant-design/icons";
+import { LogoutOutlined } from "@ant-design/icons";
 import {
     useTranslate,
     useLogout,
     useTitle,
-    ITreeMenu,
     useIsExistAuthentication,
-    useCanWithoutCache,
-    useRouterContext,
 } from "@pankod/refine-core";
 
 import { Title as DefaultTitle } from "@components";
@@ -22,97 +19,16 @@ type ItemType = NonNullable<ComponentProps<typeof Menu>["items"]>[number];
 export const Sider: React.FC = () => {
     const [collapsed, setCollapsed] = useState<boolean>(false);
     const isExistAuthentication = useIsExistAuthentication();
-    const { Link } = useRouterContext();
     const { mutate: logout } = useLogout();
     const Title = useTitle();
     const translate = useTranslate();
     const { menuItems, selectedKey, defaultOpenKeys } = useMenu();
+
     const breakpoint = Grid.useBreakpoint();
-    const { can: checkCanAccess } = useCanWithoutCache();
-    const [renderedMenuItems, setRenderedMenuItems] = React.useState<
-        Array<ItemType>
-    >([]);
 
     const isMobile = !breakpoint.lg;
 
     const RenderToTitle = Title ?? DefaultTitle;
-
-    const handleTreeMenuItem = React.useCallback(
-        async (
-            item: ITreeMenu,
-            selectedKey?: string,
-        ): Promise<ItemType | undefined> => {
-            if (item.children?.length > 0) {
-                const handledChildren = await Promise.all(
-                    item.children.map((child) =>
-                        handleTreeMenuItem(child, selectedKey),
-                    ),
-                );
-                // is sub
-                if (handledChildren.length > 0) {
-                    return {
-                        key: item.name,
-                        label: item.label,
-                        icon: item.icon ?? <UnorderedListOutlined />,
-                        title: item.label,
-                        children: handledChildren,
-                    } as ItemType;
-                }
-
-                return undefined;
-            } else {
-                const isSelected = item.route === selectedKey;
-                const isRoute = !(
-                    item.parentName !== undefined && item.children.length === 0
-                );
-
-                const { can: hasAccess } = checkCanAccess
-                    ? await checkCanAccess({
-                          resource: item.name,
-                          action: "list",
-                      })
-                    : { can: true };
-                // is leaf
-                if (hasAccess) {
-                    const LabelWrapper = item.route
-                        ? Link
-                        : (React.Fragment as typeof Link);
-                    return {
-                        key: item.name,
-                        label: (
-                            <LabelWrapper href={item.route} to={item.route}>
-                                {item.label}
-                                {!collapsed && isSelected && (
-                                    <div className="ant-menu-tree-arrow" />
-                                )}
-                            </LabelWrapper>
-                        ),
-                        icon:
-                            item.icon ?? (isRoute && <UnorderedListOutlined />),
-                        style: isSelected ? { fontWeight: "bold" } : undefined,
-                    } as ItemType;
-                }
-                return undefined;
-            }
-        },
-        [collapsed, checkCanAccess],
-    );
-
-    const renderTree = React.useCallback(
-        async (items: ITreeMenu[], selected: string) => {
-            const promises = items.map((item) =>
-                handleTreeMenuItem(item, selected),
-            );
-            setRenderedMenuItems(
-                (await Promise.all(promises)).filter(Boolean) as ItemType[],
-            );
-        },
-        [],
-    );
-
-    useEffect(() => {
-        renderTree(menuItems, selectedKey);
-    }, [menuItems, selectedKey]);
 
     return (
         <Layout.Sider
@@ -134,7 +50,7 @@ export const Sider: React.FC = () => {
                     }
                 }}
                 items={[
-                    ...renderedMenuItems,
+                    ...menuItems,
                     ...(isExistAuthentication
                         ? [
                               {
--- packages/antd/src/hooks/useMenu/index.tsx
+++ packages/antd/src/hooks/useMenu/index.tsx
@@ -1,20 +1,77 @@
-import React from "react";
-import { DashboardOutlined } from "@ant-design/icons";
+import React, { ComponentProps, useEffect } from "react";
+import { Menu } from "antd";
 import {
-    useRefineContext,
-    useTranslate,
-    useResource,
-    useRouterContext,
-    userFriendlyResourceName,
-    createTreeView,
     ITreeMenu,
+    useCanWithoutCache,
+    useMenu as useMenuCore,
+    useRouterContext,
 } from "@pankod/refine-core";
-import { IMenuItem } from "../../interfaces";
+import { UnorderedListOutlined } from "@ant-design/icons";
+
+type ItemType = NonNullable<ComponentProps<typeof Menu>["items"]>[number];
 
 type useMenuReturnType = {
     defaultOpenKeys: string[];
     selectedKey: string;
-    menuItems: ITreeMenu[];
+    menuItems: ItemType[];
+};
+
+type useMenuConfigType = {
+    collapsed?: boolean;
+    handleSubMenuItem?: (context: {
+        item: ITreeMenu;
+        children: (ItemType | undefined)[];
+    }) => ItemType | undefined;
+    handleMenuItem?: (context: {
+        item: ITreeMenu;
+        collapsed: boolean;
+        Link: React.FC<any>;
+        selectedKey?: string;
+    }) => ItemType | undefined;
+    DefaultSubMenuIcon?: React.ComponentType;
+    DefaultItemIcon?: React.ComponentType;
+};
+
+const defaultUseMenuConfig: useMenuConfigType = {
+    collapsed: false,
+    handleSubMenuItem: ({ item, children }) => {
+        if (children.length > 0) {
+            return {
+                key: item.name,
+                label: item.label,
+                icon: item.icon ?? <UnorderedListOutlined />,
+                title: item.label,
+                children: children,
+            } as ItemType;
+        }
+
+        return undefined;
+    },
+    handleMenuItem: ({ item, ...context }) => {
+        const isSelected = item.route === context.selectedKey;
+        const isRoute = !(
+            item.parentName !== undefined && item.children.length === 0
+        );
+
+        const LabelWrapper = item.route
+            ? context.Link
+            : (React.Fragment as typeof context.Link);
+        return {
+            key: item.name,
+            label: (
+                <LabelWrapper href={item.route} to={item.route}>
+                    {item.label}
+                    {!context.collapsed && isSelected && (
+                        <div className="ant-menu-tree-arrow" />
+                    )}
+                </LabelWrapper>
+            ),
+            icon: item.icon ?? (isRoute && <UnorderedListOutlined />),
+            style: isSelected ? { fontWeight: "bold" } : undefined,
+        } as ItemType;
+    },
+    DefaultSubMenuIcon: UnorderedListOutlined,
+    DefaultItemIcon: UnorderedListOutlined,
 };
 
 /**
@@ -25,87 +82,85 @@ type useMenuReturnType = {
  *
  * @see {@link https://refine.dev/docs/ui-frameworks/antd/hooks/resource/useMenu} for more details.
  */
-export const useMenu: () => useMenuReturnType = () => {
-    const { resources } = useResource();
-    const translate = useTranslate();
+export const useMenu: (config?: useMenuConfigType) => useMenuReturnType = ({
+    collapsed,
+    handleMenuItem,
+    handleSubMenuItem,
+    DefaultSubMenuIcon,
+    DefaultItemIcon,
+} = defaultUseMenuConfig) => {
+    const { defaultOpenKeys, menuItems, selectedKey } = useMenuCore();
+    const { Link } = useRouterContext();
+    const { can: checkCanAccess } = useCanWithoutCache();
+    const [renderedMenuItems, setRenderedMenuItems] = React.useState<
+        Array<ItemType>
+    >([]);
 
-    const { useLocation, useParams } = useRouterContext();
-    const location = useLocation();
-    const params = useParams<{ resource: string }>();
-
-    const { hasDashboard } = useRefineContext();
-
-    const selectedKey = React.useMemo(() => {
-        let selectedResource = resources.find(
-            (el) => location?.pathname === `/${el.route}`,
-        );
-
-        if (!selectedResource) {
-            selectedResource = resources.find(
-                (el) => params?.resource === (el.route as string),
-            );
-        }
-
-        let _selectedKey: string;
-        if (selectedResource?.route) {
-            _selectedKey = `/${selectedResource?.route}`;
-        } else if (location.pathname === "/") {
-            _selectedKey = "/";
-        } else {
-            _selectedKey = location?.pathname;
-        }
-        return _selectedKey;
-    }, [resources, location, params]);
-
-    const treeMenuItems: IMenuItem[] = React.useMemo(
-        () => [
-            ...(hasDashboard
-                ? [
-                      {
-                          name: "Dashboard",
-                          icon: <DashboardOutlined />,
-                          route: `/`,
-                          key: "dashboard",
-                          label: translate("dashboard.title", "Dashboard"),
-                      },
-                  ]
-                : []),
-            ...resources.map((resource) => {
-                const route = `/${resource.route}`;
-
-                return {
-                    ...resource,
-                    icon: resource.icon,
-                    route: route,
-                    key: resource.key ?? route,
-                    label:
-                        resource.label ??
-                        translate(
-                            `${resource.name}.${resource.name}`,
-                            userFriendlyResourceName(resource.name, "plural"),
-                        ),
-                };
-            }),
+    const handleTreeMenuItem = React.useCallback(
+        async (
+            item: ITreeMenu,
+            selectedKey?: string,
+        ): Promise<ItemType | undefined> => {
+            if (item.children?.length > 0) {
+                const handledChildren = await Promise.all(
+                    item.children.map((child) =>
+                        handleTreeMenuItem(child, selectedKey),
+                    ),
+                );
+                // is sub
+                return handleSubMenuItem?.({ item, children: handledChildren });
+            } else {
+                const { can: hasAccess } = checkCanAccess
+                    ? await checkCanAccess({
+                          resource: item.name,
+                          action: "list",
+                      })
+                    : { can: true };
+                // is leaf
+                if (hasAccess) {
+                    return handleMenuItem?.({
+                        item,
+                        collapsed: Boolean(collapsed),
+                        Link,
+                        selectedKey,
+                    });
+                }
+                return undefined;
+            }
+        },
+        [
+            collapsed,
+            checkCanAccess,
+            handleMenuItem,
+            handleSubMenuItem,
+            DefaultItemIcon,
+            DefaultSubMenuIcon,
         ],
-        [resources, hasDashboard],
-    );
-    const menuItems: ITreeMenu[] = React.useMemo(
-        () => createTreeView(treeMenuItems),
-        [treeMenuItems],
     );
 
-    const defaultOpenKeys = React.useMemo(
-        () => selectedKey.split("/").filter((x) => x !== ""),
-        [selectedKey],
+    const renderTree = React.useCallback(
+        async (items: ITreeMenu[], selected: string) => {
+            const promises = items.map((item) =>
+                handleTreeMenuItem(item, selected),
+            );
+            setRenderedMenuItems(
+                (await Promise.all(promises)).filter(Boolean) as ItemType[],
+            );
+        },
+        [handleTreeMenuItem],
     );
+
+    useEffect(() => {
+        renderTree(menuItems, selectedKey);
+    }, [renderTree, menuItems, selectedKey]);
 
     const values = React.useMemo(() => {
         return {
             defaultOpenKeys,
             selectedKey,
-            menuItems,
+            menuItems: renderedMenuItems,
         };
-    }, [defaultOpenKeys, selectedKey, menuItems]);
+    }, [defaultOpenKeys, selectedKey, renderedMenuItems]);
 
     return values;
 };
