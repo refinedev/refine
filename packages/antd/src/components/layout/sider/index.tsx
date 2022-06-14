4bd395cf6f99d6e5be0b0dbdd099813785057ea1
Ali Emir Sen <senaliemir@gmail.com>
2022-06-14 11:10:11 +0300

    refactor: fix naming

--- packages/antd/src/components/layout/sider/index.tsx
+++ packages/antd/src/components/layout/sider/index.tsx
@@ -28,80 +28,82 @@ export const Sider: React.FC = () => {
     const translate = useTranslate();
     const { menuItems, selectedKey, defaultOpenKeys } = useMenu();
     const breakpoint = Grid.useBreakpoint();
-    const { can: checkCan } = useCanWithoutCache();
-    const [renderedItems, setRenderedItems] = React.useState<Array<ItemType>>(
-        [],
-    );
+    const { can: checkCanAccess } = useCanWithoutCache();
+    const [renderedMenuItems, setRenderedMenuItems] = React.useState<
+        Array<ItemType>
+    >([]);
 
     const isMobile = !breakpoint.lg;
 
     const RenderToTitle = Title ?? DefaultTitle;
 
-    const handleMenuItem = async (
-        item: ITreeMenu,
-        selectedKey?: string,
-    ): Promise<ItemType | undefined> => {
-        if (item.children?.length > 0) {
-            const handledChildren = await Promise.all(
-                item.children.map((child) =>
-                    handleMenuItem(child, selectedKey),
-                ),
-            );
-            // is sub
-            if (handledChildren.length > 0) {
-                return {
-                    key: item.name,
-                    label: item.label,
-                    icon: item.icon ?? <UnorderedListOutlined />,
-                    title: item.label,
-                    children: handledChildren,
-                } as ItemType;
-            }
-
-            return undefined;
-        } else {
-            const isSelected = item.route === selectedKey;
-            const isRoute = !(
-                item.parentName !== undefined && item.children.length === 0
-            );
-
-            const { can: hasAccess } = checkCan
-                ? await checkCan({
-                      resource: item.name,
-                      action: "list",
-                  })
-                : { can: true };
-            // is leaf
-            if (hasAccess) {
-                return {
-                    key: item.name,
-                    label: (
-                        <>
-                            {item.label}{" "}
-                            {!collapsed && isSelected && (
-                                <div className="ant-menu-tree-arrow" />
-                            )}
-                        </>
+    const handleTreeMenuItem = React.useCallback(
+        async (
+            item: ITreeMenu,
+            selectedKey?: string,
+        ): Promise<ItemType | undefined> => {
+            if (item.children?.length > 0) {
+                const handledChildren = await Promise.all(
+                    item.children.map((child) =>
+                        handleTreeMenuItem(child, selectedKey),
                     ),
-                    icon: item.icon ?? (isRoute && <UnorderedListOutlined />),
-                    route: item.route,
-                    onClick: () => push(item.route ?? "/"),
-                    style: isSelected ? { fontWeight: "bold" } : undefined,
-                } as ItemType;
+                );
+                // is sub
+                if (handledChildren.length > 0) {
+                    return {
+                        key: item.name,
+                        label: item.label,
+                        icon: item.icon ?? <UnorderedListOutlined />,
+                        title: item.label,
+                        children: handledChildren,
+                    } as ItemType;
+                }
+
+                return undefined;
+            } else {
+                const isSelected = item.route === selectedKey;
+                const isRoute = !(
+                    item.parentName !== undefined && item.children.length === 0
+                );
+
+                const { can: hasAccess } = checkCanAccess
+                    ? await checkCanAccess({
+                          resource: item.name,
+                          action: "list",
+                      })
+                    : { can: true };
+                // is leaf
+                if (hasAccess) {
+                    return {
+                        key: item.name,
+                        label: (
+                            <>
+                                {item.label}{" "}
+                                {!collapsed && isSelected && (
+                                    <div className="ant-menu-tree-arrow" />
+                                )}
+                            </>
+                        ),
+                        icon:
+                            item.icon ?? (isRoute && <UnorderedListOutlined />),
+                        route: item.route,
+                        onClick: () => push(item.route ?? "/"),
+                        style: isSelected ? { fontWeight: "bold" } : undefined,
+                    } as ItemType;
+                }
+                return undefined;
             }
-            return undefined;
-        }
-    };
+        },
+        [collapsed, checkCanAccess],
+    );
 
     const renderTree = React.useCallback(
         async (items: ITreeMenu[], selected: string) => {
             const promises = items.map((item) =>
-                handleMenuItem(item, selected),
+                handleTreeMenuItem(item, selected),
             );
-            setRenderedItems(
-                (await Promise.all(promises)).filter(
-                    (el) => typeof el !== "undefined",
-                ) as ItemType[],
+            setRenderedMenuItems(
+                (await Promise.all(promises)).filter(Boolean) as ItemType[],
             );
         },
         [],
@@ -131,7 +133,7 @@ export const Sider: React.FC = () => {
                     }
                 }}
                 items={[
-                    ...renderedItems,
+                    ...renderedMenuItems,
                     ...(isExistAuthentication
                         ? [
                               {
