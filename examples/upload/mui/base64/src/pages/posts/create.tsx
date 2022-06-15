6a931c1daa0f2b879c52137161f34c8aafd49000
Salih <salihozdemir94@gmail.com>
2022-06-15 12:37:45 +0300

    feat: add base64 upload to edit and create page

--- examples/upload/mui/base64/src/pages/posts/create.tsx
+++ examples/upload/mui/base64/src/pages/posts/create.tsx
@@ -1,25 +1,73 @@
+import { useState } from "react";
 import {
     Create,
     Box,
     TextField,
     Autocomplete,
     useAutocomplete,
+    Input,
+    Stack,
+    Typography,
 } from "@pankod/refine-mui";
+import { LoadingButton } from "@mui/lab";
 import { Controller, useForm } from "@pankod/refine-react-hook-form";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 
 import { ICategory } from "interfaces";
 
 export const PostCreate: React.FC = () => {
+    const [isUploadLoading, setIsUploadLoading] = useState(false);
+
     const {
         saveButtonProps,
         register,
         control,
         formState: { errors },
+        setValue,
+        setError,
+        watch,
     } = useForm();
 
     const { autocompleteProps } = useAutocomplete<ICategory>({
         resource: "categories",
     });
+
+    const imageInput = watch("images");
+
+    const convertBase64 = (file: File) => {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
+
+    const onChangeHandler = async (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        try {
+            setIsUploadLoading(true);
+
+            const target = event.target;
+            const file: File = (target.files as FileList)[0];
+
+            const base64 = await convertBase64(file);
+
+            setValue("images", base64, { shouldValidate: true });
+
+            setIsUploadLoading(false);
+        } catch (error) {
+            setError("images", { message: "Upload failed. Please try again." });
+            setIsUploadLoading(false);
+        }
+    };
 
     return (
         <Create saveButtonProps={saveButtonProps}>
@@ -116,6 +164,54 @@ export const PostCreate: React.FC = () => {
                     multiline
                     rows={4}
                 />
+                <Stack
+                    direction="row"
+                    gap={4}
+                    flexWrap="wrap"
+                    sx={{ marginTop: "16px" }}
+                >
+                    <label htmlFor="images-input">
+                        <Input
+                            id="images-input"
+                            type="file"
+                            sx={{ display: "none" }}
+                            onChange={onChangeHandler}
+                        />
+                        <input
+                            id="file"
+                            {...register("images", {
+                                required: "This field is required",
+                            })}
+                            type="hidden"
+                        />
+                        <LoadingButton
+                            loading={isUploadLoading}
+                            loadingPosition="end"
+                            endIcon={<FileUploadIcon />}
+                            variant="contained"
+                            component="span"
+                        >
+                            Upload
+                        </LoadingButton>
+                        <br />
+                        {errors.images && (
+                            <Typography variant="caption" color="#fa541c">
+                                {errors.images?.message}
+                            </Typography>
+                        )}
+                    </label>
+                    {imageInput && (
+                        <Box
+                            component="img"
+                            sx={{
+                                maxWidth: 250,
+                                maxHeight: 250,
+                            }}
+                            src={imageInput}
+                            alt="Post image"
+                        />
+                    )}
+                </Stack>
             </Box>
         </Create>
     );
