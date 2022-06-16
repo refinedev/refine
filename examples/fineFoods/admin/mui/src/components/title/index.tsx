fa7f18cf2226aedef2344207269bc01353471cf8
Ömer Faruk APLAK <omer@pankod.com>
2022-06-16 16:00:31 +0300

    feat: add link to title component

--- examples/fineFoods/admin/mui/src/components/title/index.tsx
+++ examples/fineFoods/admin/mui/src/components/title/index.tsx
@@ -1,3 +1,4 @@
+import { useRouterContext } from "@pankod/refine-core";
 import { BikeWhiteIcon } from "components/icons/bike-white";
 
 type TitleProps = {
@@ -5,20 +6,24 @@ type TitleProps = {
 };
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => {
+    const { Link } = useRouterContext();
+
     return (
-        <div
-            style={{
-                height: "72px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            {collapsed ? (
-                <BikeWhiteIcon style={{ color: "white" }} />
-            ) : (
-                <img src="/images/fine-foods.svg" alt="Finefood" />
-            )}
-        </div>
+        <Link to="/">
+            <div
+                style={{
+                    height: "72px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                {collapsed ? (
+                    <BikeWhiteIcon style={{ color: "white" }} />
+                ) : (
+                    <img src="/images/fine-foods.svg" alt="Finefood" />
+                )}
+            </div>
+        </Link>
     );
 };
