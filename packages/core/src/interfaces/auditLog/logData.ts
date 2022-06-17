33e7bce8561dc79d657a22c332ef18e435fa5194
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-06-17 16:14:30 +0300

    fix: add meta key on ILog interface

--- packages/core/src/interfaces/auditLog/logData.ts
+++ packages/core/src/interfaces/auditLog/logData.ts
@@ -4,12 +4,12 @@ export interface ILog<TData = any> {
     id: BaseKey;
     createdAt: string;
     author?: Record<number | string, any>;
-    user?: Record<number | string, any>;
     name?: string;
     data: TData;
     previousData: TData;
     resource: string;
     action: string;
+    meta?: Record<number | string, any>;
 }
 
 export type ILogData<TData = any> = ILog<TData>[];
