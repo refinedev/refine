1be3392b5ec031686b94164bcc6e03e445313cae
ozkalai <melihozkalayy@gmail.com>
2022-06-21 17:28:02 +0300

    chore(import-export-example): folder structure seperated and ThemeProvider implemented

--- examples/importExport/mui/src/App.tsx
+++ examples/importExport/mui/src/App.tsx
@@ -4,32 +4,37 @@ import {
     LoginPage,
     ErrorComponent,
     ReadyPage,
+    ThemeProvider,
+    LightTheme,
+    CssBaseline,
 } from "@pankod/refine-mui";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
 
 import { authProvider } from "authProvider";
-
 import { ImportList } from "pages/list";
 
 const API_URL = "https://api.fake-rest.refine.dev";
 const App: React.FC = () => {
     return (
-        <Refine
-            authProvider={authProvider}
-            routerProvider={routerProvider}
-            dataProvider={dataProvider(API_URL)}
-            ReadyPage={ReadyPage}
-            Layout={Layout}
-            LoginPage={LoginPage}
-            catchAll={<ErrorComponent />}
-            resources={[
-                {
-                    name: "posts",
-                    list: ImportList,
-                },
-            ]}
-        />
+        <ThemeProvider theme={LightTheme}>
+            <CssBaseline />
+            <Refine
+                authProvider={authProvider}
+                routerProvider={routerProvider}
+                dataProvider={dataProvider(API_URL)}
+                ReadyPage={ReadyPage}
+                Layout={Layout}
+                LoginPage={LoginPage}
+                catchAll={<ErrorComponent />}
+                resources={[
+                    {
+                        name: "posts",
+                        list: ImportList,
+                    },
+                ]}
+            />
+        </ThemeProvider>
     );
 };
 
