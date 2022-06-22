730e30499c0ae95dc334b1443d216d041a73beab
ozkalai <melihozkalayy@gmail.com>
2022-06-22 12:44:22 +0300

    chore(fields): tsdocs updated with related link

--- packages/mui/src/components/fields/email/index.tsx
+++ packages/mui/src/components/fields/email/index.tsx
@@ -10,6 +10,7 @@ export type EmailFieldProps = FieldProps<ReactNode> & LinkProps;
  * This field is used to display email values. It uses the {@link https://mui.com/material-ui/react-typography/#main-content `<Typography>` }
  * and {@link https://mui.com/material-ui/react-link/#main-content `<Link>`} components from Material UI.
  *
+ * @see {@link https://refine.dev/docs/ui-frameworks/mui/components/fields/email} for more details.
  */
 export const EmailField: React.FC<EmailFieldProps> = ({ value, ...rest }) => {
     return (
--- packages/mui/src/components/fields/markdown/index.tsx
+++ packages/mui/src/components/fields/markdown/index.tsx
@@ -9,6 +9,7 @@ import { FieldProps } from "src/interfaces/field";
 /**
  * This field lets you display markdown content. It supports {@link https://github.github.com/gfm/ GitHub Flavored Markdown}.
  *
+ * @see {@link https://refine.dev/docs/ui-frameworks/mui/components/fields/markdown} for more details.
  */
 export const MarkdownField: React.FC<FieldProps<string | undefined>> = ({
     value = "",
