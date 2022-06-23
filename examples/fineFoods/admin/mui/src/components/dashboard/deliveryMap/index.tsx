bd11877fcc0e3ed469eb05e2be11d84693262cb1
Salih <salihozdemir94@gmail.com>
2022-06-23 17:08:00 +0300

    fix: resize delivery map

--- examples/fineFoods/admin/mui/src/components/dashboard/deliveryMap/index.tsx
+++ examples/fineFoods/admin/mui/src/components/dashboard/deliveryMap/index.tsx
@@ -4,6 +4,7 @@ import GoogleMapReact from "google-map-react";
 import { MapMarker } from "components/mapMarker";
 import { LocationIcon, CourierIcon } from "components/icons";
 import { IOrder } from "interfaces";
+import { Box } from "@pankod/refine-mui";
 
 export const DeliveryMap: React.FC = () => {
     const { data: orderData } = useList<IOrder>({
@@ -33,42 +34,44 @@ export const DeliveryMap: React.FC = () => {
     const { show } = useNavigation();
 
     return (
-        <GoogleMapReact
-            bootstrapURLKeys={{
-                key: process.env.REACT_APP_MAP_ID,
-            }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-        >
-            {orderData?.data.map((order) => {
-                return (
-                    <MapMarker
-                        key={order.id}
-                        lat={order.adress.coordinate[0]}
-                        lng={order.adress.coordinate[1]}
-                    >
-                        <LocationIcon
-                            sx={{ width: "36px", height: "36px" }}
-                            onClick={() => show("orders", order.id)}
-                        />
-                    </MapMarker>
-                );
-            })}
+        <Box sx={{ height: "576px", width: "100%", position: "relative" }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: process.env.REACT_APP_MAP_ID,
+                }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+            >
+                {orderData?.data.map((order) => {
+                    return (
+                        <MapMarker
+                            key={order.id}
+                            lat={order.adress.coordinate[0]}
+                            lng={order.adress.coordinate[1]}
+                        >
+                            <LocationIcon
+                                sx={{ width: "36px", height: "36px" }}
+                                onClick={() => show("orders", order.id)}
+                            />
+                        </MapMarker>
+                    );
+                })}
 
-            {orderData?.data.map((order) => {
-                return (
-                    <MapMarker
-                        key={order.id}
-                        lat={order.store.address.coordinate[0]}
-                        lng={order.store.address.coordinate[1]}
-                    >
-                        <CourierIcon
-                            sx={{ width: "64px", height: "64px" }}
-                            onClick={() => show("orders", order.id)}
-                        />
-                    </MapMarker>
-                );
-            })}
-        </GoogleMapReact>
+                {orderData?.data.map((order) => {
+                    return (
+                        <MapMarker
+                            key={order.id}
+                            lat={order.store.address.coordinate[0]}
+                            lng={order.store.address.coordinate[1]}
+                        >
+                            <CourierIcon
+                                sx={{ width: "64px", height: "64px" }}
+                                onClick={() => show("orders", order.id)}
+                            />
+                        </MapMarker>
+                    );
+                })}
+            </GoogleMapReact>
+        </Box>
     );
 };
