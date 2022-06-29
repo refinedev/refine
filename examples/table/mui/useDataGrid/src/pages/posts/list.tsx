8698128f228c7b759855a5f1e4d8df29b81d5b79
Salih <salihozdemir94@gmail.com>
2022-06-29 11:54:27 +0300

    fix: update base `useDataGrid` example

--- examples/table/mui/useDataGrid/src/pages/posts/list.tsx
+++ examples/table/mui/useDataGrid/src/pages/posts/list.tsx
@@ -1,42 +1,11 @@
 import React from "react";
-import { useOne } from "@pankod/refine-core";
+import { useMany } from "@pankod/refine-core";
 import { useDataGrid, DataGrid, GridColumns, List } from "@pankod/refine-mui";
 
 import { ICategory, IPost } from "interfaces";
 
 export const PostsList: React.FC = () => {
-    const columns = React.useMemo<GridColumns<IPost>>(
-        () => [
-            {
-                field: "id",
-                headerName: "ID",
-                type: "number",
-                width: 50,
-            },
-            { field: "title", headerName: "Title", minWidth: 400, flex: 1 },
-            {
-                field: "category.id",
-                headerName: "Category",
-                type: "number",
-                headerAlign: "left",
-                align: "left",
-                minWidth: 250,
-                flex: 0.5,
-                valueGetter: ({ row }) => {
-                    const { data } = useOne<ICategory>({
-                        resource: "categories",
-                        id: row.category.id,
-                    });
-                    return data?.data.title;
-                },
-            },
-            { field: "status", headerName: "Status", minWidth: 120, flex: 0.3 },
-        ],
-        [],
-    );
-
     const { dataGridProps } = useDataGrid<IPost>({
-        columns,
         initialCurrent: 2,
         initialPageSize: 10,
         initialSorter: [
@@ -55,10 +24,53 @@ export const PostsList: React.FC = () => {
         syncWithLocation: true,
     });
 
+    const categoryIds = dataGridProps.rows.map((item) => item.category.id);
+    const { data: categoriesData, isLoading } = useMany<ICategory>({
+        resource: "categories",
+        ids: categoryIds,
+        queryOptions: {
+            enabled: categoryIds.length > 0,
+        },
+    });
+
+    const columns = React.useMemo<GridColumns<IPost>>(
+        () => [
+            {
+                field: "id",
+                headerName: "ID",
+                type: "number",
+                width: 50,
+            },
+            { field: "title", headerName: "Title", minWidth: 400, flex: 1 },
+            {
+                field: "category.id",
+                headerName: "Category",
+                type: "number",
+                headerAlign: "left",
+                align: "left",
+                minWidth: 250,
+                flex: 0.5,
+                renderCell: ({ row }) => {
+                    if (isLoading) {
+                        return "Loading...";
+                    }
+
+                    const category = categoriesData?.data.find(
+                        (item) => item.id === row.category.id,
+                    );
+                    return category?.title;
+                },
+            },
+            { field: "status", headerName: "Status", minWidth: 120, flex: 0.3 },
+        ],
+        [categoriesData, isLoading],
+    );
+
     return (
         <List>
             <DataGrid
                 {...dataGridProps}
+                columns={columns}
                 autoHeight
                 rowsPerPageOptions={[10, 20, 30, 50, 100]}
             />
