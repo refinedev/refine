1d45f1d2f40e0434b0ae828c7061d157561f2e4f
Salih <salihozdemir94@gmail.com>
2022-06-29 12:20:57 +0300

    fix: update refine-mui-use-autocomplete example

--- examples/field/useSelect/mui/src/pages/posts/list.tsx
+++ examples/field/useSelect/mui/src/pages/posts/list.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useMany } from "@pankod/refine-core";
 import {
     useDataGrid,
     DataGrid,
@@ -6,40 +8,66 @@ import {
     EditButton,
 } from "@pankod/refine-mui";
 
-import { IPost } from "interfaces";
-
-const columns: GridColumns = [
-    {
-        field: "id",
-        headerName: "ID",
-        type: "number",
-    },
-    { field: "title", headerName: "Title", minWidth: 500, flex: 1 },
-    { field: "status", headerName: "Status", minWidth: 120, flex: 0.5 },
-    {
-        field: "actions",
-        headerName: "Actions",
-        renderCell: function render({ row }) {
-            return <EditButton hideText recordItemId={row.id} />;
-        },
-        align: "center",
-        headerAlign: "center",
-        minWidth: 80,
-    },
-];
+import { ICategory, IPost } from "interfaces";
 
 export const PostsList: React.FC = () => {
-    const { dataGridProps } = useDataGrid<IPost>({
-        columns,
+    const { dataGridProps } = useDataGrid<IPost>();
+
+    const categoryIds = dataGridProps.rows.map((item) => item.category.id);
+    const { data: categoriesData, isLoading } = useMany<ICategory>({
+        resource: "categories",
+        ids: categoryIds,
+        queryOptions: {
+            enabled: categoryIds.length > 0,
+        },
     });
+
+    const columns = React.useMemo<GridColumns<IPost>>(
+        () => [
+            {
+                field: "id",
+                headerName: "ID",
+                type: "number",
+                width: 50,
+            },
+            { field: "title", headerName: "Title", minWidth: 400, flex: 1 },
+            {
+                field: "category.id",
+                headerName: "Category",
+                type: "number",
+                headerAlign: "left",
+                align: "left",
+                minWidth: 250,
+                flex: 0.5,
+                renderCell: ({ row }) => {
+                    if (isLoading) {
+                        return "Loading...";
+                    }
+
+                    const category = categoriesData?.data.find(
+                        (item) => item.id === row.category.id,
+                    );
+                    return category?.title;
+                },
+            },
+            { field: "status", headerName: "Status", minWidth: 120, flex: 0.3 },
+            {
+                field: "actions",
+                headerName: "Actions",
+                renderCell: function render({ row }) {
+                    return <EditButton hideText recordItemId={row.id} />;
+                },
+                align: "center",
+                headerAlign: "center",
+                minWidth: 80,
+            },
+        ],
+        [categoriesData, isLoading],
+    );
 
     return (
         <List>
-            <DataGrid
-                {...dataGridProps}
-                autoHeight
-                rowsPerPageOptions={[10, 20, 30, 50, 100]}
-            />
+            <DataGrid {...dataGridProps} columns={columns} autoHeight />
         </List>
     );
 };
