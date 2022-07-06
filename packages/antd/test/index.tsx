573557cb16b3297b8ea6b4ef592de18ef47b9f1e
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-07-06 18:30:16 +0300

    test(telemetry): add disable telemetry

--- packages/antd/test/index.tsx
+++ packages/antd/test/index.tsx
@@ -60,6 +60,7 @@ export const TestWrapper: (props: ITestWrapperProps) => React.FC = ({
                     resources={resources ?? [{ name: "posts", list: List }]}
                     accessControlProvider={accessControlProvider}
                     DashboardPage={DashboardPage ?? undefined}
+                    disableTelemetry={true}
                 >
                     {children}
                 </Refine>
