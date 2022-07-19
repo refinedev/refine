692e0cfad0244a55c42dc069ff1fb7e84975e1ae
ozkalai <melihozkalayy@gmail.com>
2022-07-19 14:51:41 +0300

    chore(medusa-storefront-example): update navbar component

--- examples/refine-medusa-storefront/src/components/common/Navbar/Navbar.tsx
+++ examples/refine-medusa-storefront/src/components/common/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import NavbarRoot from "./NavbarRoot";
 import { Logo, Container } from "@components/ui";
 import UserNav from "@components/common/UserNav";
 import Searchbar from "@components/common/Searchbar";
+import { useList } from "@pankod/refine-core";
 
 interface Link {
     href: string;
@@ -15,37 +16,48 @@ interface NavbarProps {
     links?: Link[];
 }
 
-const Navbar: FC<NavbarProps> = ({ links }) => (
-    <NavbarRoot>
-        <Container clean className="mx-auto max-w-8xl px-6">
-            <div className={s.nav}>
-                <div className="flex items-center flex-1">
-                    <Link href="/" className={s.logo}>
-                        <Logo />
-                    </Link>
-                    <nav className={s.navMenu}>
-                        <Link href="/search">
-                            <a className={s.link}>All</a>
+const Navbar: FC<NavbarProps> = ({ links }) => {
+    const { data } = useList({
+        resource: "collections",
+    });
+
+    const collections = data?.data;
+
+    return (
+        <NavbarRoot>
+            <Container clean className="mx-auto max-w-8xl px-6">
+                <div className={s.nav}>
+                    <div className="flex items-center flex-1">
+                        <Link href="/" className={s.logo}>
+                            <Logo />
                         </Link>
-                        {links?.map((l) => (
-                            <Link href={l.href} key={l.href}>
-                                <a className={s.link}>{l.label}</a>
+                        <nav className={s.navMenu}>
+                            <Link href="/collections">
+                                <a className={s.link}>All</a>
                             </Link>
-                        ))}
-                    </nav>
+                            {collections?.map((col) => (
+                                <Link
+                                    href={`/collections/show/${col.id}`}
+                                    key={col.id}
+                                >
+                                    <a className={s.link}>{col.title}</a>
+                                </Link>
+                            ))}
+                        </nav>
+                    </div>
+                    <div className="justify-center flex-1 hidden lg:flex">
+                        <Searchbar />
+                    </div>
+                    <div className="flex items-center justify-end flex-1 space-x-8">
+                        <UserNav />
+                    </div>
                 </div>
-                <div className="justify-center flex-1 hidden lg:flex">
-                    <Searchbar />
+                <div className="flex pb-4 lg:px-6 lg:hidden">
+                    <Searchbar id="mobile-search" />
                 </div>
-                <div className="flex items-center justify-end flex-1 space-x-8">
-                    <UserNav />
-                </div>
-            </div>
-            <div className="flex pb-4 lg:px-6 lg:hidden">
-                <Searchbar id="mobile-search" />
-            </div>
-        </Container>
-    </NavbarRoot>
-);
+            </Container>
+        </NavbarRoot>
+    );
+};
 
 export default Navbar;
