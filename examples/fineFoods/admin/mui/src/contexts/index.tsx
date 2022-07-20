7372ca35e2417e561ec43e6a8054c3ca1ebd1e5d
Ömer Faruk APLAK <omer@pankod.com>
2022-07-20 10:28:11 +0300

    fix: react@18 build issues

--- examples/fineFoods/admin/mui/src/contexts/index.tsx
+++ examples/fineFoods/admin/mui/src/contexts/index.tsx
@@ -14,39 +14,42 @@ export const ColorModeContext = createContext<ColorModeContextType>(
     {} as ColorModeContextType,
 );
 
-export const ColorModeContextProvider: React.FC = ({ children }) => {
-    const colorModeFromLocalStorage = localStorage.getItem("colorMode");
-    const isSystemPreferenceDark = window?.matchMedia(
-        "(prefers-color-scheme: dark)",
-    ).matches;
+export const ColorModeContextProvider: React.FC<{ children: React.ReactNode }> =
+    ({ children }) => {
+        const colorModeFromLocalStorage = localStorage.getItem("colorMode");
+        const isSystemPreferenceDark = window?.matchMedia(
+            "(prefers-color-scheme: dark)",
+        ).matches;
 
-    const systemPreference = isSystemPreferenceDark ? "dark" : "light";
-    const [mode, setMode] = useState(
-        colorModeFromLocalStorage || systemPreference,
-    );
+        const systemPreference = isSystemPreferenceDark ? "dark" : "light";
+        const [mode, setMode] = useState(
+            colorModeFromLocalStorage || systemPreference,
+        );
 
-    useEffect(() => {
-        window.localStorage.setItem("colorMode", mode);
-    }, [mode]);
+        useEffect(() => {
+            window.localStorage.setItem("colorMode", mode);
+        }, [mode]);
 
-    const setColorMode = () => {
-        if (mode === "light") {
-            setMode("dark");
-        } else {
-            setMode("light");
-        }
+        const setColorMode = () => {
+            if (mode === "light") {
+                setMode("dark");
+            } else {
+                setMode("light");
+            }
+        };
+
+        return (
+            <ColorModeContext.Provider
+                value={{
+                    setMode: setColorMode,
+                    mode,
+                }}
+            >
+                <ThemeProvider
+                    theme={mode === "light" ? LightTheme : DarkTheme}
+                >
+                    {children}
+                </ThemeProvider>
+            </ColorModeContext.Provider>
+        );
     };
-
-    return (
-        <ColorModeContext.Provider
-            value={{
-                setMode: setColorMode,
-                mode,
-            }}
-        >
-            <ThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
-                {children}
-            </ThemeProvider>
-        </ColorModeContext.Provider>
-    );
-};
