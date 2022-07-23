fad27b963ec7c26f1d117d91853e51c81757997c
ozkalai <melihozkalayy@gmail.com>
2022-07-23 14:43:36 +0300

    chore(medusa-storefront-example): authProvider updated with medusaClient

--- examples/refine-medusa-storefront/src/authProvider.ts
+++ examples/refine-medusa-storefront/src/authProvider.ts
@@ -1,27 +1,23 @@
 import { AuthProvider } from "@pankod/refine-core";
 
 import { API_URL } from "./constants";
-import Medusa from "@medusajs/medusa-js";
-import { getCookieParser } from "next/dist/server/api-utils";
+
+import { getMedusaClient, resetMedusaClient } from "./medusaClient";
 
 export const authProvider = (): AuthProvider => {
-    const medusaClient = new Medusa({
-        baseUrl: "",
-        maxRetries: 0,
-    });
+    const medusaClient = getMedusaClient(API_URL);
     return {
-        login: async ({
-            user,
-        }: {
-            user: { email: string; password: string };
-        }) => {
+        login: async (user: { username: string; password: string }) => {
             try {
-                const { customer, response } =
-                    await medusaClient.auth.authenticate(user);
+                const { customer } = await medusaClient.auth.authenticate({
+                    email: user.username,
+                    password: user.password,
+                });
 
-                console.log(`customer`, customer);
-
-                response.headers["set-cookie"];
+                if (customer) {
+                    localStorage.setItem("user", JSON.stringify(customer));
+                    return Promise.resolve();
+                }
             } catch (error) {
                 return Promise.reject(error);
             }
@@ -29,6 +25,9 @@ export const authProvider = (): AuthProvider => {
             return Promise.resolve("/");
         },
         logout: (props) => {
+            localStorage.removeItem("user");
+            resetMedusaClient();
+
             return Promise.resolve(props?.redirectPath);
         },
         checkError: (error) => {
@@ -37,17 +36,18 @@ export const authProvider = (): AuthProvider => {
             }
             return Promise.resolve();
         },
-        checkAuth: () => Promise.resolve(),
+        checkAuth: async () => {
+            const session = await medusaClient.auth.getSession();
+            if (session?.customer) {
+                return Promise.resolve();
+            }
+            return Promise.reject();
+        },
         getPermissions: () => Promise.resolve(),
         getUserIdentity: async () => {
-            const token = getCookieParser;
-            if (!token) {
-                return Promise.reject();
-            }
+            const { customer } = await medusaClient.auth.getSession();
 
-            const userInfo = await medusaClient.auth.getSession;
-
-            return Promise.resolve(userInfo);
+            return Promise.resolve(customer);
         },
     };
 };
