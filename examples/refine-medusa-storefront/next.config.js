60419aa436dae594fa1abfe347c0fe839e743c50
ozkalai <melihozkalayy@gmail.com>
2022-07-26 12:00:54 +0300

    chore(medusa-store-example): updated next config

--- examples/refine-medusa-storefront/next.config.js
+++ examples/refine-medusa-storefront/next.config.js
@@ -1,5 +1,15 @@
 const withPlugins = require("next-compose-plugins");
 
+const rewrites = () => {
+    return [
+        {
+            source: "/store/:id*",
+            destination:
+                "https://refine-example-storefront.herokuapp.com/store/:id*",
+        },
+    ];
+};
+
 module.exports = withPlugins([], {
     experimental: {
         newNextLinkBehavior: true,
@@ -7,4 +17,5 @@ module.exports = withPlugins([], {
     images: {
         domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com"],
     },
+    rewrites,
 });
