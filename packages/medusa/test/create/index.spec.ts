9ac2fe77844f7d13b75aaa9a245822b4d3da6f1c
ozkalai <melihozkalayy@gmail.com>
2022-07-27 12:02:58 +0300

    test(refine-medusa): updated create

--- packages/medusa/test/create/index.spec.ts
+++ packages/medusa/test/create/index.spec.ts
@@ -4,8 +4,6 @@ import DataProvider from "../../src/index";
 import "./index.mock";
 
 axios.defaults.adapter = require("axios/lib/adapters/http");
-axios.defaults.headers.common.Cookie =
-    "connect.sid=s%3AcgwfOVV47QrjFqPfyMxN6TqFOVKTLZPj.ApeBjENOF58To3guqq3qFmfUHrK%2Bll5pRnXNWF7D3V8;";
 
 describe("create", () => {
     it("correct response", async () => {
@@ -27,8 +25,5 @@ describe("create", () => {
         expect(data["customer"]["first_name"]).toBe("John");
         expect(data["customer"]["last_name"]).toBe("Doe");
         expect(data["customer"]["email"]).toBe("melih@pankod.dev");
-
-        // expect(data["id"]).toBe(1001);
-        // expect(data["title"]).toBe("foo");
     });
 });
