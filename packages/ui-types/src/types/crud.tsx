72affcd6c9a0b943049b13708a06893ca4e61f6f
Ali Emir Sen <senaliemir@gmail.com>
2022-07-27 13:01:54 +0300

    feat: update crud types

--- packages/ui-types/src/types/crud.tsx
+++ packages/ui-types/src/types/crud.tsx
@@ -1,38 +1,5 @@
 import { BaseKey, MutationMode } from "@pankod/refine-core";
 
-export type RefineCrudCommonProps<
-    TWrapperProps extends {} = Record<keyof any, unknown>,
-    THeaderProps extends {} = Record<keyof any, unknown>,
-    TContentProps extends {} = Record<keyof any, unknown>,
-    TExtraProps extends {} = {},
-> = {
-    title?: string;
-    goBack?: React.ReactNode;
-    resource?: string;
-    wrapperProps?: TWrapperProps;
-    headerProps?: THeaderProps;
-    contentProps?: TContentProps;
-    breadcrumb?: React.ReactNode;
-} & TExtraProps;
-
-export type RefineCrudDataProps = {
-    dataProviderName?: string;
-};
-
-export type RefineCrudHeaderButtonProps<
-    THeaderButtonProps extends {} = Record<keyof any, unknown>,
-> = {
-    headerButtons?: React.ReactNode;
-    headerButtonProps?: THeaderButtonProps;
-};
-
-export type RefineCrudFooterButtonProps<
-    TFooterButtonProps extends {} = Record<keyof any, unknown>,
-> = {
-    footerButtons?: React.ReactNode;
-    footerButtonProps?: TFooterButtonProps;
-};
-
 /**
  * This should be the base type for `List` crud component implementations in UI integrations.
  */
@@ -43,16 +10,22 @@ export type RefineCrudListProps<
     THeaderProps extends {} = Record<keyof any, unknown>,
     TContentProps extends {} = Record<keyof any, unknown>,
     TExtraProps extends {} = {},
-> = RefineCrudCommonProps<
-    TWrapperProps,
-    THeaderProps,
-    TContentProps,
-    TExtraProps
-> &
-    RefineCrudHeaderButtonProps<THeaderButtonProps> & {
-        canCreate?: boolean;
-        createButtonProps?: TCreateButtonProps;
-    };
+> = {
+    // Common Props
+    title?: string;
+    goBack?: React.ReactNode;
+    resource?: string;
+    wrapperProps?: TWrapperProps;
+    headerProps?: THeaderProps;
+    contentProps?: TContentProps;
+    breadcrumb?: React.ReactNode;
+    // Header Action Buttons Props
+    headerButtons?: React.ReactNode;
+    headerButtonProps?: THeaderButtonProps;
+    // Crud List Props
+    canCreate?: boolean;
+    createButtonProps?: TCreateButtonProps;
+} & TExtraProps;
 
 /**
  * This should be the base type for `Create` crud component implementations in UI integrations.
@@ -65,17 +38,25 @@ export type RefineCrudCreateProps<
     THeaderProps extends {} = Record<keyof any, unknown>,
     TContentProps extends {} = Record<keyof any, unknown>,
     TExtraProps extends {} = {},
-> = RefineCrudCommonProps<
-    TWrapperProps,
-    THeaderProps,
-    TContentProps,
-    TExtraProps
-> &
-    RefineCrudHeaderButtonProps<THeaderButtonProps> &
-    RefineCrudFooterButtonProps<TFooterButtonProps> & {
-        isLoading?: boolean;
-        saveButtonProps?: TSaveButtonProps;
-    };
+> = {
+    // Common Props
+    title?: string;
+    goBack?: React.ReactNode;
+    resource?: string;
+    wrapperProps?: TWrapperProps;
+    headerProps?: THeaderProps;
+    contentProps?: TContentProps;
+    breadcrumb?: React.ReactNode;
+    // Header Action Buttons Props
+    headerButtons?: React.ReactNode;
+    headerButtonProps?: THeaderButtonProps;
+    // Footer Action Buttons Props
+    footerButtons?: React.ReactNode;
+    footerButtonProps?: TFooterButtonProps;
+    // Crud Create Props
+    isLoading?: boolean;
+    saveButtonProps?: TSaveButtonProps;
+} & TExtraProps;
 
 /**
  * This should be the base type for `Edit` crud component implementations in UI integrations.
@@ -89,22 +70,31 @@ export type RefineCrudEditProps<
     THeaderProps extends {} = Record<keyof any, unknown>,
     TContentProps extends {} = Record<keyof any, unknown>,
     TExtraProps extends {} = {},
-> = RefineCrudCommonProps<
-    TWrapperProps,
-    THeaderProps,
-    TContentProps,
-    TExtraProps
-> &
-    RefineCrudHeaderButtonProps<THeaderButtonProps> &
-    RefineCrudFooterButtonProps<TFooterButtonProps> &
-    RefineCrudDataProps & {
-        isLoading?: boolean;
-        canDelete?: boolean;
-        saveButtonProps?: TSaveButtonProps;
-        deleteButtonProps?: TDeleteButtonProps;
-        mutationMode?: MutationMode;
-        recordItemId?: BaseKey;
-    };
+> = {
+    // Common Props
+    title?: string;
+    goBack?: React.ReactNode;
+    resource?: string;
+    wrapperProps?: TWrapperProps;
+    headerProps?: THeaderProps;
+    contentProps?: TContentProps;
+    breadcrumb?: React.ReactNode;
+    // Header Action Buttons Props
+    headerButtons?: React.ReactNode;
+    headerButtonProps?: THeaderButtonProps;
+    // Footer Action Buttons Props
+    footerButtons?: React.ReactNode;
+    footerButtonProps?: TFooterButtonProps;
+    // Data Provider Props
+    dataProviderName?: string;
+    // Crud Edit Props
+    isLoading?: boolean;
+    canDelete?: boolean;
+    saveButtonProps?: TSaveButtonProps;
+    deleteButtonProps?: TDeleteButtonProps;
+    mutationMode?: MutationMode;
+    recordItemId?: BaseKey;
+} & TExtraProps;
 
 /**
  * This should be the base type for `Show` crud component implementations in UI integrations.
@@ -116,17 +106,26 @@ export type RefineCrudShowProps<
     THeaderProps extends {} = Record<keyof any, unknown>,
     TContentProps extends {} = Record<keyof any, unknown>,
     TExtraProps extends {} = {},
-> = RefineCrudCommonProps<
-    TWrapperProps,
-    THeaderProps,
-    TContentProps,
-    TExtraProps
-> &
-    RefineCrudHeaderButtonProps<THeaderButtonProps> &
-    RefineCrudFooterButtonProps<TFooterButtonProps> &
-    RefineCrudDataProps & {
-        isLoading?: boolean;
-        canDelete?: boolean;
-        canEdit?: boolean;
-        recordItemId?: BaseKey;
-    };
+> = {
+    // Common Props
+    title?: string;
+    goBack?: React.ReactNode;
+    resource?: string;
+    wrapperProps?: TWrapperProps;
+    headerProps?: THeaderProps;
+    contentProps?: TContentProps;
+    breadcrumb?: React.ReactNode;
+    // Header Action Buttons Props
+    headerButtons?: React.ReactNode;
+    headerButtonProps?: THeaderButtonProps;
+    // Footer Action Buttons Props
+    footerButtons?: React.ReactNode;
+    footerButtonProps?: TFooterButtonProps;
+    // Data Provider Props
+    dataProviderName?: string;
+    // Crud Show Props
+    isLoading?: boolean;
+    canDelete?: boolean;
+    canEdit?: boolean;
+    recordItemId?: BaseKey;
+} & TExtraProps;
