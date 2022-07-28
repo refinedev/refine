0eea24d7e96e5ccff441198052d5162c99a7f902
Salih <salihozdemir94@gmail.com>
2022-07-28 17:32:55 +0200

    fix: remove out of context test in import button

--- packages/antd/src/components/buttons/import/index.spec.tsx
+++ packages/antd/src/components/buttons/import/index.spec.tsx
@@ -1,54 +1,7 @@
-import React from "react";
-import { act, Simulate } from "react-dom/test-utils";
-import { renderHook } from "@testing-library/react-hooks";
 import { buttonImportTests } from "@pankod/refine-ui-tests";
-import { render, TestWrapper, MockJSONServer } from "@test";
 
 import { ImportButton } from "./index";
-import { useImport } from "@hooks/import";
-
-const parseMock = jest.fn();
-
-jest.mock("papaparse", () => {
-    return {
-        parse: jest.fn(() => parseMock()),
-    };
-});
 
 describe("<ImportButton /> usage with useImport", () => {
     buttonImportTests.bind(this)(ImportButton);
-
-    it("should trigger parse when used with useImport hook", async () => {
-        const {
-            result: { current: importProps },
-        } = renderHook(() => useImport(), {
-            wrapper: TestWrapper({
-                dataProvider: MockJSONServer,
-                resources: [{ name: "categories" }],
-            }),
-        });
-        const { container } = render(
-            <ImportButton {...importProps}>Test</ImportButton>,
-        );
-        await act(async () => {
-            jest.advanceTimersToNextTimer(1);
-        });
-        const file = new File(
-            [
-                `"id","title","createdAt","updatedAt"
-    "35ad97dd-9379-480a-b6ac-6fc9c13e9224","Viral Strategist Local","2021-04-09T12:03:23.933Z","2021-04-09T12:03:23.933Z"
-    "9a428977-1b03-4c3e-8cdd-1e4e2813528a","Concrete Soap Neural","2021-04-09T12:03:23.835Z","2021-04-09T12:03:23.835Z"`,
-            ],
-            "data.csv",
-            { type: "text/csv" },
-        );
-        const hiddenFileInput = container.querySelector('input[type="file"]');
-        const files = { files: [file] } as unknown as EventTarget;
-        await act(async () => {
-            Simulate.change(hiddenFileInput as Element, {
-                target: files,
-            });
-        });
-        expect(parseMock).toHaveBeenCalled();
-    });
 });
--- packages/mui/src/components/buttons/import/index.spec.tsx
+++ packages/mui/src/components/buttons/import/index.spec.tsx
@@ -1,76 +1,7 @@
-import React from "react";
 import { buttonImportTests } from "@pankod/refine-ui-tests";
-import { renderHook } from "@testing-library/react-hooks";
-import { useImport } from "@pankod/refine-core";
-import { Simulate } from "react-dom/test-utils";
-
-import { render, TestWrapper, MockJSONServer, act } from "@test";
 
 import { ImportButton } from "./";
 
-const parseMock = jest.fn();
-
-jest.mock("papaparse", () => {
-    return {
-        parse: jest.fn(() => parseMock()),
-    };
-});
-
 describe("ImportButton", () => {
     buttonImportTests.bind(this)(ImportButton);
-
-    it("should trigger parse when used with useImport hook", async () => {
-        jest.spyOn(console, "error").mockImplementation((message) => {
-            if (
-                message?.includes?.(
-                    "Can't perform a React state update on an unmounted component.",
-                )
-            ) {
-                console.log("Memory leak - unmounted state update.");
-                return;
-            }
-            console.warn(message);
-        });
-
-        const {
-            result: { current: importProps },
-        } = renderHook(() => useImport(), {
-            wrapper: TestWrapper({
-                dataProvider: MockJSONServer,
-                resources: [{ name: "categories" }],
-            }),
-        });
-
-        const { container } = render(
-            <ImportButton
-                inputProps={importProps.inputProps}
-                loading={importProps.isLoading}
-            >
-                Test
-            </ImportButton>,
-        );
-
-        await act(async () => {
-            jest.advanceTimersToNextTimer(1);
-        });
-
-        const file = new File(
-            [
-                `"id","title","createdAt","updatedAt"
-    "35ad97dd-9379-480a-b6ac-6fc9c13e9224","Viral Strategist Local","2021-04-09T12:03:23.933Z","2021-04-09T12:03:23.933Z"
-    "9a428977-1b03-4c3e-8cdd-1e4e2813528a","Concrete Soap Neural","2021-04-09T12:03:23.835Z","2021-04-09T12:03:23.835Z"`,
-            ],
-            "data.csv",
-            { type: "text/csv" },
-        );
-        const hiddenFileInput = container.querySelector('input[type="file"]');
-        const files = { files: [file] } as unknown as EventTarget;
-
-        await act(async () => {
-            Simulate.change(hiddenFileInput as Element, {
-                target: files,
-            });
-        });
-        expect(parseMock).toHaveBeenCalled();
-    });
 });
