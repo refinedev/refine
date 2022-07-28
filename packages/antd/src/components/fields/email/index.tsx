949d8f58c8dd010ae950c2ca0348848846495989
Aydın Akyol <aydinakyol11@gmail.com>
2022-07-28 12:22:45 +0300

    refactor(fields): update email prop types and test

--- packages/antd/src/components/fields/email/index.tsx
+++ packages/antd/src/components/fields/email/index.tsx
@@ -1,12 +1,11 @@
 import React, { ReactNode } from "react";
+import { RefineFieldEmailProps } from "@pankod/refine-ui-types";
 import { Typography } from "antd";
 import { LinkProps } from "antd/lib/typography/Link";
 
-import { FieldProps } from "../../../interfaces";
-
 const { Link } = Typography;
 
-export type EmailFieldProps = FieldProps<ReactNode> & LinkProps;
+export type EmailFieldProps = RefineFieldEmailProps<ReactNode, LinkProps>;
 
 /**
  * This field is used to display email values. It uses the {@link https://ant.design/components/typography/#FAQ `<Link>`} component
