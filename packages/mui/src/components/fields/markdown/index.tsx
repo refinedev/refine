edf7bdd2d1c28bdb4f78bf6997918dfd3610446d
Aydın Akyol <aydinakyol11@gmail.com>
2022-07-28 12:26:57 +0300

    refactor(fields): update markdown prop types and test

--- packages/mui/src/components/fields/markdown/index.tsx
+++ packages/mui/src/components/fields/markdown/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 
+import { RefineFieldMarkdownProps } from "@pankod/refine-ui-types";
 import ReactMarkdown from "react-markdown";
 
 import gfm from "remark-gfm";
 
-import { FieldProps } from "src/interfaces/field";
+export type MarkdownFieldProps = RefineFieldMarkdownProps<string | undefined>;
 
 /**
  * This field lets you display markdown content. It supports {@link https://github.github.com/gfm/ GitHub Flavored Markdown}.
  *
  * @see {@link https://refine.dev/docs/ui-frameworks/mui/components/fields/markdown} for more details.
  */
-export const MarkdownField: React.FC<FieldProps<string | undefined>> = ({
-    value = "",
-}) => {
+export const MarkdownField: React.FC<MarkdownFieldProps> = ({ value = "" }) => {
     return <ReactMarkdown plugins={[gfm]}>{value}</ReactMarkdown>;
 };
