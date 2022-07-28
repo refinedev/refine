f72f2d8760989e6847c49fc37f6a39fca19322a0
Salih <salihozdemir94@gmail.com>
2022-07-28 16:29:05 +0200

    refactor(clone-button): add some common tests

--- packages/ui-tests/src/tests/buttons/clone.tsx
+++ packages/ui-tests/src/tests/buttons/clone.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import {
     RefineCloneButtonProps,
     RefineButtonTestIds,
@@ -10,6 +11,11 @@ export const buttonCloneTests = function (
     CloneButton: React.ComponentType<RefineCloneButtonProps<any, any>>,
 ): void {
     describe("[@pankod/refine-ui-tests] Common Tests / Clone Button", () => {
+        beforeAll(() => {
+            jest.spyOn(console, "warn").mockImplementation(jest.fn());
+            jest.useFakeTimers();
+        });
+
         it("should render button successfuly", async () => {
             const { container } = render(<CloneButton />, {
                 wrapper: TestWrapper({}),
@@ -189,6 +195,130 @@ export const buttonCloneTests = function (
             });
 
             expect(clone).toHaveBeenCalledTimes(1);
+        });
+
+        it("should create page redirect clone route called function successfully if click the button", async () => {
+            const { getByText } = render(
+                <Routes>
+                    <Route
+                        path="/:resource"
+                        element={<CloneButton recordItemId="1" />}
+                    />
+                </Routes>,
+                {
+                    wrapper: TestWrapper({
+                        routerInitialEntries: ["/posts"],
+                    }),
+                },
+            );
+
+            await act(async () => {
+                jest.advanceTimersToNextTimer(1);
+            });
+
+            await act(async () => {
+                fireEvent.click(getByText("Clone"));
+            });
+
+            expect(window.location.pathname).toBe("/posts/clone/1");
+        });
+
+        it("should edit page redirect clone route called function successfully if click the button", async () => {
+            const { getByText } = render(
+                <Routes>
+                    <Route
+                        path="/:resource/:action/:id"
+                        element={<CloneButton />}
+                    />
+                </Routes>,
+                {
+                    wrapper: TestWrapper({
+                        routerInitialEntries: ["/posts/edit/1"],
+                    }),
+                },
+            );
+
+            await act(async () => {
+                jest.advanceTimersToNextTimer(1);
+            });
+
+            await act(async () => {
+                fireEvent.click(getByText("Clone"));
+            });
+
+            expect(window.location.pathname).toBe("/posts/clone/1");
+        });
+
+        it("should custom resource and recordItemId redirect clone route called function successfully if click the button", async () => {
+            const { getByText } = render(
+                <Routes>
+                    <Route
+                        path="/:resource"
+                        element={
+                            <CloneButton
+                                resourceNameOrRouteName="categories"
+                                recordItemId="1"
+                            />
+                        }
+                    />
+                </Routes>,
+                {
+                    wrapper: TestWrapper({
+                        resources: [{ name: "posts" }, { name: "categories" }],
+                        routerInitialEntries: ["/posts"],
+                    }),
+                },
+            );
+
+            await act(async () => {
+                jest.advanceTimersToNextTimer(1);
+            });
+
+            await act(async () => {
+                fireEvent.click(getByText("Clone"));
+            });
+
+            expect(window.location.pathname).toBe("/categories/clone/1");
+        });
+
+        it("should redirect with custom route called function successfully if click the button", async () => {
+            const { getByText } = render(
+                <Routes>
+                    <Route
+                        path="/:resource"
+                        element={
+                            <CloneButton
+                                resourceNameOrRouteName="custom-route-posts"
+                                recordItemId="1"
+                            />
+                        }
+                    />
+                </Routes>,
+                {
+                    wrapper: TestWrapper({
+                        resources: [
+                            {
+                                name: "posts",
+                                options: { route: "custom-route-posts" },
+                            },
+                            { name: "posts" },
+                        ],
+                        routerInitialEntries: ["/posts"],
+                    }),
+                },
+            );
+
+            await act(async () => {
+                jest.advanceTimersToNextTimer(1);
+            });
+
+            await act(async () => {
+                fireEvent.click(getByText("Clone"));
+            });
+
+            expect(window.location.pathname).toBe(
+                "/custom-route-posts/clone/1",
+            );
         });
     });
 };
