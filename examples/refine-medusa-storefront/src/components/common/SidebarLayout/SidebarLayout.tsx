ed90be9fe89ca6418e133a92a7ac78e90ad16fe7
ozkalai <melihozkalayy@gmail.com>
2022-08-01 12:29:20 +0300

    chore(medusa-storefront-example): updated sidebar layout

--- examples/refine-medusa-storefront/src/components/common/SidebarLayout/SidebarLayout.tsx
+++ examples/refine-medusa-storefront/src/components/common/SidebarLayout/SidebarLayout.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Cross, ChevronLeft } from "@components/icons";
-import { UserNav } from "@components/common";
+import UserNav from "@components/common/UserNav";
 import cn from "clsx";
 import s from "./SidebarLayout.module.css";
 
