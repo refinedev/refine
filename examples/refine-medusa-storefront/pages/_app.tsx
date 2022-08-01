bdfcaf5eef954d30394f43e15b8d19de475cccfc
ozkalai <melihozkalayy@gmail.com>
2022-08-01 12:34:08 +0300

    chore(medusa-storefront-example): updated app.tsx

--- examples/refine-medusa-storefront/pages/_app.tsx
+++ examples/refine-medusa-storefront/pages/_app.tsx
@@ -5,42 +5,44 @@ import "@assets/chrome-bug.css";
 import "keen-slider/keen-slider.min.css";
 
 import { Refine, LoginPage } from "@pankod/refine-core";
-import dataProvider from "../src/dataProvider";
 import routerProvider from "@pankod/refine-nextjs-router";
+import dataProvider, { authProvider } from "@pankod/refine-medusa";
 
 import { API_URL } from "../src/constants";
 
 import { CollectionsList, ProductList, ProductShow } from "@components";
-import { authProvider } from "src/authProvider";
 import { CollectionsShow } from "@components/collections/show";
 import { ManagedUIContext } from "@components/ui/context";
 import Layout from "@components/common/Layout";
+import { CardProvider } from "@lib/context";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <ManagedUIContext>
-            <Refine
-                Layout={Layout}
-                LoginPage={LoginPage}
-                authProvider={authProvider()}
-                routerProvider={routerProvider}
-                dataProvider={dataProvider(API_URL)}
-                resources={[
-                    {
-                        name: "products",
-                        list: ProductList,
-                        show: ProductShow,
-                    },
-                    {
-                        name: "collections",
-                        list: CollectionsList,
-                        show: CollectionsShow,
-                    },
-                ]}
-                warnWhenUnsavedChanges={true}
-            >
-                <Component {...pageProps} />
-            </Refine>
+            <CardProvider>
+                <Refine
+                    Layout={Layout}
+                    LoginPage={LoginPage}
+                    authProvider={authProvider(API_URL)}
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(API_URL)}
+                    resources={[
+                        {
+                            name: "products",
+                            list: ProductList,
+                            show: ProductShow,
+                        },
+                        {
+                            name: "collections",
+                            list: CollectionsList,
+                            show: CollectionsShow,
+                        },
+                    ]}
+                    warnWhenUnsavedChanges={true}
+                >
+                    <Component {...pageProps} />
+                </Refine>
+            </CardProvider>
         </ManagedUIContext>
     );
 }
