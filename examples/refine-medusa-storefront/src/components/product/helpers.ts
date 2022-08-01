7d070eb81352565b7ec90089c822115095ad47cc
ozkalai <melihozkalayy@gmail.com>
2022-08-01 12:33:04 +0300

    chore(medusa-storefront-example): updated helpers

--- examples/refine-medusa-storefront/src/components/product/helpers.ts
+++ examples/refine-medusa-storefront/src/components/product/helpers.ts
@@ -11,14 +11,10 @@ export function getProductVariant(product: any, opts: SelectedOptions) {
     const variant = product?.variants.find((variant: any) => {
         return Object.entries(opts).every(([key, value]) =>
             variant.options.find((option: any) => {
-                if (
-                    option.__typename === "MultipleChoiceOption" &&
-                    option.title.toLowerCase() === key.toLowerCase()
-                ) {
-                    return option.values.find(
-                        (v: any) => v.value.toLowerCase() === value,
-                    );
-                }
+                return (
+                    option.value.toLocaleLowerCase() ===
+                    value?.toLocaleLowerCase()
+                );
             }),
         );
     });
