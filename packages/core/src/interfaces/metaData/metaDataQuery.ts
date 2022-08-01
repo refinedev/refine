8dcf7657e534571bb1f523c29c7a30a6f8d283fa
Salih <salihozdemir94@gmail.com>
2022-08-01 12:56:29 +0200

    feat: add `queryContext` type to `MetaDataQuery`

--- packages/core/src/interfaces/metaData/metaDataQuery.ts
+++ packages/core/src/interfaces/metaData/metaDataQuery.ts
@@ -1,5 +1,7 @@
+import { QueryFunctionContext } from "@tanstack/react-query";
 import { QueryBuilderOptions } from "./queryBuilderOptions";
 
 export type MetaDataQuery = {
     [k: string]: any;
+    queryContext?: Omit<QueryFunctionContext, "meta">;
 } & QueryBuilderOptions;
