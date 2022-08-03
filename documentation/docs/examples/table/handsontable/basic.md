1959cc060781c7e43006c08464c315e7c24d4d64
Aydın <aydinakyol11@gmail.com>
2022-08-03 15:52:29 +0300

    feat: add handsontable example (#2267)
    * feat: add handson table example
    
    * docs: handsontable example document

---
+++ documentation/docs/examples/table/handsontable/basic.md
@@ -0,0 +1,11 @@
+---
+id: handsontable
+title: Basic
+---
+
+[**HandsonTable**](https://handsontable.com/) is a full-featured spreadsheet editor.
+
+<iframe loading="lazy" src="https://stackblitz.com//github/pankod/refine/tree/master/examples/table/handson/?embed=1&view=preview&theme=dark&preset=node"
+    style={{width: "100%", height:"80vh", border: "0px", borderRadius: "8px", overflow:"hidden"}}
+    title="refine-react-table-example"
+></iframe>
---
+++ examples/table/handson/src/App.tsx
@@ -0,0 +1,23 @@
+import { Refine } from "@pankod/refine-core";
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
+
+import { PostList } from "./pages/posts";
+
+const App: React.FC = () => {
+    return (
+        <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            resources={[
+                {
+                    name: "posts",
+                    list: PostList,
+                },
+            ]}
+            disableTelemetry={true}
+        />
+    );
+};
+
+export default App;
