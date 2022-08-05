99e607f2f216efc9433391fcfdfd7cbb1befa7fd
ozkalai <melihozkalayy@gmail.com>
2022-08-05 17:29:40 +0300

    chore(refine-medusa-example): profile page

--- examples/refine-medusa-storefront/pages/account/profile.tsx
+++ examples/refine-medusa-storefront/pages/account/profile.tsx
@@ -4,13 +4,16 @@ import { LayoutWrapper, useGetIdentity } from "@pankod/refine-core";
 import { Container, Text } from "@components/ui";
 import { getSearchStaticProps } from "@lib/search-props";
 import AccountLayout from "@components/account/AccountLayout/AccountLayout";
+import Profile from "@components/account/Profile/Profile";
 
 const ProfilePage: React.FC = () => {
     const { data } = useGetIdentity();
 
     return (
         <LayoutWrapper>
-            <AccountLayout></AccountLayout>
+            <AccountLayout>
+                <Profile />
+            </AccountLayout>
         </LayoutWrapper>
     );
 };
