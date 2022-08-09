39939d865240363250603b620d73ba55e36e7abf
Salih <salihozdemir94@gmail.com>
2022-08-09 16:39:38 +0200

    feat: create `PaymentDetails` component

---
+++ examples/refine-medusa-storefront/src/components/orders/PaymentDetails/PaymentDetails.tsx
@@ -0,0 +1,74 @@
+import { Payment, PaymentStatus } from "@medusajs/medusa";
+
+interface PaymentDetailsProps {
+    payments: Payment[];
+    paymentStatus: PaymentStatus;
+}
+
+const PaymentDetails: React.FC<PaymentDetailsProps> = ({
+    payments,
+    paymentStatus,
+}) => {
+    return (
+        <div>
+            <h2 className="text-base-semi">Payment</h2>
+            <div className="my-2">
+                {payments.map((p) => {
+                    switch (p.provider_id) {
+                        case "stripe":
+                            return <StripeDetails key={p.id} payment={p} />;
+                        case "paypal":
+                            return <PayPalDetails key={p.id} />;
+                        case "manual":
+                            return <TestDetails key={p.id} />;
+                        default:
+                            return null;
+                    }
+                })}
+            </div>
+        </div>
+    );
+};
+
+const PayPalDetails = () => {
+    return (
+        <div className="text-base-regular flex flex-col">
+            <span className="text-small-regular text-gray-700">PayPal</span>
+            <span>PayPal payment</span>
+        </div>
+    );
+};
+
+const StripeDetails = ({ payment }: { payment: Payment }) => {
+    const card: {
+        brand: string;
+        last4: string;
+        exp_year: number;
+        exp_month: number;
+    } = (payment.data.charges as any).data[0].payment_method_details.card;
+
+    return (
+        <div className="text-base-regular flex flex-col">
+            <span className="text-small-regular text-gray-700">
+                {card.brand.substring(0, 1).toUpperCase()}
+                {card.brand.substring(1)}
+            </span>
+            <span>************{card.last4}</span>
+            <span>
+                {card.exp_month > 9 ? card.exp_month : `0${card.exp_month}`} /{" "}
+                {card.exp_year.toString().slice(2)}
+            </span>
+        </div>
+    );
+};
+
+const TestDetails = () => {
+    return (
+        <div className="text-base-regular flex flex-col">
+            <span className="text-small-regular text-gray-700">Test</span>
+            <span>Test payment using medusa-payment-manual</span>
+        </div>
+    );
+};
+
+export default PaymentDetails;
