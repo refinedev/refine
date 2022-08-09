9da7d25e6281224b3c6c7ac3116bc72db38caf5f
Salih <salihozdemir94@gmail.com>
2022-08-09 12:58:50 +0200

    fix: move constants file under the lib

--- examples/refine-medusa-storefront/src/lib/constants.ts
+++ examples/refine-medusa-storefront/src/lib/constants.ts
@@ -2,3 +2,5 @@ export const API_URL =
     typeof window !== undefined
         ? ""
         : "https://refine-example-storefront.herokuapp.com/store";
+
+export const IS_BROWSER = typeof window !== "undefined";
