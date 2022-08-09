f879f04b84ec28af3c9600bea1a1876f6bfb7cc7
Salih <salihozdemir94@gmail.com>
2022-08-09 14:35:42 +0200

    feat: create `getPercentageDiff` util

---
+++ examples/refine-medusa-storefront/src/lib/getPercentageDiff.ts
@@ -0,0 +1,9 @@
+export const getPercentageDiff = (
+    original: number,
+    calculated: number,
+): string => {
+    const diff = original - calculated;
+    const decrease = (diff / original) * 100;
+
+    return decrease.toFixed();
+};
