2f27ee918398db23086f756ea14b48a197e5e569
Ali Emir Sen <senaliemir@gmail.com>
2022-08-10 12:42:30 +0300

    feat: update scope

--- documentation/src/theme/ReactLiveScope/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// Add react-live imports you need here
-const ReactLiveScope = {
-    React,
-    ...React,
-};
-export default ReactLiveScope;
---
+++ documentation/src/theme/ReactLiveScope/index.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import * as RefineCore from "@pankod/refine-core";
+import * as RefineReactRouterV6 from "@pankod/refine-react-router-v6";
+import * as RefineAntd from "@pankod/refine-antd";
+import * as RefineMui from "@pankod/refine-mui";
+import * as RefineSimpleRest from "@pankod/refine-simple-rest";
+
+const SIMPLE_REST_API_URL = "https://api.fake-rest.refine.dev";
+
+const RefineAntdDemo: React.FC<any> = ({ initialRoutes, ...rest }) => {
+    return (
+        <RefineCore.Refine
+            routerProvider={{
+                ...RefineReactRouterV6.default,
+                RouterComponent: RefineReactRouterV6.MemoryRouterComponent.bind(
+                    null,
+                    {
+                        initialEntries: initialRoutes,
+                    },
+                ),
+            }}
+            dataProvider={RefineSimpleRest.default(SIMPLE_REST_API_URL)}
+            notificationProvider={RefineAntd.notificationProvider}
+            Layout={RefineAntd.Layout}
+            catchAll={<RefineAntd.ErrorComponent />}
+            disableTelemetry={true}
+            {...rest}
+        />
+    );
+};
+
+const RefineHeadlessDemo: React.FC<any> = ({ initialRoutes, ...rest }) => {
+    return (
+        <RefineCore.Refine
+            routerProvider={{
+                ...RefineReactRouterV6.default,
+                RouterComponent: RefineReactRouterV6.MemoryRouterComponent.bind(
+                    null,
+                    {
+                        initialEntries: initialRoutes,
+                    },
+                ),
+            }}
+            dataProvider={RefineSimpleRest.default(SIMPLE_REST_API_URL)}
+            disableTelemetry={true}
+            {...rest}
+        />
+    );
+};
+
+const RefineMuiDemo: React.FC<any> = ({ initialRoutes, ...rest }) => {
+    return (
+        <RefineMui.ThemeProvider theme={RefineMui.LightTheme}>
+            <RefineMui.CssBaseline />
+            <RefineMui.GlobalStyles
+                styles={{ html: { WebkitFontSmoothing: "auto" } }}
+            />
+            <RefineMui.RefineSnackbarProvider>
+                <RefineCore.Refine
+                    routerProvider={{
+                        ...RefineReactRouterV6.default,
+                        RouterComponent:
+                            RefineReactRouterV6.MemoryRouterComponent.bind(
+                                null,
+                                {
+                                    initialEntries: initialRoutes,
+                                },
+                            ),
+                    }}
+                    dataProvider={RefineSimpleRest.default(SIMPLE_REST_API_URL)}
+                    notificationProvider={RefineMui.notificationProvider}
+                    Layout={RefineMui.Layout}
+                    catchAll={<RefineMui.ErrorComponent />}
+                    disableTelemetry={true}
+                    {...rest}
+                />
+            </RefineMui.RefineSnackbarProvider>
+        </RefineMui.ThemeProvider>
+    );
+};
+
+// Add react-live imports you need here
+const ReactLiveScope = {
+    React,
+    ...React,
+    RefineCore,
+    RefineSimpleRest,
+    RefineReactRouterV6,
+    RefineAntd,
+    RefineMui,
+    RefineHeadlessDemo,
+    RefineAntdDemo,
+    RefineMuiDemo,
+};
+export default ReactLiveScope;
