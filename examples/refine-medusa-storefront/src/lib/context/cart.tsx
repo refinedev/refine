00f88fad4e1cbb42dbec22f91672ecc7ce9a0ff8
ozkalai <melihozkalayy@gmail.com>
2022-08-12 12:08:29 +0300

    chore(refine-medusa-storefront): cart context updated

--- examples/refine-medusa-storefront/src/lib/context/cart.tsx
+++ examples/refine-medusa-storefront/src/lib/context/cart.tsx
@@ -1,56 +1,308 @@
-import { BaseRecord, CreateResponse, useCreate } from "@pankod/refine-core";
-import React, { createContext } from "react";
+import React, { useEffect, useState, PropsWithChildren } from "react";
+import { Region, StoreCartsRes, Cart } from "@medusajs/medusa";
+import { useCreate, useDelete, useUpdate } from "@pankod/refine-core";
+import useCart from "@lib/hooks/useCart";
 
-type CartProviderProps = {
-    children: React.ReactNode;
-};
-
-type CartContextType = {
-    addToCart?: (payload: AddToCartType) => Promise<CreateResponse<BaseRecord>>;
-    isLoading?: boolean;
-    cartId?: string;
-};
-
-type AddToCartType = {
+interface VariantInfoProps {
     variantId: string;
-    quantity?: number;
+    quantity: number;
+}
+
+interface LineInfoProps {
+    lineId: string;
+    quantity: number;
+}
+
+interface CartContext {
+    countryCode: string | undefined;
+    setRegion: (regionId: string, countryCode: string) => void;
+    addItem: (item: VariantInfoProps) => void;
+    updateItem: (item: LineInfoProps) => void;
+    deleteItem: (lineId: string) => void;
+    resetCart: () => void;
+    cartId: string | undefined;
+    cart: Omit<Cart, "refundable_amount" | "refunded_total"> | undefined;
+}
+
+const CartContext = React.createContext<CartContext | null>(null);
+
+export const useCartContext = (): CartContext => {
+    const context = React.useContext(CartContext);
+    if (context === null) {
+        throw new Error("useCart must be used within a CartProvider");
+    }
+    return context;
 };
-export const CartContext = createContext<CartContextType>({});
 
-export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
-    let cartId: string | undefined =
-        typeof window !== "undefined"
-            ? localStorage.getItem("cartId") ?? undefined
-            : undefined;
+const IS_SERVER = typeof window === "undefined";
+const CART_KEY = "medusa_cart_id";
 
-    const { mutateAsync, isLoading } = useCreate();
+export const CartProvider: React.FC<PropsWithChildren> = ({ children }) => {
+    const { mutateAsync: createMutateAsync, mutate: createMutate } =
+        useCreate<StoreCartsRes>();
+    const { mutateAsync: updateMutateAsync, mutate: updateMutate } =
+        useUpdate<StoreCartsRes>();
+    const { mutate: deleteMutate } = useDelete<StoreCartsRes>();
 
-    const addToCart = async ({ variantId, quantity = 1 }: AddToCartType) => {
-        if (cartId === undefined) {
-            // create cart
+    const [countryCode, setCountryCode] =
+        useState<string | undefined>(undefined);
+    const [cartId, setCartId] = useState<string | undefined>(undefined);
 
-            const data = await mutateAsync({
-                resource: "carts",
-                values: {},
-            });
+    const { cart } = useCart({ id: cartId });
 
-            if (data?.data.cart?.id !== undefined) {
-                cartId = data?.data.cart?.id;
-                localStorage?.setItem("cartId", cartId!);
+    const storeRegion = (regionId: string, countryCode: string) => {
+        if (!IS_SERVER) {
+            localStorage.setItem(
+                "medusa_region",
+                JSON.stringify({ regionId, countryCode }),
+            );
+
+            setCountryCode(countryCode);
+        }
+    };
+
+    useEffect(() => {
+        if (!IS_SERVER) {
+            const storedRegion = localStorage.getItem("medusa_region");
+            if (storedRegion) {
+                const { countryCode } = JSON.parse(storedRegion);
+                setCountryCode(countryCode);
             }
         }
+    }, []);
 
-        return await mutateAsync({
-            resource: `carts/${cartId}/line-items`,
-            values: {
-                variant_id: variantId,
-                quantity: quantity,
+    const getRegion = () => {
+        if (!IS_SERVER) {
+            const region = localStorage.getItem("medusa_region");
+            if (region) {
+                return JSON.parse(region) as {
+                    regionId: string;
+                    countryCode: string;
+                };
+            }
+        }
+        return null;
+    };
+
+    const setRegion = async (regionId: string, countryCode: string) => {
+        await updateMutateAsync(
+            {
+                resource: "carts",
+                id: cartId || "",
+                values: {
+                    region_id: regionId,
+                },
             },
-        });
+            {
+                onSuccess: ({ data: { cart } }) => {
+                    //TODO: make sure to update the cart in the store with the new region
+                    storeCart(cart.id);
+                    storeRegion(regionId, countryCode);
+                },
+                onError: (error) => {
+                    if (process.env.NODE_ENV === "development") {
+                        console.error(error);
+                    }
+                },
+            },
+        );
+    };
+
+    const ensureRegion = (region: Region) => {
+        if (!IS_SERVER) {
+            const { regionId, countryCode } = getRegion() || {
+                regionId: region.id,
+                countryCode: region.countries[0].iso_2,
+            };
+
+            if (regionId !== region.id) {
+                setRegion(region.id, countryCode);
+            }
+
+            storeRegion(region.id, countryCode);
+            setCountryCode(countryCode);
+        }
+    };
+
+    const storeCart = (id: string) => {
+        if (!IS_SERVER) {
+            localStorage.setItem(CART_KEY, id);
+        }
+    };
+
+    const getCart = () => {
+        if (!IS_SERVER) {
+            return localStorage.getItem(CART_KEY);
+        }
+        return null;
+    };
+
+    const deleteCart = () => {
+        if (!IS_SERVER) {
+            localStorage.removeItem(CART_KEY);
+        }
+    };
+
+    const createNewCart = async (regionId?: string) => {
+        await createMutateAsync(
+            {
+                resource: "carts",
+                values: {
+                    region_id: regionId,
+                },
+            },
+            {
+                onSuccess: ({ data: { cart } }) => {
+                    setCartId(cart.id);
+                    storeCart(cart.id);
+                    ensureRegion(cart.region);
+                },
+                onError: (error) => {
+                    if (process.env.NODE_ENV === "development") {
+                        console.error(error);
+                    }
+                },
+            },
+        );
+    };
+
+    const resetCart = () => {
+        deleteCart();
+
+        const savedRegion = getRegion();
+
+        createMutateAsync(
+            {
+                resource: "carts",
+                values: {
+                    region_id: savedRegion?.regionId,
+                },
+            },
+            {
+                onSuccess: ({ data: { cart } }) => {
+                    //TODO: make sure to create a new cart if the old one is deleted
+                    storeCart(cart.id);
+                    ensureRegion(cart.region);
+                },
+                onError: (error) => {
+                    if (process.env.NODE_ENV === "development") {
+                        console.error(error);
+                    }
+                },
+            },
+        );
+    };
+
+    useEffect(() => {
+        const ensureCart = async () => {
+            const cartId = getCart();
+            const region = getRegion();
+
+            if (cartId) {
+                if (!cart || cart.completed_at) {
+                    deleteCart();
+                    await createNewCart(region?.regionId);
+                    return;
+                }
+                // must be sure that cart resource would validated
+                ensureRegion(cart.region);
+            } else {
+                await createNewCart(region?.regionId);
+            }
+        };
+
+        if (!IS_SERVER && !cartId) {
+            ensureCart();
+        }
+    }, []);
+
+    const addItem = ({
+        variantId,
+        quantity,
+    }: {
+        variantId: string;
+        quantity: number;
+    }) => {
+        createMutate(
+            {
+                resource: `carts/${cartId}/line-items`,
+                values: {
+                    variant_id: variantId,
+                    quantity: quantity,
+                },
+                invalidates: ["detail"],
+            },
+            {
+                onSuccess: ({ data: { cart } }) => {
+                    // TODO: make sure to update the cart in the store with the new line item
+                    storeCart(cart.id);
+                },
+                onError: (error) => {
+                    console.error(error);
+                },
+            },
+        );
+    };
+
+    const deleteItem = (lineId: string) => {
+        deleteMutate(
+            {
+                resource: `carts/${cartId}/line-items`,
+                id: lineId,
+                invalidates: ["detail"],
+            },
+            {
+                onSuccess: ({ data: { cart } }) => {
+                    storeCart(cart.id);
+                },
+                onError: (error) => {
+                    console.log(error);
+                },
+            },
+        );
+    };
+
+    const updateItem = ({
+        lineId,
+        quantity,
+    }: {
+        lineId: string;
+        quantity: number;
+    }) => {
+        updateMutate(
+            {
+                resource: `carts/${cartId}/line-items`,
+                id: lineId,
+                values: {
+                    quantity: quantity,
+                },
+                invalidates: ["detail"],
+            },
+            {
+                onSuccess: ({ data: { cart } }) => {
+                    // make sure that cart resource would validated when updated with new quantity
+                    storeCart(cart.id);
+                },
+                onError: (error) => {
+                    console.log(error);
+                },
+            },
+        );
     };
 
     return (
-        <CartContext.Provider value={{ addToCart, isLoading, cartId }}>
+        <CartContext.Provider
+            value={{
+                countryCode,
+                setRegion,
+                addItem,
+                deleteItem,
+                updateItem,
+                resetCart,
+                cartId,
+                cart,
+            }}
+        >
             {children}
         </CartContext.Provider>
     );
