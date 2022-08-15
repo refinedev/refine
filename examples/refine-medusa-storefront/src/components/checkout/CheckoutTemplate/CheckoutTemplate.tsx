3ffe0cf87a23503e5cc0948ff1a2e7165340a7ea
Salih <salihozdemir94@gmail.com>
2022-08-15 10:20:47 +0200

    fix: add layoutwrapper

--- examples/refine-medusa-storefront/src/components/checkout/CheckoutTemplate/CheckoutTemplate.tsx
+++ examples/refine-medusa-storefront/src/components/checkout/CheckoutTemplate/CheckoutTemplate.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
+import { LayoutWrapper } from "@pankod/refine-core";
 
 import { CheckoutProvider } from "@lib/context/checkout";
-import { ChevronDown } from "@components/icons";
 import CheckoutForm from "../CheckoutForm";
 import CheckoutLoader from "../CheckoutLoader";
 import CheckoutSummary from "../CheckoutSummary";
@@ -9,42 +8,17 @@ import CheckoutSummary from "../CheckoutSummary";
 const CheckoutTemplate: React.FC = () => {
     return (
         <CheckoutProvider>
-            <div className="small:min-h-screen relative bg-gray-100">
-                <div className="h-16 bg-white">
-                    <nav className="content-container flex h-full items-center justify-between">
-                        <Link
-                            href="/cart"
-                            className="text-small-semi flex flex-1 basis-0 items-center gap-x-2 uppercase text-gray-700"
-                        >
-                            <ChevronDown className="rotate-90" size={16} />
-                            <span className="small:block mt-px hidden">
-                                Back to shopping cart
-                            </span>
-                            <span className="small:hidden mt-px block">
-                                Back
-                            </span>
-                        </Link>
-                        <Link className="text-xl-semi" href="/">
-                            ACME
-                        </Link>
-                        <div className="flex-1 basis-0" />
-                    </nav>
-                </div>
-                <div className="relative">
-                    <CheckoutLoader />
-                    <div className="small:grid-cols-[1fr_416px] content-container grid grid-cols-1 gap-y-8 gap-x-8 py-12">
-                        <CheckoutForm />
-                        <CheckoutSummary />
-                    </div>
-                </div>
-                <div className="flex w-full items-center justify-center py-4">
-                    <div className="flex w-full items-center justify-center py-4">
-                        <div className="content-container flex flex-1 justify-center">
-                            Refine
+            <LayoutWrapper>
+                <div className="small:min-h-screen relative bg-gray-100">
+                    <div className="relative">
+                        <CheckoutLoader />
+                        <div className="small:grid-cols-[1fr_416px] content-container grid grid-cols-1 gap-y-8 gap-x-8 py-12">
+                            <CheckoutForm />
+                            <CheckoutSummary />
                         </div>
                     </div>
                 </div>
-            </div>
+            </LayoutWrapper>
         </CheckoutProvider>
     );
 };
