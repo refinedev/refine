77f26a0bc9f4e18e9ac454ead4e6251c41699fa7
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-08-17 16:04:41 +0300

    feat: add cloud-query service

---
+++ packages/sdk/src/services/cloud-query.ts
@@ -0,0 +1,19 @@
+import { Client } from "../client";
+
+class CloudQuery {
+    private client: Client;
+
+    constructor(client: Client) {
+        this.client = client;
+    }
+
+    async run(data: { key: string; config: any }): Promise<any> {
+        return await this.client.call({
+            method: "post",
+            url: "/data-sources/connections/run",
+            data,
+        });
+    }
+}
+
+export { CloudQuery };
