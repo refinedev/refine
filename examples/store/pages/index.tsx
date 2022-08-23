dda6a5773fe71bb7410856344b75482bfb0965a2
Salih <salihozdemir94@gmail.com>
2022-08-23 12:11:27 +0300

    fix: by default, create cart server side

--- examples/store/pages/index.tsx
+++ examples/store/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from "next";
 export { NextRouteComponent as default } from "@pankod/refine-nextjs-router";
 import { dataProvider } from "@pankod/refine-medusa";
-import { Product } from "@medusajs/medusa";
+import { Product, StoreCartsRes } from "@medusajs/medusa";
 import nookies from "nookies";
 
 import { getSearchStaticProps } from "@lib/search-props";
@@ -11,27 +11,72 @@ import { CART_KEY } from "@lib/context";
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const cookies = nookies.get(context);
 
-    try {
-        const searchProps = await getSearchStaticProps();
-        const medusaDataProvider = dataProvider(API_URL);
+    const medusaDataProvider = dataProvider(API_URL);
 
-        const products = await medusaDataProvider.getList<Product>({
-            resource: "products",
-            filters: [
-                {
-                    field: "cart_id",
-                    value: cookies[CART_KEY],
-                    operator: "eq",
-                },
-            ],
+    const getRegion = () => {
+        const region = cookies["medusa_region"];
+
+        if (region) {
+            return JSON.parse(region) as {
+                regionId: string;
+                countryCode: string;
+            };
+        }
+        return null;
+    };
+
+    const createNewCart = async () => {
+        const region = getRegion();
+
+        const { data } = await medusaDataProvider.create<StoreCartsRes>({
+            resource: "carts",
+            variables: {
+                region_id: region?.regionId,
+            },
         });
 
-        return {
-            props: {
-                initialData: products,
-                ...searchProps.props,
-            },
-        };
+        nookies.set(context, CART_KEY, data.cart.id);
+        nookies.set(
+            context,
+            "medusa_region",
+            JSON.stringify({
+                regionId: data.cart.region.id,
+                countryCode:
+                    region?.countryCode ?? data.cart.region.countries[0].iso_2,
+            }),
+        );
+
+        return data.cart;
+    };
+
+    try {
+        const searchProps = await getSearchStaticProps();
+
+        const cartId = cookies[CART_KEY];
+
+        if (!cartId) {
+            const cart = await createNewCart();
+
+            const products = await medusaDataProvider.getList<Product>({
+                resource: "products",
+                filters: [
+                    {
+                        field: "cart_id",
+                        value: cart.id,
+                        operator: "eq",
+                    },
+                ],
+            });
+
+            return {
+                props: {
+                    initialData: products,
+                    ...searchProps.props,
+                },
+            };
+        }
+
+        return { props: {} };
     } catch (error) {
         return { props: {} };
     }
--- examples/store/pages/search/index.tsx
+++ examples/store/pages/search/index.tsx
@@ -7,7 +7,7 @@ import {
 import { dataProvider } from "@pankod/refine-medusa";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
-import { Product } from "@medusajs/medusa";
+import { Product, StoreCartsRes } from "@medusajs/medusa";
 import nookies from "nookies";
 
 import Search from "@components/search";
@@ -53,35 +53,78 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const cookies = nookies.get(context);
     const { query } = context;
 
+    const medusaDataProvider = dataProvider(API_URL);
+
+    const getRegion = () => {
+        const region = cookies["medusa_region"];
+
+        if (region) {
+            return JSON.parse(region) as {
+                regionId: string;
+                countryCode: string;
+            };
+        }
+        return null;
+    };
+
+    const createNewCart = async () => {
+        const region = getRegion();
+
+        const { data } = await medusaDataProvider.create<StoreCartsRes>({
+            resource: "carts",
+            variables: {
+                region_id: region?.regionId,
+            },
+        });
+
+        nookies.set(context, CART_KEY, data.cart.id);
+        nookies.set(
+            context,
+            "medusa_region",
+            JSON.stringify({
+                regionId: data.cart.region.id,
+                countryCode:
+                    region?.countryCode ?? data.cart.region.countries[0].iso_2,
+            }),
+        );
+
+        return data.cart;
+    };
+
     try {
         const searchStaticProps = await getSearchStaticProps();
 
         const { q } = query;
+        const cartId = cookies[CART_KEY];
 
-        const medusaDataProvider = dataProvider(API_URL);
+        if (!cartId) {
+            const cart = await createNewCart();
 
-        const data = await medusaDataProvider.getList<Product>({
-            resource: "products",
-            filters: [
-                {
-                    field: "q",
-                    operator: "eq",
-                    value: q,
+            const data = await medusaDataProvider.getList<Product>({
+                resource: "products",
+                filters: [
+                    {
+                        field: "q",
+                        operator: "eq",
+                        value: q,
+                    },
+                    {
+                        field: "cart_id",
+                        value: cart.id,
+                        operator: "eq",
+                    },
+                ],
+            });
+
+            return {
+                props: {
+                    initialData: data,
+                    ...searchStaticProps.props,
                 },
-                {
-                    field: "cart_id",
-                    value: cookies[CART_KEY],
-                    operator: "eq",
-                },
-            ],
-        });
+            };
+        }
 
-        return {
-            props: {
-                initialData: data,
-                ...searchStaticProps.props,
-            },
-        };
+        return { props: {} };
     } catch (error) {
         return { props: {} };
     }
