0f785b502bb8c180e4fef543857222491de21725
ozkalai <melihozkalayy@gmail.com>
2022-08-26 17:16:49 +0300

    docs(integration):  large card component and exports

---
+++ documentation/src/components/integrations/index.ts
@@ -0,0 +1,2 @@
+export { default as Card } from "./card";
+export { default as LargeCard } from "./large-card";
---
+++ documentation/src/components/integrations/large-card/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+type LargeCardProps = {
+    title: string;
+    description: string;
+    imageUrl: string;
+    linkUrl: string;
+};
+
+const LargeCard: React.FC<LargeCardProps> = ({
+    title,
+    description,
+    imageUrl,
+    linkUrl,
+}) => {
+    return (
+        <a href={linkUrl} className={styles.cardWrapper}>
+            <div className={styles.card}>
+                <div className={styles.cardHeader}>
+                    <img src={imageUrl} />
+                    <div>{title}</div>
+                </div>
+                <div className={styles.cardContent}>{description}</div>
+            </div>
+        </a>
+    );
+};
+
+export default LargeCard;
