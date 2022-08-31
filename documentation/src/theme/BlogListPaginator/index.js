cffd9293ef8dbf360417ea2bf22bdb3cf5489102
Salih <salihozdemir94@gmail.com>
2022-08-31 13:16:16 +0300

    feat: add paginator component

--- documentation/src/theme/BlogListPaginator/index.js
+++ documentation/src/theme/BlogListPaginator/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Translate, { translate } from "@docusaurus/Translate";
+import { translate } from "@docusaurus/Translate";
 import Link from "@docusaurus/Link";
-import PaginatorNavLink from "@theme/PaginatorNavLink";
 
 import { ChevronLeft, ChevronRight } from "../../components/blog/icons";
 import { usePagination, DOTS } from "../../hooks/use-pagination";
+import clsx from "clsx";
 
 export default function BlogListPaginator(props) {
     const { metadata } = props;
@@ -20,44 +20,77 @@ export default function BlogListPaginator(props) {
 
     return (
         <nav
-            className="flex justify-end"
+            className="flex justify-end font-montserrat"
             aria-label={translate({
                 id: "theme.blog.paginator.navAriaLabel",
                 message: "Blog list page navigation",
                 description: "The ARIA label for the blog pagination",
             })}
         >
-            <ul className="inline-flex items-center list-none">
+            <ul className="flex space-x-2 items-center list-none">
                 <li>
-                    <Link className="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                        <span className="sr-only">Previous</span>
-                        <ChevronLeft />
+                    <Link
+                        to={
+                            currentPage === 1
+                                ? ""
+                                : `/blog/page/${currentPage - 1}`
+                        }
+                        className="hover:no-underline"
+                    >
+                        <div
+                            className={clsx("pagination-item", {
+                                disabled: currentPage === 1,
+                            })}
+                        >
+                            <span className="sr-only">Previous</span>
+                            <ChevronLeft />
+                        </div>
                     </Link>
                 </li>
                 {paginationRange.map((pageNumber) => {
                     if (pageNumber === DOTS) {
-                        return (
-                            <li className="pagination-item dots">&#8230;</li>
-                        );
+                        return <li className="dots">&#8230;</li>;
                     }
 
                     return (
-                        <li
-                            key={pageNumber}
-                            className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            {pageNumber}
+                        <li key={pageNumber}>
+                            <Link
+                                to={
+                                    pageNumber === 1
+                                        ? "/blog"
+                                        : `/blog/page/${pageNumber}`
+                                }
+                                className="hover:no-underline"
+                            >
+                                <div
+                                    className={clsx("pagination-item", {
+                                        active: pageNumber === currentPage,
+                                    })}
+                                >
+                                    {pageNumber}
+                                </div>
+                            </Link>
                         </li>
                     );
                 })}
                 <li>
-                    <a
-                        href="#"
-                        className="block py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    <Link
+                        to={
+                            currentPage === lastPage
+                                ? ""
+                                : `/blog/page/${currentPage + 1}`
+                        }
+                        className="hover:no-underline"
                     >
-                        <span className="sr-only">Next</span>
-                        <ChevronRight />
-                    </a>
+                        <div
+                            className={clsx("pagination-item", {
+                                disabled: currentPage === lastPage,
+                            })}
+                        >
+                            <span className="sr-only">Next</span>
+                            <ChevronRight />
+                        </div>
+                    </Link>
                 </li>
             </ul>
         </nav>
