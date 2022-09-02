6c76c616ba17d1f70221572e06a8ea50435c8602
Ömer Faruk APLAK <omer@pankod.com>
2022-09-02 08:17:24 +0300

    fix: update image domains

--- examples/store/next.config.js
+++ examples/store/next.config.js
@@ -5,9 +5,6 @@ module.exports = withPlugins([], {
         newNextLinkBehavior: true,
     },
     images: {
-        domains: [
-            "medusa-public-images.s3.eu-west-1.amazonaws.com",
-            "refine-store.fra1.cdn.digitaloceanspaces.com",
-        ],
+        domains: ["refine-store.fra1.cdn.digitaloceanspaces.com"],
     },
 });
