cbff3c1968bf53f854772510270461a6c7bffe88
ozkalai <melihozkalayy@gmail.com>
2022-09-02 13:43:34 +0300

    fix: auth provider

--- examples/authProvider/googleLogin/src/App.tsx
+++ examples/authProvider/googleLogin/src/App.tsx
@@ -6,8 +6,6 @@ import {
 } from "@pankod/refine-antd";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
-
-import { useGoogleLogout, GoogleLoginResponse } from "react-google-login";
 import axios from "axios";
 
 import "@pankod/refine-antd/dist/styles.min.css";
@@ -16,53 +14,44 @@ import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
 import { Login } from "pages/login";
 
 const API_URL = "https://api.fake-rest.refine.dev";
-const clientId =
-    "149954872426-ga5qkfj6v6fjr98p4lbakvf8u6mgtnp6.apps.googleusercontent.com";
 
 const App: React.FC = () => {
-    const { signOut } = useGoogleLogout({
-        clientId,
-        cookiePolicy: "single_host_origin",
-    });
-
     const authProvider: AuthProvider = {
-        login: ({ tokenId, profileObj, tokenObj }: GoogleLoginResponse) => {
+        login: ({ credential }: CredentialResponse) => {
             axios.defaults.headers.common = {
-                Authorization: `Bearer ${tokenId}`,
+                Authorization: `Bearer ${credential}`,
             };
 
-            localStorage.setItem(
-                "user",
-                JSON.stringify({ ...profileObj, avatar: profileObj.imageUrl }),
-            );
-            localStorage.setItem("expiresAt", tokenObj.expires_at.toString());
+            localStorage.setItem("token", `${credential}`);
 
             return Promise.resolve();
         },
         logout: () => {
-            signOut();
-            localStorage.removeItem("user");
-            localStorage.removeItem("expiresAt");
+            const token = localStorage.getItem("token");
+
+            if (token && typeof window !== "undefined") {
+                localStorage.removeItem("token");
+                axios.defaults.headers.common = {};
+                window.google?.accounts.id.revoke(token, () => {
+                    return Promise.resolve();
+                });
+            }
+
             return Promise.resolve();
         },
         checkError: () => Promise.resolve(),
         checkAuth: async () => {
-            const expiresAt = localStorage.getItem("expiresAt");
+            const token = localStorage.getItem("token");
 
-            if (expiresAt) {
-                return new Date().getTime() / 1000 < +expiresAt
-                    ? Promise.resolve()
-                    : Promise.reject();
+            if (token) {
+                return Promise.resolve();
             }
             return Promise.reject();
         },
 
         getPermissions: () => Promise.resolve(),
         getUserIdentity: async () => {
-            const user = localStorage.getItem("user");
-            if (user) {
-                return Promise.resolve(JSON.parse(user));
-            }
+            return Promise.resolve({});
         },
     };
 
