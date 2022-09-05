6928b4e5fc2e8fd62dbf7f85166bbeea77a23ccc
Salih <salihozdemir94@gmail.com>
2022-09-05 17:09:55 +0300

    fix: update title fonts

--- documentation/src/theme/BlogPostItems/index.js
+++ documentation/src/theme/BlogPostItems/index.js
@@ -11,7 +11,7 @@ export default function BlogPostItems({
     return (
         <div className="font-montserrat">
             {showTitle && (
-                <h2 className="font-extrabold text-3xl">All Posts</h2>
+                <h2 className="font-extrabold text-3xl font-montserrat">All Posts</h2>
             )}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {items.map(({ content: BlogPostContent }) => (
