337caeb8cc8dbb873c5e28686ee5820433ec700a
Aydın Akyol <aydinakyol11@gmail.com>
2022-09-06 23:13:27 +0300

    feat(mui-example): added  mui auth page example

---
+++ examples/authentication/mui/src/pages/auth/login/index.tsx
@@ -0,0 +1,5 @@
+import { AuthPage } from "@pankod/refine-mui";
+
+export const LoginPage: React.FC = () => {
+    return <AuthPage type="login" remember />;
+};
---
+++ examples/authentication/mui/src/pages/auth/register/index.tsx
@@ -0,0 +1,5 @@
+import { AuthPage } from "@pankod/refine-mui";
+
+export const RegisterPage: React.FC = () => {
+    return <AuthPage type="register" />;
+};
