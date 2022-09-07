8035b58cc72ca92fb28e6fadc16665c5eba60124
ozkalai <melihozkalayy@gmail.com>
2022-09-07 17:53:39 +0300

    feat(ui-types): updated sider with topSection and bottomSection

--- packages/ui-types/src/types/layout.tsx
+++ packages/ui-types/src/types/layout.tsx
@@ -2,7 +2,14 @@ import { TitleProps, LayoutProps } from "@pankod/refine-core";
 
 export type RefineLayoutHeaderProps = {};
 
-export type RefineLayoutSiderProps = {};
+export type RefineLayoutSiderProps = {
+    bottomSection?:
+        | React.ReactNode
+        | ((defaultBottom: React.ReactNode) => React.ReactNode);
+    topSection?:
+        | React.ReactNode
+        | ((defaultTop: React.ReactNode) => React.ReactNode);
+};
 
 export type RefineLayoutFooterProps = {};
 
