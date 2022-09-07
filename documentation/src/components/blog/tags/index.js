373242668e29d917c291f2cd5306f508531007f0
Salih <salihozdemir94@gmail.com>
2022-09-07 10:25:06 +0300

    fix: add link to tags

--- documentation/src/components/blog/tags/index.js
+++ documentation/src/components/blog/tags/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "@docusaurus/Link";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
 
 export const Tags = () => {
@@ -9,12 +10,15 @@ export const Tags = () => {
             <span className="font-bold">Tags: </span>
             <div className="inline-flex flex-wrap gap-2">
                 {metadata.tags.map((tag, index) => (
-                    <span
+                    <Link
                         key={index}
-                        className="bg-[#F6F6F9] px-2 capitalize text-sm rounded border border-solid border-[#2A2A42] text-[#2A2A42] cursor-default"
+                        to={tag.permalink}
+                        className="no-underline"
                     >
-                        {tag.label}
-                    </span>
+                        <span className="bg-[#F6F6F9] px-2 py-1 capitalize text-sm rounded border border-solid border-[#2A2A42] text-[#2A2A42] hover:text-[#1890FF] hover:border-[#1890FF] transition duration-150">
+                            {tag.label}
+                        </span>
+                    </Link>
                 ))}
             </div>
         </div>
