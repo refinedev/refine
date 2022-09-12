545058f6e1b04a821fc2d2339b0b52d90a3f606e
Ali Emir Sen <senaliemir@gmail.com>
2022-09-12 18:19:16 +0300

    docs(navbar): update breakpoints

--- documentation/src/theme/Navbar/Content/index.js
+++ documentation/src/theme/Navbar/Content/index.js
@@ -31,7 +31,7 @@ function NavbarItems({ items }) {
 function NavbarContentLayout({ left, right }) {
     return (
         <div className="navbar__inner">
-            <div className="navbar__items flex gap-0 lg:gap-8 font-montserrat font-semibold with-hoverline">
+            <div className="navbar__items flex gap-0 lg:gap-3 xl:gap-6 font-montserrat font-semibold with-hoverline">
                 {left}
             </div>
             <div className="navbar__items navbar__items--right flex gap-4">
