4a1924955e0996ca78c0952870e0e29bca10c370
ozkalai <melihozkalayy@gmail.com>
2022-09-14 18:08:12 +0300

    chore: team and community data

---
+++ documentation/src/assets/community.ts
@@ -0,0 +1,26 @@
+export const community = [
+    {
+        title: "VISIT OUR",
+        subtitle: "GITHUB REPO",
+        iconPath: "/icons/github.svg",
+        url: "https://github.com/pankod/refine",
+    },
+    {
+        title: "JOIN OUR",
+        subtitle: "DISCORD SERVER",
+        iconPath: "/icons/discord.svg",
+        url: "https://discord.gg/refine",
+    },
+    {
+        title: "FOLLOW US",
+        subtitle: "ON REDDIT",
+        iconPath: "/icons/reddit.svg",
+        url: "https://www.reddit.com/r/refine/",
+    },
+    {
+        title: "FOLLOW US",
+        subtitle: "ON TWITTER",
+        iconPath: "/icons/twitter.svg",
+        url: "https://twitter.com/refine_dev",
+    },
+];
