3e9e5d5bf80cd8ff190f5a7c1fb018cfa8785564
Ali Emir Sen <senaliemir@gmail.com>
2022-09-14 12:53:08 +0300

    docs(navbar): update icon

--- documentation/src/theme/Navbar/Logo/index.js
+++ documentation/src/theme/Navbar/Logo/index.js
@@ -1,16 +1,21 @@
 import React from "react";
-import Logo from "@theme/Logo";
+import Link from "@docusaurus/Link";
+
+import { RefineIcon } from "../../../components/landing/icons/refine-icon";
+
 export default function NavbarLogo({ className, ...props }) {
     return (
-        <Logo
-            className={
-                className
-                    ? className
-                    : "select-none mx-auto pr-6 lg:ml-0 lg:mr-2 lg:pr-0 items-center flex min-w-0"
-            }
-            imageClassName="navbar__logo"
-            titleClassName="navbar__title text--truncate"
-            {...props}
-        />
+        <Link to="/">
+            <RefineIcon
+                className={
+                    className
+                        ? className
+                        : "select-none mx-auto pr-6 lg:ml-0 lg:mr-2 lg:pr-0 items-center flex min-w-[110px] max-w-[110px]"
+                }
+                imageClassName="navbar__logo"
+                titleClassName="navbar__title text--truncate"
+                {...props}
+            />
+        </Link>
     );
 }
