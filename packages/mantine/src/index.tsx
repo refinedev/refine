d7d1e54f74521c2a3e61a7c44e505157e785381c
Salih <salihozdemir94@gmail.com>
2022-09-14 12:01:20 +0300

    feat: add useForm hook with mantine hook package

--- packages/mantine/src/index.tsx
+++ packages/mantine/src/index.tsx
@@ -8,4 +8,5 @@ export * from "./components";
 export * from "./providers";
 
 export { List, ListProps } from "./components";
+export * from "./hooks";
 export { MantineProvider, MantineProviderProps } from "./providers";
