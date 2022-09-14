56d4d25ac9306e67c3e8f2d5fa9774e14975f038
ozkalai <melihozkalayy@gmail.com>
2022-09-14 18:08:12 +0300

    chore: team and community data

---
+++ documentation/src/assets/team.ts
@@ -0,0 +1,66 @@
+export const team = [
+    {
+        name: "Ömer Aplak",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Co-Founder",
+        role2: "CTO",
+        url: "https://www.linkedin.com/in/%C3%B6mer-faruk-aplak-14b87099/",
+    },
+    {
+        name: "Civan Özseyhan",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Co-Founder",
+        role2: "CEO",
+        url: "https://www.linkedin.com/in/civan-%C3%B6zseyhan-1ab48814/",
+    },
+    {
+        name: "Eren Erkalkan",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Co-Founder",
+        role2: "Head of Product",
+        url: "https://www.linkedin.com/in/eren-erkalkan-1b31a89a/",
+    },
+    {
+        name: "Ali Emir Şen",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Frontend Team Lead",
+        url: "https://www.linkedin.com/in/aliemir/",
+    },
+    {
+        name: "Yıldıray Ünlü",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Backend Team Lead",
+        url: "https://www.linkedin.com/in/yildirayunlu/",
+    },
+    {
+        name: "Necati Ozmen",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Software Developer",
+        role2: "Head of Content",
+        url: "https://www.linkedin.com/in/necatiozmen/",
+    },
+    {
+        name: "Aydın Akyol",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Software Developer",
+        url: "https://www.linkedin.com/in/aydin-akyol/",
+    },
+    {
+        name: "Melih Özkalay",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Software Developer",
+        url: "https://www.linkedin.com/in/melihozkalay/",
+    },
+    {
+        name: "Salih Özdemir",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "Software Developer",
+        url: "https://www.linkedin.com/in/salihozdemr/",
+    },
+    {
+        name: "Şerif Ali Ünal",
+        avatar: "/assets/team-avatars/omer.jpg",
+        role1: "UI/UX Designer",
+        url: "https://www.linkedin.com/in/serifaliunal/",
+    },
+];
