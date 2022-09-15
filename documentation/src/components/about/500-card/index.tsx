0a15d415e8a01d2dbeb02c7860e1c46461fc6662
ozkalai <melihozkalayy@gmail.com>
2022-09-15 18:15:33 +0300

    docs: 500-card component

---
+++ documentation/src/components/about/500-card/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export const Card500: React.FC = () => {
+    return (
+        <a href="linkedin.com/refine" className={styles.container}>
+            <div className={styles.card}>
+                <img className={styles.icon} src="/icons/500.svg" />
+                <p className={styles.text}>
+                    500
+                    <br />
+                    <div className={styles.emerging}>Emerging Europe</div>
+                </p>
+            </div>
+        </a>
+    );
+};
