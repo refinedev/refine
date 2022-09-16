3a695afed3515b315c8469cfbb52ec929d6e9ace
Ali Emir Şen <senaliemir@gmail.com>
2022-09-16 18:46:42 +0300

    chore: rename sider and export

--- packages/mantine/src/components/index.ts
+++ packages/mantine/src/components/index.ts
@@ -1,5 +1,12 @@
 export * from "./ring-countdown";
+
 export * from "./fields";
+
 export * from "./buttons";
-export * from "./layout";
+
 export * from "./crud";
+
+export * from "./layout";
+export * from "./layout/sider";
+export * from "./layout/header";
+export * from "./layout/sider";
