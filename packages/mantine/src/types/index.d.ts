027b472df287b5c143d838d0d263f6f520e8e329
Salih <salihozdemir94@gmail.com>
2022-09-19 10:51:48 +0300

    fix: theme declare module issues

--- packages/mantine/src/types/index.d.ts
+++ packages/mantine/src/types/index.d.ts
@@ -1,14 +1,4 @@
-import { Tuple, DefaultMantineColor } from "@mantine/core";
-
 declare module "*.svg" {
     const content: string;
     export default content;
-}
-
-type ExtendedCustomColors = "primary" | DefaultMantineColor;
-
-declare module "@mantine/core" {
-    export interface MantineThemeColorsOverride {
-        colors: Record<ExtendedCustomColors, Tuple<string, 10>>;
-    }
 }
