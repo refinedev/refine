342dde129d649d10420f3c4e4ad69f1633d7e812
Ömer Faruk APLAK <omer@pankod.com>
2022-09-21 16:43:39 +0300

    docs: fix nofollow attr

--- documentation/src/theme/MDXComponents/A.js
+++ documentation/src/theme/MDXComponents/A.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Link from "@docusaurus/Link";
+import isInternalUrl from "@docusaurus/isInternalUrl";
+
 export default function MDXA(props) {
     let rel = "noopener noreferrer";
-    if (props.href?.includes("github.com/pankod/refine")) {
+    const isInternalURL = isInternalUrl(props?.href);
+    const isGitHubRepoURL = props.href?.includes("github.com/pankod/refine");
+    if (isGitHubRepoURL) {
         rel = "noopener";
+    } else if (!isInternalURL) {
+        rel = `${rel} nofollow`;
     }
+
     return <Link {...props} rel={rel} />;
 }
