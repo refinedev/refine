26045844ed194ad64c8e905e533883a52874b0d8
Ömer Faruk APLAK <omer@pankod.com>
2022-09-21 11:24:38 +0300

    docs: fix includes function usage

--- documentation/src/theme/MDXComponents/A.js
+++ documentation/src/theme/MDXComponents/A.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "@docusaurus/Link";
 export default function MDXA(props) {
     let rel = "noopener noreferrer";
-    if (props.href.includes("github.com/pankod/refine")) {
+    if (props.href?.includes("github.com/pankod/refine")) {
         rel = "noopener";
     }
     return <Link {...props} rel={rel} />;
--- documentation/src/theme/NavbarItem/DefaultNavbarItem.js
+++ documentation/src/theme/NavbarItem/DefaultNavbarItem.js
@@ -6,7 +6,7 @@ function DefaultNavbarItemDesktop({
     isDropdownItem = false,
     ...props
 }) {
-    const isGitHubLink = props?.href?.includes?.("github.com/pankod/refine");
+    const isGitHubLink = props?.href?.includes("github.com/pankod/refine");
     const element = (
         <NavbarNavLink
             className={clsx(
