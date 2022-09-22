5f2a38cbeee7ff579f073e2f11aef49fd845ef49
Salih <salihozdemir94@gmail.com>
2022-09-22 12:21:33 +0300

    feat: add mantine base example

---
+++ examples/base/mantine/src/pages/posts/edit.tsx
@@ -0,0 +1,68 @@
+import {
+    Edit,
+    Select,
+    TextInput,
+    useForm,
+    useSelect,
+    RichTextEditor,
+    Text,
+} from "@pankod/refine-mantine";
+
+import { ICategory } from "../../interfaces";
+
+export const PostEdit: React.FC = () => {
+    const {
+        saveButtonProps,
+        getInputProps,
+        refineCore: { queryResult },
+    } = useForm({
+        initialValues: {
+            title: "",
+            status: "",
+            category: {
+                id: "",
+            },
+            content: "",
+        },
+    });
+
+    const { selectProps } = useSelect<ICategory>({
+        resource: "categories",
+        defaultValue: queryResult?.data?.data.category.id,
+    });
+
+    return (
+        <Edit saveButtonProps={saveButtonProps}>
+            <form>
+                <TextInput
+                    mt={8}
+                    label="Title"
+                    placeholder="Title"
+                    {...getInputProps("title")}
+                />
+                <Select
+                    mt={8}
+                    label="Status"
+                    placeholder="Pick one"
+                    {...getInputProps("status")}
+                    data={[
+                        { label: "Published", value: "published" },
+                        { label: "Draft", value: "draft" },
+                        { label: "Rejected", value: "rejected" },
+                    ]}
+                />
+                <Select
+                    mt={8}
+                    label="Category"
+                    placeholder="Pick one"
+                    {...getInputProps("category.id")}
+                    {...selectProps}
+                />
+                <Text mt={8} weight={500} size="sm" color="#212529">
+                    Content
+                </Text>
+                <RichTextEditor {...getInputProps("content")} />
+            </form>
+        </Edit>
+    );
+};
