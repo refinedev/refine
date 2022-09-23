a1c62aab56ccaff305ef16f983704e3999773a4f
Ali Emir Sen <senaliemir@gmail.com>
2022-09-23 10:18:44 +0300

    docs: remove `react-live`

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -85,7 +85,6 @@ const siteConfig = {
             },
         ],
     ],
-    themes: ["@docusaurus/theme-live-codeblock"],
     themeConfig: {
         prism: {
             magicComments: [
--- documentation/src/theme/Playground/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import type { Props } from "@theme/Playground";
-export default function Playground({
-    children,
-    transformCode,
-    ...props
-}: Props): JSX.Element;
