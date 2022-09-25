63b025c724b0c48ae132c7589215116ab4c64aec
Ali Emir Sen <senaliemir@gmail.com>
2022-09-25 12:27:46 +0300

    docs(live-preview): run conditional once

--- documentation/src/components/conditional/index.tsx
+++ documentation/src/components/conditional/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 type Props = {
     if: boolean;
     children?: () => JSX.Element;
@@ -7,9 +9,27 @@ export const Conditional = ({
     if: condition,
     children,
 }: Props): JSX.Element => {
+    const [show, setShow] = React.useState(condition);
+
+    React.useEffect(() => {
+        if (!show && condition) {
+            setShow(true);
+        }
+    }, [condition]);
+
+    const memoized = React.useMemo(() => {
+        if (!show) {
+            return null;
+        }
+        if (typeof children === "function") {
+            return children();
+        }
+        return null;
+    }, [show]);
+
     if (typeof children !== "undefined" && typeof children !== "function") {
         throw new Error("Conditional component requires a function as a child");
     }
 
-    return condition ? children?.() : null;
+    return memoized;
 };
