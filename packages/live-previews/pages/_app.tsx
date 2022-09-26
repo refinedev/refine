89dfbd87bc07d43f3d4c47cf46e8e9bb5927b673
Ali Emir Sen <senaliemir@gmail.com>
2022-09-27 01:47:51 +0300

    feat(live-previews): initialize live-previews app

---
+++ packages/live-previews/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from "next/app";
+import "@/src/styles/global.css";
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+    return <Component {...pageProps} />;
+}
+
+export default App;
