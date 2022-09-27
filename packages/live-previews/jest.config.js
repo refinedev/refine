de5a2fafe5612e4dd698f2719b7026069d84e499
Ali Emir Sen <senaliemir@gmail.com>
2022-09-27 13:34:09 +0300

    test(live-previews): fix jest

--- packages/live-previews/jest.config.js
+++ packages/live-previews/jest.config.js
@@ -1,5 +1,10 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    moduleNameMapper: {
+        ...pathsToModuleNameMapper(paths, { prefix: "<rootDir>/" }),
+    },
 };
