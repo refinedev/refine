a8376aab7f287609d728a8bcc9b39ed62efd0a43
Ali Emir Sen <senaliemir@gmail.com>
2022-09-27 14:09:33 +0300

    test(live-previews): fix jest issues

--- packages/live-previews/jest.config.js
+++ packages/live-previews/jest.config.js
@@ -6,8 +6,17 @@ const paths = compilerOptions.paths ? compilerOptions.paths : {};
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: "ts-jest",
+    rootDir: "./",
+    globals: {
+        "ts-jest": {
+            tsconfig: "<rootDir>/tsconfig.test.json",
+        },
+    },
     testEnvironment: "node",
     moduleNameMapper: {
         ...pathsToModuleNameMapper(paths, { prefix: "<rootDir>/" }),
     },
+    testEnvironment: "jsdom",
+    name: "live-previews",
+    displayName: "live-previews",
 };
