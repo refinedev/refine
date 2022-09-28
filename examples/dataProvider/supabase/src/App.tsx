9f2c9fbd87bb9dd6eabcfff4f89c2c2835775d6c
necatiozmen <necatiozmn@gmail.com>
2022-09-28 15:53:32 +0300

    refactor: add auth component

--- examples/dataProvider/supabase/src/App.tsx
+++ examples/dataProvider/supabase/src/App.tsx
@@ -4,19 +4,38 @@ import {
     Layout,
     ErrorComponent,
     AuthPage,
+    notification,
 } from "@pankod/refine-antd";
 import { dataProvider, liveProvider } from "@pankod/refine-supabase";
 import routerProvider from "@pankod/refine-react-router-v6";
+import { GoogleOutlined } from "@ant-design/icons";
 
 import "@pankod/refine-antd/dist/styles.min.css";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
-import { Login } from "pages/login";
 import { supabaseClient } from "utility";
 
 const authProvider: AuthProvider = {
-    login: async ({ email, password }) => {
+    login: async ({ email, password, providerName }) => {
         const { user, error } = await supabaseClient.auth.signIn({
+            email,
+            password,
+            provider: providerName,
+        });
+
+        if (error) {
+            return Promise.reject(error);
+        }
+
+        if (user) {
+            return Promise.resolve();
+        }
+
+        // for third-party login
+        return Promise.resolve(false);
+    },
+    register: async ({ email, password }) => {
+        const { user, error } = await supabaseClient.auth.signUp({
             email,
             password,
         });
@@ -27,6 +46,37 @@ const authProvider: AuthProvider = {
 
         if (user) {
             return Promise.resolve();
+        }
+    },
+    forgotPassword: async ({ email }) => {
+        const { data, error } =
+            await supabaseClient.auth.api.resetPasswordForEmail(email, {
+                redirectTo: `${window.location.origin}/update-password`,
+            });
+
+        if (error) {
+            return Promise.reject(error);
+        }
+
+        if (data) {
+            notification.open({
+                type: "success",
+                message: "Success",
+                description:
+                    "Please check your email for a link to reset your password. If it doesn't appear within a few minutes, check your spam folder.",
+            });
+            return Promise.resolve();
+        }
+    },
+    updatePassword: async ({ password }) => {
+        const { data, error } = await supabaseClient.auth.update({ password });
+
+        if (error) {
+            return Promise.reject(error);
+        }
+
+        if (data) {
+            return Promise.resolve("/");
         }
     },
     logout: async () => {
@@ -91,7 +141,28 @@ const App: React.FC = () => {
                 ],
             }}
             authProvider={authProvider}
-            LoginPage={() => <AuthPage type="register" />}
+            LoginPage={() => (
+                <AuthPage
+                    type="login"
+                    providers={[
+                        {
+                            name: "google",
+                            label: "Sign in with Google",
+                            icon: (
+                                <GoogleOutlined
+                                    style={{ fontSize: 18, lineHeight: 0 }}
+                                />
+                            ),
+                        },
+                    ]}
+                    formProps={{
+                        initialValues: {
+                            email: "info@refine.dev",
+                            password: "refine-supabase",
+                        },
+                    }}
+                />
+            )}
             resources={[
                 {
                     name: "posts",
