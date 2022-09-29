b0ac937df91a74e7b5542a5e72064e8d7b66bb10
Salih <salihozdemir94@gmail.com>
2022-09-29 11:43:54 +0300

    fix: add radius to header avatar

--- packages/mantine/src/components/layout/header/index.tsx
+++ packages/mantine/src/components/layout/header/index.tsx
@@ -17,7 +17,7 @@ export const Header: React.FC<RefineLayoutHeaderProps> = () => {
         <MantineHeader height={50} py={6} px="sm">
             <Group position="right">
                 <MantineTitle order={6}>{user?.name}</MantineTitle>
-                <Avatar src={user?.avatar} alt={user?.name} />
+                <Avatar src={user?.avatar} alt={user?.name} radius="xl" />
             </Group>
         </MantineHeader>
     ) : null;
