1fbd3a8bed54695e0c248d24f01962b89671eae6
Ali Emir Sen <senaliemir@gmail.com>
2022-10-02 23:28:17 +0300

    docs(docgen): add jsdoc definitions to `useShow`

--- packages/core/src/interfaces/live/LiveModeProps.ts
+++ packages/core/src/interfaces/live/LiveModeProps.ts
@@ -2,8 +2,17 @@ import { LiveEvent } from ".";
 import { BaseKey } from "..";
 
 export type LiveModeProps = {
+    /**
+     * Live interaction mode to be used when `liveProvider` is set in `Refine` component
+     */
     liveMode?: "auto" | "manual" | "off";
+    /**
+     * Callback function to be called when a related subscription is triggered
+     */
     onLiveEvent?: (event: LiveEvent) => void;
+    /**
+     * Additional props to be passed to the live provider subscription
+     */
     liveParams?: {
         ids?: BaseKey[];
         [key: string]: any;
