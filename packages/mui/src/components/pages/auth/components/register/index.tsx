d5ff1a5d49d61805dd3f1c9d95880edebbff7d77
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-10-05 22:28:34 +0300

    fix(mui): register form type

--- packages/mui/src/components/pages/auth/components/register/index.tsx
+++ packages/mui/src/components/pages/auth/components/register/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { RegisterFormTypes, RegisterPageProps } from "@pankod/refine-ui-types";
+import { RegisterFormTypes, RegisterPageProps } from "@pankod/refine-core";
 import { useForm } from "@pankod/refine-react-hook-form";
 
 import {
