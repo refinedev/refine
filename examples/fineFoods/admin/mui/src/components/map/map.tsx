ba6288204319f221772e639b4b9e9b84eeda7e24
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2022-10-12 11:07:38 +0300

    fix(examples): fine foods google map not work on react@18 (#2720)
    * feat(example): change mui-fine-foods google map library
    
    * feat(example): change antd-fine-foods google map library
    
    * refactor(mui-fine-foods): move svg icons to public folder
    
    * refactor(antd-fine-foods): move svg icons to public folder

---
+++ examples/fineFoods/admin/mui/src/components/map/map.tsx
@@ -0,0 +1,59 @@
+import {
+    Children,
+    cloneElement,
+    FC,
+    isValidElement,
+    PropsWithChildren,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
+import { Wrapper } from "@googlemaps/react-wrapper";
+
+interface MapProps extends Exclude<google.maps.MapOptions, "center"> {
+    center?: google.maps.LatLngLiteral;
+}
+
+const MapComponent: FC<PropsWithChildren<MapProps>> = ({
+    children,
+    center,
+    zoom = 12,
+    ...options
+}) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const [map, setMap] = useState<google.maps.Map>();
+
+    useEffect(() => {
+        if (map) {
+            map.setOptions({ ...options, zoom, center });
+        }
+    }, [map]);
+
+    useEffect(() => {
+        if (ref.current && !map) {
+            setMap(new window.google.maps.Map(ref.current, {}));
+        }
+    }, [ref, map]);
+
+    return (
+        <>
+            <div ref={ref} style={{ flexGrow: "1", height: "100%" }} />
+            {Children.map(children, (child) => {
+                if (isValidElement(child)) {
+                    // eslint-disable-next-line
+                    return cloneElement<any>(child, { map });
+                }
+            })}
+        </>
+    );
+};
+
+const MapWrapper: FC<PropsWithChildren<MapProps>> = (props) => {
+    return (
+        <Wrapper apiKey={process.env.REACT_APP_MAP_ID}>
+            <MapComponent {...props} />
+        </Wrapper>
+    );
+};
+
+export default MapWrapper;
