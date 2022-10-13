951a0c18b45fbacffc7f3ce40afef67c57a3324a
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2022-10-13 18:51:21 +0530

    fix: code formatting

--- examples/accessControl/casbin/src/App.tsx
+++ examples/accessControl/casbin/src/App.tsx
@@ -1,90 +1,94 @@
-import { Refine } from '@pankod/refine-core';
+import { Refine } from "@pankod/refine-core";
 import {
-  notificationProvider,
-  Layout,
-  ErrorComponent,
-} from '@pankod/refine-antd';
-import dataProvider from '@pankod/refine-simple-rest';
-import routerProvider from '@pankod/refine-react-router-v6';
-import { newEnforcer } from 'casbin';
-import '@pankod/refine-antd/dist/styles.min.css';
+    notificationProvider,
+    Layout,
+    ErrorComponent,
+} from "@pankod/refine-antd";
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { newEnforcer } from "casbin";
+import "@pankod/refine-antd/dist/styles.min.css";
 
-import { model, adapter } from 'accessControl';
-import { Header } from 'components/header';
-import { PostList, PostCreate, PostEdit, PostShow } from 'pages/posts';
-import { UserList, UserCreate, UserEdit, UserShow } from 'pages/users';
+import { model, adapter } from "accessControl";
+import { Header } from "components/header";
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import { UserList, UserCreate, UserEdit, UserShow } from "pages/users";
 import {
-  CategoryList,
-  CategoryCreate,
-  CategoryEdit,
-  CategoryShow,
-} from 'pages/categories';
+    CategoryList,
+    CategoryCreate,
+    CategoryEdit,
+    CategoryShow,
+} from "pages/categories";
 
-const API_URL = 'https://api.fake-rest.refine.dev';
+const API_URL = "https://api.fake-rest.refine.dev";
 
 const App: React.FC = () => {
-  const role = localStorage.getItem('role') ?? 'admin';
+    const role = localStorage.getItem("role") ?? "admin";
 
-  return (
-    <Refine
-      routerProvider={routerProvider}
-      dataProvider={dataProvider(API_URL)}
-      accessControlProvider={{
-        can: async ({ action, params, resource }) => {
-          const enforcer = await newEnforcer(model, adapter);
-          if (action === 'delete' || action === 'edit' || action === 'show') {
-            return Promise.resolve({
-              can: await enforcer.enforce(
-                role,
-                `${resource}/${params?.id}`,
-                action
-              ),
-            });
-          }
-          if (action === 'field') {
-            return Promise.resolve({
-              can: await enforcer.enforce(
-                role,
-                `${resource}/${params?.field}`,
-                action
-              ),
-            });
-          }
-          return Promise.resolve({
-            can: await enforcer.enforce(role, resource, action),
-          });
-        },
-      }}
-      resources={[
-        {
-          name: 'posts',
-          list: PostList,
-          create: PostCreate,
-          edit: PostEdit,
-          show: PostShow,
-          canDelete: true,
-        },
-        {
-          name: 'users',
-          list: UserList,
-          create: UserCreate,
-          edit: UserEdit,
-          show: UserShow,
-        },
-        {
-          name: 'categories',
-          list: CategoryList,
-          create: CategoryCreate,
-          edit: CategoryEdit,
-          show: CategoryShow,
-        },
-      ]}
-      Header={() => <Header role={role} />}
-      notificationProvider={notificationProvider}
-      Layout={Layout}
-      catchAll={<ErrorComponent />}
-    />
-  );
+    return (
+        <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider(API_URL)}
+            accessControlProvider={{
+                can: async ({ action, params, resource }) => {
+                    const enforcer = await newEnforcer(model, adapter);
+                    if (
+                        action === "delete" ||
+                        action === "edit" ||
+                        action === "show"
+                    ) {
+                        return Promise.resolve({
+                            can: await enforcer.enforce(
+                                role,
+                                `${resource}/${params?.id}`,
+                                action,
+                            ),
+                        });
+                    }
+                    if (action === "field") {
+                        return Promise.resolve({
+                            can: await enforcer.enforce(
+                                role,
+                                `${resource}/${params?.field}`,
+                                action,
+                            ),
+                        });
+                    }
+                    return Promise.resolve({
+                        can: await enforcer.enforce(role, resource, action),
+                    });
+                },
+            }}
+            resources={[
+                {
+                    name: "posts",
+                    list: PostList,
+                    create: PostCreate,
+                    edit: PostEdit,
+                    show: PostShow,
+                    canDelete: true,
+                },
+                {
+                    name: "users",
+                    list: UserList,
+                    create: UserCreate,
+                    edit: UserEdit,
+                    show: UserShow,
+                },
+                {
+                    name: "categories",
+                    list: CategoryList,
+                    create: CategoryCreate,
+                    edit: CategoryEdit,
+                    show: CategoryShow,
+                },
+            ]}
+            Header={() => <Header role={role} />}
+            notificationProvider={notificationProvider}
+            Layout={Layout}
+            catchAll={<ErrorComponent />}
+        />
+    );
 };
 
 export default App;
