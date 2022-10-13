a7e737eb12ba78a9816100510b57be89ea158caa
alicanerdurmaz <alicanerdurmaz@gmail.com>
2022-10-13 15:18:13 +0300

    feat(docs): add <PropsTable> to antd hooks

--- packages/antd/src/hooks/fields/useRadioGroup/index.ts
+++ packages/antd/src/hooks/fields/useRadioGroup/index.ts
@@ -27,7 +27,12 @@ export type UseRadioGroupReturnType<TData extends BaseRecord = BaseRecord> = {
 type UseRadioGroupProps<TData, TError> = Omit<
     UseSelectProps<TData, TError>,
     "defaultValue"
-> & { defaultValue?: BaseKey };
+> & {
+    /**
+     * Sets the default value
+     */
+    defaultValue?: BaseKey;
+};
 
 export const useRadioGroup = <
     TData extends BaseRecord = BaseRecord,
