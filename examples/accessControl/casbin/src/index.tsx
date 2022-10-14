ead523bdedf9195e4197f0f3b3b5d634ec3149d1
Salih <salihozdemir94@gmail.com>
2022-10-14 11:45:36 +0300

    fix: eslint errors

--- examples/accessControl/casbin/src/index.tsx
+++ examples/accessControl/casbin/src/index.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable */
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
 const container = document.getElementById("root");
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 root.render(
     <React.StrictMode>
