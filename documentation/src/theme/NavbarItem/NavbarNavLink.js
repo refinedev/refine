31990929b45a3e6474ea1f824a6cdca66d382036
alicanerdurmaz <alicanerdurmaz@gmail.com>
2022-10-17 18:30:19 +0300

    fix(docs): remove blank line

--- documentation/src/theme/NavbarItem/NavbarNavLink.js
+++ documentation/src/theme/NavbarItem/NavbarNavLink.js
@@ -21,7 +21,6 @@ export default function NavbarNavLink({
     const activeBaseUrl = useBaseUrl(activeBasePath);
     const normalizedHref = useBaseUrl(href, { forcePrependBaseUrl: true });
     const isExternalLink = label && href && !isInternalUrl(href);
-
     // Link content is set through html XOR label
     const linkContentProps = html
         ? { dangerouslySetInnerHTML: { __html: html } }
