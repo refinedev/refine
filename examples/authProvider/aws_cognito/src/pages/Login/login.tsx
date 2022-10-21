cd0278e8d559e53e51fec591604e83539fc23d2b
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2022-10-21 08:47:01 +0530

    Create login.tsx

---
+++ examples/authProvider/aws_cognito/src/pages/Login/login.tsx
@@ -0,0 +1,32 @@
+import { AntdLayout, Button } from "@pankod/refine-antd";
+import { SignIn } from "./pages/SignIn";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+    return (
+        <AntdLayout
+            style={{
+                background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
+                backgroundSize: "cover",
+            }}
+        >
+            <div style={{ height: "100vh", display: "flex" }}>
+                <div style={{ maxWidth: "200px", margin: "auto" }}>
+                    <div style={{ marginBottom: "28px" }}>
+                        <img src="./refine.svg" alt="Refine" />
+                    </div>
+                    <Router>
+                        <Link to="/signin">
+                            <Button
+                                type="primary"
+                                size="large"
+                                block
+                            >
+                                Sign in
+                            </Button>
+                        </Link>
+                    </Router>  
+                </div>
+            </div>
+        </AntdLayout>
+    );
+};
