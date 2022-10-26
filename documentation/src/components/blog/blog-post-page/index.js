043b8e4013bb8a602682f2944c4b0a8622362305
alican <alicanerdurmaz@gmail.com>
2022-10-26 16:29:39 +0300

    feat(docs): add newsletter cta ui

--- documentation/src/components/blog/blog-post-page/index.js
+++ documentation/src/components/blog/blog-post-page/index.js
@@ -18,6 +18,7 @@ import { Tags } from "@site/src/components/blog";
 import { Date, ReadingTime, Spacer } from "@site/src/components/blog/common";
 import useScrollTracker from "@site/src/hooks/use-scroll-tracker";
 import useLocalStorage from "@site/src/hooks/use-localstorage";
+import NewsletterCta from "../../newsletter-cta";
 
 export const BlogPostPageView = ({ children }) => {
     const { scrollY } = useScrollTracker([50]);
@@ -52,7 +53,7 @@ export const BlogPostPageView = ({ children }) => {
         <BlogPostItemContainer className="blog-post-item-shadow rounded-[10px] p-4">
             <div className="relative">
                 <div className="absolute top-0 right-0 rounded-bl-[10px] bg-white">
-                    <div className="flex items-center space-x-2 py-1 px-2">
+                    <div className="flex items-center px-2 py-1 space-x-2">
                         <TwitterShareButton
                             windowWidth={750}
                             windowHeight={800}
@@ -116,77 +117,13 @@ export const BlogPostPageView = ({ children }) => {
                 <br />
                 <Tags />
             </div>
-            {showNewsletterWidget && (
-                <aside className="fixed bottom-4 right-4 z-50 flex items-center justify-center rounded-lg px-5 py-3 ">
-                    <section className="bg-gray-50">
-                        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
-                            <div className="mx-auto max-w-lg text-center">
-                                <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
-                                    This is your weekly frontend update
-                                </h2>
-
-                                <p className="hidden text-gray-500 sm:mt-4 sm:block">
-                                    Stay up to date with the latest news, guides
-                                    & tips from our team of experts!{" "}
-                                    <b>No Spam!</b>
-                                </p>
-                            </div>
-                            <button
-                                onClick={() => {
-                                    setCanShowNewsletterWidget(true);
-                                    setShowNewsletterWidget(false);
-                                }}
-                            >
-                                close
-                            </button>
-
-                            <div className="mx-auto mt-8 max-w-xl">
-                                <form action="#" className="sm:flex sm:gap-4">
-                                    <div className="sm:flex-1">
-                                        <label
-                                            htmlFor="email"
-                                            className="sr-only"
-                                        >
-                                            Email
-                                        </label>
-
-                                        <input
-                                            type="email"
-                                            placeholder="Email address"
-                                            className="w-full rounded-md border-gray-200 bg-white p-3 text-gray-700 shadow-sm transition focus:border-white focus:outline-none focus:ring focus:ring-yellow-400"
-                                        />
-                                    </div>
-
-                                    <button
-                                        type="submit"
-                                        className="group mt-4 flex w-full items-center justify-center rounded-md bg-rose-600 px-5 py-3 text-white transition focus:outline-none focus:ring focus:ring-yellow-400 sm:mt-0 sm:w-auto"
-                                    >
-                                        <span className="text-sm font-medium">
-                                            {" "}
-                                            Sign Up{" "}
-                                        </span>
-
-                                        <svg
-                                            className="ml-3 h-5 w-5"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M17 8l4 4m0 0l-4 4m4-4H3"
-                                            />
-                                        </svg>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    </section>
-                </aside>
-            )}
+            <NewsletterCta
+                visible={showNewsletterWidget}
+                onCloseClick={() => {
+                    setCanShowNewsletterWidget(true);
+                    setShowNewsletterWidget(false);
+                }}
+            />
         </BlogPostItemContainer>
     );
 };
---
+++ documentation/src/components/newsletter-cta/index.tsx
@@ -0,0 +1,96 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React, { FC } from "react";
+import { CancelIcon } from "../landing/icons";
+
+interface Props {
+    visible: boolean;
+    onCloseClick: () => void;
+}
+
+const NewsletterCta: FC<Props> = ({ visible, onCloseClick }) => {
+    return (
+        <AnimatePresence>
+            {visible && (
+                <motion.aside
+                    key="NewsletterCta"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="fixed bottom-4 right-4 z-50 flex flex-col items-center w-[312px] lg:w-[384px] rounded-2xl shadow-modal bg-white"
+                >
+                    <button
+                        className="absolute text-white bg-transparent border-none cursor-pointer top-2 right-2 hover:scale-110"
+                        onClick={onCloseClick}
+                    >
+                        <CancelIcon />
+                    </button>
+
+                    <img
+                        srcSet="
+                    /img/newsletter-logo2x.jpg 1920w,
+                    /img/newsletter-logo.jpg 1200w"
+                        src="/img/newsletter-logo.jpg"
+                        sizes="100vw"
+                        alt="Refine app screenshot"
+                        className="bg-[#5D26D3] block object-cover w-full rounded-tl-2xl rounded-tr-2xl"
+                    />
+
+                    <section className="w-[280px] lg:w-[320px] font-montserrat pt-3 pb-4 lg:pt-8 lg:pb-8">
+                        <div className="flex flex-col items-center text-center">
+                            <h2 className="font-bold text-xl uppercase text-[#1890FF] mb-0 lg:mb-4">
+                                Join our Newsletter
+                            </h2>
+                            <p className="text-xs leading-[18px] font-medium tracking-[-0.01rem] mb-0 text-[#2A2A42]">
+                                Get started with our monthly newsletter for
+                                helpful tips on how to run your business
+                                smoothly.
+                            </p>
+                        </div>
+
+                        <form
+                            action="#"
+                            className="flex flex-col w-full mt-2 lg:mt-6"
+                        >
+                            <div className="flex gap-2 items-center w-full p-2 border border-[#EDEDEF] border-solid rounded-[4px]">
+                                <svg
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={14}
+                                    height={11}
+                                    viewBox="0 0 14 11"
+                                >
+                                    <path
+                                        d="M13.5 0H.5C.2234 0 0 .2234 0 .5v10c0 .2766.2234.5.5.5h13c.2766 0 .5-.2234.5-.5V.5c0-.2766-.2234-.5-.5-.5Zm-.625 1.7312V9.875H1.125V1.7312l-.4313-.3359.6141-.789.6688.5203H12.025l.6688-.5204.614.7891-.4328.336Zm-.85-.6062L7 5.0313 1.975 1.125 1.3062.6047l-.614.789.4312.336 5.3375 4.15a.8748.8748 0 0 0 1.0735 0l5.3406-4.1484.4312-.336-.614-.789-.6672.5187Z"
+                                        fill="#81818F"
+                                    />
+                                </svg>
+
+                                <label htmlFor="email" className="sr-only">
+                                    Email
+                                </label>
+                                <input
+                                    id="email-newsletter"
+                                    type="email"
+                                    placeholder="example@refine.com"
+                                    className="text-[#2A2A42] font-medium text-xs  w-full bg-transparent border-none outline-none"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                style={{
+                                    background:
+                                        "linear-gradient(265.27deg, #1890FF -1.58%, #47EBF5 90.77%, #47EBF5 90.77%)",
+                                }}
+                                className="mt-2 lg:mt-4 font-bold text-xs p-2 shadow-modal rounded-[4px] border-none"
+                            >
+                                Subscribe
+                            </button>
+                        </form>
+                    </section>
+                </motion.aside>
+            )}
+        </AnimatePresence>
+    );
+};
+
+export default NewsletterCta;
