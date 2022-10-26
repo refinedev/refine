7af16e5610a2271653a56ba2b402565370503f13
Salih <salihozdemir94@gmail.com>
2022-10-26 12:36:12 +0300

    feat: add custom theme example

---
+++ examples/customization/customTheme/chakra-ui/src/App.tsx
@@ -0,0 +1,39 @@
+import { Refine } from "@pankod/refine-core";
+import {
+    ChakraProvider,
+    ErrorComponent,
+    Layout,
+    refineTheme,
+    ReadyPage,
+} from "@pankod/refine-chakra-ui";
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
+
+import { PostList, PostCreate, PostEdit, PostShow } from "./pages";
+import { Header } from "./components/header";
+
+const App: React.FC = () => {
+    return (
+        <ChakraProvider theme={refineTheme}>
+            <Refine
+                routerProvider={routerProvider}
+                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                ReadyPage={ReadyPage}
+                catchAll={<ErrorComponent />}
+                Layout={Layout}
+                Header={Header}
+                resources={[
+                    {
+                        name: "posts",
+                        list: PostList,
+                        show: PostShow,
+                        create: PostCreate,
+                        edit: PostEdit,
+                    },
+                ]}
+            />
+        </ChakraProvider>
+    );
+};
+
+export default App;
