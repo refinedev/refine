7b6dfd2a78d70432b2034c6a890f57fd96447b2e
Ömer Faruk APLAK <omer@pankod.com>
2022-10-26 14:12:10 +0300

    docs: add newsletter widget

--- documentation/src/components/blog/blog-post-page/index.js
+++ documentation/src/components/blog/blog-post-page/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "@docusaurus/Link";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
 import { blogPostContainerID } from "@docusaurus/utils-common";
@@ -16,8 +16,22 @@ import {
 
 import { Tags } from "@site/src/components/blog";
 import { Date, ReadingTime, Spacer } from "@site/src/components/blog/common";
+import useScrollTracker from "@site/src/hooks/use-scroll-tracker";
+import useLocalStorage from "@site/src/hooks/use-localstorage";
 
 export const BlogPostPageView = ({ children }) => {
+    const { scrollY } = useScrollTracker([50]);
+    const [canShowNewsletterWidget, setCanShowNewsletterWidget] =
+        useLocalStorage("newsletter-popup-visible", true);
+
+    const [showNewsletterWidget, setShowNewsletterWidget] = useState(false);
+
+    useEffect(() => {
+        if (canShowNewsletterWidget === true && scrollY >= 50) {
+            setShowNewsletterWidget(true);
+        }
+    }, [scrollY]);
+
     const { metadata, isBlogPostPage } = useBlogPost();
     const {
         permalink,
@@ -102,6 +116,77 @@ export const BlogPostPageView = ({ children }) => {
                 <br />
                 <Tags />
             </div>
+            {showNewsletterWidget && (
+                <aside className="fixed bottom-4 right-4 z-50 flex items-center justify-center rounded-lg px-5 py-3 ">
+                    <section className="bg-gray-50">
+                        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+                            <div className="mx-auto max-w-lg text-center">
+                                <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
+                                    This is your weekly frontend update
+                                </h2>
+
+                                <p className="hidden text-gray-500 sm:mt-4 sm:block">
+                                    Stay up to date with the latest news, guides
+                                    & tips from our team of experts!{" "}
+                                    <b>No Spam!</b>
+                                </p>
+                            </div>
+                            <button
+                                onClick={() => {
+                                    setCanShowNewsletterWidget(true);
+                                    setShowNewsletterWidget(false);
+                                }}
+                            >
+                                close
+                            </button>
+
+                            <div className="mx-auto mt-8 max-w-xl">
+                                <form action="#" className="sm:flex sm:gap-4">
+                                    <div className="sm:flex-1">
+                                        <label
+                                            htmlFor="email"
+                                            className="sr-only"
+                                        >
+                                            Email
+                                        </label>
+
+                                        <input
+                                            type="email"
+                                            placeholder="Email address"
+                                            className="w-full rounded-md border-gray-200 bg-white p-3 text-gray-700 shadow-sm transition focus:border-white focus:outline-none focus:ring focus:ring-yellow-400"
+                                        />
+                                    </div>
+
+                                    <button
+                                        type="submit"
+                                        className="group mt-4 flex w-full items-center justify-center rounded-md bg-rose-600 px-5 py-3 text-white transition focus:outline-none focus:ring focus:ring-yellow-400 sm:mt-0 sm:w-auto"
+                                    >
+                                        <span className="text-sm font-medium">
+                                            {" "}
+                                            Sign Up{" "}
+                                        </span>
+
+                                        <svg
+                                            className="ml-3 h-5 w-5"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                            />
+                                        </svg>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </section>
+                </aside>
+            )}
         </BlogPostItemContainer>
     );
 };
