6103986c8b4fe77beeb16465491556946cadec1c
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-10-28 10:16:16 +0300

    refactor(chakra): fix table examples

--- examples/table/chakra-ui/advanced/src/App.tsx
+++ examples/table/chakra-ui/advanced/src/App.tsx
@@ -5,6 +5,7 @@ import {
     Layout,
     refineTheme,
     ReadyPage,
+    notificationProvider,
 } from "@pankod/refine-chakra-ui";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
@@ -15,6 +16,7 @@ const App: React.FC = () => {
     return (
         <ChakraProvider theme={refineTheme}>
             <Refine
+                notificationProvider={notificationProvider()}
                 routerProvider={routerProvider}
                 dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
                 Layout={Layout}
