1db2c4643f17ac8cd20cc2d193d1baeff35430f7
Ali Emir Sen <senaliemir@gmail.com>
2022-11-10 02:05:00 +0300

    feat(guesser): `printImport` helper for merge and print import lines

---
+++ packages/guesser/src/utilities/print-imports/index.ts
@@ -0,0 +1,33 @@
+export const printImports = (
+    imports: Array<
+        [element: string, module: string, isDefaultImport?: boolean]
+    >,
+) => {
+    const byModule = imports.reduce((acc, [element, module, isDefault]) => {
+        if (!acc[module]) {
+            acc[module] = [] as Array<
+                string | [variable: string, isDefault?: boolean]
+            >;
+        }
+
+        if (!acc[module].includes(element)) {
+            if (isDefault) {
+                acc[module].push([element, true]);
+            } else {
+                acc[module].push(element);
+            }
+        }
+
+        return acc;
+    }, {} as Record<string, Array<string | [variable: string, isDefault?: boolean]>>);
+
+    return Object.entries(byModule).map(([module, elements]) => {
+        const defaultImport = elements.find((e) => Array.isArray(e) && e[1]);
+        const named = elements.filter((e) => typeof e === "string");
+        const defaultStr = defaultImport ? `${defaultImport[0]}` : "";
+        const namedStr = named.length ? `{ ${named.join(", ")} }` : "";
+        return `import ${defaultStr}${
+            defaultStr && namedStr ? ", " : ""
+        }${namedStr} from "${module}";`;
+    });
+};
