a7737b7d9c7233f5433bc758497b0b669ed31f43
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-11-09 16:23:29 +0300

    feat: add bootstrap

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,20 @@
-console.log("cli");
+#!/usr/bin/env node
+import { Command } from "commander";
+
+const bootstrap = () => {
+    const program = new Command();
+    program
+        .version("0.0.0", "-v, --version", "Output the current version.")
+        .usage("<command> [options]")
+        .helpOption("-h, --help", "Output usage information.");
+
+    // TODO: load commands
+
+    program.parse(process.argv);
+
+    if (!process.argv.slice(2).length) {
+        program.outputHelp();
+    }
+};
+
+bootstrap();
