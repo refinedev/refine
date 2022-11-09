c0ecd9a3beb933c5f69555332cf35a3a2b7126cf
Ali Emir Sen <senaliemir@gmail.com>
2022-11-10 02:11:30 +0300

    feat(guesser): add index export to `utilities`

---
+++ packages/guesser/src/utilities/index.ts
@@ -0,0 +1,32 @@
+export { getFieldableKeys } from "./get-fieldable-keys";
+
+export {
+    pickDataProvider,
+    dataProviderFromResource,
+} from "./pick-data-provider";
+export { pickGuessedField } from "./pick-guessed-field";
+
+export { prettierFormat } from "./prettier-format";
+export { prettySpaces } from "./pretty-spaces";
+export { prettyString } from "./pretty-string";
+
+export { removeRelationSuffix } from "./remove-relation-suffix";
+export { replaceExports } from "./replace-exports";
+export { replaceImports } from "./replace-imports";
+
+export { resourceFromGuess } from "./resource-from-guess";
+
+export { componentName } from "./component-name";
+
+export { jsx } from "./jsx";
+
+export { prepareLiveCode } from "./prepare-live-code";
+export { removeHiddenCode } from "./remove-hidden-code";
+
+export { accessor } from "./accessor";
+export { printImports } from "./print-imports";
+
+export { toSingular } from "./to-singular";
+export { toPlural } from "./to-plural";
+
+export { isIDKey } from "./is-id-key";
