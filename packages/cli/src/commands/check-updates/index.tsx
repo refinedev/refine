4040bdbf9bf55e5966073249c00f73c649d87b00
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2022-11-14 15:05:12 +0300

    feat(cli): add check-updates command (#2989)
    * feat(cli): add check-updates command
    
    * fix(cli): interface typo
    
    * chore(cli): use execa instead of shelljs
    
    * chore(cli): fix react version
    
    * chore(cli): fix return value type on jsdoc
    
    * feat(cli): add colors that show the differences
    
    * chore(cli): change variable name
    
    * fix(cli): update table colors
    
    * feat(cli): semantic version info
    
    * feat(cli): remove info table
    
    * feat(cli): add package manager not found error handling
    
    * chore(cli): change package manager not found text
    
    * chore: remove package.lock.json and nock

---
+++ packages/cli/src/commands/check-updates/index.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Command } from "commander";
+import preferredPM from "preferred-pm";
+import { render } from "ink";
+import UpdateWarningTable from "src/components/update-warning-table";
+import {
+    NpmOutdatedResponse,
+    RefinePackageInstalledVersionData,
+} from "src/interfaces";
+import execa from "execa";
+import spinner from "src/utils/spinner";
+
+const load = (program: Command) => {
+    return program
+        .command("check-updates")
+        .description("Check all installed `refine` packages are up to date")
+        .action(action);
+};
+
+const action = async () => {
+    const packages = await spinner(isRefineUptoDate, "Checking for updates...");
+
+    if (packages === null) {
+        console.log("Package manager not found.");
+        return;
+    }
+
+    if (!packages.length) {
+        console.log("All `refine` packages are up to date 🎉");
+        return;
+    }
+
+    render(<UpdateWarningTable data={packages} />);
+};
+
+/**
+ * Displays update warning if there is any `refine` package update available.
+ */
+export const getUpdateWarning = async () => {
+    const packages = await isRefineUptoDate();
+    if (!packages) return;
+
+    render(<UpdateWarningTable data={packages} />);
+};
+
+/**
+ *
+ * @returns `refine` packages that have updates.
+ * @returns `null` if there is no `package manager` found.
+ * @returns `[]` if no refine package found.
+ * @returns `[]` if all `refine` packages are up to date.
+ */
+export const isRefineUptoDate = async () => {
+    const pm = await preferredPM(process.cwd());
+    if (!pm) return null;
+
+    const refinePackages = await getOutdatedRefinePackages(pm.name);
+
+    return refinePackages;
+};
+
+const getOutdatedRefinePackages = async (pm: "npm" | "pnpm" | "yarn") => {
+    const packages = await getOutdatedPackageList(pm);
+    if (!packages) return [];
+
+    const list: RefinePackageInstalledVersionData[] = [];
+
+    Object.keys(packages).forEach((packageName) => {
+        const dependency = packages[packageName];
+
+        if (packageName.includes("@pankod/refine")) {
+            list.push({
+                name: packageName,
+                current: dependency.current,
+                wanted: dependency.wanted,
+                latest: dependency.latest,
+            });
+        }
+    });
+
+    return list;
+};
+
+const getOutdatedPackageList = async (pm: "npm" | "pnpm" | "yarn") => {
+    try {
+        const { stdout } = await execa(`${pm}`, [`outdated`, `--json`], {
+            reject: false,
+            timeout: 10000,
+        });
+
+        if (!stdout) return null;
+
+        return JSON.parse(stdout) as NpmOutdatedResponse | null;
+    } catch (e) {
+        return null;
+    }
+};
+
+export default load;
