c8600dd7c3594341ed74aab04cc4eecab2236c9b
Salih <salihozdemir94@gmail.com>
2022-11-16 14:59:32 +0300

    feat: add chakra edit guesser

--- packages/guesser/src/guessers/chakra-ui/index.ts
+++ packages/guesser/src/guessers/chakra-ui/index.ts
@@ -1,2 +1,3 @@
 export { ListGuesser as ChakraUIListGuesser } from "./list";
 export { ShowGuesser as ChakraUIShowGuesser } from "./show";
+export { EditGuesser as ChakraUIEditGuesser } from "./edit";
