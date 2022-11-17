8b6a9dee65ad29d2e65972d7ddc2a5da47c4690a
Salih <salihozdemir94@gmail.com>
2022-11-17 16:41:23 +0300

    fix(mantine): add default checked value to TagField

--- packages/mantine/src/components/fields/tag/index.tsx
+++ packages/mantine/src/components/fields/tag/index.tsx
@@ -14,5 +14,9 @@ export type TagFieldProps = RefineFieldTagProps<
  * @see {@link https://refine.dev/docs/api-reference/mantine/components/fields/tag} for more details.
  */
 export const TagField: React.FC<TagFieldProps> = ({ value, ...rest }) => {
-    return <Chip {...rest}>{value?.toString()}</Chip>;
+    return (
+        <Chip checked={false} {...rest}>
+            {value?.toString()}
+        </Chip>
+    );
 };
