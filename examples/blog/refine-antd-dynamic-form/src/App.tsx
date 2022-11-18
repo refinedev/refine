e3afe2244132aa4a92a0c8ef1087aa95abef99dd
necatiozmen <necatiozmn@gmail.com>
2022-11-18 14:36:44 +0300

    chore: add antd dynamic example

---
+++ examples/blog/refine-antd-dynamic-form/src/App.tsx
@@ -0,0 +1,36 @@
+import { Refine } from "@pankod/refine-core";
+import {
+    notificationProvider,
+    Layout,
+    ReadyPage,
+    ErrorComponent,
+} from "@pankod/refine-antd";
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+import PostCreate from "pages/PostCreate";
+import PostEdit from "pages/PostEdit";
+import PostList from "pages/PostList";
+
+function App() {
+    return (
+        <Refine
+            notificationProvider={notificationProvider}
+            Layout={Layout}
+            ReadyPage={ReadyPage}
+            catchAll={<ErrorComponent />}
+            routerProvider={routerProvider}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            resources={[
+                {
+                    name: "users",
+                    list: PostList,
+                    create: PostCreate,
+                    edit: PostEdit,
+                },
+            ]}
+        />
+    );
+}
+
+export default App;
