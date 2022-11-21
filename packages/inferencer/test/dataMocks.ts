48389a81a4a76e3c35eaf2bc491045ce155655ee
Ali Emir Sen <senaliemir@gmail.com>
2022-11-22 01:54:57 +0300

    test(inferencer): update mocks

--- packages/inferencer/test/dataMocks.ts
+++ packages/inferencer/test/dataMocks.ts
@@ -1,3 +1,4 @@
+import { DataProvider } from "@pankod/refine-core";
 import { useParams, useLocation, Link, useNavigate } from "react-router-dom";
 
 /* import {
@@ -7,44 +8,195 @@ import { useParams, useLocation, Link, useNavigate } from "react-router-dom";
     ILiveContext,
 } from "@pankod/refine-core"; */
 
-export const posts = [
+export const categories = [
     {
-        id: "1",
-        title: "Necessitatibus necessitatibus id et cupiditate provident est qui amet.",
-        slug: "ut-ad-et",
-        content:
-            "Cupiditate labore quaerat cum incidunt vel et consequatur modi illo. Et maxime aut commodi occaecati omnis. Est voluptatem quibusdam aliquam. Esse tenetur omnis eaque. Consequatur necessitatibus illum ipsum aspernatur architecto qui. Ut temporibus qui nobis. Reiciendis est magnam ipsa quasi dolor ipsa error. Et eaque cumque est. Eos et odit corporis delectus aut corrupti tempora velit. Perferendis ratione voluptas corrupti id temporibus nam.",
-        categoryId: 1,
-        status: "active",
-        userId: 5,
-        tags: [16, 31, 45],
+        id: 16,
+        title: "Numquam Saepe Illo",
     },
     {
-        id: "2",
-        title: "Recusandae consectetur aut atque est.",
-        slug: "consequatur-molestiae-rerum",
-        content:
-            "Quia ut autem. Hic dolorum magni est quisquam. Modi est id et est. Est sapiente velit iure non voluptatem natus enim. Distinctio ipsa repellendus est. Sunt ipsam dignissimos vero error est cumque eaque. Consequatur voluptas suscipit optio incidunt doloremque quia harum harum. Totam voluptatibus aperiam quia. Est omnis deleniti et aut at fugit temporibus debitis modi. Magni aut vel quod magnam.",
-        categoryId: 38,
-        status: "active",
-        userId: 36,
-        tags: [16, 30, 46],
+        id: 7,
+        title: "Sit Natus Voluptatibus",
     },
 ];
 
-const MockDataProvider = () => {
+export const users = [
+    {
+        id: 21,
+        firstName: "Eriberto",
+        lastName: "Leannon",
+        email: "dereck_king@hotmail.com",
+        status: false,
+        birthday: "2022-11-04T09:46:18.482Z",
+        skills: ["redis", "swift", "angular"],
+        avatar: [
+            {
+                name: "Eriberto.jpg",
+                percent: 100,
+                size: 40088,
+                status: "done",
+                type: "image/jpeg",
+                uid: "rc-upload-1669061034217",
+                url: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/670.jpg",
+            },
+        ],
+    },
+    {
+        id: 49,
+        firstName: "Tressie",
+        lastName: "Reichel",
+        email: "chanel.ullrich@yahoo.com",
+        status: true,
+        birthday: "2022-09-27T03:22:44.255Z",
+        skills: ["postgresql", "refine", "tailwind"],
+        avatar: [
+            {
+                name: "Tressie.jpg",
+                percent: 100,
+                size: 40088,
+                status: "done",
+                type: "image/jpeg",
+                uid: "rc-upload-1669061034218",
+                url: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/900.jpg",
+            },
+        ],
+    },
+];
+
+export const tags = [
+    {
+        id: 5,
+        firstName: "Pauline",
+        lastName: "Kessler",
+        email: "eliane_friesen27@gmail.com",
+        status: false,
+        birthday: "2022-08-23T03:45:48.000Z",
+        skills: ["ember.js", "html", "redis"],
+        avatar: [
+            {
+                name: "Pauline.jpg",
+                percent: 100,
+                size: 40088,
+                status: "done",
+                type: "image/jpeg",
+                uid: "rc-upload-1669061034215",
+                url: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/7.jpg",
+            },
+        ],
+    },
+    {
+        id: 9,
+        firstName: "Juston",
+        lastName: "Keeling",
+        email: "erick.klocko19@yahoo.com",
+        status: true,
+        birthday: "2022-10-05T13:44:47.127Z",
+        skills: ["swift", "refine", "react"],
+        avatar: [
+            {
+                name: "Juston.jpg",
+                percent: 100,
+                size: 40088,
+                status: "done",
+                type: "image/jpeg",
+                uid: "rc-upload-1669061034215",
+                url: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/638.jpg",
+            },
+        ],
+    },
+];
+
+export const posts = [
+    {
+        id: 11,
+        title: "Nobis exercitationem officia quia est.",
+        slug: "provident-culpa-facere",
+        content:
+            "Non velit blanditiis optio quo quaerat nam aperiam. Odit tempore consequatur voluptatibus molestias. Est nemo aut. Fugit aliquam enim eveniet veritatis. Magni et aut. Consequatur aliquid rerum vero. Quaerat debitis enim magnam. Dolorem est delectus architecto et accusantium. Velit quia vero. Aut necessitatibus quia at consequatur soluta accusantium.",
+        hit: 920557,
+        category: {
+            id: 16,
+        },
+        user: {
+            id: 21,
+        },
+        status: "draft",
+        status_color: "orange",
+        createdAt: "2022-04-02T04:25:44.933Z",
+        publishedAt: "2021-08-10T08:07:39.705Z",
+        image: [
+            {
+                url: "http://loremflickr.com/640/480/abstract",
+                name: "77b8451c-3467-4072-a4cf-bddfd773c5ed",
+                status: "done",
+                type: "image/jpeg",
+                uid: "cdf0ee1d-ccfe-4b7a-bc06-493531729487",
+            },
+        ],
+        tags: [9],
+        language: 2,
+    },
+    {
+        id: 21,
+        title: "Eaque in nesciunt.",
+        slug: "quod-quos-commodi",
+        content:
+            "Ut velit quo. Sed beatae et sit ea. Perferendis pariatur doloremque deleniti sapiente. Doloribus iste est minima quam laboriosam quia aut. Voluptatem hic debitis. Aut et atque sapiente accusamus et labore consequatur quo eligendi. Harum debitis vel in repellat neque dolor. Dolores itaque eveniet minima aut hic ut incidunt officiis. Accusamus debitis quasi est sint recusandae illum ut unde. Doloribus rem et.",
+        hit: 175149,
+        category: {
+            id: 7,
+        },
+        user: {
+            id: 49,
+        },
+        status: "published",
+        status_color: "lime",
+        createdAt: "2022-09-24T08:33:21.883Z",
+        publishedAt: "2021-09-01T03:33:43.753Z",
+        image: [
+            {
+                url: "http://loremflickr.com/640/480/abstract",
+                name: "496e010f-2db7-4809-8b83-21f735d4957b",
+                status: "done",
+                type: "image/jpeg",
+                uid: "febbcad1-edbb-482a-9a08-b8069d18f1e7",
+            },
+        ],
+        tags: [5, 9],
+        language: 2,
+    },
+];
+
+const data = {
+    users,
+    tags,
+    posts,
+    categories,
+};
+
+const MockDataProvider = (): DataProvider => {
     return {
-        create: () => Promise.resolve({ data: posts[0] }),
-        createMany: () => Promise.resolve({ data: posts }),
-        deleteOne: () => Promise.resolve({ data: posts[0] }),
-        deleteMany: () => Promise.resolve({ data: [] }),
-        getList: () => Promise.resolve({ data: posts, total: 2 }),
-        getMany: () => Promise.resolve({ data: [...posts] }),
-        getOne: () => Promise.resolve({ data: posts[0] }),
-        update: () => Promise.resolve({ data: posts[0] }),
+        create: ({ resource = "posts" }) => {
+            return Promise.resolve({ data: data[resource][0] as any });
+        },
+        createMany: ({ resource = "posts" }) =>
+            Promise.resolve({ data: data[resource] as any }),
+        deleteOne: ({ resource = "posts" }) =>
+            Promise.resolve({ data: data[resource][0] as any }),
+        deleteMany: ({}) => Promise.resolve({ data: [] }),
+        getList: ({ resource = "posts" }) =>
+            Promise.resolve({
+                data: data[resource] as any,
+                total: data[resource].length,
+            }),
+        getMany: ({ resource = "posts" }) =>
+            Promise.resolve({ data: [...data[resource]] as any }),
+        getOne: ({ resource = "posts" }) =>
+            Promise.resolve({ data: data[resource][0] as any }),
+        update: ({ resource = "posts" }) =>
+            Promise.resolve({ data: data[resource][0] as any }),
         updateMany: () => Promise.resolve({ data: [] }),
         getApiUrl: () => "https://api.fake-rest.refine.dev",
-        custom: () => Promise.resolve({ data: [...posts] }),
+        custom: () => Promise.resolve({ data: [...posts] as any }),
     };
 };
 
@@ -73,6 +225,7 @@ export const MockRouterProvider = {
     Link,
     Prompt: () => null,
 };
+
 export const MockAccessControlProvider: any = {
     can: () => Promise.resolve({ can: true }),
 };
