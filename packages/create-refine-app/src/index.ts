86e60caaf7b65a614e2df20da6c95ca985f07ab3
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-11-22 17:19:04 +0300

    feat: read version on package.json and pass all params on execa

--- packages/create-refine-app/src/index.ts
+++ packages/create-refine-app/src/index.ts
@@ -1,20 +1,30 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import execa from "execa";
+import { readFileSync } from "fs";
 
 const bootstrap = () => {
+    const packageJson = JSON.parse(
+        readFileSync(`${__dirname}/../package.json`, "utf8"),
+    );
+
     const program = new Command();
     program
-        .version("0.0.0", "-v, --version", "Output the current version.")
+        .version(
+            packageJson.version,
+            "-v, --version",
+            "Output the current version.",
+        )
         .usage("<command> [options]")
         .helpOption("-h, --help", "Output usage information.")
-        .arguments("<projectName>")
-        .action((projectName) => {
+        .allowUnknownOption(true)
+        .argument("<projectName>")
+        .action((projectName, _, command: Command) => {
             const superplateExecutable = require.resolve(".bin/superplate");
 
             execa.sync(
                 superplateExecutable,
-                [projectName, "--project=refine"],
+                [...command.args, "--project=refine"],
                 {
                     stdio: "inherit",
                 },
