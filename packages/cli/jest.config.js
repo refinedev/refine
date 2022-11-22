7a52c970f68ec87a370e4a6469dda3973568a877
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-11-22 17:49:20 +0300

    fix(cli): test env

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -4,10 +4,10 @@ const { compilerOptions } = require("./tsconfig");
 module.exports = {
     preset: "ts-jest",
     rootDir: "./",
-    name: "cli",
-    displayName: "cli",
+    name: "refine-cli",
+    displayName: "refine-cli",
     setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-    testEnvironment: "jsdom",
+    testEnvironment: "node",
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
     modulePaths: ["<rootDir>", "src"],
     moduleDirectories: ["node_modules", "src"],
