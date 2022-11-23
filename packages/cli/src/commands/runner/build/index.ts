b40cbe603a926cde9c31fb9acdb8f522a4d1ba88
alican <alicanerdurmaz@gmail.com>
2022-11-23 11:56:40 +0300

    feat(cli): add update notifier to build and dev commands

--- packages/cli/src/commands/runner/build/index.ts
+++ packages/cli/src/commands/runner/build/index.ts
@@ -2,6 +2,7 @@ import { Command } from "commander";
 import { getProjectType } from "@utils/project";
 import { projectScripts } from "../projectScripts";
 import { runScript } from "../runScript";
+import { updateNotifier } from "src/update-notifier";
 
 const build = (program: Command) => {
     return program
@@ -18,6 +19,8 @@ const action = async (args: string[]) => {
     const binPath = projectScripts[projectType].getBin("build");
     const script = projectScripts[projectType].build;
     const command = [...script, ...args];
+
+    await updateNotifier();
 
     runScript(binPath, command);
 };
