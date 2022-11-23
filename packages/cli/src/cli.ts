cf22ac51b6bf1f958cfa66995a00204cd367948c
Ali Emir Sen <senaliemir@gmail.com>
2022-11-23 23:03:31 +0300

    refactor(cli): move to `cli.ts` for type exports

---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import { readFileSync } from "fs-extra";
+
+import checkUpdates from "./commands/check-updates";
+import resourceGenerate from "./commands/generate/resource";
+import update from "./commands/update";
+import { dev, build, start, run } from "./commands/runner";
+import "@utils/env";
+import swizzle from "@commands/swizzle";
+
+const bootstrap = () => {
+    const packageJson = JSON.parse(
+        readFileSync(`${__dirname}/../package.json`, "utf8"),
+    );
+
+    const program = new Command();
+
+    program
+        .version(
+            packageJson.version,
+            "-v, --version",
+            "Output the current version.",
+        )
+        .usage("<command> [options]")
+        .helpOption("-h, --help", "Output usage information.");
+
+    // load commands
+    // resourceGenerate(program);
+    checkUpdates(program);
+    update(program);
+    dev(program);
+    build(program);
+    start(program);
+    run(program);
+    swizzle(program);
+
+    program.parse(process.argv);
+
+    if (!process.argv.slice(2).length) {
+        program.outputHelp();
+    }
+};
+
+bootstrap();
