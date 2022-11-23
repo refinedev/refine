a03b2748f6b07af869a8eba758194f0646a1d208
Ali Emir Sen <senaliemir@gmail.com>
2022-11-23 22:44:29 +0300

    feat(swizzle): make command async

--- packages/cli/src/commands/swizzle/index.ts
+++ packages/cli/src/commands/swizzle/index.ts
@@ -1,28 +1,11 @@
 import path from "path";
 import inquirer from "inquirer";
 import { Command, OptionValues } from "commander";
-import { getInstalledRefinePackages } from "@utils/package";
-import {
-    ensureFileSync,
-    existsSync,
-    readFileSync,
-    writeFileSync,
-} from "fs-extra";
-
-type SwizzleFile = {
-    label: string;
-    files: { src: string; dist: string }[];
-};
-
-type SwizzleConfig = {
-    items: Array<SwizzleFile>;
-    transform?: any;
-    move?: any;
-};
-
-type RefineConfig = {
-    swizzle: SwizzleConfig;
-};
+import { isPackageHaveRefineConfig } from "../../utils/package/index";
+import { getInstalledRefinePackagesFromNodeModules } from "@utils/package";
+import { ensureFile, pathExists, readFile, writeFile } from "fs-extra";
+import { getRefineConfig } from "@utils/swizzle";
+import { SwizzleFile } from "@definitions";
 
 const swizzle = (program: Command) => {
     return (
@@ -35,97 +18,114 @@ const swizzle = (program: Command) => {
     );
 };
 
-const action = async (options: OptionValues) => {
-    // get installed packages
-    const installedPackages = await getInstalledRefinePackages();
+const action = async (_options: OptionValues) => {
+    const installedPackages = await getInstalledRefinePackagesFromNodeModules();
 
-    // check for packages with refine.config.js
-    const configPackages = installedPackages.flatMap(({ name }) => {
-        const configPath = path.join(
-            "./node_modules",
-            name,
-            "refine.config.js",
-        );
-        const isExist = existsSync(configPath);
+    const packagesWithConfig: Array<{ name: string; path: string }> = [];
 
-        return isExist
-            ? [
-                  {
-                      name,
-                      path: path.join(process.cwd(), configPath),
-                  },
-              ]
-            : [];
-    });
-
-    // get selected package
-    const { package: selectedPackageName } = await inquirer.prompt<{
-        package: string;
-    }>([
-        {
-            type: "list",
-            name: "package",
-            message: "Which package do you want to swizzle?",
-            choices: configPackages.map(({ name }) => name),
-        },
-    ]);
-
-    const selectedPackage = configPackages.find(
-        ({ name }) => name === selectedPackageName,
+    await Promise.all(
+        installedPackages.map(async (pkg) => {
+            const hasConfig = await isPackageHaveRefineConfig(pkg.path);
+            if (hasConfig) {
+                packagesWithConfig.push(pkg);
+            }
+        }),
     );
 
-    if (!selectedPackage) {
-        console.log(":(");
+    if (packagesWithConfig.length === 0) {
+        console.log("No refine packages found with refine.config.js");
         return;
     }
 
-    try {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const { swizzle } = require(selectedPackage.path) as RefineConfig;
+    const { selectedPackage } = await inquirer.prompt<{
+        selectedPackage: { name: string; path: string };
+    }>([
+        {
+            type: "list",
+            name: "selectedPackage",
+            message: "Which package do you want to swizzle?",
+            choices: packagesWithConfig.map((pkg) => ({
+                name: pkg.name,
+                value: pkg,
+            })),
+        },
+    ]);
 
-        const { items } = swizzle;
+    const {
+        swizzle: { items, transform },
+    } = await getRefineConfig(selectedPackage.path);
 
-        // get selected package
-        const { selectedItem } = await inquirer.prompt<{
-            selectedItem: SwizzleFile;
-        }>([
-            {
-                type: "list",
-                name: "selectedItem",
-                message: "Which item do you want to swizzle?",
-                choices: items.map((item) => ({
-                    name: item.label,
-                    value: item,
-                })),
-            },
-        ]);
+    const { selectedComponent } = await inquirer.prompt<{
+        selectedComponent: SwizzleFile;
+    }>([
+        {
+            type: "list",
+            name: "selectedComponent",
+            message: "Which component do you want to swizzle?",
+            choices: items.map((component) => ({
+                name: component.label,
+                value: component,
+            })),
+        },
+    ]);
 
-        const { src, dist } = selectedItem.files[0];
+    const createdResponse = await Promise.all(
+        selectedComponent.files.map(async (file) => {
+            const srcPath = file.src
+                ? path.join(selectedPackage.path, file.src)
+                : undefined;
+            const destPath = file.dest
+                ? path.join(process.cwd(), file.dest)
+                : undefined;
 
-        const srcPath = path.join(path.dirname(selectedPackage.path), src);
+            if (!srcPath) {
+                console.log("Source path is not defined");
+                return;
+            }
 
-        if (!existsSync(srcPath)) {
-            console.log("File not found");
-            return;
-        }
+            if (!destPath) {
+                console.log("Destination path is not defined");
+                return;
+            }
 
-        const fileContent = readFileSync(srcPath, "utf-8");
+            const hasSrc = await pathExists(srcPath);
 
-        const distPath = path.join(process.cwd(), dist);
+            if (!hasSrc) {
+                console.log(`Source file not found: ${srcPath}`);
+                return;
+            }
 
-        const transformed = fileContent;
+            if (hasSrc) {
+                const srcContent = await readFile(srcPath, "utf-8");
+                const isDestExist = await pathExists(destPath);
 
-        if (existsSync(distPath)) {
-            console.log("already exist");
-            return;
-        }
+                if (isDestExist) {
+                    console.log("File already exist");
+                    return;
+                }
 
-        ensureFileSync(distPath);
-        writeFileSync(distPath, transformed);
+                await ensureFile(destPath);
 
-        console.log("ok");
-    } catch (error) {
-        console.log("er", error);
+                const transformedContent =
+                    transform?.(srcContent) ?? srcContent;
+
+                await writeFile(destPath, transformedContent);
+
+                return destPath;
+            }
+
+            return undefined;
+        }),
+    );
+
+    const createdFiles = createdResponse.filter(Boolean);
+
+    if (createdFiles.length > 0) {
+        console.log("Done swizzling ${selectedComponent.label} component.");
+        console.log("Created files:");
+        console.log(
+            createdFiles.filter(Boolean).map((file) => ` - ${file} \n`),
+        );
     }
 };
 
