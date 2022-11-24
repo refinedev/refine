bb4a0df8e3a5140ce694fa75a6445c12aeb4934f
Ali Emir Sen <senaliemir@gmail.com>
2022-11-24 13:21:13 +0300

    feat(swizzle): add `parseSwizzleBlocks` helper
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>

---
+++ packages/cli/src/utils/swizzle/parseSwizzleBlocks.test.ts
@@ -0,0 +1,25 @@
+import { parseSwizzleBlocks } from "./parseSwizzleBlocks";
+
+describe("parseSwizzleBlocks", () => {
+    it("should remove swizzle blocks", () => {
+        const content = `
+            1
+            // swizzle-remove-start
+            remove-this
+            //swizzle-remove-end
+            2
+            /* swizzle-remove-start */
+            remove-this-too
+            /* swizzle-remove-end */
+        `;
+
+        const result = parseSwizzleBlocks(content);
+
+        expect(result).not.toContain("remove-this");
+        expect(result).not.toContain("remove-this-too");
+        expect(result).toContain("1");
+        expect(result).toContain("2");
+        expect(result).not.toContain("swizzle-remove-start");
+        expect(result).not.toContain("swizzle-remove-end");
+    });
+});
