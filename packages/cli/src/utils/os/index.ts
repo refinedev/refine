8b9c6474207a8fefe2a4b926aede483ac2107542
alican <alicanerdurmaz@gmail.com>
2022-11-25 16:09:55 +0300

    fix(cli): get OS name from node-os package

--- packages/cli/src/utils/os/index.ts
+++ packages/cli/src/utils/os/index.ts
@@ -1,15 +1,25 @@
 import envinfo from "envinfo";
+import os from "os";
+
+export const getOSType = () => {
+    const osPlatform = os.type();
+
+    const types: Record<string, "macOS" | "Linux" | "Windows"> = {
+        Darwin: "macOS",
+        Linux: "Linux",
+        Windows_NT: "Windows",
+    };
+
+    return types[osPlatform];
+};
 
 export const getOS = async () => {
     // returns as a ['OS', 'macOS Mojave 10.14.5']
-    const [_, OS] = (await envinfo.helpers.getOSInfo()) as unknown as string[];
-
-    const parsed = OS.split(" ");
-    const name = parsed.at(0) || "";
-    const version = parsed.at(-1) || "";
+    const [_, OSInfo] =
+        (await envinfo.helpers.getOSInfo()) as unknown as string[];
 
     return {
-        name,
-        version,
+        name: getOSType(),
+        version: OSInfo,
     };
 };
