ca95e3f38f95b4d3924ff988dea81ed87d177fd6
Ömer Faruk APLAK <omer@pankod.com>
2022-11-25 10:37:32 +0300

    chore: fix mui grid actions cell item issue

--- examples/table/mui/advancedTable/src/pages/dataGrid/index.tsx
+++ examples/table/mui/advancedTable/src/pages/dataGrid/index.tsx
@@ -66,7 +66,9 @@ export const BasicDataGrid: React.FC = () => {
                 type: "actions",
                 headerName: "Actions",
                 getActions: () => [
+                    // @ts-expect-error `@mui/x-data-grid@5.17.12` broke the props of `GridActionsCellItem` and requires `onResize` and `onResizeCapture` props which should be optional.
                     <GridActionsCellItem key={1} label="Delete" showInMenu />,
+                    // @ts-expect-error `@mui/x-data-grid@5.17.12` broke the props of `GridActionsCellItem` and requires `onResize` and `onResizeCapture` props which should be optional.
                     <GridActionsCellItem key={2} label="Print" showInMenu />,
                 ],
             },
