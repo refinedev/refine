6d377e2e9112a952d0379db6db208bc09b384121
Salih <salihozdemir94@gmail.com>
2022-11-25 13:42:35 +0300

    feat(mantine): export button definition function

--- packages/mantine/src/index.tsx
+++ packages/mantine/src/index.tsx
@@ -442,4 +442,5 @@ export * from "./components";
 export * from "./providers";
 export * from "./hooks";
 export * from "./theme";
+export * from "./definitions";
 export { FormContext } from "./contexts/form-context";
