ec0066ebb63d00370ad924bf241e0860c43385b2
Ali Emir Sen <senaliemir@gmail.com>
2022-11-30 21:16:37 +0300

    feat(swizzle): add basic types for `cardinal`

--- packages/cli/src/definitions/cardinal.d.ts
+++ packages/cli/src/definitions/cardinal.d.ts
@@ -1,1 +1,6 @@
-declare module "cardinal";
+declare module "cardinal" {
+    export function highlight(
+        code: string,
+        options?: { jsx?: boolean; theme?: string; linenos?: boolean },
+    ): string;
+}
