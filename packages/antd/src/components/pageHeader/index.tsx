2c34e43d72706dd2a31bf7f0054dccba22e6bd80
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2022-12-07 13:48:13 +0300

    feat(antd): antd page header theme (#3151)

--- packages/antd/src/components/pageHeader/index.tsx
+++ packages/antd/src/components/pageHeader/index.tsx
@@ -3,9 +3,49 @@ import {
     PageHeader as AntdPageHeader,
     PageHeaderProps as AntdPageHeaderProps,
 } from "@ant-design/pro-layout";
+import { Button, Typography } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 export type PageHeaderProps = AntdPageHeaderProps;
 
 export const PageHeader: FC<AntdPageHeaderProps> = ({ children, ...props }) => {
-    return <AntdPageHeader {...props}>{children}</AntdPageHeader>;
+    const backIcon =
+        typeof props.backIcon === "undefined" ? (
+            <Button type="text" icon={<ArrowLeftOutlined />} />
+        ) : (
+            props.backIcon
+        );
+
+    const title =
+        typeof props.title === "string" ? (
+            <Typography.Title level={4} style={{ marginBottom: 0 }}>
+                {props.title}
+            </Typography.Title>
+        ) : (
+            props.title
+        );
+
+    const subtitle =
+        typeof props.title === "string" ? (
+            <Typography.Title
+                level={5}
+                type="secondary"
+                style={{ marginBottom: 0 }}
+            >
+                {props.subTitle}
+            </Typography.Title>
+        ) : (
+            props.subTitle
+        );
+
+    return (
+        <AntdPageHeader
+            {...props}
+            backIcon={backIcon}
+            title={title}
+            subTitle={subtitle}
+        >
+            {children}
+        </AntdPageHeader>
+    );
 };
