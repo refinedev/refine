691fbdcd8b301859ba11e2891938cfc3a883e00c
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-12-07 10:42:46 +0300

    refactor: export reset.css

--- packages/antd/tsup.config.ts
+++ packages/antd/tsup.config.ts
@@ -36,12 +36,8 @@ export default defineConfig({
             },
         },
         copyStaticFiles({
-            src: "./src/assets/styles/styles.min.css",
-            dest: "./dist/styles.min.css",
-        }),
-        copyStaticFiles({
-            src: "./src/assets/styles/reset.min.css",
-            dest: "./dist/reset.min.css",
+            src: "./src/assets/styles/reset.css",
+            dest: "./dist/reset.css",
         }),
         NodeResolvePlugin({
             extensions: [".js", "ts", "tsx", "jsx"],
