cc08f1def5ee0b6c2e981bc2190b75b728f136ee
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-12-09 13:16:34 +0300

    fix: add useModalProps type description

--- packages/antd/src/hooks/modal/useModal/index.tsx
+++ packages/antd/src/hooks/modal/useModal/index.tsx
@@ -9,9 +9,17 @@ export type useModalReturnType = {
 } & Omit<useCoreModelReturnType, "visible">;
 
 export type useModalProps = {
+    /**
+     * Default props for Ant Design {@link https://ant.design/components/modal/ `<Modal>`} component.
+     */
     modalProps?: ModalProps;
 };
 
+/**
+ * By using `useModal` you get props for your records from API in accordance with Ant Design {@link https://ant.design/components/modal/ `<Modal>`} component.
+ *
+ * @see {@link https://refine.dev/docs/ui-frameworks/antd/hooks/ui/useModal} for more details.
+ */
 export const useModal = ({
     modalProps = {},
 }: useModalProps = {}): useModalReturnType => {
