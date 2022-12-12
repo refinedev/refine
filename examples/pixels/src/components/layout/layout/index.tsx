14c2d176d96badbd6b7cbf28b7f2e6efc9757b09
Salih <salihozdemir94@gmail.com>
2022-12-12 11:40:19 +0300

    feat: add background img

--- examples/pixels/src/components/layout/layout/index.tsx
+++ examples/pixels/src/components/layout/layout/index.tsx
@@ -6,7 +6,13 @@ import { Header } from "components/layout";
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
-        <AntdLayout style={{ minHeight: "100vh" }}>
+        <AntdLayout
+            style={{
+                minHeight: "100vh",
+                backgroundImage: "url('/bg.png')",
+                backgroundRepeat: "repeat-x",
+            }}
+        >
             <Header />
             <AntdLayout.Content>{children}</AntdLayout.Content>
         </AntdLayout>
