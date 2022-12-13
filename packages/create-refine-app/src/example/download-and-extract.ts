1071c9a7dec3b1ee8b2c000d43436b6e155ef490
Ali Emir Sen <senaliemir@gmail.com>
2022-12-13 16:45:50 +0300

    fix(create-refine-app): handle tmpdir manually

--- packages/create-refine-app/src/example/download-and-extract.ts
+++ packages/create-refine-app/src/example/download-and-extract.ts
@@ -3,16 +3,15 @@ import tar from "tar";
 import { Stream } from "stream";
 import { promisify } from "util";
 import { join } from "path";
-import { tmpdir } from "os";
 import { createWriteStream, promises as fs } from "fs";
 
 const pipeline = promisify(Stream.pipeline);
 
-const TEMP_PREFIX = "refine-example.temp";
+const TEMP_PREFIX = ".refine-example.temp";
 
 async function downloadTar(url: string) {
     try {
-        const tempFile = join(tmpdir(), `${TEMP_PREFIX}-${Date.now()}`);
+        const tempFile = join(process.cwd(), `${TEMP_PREFIX}-${Date.now()}`);
         await pipeline(got.stream(url), createWriteStream(tempFile));
         return tempFile;
     } catch (err) {
