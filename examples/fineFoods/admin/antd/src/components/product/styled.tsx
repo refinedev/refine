0ba1a3ab45b0a9de75e4b7f101aa8d4cfff0fd74
alican <alicanerdurmaz@gmail.com>
2022-12-14 10:25:23 +0300

    fix(antd-fine-foods): add padding to drawer title

---
+++ examples/fineFoods/admin/antd/src/components/product/styled.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import { Drawer as AntdDrawer } from "@pankod/refine-antd";
+
+export const Drawer = styled(AntdDrawer)`
+    .ant-page-header-heading {
+        padding-left: 24px;
+        padding-top: 12px;
+    }
+`;
