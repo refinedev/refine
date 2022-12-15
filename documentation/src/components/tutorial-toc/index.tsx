fd04b993bf384d43c5a1829e3fa9f7ab16faa002
Ali Emir Sen <senaliemir@gmail.com>
2022-12-16 01:38:13 +0300

    docs(tutorial): add custom toc

--- documentation/src/components/tutorial-toc/index.tsx
+++ documentation/src/components/tutorial-toc/index.tsx
@@ -8,13 +8,23 @@ import DocItemTOCMobile from "@theme/DocItem/TOC/Mobile";
 import DocItemTOCDesktop from "@theme/DocItem/TOC/Desktop";
 
 import { useCurrentTutorial } from "../../hooks/use-current-tutorial";
-import { useTutorialConfig } from "../../hooks/use-tutorial-config";
+import { useLocation } from "@docusaurus/router";
+// import { useTutorialConfig } from "../../hooks/use-tutorial-config";
 // import useGlobalData from "@docusaurus/useGlobalData";
 
 export const TutorialTOC = () => {
-    const tutorialConfig = useTutorialConfig();
-    const { /* frontMatter, */ toc } = useDoc();
+    // const tutorialConfig = useTutorialConfig();
+    const {
+        /* frontMatter, */ toc,
+        metadata: { id: currentDocId },
+    } = useDoc();
     const currentTutorial = useCurrentTutorial();
+
+    const { hash } = useLocation();
+
+    const [selectedUnit, setSelectedUnit] = React.useState(
+        currentTutorial.unit,
+    );
 
     // const test = useGlobalData();
 
@@ -23,16 +33,28 @@ export const TutorialTOC = () => {
             <li
                 key={item.id}
                 style={{
-                    marginLeft: item.level * 8,
+                    marginLeft: (item.level - 2) * 12,
                 }}
+                className="mb-1"
             >
-                <a href={`#${item.id}`}>{item.value}</a>
+                <a
+                    href={`#${item.id}`}
+                    className={`${
+                        `${hash}`.slice(1) === item.id
+                            ? "text-[#1890ff]"
+                            : "text-slate-500 hover:text-[#1890ff] active:text-[#1890ff]"
+                    }`}
+                >
+                    {item.value}
+                </a>
             </li>
         );
     };
 
     const renderTOC = () => {
-        return <ul style={{ paddingLeft: 0 }}>{toc.map(renderTocItem)}</ul>;
+        return (
+            <ul className="list-none pl-0 mt-1">{toc.map(renderTocItem)}</ul>
+        );
     };
 
     const renderUnitItem = (
@@ -41,16 +63,26 @@ export const TutorialTOC = () => {
         >["units"][number]["docs"][number],
     ) => {
         return (
-            <li key={doc.id}>
-                <a
-                    onClick={(e) => {
-                        doc.current ? e.preventDefault() : undefined;
-                    }}
-                    href={"/docs/" + doc.id}
-                >
-                    {doc.title}
-                </a>
-                {doc.current && renderTOC()}
+            <li key={doc.id} className="pb-2 flex flex-row gap-2 items-start">
+                <div className="w-4 h-4 bg-slate-300 rounded-full mt-0.5" />
+                <div className="flex flex-col gap-2">
+                    <a
+                        onClick={(e) => {
+                            doc.current ? e.preventDefault() : undefined;
+                        }}
+                        href={"/docs/" + doc.id}
+                        className={`${
+                            currentDocId === doc.id ? "font-bold" : ""
+                        } text-slate-600 ${
+                            doc.current
+                                ? "hover:cursor-default hover:no-underline"
+                                : ""
+                        }`}
+                    >
+                        {doc.title}
+                    </a>
+                    {doc.current && renderTOC()}
+                </div>
             </li>
         );
     };
@@ -60,22 +92,56 @@ export const TutorialTOC = () => {
     ) => {
         return (
             <div>
-                <h3>
-                    {tutorialConfig.tutorial.units[unit.unit]?.label ??
-                        unit.unit}
-                </h3>
-                <ul style={{ paddingLeft: 0 }}>
+                <ul className="list-none pl-0">
                     {unit.docs.map(renderUnitItem)}
                 </ul>
             </div>
         );
     };
 
-    const renderUnits = () => {
-        return currentTutorial?.units.map(renderUnitDocs);
+    const renderUnitTab = (unit: typeof currentTutorial["units"][number]) => {
+        return (
+            <div
+                key={unit.no}
+                className="w-7 text-center h-9 flex justify-center items-start pt-1"
+            >
+                <button
+                    type="button"
+                    onClick={() => setSelectedUnit(unit.unit)}
+                    className={`w-7 h-7 cursor-pointer bg-slate-200 text-slate-500 border-solid border-2 ${
+                        selectedUnit === unit.unit
+                            ? "border-slate-400"
+                            : "border-transparent"
+                    } font-semibold flex justify-center items-center`}
+                >
+                    {unit.no}
+                </button>
+            </div>
+        );
     };
 
-    return <div>{renderUnits()}</div>;
+    const currentUnit = currentTutorial?.units.find(
+        (unit) => unit.unit === selectedUnit,
+    );
+
+    // max-height: calc(100vh - var(--ifm-navbar-height) - 2rem);
+    // overflow-y: auto;
+    // position: sticky;
+    // top: calc(var(--ifm-navbar-height) + 1rem);
+
+    return (
+        <div className="sticky top-[5rem] max-h-[calc(100vh-6rem]">
+            <div className="unit-tabs flex gap-1 mb-1">
+                {currentTutorial?.units.map(renderUnitTab)}
+            </div>
+            <div className="unit-list-container bg-slate-100 py-3 px-3">
+                <div className="font-bold text-sm mb-2 text-slate-700">
+                    {currentUnit.title ?? currentUnit.unit}
+                </div>
+                <div className="text-sm">{renderUnitDocs(currentUnit)}</div>
+            </div>
+        </div>
+    );
 };
 
 export const useDocTOCwithTutorial = () => {
