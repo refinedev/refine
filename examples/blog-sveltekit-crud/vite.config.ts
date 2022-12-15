324a59701ab4210bf17f4b69c65046fcfae7b250
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-12-15 12:15:55 +0300

    fix: blog-sveltekit-crud

--- examples/blog-sveltekit-crud/vite.config.ts
+++ examples/blog-sveltekit-crud/vite.config.ts
@@ -1,8 +1,6 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+	plugins: [svelte({})]
+});
