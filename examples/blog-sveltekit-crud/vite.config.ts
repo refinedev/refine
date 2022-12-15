0618b151b3fb04411d0b97a7a181bb7cfa2f20bb
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-12-15 12:25:14 +0300

    fix: blog-sveltekit-crud config

--- examples/blog-sveltekit-crud/vite.config.ts
+++ examples/blog-sveltekit-crud/vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [svelte({})]
+	plugins: [
+		svelte({
+			// config
+		})
+	]
 });
