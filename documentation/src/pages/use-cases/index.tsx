deb3a7505eef14dbbc6dc6e20bc37785f225df70
Salih <salihozdemir94@gmail.com>
2022-12-16 15:56:46 +0300

    fix: complete the use-cases page

--- documentation/src/pages/use-cases/index.tsx
+++ documentation/src/pages/use-cases/index.tsx
@@ -3,6 +3,8 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import Head from "@docusaurus/Head";
 
+import { GiftCard } from "../../components/use-case/gift-card";
+
 import { USE_CASES } from "../../assets/use-cases";
 
 export const UseCases = () => {
@@ -18,20 +20,42 @@ export const UseCases = () => {
                 </h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     {USE_CASES.map((useCase) => (
-                        <div key={useCase.route} className="flex flex-col">
-                            <img
-                                src={useCase.thumbImgURL}
-                                alt={useCase.title}
-                            />
-                            <div>{useCase.title}</div>
-                            <div
-                                dangerouslySetInnerHTML={{
-                                    __html: useCase.description,
-                                }}
-                            />
+                        <div
+                            key={useCase.route}
+                            className="flex flex-col justify-between"
+                        >
+                            <div>
+                                <Link to={useCase.route}>
+                                    <img
+                                        src={useCase.thumbImgURL}
+                                        alt={useCase.title}
+                                    />
+                                </Link>
+                                <div className="font-montserrat font-bold text-[#242436] my-2">
+                                    {useCase.title}
+                                </div>
+                                <div
+                                    className="font-montserrat text-[#242436] text-sm"
+                                    dangerouslySetInnerHTML={{
+                                        __html: useCase.description,
+                                    }}
+                                />
+                            </div>
+
+                            <Link
+                                className="shadow-startTiles appearance-none flex items-center justify-center mt-2 mb-4 no-underline font-bold font-montserrat text-sm h-8 w-44 text-[#1890FF] text-center bg-white rounded-[4px] cursor-pointer border border-solid border-[#EDEDEF]"
+                                to={useCase.route}
+                            >
+                                Read more
+                            </Link>
                         </div>
                     ))}
                 </div>
+                <br />
+                <br />
+                <GiftCard />
+                <br />
+                <br />
             </div>
         </Layout>
     );
