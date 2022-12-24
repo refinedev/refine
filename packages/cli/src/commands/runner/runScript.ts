a975290c21485274291f2666eb22685418f9c69b
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2022-12-24 18:46:44 +0100

    fix: terminal console output color

--- packages/cli/src/commands/runner/runScript.ts
+++ packages/cli/src/commands/runner/runScript.ts
@@ -4,6 +4,10 @@ export const runScript = async (binPath: string, args: string[]) => {
     const execution = execa(binPath, args, {
         stdio: "pipe",
         windowsHide: false,
+        env: {
+            FORCE_COLOR: "true",
+            ...process.env,
+        },
     });
 
     execution.stdout?.pipe(process.stdout);
