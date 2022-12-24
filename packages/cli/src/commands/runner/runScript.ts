4cfdc655ea118fc9df5a9cc7592236c3bc7af1db
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2022-12-24 18:38:40 +0100

    fix: server keeps running on Windows

--- packages/cli/src/commands/runner/runScript.ts
+++ packages/cli/src/commands/runner/runScript.ts
@@ -3,6 +3,7 @@ import execa from "execa";
 export const runScript = async (binPath: string, args: string[]) => {
     const execution = execa(binPath, args, {
         stdio: "pipe",
+        windowsHide: false,
     });
 
     execution.stdout?.pipe(process.stdout);
