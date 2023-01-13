c46179cbc2126e7d3e4765709d492787fb59bdf6
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-01-13 16:51:27 +0300

    test: add useInfinityGetList hook

---
+++ packages/core/src/hooks/data/useInfinityGetList.spec.tsx
@@ -0,0 +1,207 @@
+import { renderHook, waitFor } from "@testing-library/react";
+
+import { MockJSONServer, TestWrapper } from "@test";
+
+import { useInfinityGetList } from "./useInfinityGetList";
+import { defaultRefineOptions } from "@contexts/refine";
+import { IRefineContextProvider } from "../../interfaces";
+
+const mockRefineProvider: IRefineContextProvider = {
+    hasDashboard: false,
+    ...defaultRefineOptions,
+    options: defaultRefineOptions,
+};
+
+describe("useInfinityGetList Hook", () => {
+    it("with rest json server", async () => {
+        const { result } = renderHook(
+            () => useInfinityGetList({ resource: "posts" }),
+            {
+                wrapper: TestWrapper({
+                    dataProvider: MockJSONServer,
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+
+        await waitFor(() => {
+            expect(result.current.isSuccess).toBeTruthy();
+        });
+
+        const { data } = result.current;
+
+        expect(data?.pages).toHaveLength(1);
+        expect(data?.pages[0].data).toHaveLength(2);
+        expect(data?.pages[0].total).toEqual(2);
+    });
+
+    it("hasNextPage is truthy", async () => {
+        const { result } = renderHook(
+            () =>
+                useInfinityGetList({
+                    resource: "posts",
+                    config: {
+                        pagination: {
+                            pageSize: 1,
+                        },
+                    },
+                }),
+            {
+                wrapper: TestWrapper({
+                    dataProvider: MockJSONServer,
+                    resources: [{ name: "posts" }],
+                }),
+            },
+        );
+
+        await waitFor(() => {
+            expect(result.current.isSuccess).toBeTruthy();
+        });
+
+        const { hasNextPage } = result.current;
+        expect(hasNextPage).toBeTruthy();
+    });
+
+    describe("useResourceSubscription", () => {
+        it("useSubscription", async () => {
+            const onSubscribeMock = jest.fn();
+
+            const { result } = renderHook(
+                () =>
+                    useInfinityGetList({
+                        resource: "posts",
+                    }),
+                {
+                    wrapper: TestWrapper({
+                        dataProvider: MockJSONServer,
+                        resources: [{ name: "posts" }],
+                        liveProvider: {
+                            unsubscribe: jest.fn(),
+                            subscribe: onSubscribeMock,
+                        },
+                        refineProvider: {
+                            ...mockRefineProvider,
+                            liveMode: "auto",
+                        },
+                    }),
+                },
+            );
+
+            await waitFor(() => {
+                expect(result.current.isSuccess).toBeTruthy();
+            });
+
+            expect(onSubscribeMock).toBeCalled();
+            expect(onSubscribeMock).toHaveBeenCalledWith({
+                channel: "resources/posts",
+                callback: expect.any(Function),
+                params: {
+                    filters: undefined,
+                    hasPagination: undefined,
+                    metaData: undefined,
+                    pagination: undefined,
+                    resource: "posts",
+                    sort: undefined,
+                    subscriptionType: "useList",
+                },
+                types: ["*"],
+            });
+        });
+
+        it("liveMode = Off useSubscription", async () => {
+            const onSubscribeMock = jest.fn();
+
+            const { result } = renderHook(
+                () =>
+                    useInfinityGetList({
+                        resource: "posts",
+                    }),
+                {
+                    wrapper: TestWrapper({
+                        dataProvider: MockJSONServer,
+                        resources: [{ name: "posts" }],
+                        liveProvider: {
+                            unsubscribe: jest.fn(),
+                            subscribe: onSubscribeMock,
+                        },
+                        refineProvider: {
+                            ...mockRefineProvider,
+                            liveMode: "off",
+                        },
+                    }),
+                },
+            );
+
+            await waitFor(() => {
+                expect(result.current.isSuccess).toBeTruthy();
+            });
+
+            expect(onSubscribeMock).not.toBeCalled();
+        });
+
+        it("liveMode = Off and liveMode hook param auto", async () => {
+            const onSubscribeMock = jest.fn();
+
+            const { result } = renderHook(
+                () =>
+                    useInfinityGetList({ resource: "posts", liveMode: "auto" }),
+                {
+                    wrapper: TestWrapper({
+                        dataProvider: MockJSONServer,
+                        resources: [{ name: "posts" }],
+                        liveProvider: {
+                            unsubscribe: jest.fn(),
+                            subscribe: onSubscribeMock,
+                        },
+                        refineProvider: {
+                            ...mockRefineProvider,
+                            liveMode: "off",
+                        },
+                    }),
+                },
+            );
+
+            await waitFor(() => {
+                expect(result.current.isSuccess).toBeTruthy();
+            });
+
+            expect(onSubscribeMock).toBeCalled();
+        });
+
+        it("unsubscribe call on unmount", async () => {
+            const onSubscribeMock = jest.fn(() => true);
+            const onUnsubscribeMock = jest.fn();
+
+            const { result, unmount } = renderHook(
+                () =>
+                    useInfinityGetList({
+                        resource: "posts",
+                    }),
+                {
+                    wrapper: TestWrapper({
+                        dataProvider: MockJSONServer,
+                        resources: [{ name: "posts" }],
+                        liveProvider: {
+                            unsubscribe: onUnsubscribeMock,
+                            subscribe: onSubscribeMock,
+                        },
+                        refineProvider: {
+                            ...mockRefineProvider,
+                            liveMode: "auto",
+                        },
+                    }),
+                },
+            );
+
+            await waitFor(() => {
+                expect(result.current.isSuccess).toBeTruthy();
+            });
+
+            expect(onSubscribeMock).toBeCalled();
+
+            unmount();
+            expect(onUnsubscribeMock).toBeCalledWith(true);
+            expect(onUnsubscribeMock).toBeCalledTimes(1);
+        });
+    });
+});
