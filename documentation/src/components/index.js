de6943824b949dbeead932555916278f83d1a043
Ömer Faruk APLAK <omer@pankod.com>
2023-01-18 12:32:12 +0300

    fix: remove missed file import

--- documentation/src/components/index.js
+++ documentation/src/components/index.js
@@ -2,7 +2,6 @@ export * from "./hero";
 export * from "./fast-and-flexible";
 export * from "./comments";
 export * from "./key-features";
-export * from "./perfect-implementation";
 export * from "./get-started";
 export * from "./browser-window";
 export * from "./integrations";
