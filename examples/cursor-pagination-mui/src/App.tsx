31c223abc79dd2b38a18642e8458149be5b9010d
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-01-23 15:06:30 +0300

    chore: base example

---
+++ examples/cursor-pagination-mui/src/App.tsx
@@ -0,0 +1,49 @@
+import { Refine } from "@pankod/refine-core";
+import {
+    Layout,
+    LoginPage,
+    ErrorComponent,
+    ReadyPage,
+    LightTheme,
+    ThemeProvider,
+    notificationProvider,
+    RefineSnackbarProvider,
+    CssBaseline,
+    GlobalStyles,
+} from "@pankod/refine-mui";
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
+
+import { PostsList, PostCreate, PostEdit } from "pages/posts";
+
+const App: React.FC = () => {
+    return (
+        <ThemeProvider theme={LightTheme}>
+            <CssBaseline />
+            <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+            <RefineSnackbarProvider>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(
+                        "https://api.fake-rest.refine.dev",
+                    )}
+                    notificationProvider={notificationProvider}
+                    ReadyPage={ReadyPage}
+                    Layout={Layout}
+                    LoginPage={LoginPage}
+                    catchAll={<ErrorComponent />}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: PostsList,
+                            create: PostCreate,
+                            edit: PostEdit,
+                        },
+                    ]}
+                />
+            </RefineSnackbarProvider>
+        </ThemeProvider>
+    );
+};
+
+export default App;
