f1098c679c13d731f561971a559ce17197a8ba70
Salih <salihozdemir94@gmail.com>
2023-01-23 12:36:04 +0300

    feat(core): add `refine.config` and AuthPage swizzle config

---
+++ packages/core/refine.config.js
@@ -0,0 +1,98 @@
+const { getImports, appendAfterImports } = require("@pankod/refine-cli");
+
+/** @type {import('@pankod/refine-cli').RefineConfig} */
+module.exports = {
+    group: "UI Framework",
+    swizzle: {
+        items: [
+            {
+                group: "Pages",
+                label: "AuthPage",
+                message: ` 
+                **\`Warning:\`**
+                If you want to change the default auth pages;
+                You should pass it with the **LoginPage** prop to the **<Refine/>** component.
+
+                \`\`\`
+                // title: App.tsx
+                import { AuthPage } from "components/pages/auth";
+
+                const App = () => {
+                    return (
+                        <Refine
+                            LoginPage={AuthPage}
+                            /* ... */
+                        />
+                    );
+                }
+                \`\`\`
+                `,
+                files: [
+                    {
+                        src: "./src/components/pages/auth/index.tsx",
+                        dest: "./components/pages/auth/index.tsx",
+                    },
+                    {
+                        src: "./src/components/pages/auth/components/forgotPassword/index.tsx",
+                        dest: "./components/pages/auth/components/forgotPassword.tsx",
+                    },
+                    {
+                        src: "./src/components/pages/auth/components/login/index.tsx",
+                        dest: "./components/pages/auth/components/login.tsx",
+                    },
+                    {
+                        src: "./src/components/pages/auth/components/register/index.tsx",
+                        dest: "./components/pages/auth/components/register.tsx",
+                    },
+                    {
+                        src: "./src/components/pages/auth/components/updatePassword/index.tsx",
+                        dest: "./components/pages/auth/components/updatePassword.tsx",
+                    },
+                    {
+                        src: "./src/components/pages/auth/components/index.ts",
+                        dest: "./components/pages/auth/components/index.ts",
+                    },
+                ],
+            },
+        ],
+        transform: (content) => {
+            let newContent = content;
+            const imports = getImports(content);
+
+            imports.map((importItem) => {
+                if (
+                    importItem.importPath === "../../../../../interfaces" ||
+                    importItem.importPath === "../../../interfaces" ||
+                    importItem.importPath === "@hooks" ||
+                    importItem.importPath === "@hooks/translate"
+                ) {
+                    const newStatement = `import ${importItem.namedImports} from "@pankod/refine-core";`;
+
+                    newContent = newContent.replace(
+                        importItem.statement,
+                        newStatement,
+                    );
+                }
+
+                if (importItem.importPath === "../..") {
+                    const typesLine = `
+                    type DivPropsType = React.DetailedHTMLProps<
+                        React.HTMLAttributes<HTMLDivElement>,
+                        HTMLDivElement
+                    >;
+                    type FormPropsType = React.DetailedHTMLProps<
+                        React.FormHTMLAttributes<HTMLFormElement>,
+                        HTMLFormElement
+                    >;
+                    `;
+
+                    newContent = newContent.replace(importItem.statement, "");
+
+                    newContent = appendAfterImports(newContent, typesLine);
+                }
+            });
+
+            return newContent;
+        },
+    },
+};
