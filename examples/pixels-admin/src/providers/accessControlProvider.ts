fd00bcd61010480b639ddedabecaef391efd7cec
Ömer Faruk APLAK <omer@pankod.com>
2023-01-31 13:54:03 +0300

    fix: getPermissions usage

--- examples/pixels-admin/src/providers/accessControlProvider.ts
+++ examples/pixels-admin/src/providers/accessControlProvider.ts
@@ -5,7 +5,7 @@ import { authProvider } from "./authProvider";
 
 export const accessControlProvider = {
     can: async ({ resource, action }: CanParams): Promise<CanReturnType> => {
-        const role = await authProvider.getPermissions();
+        const role = await authProvider.getPermissions?.();
 
         const enforcer = await newEnforcer(model, adapter);
         const can = await enforcer.enforce(role, resource, action);
