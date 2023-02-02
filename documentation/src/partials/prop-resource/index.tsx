4df15647729ca252a4cfb6eae6b7e18cb3ef7ce9
alican <alicanerdurmaz@gmail.com>
2023-02-02 14:05:26 +0300

    docs(use-table): update PropResource default text

--- documentation/src/partials/prop-resource/index.tsx
+++ documentation/src/partials/prop-resource/index.tsx
@@ -17,18 +17,17 @@ const PropResource: React.FC<Props> = ({ hook, method, hasDefault = true }) => {
     const md = `It will be passed to the [\`${method?.name}\`](${method?.URL}) method from the \`dataProvider\` as parameter via the [\`${hook?.name}\`](${hook?.URL}) hook. 
     The parameter is usually used as an API endpoint path.
     It all depends on how to handle the \`resource\` in the [\`${method?.name}\`](${method?.URL}) method. 
-    See the [creating a data provider](/docs/api-reference/core/providers/data-provider/#creating-a-data-provider) section for an example of how resource are handled.`;
+    See the [creating a data provider](/docs/api-reference/core/providers/data-provider/#creating-a-data-provider) section for an example of how resources are handled.`;
 
     return (
         <>
-            <ReactMarkdown>{md}</ReactMarkdown>
             {hasDefault && (
                 <ReactMarkdown>
-                    By default, the `resource` value is determined from the
-                    active route where the component or the hook is used. It can
-                    be overridden by passing the `resource` prop.
+                    &gt; Default: It reads the `resource` value from the current
+                    URL.
                 </ReactMarkdown>
             )}
+            <ReactMarkdown>{md}</ReactMarkdown>
         </>
     );
 };
