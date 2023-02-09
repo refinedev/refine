d01892c1cdb4917e025c1d6b1f1bf6b37852adf7
necatiozmen <necatiozmn@gmail.com>
2023-02-09 20:44:01 +0300

    chore: disable eslint

--- examples/mern-dashboard/client/src/interfaces/theme.d.ts
+++ examples/mern-dashboard/client/src/interfaces/theme.d.ts
@@ -8,11 +8,9 @@ export interface CustomTheme {
 }
 
 declare module "@pankod/refine-mui" {
-    // eslint-disable-next-line no-use-before-define
     // @ts-ignore
     interface Theme extends import("@pankod/refine-mui").Theme, CustomTheme {}
     interface ThemeOptions
-        // eslint-disable-next-line
         // @ts-ignore
         extends import("@pankod/refine-mui").ThemeOptions,
             CustomTheme {}
