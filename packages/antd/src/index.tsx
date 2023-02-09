b02b644415db788d97671dc62d00d41c25667cf1
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-02-09 16:52:20 +0300

    chore: remove antd exports

--- packages/antd/src/index.tsx
+++ packages/antd/src/index.tsx
@@ -1,11 +1,6 @@
 export * from "./hooks";
 export * from "./providers";
-export * from "./components/antd";
 export * from "./components";
-
-// antd filterDropDownProps (using for <FilterDropDown> component)
-export type { FilterDropdownProps } from "antd/lib/table/interface";
-export type { RcFile, UploadFile } from "antd/lib/upload/interface";
 
 export {
     getDefaultSortOrder,
