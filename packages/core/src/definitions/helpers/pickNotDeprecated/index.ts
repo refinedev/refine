3d6d7b756d8a0cb072e4e56cdc0cebbc2ccd2e98
alican <alicanerdurmaz@gmail.com>
2023-02-09 15:33:45 +0300

    feat(core): add internal info to pickNotDeprecated

--- packages/core/src/definitions/helpers/pickNotDeprecated/index.ts
+++ packages/core/src/definitions/helpers/pickNotDeprecated/index.ts
@@ -1,5 +1,6 @@
 /*
  * Returns first value that is not undefined.
+ * @internal This is an internal helper function. Please do not use externally.
  */
 export const pickNotDeprecated = <T extends unknown[]>(
     ...args: T
