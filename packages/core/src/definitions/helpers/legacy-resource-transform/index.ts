5191fce8ef2a155713ab2e2a6edc070f815bbea5
Ali Emir Sen <senaliemir@gmail.com>
2023-02-13 09:10:03 +0300

    feat(helpers): add legacy transform for resources

---
+++ packages/core/src/definitions/helpers/legacy-resource-transform/index.ts
@@ -0,0 +1,30 @@
+import {
+    IResourceItem,
+    ResourceProps,
+} from "../../../interfaces/bindings/resource";
+
+import { routeGenerator } from "../routeGenerator";
+
+/**
+ * For the legacy definition of resources, we did a basic transformation for provided resources
+ * - This is meant to provide an easier way to access properties.
+ * - In the new definition, we don't need to do transformations and properties can be accessed via helpers or manually.
+ * This is kept for backward compability
+ */
+export const legacyResourceTransform = (resources: ResourceProps[]) => {
+    const _resources: IResourceItem[] = [];
+
+    resources?.forEach((resource) => {
+        _resources.push({
+            ...resource,
+            label: resource.options?.label,
+            route: routeGenerator(resource, resources),
+            canCreate: !!resource.create,
+            canEdit: !!resource.edit,
+            canShow: !!resource.show,
+            canDelete: resource.canDelete,
+        });
+    });
+
+    return _resources;
+};
