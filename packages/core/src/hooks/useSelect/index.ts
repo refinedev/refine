7114782bc3bd04630bd68b032893bdbf6b24dfe7
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-02-13 07:55:54 +0300

    feat(v4): add generic to option label-value on `useSelect` (#3568)
    * feat(core): add generic to option label-value on use select
    
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>
    
    * fix(finefoods-antd): useSelect types
    
    * fix(core): changeset semver patch to minor
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    ---------
    
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- packages/core/src/hooks/useSelect/index.ts
+++ packages/core/src/hooks/useSelect/index.ts
@@ -30,12 +30,12 @@ export type UseSelectProps<TData, TError> = {
      * Set the option's value
      * @default `"title"`
      */
-    optionLabel?: string;
+    optionLabel?: keyof TData extends string ? keyof TData : never;
     /**
      * Set the option's label value
      * @default `"id"`
      */
-    optionValue?: string;
+    optionValue?: keyof TData extends string ? keyof TData : never;
     /**
      * Allow us to sort the options
      * @deprecated Use `sorters` instead
@@ -151,8 +151,8 @@ export const useSelect = <
         (data: GetManyResponse<TData>) => {
             setSelectedOptions(
                 data.data.map((item) => ({
-                    label: get(item, optionLabel),
-                    value: get(item, optionValue),
+                    label: get(item, optionLabel) as string,
+                    value: get(item, optionValue) as string,
                 })),
             );
         },
@@ -185,8 +185,8 @@ export const useSelect = <
             {
                 setOptions(
                     data.data.map((item) => ({
-                        label: get(item, optionLabel),
-                        value: get(item, optionValue),
+                        label: get(item, optionLabel) as string,
+                        value: get(item, optionValue) as string,
                     })),
                 );
             }
