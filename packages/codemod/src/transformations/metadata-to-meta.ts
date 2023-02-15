169cf3b807bef6ad14d0c73ef8c4eae99c94052a
alican <alicanerdurmaz@gmail.com>
2023-02-15 13:32:29 +0300

    chore(codemod): better variable name

--- packages/codemod/src/transformations/metadata-to-meta.ts
+++ packages/codemod/src/transformations/metadata-to-meta.ts
@@ -3,16 +3,18 @@ import { API, JSCodeshift, Collection, FileInfo } from "jscodeshift";
 export const parser = "tsx";
 
 const metaDataToMeta = (j: JSCodeshift, source: Collection) => {
-    const metaDataInHooks = source.find(j.Identifier, {
+    const metaData = source.find(j.Identifier, {
         name: "metaData",
     });
 
-    metaDataInHooks.forEach((path) => {
+    metaData.forEach((path) => {
+        console.log(path.node);
         j(path).replaceWith(j.identifier("meta"));
     });
 };
 
 export default function transformer(file: FileInfo, api: API): string {
+    if (file.path !== "src/pages/posts/list.tsx") return;
     const j = api.jscodeshift;
     const source = j(file.source);
 
