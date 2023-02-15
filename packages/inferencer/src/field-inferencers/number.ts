b516f98127eefb1f111dfdc60e645206228e12de
Ali Emir Sen <senaliemir@gmail.com>
2023-02-15 16:53:32 +0300

    fix(inferencer): add string to number check

--- packages/inferencer/src/field-inferencers/number.ts
+++ packages/inferencer/src/field-inferencers/number.ts
@@ -1,9 +1,25 @@
 import { FieldInferencer } from "@/types";
 
 export const numberInfer: FieldInferencer = (key, value) => {
+    const isNonEmptyString = typeof value === "string" && value.length > 0;
+    const isNotNaN = !isNaN(value as number);
+
+    const isNumericString = isNonEmptyString && isNotNaN;
     const isNumber = typeof value === "number";
 
-    if (isNumber) {
+    const isNumeric = isNumericString || isNumber;
+
+    console.log({
+        key,
+        value,
+        isNumeric,
+        isNumber,
+        isNumericString,
+        isNotNaN,
+        isNonEmptyString,
+    });
+
+    if (isNumeric) {
         return {
             key,
             type: "number",
