ccde7d50757deee4815e849cf0cbe4fb370f2c7d
alican <alicanerdurmaz@gmail.com>
2023-02-16 14:55:16 +0300

    feat(codemod): improve metaData to meta algorithm

--- packages/codemod/src/transformations/metadata-to-meta.ts
+++ packages/codemod/src/transformations/metadata-to-meta.ts
@@ -3,16 +3,48 @@ import { API, JSCodeshift, Collection, FileInfo } from "jscodeshift";
 export const parser = "tsx";
 
 const metaDataToMeta = (j: JSCodeshift, source: Collection) => {
-    const metaData = source.find(j.Identifier, {
-        name: "metaData",
+    // find all JSX elements
+    source.find(j.JSXElement).forEach((path) => {
+        const attributes = path.node.openingElement.attributes;
+
+        // if they have a meta attribute, skip them.
+        const hasMeta = attributes.some(
+            (attribute) => attribute["name"]["name"] === "meta",
+        );
+        if (hasMeta) return;
+
+        // if they have a metaData change it to meta.
+        path.node.openingElement.attributes.forEach((attribute) => {
+            if (attribute["name"]["name"] === "metaData") {
+                attribute["name"]["name"] = "meta";
+            }
+        });
     });
 
-    metaData.forEach((path) => {
-        j(path).replaceWith(j.identifier("meta"));
+    // find all call expressions
+    source.find(j.CallExpression).forEach((path) => {
+        // find all arguments
+        path.node.arguments.forEach((argument) => {
+            const properties = argument["properties"];
+
+            // if they have a meta argument, skip them.
+            const hasMeta = properties.some(
+                (property) => property["key"]["name"] === "meta",
+            );
+            if (hasMeta) return;
+
+            // if they have a metaData change it to meta.
+            properties.forEach((property) => {
+                if (property["key"]["name"] === "metaData") {
+                    property["key"]["name"] = "meta";
+                }
+            });
+        });
     });
 };
 
 export default function transformer(file: FileInfo, api: API): string {
+    if (file.path !== "src/pages/admin/movies/list.tsx") return;
     const j = api.jscodeshift;
     const source = j(file.source);
 
