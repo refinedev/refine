d7647b4cca81f32ff34a3d7ca83855018f332010
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-02-17 09:37:22 +0300

    feat(codemod): add base separate imports

---
+++ packages/codemod/src/transformations/separate-imports-mui.ts
@@ -0,0 +1,57 @@
+import { API, FileInfo } from "jscodeshift";
+import fs from "fs";
+import path from "path";
+import { install } from "../helpers";
+import checkPackageLock from "../helpers/checkPackageLock";
+import separateImports from "../helpers/separateImports";
+import { exported } from "../definitions/separated-imports/mui";
+
+export const parser = "tsx";
+
+export async function postTransform(files: any, flags: any) {
+    const rootDir = path.join(process.cwd(), files[0]);
+    const packageJsonPath = path.join(rootDir, "package.json");
+    const useYarn = checkPackageLock(rootDir) === "yarn.lock";
+
+    // Check root package.json exists
+    try {
+        JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+    } catch (err) {
+        console.error(
+            `Error: failed to load package.json from ${packageJsonPath}, ensure provided directory is root.`,
+        );
+    }
+
+    if (!flags.dry) {
+        await install(
+            rootDir,
+            [
+                `@mui/icons-material@^5.8.3`,
+                `@mui/lab@^5.0.0-alpha.85`,
+                `@mui/material@^5.8.6`,
+                `@mui/x-data-grid@^5.12.1`,
+            ],
+            {
+                useYarn,
+                isOnline: true,
+            },
+        );
+    }
+}
+
+export default function transformer(file: FileInfo, api: API): string {
+    const j = api.jscodeshift;
+    const source = j(file.source);
+
+    separateImports({
+        j,
+        source,
+        imports: [],
+        renameImports: {},
+        otherImports: exported,
+        currentLibName: "@pankod/refine-mui",
+        nextLibName: "",
+    });
+
+    return source.toSource();
+}
