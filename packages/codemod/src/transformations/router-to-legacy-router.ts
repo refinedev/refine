9307969159164bfe22e49d324b6de6ac649cd8be
Ali Emir Sen <senaliemir@gmail.com>
2023-02-20 15:13:04 +0300

    feat(codemod): rename to legacy router provider prop

--- packages/codemod/src/transformations/router-to-legacy-router.ts
+++ packages/codemod/src/transformations/router-to-legacy-router.ts
@@ -1,4 +1,10 @@
-import { API, JSCodeshift, Collection, FileInfo } from "jscodeshift";
+import {
+    API,
+    JSCodeshift,
+    Collection,
+    FileInfo,
+    JSXAttribute,
+} from "jscodeshift";
 
 export const parser = "tsx";
 
@@ -11,6 +17,9 @@ const legacyMap = {
         "@pankod/refine-nextjs-router/legacy-pages",
     "@pankod/refine-remix-router": "@pankod/refine-remix-router/legacy",
 };
+
+const oldRouterProp = "routerProvider";
+const newRouterProp = "legacyRouterProvider";
 
 const renameImport = (
     j: JSCodeshift,
@@ -28,6 +37,33 @@ const renameImport = (
         });
 };
 
+const renameProp = (
+    j: JSCodeshift,
+    source: Collection,
+    from: string,
+    to: string,
+) => {
+    source
+        .find(j.JSXOpeningElement, { name: { name: "Refine" } })
+        .forEach((path) => {
+            const props = path.node.attributes;
+
+            const propIndex = props.findIndex(
+                (prop) => (prop as JSXAttribute)?.name?.name === from,
+            );
+
+            if (propIndex !== -1) {
+                const prop = props[propIndex];
+
+                if ((prop as JSXAttribute)?.name) {
+                    if ("name" in prop) {
+                        prop.name.name = to;
+                    }
+                }
+            }
+        });
+};
+
 export default function transformer(file: FileInfo, api: API): string {
     const j = api.jscodeshift;
     const source = j(file.source);
@@ -35,6 +71,8 @@ export default function transformer(file: FileInfo, api: API): string {
     Object.entries(legacyMap).forEach(([from, to]) => {
         renameImport(j, source, from, to);
     });
+
+    renameProp(j, source, oldRouterProp, newRouterProp);
 
     return source.toSource();
 }
