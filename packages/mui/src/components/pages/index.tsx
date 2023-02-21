9e0869c1c260411e6891f4c6469ba1039c6ffbab
Salih Özdemir <salihozdemir94@gmail.com>
2023-02-21 09:48:01 +0300

    feat(v4): add `WelcomePage`, deprecate `ReadyPage` (#3642)
    * fix(core): deprecate ReadyPage, add WelcomePage
    
    * fix(antd): deprecate ReadyPage, add WelcomePage
    
    * fix(mui): deprecate ReadyPage, add WelcomePage
    
    * fix(mantine): deprecate ReadyPage, add WelcomePage
    
    * fix(chakra-ui): deprecate ReadyPage, add WelcomePage
    
    * feat(chakra-ui): add ts doc about welcome page
    
    * chore: add changeset

--- packages/mui/src/components/pages/index.tsx
+++ packages/mui/src/components/pages/index.tsx
@@ -1,5 +1,6 @@
 export { LoginPage } from "./login";
 export { ReadyPage } from "./ready";
+export { WelcomePage } from "./welcome";
 export { ErrorComponent } from "./error";
 export { AuthPage } from "./auth";
 export type { AuthProps } from "./auth";
