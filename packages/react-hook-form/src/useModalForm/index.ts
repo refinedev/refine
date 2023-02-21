26be15aacfffd6d6d51469f7f9e99d6cd84f47ba
Ali Emir Sen <senaliemir@gmail.com>
2023-02-22 00:23:24 +0300

    feat(hook-form): update with new routing

--- packages/react-hook-form/src/useModalForm/index.ts
+++ packages/react-hook-form/src/useModalForm/index.ts
@@ -108,9 +108,7 @@ export const useModalForm = <
         }
     };
 
-    const { resourceName } = useResource({
-        resourceNameOrRouteName: resourceProp,
-    });
+    const { resource } = useResource(resourceProp);
 
     const { warnWhen, setWarnWhen } = useWarnAboutChange();
     const handleClose = useCallback(() => {
@@ -140,10 +138,15 @@ export const useModalForm = <
     }, []);
 
     const title = translate(
-        `${resourceName}.titles.${actionProp}`,
+        `${resource?.name}.titles.${actionProp}`,
         undefined,
         `${userFriendlyResourceName(
-            `${actionProp} ${resourceName}`,
+            `${actionProp} ${
+                resource?.meta?.label ??
+                resource?.options?.label ??
+                resource?.label ??
+                resource?.name
+            }`,
             "singular",
         )}`,
     );
