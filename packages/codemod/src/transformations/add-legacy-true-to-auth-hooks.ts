02cdf6a1edbe856d78c8954e1ab463f7c12b7372
alican <alicanerdurmaz@gmail.com>
2023-02-23 14:13:43 +0300

    feat(codemod): add missing hook name to to legacy true to auth hooks

--- packages/codemod/src/transformations/add-legacy-true-to-auth-hooks.ts
+++ packages/codemod/src/transformations/add-legacy-true-to-auth-hooks.ts
@@ -14,6 +14,7 @@ const authHookNames = [
     "usePermission",
     "useRegister",
     "useUpdatePassword",
+    "usePermissions",
 ];
 
 const addLegacyTrueToAuthHooks = (j: JSCodeshift, root: Collection<any>) => {
