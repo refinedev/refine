8c749132b1715817faf5ae35ae914dd28d0a6309
alican <alicanerdurmaz@gmail.com>
2023-02-24 14:19:43 +0300

    chore(v4-core): add new test to useActiveAuthProvider

--- packages/core/src/definitions/helpers/useActiveAuthProvider/index.spec.ts
+++ packages/core/src/definitions/helpers/useActiveAuthProvider/index.spec.ts
@@ -36,6 +36,27 @@ describe("useActiveAuthProvider", () => {
         expect(result.current?.isLegacy).toBeTruthy();
     });
 
+    it("returns new authProvider when both provided", async () => {
+        const { result } = renderHook(() => useActiveAuthProvider(), {
+            wrapper: TestWrapper({
+                legacyAuthProvider: {
+                    login: () => Promise.resolve(),
+                    checkAuth: () => Promise.resolve(),
+                    logout: () => Promise.resolve(),
+                    checkError: () => Promise.resolve(),
+                },
+                authProvider: {
+                    login: () => Promise.resolve({ success: true }),
+                    check: () => Promise.resolve({ authenticated: true }),
+                    onError: () => Promise.resolve({}),
+                    logout: () => Promise.resolve({ success: true }),
+                },
+            }),
+        });
+
+        expect(result.current?.isLegacy).toBeFalsy();
+    });
+
     it("returns null", async () => {
         const { result } = renderHook(() => useActiveAuthProvider(), {
             wrapper: TestWrapper({}),
