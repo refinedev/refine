f314b37ae0b1da293b6dbc0026cedb1b04520245
Ali Emir Sen <senaliemir@gmail.com>
2023-02-27 00:01:32 +0300

    feat(remix): add `navigate-to-resource` component

--- packages/remix/src/index.ts
+++ packages/remix/src/index.ts
@@ -1,3 +1,4 @@
 export { routerBindings as default, stringifyConfig } from "./bindings";
 export { RefineRoutes } from "./refine-routes";
 export { UnsavedChangesNotifier } from "./unsaved-changes-notifier";
+export { NavigateToResource } from "./navigate-to-resource";
