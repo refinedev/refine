782fd007c69165b52e157ba588865bbd4384078e
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-01 11:59:24 +0300

    Revert "chore(examples): codemod"
    This reverts commit db1249d8a829f766ff971125453d81fef01e4fa5.

--- examples/with-persist-query/src/App.tsx
+++ examples/with-persist-query/src/App.tsx
@@ -1,5 +1,4 @@
-import { Refine } from "@pankod/refine-core";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, Refine } from "@pankod/refine-core";
 import routerProvider from "@pankod/refine-react-router-v6/legacy";
 import dataProvider from "@pankod/refine-simple-rest";
 import "./App.css";
