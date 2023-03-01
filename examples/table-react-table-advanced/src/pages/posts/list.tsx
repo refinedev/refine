813e0f762e04c917758d432a163f606837d86ee0
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-01 12:46:40 +0300

    fix(table-react-table-advanced): build error

--- examples/table-react-table-advanced/src/pages/posts/list.tsx
+++ examples/table-react-table-advanced/src/pages/posts/list.tsx
@@ -7,12 +7,8 @@ import {
 } from "@pankod/refine-core";
 import { useForm } from "@pankod/refine-react-hook-form";
 import { Controller } from "react-hook-form";
-import {
-    useTable,
-    ColumnDef,
-    flexRender,
-    Row,
-} from "@pankod/refine-react-table";
+import { useTable } from "@pankod/refine-react-table";
+import { ColumnDef, flexRender, Row } from "@tanstack/react-table";
 
 import MDEditor from "@uiw/react-md-editor";
 
