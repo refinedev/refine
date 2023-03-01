9073205b064cb604b8af825ca3e16262658ec47c
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-01 23:10:14 +0300

    feat(v4): remove `@tanstack/react-query` exports items for `@pankod/refine-core` (#3674)
    * feat(codemod): separate imports
    
    * feat(core): remove exports
    
    * chore(examples): codemod
    
    * chore: changeset
    
    * chore(with-persist-query): add react-query dependency
    
    * feat(codemod): need add to package
    
    * Revert "chore(examples): codemod"
    
    This reverts commit db1249d8a829f766ff971125453d81fef01e4fa5.
    
    * chore(with-persist-query): remove dependency
    
    * chore(examples): codemod
    
    * build(blog-ecommerce): update next config
    
    * fix(nextjs-examples): next config
    
    * chore: update changeset
    
    * chore(examples): fix
    
    ---------
    
    Co-authored-by: Batuhan Wilhelm <batuhanwilhelm@gmail.com>
    Co-authored-by: Salih Özdemir <salihozdemir94@gmail.com>
    Co-authored-by: Ali Emir Sen <senaliemir@gmail.com>

---
+++ packages/codemod/src/transformations/separate-imports-react-query.ts
@@ -0,0 +1,65 @@
+import { API, FileInfo } from "jscodeshift";
+import fs from "fs";
+import path from "path";
+import { install, addPackage, isPackageJsonUpdated } from "../helpers";
+import checkPackageLock from "../helpers/checkPackageLock";
+import separateImports from "../helpers/separateImports";
+import { exported } from "../definitions/separated-imports/react-query";
+
+export const parser = "tsx";
+
+export async function postTransform(files: any, flags: any) {
+    const rootDir = path.join(process.cwd(), files[0]);
+    const packageJsonPath = path.join(rootDir, "package.json");
+    const useYarn = checkPackageLock(rootDir) === "yarn.lock";
+
+    // Check root package.json exists
+    try {
+        JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+    } catch (err) {
+        console.error(
+            `Error: failed to load package.json from ${packageJsonPath}, ensure provided directory is root.`,
+        );
+    }
+
+    if (!flags.dry) {
+        if (isPackageJsonUpdated(rootDir)) {
+            await install(rootDir, null, {
+                useYarn,
+                isOnline: true,
+            });
+        }
+    }
+}
+
+const REFINE_LIB_PATH = "@pankod/refine-core";
+const REACT_QUERY_PATH = "@tanstack/react-query";
+const REACT_QUERY_VERSION = "^4.10.1";
+
+export default function transformer(file: FileInfo, api: API): string {
+    const j = api.jscodeshift;
+    const source = j(file.source);
+
+    separateImports({
+        j,
+        source,
+        imports: exported,
+        renameImports: {},
+        otherImports: {},
+        currentLibName: REFINE_LIB_PATH,
+        nextLibName: REACT_QUERY_PATH,
+    });
+
+    // if use `@tanstack/react-query` add package.json
+    const reactQuery = source.find(j.ImportDeclaration, {
+        source: {
+            value: REACT_QUERY_PATH,
+        },
+    });
+
+    if (reactQuery.length) {
+        addPackage(process.cwd(), { [REACT_QUERY_PATH]: REACT_QUERY_VERSION });
+    }
+
+    return source.toSource();
+}
