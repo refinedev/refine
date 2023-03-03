1d00421ec35a0c03495e0cc90ddfc019a9c033c0
Ali Emir Şen <senaliemir@gmail.com>
2023-03-03 11:04:02 +0300

    build: chunked examples CI (#3700)
    * build(examples): run examples CI in chunks
    
    * build: fix script name
    
    * build: fix output
    
    * build: fix command
    
    * build: separate steps

---
+++ .github/workflows/build-example-chunks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+const EXAMPLES_DIR = "./examples";
+
+const CHUNK_COUNT = Number(process.env.CHUNKS ? process.env.CHUNKS : 0);
+
+const examples = fs.readdirSync(EXAMPLES_DIR).filter((dir) => {
+    return fs.statSync(EXAMPLES_DIR + "/" + dir).isDirectory();
+});
+
+const chunkSize = Math.ceil(examples.length / CHUNK_COUNT);
+
+const chunks = [];
+
+for (let i = 0; i < examples.length; i += chunkSize) {
+    chunks.push(examples.slice(i, i + chunkSize));
+}
+
+chunks.forEach((chunk, i) => {
+    console.log("::set-output name=CHUNK_" + (i + 1) + "::" + chunk.join(","));
+});
