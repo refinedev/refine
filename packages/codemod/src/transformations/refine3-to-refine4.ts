8d3624b44766e2dd4f5977de5962930532c8ea96
Ali Emir Sen <senaliemir@gmail.com>
2023-03-07 01:55:17 +0300

    feat(codemod): update post transform in v3 to v4 codemod

--- packages/codemod/src/transformations/refine3-to-refine4.ts
+++ packages/codemod/src/transformations/refine3-to-refine4.ts
@@ -1,59 +1,55 @@
 import { API, FileInfo } from "jscodeshift";
+import path from "path";
 import { addV3LegacyAuthProviderCompatibleTrueToAuthHooks } from "./v4/add-v3LegacyAuthProviderCompatible-true-to-auth-hooks";
 import { authProviderToLegacyAuthProvider } from "./v4/authProvider-to-legacyAuthProvider";
 import { metaDataToMeta } from "./v4/metadata-to-meta";
 import { moveDeprecatedAccessControlProps } from "./v4/move-deprecated-access-control";
 import { routerToLegacyRouter } from "./v4/router-to-legacy-router";
 import { renameResourcePropInButtons } from "./v4/rename-buttons-resource-prop";
-import {
-    separateImportsAntD,
-    separateImportsAntDPostTransform,
-} from "./v4/separate-imports-antd";
-import {
-    separateImportsChakra,
-    separateImportsChakraPostTransform,
-} from "./v4/separate-imports-chakra";
-import {
-    separateImportsMantine,
-    separateImportsMantinePostTransform,
-} from "./v4/separate-imports-mantine";
-import {
-    separateImportsMUI,
-    separateImportsMUIPostTransform,
-} from "./v4/separate-imports-mui";
-import {
-    separateImportsReactHookForm,
-    separateImportsReactHookFormPostTransform,
-} from "./v4/separate-imports-react-hook-form";
-import {
-    separateImportsReactQuery,
-    separateImportsReactQueryPostTransform,
-} from "./v4/separate-imports-react-query";
-import {
-    separateImportsReactTable,
-    separateImportsReactTablePostTransform,
-} from "./v4/separate-imports-react-table";
+import { separateImportsAntD } from "./v4/separate-imports-antd";
+import { separateImportsChakra } from "./v4/separate-imports-chakra";
+import { separateImportsMantine } from "./v4/separate-imports-mantine";
+import { separateImportsMUI } from "./v4/separate-imports-mui";
+import { separateImportsReactHookForm } from "./v4/separate-imports-react-hook-form";
+import { separateImportsReactQuery } from "./v4/separate-imports-react-query";
+import { separateImportsReactTable } from "./v4/separate-imports-react-table";
 import { useMenuToCore } from "./v4/use-menu-to-core";
-import {
-    separateImportsReactRouterV6,
-    separateImportsReactRouterV6PostTransform,
-} from "./v4/separate-imports-react-router-v6";
+import { separateImportsReactRouterV6 } from "./v4/separate-imports-react-router-v6";
 import { fixV4Deprecations } from "./v4/fix-v4-deprecations";
-import {
-    replacePankodImportsWithRefineDev,
-    replacePankodImportsWithRefineDevPostTransform,
-} from "./v4/replace-pankod-imports-with-refinedev";
+import { replacePankodImportsWithRefineDev } from "./v4/replace-pankod-imports-with-refinedev";
+import { CONFIG_FILE_NAME, CodemodConfig, install, remove } from "../helpers";
+import checkPackageLock from "../helpers/checkPackageLock";
 
 export async function postTransform(files: any, flags: any) {
-    await separateImportsAntDPostTransform(files, flags);
-    await separateImportsChakraPostTransform(files, flags);
-    await separateImportsMantinePostTransform(files, flags);
-    await separateImportsMUIPostTransform(files, flags);
-    await separateImportsReactHookFormPostTransform(files, flags);
-    await separateImportsReactQueryPostTransform(files, flags);
-    await separateImportsReactTablePostTransform(files, flags);
-    await separateImportsReactRouterV6PostTransform(files, flags);
-    await replacePankodImportsWithRefineDevPostTransform(files, flags);
+    if (flags.dry) {
+        return;
+    }
+
+    const config = new CodemodConfig(CONFIG_FILE_NAME);
+    const rootDir = path.join(process.cwd(), files[0]);
+
+    const useYarn = checkPackageLock(rootDir) === "yarn.lock";
+
+    if (Object.keys(config.getInstalls()).length > 0) {
+        await install(
+            rootDir,
+            Object.entries(config.getInstalls()).map(
+                ([key, value]) => `${key}@${value}`,
+            ),
+            {
+                useYarn,
+                isOnline: true,
+            },
+        );
+    }
+
+    if (config.getUninstalls().length > 0) {
+        await remove(rootDir, config.getUninstalls(), {
+            useYarn,
+        });
+    }
+
+    config.destroy();
 }
 
 export default function transformer(file: FileInfo, api: API): string {
