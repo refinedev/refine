42d38a4d439ac6b376b565a3e501454a9f8c84d9
Ali Emir Sen <senaliemir@gmail.com>
2023-03-07 10:48:04 +0300

    chore(react-router-v6): remove `NavigateAndKeepCurrent`

--- packages/react-router-v6/src/index.ts
+++ packages/react-router-v6/src/index.ts
@@ -2,4 +2,3 @@ export { routerBindings as default, stringifyConfig } from "./bindings";
 export { RefineRoutes } from "./refine-routes";
 export { NavigateToResource } from "./navigate-to-resource";
 export { UnsavedChangesNotifier } from "./unsaved-changes-notifier";
-export { NavigateAndKeepCurrent } from "./navigate-and-keep-current";
