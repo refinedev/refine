25687cfe38903abc639d3d38f60b7a6b8a71dd91
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-03-07 13:29:42 +0300

    feat(examples): migrate audit log examples to new router

--- examples/antd-audit-log/src/App.tsx
+++ examples/antd-audit-log/src/App.tsx
@@ -1,4 +1,4 @@
-import { AuthBindings, Refine } from "@refinedev/core";
+import { AuthBindings, Authenticated, Refine } from "@refinedev/core";
 import {
     notificationProvider,
     Layout,
@@ -6,7 +6,14 @@ import {
     AuthPage,
 } from "@refinedev/antd";
 import dataProvider from "@refinedev/simple-rest";
-import routerProvider from "@refinedev/react-router-v6/legacy";
+import routerProvider, { NavigateToResource } from "@refinedev/react-router-v6";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Outlet,
+    Navigate,
+} from "react-router-dom";
 import "@refinedev/antd/dist/reset.css";
 
 import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
@@ -98,58 +105,110 @@ const authProvider: AuthBindings = {
 
 const App: React.FC = () => {
     return (
-        <Refine
-            legacyRouterProvider={routerProvider}
-            dataProvider={dataProvider(API_URL)}
-            authProvider={authProvider}
-            resources={[
-                {
-                    name: "posts",
-                    list: PostList,
-                    create: PostCreate,
-                    edit: PostEdit,
-                    show: PostShow,
-                    canDelete: true,
-                    options: {
-                        auditLog: {
-                            permissions: ["create", "delete", "update"],
+        <BrowserRouter>
+            <Refine
+                routerProvider={routerProvider}
+                dataProvider={dataProvider(API_URL)}
+                authProvider={authProvider}
+                resources={[
+                    {
+                        name: "posts",
+                        list: "/posts",
+                        show: "/posts/show/:id",
+                        create: "/posts/create",
+                        edit: "/posts/edit/:id",
+                        meta: {
+                            canDelete: true,
+                            audit: ["create", "delete", "update"],
                         },
                     },
-                },
-                {
-                    name: "categories",
-                    list: CategoryList,
-                    create: CategoryCreate,
-                    edit: CategoryEdit,
-                    show: CategoryShow,
-                    canDelete: true,
-                },
-            ]}
-            notificationProvider={notificationProvider}
-            Layout={Layout}
-            catchAll={<ErrorComponent />}
-            Header={() => null}
-            LoginPage={AuthPage}
-            DashboardPage={DashboardPage}
-            auditLogProvider={{
-                create: async ({ ...params }) => {
-                    await refineSDK.log.create(params);
-                },
-                get: async ({ resource, action, meta, author }) => {
-                    return await refineSDK.log.get({
-                        resource,
-                        action,
-                        meta,
-                        author,
-                    });
-                },
-                update: async ({ id, name }) => {
-                    return await refineSDK.log.update(id, {
-                        name,
-                    });
-                },
-            }}
-        />
+                    {
+                        name: "categories",
+                        list: "/categories",
+                        show: "/categories/show/:id",
+                        create: "/categories/create",
+                        edit: "/categories/edit/:id",
+                        meta: {
+                            canDelete: true,
+                        },
+                    },
+                ]}
+                notificationProvider={notificationProvider}
+                auditLogProvider={{
+                    create: async ({ ...params }) => {
+                        await refineSDK.log.create(params);
+                    },
+                    get: async ({ resource, action, meta, author }) => {
+                        return await refineSDK.log.get({
+                            resource,
+                            action,
+                            meta,
+                            author,
+                        });
+                    },
+                    update: async ({ id, name }) => {
+                        return await refineSDK.log.update(id, {
+                            name,
+                        });
+                    },
+                }}
+            >
+                <Routes>
+                    <Route
+                        element={
+                            <Authenticated fallback={<Navigate to="/login" />}>
+                                <Layout>
+                                    <Outlet />
+                                </Layout>
+                            </Authenticated>
+                        }
+                    >
+                        <Route index element={<DashboardPage />} />
+
+                        <Route path="/posts" element={<PostList />} />
+                        <Route path="/posts/show/:id" element={<PostShow />} />
+                        <Route path="/posts/create" element={<PostCreate />} />
+                        <Route path="/posts/edit/:id" element={<PostEdit />} />
+
+                        <Route path="/categories" element={<CategoryList />} />
+                        <Route
+                            path="/categories/show/:id"
+                            element={<CategoryShow />}
+                        />
+                        <Route
+                            path="/categories/create"
+                            element={<CategoryCreate />}
+                        />
+                        <Route
+                            path="/categories/edit/:id"
+                            element={<CategoryEdit />}
+                        />
+                    </Route>
+
+                    <Route
+                        element={
+                            <Authenticated fallback={<Outlet />}>
+                                <NavigateToResource resource="posts" />
+                            </Authenticated>
+                        }
+                    >
+                        <Route path="/login" element={<AuthPage />} />
+                    </Route>
+
+                    <Route
+                        element={
+                            <Authenticated fallback={<Outlet />}>
+                                <Layout>
+                                    <Outlet />
+                                </Layout>
+                            </Authenticated>
+                        }
+                    >
+                        <Route path="*" element={<ErrorComponent />} />
+                    </Route>
+                </Routes>
+            </Refine>
+        </BrowserRouter>
     );
 };
 
--- examples/audit-log-provider/src/App.tsx
+++ examples/audit-log-provider/src/App.tsx
@@ -1,6 +1,7 @@
-import { Refine } from "@refinedev/core";
+import { ErrorComponent, Refine } from "@refinedev/core";
 import dataProvider from "@refinedev/simple-rest";
-import routerProvider from "@refinedev/react-router-v6/legacy";
+import routerProvider, { NavigateToResource } from "@refinedev/react-router-v6";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import { PostList, PostCreate, PostEdit } from "./pages/posts";
 
@@ -8,54 +9,73 @@ const API_URL = "https://api.fake-rest.refine.dev";
 
 const App: React.FC = () => {
     return (
-        <Refine
-            auditLogProvider={{
-                get: async ({ resource, meta }) => {
-                    const { data } = await dataProvider(API_URL).getList({
-                        resource: "logs",
-                        filters: [
-                            {
-                                field: "resource",
-                                operator: "eq",
-                                value: resource,
-                            },
-                            {
-                                field: "meta.id",
-                                operator: "eq",
-                                value: meta?.id,
-                            },
-                        ],
-                    });
+        <BrowserRouter>
+            <Refine
+                auditLogProvider={{
+                    get: async ({ resource, meta }) => {
+                        const { data } = await dataProvider(API_URL).getList({
+                            resource: "logs",
+                            filters: [
+                                {
+                                    field: "resource",
+                                    operator: "eq",
+                                    value: resource,
+                                },
+                                {
+                                    field: "meta.id",
+                                    operator: "eq",
+                                    value: meta?.id,
+                                },
+                            ],
+                        });
 
-                    return data;
-                },
-                create: (params) => {
-                    return dataProvider(API_URL).create({
-                        resource: "logs",
-                        variables: params,
-                    });
-                },
-                update: async ({ id, name }) => {
-                    const { data } = await dataProvider(API_URL).update({
-                        resource: "logs",
-                        id,
-                        variables: { name },
-                    });
-                    return data;
-                },
-            }}
-            legacyRouterProvider={routerProvider}
-            dataProvider={dataProvider(API_URL)}
-            resources={[
-                {
-                    name: "posts",
-                    list: PostList,
-                    create: PostCreate,
-                    edit: PostEdit,
-                    canDelete: true,
-                },
-            ]}
-        />
+                        return data;
+                    },
+                    create: (params) => {
+                        return dataProvider(API_URL).create({
+                            resource: "logs",
+                            variables: params,
+                        });
+                    },
+                    update: async ({ id, name }) => {
+                        const { data } = await dataProvider(API_URL).update({
+                            resource: "logs",
+                            id,
+                            variables: { name },
+                        });
+                        return data;
+                    },
+                }}
+                routerProvider={routerProvider}
+                dataProvider={dataProvider(API_URL)}
+                resources={[
+                    {
+                        name: "posts",
+                        list: "/posts",
+                        create: "/posts/create",
+                        edit: "/posts/edit/:id",
+                        meta: {
+                            canDelete: true,
+                        },
+                    },
+                ]}
+            >
+                <Routes>
+                    <Route element={<Outlet />}>
+                        <Route
+                            index
+                            element={<NavigateToResource resource="posts" />}
+                        />
+
+                        <Route path="/posts" element={<PostList />} />
+                        <Route path="/posts/create" element={<PostCreate />} />
+                        <Route path="/posts/edit/:id" element={<PostEdit />} />
+
+                        <Route path="*" element={<ErrorComponent />} />
+                    </Route>
+                </Routes>
+            </Refine>
+        </BrowserRouter>
     );
 };
 
