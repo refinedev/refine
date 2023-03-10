1f5bc96674f05928187abfb8af3009b321d37c2f
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-03-10 22:49:20 +0300

    fix(examples): add label to dashboard resource (#3790)

--- examples/antd-audit-log/src/App.tsx
+++ examples/antd-audit-log/src/App.tsx
@@ -119,6 +119,7 @@ const App: React.FC = () => {
                         name: "dashboard",
                         list: "/",
                         meta: {
+                            label: "Dashboard",
                             icon: <DashboardOutlined />,
                         },
                     },
