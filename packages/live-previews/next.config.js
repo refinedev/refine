67a7b21d3beb1e785ae2aa3773561a1053a2c4cc
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-03-14 13:16:31 +0300

    chore(live-previews): revert change on package.json

--- packages/live-previews/next.config.js
+++ packages/live-previews/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     reactStrictMode: true,
     // productionBrowserSourceMaps: true, // Disabled for now, as it causes build to hang on deploy
     swcMinify: false,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
