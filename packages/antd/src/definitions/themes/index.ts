13cbb6672d343b1af7c32b7f91b96f6d51a94d8d
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-03-20 14:38:07 +0300

    chore(antd): update themes naming

--- packages/antd/src/definitions/themes/index.ts
+++ packages/antd/src/definitions/themes/index.ts
@@ -1,42 +1,42 @@
 import { ThemeConfig } from "antd";
 
-export const BLUE_THEME: ThemeConfig = {
+export const BlueTheme: ThemeConfig = {
     token: {
         colorPrimary: "#1677FF",
     },
 };
 
-export const PURPLE_THEME: ThemeConfig = {
+export const PurpleTheme: ThemeConfig = {
     token: {
         colorPrimary: "#722ED1",
     },
 };
 
-export const MAGENTA_THEME: ThemeConfig = {
+export const MagentaTheme: ThemeConfig = {
     token: {
         colorPrimary: "#EB2F96",
     },
 };
 
-export const RED_THEME: ThemeConfig = {
+export const RedTheme: ThemeConfig = {
     token: {
         colorPrimary: "#F5222D",
     },
 };
 
-export const ORANGE_THEME: ThemeConfig = {
+export const OrangeTheme: ThemeConfig = {
     token: {
         colorPrimary: "#FA541C",
     },
 };
 
-export const YELLOW_THEME: ThemeConfig = {
+export const YellowTheme: ThemeConfig = {
     token: {
         colorPrimary: "#FAAD14",
     },
 };
 
-export const GREEN_THEME: ThemeConfig = {
+export const GreenTheme: ThemeConfig = {
     token: {
         colorPrimary: "#52C41A",
     },
