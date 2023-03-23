c0e070c55354350812f79b979ccc8a719cfb9e57
metamodern <shabanovbunya@gmail.com>
2023-03-23 18:04:25 +0200

    prettify

--- packages/core/src/hooks/data/useCustomMutation.ts
+++ packages/core/src/hooks/data/useCustomMutation.ts
@@ -1,70 +1,70 @@
 import {
-  useMutation,
-  UseMutationOptions,
-  UseMutationResult,
+    useMutation,
+    UseMutationOptions,
+    UseMutationResult,
 } from "@tanstack/react-query";
 
 import {
-  useDataProvider,
-  useHandleNotification,
-  useTranslate,
-  useOnError,
+    useDataProvider,
+    useHandleNotification,
+    useTranslate,
+    useOnError,
 } from "@hooks";
 import {
-  CreateResponse,
-  BaseRecord,
-  HttpError,
-  SuccessErrorNotification,
-  MetaQuery,
+    CreateResponse,
+    BaseRecord,
+    HttpError,
+    SuccessErrorNotification,
+    MetaQuery,
 } from "../../interfaces";
 import { pickNotDeprecated, useActiveAuthProvider } from "@definitions/helpers";
 
 interface UseCustomMutationConfig {
-  headers?: {};
+    headers?: {};
 }
 
 type useCustomMutationParams<TData, TError, TVariables> = {
-  url: string;
-  method: "post" | "put" | "patch" | "delete";
-  values: TVariables;
-  /**
-   * Meta data for `dataProvider`
-   */
-  meta?: MetaQuery;
-  /**
-   * Meta data for `dataProvider`
-   * @deprecated `metaData` is deprecated with refine@4, refine will pass `meta` instead, however, we still support `metaData` for backward compatibility.
-   */
-  metaData?: MetaQuery;
-  dataProviderName?: string;
-  config?: UseCustomMutationConfig;
+    url: string;
+    method: "post" | "put" | "patch" | "delete";
+    values: TVariables;
+    /**
+     * Meta data for `dataProvider`
+     */
+    meta?: MetaQuery;
+    /**
+     * Meta data for `dataProvider`
+     * @deprecated `metaData` is deprecated with refine@4, refine will pass `meta` instead, however, we still support `metaData` for backward compatibility.
+     */
+    metaData?: MetaQuery;
+    dataProviderName?: string;
+    config?: UseCustomMutationConfig;
 } & SuccessErrorNotification<TData, TError, TVariables>;
 
 export type UseCustomMutationReturnType<
-  TData extends BaseRecord = BaseRecord,
-  TError extends HttpError = HttpError,
-  TVariables = {}
+    TData extends BaseRecord = BaseRecord,
+    TError extends HttpError = HttpError,
+    TVariables = {},
 > = UseMutationResult<
-  CreateResponse<TData>,
-  TError,
-  useCustomMutationParams<TData, TError, TVariables>,
-  unknown
+    CreateResponse<TData>,
+    TError,
+    useCustomMutationParams<TData, TError, TVariables>,
+    unknown
 >;
 
 export type UseCustomMutationProps<
-  TData extends BaseRecord = BaseRecord,
-  TError extends HttpError = HttpError,
-  TVariables = {}
+    TData extends BaseRecord = BaseRecord,
+    TError extends HttpError = HttpError,
+    TVariables = {},
 > = {
-  mutationOptions?: Omit<
-    UseMutationOptions<
-      CreateResponse<TData>,
-      TError,
-      useCustomMutationParams<TData, TError, TVariables>,
-      unknown
-    >,
-    "mutationFn" | "onError" | "onSuccess"
-  >;
+    mutationOptions?: Omit<
+        UseMutationOptions<
+            CreateResponse<TData>,
+            TError,
+            useCustomMutationParams<TData, TError, TVariables>,
+            unknown
+        >,
+        "mutationFn" | "onError" | "onSuccess"
+    >;
 };
 
 /**
@@ -81,108 +81,108 @@ export type UseCustomMutationProps<
  */
 
 export const useCustomMutation = <
-  TData extends BaseRecord = BaseRecord,
-  TError extends HttpError = HttpError,
-  TVariables = {}
+    TData extends BaseRecord = BaseRecord,
+    TError extends HttpError = HttpError,
+    TVariables = {},
 >({
-  mutationOptions,
+    mutationOptions,
 }: UseCustomMutationProps<
-  TData,
-  TError,
-  TVariables
-> = {}): UseCustomMutationReturnType<TData, TError, TVariables> => {
-  const authProvider = useActiveAuthProvider();
-  const { mutate: checkError } = useOnError({
-    v3LegacyAuthProviderCompatible: Boolean(authProvider?.isLegacy),
-  });
-  const handleNotification = useHandleNotification();
-  const dataProvider = useDataProvider();
-  const translate = useTranslate();
-
-  const mutation = useMutation<
-    CreateResponse<TData>,
+    TData,
     TError,
-    useCustomMutationParams<TData, TError, TVariables>,
-    unknown
-  >(
-    ({
-      url,
-      method,
-      values,
-      meta,
-      metaData,
-      dataProviderName,
-      config,
-    }: useCustomMutationParams<TData, TError, TVariables>) => {
-      const { custom } = dataProvider(dataProviderName);
+    TVariables
+> = {}): UseCustomMutationReturnType<TData, TError, TVariables> => {
+    const authProvider = useActiveAuthProvider();
+    const { mutate: checkError } = useOnError({
+        v3LegacyAuthProviderCompatible: Boolean(authProvider?.isLegacy),
+    });
+    const handleNotification = useHandleNotification();
+    const dataProvider = useDataProvider();
+    const translate = useTranslate();
 
-      if (custom) {
-        return custom<TData>({
-          url,
-          method,
-          payload: values,
-          meta: pickNotDeprecated(meta, metaData),
-          metaData: pickNotDeprecated(meta, metaData),
-          headers: { ...config?.headers },
-        });
-      }
+    const mutation = useMutation<
+        CreateResponse<TData>,
+        TError,
+        useCustomMutationParams<TData, TError, TVariables>,
+        unknown
+    >(
+        ({
+            url,
+            method,
+            values,
+            meta,
+            metaData,
+            dataProviderName,
+            config,
+        }: useCustomMutationParams<TData, TError, TVariables>) => {
+            const { custom } = dataProvider(dataProviderName);
 
-      throw Error("Not implemented custom on data provider.");
-    },
-    {
-      onSuccess: (
-        data,
+            if (custom) {
+                return custom<TData>({
+                    url,
+                    method,
+                    payload: values,
+                    meta: pickNotDeprecated(meta, metaData),
+                    metaData: pickNotDeprecated(meta, metaData),
+                    headers: { ...config?.headers },
+                });
+            }
+
+            throw Error("Not implemented custom on data provider.");
+        },
         {
-          successNotification: successNotificationFromProp,
-          config,
-          meta,
-          metaData,
-        }
-      ) => {
-        const notificationConfig =
-          typeof successNotificationFromProp === "function"
-            ? successNotificationFromProp(data, {
-                ...config,
-                ...(pickNotDeprecated(meta, metaData) || {}),
-              })
-            : successNotificationFromProp;
+            onSuccess: (
+                data,
+                {
+                    successNotification: successNotificationFromProp,
+                    config,
+                    meta,
+                    metaData,
+                },
+            ) => {
+                const notificationConfig =
+                    typeof successNotificationFromProp === "function"
+                        ? successNotificationFromProp(data, {
+                              ...config,
+                              ...(pickNotDeprecated(meta, metaData) || {}),
+                          })
+                        : successNotificationFromProp;
 
-        handleNotification(notificationConfig);
-      },
-      onError: (
-        err: TError,
-        {
-          errorNotification: errorNotificationFromProp,
-          method,
-          config,
-          meta,
-          metaData,
-        }
-      ) => {
-        checkError(err);
+                handleNotification(notificationConfig);
+            },
+            onError: (
+                err: TError,
+                {
+                    errorNotification: errorNotificationFromProp,
+                    method,
+                    config,
+                    meta,
+                    metaData,
+                },
+            ) => {
+                checkError(err);
 
-        const notificationConfig =
-          typeof errorNotificationFromProp === "function"
-            ? errorNotificationFromProp(err, {
-                ...config,
-                ...(pickNotDeprecated(meta, metaData) || {}),
-              })
-            : errorNotificationFromProp;
+                const notificationConfig =
+                    typeof errorNotificationFromProp === "function"
+                        ? errorNotificationFromProp(err, {
+                              ...config,
+                              ...(pickNotDeprecated(meta, metaData) || {}),
+                          })
+                        : errorNotificationFromProp;
 
-        handleNotification(notificationConfig, {
-          key: `${method}-notification`,
-          message: translate(
-            "notifications.error",
-            { statusCode: err.statusCode },
-            `Error (status code: ${err.statusCode})`
-          ),
-          description: err.message,
-          type: "error",
-        });
-      },
-      ...mutationOptions,
-    }
-  );
+                handleNotification(notificationConfig, {
+                    key: `${method}-notification`,
+                    message: translate(
+                        "notifications.error",
+                        { statusCode: err.statusCode },
+                        `Error (status code: ${err.statusCode})`,
+                    ),
+                    description: err.message,
+                    type: "error",
+                });
+            },
+            ...mutationOptions,
+        },
+    );
 
-  return mutation;
+    return mutation;
 };
