e717bf433eebbf717ddd8d69702ec59790f4e887
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-03-24 17:13:17 +0300

    fix: --version is incorrect

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from "tsup";
 import { NodeResolvePlugin } from "@esbuild-plugins/node-resolve";
+import fs from "fs";
+import path from "path";
+
+const JS_EXTENSIONS = new Set(["js", "cjs", "mjs"]);
+
+const getRefineCLIVersion = async () => {
+    const packages = await fs.promises.readFile("./package.json", "utf8");
+    const { version } = JSON.parse(packages);
+    return version;
+};
 
 export default defineConfig({
     entry: ["src/index.ts", "src/cli.ts"],
@@ -9,6 +19,42 @@ export default defineConfig({
     clean: false,
     platform: "node",
     esbuildPlugins: [
+        {
+            name: "textReplace",
+            setup: (build) => {
+                build.onLoad({ filter: /\.ts$/ }, async (args) => {
+                    const contents = await fs.promises.readFile(
+                        args.path,
+                        "utf8",
+                    );
+
+                    const extension = path.extname(args.path).replace(".", "");
+                    const loader = JS_EXTENSIONS.has(extension)
+                        ? "jsx"
+                        : (extension as any);
+
+                    const versionRegex =
+                        /const REFINE_CLI_VERSION = "\d.\d.\d";/gm;
+                    const hasVersion = contents.match(versionRegex);
+
+                    if (!hasVersion) {
+                        return {
+                            loader,
+                            contents,
+                        };
+                    }
+
+                    const version = await getRefineCLIVersion();
+                    return {
+                        loader,
+                        contents: contents.replace(
+                            versionRegex,
+                            `const REFINE_CLI_VERSION = "${version}";`,
+                        ),
+                    };
+                });
+            },
+        },
         NodeResolvePlugin({
             extensions: [".js", "ts", "tsx", "jsx"],
             onResolved: (resolved) => {
