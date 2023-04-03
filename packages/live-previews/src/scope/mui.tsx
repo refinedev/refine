e027bfe01d24cb1f549f34846f1d0a243f2c021a
Ali Emir Şen <senaliemir@gmail.com>
2023-04-03 18:17:46 +0300

    feat(live-previews): mock `RefineThemes` and `ThemedTitle` for dynamic values through query params (#4041)
    * feat(live-previews): mock `RefineThemes` and `ThemedTitle` to dynamically set values
    
    * feat(live-previews): mock `auth0` package
    
    * feat(live-previews): mock `keycloak` package
    
    * fix(refine-themes): fix proxy
    
    * fix(live-previews): add missing icon in mui
    
    * feat(live-previews): dynamic logic with message listeners
    
    * feat(live-previews): add icons for same origin access
    
    * chore(live-previews): add missing flag images
    
    * feat(live-previews): persist dynamic state on mode changes
    
    * chore(live-previews): add missing icons
    
    * fix(live-previews): replace render root if possible

--- packages/live-previews/src/scope/mui.tsx
+++ packages/live-previews/src/scope/mui.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import parseHtml from "html-react-parser";
 import type { RefineProps } from "@refinedev/core";
 import { RefineCommonScope } from "./common";
 import * as RefineMui from "@refinedev/mui";
@@ -11,7 +12,6 @@ import * as MuiMaterial from "@mui/material";
 import * as MuiXDataGrid from "@mui/x-data-grid";
 import * as ReactHookForm from "react-hook-form";
 
-import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline, GlobalStyles } from "@mui/material";
 
 import {
@@ -26,10 +26,12 @@ import {
     ChevronLeft,
     ChevronRight,
     MenuRounded,
+    Menu,
     Dashboard,
     Check,
     Close,
 } from "@mui/icons-material";
+import axios from "axios";
 
 const SIMPLE_REST_API_URL = "https://api.fake-rest.refine.dev";
 
@@ -71,16 +73,154 @@ const RefineMuiDemo: React.FC<
     );
 };
 
+const ThemedTitle: typeof RefineMui.ThemedTitle = ({
+    collapsed,
+    wrapperStyles,
+    text: textFromProps,
+    icon: iconFromProps,
+}) => {
+    const [svgContent, setSvgContent] = React.useState<string | undefined>(
+        window.__refineIconSVGContent || undefined,
+    );
+    const [title, setTitle] = React.useState<string | undefined>(
+        window.__refineTitleContent || undefined,
+    );
+
+    React.useEffect(() => {
+        if (typeof window !== "undefined") {
+            const messageListener = (event: MessageEvent) => {
+                if (event.data.type === "UPDATE_DYNAMIC_VALUES") {
+                    if (event.data.payload?.title) {
+                        setTitle(event.data.payload?.title);
+
+                        if (typeof window !== "undefined") {
+                            window.__refineTitleContent =
+                                event.data.payload?.title;
+                        }
+                    }
+
+                    if (event.data.payload?.icon) {
+                        try {
+                            axios
+                                .get(`/assets/icons/${event.data.payload.icon}`)
+                                .then((res) => {
+                                    const content = res.data
+                                        .replace(
+                                            /fill\=\"white\"/g,
+                                            `fill="currentColor"`,
+                                        )
+                                        .replace(
+                                            /stroke\=\"white\"/g,
+                                            `stroke="currentColor"`,
+                                        );
+
+                                    setSvgContent(content);
+
+                                    if (typeof window !== "undefined") {
+                                        window.__refineIconSVGContent = content;
+                                    }
+                                });
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    }
+                }
+            };
+
+            window.addEventListener("message", messageListener);
+
+            return () => {
+                window.removeEventListener("message", messageListener);
+            };
+        }
+
+        return () => undefined;
+    }, []);
+
+    return (
+        <RefineMui.ThemedTitle
+            collapsed={collapsed}
+            wrapperStyles={wrapperStyles}
+            text={title || textFromProps}
+            icon={svgContent ? parseHtml(svgContent) : iconFromProps}
+        />
+    );
+};
+
+const ThemeProvider = ({
+    children,
+    theme,
+}: {
+    children?: React.ReactNode;
+    theme?: { palette?: { mode?: "light" | "dark" } };
+}) => {
+    const [themeFromWindow, setThemeFromWindow] = React.useState<
+        undefined | string
+    >(undefined);
+
+    React.useEffect(() => {
+        if (typeof window !== "undefined") {
+            const messageListener = (event: MessageEvent) => {
+                if (event.data.type === "UPDATE_DYNAMIC_VALUES") {
+                    if (event.data.payload?.theme) {
+                        setThemeFromWindow(event.data.payload.theme);
+                    }
+                }
+            };
+
+            window.addEventListener("message", messageListener);
+
+            return () => {
+                window.removeEventListener("message", messageListener);
+            };
+        }
+
+        return () => undefined;
+    }, []);
+
+    const isLightPalette = theme?.palette?.mode === "light";
+
+    const RefineThemeFromWindow = React.useMemo(() => {
+        if (themeFromWindow && themeFromWindow in RefineMui.RefineThemes) {
+            if (isLightPalette) {
+                return RefineMui.RefineThemes[
+                    themeFromWindow as keyof typeof RefineMui.RefineThemes
+                ];
+            } else {
+                return RefineMui.RefineThemes[
+                    `${themeFromWindow}Dark` as keyof typeof RefineMui.RefineThemes
+                ];
+            }
+        }
+
+        return undefined;
+    }, [themeFromWindow, isLightPalette]);
+
+    return (
+        <MuiMaterialStyles.ThemeProvider
+            theme={(RefineThemeFromWindow as any) ?? theme}
+        >
+            {children}
+        </MuiMaterialStyles.ThemeProvider>
+    );
+};
+
 const MuiScope = {
     // ...RefineCommonScope,
     RefineMuiDemo,
-    RefineMui,
+    RefineMui: {
+        ...RefineMui,
+        ThemedTitle,
+    },
     EmotionReact,
     EmotionStyled,
     MuiLab,
     MuiMaterial,
     MuiXDataGrid,
-    MuiMaterialStyles,
+    MuiMaterialStyles: {
+        ...MuiMaterialStyles,
+        ThemeProvider,
+    },
     ReactHookForm,
     MuiIconsMaterial: {
         Close,
@@ -96,15 +236,9 @@ const MuiScope = {
         ChevronLeft,
         ChevronRight,
         MenuRounded,
+        Menu,
         Dashboard,
     },
-    // RefineMantine,
-    // RefineMantineDemo,
-    // RefineChakra,
-    // RefineChakraDemo,
-    // // Other Packages
-    // RefineReactHookForm,
-    // RefineReactTable,
 };
 
 export default MuiScope;
