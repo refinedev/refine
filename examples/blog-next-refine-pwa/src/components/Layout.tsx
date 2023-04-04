03e55888dafd607eb23279ebc3ac334c1d572b99
Salih <salihozdemir94@gmail.com>
2023-04-04 10:47:46 +0300

    chore: fix lint

--- examples/blog-next-refine-pwa/src/components/Layout.tsx
+++ examples/blog-next-refine-pwa/src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { LayoutProps } from "@refinedev/core";
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className="bg-red">
-            <div className="relative sticky top-0 z-50 flex items-center justify-between bg-[#fff] px-24 py-8">
+            <div className="sticky top-0 z-50 flex items-center justify-between bg-[#fff] px-24 py-8">
                 <a href="https://refine.dev">
                     <img src="./refine_logo.png" alt="refine logo" />
                 </a>
