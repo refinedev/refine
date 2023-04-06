ae8a2dc180ccdcb913c42f01e451bc95c687bbcb
Doguhan Ozgur Akca <doguhanozgurakca@gmail.com>
2023-04-06 15:22:08 +0300

    feat: migrate to vite in base-antd

--- examples/base-antd/src/App.tsx
+++ examples/base-antd/src/App.tsx
@@ -14,7 +14,7 @@ import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import "@refinedev/antd/dist/reset.css";
 
-import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import { PostList, PostCreate, PostEdit, PostShow } from "../src/pages/posts";
 
 const API_URL = "https://api.fake-rest.refine.dev";
 
