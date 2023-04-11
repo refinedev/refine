6c8b6813a17b04b40076028da679177334793c92
Salih <salihozdemir94@gmail.com>
2023-04-11 11:26:02 +0300

    fix: update `useMeta` to use mutation hooks

--- packages/core/src/hooks/useMeta/index.ts
+++ packages/core/src/hooks/useMeta/index.ts
@@ -1,26 +1,30 @@
 import { useResource } from "@hooks/resource";
 import { useParsed } from "@hooks/router";
 import { sanitizeResource } from "@definitions/helpers/sanitize-resource";
+import { pickResource } from "@definitions/helpers/pick-resource";
 import { MetaQuery } from "../../interfaces";
-
-export type UseMetaProps = {
-    resource?: string;
-    meta?: MetaQuery;
-};
 
 /**
  * Hook that returns the meta object.
  * @internal
  */
-export const useMeta = ({
-    resource: resourceFromProp,
-    meta: metaFromProp,
-}: UseMetaProps = {}) => {
-    const { resource } = useResource(resourceFromProp);
-
-    const { meta } = sanitizeResource(resource ?? {});
-
+export const useMeta = () => {
+    const { resources } = useResource();
     const { params } = useParsed();
 
-    return { ...meta, ...params, ...metaFromProp };
+    const getMetaFn = ({
+        resource: resourceFromProp,
+        meta: metaFromProp,
+    }: {
+        resource?: string;
+        meta?: MetaQuery;
+    } = {}) => {
+        const resource = pickResource(resourceFromProp, resources);
+
+        const { meta } = sanitizeResource(resource ?? {});
+
+        return { ...meta, ...params, ...metaFromProp };
+    };
+
+    return getMetaFn;
 };
