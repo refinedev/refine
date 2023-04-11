4aaee717e4e47d84b493cef958a6b3605de7b1b3
Salih <salihozdemir94@gmail.com>
2023-04-11 11:26:40 +0300

    test: update the `useMeta` tests

--- packages/core/src/hooks/useMeta/index.spec.tsx
+++ packages/core/src/hooks/useMeta/index.spec.tsx
@@ -6,104 +6,92 @@ import { useMeta } from "./";
 
 describe("useMeta Hook", () => {
     it("should return the resource meta", () => {
-        const { result } = renderHook(
-            () =>
-                useMeta({
-                    resource: "posts",
-                }),
-            {
-                wrapper: TestWrapper({
-                    resources: [
-                        {
-                            name: "posts",
-                            meta: {
-                                foo: "bar",
-                                lorem: "ipsum",
-                            },
+        const { result } = renderHook(() => useMeta(), {
+            wrapper: TestWrapper({
+                resources: [
+                    {
+                        name: "posts",
+                        meta: {
+                            foo: "bar",
+                            lorem: "ipsum",
                         },
-                    ],
-                }),
-            },
-        );
+                    },
+                ],
+            }),
+        });
 
-        expect(result.current).toEqual({
+        const combinedMeta = result.current({ resource: "posts" });
+
+        expect(combinedMeta).toEqual({
             foo: "bar",
             lorem: "ipsum",
         });
     });
 
     it("should return the params from the router in the meta", () => {
-        const { result } = renderHook(
-            () =>
-                useMeta({
-                    resource: "posts",
+        const { result } = renderHook(() => useMeta(), {
+            wrapper: TestWrapper({
+                resources: [{ name: "posts" }],
+                routerProvider: mockRouterBindings({
+                    params: { id: "123", authorId: "456" },
                 }),
-            {
-                wrapper: TestWrapper({
-                    resources: [{ name: "posts" }],
-                    routerProvider: mockRouterBindings({
-                        params: { id: "123", authorId: "456" },
-                    }),
-                }),
-            },
-        );
+            }),
+        });
 
-        expect(result.current).toEqual({
+        const combinedMeta = result.current({ resource: "posts" });
+
+        expect(combinedMeta).toEqual({
             id: "123",
             authorId: "456",
         });
     });
 
     it("should return the meta from the props", () => {
-        const { result } = renderHook(
-            () =>
-                useMeta({
-                    resource: "posts",
-                    meta: {
-                        foo: "bar",
-                        lorem: "ipsum",
-                    },
-                }),
-            {
-                wrapper: TestWrapper({
-                    resources: [{ name: "posts" }],
-                }),
-            },
-        );
+        const { result } = renderHook(() => useMeta(), {
+            wrapper: TestWrapper({
+                resources: [{ name: "posts" }],
+            }),
+        });
 
-        expect(result.current).toEqual({
+        const combinedMeta = result.current({
+            resource: "posts",
+            meta: {
+                foo: "bar",
+                lorem: "ipsum",
+            },
+        });
+
+        expect(combinedMeta).toEqual({
             foo: "bar",
             lorem: "ipsum",
         });
     });
 
     it("should return the combined meta from the resource, router and props", () => {
-        const { result } = renderHook(
-            () =>
-                useMeta({
-                    resource: "posts",
-                    meta: {
-                        lorem: "ipsum",
-                    },
-                }),
-            {
-                wrapper: TestWrapper({
-                    resources: [
-                        {
-                            name: "posts",
-                            meta: {
-                                foo: "bar",
-                            },
+        const { result } = renderHook(() => useMeta(), {
+            wrapper: TestWrapper({
+                resources: [
+                    {
+                        name: "posts",
+                        meta: {
+                            foo: "bar",
                         },
-                    ],
-                    routerProvider: mockRouterBindings({
-                        params: { authorId: "123" },
-                    }),
+                    },
+                ],
+                routerProvider: mockRouterBindings({
+                    params: { authorId: "123" },
                 }),
-            },
-        );
+            }),
+        });
 
-        expect(result.current).toEqual({
+        const combinedMeta = result.current({
+            resource: "posts",
+            meta: {
+                lorem: "ipsum",
+            },
+        });
+
+        expect(combinedMeta).toEqual({
             foo: "bar",
             lorem: "ipsum",
             authorId: "123",
@@ -111,82 +99,73 @@ describe("useMeta Hook", () => {
     });
 
     it("should be prioritized in the order: props, router, resource", () => {
-        const { result } = renderHook(
-            () =>
-                useMeta({
-                    resource: "posts",
-                }),
-            {
-                wrapper: TestWrapper({
-                    resources: [
-                        {
-                            name: "posts",
-                            meta: {
-                                foo: "baz",
-                            },
+        const { result } = renderHook(() => useMeta(), {
+            wrapper: TestWrapper({
+                resources: [
+                    {
+                        name: "posts",
+                        meta: {
+                            foo: "baz",
                         },
-                    ],
-                    routerProvider: mockRouterBindings({
-                        params: { foo: "qux" },
-                    }),
+                    },
+                ],
+                routerProvider: mockRouterBindings({
+                    params: { foo: "qux" },
                 }),
-            },
-        );
+            }),
+        });
 
-        expect(result.current).toEqual({
+        const combinedMeta = result.current({
+            resource: "posts",
+        });
+
+        expect(combinedMeta).toEqual({
             foo: "qux",
         });
 
-        const { result: result2 } = renderHook(
-            () =>
-                useMeta({
-                    resource: "posts",
-                    meta: {
-                        foo: "bar",
-                    },
-                }),
-            {
-                wrapper: TestWrapper({
-                    resources: [
-                        {
-                            name: "posts",
-                            meta: {
-                                foo: "baz",
-                            },
+        const { result: result2 } = renderHook(() => useMeta(), {
+            wrapper: TestWrapper({
+                resources: [
+                    {
+                        name: "posts",
+                        meta: {
+                            foo: "baz",
                         },
-                    ],
-                    routerProvider: mockRouterBindings({
-                        params: { foo: "qux" },
-                    }),
+                    },
+                ],
+                routerProvider: mockRouterBindings({
+                    params: { foo: "qux" },
                 }),
+            }),
+        });
+        const combinedMeta2 = result2.current({
+            resource: "posts",
+            meta: {
+                foo: "bar",
             },
-        );
+        });
 
-        expect(result2.current).toEqual({
+        expect(combinedMeta2).toEqual({
             foo: "bar",
         });
     });
 
     it("should not return the icon prop of resource meta", () => {
-        const { result } = renderHook(
-            () =>
-                useMeta({
-                    resource: "posts",
-                }),
-            {
-                wrapper: TestWrapper({
-                    resources: [
-                        {
-                            name: "posts",
-                            meta: {
-                                icon: <span>🌙</span>,
-                            },
+        const { result } = renderHook(() => useMeta(), {
+            wrapper: TestWrapper({
+                resources: [
+                    {
+                        name: "posts",
+                        meta: {
+                            icon: <span>🌙</span>,
                         },
-                    ],
-                }),
-            },
-        );
+                    },
+                ],
+            }),
+        });
 
-        expect(result.current).toEqual({});
+        const combinedData = result.current({ resource: "posts" });
+
+        expect(combinedData).toEqual({});
     });
 });
