947b304c215b8eb69856caec632bd9ef6f9bb60b
Salih <salihozdemir94@gmail.com>
2023-04-12 15:57:39 +0300

    feat: create base example

---
+++ examples/with-meta-properties/src/pages/posts/show.tsx
@@ -0,0 +1,42 @@
+import { useShow, IResourceComponentsProps, useOne } from "@refinedev/core";
+
+import { Show, MarkdownField } from "@refinedev/antd";
+
+import { Typography } from "antd";
+
+import { IPost, ICategory } from "../../interfaces";
+
+const { Title, Text } = Typography;
+
+export const PostShow: React.FC<IResourceComponentsProps> = () => {
+    const { queryResult } = useShow<IPost>();
+    const { data, isLoading } = queryResult;
+    const record = data?.data;
+
+    const { data: categoryData, isLoading: categoryIsLoading } =
+        useOne<ICategory>({
+            resource: "categories",
+            id: record?.category.id || "",
+            queryOptions: {
+                enabled: !!record,
+            },
+        });
+
+    return (
+        <Show isLoading={isLoading}>
+            <Title level={5}>Id</Title>
+            <Text>{record?.id}</Text>
+
+            <Title level={5}>Title</Title>
+            <Text>{record?.title}</Text>
+
+            <Title level={5}>Category</Title>
+            <Text>
+                {categoryIsLoading ? "Loading..." : categoryData?.data.title}
+            </Text>
+
+            <Title level={5}>Content</Title>
+            <MarkdownField value={record?.content} />
+        </Show>
+    );
+};
