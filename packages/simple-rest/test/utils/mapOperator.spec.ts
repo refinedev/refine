43b863e4676cf93bdecfb278a3c676997b0714f2
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-04-13 10:57:17 +0300

    feat(simple-rest): add tests for util functions

---
+++ packages/simple-rest/test/utils/mapOperator.spec.ts
@@ -0,0 +1,49 @@
+import { CrudOperators } from "@refinedev/core";
+import { mapOperator } from "../../src/utils";
+
+describe("mapOperator", () => {
+    it("should return correct mapping for given operator", () => {
+        const operatorMappings: Record<CrudOperators, string> = {
+            ne: "_ne",
+            gte: "_gte",
+            lte: "_lte",
+            contains: "_like",
+            eq: "",
+            and: "",
+            between: "",
+            containss: "",
+            endswith: "",
+            endswiths: "",
+            gt: "",
+            in: "",
+            lt: "",
+            ncontains: "",
+            ncontainss: "",
+            nendswith: "",
+            nendswiths: "",
+            nnull: "",
+            nin: "",
+            nbetween: "",
+            nstartswith: "",
+            nstartswiths: "",
+            null: "",
+            or: "",
+            startswith: "",
+            startswiths: "",
+        };
+
+        for (const operator in operatorMappings) {
+            const expectedResult = operatorMappings[operator as CrudOperators];
+            expect(mapOperator(operator as CrudOperators)).toEqual(
+                expectedResult,
+            );
+        }
+    });
+
+    it.each(["unsupported", "", undefined, null])(
+        "should return empty string for %s operator ",
+        (operator) => {
+            expect(mapOperator(operator as CrudOperators)).toEqual("");
+        },
+    );
+});
