eb1b606b9e37d41881c6e26a59ca851e71706c6f
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-04-13 12:02:07 +0300

    fix(simple-rest): remove deprecated warning for stringify export

--- packages/simple-rest/src/index.ts
+++ packages/simple-rest/src/index.ts
@@ -1,13 +1,8 @@
-import { stringify as queryStringStringify } from "query-string";
+import { stringify } from "query-string";
 import { dataProvider } from "./provider";
 
 export default dataProvider;
 
 export * from "./utils";
-
-/**
- * @deprecated Use `stringify` from `query-string` instead. This function will be removed in the next major version. To install `query-string` run `npm install query-string`
- **/
-const stringify = queryStringStringify;
 
 export { stringify };
