659c0284f47e91dc76d7ff74ae988c7605b66a44
Salih <salihozdemir94@gmail.com>
2023-04-18 10:39:15 +0300

    fix: fixed the strapi example

--- examples/data-provider-strapi-v4/src/constants.ts
+++ examples/data-provider-strapi-v4/src/constants.ts
@@ -1,2 +1,3 @@
-export const API_URL = "https://automatic-sweltering-dog.strapiapp.com";
+// export const API_URL = "https://automatic-sweltering-dog.strapiapp.com";
+export const API_URL = "http://localhost:1337";
 export const TOKEN_KEY = "strapi-jwt-token";
