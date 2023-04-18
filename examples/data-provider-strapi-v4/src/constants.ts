8399e973c7c4d99a01b3b15e880166f1d2c0846f
Salih <salihozdemir94@gmail.com>
2023-04-18 15:17:09 +0300

    fix: strapi upload examples

--- examples/data-provider-strapi-v4/src/constants.ts
+++ examples/data-provider-strapi-v4/src/constants.ts
@@ -1,3 +1,2 @@
-// export const API_URL = "https://automatic-sweltering-dog.strapiapp.com";
-export const API_URL = "http://localhost:1337";
+export const API_URL = "https://automatic-sweltering-dog.strapiapp.com";
 export const TOKEN_KEY = "strapi-jwt-token";
