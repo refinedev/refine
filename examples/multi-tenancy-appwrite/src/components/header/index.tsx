adbfb6a95e3907c9dc91da02bdbfd2f646e0576f
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-04-27 09:49:39 +0300

    refactor: appwrite multitenancy support (#4221)
    * refactor: migrate route
    
    * refactor: update resources
    
    * refactor: product and order crud pages
    
    * fix: typo
    
    * docs: refactor new implementation
    
    * fix: update inital tenant
    
    * docs: update screen shots
    
    * docs: add dataprovider tip

---
+++ examples/multi-tenancy-appwrite/src/components/header/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Layout as AntdLayout, Typography, Avatar, Space, theme } from "antd";
+import { useActiveAuthProvider, useGetIdentity } from "@refinedev/core";
+import { RefineThemedLayoutV2HeaderProps } from "@refinedev/antd";
+
+import { StoreSelect } from "../select";
+
+const { Text } = Typography;
+const { useToken } = theme;
+
+export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = () => {
+    const { token } = useToken();
+
+    const authProvider = useActiveAuthProvider();
+    const { data: user } = useGetIdentity({
+        v3LegacyAuthProviderCompatible: Boolean(authProvider?.isLegacy),
+    });
+
+    const shouldRenderHeader = user && (user.name || user.avatar);
+
+    if (!shouldRenderHeader) {
+        return null;
+    }
+
+    return (
+        <AntdLayout.Header
+            style={{
+                backgroundColor: token.colorBgElevated,
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                padding: "0px 24px",
+                height: "64px",
+                position: "sticky",
+                top: 0,
+                zIndex: 1,
+            }}
+        >
+            <Space
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}
+            >
+                <StoreSelect />
+                <Space size="middle">
+                    {user?.name && <Text strong>{user.name}</Text>}
+                    {user?.avatar && (
+                        <Avatar src={user?.avatar} alt={user?.name} />
+                    )}
+                </Space>
+            </Space>
+        </AntdLayout.Header>
+    );
+};
