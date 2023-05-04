6977cccf0869d7ee65b4e681eeae317522ab302c
abdellah711 <alaouiabdellah711@gmail.com>
2023-05-04 13:06:48 +0200

    fix: convert resource name to singular for actions other than list

--- packages/core/src/definitions/helpers/generateDocumentTitle/index.spec.ts
+++ packages/core/src/definitions/helpers/generateDocumentTitle/index.spec.ts
@@ -9,19 +9,19 @@ describe("generateDocumentTitle", () => {
 
     it("should return `Create new resource name` when action is `create`", () => {
         expect(generateDefaultDocumentTitle({ name: "posts" }, "create")).toBe(
-            "Create new Posts | Refine",
+            "Create new Post | Refine",
         );
     });
 
     it("should return `#id Edit resource name` when action is `edit`", () => {
         expect(
             generateDefaultDocumentTitle({ name: "posts" }, "edit", "1"),
-        ).toBe("#1 Edit Posts | Refine");
+        ).toBe("#1 Edit Post | Refine");
     });
 
     it("should return `#id Show resource name` when action is `show`", () => {
         expect(
             generateDefaultDocumentTitle({ name: "posts" }, "show", "1"),
-        ).toBe("#1 Show Posts | Refine");
+        ).toBe("#1 Show Post | Refine");
     });
 });
--- packages/core/src/definitions/helpers/generateDocumentTitle/index.ts
+++ packages/core/src/definitions/helpers/generateDocumentTitle/index.ts
@@ -11,12 +11,18 @@ export function generateDefaultDocumentTitle(
 ) {
     const actionPrefixMatcher = {
         create: "Create new ",
-        edit: `#${id} Edit `,
-        show: `#${id} Show `,
+        edit: `#${id ?? ""} Edit `,
+        show: `#${id ?? ""} Show `,
         list: "",
     };
 
+    // convert resource name to singular for actions other than list
+    const resourceName =
+        action !== "list" && resource.name.endsWith("s")
+            ? resource.name.slice(0, -1)
+            : resource.name;
+
     return `${
         actionPrefixMatcher[action as keyof typeof actionPrefixMatcher]
-    }${capitalize(resource.name)} | Refine`;
+    }${capitalize(resourceName)} | Refine`;
 }
