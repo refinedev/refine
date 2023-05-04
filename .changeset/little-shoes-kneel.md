118681eb65e850a1881c0a401ae75f6a799cd598
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-05-04 15:30:11 +0300

    feat(appwrite): add swizzle to appwrite

---
+++ .changeset/little-shoes-kneel.md
@@ -0,0 +1,5 @@
+---
+"@refinedev/appwrite": major
+---
+
+feat: added swizzle to appwrite
---
+++ packages/appwrite/src/utils/getAppwriteSorting.ts
@@ -0,0 +1,23 @@
+import { CrudSorting } from "@refinedev/core";
+import { Query } from "appwrite";
+
+type GetAppwriteSortingType = {
+    (sorts?: CrudSorting): string[];
+};
+
+export const getAppwriteSorting: GetAppwriteSortingType = (sorters) => {
+    const sorts: string[] = [];
+
+    if (sorters) {
+        sorters.map((item) => {
+            const field = item.field === "id" ? "$id" : item.field;
+            if (item.order === "asc") {
+                sorts.push(Query.orderAsc(field));
+            } else {
+                sorts.push(Query.orderDesc(field));
+            }
+        });
+    }
+
+    return sorts;
+};
