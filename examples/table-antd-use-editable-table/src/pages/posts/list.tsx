93daaf16640a0a946f36d26fc80c1def1bd2921b
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-05-05 14:15:45 +0300

    chore(examples): remove debug code

--- examples/table-antd-use-editable-table/src/pages/posts/list.tsx
+++ examples/table-antd-use-editable-table/src/pages/posts/list.tsx
@@ -21,9 +21,7 @@ export const PostList: React.FC<IResourceComponentsProps> = () => {
         saveButtonProps,
         cancelButtonProps,
         editButtonProps,
-    } = useEditableTable<IPost>({
-        autoSubmitClose: false,
-    });
+    } = useEditableTable<IPost>();
 
     return (
         <List>
