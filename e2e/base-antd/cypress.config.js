ca163a246d86b9ea738cf2783fc9c5eeb0a3fc01
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-05-09 13:52:42 +0300

    build: e2e

--- e2e/base-antd/cypress.config.js
+++ e2e/base-antd/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     projectId: "sq5j3e",
     e2e: {
-        baseUrl: "http://localhost:3000",
+        baseUrl: "http://127.0.0.1:5173",
     },
     chromeWebSecurity: false,
     experimentalMemoryManagement: true,
