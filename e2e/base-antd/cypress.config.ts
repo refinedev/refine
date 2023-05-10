10920678c2ce0e765db0f57e9844b3d3b817e3b3
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-05-10 11:29:50 +0300

    chore: remove e2e folders

--- e2e/base-antd/cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-    projectId: "sq5j3e",
-    e2e: {},
-    chromeWebSecurity: false,
-    experimentalMemoryManagement: true,
-    numTestsKeptInMemory: 1,
-});
