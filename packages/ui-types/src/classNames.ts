35561ec4d38654897c63490c0d368fb6fd62beb1
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-05-10 15:34:15 +0300

    feat: add className on antd

---
+++ packages/ui-types/src/classNames.ts
@@ -0,0 +1,17 @@
+export const RefinePageHeaderClassNames = {
+    Title: "refine-pageHeader-title",
+    SubTitle: "refine-pageHeader-subTitle",
+};
+
+export const RefineButtonClassNames = {
+    CloneButton: "refine-clone-button",
+    DeleteButton: "refine-delete-button",
+    EditButton: "refine-edit-button",
+    SaveButton: "refine-save-button",
+    CreateButton: "refine-create-button",
+    ImportButton: "refine-import-button",
+    ExportButton: "refine-export-button",
+    ListButton: "refine-list-button",
+    RefreshButton: "refine-refresh-button",
+    ShowButton: "refine-show-button",
+};
