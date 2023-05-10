2ebb5d29b2194d8a870f48f2bacb40c73fc6decc
Salih <salihozdemir94@gmail.com>
2023-05-10 18:35:45 +0300

    test: increase test coverage on data hooks

--- packages/core/src/hooks/data/useUpdate.spec.tsx
+++ packages/core/src/hooks/data/useUpdate.spec.tsx
@@ -154,5 +154,255 @@ describe("useUpdate Hook", () => {
                 },
             });
         });
+
+        it("publish live event without `ids` if no `id` is returned from the dataProvider", async () => {
+            const onPublishMock = jest.fn();
+
+            const { result } = renderHook(() => useUpdate(), {
+                wrapper: TestWrapper({
+                    dataProvider: {
+                        default: {
+                            ...MockJSONServer.default,
+                            update: jest.fn().mockResolvedValue({ data: {} }),
+                        },
+                    },
+                    resources: [{ name: "posts" }],
+                    liveProvider: {
+                        unsubscribe: jest.fn(),
+                        subscribe: jest.fn(),
+                        publish: onPublishMock,
+                    },
+                }),
+            });
+
+            result.current.mutate({
+                resource: "posts",
+                id: "1",
+                values: { title: "foo" },
+            });
+
+            await waitFor(() => {
+                expect(result.current.isSuccess).toBeTruthy();
+            });
+
+            expect(onPublishMock).toHaveBeenCalledWith({
+                channel: "resources/posts",
+                date: expect.any(Date),
+                type: "updated",
+                payload: {},
+            });
+        });
+    });
+
+    describe("useNotification", () => {
+        it("should call `open` from the notification provider on success", async () => {
+            const openNotificationMock = jest.fn();
+
+            const { result } = renderHook(() => useUpdate(), {
+                wrapper: TestWrapper({
+                    dataProvider: MockJSONServer,
+                    notificationProvider: {
+                        open: openNotificationMock,
+                    },
+                    resources: [{ name: "posts" }],
+                }),
+            });
+
+            result.current.mutate({
+                resource: "posts",
+                id: "1",
+                values: {},
+            });
+
+            await waitFor(() => {
+                expect(result.current.isSuccess).toBeTruthy();
+            });
+
+            expect(openNotificationMock).toBeCalledWith({
+                description: "Successful",
+                key: "1-posts-notification",
+                message: "Successfully updated post",
+                type: "success",
+            });
+        });
+
+        it("should call `open` from the notification provider on error", async () => {
+            const updateMock = jest.fn().mockRejectedValue(new Error("Error"));
+            const notificationMock = jest.fn();
+
+            const { result } = renderHook(() => useUpdate(), {
+                wrapper: TestWrapper({
+                    dataProvider: {
+                        default: {
+                            ...MockJSONServer.default,
+                            update: updateMock,
+                        },
+                    },
+                    notificationProvider: {
+                        open: notificationMock,
+                    },
+                    resources: [{ name: "posts" }],
+                }),
+            });
+
+            result.current.mutate({
+                id: "1",
+                resource: "posts",
+                values: {},
+            });
+
+            await waitFor(() => {
+                expect(result.current.isError).toBeTruthy();
+            });
+
+            expect(notificationMock).toBeCalledWith({
+                description: "Error",
+                key: "1-posts-notification",
+                message: "Error when updating post (status code: undefined)",
+                type: "error",
+            });
+        });
+
+        it("should call `open` from notification provider on success with custom notification params", async () => {
+            const openNotificationMock = jest.fn();
+
+            const { result } = renderHook(() => useUpdate(), {
+                wrapper: TestWrapper({
+                    dataProvider: MockJSONServer,
+                    notificationProvider: {
+                        open: openNotificationMock,
+                    },
+                    resources: [{ name: "posts" }],
+                }),
+            });
+
+            result.current.mutate({
+                resource: "posts",
+                id: "1",
+                values: {},
+                successNotification: () => ({
+                    message: "Success",
+                    description: "Successfully created post",
+                    type: "success",
+                }),
+            });
+
+            await waitFor(() => {
+                expect(result.current.isSuccess).toBeTruthy();
+            });
+
+            expect(openNotificationMock).toBeCalledWith({
+                description: "Successfully created post",
+                message: "Success",
+                type: "success",
+            });
+        });
+
+        it("should call `open` from notification provider on error with custom notification params", async () => {
+            const updateMock = jest.fn().mockRejectedValue(new Error("Error"));
+            const openNotificationMock = jest.fn();
+
+            const { result } = renderHook(() => useUpdate(), {
+                wrapper: TestWrapper({
+                    dataProvider: {
+                        default: {
+                            ...MockJSONServer.default,
+                            update: updateMock,
+                        },
+                    },
+                    notificationProvider: {
+                        open: openNotificationMock,
+                    },
+                    resources: [{ name: "posts" }],
+                }),
+            });
+
+            result.current.mutate({
+                resource: "posts",
+                id: "1",
+                values: {},
+                errorNotification: () => ({
+                    message: "Error",
+                    description: "There was an error creating post",
+                    type: "error",
+                }),
+            });
+
+            await waitFor(() => {
+                expect(result.current.isError).toBeTruthy();
+            });
+
+            expect(openNotificationMock).toBeCalledWith({
+                description: "There was an error creating post",
+                message: "Error",
+                type: "error",
+            });
+        });
+    });
+
+    describe("useOnError", () => {
+        it("should call `onError` from the auth provider on error", async () => {
+            const updateMock = jest.fn().mockRejectedValue(new Error("Error"));
+            const onErrorMock = jest.fn();
+
+            const { result } = renderHook(() => useUpdate(), {
+                wrapper: TestWrapper({
+                    dataProvider: {
+                        default: {
+                            ...MockJSONServer.default,
+                            update: updateMock,
+                        },
+                    },
+                    authProvider: {
+                        onError: onErrorMock,
+                    } as any,
+                    resources: [{ name: "posts" }],
+                }),
+            });
+
+            result.current.mutate({
+                resource: "posts",
+                id: "1",
+                values: {},
+            });
+
+            await waitFor(() => {
+                expect(result.current.isError).toBeTruthy();
+            });
+
+            expect(onErrorMock).toBeCalledWith(new Error("Error"));
+        });
+
+        it("should call `checkError` from the legacy auth provider on error", async () => {
+            const updateMock = jest.fn().mockRejectedValue(new Error("Error"));
+            const onErrorMock = jest.fn();
+
+            const { result } = renderHook(() => useUpdate(), {
+                wrapper: TestWrapper({
+                    dataProvider: {
+                        default: {
+                            ...MockJSONServer.default,
+                            update: updateMock,
+                        },
+                    },
+                    legacyAuthProvider: {
+                        checkError: onErrorMock,
+                    },
+                    resources: [{ name: "posts" }],
+                }),
+            });
+
+            result.current.mutate({
+                resource: "posts",
+                id: "1",
+                values: {},
+            });
+
+            await waitFor(() => {
+                expect(result.current.isError).toBeTruthy();
+            });
+
+            expect(onErrorMock).toBeCalledWith(new Error("Error"));
+        });
     });
 });
