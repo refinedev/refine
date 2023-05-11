b1f5e6d160b2df8a34e52f2c918170e6f3f5c32a
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-05-11 17:18:59 +0300

    chore: update port and host config

--- examples/base-antd/vite.config.ts
+++ examples/base-antd/vite.config.ts
@@ -3,8 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 3000,
-        host: "127.0.0.1",
-    },
 });
--- examples/base-mantine/vite.config.ts
+++ examples/base-mantine/vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 3000,
-        host: "127.0.0.1",
-    },
 });
