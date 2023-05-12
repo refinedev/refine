6b92ba1ff1cb975b4a0ed846f44f86e735333e58
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-05-12 14:54:19 +0300

    feat: add show page on base-material-ui

---
+++ examples/base-material-ui/src/pages/posts/show.tsx
@@ -0,0 +1,60 @@
+import { useShow, useOne } from "@refinedev/core";
+import {
+    Show,
+    NumberField,
+    TextFieldComponent as TextField,
+    MarkdownField,
+    DateField,
+} from "@refinedev/mui";
+import { Typography, Stack } from "@mui/material";
+
+export const PostShow = () => {
+    const { queryResult } = useShow();
+    const { data, isLoading } = queryResult;
+
+    const record = data?.data;
+
+    const { data: categoryData, isLoading: categoryIsLoading } = useOne({
+        resource: "categories",
+        id: record?.category?.id || "",
+        queryOptions: {
+            enabled: !!record,
+        },
+    });
+
+    return (
+        <Show isLoading={isLoading}>
+            <Stack gap={1}>
+                <Typography variant="body1" fontWeight="bold">
+                    Id
+                </Typography>
+                <NumberField value={record?.id ?? ""} />
+                <Typography variant="body1" fontWeight="bold">
+                    Title
+                </Typography>
+                <TextField value={record?.title} />
+                <Typography variant="body1" fontWeight="bold">
+                    Content
+                </Typography>
+                <MarkdownField value={record?.content} />
+                <Typography variant="body1" fontWeight="bold">
+                    Category
+                </Typography>
+
+                {categoryIsLoading ? (
+                    <>Loading...</>
+                ) : (
+                    <>{categoryData?.data?.title}</>
+                )}
+                <Typography variant="body1" fontWeight="bold">
+                    Status
+                </Typography>
+                <TextField value={record?.status} />
+                <Typography variant="body1" fontWeight="bold">
+                    Created At
+                </Typography>
+                <DateField value={record?.createdAt} />
+            </Stack>
+        </Show>
+    );
+};
