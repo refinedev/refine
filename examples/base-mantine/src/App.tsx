c1295b0d606aa584788524457b96021967e2dc64
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-05-12 09:40:58 +0300

    Revert "chore: temp disable edit pages"
    This reverts commit 6fdf87f0e2dc6f3af9b72e391e3678a0adc839fb.

--- examples/base-mantine/src/App.tsx
+++ examples/base-mantine/src/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
                                 list: "/posts",
                                 show: "/posts/show/:id",
                                 create: "/posts/create",
-                                // edit: "/posts/edit/:id",
+                                edit: "/posts/edit/:id",
                             },
                         ]}
                         options={{
