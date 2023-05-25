f4babef36478fa2522c652c095dd755f2dd392ab
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-05-25 16:54:21 +0300

    test(examples): add e2e to with-remix-antd with-remix-headless

---
+++ examples/with-remix-headless/app/components/breadcrumb/index.tsx
@@ -0,0 +1,20 @@
+import { useBreadcrumb } from "@refinedev/core";
+import { Link } from "react-router-dom";
+
+export const Breadcrumb = () => {
+    const { breadcrumbs } = useBreadcrumb();
+
+    return (
+        <ul className="breadcrumb">
+            {breadcrumbs.map((breadcrumb) => (
+                <li key={`breadcrumb-${breadcrumb.label}`}>
+                    {breadcrumb.href ? (
+                        <Link to={breadcrumb.href}>{breadcrumb.label}</Link>
+                    ) : (
+                        <span>{breadcrumb.label}</span>
+                    )}
+                </li>
+            ))}
+        </ul>
+    );
+};
