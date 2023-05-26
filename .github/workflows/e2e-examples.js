c8e88a077518a4c5d14a16874f6c285e0b0ed36a
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-05-26 11:49:11 +0300

    fix: build first

--- .github/workflows/e2e-examples.js
+++ .github/workflows/e2e-examples.js
@@ -28,21 +28,22 @@ for (const path of hasE2EExamples) {
         stdio: "inherit",
     });
 
-    console.log("|- wait: ", path);
-    const tests = exec(
-        `npx wait-on tcp:3000 -i 1000 -d 10000 --verbose && npm run lerna run cypress:run -- --scope ${path} -- --record --key ${KEY} --ci-build-id=${CI_BUILD_ID} --parallel && npx kill-port 3000`,
-    );
+    execSync(`npm run build -- --scope base-antd --include-dependencies`, {
+        stdio: "inherit",
+    });
 
     console.log("|- start: ", path);
-    const start = exec(
-        `npm run start -- --scope ${path} -- --port 3000`,
-    );
+
+    const start = exec(`npm run start -- --scope ${path} -- --port 3000`);
 
     start.stdout.on("data", (data) => console.log(data));
+    start.stderr.on("data", (data) => console.log(data));
 
-
-
-    tests.stdout.on("data", (data) => console.log(data));
+    execSync(
+        `npx wait-on tcp:3000 -i 1000 -d 10000 --timeout 25000 --verbose && npm run lerna run cypress:run -- --scope ${path} -- --record --key ${KEY} --ci-build-id=${CI_BUILD_ID} --parallel`,
+    );
 
     console.log("|- finished: ", path);
+
+    start.kill("SIGTERM");
 }
