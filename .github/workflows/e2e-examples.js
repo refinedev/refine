6d95ac10591abdf6411e0cf8a72f5588603d8e87
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-05-26 11:05:49 +0300

    fix: process

--- .github/workflows/e2e-examples.js
+++ .github/workflows/e2e-examples.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const { exec, execSync } = require('child_process');
+const { exec, execSync } = require("child_process");
 const KEY = process.env.KEY;
 const CI_BUILD_ID = process.env.CI_BUILD_ID;
 
@@ -11,7 +11,10 @@ const EXAMPLES = process.env.EXAMPLES ? process.env.EXAMPLES : [];
 const hasE2EExamples = [];
 EXAMPLES.split(",").map((path) => {
     const dir = EXAMPLES_DIR + "/" + path;
-    if (fs.statSync(dir).isDirectory() && fs.existsSync(`${dir}/cypress.config.ts`)) {
+    if (
+        fs.statSync(dir).isDirectory() &&
+        fs.existsSync(`${dir}/cypress.config.ts`)
+    ) {
         hasE2EExamples.push(path);
     }
 });
@@ -22,16 +25,23 @@ for (const path of hasE2EExamples) {
 
     console.log("|- bootstrap: ", path);
     execSync(`npm run bootstrap -- --scope ${path}`, {
-        stdio: 'inherit',
+        stdio: "inherit",
     });
     console.log("|- start: ", path);
     const start = exec(`npm run start -- --scope ${path}`);
 
-    start.stdout.on('data', (data) => console.log(data));
+    start.stdout.on("data", (data) => console.log(data));
 
-    execSync(`npx wait-on tcp:3000 --verbose && npm run lerna run cypress:run -- --scope ${path} -- --record --key ${KEY} --ci-build-id=${CI_BUILD_ID} --parallel`, {
-        stdio: 'inherit',
+    const tests = exec(
+        `npx wait-on tcp:3000 --verbose && npm run lerna run cypress:run -- --scope ${path} -- --record --key ${KEY} --ci-build-id=${CI_BUILD_ID} --parallel`,
+        {
+            stdio: "inherit",
+        },
+    );
+
+    tests.on("exit", (code) => {
+        start.kill("SIGINT");
     });
+
     console.log("|- finished: ", path);
-    start.kill("SIGINT");
 }
