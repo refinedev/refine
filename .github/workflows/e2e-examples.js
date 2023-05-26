6b437dcbb803ab8b1b41b0112dabb2f71337866f
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-05-26 15:09:09 +0300

    fix: remove other execs

--- .github/workflows/e2e-examples.js
+++ .github/workflows/e2e-examples.js
@@ -33,35 +33,35 @@ const runTests = () => {
             stdio: "inherit",
         });
 
-        execSync(`npm run build -- --scope base-antd --include-dependencies`, {
-            stdio: "inherit",
-        });
+        // execSync(`npm run build -- --scope base-antd --include-dependencies`, {
+        //     stdio: "inherit",
+        // });
 
-        console.log("|- start: ", path);
+        // console.log("|- start: ", path);
 
-        const start = exec(`npm run start -- --scope ${path}`);
+        // const start = exec(`npm run start -- --scope ${path}`);
 
-        start.stdout.on("data", (data) => console.log(data));
-        start.stderr.on("data", (data) => console.log(data));
+        // start.stdout.on("data", (data) => console.log(data));
+        // start.stderr.on("data", (data) => console.log(data));
 
-        execSync(
-            `npx wait-on tcp:3000 -i 1000 -d 5000 --timeout 25000 --verbose`,
-            { stdio: "inherit" },
-        );
+        // execSync(
+        //     `npx wait-on tcp:3000 -i 1000 -d 5000 --timeout 25000 --verbose`,
+        //     { stdio: "inherit" },
+        // );
 
-        execSync(
-            `npm run lerna run cypress:run -- --scope ${path} -- --record --key ${KEY} --ci-build-id=${CI_BUILD_ID} --parallel`,
-            { stdio: "inherit" },
-        );
+        // execSync(
+        //     `npm run lerna run cypress:run -- --scope ${path} -- --record --key ${KEY} --ci-build-id=${CI_BUILD_ID} --parallel`,
+        //     { stdio: "inherit" },
+        // );
 
-        pids(3000).then((pids) => {
-            console.log("|- kill: ", pids.all);
-            pids.all.forEach((pid) => {
-                process.kill(pid, "SIGTERM");
-            });
-        });
+        // pids(3000).then((pids) => {
+        //     console.log("|- kill: ", pids.all);
+        //     pids.all.forEach((pid) => {
+        //         process.kill(pid, "SIGTERM");
+        //     });
+        // });
 
-        start.kill("SIGTERM");
+        // start.kill("SIGTERM");
     }
 };
 
