91a7dce93c693e50b58c96b6e3eac139fa58f736
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-05-29 15:04:59 +0300

    fix: add missing methods for auth provider

--- examples/with-nextjs/src/authProvider.ts
+++ examples/with-nextjs/src/authProvider.ts
@@ -10,6 +10,10 @@ const mockUsers = [
         email: "editor@refine.dev",
         roles: ["editor"],
     },
+    {
+        email: "demo@refine.dev",
+        roles: ["admin"],
+    },
 ];
 
 export const authProvider: AuthBindings = {
@@ -30,6 +34,69 @@ export const authProvider: AuthBindings = {
 
         return {
             success: false,
+            error: {
+                message: "Login failed",
+                name: "Invalid email or password",
+            },
+        };
+    },
+    register: async (params) => {
+        // Suppose we actually send a request to the back end here.
+        const user = mockUsers.find((item) => item.email === params.email);
+
+        if (user) {
+            nookies.set(null, "auth", JSON.stringify(user), {
+                maxAge: 30 * 24 * 60 * 60,
+                path: "/",
+            });
+            return {
+                success: true,
+                redirectTo: "/",
+            };
+        }
+        return {
+            success: false,
+            error: {
+                message: "Register failed",
+                name: "Invalid email or password",
+            },
+        };
+    },
+    forgotPassword: async (params) => {
+        // Suppose we actually send a request to the back end here.
+        const user = mockUsers.find((item) => item.email === params.email);
+
+        if (user) {
+            //we can send email with reset password link here
+            return {
+                success: true,
+            };
+        }
+        return {
+            success: false,
+            error: {
+                message: "Forgot password failed",
+                name: "Invalid email",
+            },
+        };
+    },
+    updatePassword: async (params) => {
+        // Suppose we actually send a request to the back end here.
+        const isPasswordInvalid =
+            params.password === "123456" || !params.password;
+
+        if (isPasswordInvalid) {
+            return {
+                success: false,
+                error: {
+                    message: "Update password failed",
+                    name: "Invalid password",
+                },
+            };
+        }
+
+        return {
+            success: true,
         };
     },
     logout: async () => {
@@ -74,11 +141,14 @@ export const authProvider: AuthBindings = {
         }
         return null;
     },
-    getIdentity: () => {
+    getIdentity: async () => {
         const auth = nookies.get()["auth"];
         if (auth) {
-            const parsedUser = JSON.parse(auth);
-            return parsedUser.username;
+            return {
+                id: 1,
+                name: "Jane Doe",
+                avatar: "https://unsplash.com/photos/IWLOvomUmWU/download?force=true&w=640",
+            };
         }
         return null;
     },
