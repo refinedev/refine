f67967e8c871b2252b4c1b827de3656bf153d1ee
Salih Özdemir <salihozdemir94@gmail.com>
2023-05-30 18:39:24 +0300

    refactor: fix name and state inconsistency in `<ThemedLayoutV2>`. (#4404)
    * fix(antd): update the layout context
    
    * fix(antd): provide backward compability on `useSiderVisible`
    
    * fix(antd): update the state name on Sider
    
    * fix: create `useSiderState`, deprecate `useSiderVisible`
    
    * refactor: update the context type
    
    * refactor(mui): refactor the layout context
    
    * chore: formatting
    
    * refactor(chakra-ui): refactor the layout context
    
    * refactor(mantine): refactor the layout context
    
    * docs: update the docs with new states
    
    * chore: update the import path
    
    * chore: add changeset
    
    * chore: remove period
    
    * fix(antd): setSiderCollapsed when click the menu item
    
    * refactor(antd): rename `useSiderState` to `useThemedLayoutContext`
    
    * refactor(material-ui): rename `useSiderState` to `useThemedLayoutContext`
    
    * fix(mui): swizzle issue
    
    * refactor(chakra-ui): rename `useSiderState` to `useThemedLayoutContext`
    
    * refactor(mantine): rename `useSiderState` to `useThemedLayoutContext`
    
    * chore(use-sider-state): replace `useSiderState` references with `useThemedLayoutContext`
    
    ---------
    
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>

--- packages/mui/src/contexts/themedLayoutContext/IThemedLayoutContext.ts
+++ packages/mui/src/contexts/themedLayoutContext/IThemedLayoutContext.ts
@@ -1,6 +1,6 @@
 export interface IThemedLayoutContext {
-    siderVisible?: boolean;
-    drawerSiderVisible?: boolean;
-    setSiderVisible?: (visible: boolean) => void;
-    setDrawerSiderVisible?: (visible: boolean) => void;
+    siderCollapsed: boolean;
+    setSiderCollapsed: (visible: boolean) => void;
+    mobileSiderOpen: boolean;
+    setMobileSiderOpen: (visible: boolean) => void;
 }
