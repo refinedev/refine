3f5e5290c64de065a3b8325f146925875fb2ac49
Salih <salihozdemir94@gmail.com>
2023-06-01 12:32:12 +0300

    fix: remove auth check on index page

--- examples/with-nextjs-appdir/app/page.tsx
+++ examples/with-nextjs-appdir/app/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import { Authenticated } from "@refinedev/core";
 import { NavigateToResource } from "@refinedev/nextjs-router/app";
 
 export default function IndexPage() {
-    return (
-        <Authenticated redirectOnFail="/login" appendCurrentPathToQuery={false}>
-            <NavigateToResource />
-        </Authenticated>
-    );
+    return <NavigateToResource />;
 }
