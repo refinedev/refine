6c7e9c1051bc1a5edee5b0d479aadef7faa3b49b
Ali Emir Şen <senaliemir@gmail.com>
2023-06-05 15:26:26 +0300

    style(core): fix lint error in tests

--- packages/core/src/definitions/helpers/check-router-prop-misuse/index.spec.ts
+++ packages/core/src/definitions/helpers/check-router-prop-misuse/index.spec.ts
@@ -3,14 +3,10 @@ import { checkRouterPropMisuse } from ".";
 
 describe("checkRouterPropMisuse", () => {
     it("should return false when pass routerBindings", () => {
-        expect(
-            checkRouterPropMisuse(mockRouterBindings()),
-        ).toBeFalsy();
+        expect(checkRouterPropMisuse(mockRouterBindings())).toBeFalsy();
     });
 
     it("should return true when pass legacyRouterProvider", () => {
-        expect(
-            checkRouterPropMisuse(mockLegacyRouterProvider()),
-        ).toBeTruthy();
+        expect(checkRouterPropMisuse(mockLegacyRouterProvider())).toBeTruthy();
     });
 });
