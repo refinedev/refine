2fb6f690f78f6a662669ad63e8be5113cdcf175e
Ali Emir Şen <senaliemir@gmail.com>
2023-06-05 16:38:22 +0300

    chore(theme-toggle): add theme toggle

--- documentation/src/refine-theme/common-theme-toggle.tsx
+++ documentation/src/refine-theme/common-theme-toggle.tsx
@@ -1,33 +1,68 @@
-import clsx from "clsx";
 import React from "react";
-import { LightModeIcon } from "./icons/light-mode";
+import clsx from "clsx";
 import { useColorMode } from "@docusaurus/theme-common";
+
+import { LightModeIcon } from "./icons/light-mode";
+import { DarkModeIcon } from "./icons/dark-mode";
 
 export const CommonThemeToggle = () => {
     const { colorMode, setColorMode } = useColorMode();
-    console.log(colorMode);
+
+    const toggle = () => {
+        setColorMode(colorMode === "dark" ? "light" : "dark");
+    };
 
     return (
         <button
             type="button"
             className={clsx(
-                "bg-gray-700",
+                "bg-gray-200 dark:bg-gray-700",
                 "w-10 h-10",
                 "rounded-full",
-                "flex items-center justify-center",
                 "border-0",
                 "appearance-none",
                 "focus:outline-none",
-                "text-gray-400",
-                "hover:brightness-125",
-                "focus:brightness-110",
-                "active:brightness-110",
+                "relative",
+                "text-gray-500 dark:text-gray-400",
+                "hover:brightness-110",
+                "dark:hover:brightness-110",
                 "transition-all",
                 "duration-200",
                 "ease-in-out",
+                "overflow-hidden",
             )}
+            onClick={toggle}
         >
-            <LightModeIcon />
+            <div
+                className={clsx(
+                    "absolute",
+                    "w-10 h-10",
+                    "flex items-center justify-center",
+                    "flex-shrink-0",
+                    "top-10",
+                    "dark:top-0",
+                    "duration-200",
+                    "ease-in-out",
+                    "transition-all",
+                )}
+            >
+                <LightModeIcon />
+            </div>
+            <div
+                className={clsx(
+                    "absolute",
+                    "w-10 h-10",
+                    "flex items-center justify-center",
+                    "flex-shrink-0",
+                    "top-0",
+                    "dark:top-10",
+                    "duration-200",
+                    "ease-in-out",
+                    "transition-all",
+                )}
+            >
+                <DarkModeIcon />
+            </div>
         </button>
     );
 };
--- documentation/tailwind.config.js
+++ documentation/tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class", '[data-theme="dark"]'],
-    important: "#__docusaurus",
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
