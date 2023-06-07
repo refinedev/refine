58e0e16fa283638e49233abdcfa6a38835587548
Ali Emir Şen <senaliemir@gmail.com>
2023-06-08 00:56:50 +0300

    docs: add landing tiles

---
+++ documentation/src/refine-theme/landing-tile-with-image.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import clsx from "clsx";
+
+type Props = {
+    image: React.ReactNode;
+    children: React.ReactNode;
+};
+
+export const LandingTileWithImage = ({ image, children }: Props) => {
+    return (
+        <div
+            className={clsx(
+                "rounded-xl",
+                "p-px",
+                "relative",
+                "z-[1]",
+                "overflow-hidden",
+                "group",
+            )}
+        >
+            <div
+                className={clsx(
+                    "z-[-1]",
+                    "absolute",
+                    "w-full",
+                    "h-auto",
+                    "bg-landing-tile-border-bg",
+                    "bg-[length:200%]",
+                    "aspect-square",
+                    "w-[200%]",
+                    "-left-1/2",
+                    "-top-1/3",
+                    "animate-spin-slow",
+                    "animation-paused",
+                    "group-hover:animation-running",
+                    "group-hover:brightness-125",
+                )}
+            />
+            <div
+                className={clsx(
+                    "z-[1]",
+                    "w-full",
+                    "h-full",
+                    "bg-refine-bg",
+                    "bg-landing-tile-tile-bg",
+                    "rounded-xl",
+                    "overflow-hidden",
+                )}
+            >
+                <div
+                    className={clsx(
+                        "bg-[rgba(77,77,178,0.1)]",
+                        "bg-landing-tile-image-bg",
+                        "h-[183px]",
+                        "w-full",
+                        "border-b",
+                        "border-b-[#272762]",
+                        "flex items-center justify-center",
+                        "backdrop-blur-md",
+                        "group-hover:brightness-105",
+                    )}
+                >
+                    {image}
+                </div>
+                <div className={clsx("p-8", "text-base", "text-gray-0")}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ documentation/src/refine-theme/landing-tile.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import clsx from "clsx";
+
+type Props = {
+    icon: React.ReactNode;
+    children: React.ReactNode;
+};
+
+export const LandingTile = ({ icon, children }: Props) => {
+    return (
+        <div
+            className={clsx(
+                "rounded-xl",
+                "p-px",
+                "relative",
+                "z-[1]",
+                "overflow-hidden",
+                "group",
+            )}
+        >
+            <div
+                className={clsx(
+                    "z-[-1]",
+                    "absolute",
+                    "w-full",
+                    "h-auto",
+                    "bg-landing-tile-border-bg",
+                    "bg-[length:150%]",
+                    "aspect-square",
+                    "w-[150%]",
+                    "-left-1/4",
+                    "-top-1/2",
+                    "animate-spin-slow",
+                    "animation-paused",
+                    "group-hover:animation-running",
+                    "group-hover:brightness-125",
+                )}
+            />
+            <div
+                className={clsx(
+                    "z-[1]",
+                    "w-full",
+                    "h-full",
+                    "bg-refine-bg",
+                    "bg-landing-tile-tile-bg",
+                    "rounded-xl",
+                    "overflow-hidden",
+                    "p-2",
+                )}
+            >
+                <div
+                    className={clsx(
+                        "flex flex-col gap-4",
+                        "p-6",
+                        "bg-landing-tile-grid-bg",
+                        "bg-contain",
+                    )}
+                >
+                    <div
+                        className={clsx(
+                            "w-full",
+                            "flex items-center justify-start",
+                        )}
+                    >
+                        <div
+                            className={clsx(
+                                "w-16",
+                                "h-16",
+                                "flex-shrink-0",
+                                "border border-[#4D4DB2]",
+                                "rounded-full",
+                                "bg-refine-bg",
+                                "flex items-center justify-center",
+                            )}
+                        >
+                            {icon}
+                        </div>
+                    </div>
+                    <div className={clsx("text-base", "text-gray-0")}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
