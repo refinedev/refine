c5dfaa206015b65c8e4ab3fbf598899367060f1d
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-06-08 16:43:26 +0300

    fix: image ratio

--- documentation/src/components/blog/featured-blog-post-items/index.js
+++ documentation/src/components/blog/featured-blog-post-items/index.js
@@ -6,17 +6,18 @@ import clsx from "clsx";
 
 export const FeaturedBlogPostItems = ({ items }) => {
     return (
-        <div className="mb-6">
+        <div className="mb-6 mx-2">
             <h2
                 className={clsx(
                     "text-4xl leading-10 text-gray-900",
                     "dark:text-gray-0",
-                    "mt-3",
+                    "mb-8",
+                    "px-4",
                 )}
             >
                 Featured Posts
             </h2>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 border-b pb-6 border-gray-200 dark:border-gray-700">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 border-b pb-6 border-gray-200 dark:border-gray-700">
                 {items.map(({ content: BlogPostContent }) => (
                     <BlogPostProvider
                         key={BlogPostContent.metadata.permalink}
