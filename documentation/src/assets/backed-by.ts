3ea26c5a7424418b2332c5f1cbcb086e5b109b20
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-06-09 14:13:14 +0300

    feat: update team foto url

--- documentation/src/assets/backed-by.ts
+++ documentation/src/assets/backed-by.ts
@@ -1,28 +1,32 @@
 export const backedBy = [
     {
         name: "Emre Baran",
-        avatar: "/about/images/team-avatars/emre.png",
-        avatar2x: "/about/images/team-avatars/emre2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/emre.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/emre2x.png",
         role1: "CEO & Co-founder at Cerbos",
         role2: "Expert-in-Residence at Seedcamp",
     },
     {
         name: "Burak Emre Kabakçı",
-        avatar: "/about/images/team-avatars/burak.png",
-        avatar2x: "/about/images/team-avatars/burak2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/burak.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/burak2x.png",
         role1: "Staff SWE @ LiveRamp Maker",
     },
     {
         name: "Piotr Karwatka",
-        avatar: "/about/images/team-avatars/piotr.png",
-        avatar2x: "/about/images/team-avatars/piotr2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/piotr.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/piotr2x.png",
         role1: "Co-founder @ Catch The Tornado",
         role2: "Business Angel investor",
     },
     {
         name: "Aksel Kastoryano",
-        avatar: "/about/images/team-avatars/aksel.png",
-        avatar2x: "/about/images/team-avatars/aksel2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/aksel.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/aksel2x.png",
         role1: "Entrepreneur - Investor",
     },
 ];
--- documentation/src/assets/team.ts
+++ documentation/src/assets/team.ts
@@ -1,65 +1,75 @@
 export const team = [
     {
         name: "Civan Özseyhan",
-        avatar: "/about/images/team-avatars/civan.png",
-        avatar2x: "/about/images/team-avatars/civan2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/civan.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/civan2x.png",
         role1: "Co-Founder",
         role2: "CEO",
     },
     {
         name: "Ömer Aplak",
-        avatar: "/about/images/team-avatars/omer.png",
-        avatar2x: "/about/images/team-avatars/omer2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/omer.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/omer2x.png",
         role1: "Co-Founder",
         role2: "CTO",
     },
     {
         name: "Eren Erkalkan",
-        avatar: "/about/images/team-avatars/eren.png",
-        avatar2x: "/about/images/team-avatars/eren2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/eren.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/eren2x.png",
         role1: "Co-Founder",
         role2: "Head of Product",
     },
     {
         name: "Batuhan Özgür Özdemir",
-        avatar: "/about/images/team-avatars/batuhan.png",
-        avatar2x: "/about/images/team-avatars/batuhan2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/batuhan2x.png",
         role1: "Tech Lead",
     },
     {
         name: "Ali Emir Şen",
-        avatar: "/about/images/team-avatars/ali.png",
-        avatar2x: "/about/images/team-avatars/ali2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/ali.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/ali2x.png",
         role1: "Frontend Team Lead",
     },
     {
         name: "Yıldıray Ünlü",
-        avatar: "/about/images/team-avatars/yildiray.png",
-        avatar2x: "/about/images/team-avatars/yildiray2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/yildiray.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/yildiray2x.png",
         role1: "Backend Team Lead",
     },
     {
         name: "Salih Özdemir",
-        avatar: "/about/images/team-avatars/salih.png",
-        avatar2x: "/about/images/team-avatars/salih2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/salih.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/salih2x.png",
         role1: "Software Developer",
     },
     {
         name: "Alican Erdurmaz",
-        avatar: "/about/images/team-avatars/alican.png",
-        avatar2x: "/about/images/team-avatars/alican2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/alican.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/alican2x.png",
         role1: "Software Developer",
     },
     {
         name: "Necati Ozmen",
-        avatar: "/about/images/team-avatars/necati.png",
-        avatar2x: "/about/images/team-avatars/necati2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/necati.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/necati2x.png",
         role1: "Head of Content",
     },
     {
         name: "Recep Kütük",
-        avatar: "/about/images/team-avatars/recep.png",
-        avatar2x: "/about/images/team-avatars/recep2x.png",
+        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/recep.png",
+        avatar2x:
+            "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/recep2x.png",
         role1: "Senior Product Designer",
     },
 ];
