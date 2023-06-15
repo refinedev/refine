3f09c1a31da2097088dd21a879c05a511096cef7
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-06-15 15:39:56 +0300

    feat(examples): add e2e to inferencer-mantine

--- examples/inferencer-mantine/src/App.tsx
+++ examples/inferencer-mantine/src/App.tsx
@@ -1,157 +1,220 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
 import {
-    ThemedLayoutV2,
     ErrorComponent,
     notificationProvider,
     RefineThemes,
+    ThemedLayoutV2,
 } from "@refinedev/mantine";
+
+import {
+    ColorScheme,
+    ColorSchemeProvider,
+    Global,
+    MantineProvider,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
 import { NotificationsProvider } from "@mantine/notifications";
-import { MantineProvider, Global } from "@mantine/styles";
-import { MantineInferencer } from "@refinedev/inferencer/mantine";
-import dataProvider from "@refinedev/simple-rest";
-import routerProvider, {
+import routerBindings, {
+    CatchAllNavigate,
+    DocumentTitleHandler,
     NavigateToResource,
     UnsavedChangesNotifier,
-    DocumentTitleHandler,
 } from "@refinedev/react-router-v6";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import dataProvider from "@refinedev/simple-rest";
+import { useTranslation } from "react-i18next";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { authProvider } from "./authProvider";
+import { Header } from "./components/header";
+import {
+    BlogPostCreate,
+    BlogPostEdit,
+    BlogPostList,
+    BlogPostShow,
+} from "./pages/blog-posts";
+import {
+    CategoryCreate,
+    CategoryEdit,
+    CategoryList,
+    CategoryShow,
+} from "./pages/categories";
+import { ForgotPassword } from "./pages/forgotPassword";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
 
-const App: React.FC = () => {
+function App() {
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: "mantine-color-scheme",
+        defaultValue: "light",
+        getInitialValueInEffect: true,
+    });
+    const { t, i18n } = useTranslation();
+
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+    const i18nProvider = {
+        translate: (key: string, params: object) => t(key, params),
+        changeLocale: (lang: string) => i18n.changeLanguage(lang),
+        getLocale: () => i18n.language,
+    };
+
     return (
         <BrowserRouter>
             <GitHubBanner />
-            <MantineProvider
-                theme={RefineThemes.Blue}
-                withNormalizeCSS
-                withGlobalStyles
-            >
-                <Global styles={{ body: { WebkitFontSmoothing: "auto" } }} />
-                <NotificationsProvider position="top-right">
-                    <Refine
-                        routerProvider={routerProvider}
-                        dataProvider={dataProvider(
-                            "https://api.fake-rest.refine.dev",
-                        )}
-                        notificationProvider={notificationProvider}
-                        resources={[
-                            {
-                                name: "samples",
-                                list: "/samples",
-                                create: "/samples/create",
-                                edit: "/samples/edit/:id",
-                                show: "/samples/show/:id",
-                                meta: {
-                                    canDelete: true,
-                                },
-                            },
-                            {
-                                name: "categories",
-                                list: "/categories",
-                                create: "/categories/create",
-                                edit: "/categories/edit/:id",
-                                show: "/categories/show/:id",
-                                meta: {
-                                    canDelete: true,
-                                },
-                            },
-                            {
-                                name: "users",
-                                list: "/users",
-                                create: "/users/create",
-                                edit: "/users/edit/:id",
-                                show: "/users/show/:id",
-                                meta: {
-                                    canDelete: true,
-                                },
-                            },
-                        ]}
-                        options={{
-                            syncWithLocation: true,
-                            warnWhenUnsavedChanges: true,
+            <RefineKbarProvider>
+                <ColorSchemeProvider
+                    colorScheme={colorScheme}
+                    toggleColorScheme={toggleColorScheme}
+                >
+                    {/* You can change the theme colors here. example: theme={{ ...RefineThemes.Magenta, colorScheme:colorScheme }} */}
+                    <MantineProvider
+                        theme={{
+                            ...RefineThemes.Blue,
+                            colorScheme: colorScheme,
                         }}
+                        withNormalizeCSS
+                        withGlobalStyles
                     >
-                        <Routes>
-                            <Route
-                                element={
-                                    <ThemedLayoutV2>
-                                        <Outlet />
-                                    </ThemedLayoutV2>
-                                }
+                        <Global
+                            styles={{ body: { WebkitFontSmoothing: "auto" } }}
+                        />
+                        <NotificationsProvider position="top-right">
+                            <Refine
+                                dataProvider={dataProvider(
+                                    "https://api.fake-rest.refine.dev",
+                                )}
+                                notificationProvider={notificationProvider}
+                                routerProvider={routerBindings}
+                                authProvider={authProvider}
+                                i18nProvider={i18nProvider}
+                                resources={[
+                                    {
+                                        name: "blog_posts",
+                                        list: "/blog-posts",
+                                        create: "/blog-posts/create",
+                                        edit: "/blog-posts/edit/:id",
+                                        show: "/blog-posts/show/:id",
+                                        meta: {
+                                            canDelete: true,
+                                        },
+                                    },
+                                    {
+                                        name: "categories",
+                                        list: "/categories",
+                                        create: "/categories/create",
+                                        edit: "/categories/edit/:id",
+                                        show: "/categories/show/:id",
+                                        meta: {
+                                            canDelete: true,
+                                        },
+                                    },
+                                ]}
+                                options={{
+                                    syncWithLocation: true,
+                                    warnWhenUnsavedChanges: true,
+                                }}
                             >
-                                <Route
-                                    index
-                                    element={
-                                        <NavigateToResource resource="samples" />
-                                    }
-                                />
+                                <Routes>
+                                    <Route
+                                        element={
+                                            <Authenticated
+                                                fallback={
+                                                    <CatchAllNavigate to="/login" />
+                                                }
+                                            >
+                                                <ThemedLayoutV2
+                                                    Header={() => (
+                                                        <Header sticky />
+                                                    )}
+                                                >
+                                                    <Outlet />
+                                                </ThemedLayoutV2>
+                                            </Authenticated>
+                                        }
+                                    >
+                                        <Route
+                                            index
+                                            element={
+                                                <NavigateToResource resource="blog_posts" />
+                                            }
+                                        />
+                                        <Route path="/blog-posts">
+                                            <Route
+                                                index
+                                                element={<BlogPostList />}
+                                            />
+                                            <Route
+                                                path="create"
+                                                element={<BlogPostCreate />}
+                                            />
+                                            <Route
+                                                path="edit/:id"
+                                                element={<BlogPostEdit />}
+                                            />
+                                            <Route
+                                                path="show/:id"
+                                                element={<BlogPostShow />}
+                                            />
+                                        </Route>
+                                        <Route path="/categories">
+                                            <Route
+                                                index
+                                                element={<CategoryList />}
+                                            />
+                                            <Route
+                                                path="create"
+                                                element={<CategoryCreate />}
+                                            />
+                                            <Route
+                                                path="edit/:id"
+                                                element={<CategoryEdit />}
+                                            />
+                                            <Route
+                                                path="show/:id"
+                                                element={<CategoryShow />}
+                                            />
+                                        </Route>
+                                        <Route
+                                            path="*"
+                                            element={<ErrorComponent />}
+                                        />
+                                    </Route>
+                                    <Route
+                                        element={
+                                            <Authenticated
+                                                fallback={<Outlet />}
+                                            >
+                                                <NavigateToResource />
+                                            </Authenticated>
+                                        }
+                                    >
+                                        <Route
+                                            path="/login"
+                                            element={<Login />}
+                                        />
+                                        <Route
+                                            path="/register"
+                                            element={<Register />}
+                                        />
+                                        <Route
+                                            path="/forgot-password"
+                                            element={<ForgotPassword />}
+                                        />
+                                    </Route>
+                                </Routes>
 
-                                <Route path="samples">
-                                    <Route
-                                        index
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="create"
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="edit/:id"
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="show/:id"
-                                        element={<MantineInferencer />}
-                                    />
-                                </Route>
-
-                                <Route path="categories">
-                                    <Route
-                                        index
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="create"
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="edit/:id"
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="show/:id"
-                                        element={<MantineInferencer />}
-                                    />
-                                </Route>
-
-                                <Route path="users">
-                                    <Route
-                                        index
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="create"
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="edit/:id"
-                                        element={<MantineInferencer />}
-                                    />
-                                    <Route
-                                        path="show/:id"
-                                        element={<MantineInferencer />}
-                                    />
-                                </Route>
-
-                                <Route path="*" element={<ErrorComponent />} />
-                            </Route>
-                        </Routes>
-                        <UnsavedChangesNotifier />
-                        <DocumentTitleHandler />
-                    </Refine>
-                </NotificationsProvider>
-            </MantineProvider>
+                                <RefineKbar />
+                                <UnsavedChangesNotifier />
+                                <DocumentTitleHandler />
+                            </Refine>
+                        </NotificationsProvider>
+                    </MantineProvider>
+                </ColorSchemeProvider>
+            </RefineKbarProvider>
         </BrowserRouter>
     );
-};
+}
 
 export default App;
