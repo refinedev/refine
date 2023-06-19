339d5a43096dda134001b87b2160aaf19098ccd9
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-06-19 20:03:33 +0300

    fix(auth-material-ui): type issues

--- examples/auth-material-ui/src/pages/posts/create.tsx
+++ examples/auth-material-ui/src/pages/posts/create.tsx
@@ -7,7 +7,7 @@ import { useForm } from "@refinedev/react-hook-form";
 
 import { Controller } from "react-hook-form";
 
-import { IPost, ICategory } from "interfaces";
+import { IPost, ICategory, IStatus, Nullable } from "interfaces";
 
 export const PostCreate: React.FC = () => {
     const {
@@ -15,7 +15,7 @@ export const PostCreate: React.FC = () => {
         register,
         control,
         formState: { errors },
-    } = useForm<IPost, HttpError, IPost & { category: ICategory }>();
+    } = useForm<IPost, HttpError, Nullable<IPost>>();
 
     const { autocompleteProps } = useAutocomplete<ICategory>({
         resource: "categories",
@@ -47,7 +47,7 @@ export const PostCreate: React.FC = () => {
                     // eslint-disable-next-line
                     defaultValue={null as any}
                     render={({ field }) => (
-                        <Autocomplete
+                        <Autocomplete<IStatus>
                             options={["published", "draft", "rejected"]}
                             {...field}
                             onChange={(_, value) => {
