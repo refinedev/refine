ce993e047ad6978fb6137597174fbf493fd296dd
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-06-23 12:47:38 +0300

    feat(chakra-ui): add `AutoSaveIndicator` component

---
+++ packages/chakra-ui/src/components/autoSaveIndicator/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { AutoSaveIndicatorProps } from "@refinedev/core";
+
+export const AutoSaveIndicator: React.FC<AutoSaveIndicatorProps> = ({
+    status,
+}) => {
+    let message = null;
+
+    switch (status) {
+        case "success":
+            message = "saved";
+            break;
+        case "error":
+            message = "autosave error";
+            break;
+        case "loading":
+            message = "saving...";
+            break;
+        default:
+            // for idle
+            message = "waiting for changes";
+            break;
+    }
+    return <span>{message}</span>;
+};
