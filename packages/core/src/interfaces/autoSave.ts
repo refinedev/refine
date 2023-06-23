6a43863db493fb0a96142997f06c044274440ab0
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-06-23 09:33:23 +0300

    fix(core): autoSave interface

---
+++ packages/core/src/interfaces/autoSave.ts
@@ -0,0 +1,31 @@
+import { BaseRecord, UpdateResponse } from ".";
+import { MutationStatus } from "@tanstack/react-query";
+
+export type AutoSaveProps<TResponse, TResponseError, TVariables> = {
+    autoSave?: boolean;
+    onAutoSaveSuccess?: (
+        data: UpdateResponse<TResponse>,
+        variables: TVariables,
+        context: any,
+    ) => void;
+    onAutoSaveError?: (
+        error: TResponseError,
+        variables: TVariables,
+        context: any,
+    ) => void;
+};
+
+export type AutoSaveReturnType<
+    TData extends BaseRecord = BaseRecord,
+    // TError extends HttpError = HttpError,
+    TVariables = {},
+> = {
+    autoSaveProps: AutoSaveIndicatorProps;
+    onFinishAutoSave: (
+        values: TVariables,
+    ) => Promise<UpdateResponse<TData> | void> | void;
+};
+
+export type AutoSaveIndicatorProps = {
+    status: MutationStatus;
+}; // UseUpdateReturnType<TData, TError, TVariables>;
