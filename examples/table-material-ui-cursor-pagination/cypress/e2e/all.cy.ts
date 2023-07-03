baca31e02d5ad15983005571abe96db352ca8583
Salih <salihozdemir94@gmail.com>
2023-07-03 20:44:10 +0300

    fix: update the used port

--- examples/table-material-ui-cursor-pagination/cypress/e2e/all.cy.ts
+++ examples/table-material-ui-cursor-pagination/cypress/e2e/all.cy.ts
@@ -3,7 +3,7 @@
 
 describe("table-material-ui-cursor-pagination", () => {
     beforeEach(() => {
-        cy.visit("http://localhost:3000");
+        cy.visit("http://localhost:5173");
     });
 
     it("should work with pagination", () => {
