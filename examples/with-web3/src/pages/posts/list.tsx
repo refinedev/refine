d925cd9b014f75183ea1d80547410e0debb25714
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-07-11 11:21:39 +0300

    fix: `with-web3`

--- examples/with-web3/src/pages/posts/list.tsx
+++ examples/with-web3/src/pages/posts/list.tsx
@@ -16,7 +16,7 @@ import {
 
 import { Table, Space, Select } from "antd";
 
-import { IPost, ICategory } from "interfaces";
+import { IPost, ICategory } from "../../interfaces";
 
 export const PostList: React.FC<IResourceComponentsProps> = () => {
     const { tableProps, sorter } = useTable<IPost>({
