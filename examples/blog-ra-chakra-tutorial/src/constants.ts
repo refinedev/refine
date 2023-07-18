220fc70c60595d0bd2445f6843f7d9f5521128c7
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-07-18 16:57:13 +0300

    chore: remove `blog-ra-chakra-tutorial` old example

--- examples/blog-ra-chakra-tutorial/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const API_URL = "https://api.strapi-v4.refine.dev";
-export const TOKEN_KEY = "strapi-jwt-token";
--- examples/blog-ra-chakra-tutorial/src/interfaces/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IPost {
-    id: number;
-    title: string;
-    content: string;
-    category: any;
-    publishedAt: string;
-    createdAt: string;
-}
