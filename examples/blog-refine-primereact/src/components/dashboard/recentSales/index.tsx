75ac7b4549c1f7a590ce97494ed4611d6450eddb
Salih <salihozdemir94@gmail.com>
2023-07-24 16:04:58 +0300

    feat: add the blog example

---
+++ examples/blog-refine-primereact/src/components/dashboard/recentSales/index.tsx
@@ -0,0 +1,192 @@
+import { useTable, getDefaultFilter } from "@refinedev/core";
+
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Card } from "primereact/card";
+import { Tag } from "primereact/tag";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+
+import { IOrder, IOrderStatus } from "../../../interfaces";
+
+export const RecentSales = () => {
+    const {
+        tableQueryResult,
+        pageCount,
+        current,
+        pageSize,
+        sorters,
+        filters,
+        setCurrent,
+        setPageSize,
+        setSorters,
+        setFilters,
+    } = useTable<IOrder>({
+        resource: "orders",
+        pagination: {
+            pageSize: 5,
+        },
+    });
+
+    const orders = tableQueryResult?.data?.data;
+
+    const formatCurrency = (value: number) => {
+        return value.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+        });
+    };
+
+    const formatDateTime = (value: string) => {
+        return new Date(value).toLocaleString("en-US", {
+            dateStyle: "short",
+            timeStyle: "short",
+        });
+    };
+
+    const getSeverity = (status: IOrderStatus["text"]) => {
+        switch (status) {
+            case "Cancelled":
+                return "danger";
+
+            case "Ready":
+                return "success";
+
+            case "On The Way":
+                return "info";
+
+            case "Pending":
+                return "warning";
+
+            case "Delivered":
+                return null;
+        }
+    };
+
+    const amountBodyTemplate = (rowData: IOrder) => {
+        return formatCurrency(rowData.amount);
+    };
+
+    const statusBodyTemplate = (rowData: IOrder) => {
+        return (
+            <Tag
+                value={rowData.status.text}
+                severity={getSeverity(rowData.status.text)}
+            />
+        );
+    };
+
+    const dateBodyTemplate = (rowData: IOrder) => {
+        return formatDateTime(rowData.createdAt);
+    };
+
+    const header = (
+        <div className="flex justify-content-between">
+            <Button
+                type="button"
+                icon="pi pi-filter-slash"
+                label="Clear"
+                outlined
+                onClick={() => {
+                    setCurrent(1);
+                    setFilters([], "replace");
+                }}
+            />
+            <span className="p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText
+                    value={getDefaultFilter("q", filters)}
+                    onChange={(e) => {
+                        setCurrent(1);
+                        setFilters([
+                            {
+                                field: "q",
+                                value: e.target.value,
+                                operator: "contains",
+                            },
+                        ]);
+                    }}
+                    placeholder="Keyword Search"
+                />
+            </span>
+        </div>
+    );
+
+    return (
+        <Card className="shadow-1" title="Recent Sales">
+            <DataTable
+                value={orders}
+                dataKey="id"
+                lazy
+                paginator
+                rows={pageSize}
+                rowsPerPageOptions={[5, 10, 25, 50]}
+                first={current * pageSize - pageSize}
+                totalRecords={pageCount * pageSize}
+                onPage={(event) => {
+                    setCurrent((event.page ?? 0) + 1);
+                    setPageSize(event.rows);
+                }}
+                onSort={(event) => {
+                    setSorters([
+                        {
+                            field: event.sortField,
+                            order: event.sortOrder === 1 ? "asc" : "desc",
+                        },
+                    ]);
+                }}
+                sortField={sorters[0]?.field}
+                sortOrder={sorters[0]?.order === "asc" ? 1 : -1}
+                loading={tableQueryResult?.isLoading}
+                header={header}
+            >
+                <Column
+                    field="id"
+                    header="Id"
+                    sortable
+                    style={{ minWidth: "2rem" }}
+                />
+                <Column
+                    field="amount"
+                    header="Amount"
+                    body={amountBodyTemplate}
+                    sortable
+                />
+                <Column
+                    field="user.fullName"
+                    header="Ordered By"
+                    style={{ minWidth: "10rem" }}
+                    sortable
+                />
+                <Column field="user.gender" header="Gender" sortable />
+                <Column
+                    field="user.gsm"
+                    header="Tel"
+                    align="center"
+                    style={{ minWidth: "12rem" }}
+                />
+                <Column
+                    field="adress.text"
+                    header="Delivery Address"
+                    style={{ minWidth: "16rem" }}
+                />
+                <Column
+                    field="status.text"
+                    header="Delivery Status"
+                    body={statusBodyTemplate}
+                    align="center"
+                    sortable
+                    style={{ minWidth: "10rem" }}
+                />
+                <Column
+                    field="createdAt"
+                    header="Created At"
+                    body={dateBodyTemplate}
+                    align="center"
+                    sortable
+                    style={{ minWidth: "12rem" }}
+                />
+            </DataTable>
+        </Card>
+    );
+};
---
+++ examples/blog-refine-primereact/src/pages/dashboard/index.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useApiUrl, useCustom } from "@refinedev/core";
+import dayjs from "dayjs";
+
+import { KpiCard } from "../../components/dashboard/kpiCard";
+import { ChartView } from "../../components/dashboard/chartView";
+import { RecentSales } from "../../components/dashboard/recentSales";
+
+import { IChart } from "../../interfaces";
+
+const query = {
+    start: dayjs().subtract(7, "days").startOf("day"),
+    end: dayjs().startOf("day"),
+};
+
+export const Dashboard: React.FC = () => {
+    const API_URL = useApiUrl();
+
+    const { data: dailyRevenue } = useCustom<IChart>({
+        url: `${API_URL}/dailyRevenue`,
+        method: "get",
+        config: {
+            query,
+        },
+    });
+
+    const { data: dailyOrders } = useCustom<IChart>({
+        url: `${API_URL}/dailyOrders`,
+        method: "get",
+        config: {
+            query,
+        },
+    });
+
+    const { data: newCustomers } = useCustom<IChart>({
+        url: `${API_URL}/newCustomers`,
+        method: "get",
+        config: {
+            query,
+        },
+    });
+
+    return (
+        <div className="grid">
+            <div className="col-12 lg:col-6 xl:col-4">
+                <KpiCard
+                    title="Weekly Revenue"
+                    color="blue-300"
+                    total={dailyRevenue?.data.total ?? 0}
+                    trend={dailyRevenue?.data.trend ?? 0}
+                    formatTotal={(value) => `$ ${value}`}
+                    icon="pi-dollar"
+                />
+            </div>
+            <div className="col-12 lg:col-6 xl:col-4">
+                <KpiCard
+                    title="Weekly Orders"
+                    color="orange-300"
+                    total={dailyOrders?.data.total ?? 0}
+                    trend={dailyOrders?.data.trend ?? 0}
+                    icon="pi-shopping-cart"
+                />
+            </div>
+            <div className="col-12 lg:col-6 xl:col-4">
+                <KpiCard
+                    title="New Customers"
+                    color="green-500"
+                    total={newCustomers?.data.total ?? 0}
+                    trend={newCustomers?.data.trend ?? 0}
+                    icon="pi-users"
+                />
+            </div>
+            <div className="col-12">
+                <ChartView
+                    revenue={dailyRevenue?.data.data ?? []}
+                    orders={dailyOrders?.data.data ?? []}
+                    customers={newCustomers?.data.data ?? []}
+                />
+            </div>
+            <div className="col-12">
+                <RecentSales />
+            </div>
+        </div>
+    );
+};
