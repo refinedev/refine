0601e9d9124fc89b0ffb00632dae66a558d18bcb
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-07-24 15:33:11 +0300

    fix: build error

--- examples/access-control-casbin/vite.config.js
+++ examples/access-control-casbin/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-polyfill-node";
 
 export default defineConfig({
     plugins: [react()],
@@ -14,8 +17,15 @@ export default defineConfig({
             plugins: [
                 NodeGlobalsPolyfillPlugin({
                     buffer: true,
+                    process: true,
                 }),
+                NodeModulesPolyfillPlugin(),
             ],
+        },
+    },
+    build: {
+        rollupOptions: {
+            plugins: [rollupNodePolyFill()],
         },
     },
 });
