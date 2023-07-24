f7ed00793150e0317c0e5babfaef0a800950c021
Özgür Akça <doguhanozgurakca@gmail.com>
2023-07-24 14:24:49 +0300

    docs: antd hooks revision (#4676)
    * docs: antd-hooks revision
    
    * docs: reversal of accidental changes

--- documentation/src/components/general-concepts-link/index.tsx
+++ documentation/src/components/general-concepts-link/index.tsx
@@ -1,10 +1,11 @@
+import Admonition from "@theme/Admonition";
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import Admonition from "@theme/Admonition";
 
 const md = `
 All the data related hooks(useTable, useForm, useList etc.) of refine can be given some common properties like \`resource\`, \`meta\` etc.
-> Please refer to [General Concepts](/docs/api-reference/general-concepts) for more information.
+
+For more information, refer to the [General Concepts documentation&#8594](/docs/api-reference/general-concepts).
 `;
 
 const GeneralConceptsLink = () => {
