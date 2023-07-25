7954f2fc624b1af06f2dcf4c0c9f26ef5b0f55db
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-07-25 15:16:53 +0300

    chore(examples): remove supabase key

--- examples/blog-refine-supabase-auth/src/utility/supabaseClient.ts
+++ examples/blog-refine-supabase-auth/src/utility/supabaseClient.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@refinedev/supabase";
 
-// QBMZ87LcFNeiWFw6
-
-const SUPABASE_URL = "https://ezeyemegrlmndvfbzmut.supabase.co";
-const SUPABASE_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6ZXllbWVncmxtbmR2ZmJ6bXV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAyNjc4NDYsImV4cCI6MjAwNTg0Mzg0Nn0.gfNeWZ0__3rGkBbx1vPZGce3OIRfkhJjPMy2SbxKKoc";
+const SUPABASE_URL = "YOUR SUPABASE URL";
+const SUPABASE_KEY = "YOUR SUPABASE KEY";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
     db: {
