7c3c435af05097fc55ba5d0c9a049cd0cce5e207
Salih Özdemir <salihozdemir94@gmail.com>
2023-07-28 11:57:24 +0300

    refactor(blog): update the Material UI DataGrid blog post (#4735)
    * chore: update cra to vite
    
    * fix: update the example
    
    * docs: update the blog post
    
    * chore: fix typo
    
    * fix: update documentation/blog/2022-08-23-mui-usedatagrid.md
    
    * chore: remove links in we'll cover section

--- examples/blog-material-ui-datagrid/src/App.tsx
+++ examples/blog-material-ui-datagrid/src/App.tsx
@@ -1,62 +1,75 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
-import { ErrorComponent, RefineThemes } from "@refinedev/mui";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import GlobalStyles from "@mui/material/GlobalStyles";
-import routerProvider, {
+import { Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { notificationProvider, RefineSnackbarProvider } from "@refinedev/mui";
+import routerBindings, {
+    DocumentTitleHandler,
     NavigateToResource,
     UnsavedChangesNotifier,
-    DocumentTitleHandler,
 } from "@refinedev/react-router-v6";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import dataProvider from "@refinedev/simple-rest";
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 
-import Layout from "./components/Layout";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+import Layout from "./components/layout";
 import EmployeeList from "./pages/employees";
 
 function App() {
     return (
         <BrowserRouter>
-            <GitHubBanner />
-            <ThemeProvider theme={RefineThemes.Blue}>
-                <CssBaseline />
-                <GlobalStyles
-                    styles={{ html: { WebkitFontSmoothing: "auto" } }}
-                />
-                <Refine
-                    routerProvider={routerProvider}
-                    dataProvider={dataProvider(
-                        "https://my-json-server.typicode.com/Mich45/employee-data",
-                    )}
-                    resources={[{ name: "employees", list: "/employees" }]}
-                >
-                    <Routes>
-                        <Route
-                            element={
-                                <Layout>
-                                    <Outlet />
-                                </Layout>
-                            }
+            <RefineKbarProvider>
+                <ColorModeContextProvider>
+                    <CssBaseline />
+                    <GlobalStyles
+                        styles={{ html: { WebkitFontSmoothing: "auto" } }}
+                    />
+                    <RefineSnackbarProvider>
+                        <Refine
+                            dataProvider={dataProvider(
+                                "https://my-json-server.typicode.com/Mich45/employee-data",
+                            )}
+                            notificationProvider={notificationProvider}
+                            routerProvider={routerBindings}
+                            resources={[
+                                {
+                                    name: "employees",
+                                    list: "/employees",
+                                },
+                            ]}
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                            }}
                         >
-                            <Route
-                                index
-                                element={
-                                    <NavigateToResource resource="employees" />
-                                }
-                            />
+                            <Routes>
+                                <Route
+                                    element={
+                                        <Layout>
+                                            <Outlet />
+                                        </Layout>
+                                    }
+                                >
+                                    <Route
+                                        index
+                                        element={
+                                            <NavigateToResource resource="employees" />
+                                        }
+                                    />
 
-                            <Route
-                                path="/employees"
-                                element={<EmployeeList />}
-                            />
-
-                            <Route path="*" element={<ErrorComponent />} />
-                        </Route>
-                    </Routes>
-                    <UnsavedChangesNotifier />
-                    <DocumentTitleHandler />
-                </Refine>
-            </ThemeProvider>
+                                    <Route
+                                        path="/employees"
+                                        element={<EmployeeList />}
+                                    />
+                                </Route>
+                            </Routes>
+                            <RefineKbar />
+                            <UnsavedChangesNotifier />
+                            <DocumentTitleHandler />
+                        </Refine>
+                    </RefineSnackbarProvider>
+                </ColorModeContextProvider>
+            </RefineKbarProvider>
         </BrowserRouter>
     );
 }
