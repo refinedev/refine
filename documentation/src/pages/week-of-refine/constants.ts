228c5b7d5f69fcf730a651faee52b1a864f7f45c
Salih <salihozdemir94@gmail.com>
2023-08-02 10:37:21 +0300

    docs: add week of refine landing page

---
+++ documentation/src/pages/week-of-refine/constants.ts
@@ -0,0 +1,27 @@
+import { CardProps } from ".";
+import { StrapiWithText, SupabaseWithText } from "./icons";
+
+export const weekOfRefineCards: CardProps[] = [
+    {
+        title: "Invoicer",
+        imgURL: "https://refine.ams3.cdn.digitaloceanspaces.com/week-of-refine/week-of-refine-invoicer.png",
+        dateRange: "April 10 - April 14, 2023",
+        description:
+            "Stay tuned all week and learn the basics of creating a CRUD app via refine & Strapi on a fun project in a week.",
+        logo: StrapiWithText,
+        bgLinearGradient:
+            "bg-week-of-refine-strapi-card-light dark:bg-week-of-refine-strapi-card",
+        link: "/week-of-refine-strapi",
+    },
+    {
+        title: "Pixels!",
+        imgURL: "https://refine.ams3.cdn.digitaloceanspaces.com/week-of-refine/week-of-refine-pixels.png",
+        dateRange: "February 14 - February 20, 2023",
+        description:
+            "Stay tuned all week and learn the basics of creating a CRUD app via refine & Strapi on a fun project in a week.",
+        logo: SupabaseWithText,
+        bgLinearGradient:
+            "bg-week-of-refine-supabase-card-light dark:bg-week-of-refine-supabase-card",
+        link: "/week-of-refine-supabase",
+    },
+];
---
+++ documentation/src/pages/week-of-refine/icons/index.ts
@@ -0,0 +1,3 @@
+export * from "./date";
+export * from "./strapi";
+export * from "./supabase";
