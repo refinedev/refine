918725e17fe566b9e5819cc580c0aa742dffbd3f
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-08-04 11:52:09 +0300

    fix: updated deleteMany tests

--- packages/hasura/test/deleteMany/index.mock.ts
+++ packages/hasura/test/deleteMany/index.mock.ts
@@ -1,9 +1,5 @@
 import nock from "nock";
 
-/**
- * Hasura default 'snake_case' naming convention
- */
-
 nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
     .post("/v1/graphql", {
         query: "mutation ($where: posts_bool_exp!) {\n      delete_posts (where: $where) {\n    returning { id, title }\n  }\n    }",
@@ -11,8 +7,8 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             where: {
                 id: {
                     _in: [
-                        "cddd4ced-651d-4039-abe0-2a9dffbc8c82",
-                        "c82c71c5-0f0b-4042-b9a3-db977fe28a83",
+                        "9848cac2-80d7-4846-9a73-2f312459929a",
+                        "a5bbd909-9bc5-486d-be07-ece9c17523f9",
                     ],
                 },
             },
@@ -21,11 +17,11 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
     .reply(
         200,
         [
-            "1f8b08000000000004034dccbd0ac2301440e15709773650d3d6a61d05c5dd51446e726f34585bc9cf54f2eed641703c70f816204c08c302c42327bebde798e2b703a71c263fdd410ce2b2802718c012516399e4aedd926caaba9768b8920a7b72ce586db5820d249f465ef7e36c7314f730e7b738e5174ee23c323fa16cc40fd4ca765bdbcaca5566051b254d8fb524d3779d63a551d77fe0419a1cfdc4310a1c31bcc49ec9cd81e2c30786722da57c00e1b7e862d2000000",
+            "1f8b08000000000000034ccc4b0a83301000d0ab84591bd09868c6ab945226c95802d6da7c5692bb17a18b2edfe69d10a8102c2704deb8f0e378e7922f272e35ed717f8258c4ed84186001b4da7af24ada3ecc525b3d49a479946a1d07a50da242820e4a2c1bc3029f4a2f516a3a621614e211b3bfc2d689df47c6b9803d4a74de486da7201df7b364cfe887d9a871c5bf8fabc87444de85af29d70cedde5afb020000ffff0300025a53bac4000000",
         ],
         [
             "Date",
-            "Wed, 30 Nov 2022 12:00:04 GMT",
+            "Fri, 04 Aug 2023 08:58:16 GMT",
             "Content-Type",
             "application/json; charset=utf-8",
             "Transfer-Encoding",
@@ -33,9 +29,7 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Connection",
             "close",
             "x-request-id",
-            "9aabb8e41ed5a35ecec813cd8e871c64",
-            "Content-Encoding",
-            "gzip",
+            "f4f3a142188815f203b241ec952323f3",
             "CF-Cache-Status",
             "DYNAMIC",
             "Content-Security-Policy",
@@ -53,7 +47,9 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Server",
             "cloudflare",
             "CF-RAY",
-            "77236ba6586ebb86-FRA",
+            "7f1598f4ca3492d8-IST",
+            "Content-Encoding",
+            "gzip",
         ],
     );
 
@@ -64,8 +60,8 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             where: {
                 id: {
                     _in: [
-                        "ecd7aa21-19f4-46c9-bc3e-227dcd0807fd",
-                        "88a479ec-296d-48a4-9c57-7b48048a8067",
+                        "54d9f65c-592a-4a0a-9743-4b6761030853",
+                        "b97c56d0-83b1-483f-bbcc-45305c6340a4",
                     ],
                 },
             },
@@ -74,11 +70,11 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
     .reply(
         200,
         [
-            "1f8b08000000000004032dca510ac3200c00d0ab48be17b0ce19f52a63149ba4a330bad1ba2ff1eeed609f0f5e0329b5406e20fad2aae3e7bdd7fde74deb775b97f509269b7b83452083b250296ec021cd1e7de084135f159d2361b1d1d22cd02fe6df632c9e9232ba1404fd294c7c23a4c9477b32da40d01fbdf70364bfacd488000000",
+            "1f8b08000000000000032cca4b0ac2400c00d0ab94ac0da42699df558ac8fc2a05a9d28eab61ee2e82cb07af43892d42e850eab3b67a7fbfce76fe7cd4f639f66d7fc014a6a5c35620804af1abd18ceaaf112552446f855192b1662626a70ce332fd7bf236ab29848ed38ce278c59472465126cd8685a2c0b88d31be000000ffff0300a34cfbeb88000000",
         ],
         [
             "Date",
-            "Wed, 30 Nov 2022 12:00:46 GMT",
+            "Fri, 04 Aug 2023 08:58:17 GMT",
             "Content-Type",
             "application/json; charset=utf-8",
             "Transfer-Encoding",
@@ -86,9 +82,7 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Connection",
             "close",
             "x-request-id",
-            "4ed8219a525a43cc8446341eecaf4ba0",
-            "Content-Encoding",
-            "gzip",
+            "02fbba97893224b22d915be23c59395e",
             "CF-Cache-Status",
             "DYNAMIC",
             "Content-Security-Policy",
@@ -106,23 +100,21 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Server",
             "cloudflare",
             "CF-RAY",
-            "77236cabf9099170-FRA",
+            "7f1598fd9b8f92db-IST",
+            "Content-Encoding",
+            "gzip",
         ],
     );
 
-/**
- * Graphql default 'camelCase' naming convention
- */
-
-nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
+nock("https://ruling-redbird-23.hasura.app:443", { encodedQueryParams: true })
     .post("/v1/graphql", {
         query: "mutation ($where: PostsBoolExp!) {\n      deletePosts (where: $where) {\n    returning { id, title }\n  }\n    }",
         variables: {
             where: {
                 id: {
                     _in: [
-                        "cddd4ced-651d-4039-abe0-2a9dffbc8c82",
-                        "c82c71c5-0f0b-4042-b9a3-db977fe28a83",
+                        "b8e8c884-d169-44ce-9fb1-118256a64181",
+                        "522d566a-a25d-4339-952d-bd95a3fe9f80",
                     ],
                 },
             },
@@ -131,11 +123,11 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
     .reply(
         200,
         [
-            "1f8b08000000000000134dccbd0ac2301440e157097736d0a6d5a61d05c5517014879bdc1b0dd656f23395bcbb75101c0f1cbe050813c2b000f1c889cf734cf19b81530e939fee2006715dc0130c6089a8b54c72b7ad49b655d34b345c49853d3967acb65ac106924f23affb71b6398a7b98f35b9cf20b277119999f5036e2076a65bbda6e65e52ab382ad92a6c74692e9bbceb1d2a89b3ff0204d8e7ee218058e185e62cfe4e640f1e10343b995523e92a9e564d1000000",
+            "1f8b08000000000000034cccb16ec3201000d05f39dd1ca4800181bf227bd5e16cce111121a939bc58fc7b55a943c6b7bc131309e17c62e2c2c2b75793f6c79da5ef35d73bc20c5f27e684332e81c31a825549fba8ac5d59c56dd14aeb609c276f75d07841c95218673cf241cfdee0e02679e9a53fa1d13d8be486e302ffa9332639ef49917149d9698a2a3a93d492a2a369e3b885eb475af9a733347a67aef02eb4f24e025485a1f652081ebdc90bc7f718e3170000ffff030004333c35de000000",
         ],
         [
             "Date",
-            "Wed, 30 Nov 2022 12:00:04 GMT",
+            "Fri, 04 Aug 2023 08:58:19 GMT",
             "Content-Type",
             "application/json; charset=utf-8",
             "Transfer-Encoding",
@@ -143,9 +135,7 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Connection",
             "close",
             "x-request-id",
-            "9aabb8e41ed5a35ecec813cd8e871c64",
-            "Content-Encoding",
-            "gzip",
+            "57b3798dbb5fe61bda0ac0b12f416a3d",
             "CF-Cache-Status",
             "DYNAMIC",
             "Content-Security-Policy",
@@ -163,19 +153,21 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Server",
             "cloudflare",
             "CF-RAY",
-            "77236ba6586ebb86-FRA",
+            "7f1599062921548e-IST",
+            "Content-Encoding",
+            "gzip",
         ],
     );
 
-nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
+nock("https://ruling-redbird-23.hasura.app:443", { encodedQueryParams: true })
     .post("/v1/graphql", {
         query: "mutation ($where: PostsBoolExp!) {\n      deletePosts (where: $where) {\n    returning { id }\n  }\n    }",
         variables: {
             where: {
                 id: {
                     _in: [
-                        "ecd7aa21-19f4-46c9-bc3e-227dcd0807fd",
-                        "88a479ec-296d-48a4-9c57-7b48048a8067",
+                        "f79a1033-62f3-4551-b9b4-4bd40ba4b51b",
+                        "bc86c10a-d807-4196-9fc3-af5619e4228a",
                     ],
                 },
             },
@@ -184,11 +176,11 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
     .reply(
         200,
         [
-            "1f8b08000000000000132dcac10ac3200c00d05f919c17b0ce19f52b761f3dd8241d85d1416b4fe2bfaf851d1fbc06526a81dc40f4a3559fdfbdee1737adc7b62eeb1b4c36af068b400665a152dc80439a3dfac00927be2b3a47c262a3a559a0dfccbfc7583c2565742908fa5398f84148938ff664b481a08fbdf71f0022ca3387000000",
+            "1f8b08000000000000032cca3b0ac3300c00d0ab18cd15f8233b764ed1bd7490fc29819242e24cc6772f858e0fde80c29d611d50eabbf67aff9cfdfcf1a8fd3af66d7f815ad563c0566085b62436da390cb63924ef0d4a124292425a98c41b817953ff2e39866c3463897a413229606ad921371f4caa646d6498cf39e7170000ffff0300e2e9682887000000",
         ],
         [
             "Date",
-            "Wed, 30 Nov 2022 12:00:46 GMT",
+            "Fri, 04 Aug 2023 08:58:19 GMT",
             "Content-Type",
             "application/json; charset=utf-8",
             "Transfer-Encoding",
@@ -196,9 +188,7 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Connection",
             "close",
             "x-request-id",
-            "4ed8219a525a43cc8446341eecaf4ba0",
-            "Content-Encoding",
-            "gzip",
+            "0b215a0b932d25d8b02224f7fb35ae92",
             "CF-Cache-Status",
             "DYNAMIC",
             "Content-Security-Policy",
@@ -216,6 +206,8 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Server",
             "cloudflare",
             "CF-RAY",
-            "77236cabf9099170-FRA",
+            "7f15990f6f4192cf-IST",
+            "Content-Encoding",
+            "gzip",
         ],
     );
