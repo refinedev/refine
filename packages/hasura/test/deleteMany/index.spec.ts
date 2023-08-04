4638644835891f8cc82f100c54504f9acdca8620
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-08-04 12:09:35 +0300

    fix: updated deleteOne tests

--- packages/hasura/test/deleteMany/index.spec.ts
+++ packages/hasura/test/deleteMany/index.spec.ts
@@ -5,19 +5,16 @@ import "./index.mock";
 describe.each(["hasura-default", "graphql-default"] as const)(
     "custom with %s naming convention",
     (namingConvention) => {
-        let withMetaIds: string[] = [];
-        let withoutMetaIds: string[] = [];
+        let withMetaIds = [
+            "9848cac2-80d7-4846-9a73-2f312459929a",
+            "a5bbd909-9bc5-486d-be07-ece9c17523f9",
+        ];
+        let withoutMetaIds = [
+            "54d9f65c-592a-4a0a-9743-4b6761030853",
+            "b97c56d0-83b1-483f-bbcc-45305c6340a4",
+        ];
 
-        if (namingConvention === "hasura-default") {
-            withMetaIds = [
-                "9848cac2-80d7-4846-9a73-2f312459929a",
-                "a5bbd909-9bc5-486d-be07-ece9c17523f9",
-            ];
-            withoutMetaIds = [
-                "54d9f65c-592a-4a0a-9743-4b6761030853",
-                "b97c56d0-83b1-483f-bbcc-45305c6340a4",
-            ];
-        } else {
+        if (namingConvention === "graphql-default") {
             withMetaIds = [
                 "b8e8c884-d169-44ce-9fb1-118256a64181",
                 "522d566a-a25d-4339-952d-bd95a3fe9f80",
--- packages/hasura/test/deleteOne/index.mock.ts
+++ packages/hasura/test/deleteOne/index.mock.ts
@@ -1,22 +1,18 @@
 import nock from "nock";
 
-/**
- * Hasura default 'snake_case' naming convention
- */
-
 nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
     .post("/v1/graphql", {
         query: "mutation ($id: uuid!) {\n      delete_posts_by_pk (id: $id) {\n    id, title\n  }\n    }",
-        variables: { id: "bac2ef0a-899f-4694-84ef-b9c6fe4dc2b7" },
+        variables: { id: "56c5a2cd-3b4d-4465-9d41-67f7991d833c" },
     })
     .reply(
         200,
         [
-            "1f8b080000000000040315cac10a80200c80e177d93921422c7d19d9dc065290e02e11be7b79f82f1fff0b8c86905e60b9c424b7bb5bcff4e4764ead0c0908cb26baa23b6254e743f4eef0a28e62092a9ecb463b2c60d52ef977ecacb38eac30c6f800141bc47064000000",
+            "1f8b080000000000000314c8490ec3200c00c0af209fcb216549e133c860b741258b84738822fe5e758e7303a120c41b881b0ba763efd253bed2f1fd6f2588e07c71f82ca44db6a4adf54e07b293f6f37b0e61a29731051e20551a4384b26f9f9315375e7993735575530b16b560ae82bd338c317e000000ffff03006735476179000000",
         ],
         [
             "Date",
-            "Wed, 30 Nov 2022 11:57:37 GMT",
+            "Fri, 04 Aug 2023 09:06:40 GMT",
             "Content-Type",
             "application/json; charset=utf-8",
             "Transfer-Encoding",
@@ -24,9 +20,7 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Connection",
             "close",
             "x-request-id",
-            "b9552b07bf02a0852ba560fef7fd64be",
-            "Content-Encoding",
-            "gzip",
+            "b0b15af8896af36b82e960766e5ef79f",
             "CF-Cache-Status",
             "DYNAMIC",
             "Content-Security-Policy",
@@ -44,23 +38,25 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Server",
             "cloudflare",
             "CF-RAY",
-            "7723680ded00bb9b-FRA",
+            "7f15a543cabe92db-IST",
+            "Content-Encoding",
+            "gzip",
         ],
     );
 
 nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
     .post("/v1/graphql", {
         query: "mutation ($id: uuid!) {\n      delete_posts_by_pk (id: $id) {\n    id\n  }\n    }",
-        variables: { id: "b19c9bd4-dff9-4ecf-acfe-aea2c4c9ec41" },
+        variables: { id: "312b993d-9648-4a15-aa92-11e7b77e0071" },
     })
     .reply(
         200,
         [
-            "1f8b080000000000040315c83b0ac0200c00d0bb64cf60c9122f13623e505aa8a04b11ef5efac6b7c0752ad4051e77cc90fe8c39a4bdd2af7f4f870aadb0717342cf64a4b044b50cd4d0c3c8388c0aecbd3f84a6ff974d000000",
+            "1f8b080000000000000314c8c10a80200c00d07fd97de0d4b2f93332d90e5190a09710ff3d7ac737416508e4096ab70d2bede9a397fa9676fd7b2a6408e42b7350e43d1e18853614618f44966a4ae65c22586b7d000000ffff0300a1517d4d4d000000",
         ],
         [
             "Date",
-            "Wed, 30 Nov 2022 11:58:17 GMT",
+            "Fri, 04 Aug 2023 09:06:42 GMT",
             "Content-Type",
             "application/json; charset=utf-8",
             "Transfer-Encoding",
@@ -68,9 +64,7 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Connection",
             "close",
             "x-request-id",
-            "9d3d79109c7d732bb1529b768af1fa06",
-            "Content-Encoding",
-            "gzip",
+            "ce1f33b237188aecaa66efae407b814d",
             "CF-Cache-Status",
             "DYNAMIC",
             "Content-Security-Policy",
@@ -88,7 +82,97 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "Server",
             "cloudflare",
             "CF-RAY",
-            "7723690bafb49170-FRA",
+            "7f15a54c880492cb-IST",
+            "Content-Encoding",
+            "gzip",
+        ],
+    );
+
+nock("https://ruling-redbird-23.hasura.app:443", { encodedQueryParams: true })
+    .post("/v1/graphql", {
+        query: "mutation ($id: uuid!) {\n      deletePostsByPk (id: $id) {\n    id, title\n  }\n    }",
+        variables: { id: "bc7025dc-f5d6-414a-b335-20b58a451af8" },
+    })
+    .reply(
+        200,
+        [
+            "1f8b080000000000000314c84d0a84300c06d0ab0cdf7a0a551b47ba9c137885d4a450747ed0b810e9ddc5b77c27848d114f882e6a3afe36dbdec738df550411697af996647299a477a109ec52d7916b7da28103359c073c61c51645c4be7ef9f15fd5cafe41adf5020000ffff03008fd491cb61000000",
+        ],
+        [
+            "Date",
+            "Fri, 04 Aug 2023 09:06:43 GMT",
+            "Content-Type",
+            "application/json; charset=utf-8",
+            "Transfer-Encoding",
+            "chunked",
+            "Connection",
+            "close",
+            "x-request-id",
+            "5fb67bc09b055fa4bf40c3da04d56563",
+            "CF-Cache-Status",
+            "DYNAMIC",
+            "Content-Security-Policy",
+            "upgrade-insecure-requests",
+            "Referrer-Policy",
+            "strict-origin-when-cross-origin",
+            "Strict-Transport-Security",
+            "max-age=31536000; includeSubDomains",
+            "X-Content-Type-Options",
+            "nosniff",
+            "X-Frame-Options",
+            "SAMEORIGIN",
+            "X-XSS-Protection",
+            "0",
+            "Server",
+            "cloudflare",
+            "CF-RAY",
+            "7f15a555e86892cc-IST",
+            "Content-Encoding",
+            "gzip",
+        ],
+    );
+
+nock("https://ruling-redbird-23.hasura.app:443", { encodedQueryParams: true })
+    .post("/v1/graphql", {
+        query: "mutation ($id: uuid!) {\n      deletePostsByPk (id: $id) {\n    id\n  }\n    }",
+        variables: { id: "f39fbacb-c0e8-4fa0-97d9-833f15866ab7" },
+    })
+    .reply(
+        200,
+        [
+            "1f8b0800000000000003aa564a492c4954b2aa564a49cd492d490dc82f2e2976aa0cc8060965a6285929a5195ba625252627e9261ba45ae89aa4251ae85a9aa758ea5a181ba7199a5a98992526992bd5d6d602000000ffff0300563f09524a000000",
+        ],
+        [
+            "Date",
+            "Fri, 04 Aug 2023 09:06:44 GMT",
+            "Content-Type",
+            "application/json; charset=utf-8",
+            "Transfer-Encoding",
+            "chunked",
+            "Connection",
+            "close",
+            "x-request-id",
+            "8c1383247f0220ef03f4e21bde7748d8",
+            "CF-Cache-Status",
+            "DYNAMIC",
+            "Content-Security-Policy",
+            "upgrade-insecure-requests",
+            "Referrer-Policy",
+            "strict-origin-when-cross-origin",
+            "Strict-Transport-Security",
+            "max-age=31536000; includeSubDomains",
+            "X-Content-Type-Options",
+            "nosniff",
+            "X-Frame-Options",
+            "SAMEORIGIN",
+            "X-XSS-Protection",
+            "0",
+            "Server",
+            "cloudflare",
+            "CF-RAY",
+            "7f15a5601ff65487-IST",
+            "Content-Encoding",
+            "gzip",
         ],
     );
 
@@ -135,100 +219,7 @@ nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
             "7723690bafb49170-FRA",
         ],
     );
-
-/**
- * Graphql default 'camelCase' naming convention
- */
-
-nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
-    .post("/v1/graphql", {
-        query: "mutation ($id: uuid!) {\n      deletePostsByPk (id: $id) {\n    id, title\n  }\n    }",
-        variables: { id: "bac2ef0a-899f-4694-84ef-b9c6fe4dc2b7" },
-    })
-    .reply(
-        200,
-        [
-            "1f8b080000000000001315ca310ac0200c40d1bb64ae50445a75ec09bc42340948850e6629e2dd5b87bf3cfe0042458803881b2ba7a76bbfde742faa04113216cbb2a3f12188714770c63b16934339841d159b4fd840ab36fe77ec24ab8e2430e7fc009d078ee661000000",
-        ],
-        [
-            "Date",
-            "Wed, 30 Nov 2022 11:57:37 GMT",
-            "Content-Type",
-            "application/json; charset=utf-8",
-            "Transfer-Encoding",
-            "chunked",
-            "Connection",
-            "close",
-            "x-request-id",
-            "b9552b07bf02a0852ba560fef7fd64be",
-            "Content-Encoding",
-            "gzip",
-            "CF-Cache-Status",
-            "DYNAMIC",
-            "Content-Security-Policy",
-            "upgrade-insecure-requests",
-            "Referrer-Policy",
-            "strict-origin-when-cross-origin",
-            "Strict-Transport-Security",
-            "max-age=31536000; includeSubDomains",
-            "X-Content-Type-Options",
-            "nosniff",
-            "X-Frame-Options",
-            "SAMEORIGIN",
-            "X-XSS-Protection",
-            "0",
-            "Server",
-            "cloudflare",
-            "CF-RAY",
-            "7723680ded00bb9b-FRA",
-        ],
-    );
-
-nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
-    .post("/v1/graphql", {
-        query: "mutation ($id: uuid!) {\n      deletePostsByPk (id: $id) {\n    id\n  }\n    }",
-        variables: { id: "b19c9bd4-dff9-4ecf-acfe-aea2c4c9ec41" },
-    })
-    .reply(
-        200,
-        [
-            "1f8b080000000000001315c8310a80300c05d0bb64cf50c912474fd02ba4c90f888243bb48e9ddc537be4961c3689f14b831509f3efaf1d6ebaf3368a756d4b58570642a0b3cd93cc106db5c5ce15268adf5017fe51a484a000000",
-        ],
-        [
-            "Date",
-            "Wed, 30 Nov 2022 11:58:17 GMT",
-            "Content-Type",
-            "application/json; charset=utf-8",
-            "Transfer-Encoding",
-            "chunked",
-            "Connection",
-            "close",
-            "x-request-id",
-            "9d3d79109c7d732bb1529b768af1fa06",
-            "Content-Encoding",
-            "gzip",
-            "CF-Cache-Status",
-            "DYNAMIC",
-            "Content-Security-Policy",
-            "upgrade-insecure-requests",
-            "Referrer-Policy",
-            "strict-origin-when-cross-origin",
-            "Strict-Transport-Security",
-            "max-age=31536000; includeSubDomains",
-            "X-Content-Type-Options",
-            "nosniff",
-            "X-Frame-Options",
-            "SAMEORIGIN",
-            "X-XSS-Protection",
-            "0",
-            "Server",
-            "cloudflare",
-            "CF-RAY",
-            "7723690bafb49170-FRA",
-        ],
-    );
-
-nock("https://flowing-mammal-24.hasura.app:443", { encodedQueryParams: true })
+nock("https://ruling-redbird-23.hasura.app:443", { encodedQueryParams: true })
     .post("/v1/graphql", {
         query: "mutation ($id: Int!) {\n      deleteUsersByPk (id: $id) {\n    id, name\n  }\n    }",
         variables: { id: 1 },
