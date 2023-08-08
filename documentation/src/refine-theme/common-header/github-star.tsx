ada97f22ae39a92b82ef54b4dcffb98b41385115
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-08-08 16:14:11 +0300

    feat(docs): standardizing github star count (#4786)
    * feat(docs): standardizing github star count
    
    * chore(docs): use intl instead of toFixed

--- documentation/src/refine-theme/common-header/github-star.tsx
+++ documentation/src/refine-theme/common-header/github-star.tsx
@@ -1,24 +1,17 @@
 import Link from "@docusaurus/Link";
 import clsx from "clsx";
-import React, { useMemo } from "react";
+import React from "react";
 
 import { useCommunityStatsContext } from "@site/src/context/CommunityStats";
 import { HeaderGithubIcon } from "../icons/header-github";
+import { Spinner } from "../spinner";
 
 type GitHubStarProps = {
     isPermanentDark?: boolean;
 };
 
 export const GitHubStar: React.FC<GitHubStarProps> = ({ isPermanentDark }) => {
-    const { githubStarCount, loading } = useCommunityStatsContext();
-
-    const formattedStarCount = useMemo(() => {
-        if (loading || !githubStarCount) return <div className="w-5 h-5" />;
-
-        return new Intl.NumberFormat("en", {
-            notation: "compact",
-        }).format(githubStarCount);
-    }, [githubStarCount, loading]);
+    const { githubStarCountText, loading } = useCommunityStatsContext();
 
     return (
         <Link
@@ -31,16 +24,27 @@ export const GitHubStar: React.FC<GitHubStarProps> = ({ isPermanentDark }) => {
                     isPermanentDark && "!text-white",
                 )}
             />
-            <span
+            <div
                 className={clsx(
+                    "flex items-center",
+                    "w-10 h-6",
                     "ml-2",
                     "text-sm font-medium ",
                     "text-gray-500 dark:text-gray-400",
                     isPermanentDark && "!text-white",
                 )}
             >
-                {formattedStarCount}
-            </span>
+                {loading ? (
+                    <Spinner
+                        className={clsx("w-5 h-5")}
+                        wrapperProps={{
+                            className: clsx("mx-auto"),
+                        }}
+                    />
+                ) : (
+                    <span className="tabular-nums">{githubStarCountText}</span>
+                )}
+            </div>
         </Link>
     );
 };
