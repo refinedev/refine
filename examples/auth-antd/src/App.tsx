82e2b33c319e83cb9dd5249f982f3fcd7a52153c
Ali Emir Şen <senaliemir@gmail.com>
2023-08-09 17:20:14 +0300

    docs(examples): add `other-examples` scrollable id

--- examples/auth-antd/src/App.tsx
+++ examples/auth-antd/src/App.tsx
@@ -25,9 +25,9 @@ import routerProvider, {
     DocumentTitleHandler,
 } from "@refinedev/react-router-v6";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import { ConfigProvider } from "antd";
+import { App as AntdApp, ConfigProvider } from "antd";
 
-import "@refinedev/antd/dist/reset.css";
+// import "@refinedev/antd/dist/reset.css";
 
 import { PostList, PostEdit, PostShow } from "../src/pages/posts";
 import { DashboardPage } from "../src/pages/dashboard";
@@ -160,158 +160,168 @@ const App: React.FC = () => {
         <BrowserRouter>
             <GitHubBanner />
             <ConfigProvider theme={RefineThemes.Blue}>
-                <Refine
-                    authProvider={authProvider}
-                    dataProvider={dataProvider(API_URL)}
-                    routerProvider={routerProvider}
-                    resources={[
-                        {
-                            name: "dashboard",
-                            list: "/",
-                            meta: {
-                                label: "Dashboard",
-                                icon: <DashboardOutlined />,
+                <AntdApp>
+                    <Refine
+                        authProvider={authProvider}
+                        dataProvider={dataProvider(API_URL)}
+                        routerProvider={routerProvider}
+                        resources={[
+                            {
+                                name: "dashboard",
+                                list: "/",
+                                meta: {
+                                    label: "Dashboard",
+                                    icon: <DashboardOutlined />,
+                                },
                             },
-                        },
-                        {
-                            name: "posts",
-                            list: "/posts",
-                            show: "/posts/show/:id",
-                            edit: "/posts/edit/:id",
-                        },
-                    ]}
-                    notificationProvider={notificationProvider}
-                    options={{
-                        syncWithLocation: true,
-                        warnWhenUnsavedChanges: true,
-                    }}
-                >
-                    <Routes>
-                        <Route
-                            element={
-                                <Authenticated
-                                    fallback={<CatchAllNavigate to="/login" />}
-                                >
-                                    <ThemedLayoutV2>
-                                        <Outlet />
-                                    </ThemedLayoutV2>
-                                </Authenticated>
-                            }
-                        >
-                            <Route index element={<DashboardPage />} />
+                            {
+                                name: "posts",
+                                list: "/posts",
+                                show: "/posts/show/:id",
+                                edit: "/posts/edit/:id",
+                            },
+                        ]}
+                        notificationProvider={notificationProvider}
+                        options={{
+                            syncWithLocation: true,
+                            warnWhenUnsavedChanges: true,
+                        }}
+                    >
+                        <Routes>
+                            <Route
+                                element={
+                                    <Authenticated
+                                        fallback={
+                                            <CatchAllNavigate to="/login" />
+                                        }
+                                    >
+                                        <ThemedLayoutV2>
+                                            <Outlet />
+                                        </ThemedLayoutV2>
+                                    </Authenticated>
+                                }
+                            >
+                                <Route index element={<DashboardPage />} />
 
-                            <Route path="/posts">
-                                <Route index element={<PostList />} />
-                                <Route path="edit/:id" element={<PostEdit />} />
-                                <Route path="show/:id" element={<PostShow />} />
+                                <Route path="/posts">
+                                    <Route index element={<PostList />} />
+                                    <Route
+                                        path="edit/:id"
+                                        element={<PostEdit />}
+                                    />
+                                    <Route
+                                        path="show/:id"
+                                        element={<PostShow />}
+                                    />
+                                </Route>
                             </Route>
-                        </Route>
 
-                        <Route
-                            element={
-                                <Authenticated fallback={<Outlet />}>
-                                    <NavigateToResource resource="posts" />
-                                </Authenticated>
-                            }
-                        >
                             <Route
-                                path="/login"
                                 element={
-                                    <AuthPage
-                                        type="login"
-                                        formProps={{
-                                            initialValues: {
-                                                ...authCredentials,
-                                            },
-                                        }}
-                                        providers={[
-                                            {
-                                                name: "google",
-                                                label: "Sign in with Google",
-                                                icon: (
-                                                    <GoogleOutlined
-                                                        style={{
-                                                            fontSize: 24,
-                                                            lineHeight: 0,
-                                                        }}
-                                                    />
-                                                ),
-                                            },
-                                            {
-                                                name: "github",
-                                                label: "Sign in with GitHub",
-                                                icon: (
-                                                    <GithubOutlined
-                                                        style={{
-                                                            fontSize: 24,
-                                                            lineHeight: 0,
-                                                        }}
-                                                    />
-                                                ),
-                                            },
-                                        ]}
-                                    />
+                                    <Authenticated fallback={<Outlet />}>
+                                        <NavigateToResource resource="posts" />
+                                    </Authenticated>
                                 }
-                            />
-                            <Route
-                                path="/register"
-                                element={
-                                    <AuthPage
-                                        type="register"
-                                        providers={[
-                                            {
-                                                name: "google",
-                                                label: "Sign in with Google",
-                                                icon: (
-                                                    <GoogleOutlined
-                                                        style={{
-                                                            fontSize: 24,
-                                                            lineHeight: 0,
-                                                        }}
-                                                    />
-                                                ),
-                                            },
-                                            {
-                                                name: "github",
-                                                label: "Sign in with GitHub",
-                                                icon: (
-                                                    <GithubOutlined
-                                                        style={{
-                                                            fontSize: 24,
-                                                            lineHeight: 0,
-                                                        }}
-                                                    />
-                                                ),
-                                            },
-                                        ]}
-                                    />
-                                }
-                            />
-                            <Route
-                                path="/forgot-password"
-                                element={<AuthPage type="forgotPassword" />}
-                            />
-                            <Route
-                                path="/update-password"
-                                element={<AuthPage type="updatePassword" />}
-                            />
-                        </Route>
+                            >
+                                <Route
+                                    path="/login"
+                                    element={
+                                        <AuthPage
+                                            type="login"
+                                            formProps={{
+                                                initialValues: {
+                                                    ...authCredentials,
+                                                },
+                                            }}
+                                            providers={[
+                                                {
+                                                    name: "google",
+                                                    label: "Sign in with Google",
+                                                    icon: (
+                                                        <GoogleOutlined
+                                                            style={{
+                                                                fontSize: 24,
+                                                                lineHeight: 0,
+                                                            }}
+                                                        />
+                                                    ),
+                                                },
+                                                {
+                                                    name: "github",
+                                                    label: "Sign in with GitHub",
+                                                    icon: (
+                                                        <GithubOutlined
+                                                            style={{
+                                                                fontSize: 24,
+                                                                lineHeight: 0,
+                                                            }}
+                                                        />
+                                                    ),
+                                                },
+                                            ]}
+                                        />
+                                    }
+                                />
+                                <Route
+                                    path="/register"
+                                    element={
+                                        <AuthPage
+                                            type="register"
+                                            providers={[
+                                                {
+                                                    name: "google",
+                                                    label: "Sign in with Google",
+                                                    icon: (
+                                                        <GoogleOutlined
+                                                            style={{
+                                                                fontSize: 24,
+                                                                lineHeight: 0,
+                                                            }}
+                                                        />
+                                                    ),
+                                                },
+                                                {
+                                                    name: "github",
+                                                    label: "Sign in with GitHub",
+                                                    icon: (
+                                                        <GithubOutlined
+                                                            style={{
+                                                                fontSize: 24,
+                                                                lineHeight: 0,
+                                                            }}
+                                                        />
+                                                    ),
+                                                },
+                                            ]}
+                                        />
+                                    }
+                                />
+                                <Route
+                                    path="/forgot-password"
+                                    element={<AuthPage type="forgotPassword" />}
+                                />
+                                <Route
+                                    path="/update-password"
+                                    element={<AuthPage type="updatePassword" />}
+                                />
+                            </Route>
 
-                        <Route
-                            element={
-                                <Authenticated>
-                                    <ThemedLayoutV2>
-                                        <Outlet />
-                                    </ThemedLayoutV2>
-                                </Authenticated>
-                            }
-                        >
-                            <Route path="*" element={<ErrorComponent />} />
-                        </Route>
-                    </Routes>
-                    <UnsavedChangesNotifier />
-                    <DocumentTitleHandler />
-                </Refine>
+                            <Route
+                                element={
+                                    <Authenticated>
+                                        <ThemedLayoutV2>
+                                            <Outlet />
+                                        </ThemedLayoutV2>
+                                    </Authenticated>
+                                }
+                            >
+                                <Route path="*" element={<ErrorComponent />} />
+                            </Route>
+                        </Routes>
+                        <UnsavedChangesNotifier />
+                        <DocumentTitleHandler />
+                    </Refine>
+                </AntdApp>
             </ConfigProvider>
         </BrowserRouter>
     );
