f4d22d285dd02adb6f654eaf07a77585c940226f
Salih <salihozdemir94@gmail.com>
2023-08-10 14:26:55 +0300

    chore: fix the example

--- examples/blog-refine-filtering/src/App.tsx
+++ examples/blog-refine-filtering/src/App.tsx
@@ -14,7 +14,7 @@ import "./App.css";
 function App() {
     return (
         <BrowserRouter>
-            {/* <GitHubBanner /> */}
+            <GitHubBanner />
             <RefineKbarProvider>
                 <Refine
                     dataProvider={dataProvider(
