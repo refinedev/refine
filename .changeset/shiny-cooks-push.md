d366a15f56cf1d5c2656d73b5d8c37331b1d261a
mikeyfarina <mikeyfarina7@gmail.com>
2023-08-16 17:39:07 -0400

    feat(@refinedev/cli): add "--platform" option to specify project type

---
+++ .changeset/shiny-cooks-push.md
@@ -0,0 +1,5 @@
+---
+"@refinedev/cli": minor
+---
+
+add "--platform" option to run, dev, and build to specify which project type to use for the commands
--- packages/cli/src/commands/runner/build/index.ts
+++ packages/cli/src/commands/runner/build/index.ts
@@ -1,21 +1,35 @@
-import { Command } from "commander";
+import { Command, Option } from "commander";
 import { getProjectType } from "@utils/project";
 import { projectScripts } from "../projectScripts";
 import { runScript } from "../runScript";
 import { updateNotifier } from "src/update-notifier";
-import { getRunnerDescription } from "../utils";
+import { getPlatformOptionDescription, getRunnerDescription } from "../utils";
+import { ProjectTypes } from "@definitions/projectTypes";
 
 const build = (program: Command) => {
     return program
         .command("build")
         .description(getRunnerDescription("build"))
         .allowUnknownOption(true)
+        .addOption(
+            new Option(
+                "-p, --platform <platform>",
+                getPlatformOptionDescription(),
+            ).choices(
+                Object.values(ProjectTypes).filter(
+                    (type) => type !== ProjectTypes.UNKNOWN,
+                ),
+            ),
+        )
         .argument("[args...]")
         .action(action);
 };
 
-const action = async (args: string[]) => {
-    const projectType = getProjectType();
+const action = async (
+    args: string[],
+    { platform }: { platform: ProjectTypes },
+) => {
+    const projectType =  getProjectType(platform);
 
     const binPath = projectScripts[projectType].getBin("build");
     const script = projectScripts[projectType].build;
