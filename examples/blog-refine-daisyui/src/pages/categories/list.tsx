7df89de7625df0bc754146486d611273653d4696
Abdullah Numan <anewman15@hotmail.com>
2023-08-17 18:35:28 +0600

    feat(blog): blog-refine-daisyui - update CategoryList comp

--- examples/blog-refine-daisyui/src/pages/categories/list.tsx
+++ examples/blog-refine-daisyui/src/pages/categories/list.tsx
@@ -1,6 +1,198 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { HeadlessListInferencer } from "@refinedev/inferencer/headless";
+import React from "react";
+import { IResourceComponentsProps, useNavigation } from "@refinedev/core";
+import { useTable } from "@refinedev/react-table";
+import { ColumnDef, flexRender } from "@tanstack/react-table";
 
 export const CategoryList: React.FC<IResourceComponentsProps> = () => {
-    return <HeadlessListInferencer />;
+    const columns = React.useMemo<ColumnDef<any>[]>(
+        () => [
+            {
+                id: "id",
+                accessorKey: "id",
+                header: "Id",
+            },
+            {
+                id: "title",
+                accessorKey: "title",
+                header: "Title",
+            },
+            {
+                id: "isActive",
+                accessorKey: "isActive",
+                header: "Is Active",
+                cell: function render({ getValue }) {
+                    return getValue<any>() ? "yes" : "no";
+                },
+            },
+            {
+                id: "cover",
+                accessorKey: "cover",
+                header: "Cover",
+            },
+            {
+                id: "actions",
+                accessorKey: "id",
+                header: "Actions",
+                cell: function render({ getValue }) {
+                    return (
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                flexWrap: "wrap",
+                                gap: "4px",
+                            }}
+                        >
+                            <button
+                                onClick={() => {
+                                    show("categories", getValue() as string);
+                                }}
+                            >
+                                Show
+                            </button>
+                            <button
+                                onClick={() => {
+                                    edit("categories", getValue() as string);
+                                }}
+                            >
+                                Edit
+                            </button>
+                        </div>
+                    );
+                },
+            },
+        ],
+        [],
+    );
+
+    const { edit, show, create } = useNavigation();
+
+    const {
+        getHeaderGroups,
+        getRowModel,
+        setOptions,
+        refineCore: {
+            tableQueryResult: { data: tableData },
+        },
+        getState,
+        setPageIndex,
+        getCanPreviousPage,
+        getPageCount,
+        getCanNextPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        getColumn,
+    } = useTable({
+        columns,
+    });
+
+    setOptions((prev) => ({
+        ...prev,
+        meta: {
+            ...prev.meta,
+        },
+    }));
+
+    return (
+        <div style={{ padding: "16px" }}>
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                }}
+            >
+                <h1>Categories</h1>
+                <button onClick={() => create("categories")}>Create</button>
+            </div>
+            <div style={{ maxWidth: "100%", overflowY: "scroll" }}>
+                <table>
+                    <thead>
+                        {getHeaderGroups().map((headerGroup) => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <th key={header.id}>
+                                        {!header.isPlaceholder &&
+                                            flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext(),
+                                            )}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody>
+                        {getRowModel().rows.map((row) => (
+                            <tr key={row.id}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <td key={cell.id}>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext(),
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div style={{ marginTop: "12px" }}>
+                <button
+                    onClick={() => setPageIndex(0)}
+                    disabled={!getCanPreviousPage()}
+                >
+                    {"<<"}
+                </button>
+                <button
+                    onClick={() => previousPage()}
+                    disabled={!getCanPreviousPage()}
+                >
+                    {"<"}
+                </button>
+                <button onClick={() => nextPage()} disabled={!getCanNextPage()}>
+                    {">"}
+                </button>
+                <button
+                    onClick={() => setPageIndex(getPageCount() - 1)}
+                    disabled={!getCanNextPage()}
+                >
+                    {">>"}
+                </button>
+                <span>
+                    <strong>
+                        {" "}
+                        {getState().pagination.pageIndex + 1} / {getPageCount()}{" "}
+                    </strong>
+                </span>
+                <span>
+                    | Go to Page:{" "}
+                    <input
+                        type="number"
+                        defaultValue={getState().pagination.pageIndex + 1}
+                        onChange={(e) => {
+                            const page = e.target.value
+                                ? Number(e.target.value) - 1
+                                : 0;
+                            setPageIndex(page);
+                        }}
+                    />
+                </span>{" "}
+                <select
+                    value={getState().pagination.pageSize}
+                    onChange={(e) => {
+                        setPageSize(Number(e.target.value));
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
 };
