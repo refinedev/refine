5d340fec3a9ba14f85c55ab153034b9f424a9f65
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-08-17 10:38:41 +0300

    chore(app-crm): remove affix on kanban column

--- examples/app-crm/src/components/kanban/column.tsx
+++ examples/app-crm/src/components/kanban/column.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren } from "react";
 import { UseDroppableArguments, useDroppable } from "@dnd-kit/core";
-import { Affix, Button, Dropdown, MenuProps, Space } from "antd";
+import { Button, Dropdown, MenuProps } from "antd";
 import { PlusOutlined, MoreOutlined } from "@ant-design/icons";
 
 import cn from "classnames";
@@ -37,61 +37,59 @@ export const KanbanColumn: FC<PropsWithChildren<Props>> = ({
 
     return (
         <div ref={setNodeRef} className={styles.container}>
-            <Affix offsetTop={64}>
-                <div className={styles.header}>
-                    <div className={styles.titleContainer}>
-                        <Text
-                            ellipsis={{ tooltip: title }}
-                            size="xs"
-                            strong
-                            style={{
-                                textTransform: "uppercase",
-                                whiteSpace: "nowrap",
-                            }}
-                        >
-                            {title}
-                        </Text>
-                        <div className={styles.count}>
-                            <Text size="xs">{count}</Text>
-                        </div>
-                    </div>
-                    <div className={styles.actionContainer}>
-                        {contextMenuItems && (
-                            <Dropdown
-                                trigger={["click"]}
-                                menu={{
-                                    items: contextMenuItems,
-                                    onPointerDown: (e) => {
-                                        e.stopPropagation();
-                                    },
-                                    onClick: (e) => {
-                                        e.domEvent.stopPropagation();
-                                    },
-                                }}
-                                placement="bottomCenter"
-                                arrow={{ pointAtCenter: true }}
-                            >
-                                <Button
-                                    type="text"
-                                    shape="circle"
-                                    icon={
-                                        <MoreOutlined
-                                            style={{
-                                                transform: "rotate(90deg)",
-                                            }}
-                                        />
-                                    }
-                                />
-                            </Dropdown>
-                        )}
-                        <Button
-                            shape="circle"
-                            icon={<PlusOutlined />}
-                            onClick={onAddClickHandler}
-                        />
+            <div className={styles.header}>
+                <div className={styles.titleContainer}>
+                    <Text
+                        ellipsis={{ tooltip: title }}
+                        size="xs"
+                        strong
+                        style={{
+                            textTransform: "uppercase",
+                            whiteSpace: "nowrap",
+                        }}
+                    >
+                        {title}
+                    </Text>
+                    <div className={styles.count}>
+                        <Text size="xs">{count}</Text>
                     </div>
                 </div>
-            </Affix>
+                <div className={styles.actionContainer}>
+                    {contextMenuItems && (
+                        <Dropdown
+                            trigger={["click"]}
+                            menu={{
+                                items: contextMenuItems,
+                                onPointerDown: (e) => {
+                                    e.stopPropagation();
+                                },
+                                onClick: (e) => {
+                                    e.domEvent.stopPropagation();
+                                },
+                            }}
+                            placement="bottomCenter"
+                            arrow={{ pointAtCenter: true }}
+                        >
+                            <Button
+                                type="text"
+                                shape="circle"
+                                icon={
+                                    <MoreOutlined
+                                        style={{
+                                            transform: "rotate(90deg)",
+                                        }}
+                                    />
+                                }
+                            />
+                        </Dropdown>
+                    )}
+                    <Button
+                        shape="circle"
+                        icon={<PlusOutlined />}
+                        onClick={onAddClickHandler}
+                    />
+                </div>
+            </div>
             <div
                 className={cn(styles.childrenWrapper, {
                     [styles.isOver]: isOver,
