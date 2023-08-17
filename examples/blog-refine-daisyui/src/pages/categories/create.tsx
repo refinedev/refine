6051a45e7745db1c4794eb3023e6be70734c70a6
Abdullah Numan <anewman15@hotmail.com>
2023-08-17 18:36:15 +0600

    feat(blog): blog-refine-daisyui - update CategoryCreate comp

--- examples/blog-refine-daisyui/src/pages/categories/create.tsx
+++ examples/blog-refine-daisyui/src/pages/categories/create.tsx
@@ -1,6 +1,83 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { HeadlessCreateInferencer } from "@refinedev/inferencer/headless";
+import React from "react";
+import { useNavigation, IResourceComponentsProps } from "@refinedev/core";
+import { useForm } from "@refinedev/react-hook-form";
 
 export const CategoryCreate: React.FC<IResourceComponentsProps> = () => {
-    return <HeadlessCreateInferencer />;
+    const { list } = useNavigation();
+
+    const {
+        refineCore: { onFinish, formLoading },
+        register,
+        handleSubmit,
+        resetField,
+        formState: { errors },
+    } = useForm();
+
+    return (
+        <div style={{ padding: "16px" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h1>Category Create</h1>
+                <div>
+                    <button
+                        onClick={() => {
+                            list("categories");
+                        }}
+                    >
+                        Categories
+                    </button>
+                </div>
+            </div>
+            <form onSubmit={handleSubmit(onFinish)}>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "8px",
+                    }}
+                >
+                    <label>
+                        <span style={{ marginRight: "8px" }}>Title</span>
+                        <input
+                            type="text"
+                            {...register("title", {
+                                required: "This field is required",
+                            })}
+                        />
+                        <span style={{ color: "red" }}>
+                            {(errors as any)?.title?.message as string}
+                        </span>
+                    </label>
+                    <label>
+                        <span style={{ marginRight: "8px" }}>Is Active</span>
+                        <input
+                            type="checkbox"
+                            {...register("isActive", {
+                                required: "This field is required",
+                            })}
+                        />
+                        <span style={{ color: "red" }}>
+                            {errors?.isActive?.message as string}
+                        </span>
+                    </label>
+                    <label>
+                        <span style={{ marginRight: "8px" }}>Cover</span>
+                        <textarea
+                            rows={5}
+                            cols={33}
+                            style={{ verticalAlign: "top" }}
+                            {...register("cover", {
+                                required: "This field is required",
+                            })}
+                        />
+                        <span style={{ color: "red" }}>
+                            {(errors as any)?.cover?.message as string}
+                        </span>
+                    </label>
+                    <div>
+                        <input type="submit" value="Save" />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 };
