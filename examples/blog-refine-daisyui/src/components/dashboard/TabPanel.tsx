8857ea38377a91bee0d33921d18ae7ba8837dff1
Abdullah Numan <anewman15@hotmail.com>
2023-08-22 22:31:50 +0600

    feat(blog): blog-refine-daisyui - add TabItem comp

---
+++ examples/blog-refine-daisyui/src/components/dashboard/TabPanel.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type TTabPanelProps = {
+    isActive: Boolean;
+    children: JSX.Element;
+};
+
+export const TabPanel = ({ isActive, children }: TTabPanelProps) => {
+  return isActive ? (
+    <div className="mx-auto">
+        {children}
+    </div>
+  ) : null;
+};
