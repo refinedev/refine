2ea4bf5f7b3d6321790184b102ead90f5d710283
Abdullah Numan <anewman15@hotmail.com>
2023-08-22 22:06:18 +0600

    feat(blog): blog-refine-daisyui - add interfaces

---
+++ examples/blog-refine-daisyui/src/interfaces/index.ts
@@ -0,0 +1,61 @@
+export interface IOrder {
+    id: number;
+    user: IUser;
+    createdAt: string;
+    status: IOrderStatus;
+    adress: IAddress;
+    amount: number;
+}
+
+export interface IUser {
+    id: number;
+    fullName: string;
+    gender: string;
+    gsm: string;
+    createdAt: string;
+    addresses: IAddress[];
+}
+
+export interface IOrderStatus {
+    id: number;
+    text: "Pending" | "Ready" | "On The Way" | "Delivered" | "Cancelled";
+}
+
+export interface IAddress {
+    text: string;
+    coordinate: [string, string];
+}
+
+export interface IChartDatum {
+    date: string;
+    value: string;
+}
+
+export interface IChart {
+    data: IChartDatum[];
+    total: number;
+    trend: number;
+}
+
+export interface IProduct {
+    id: number;
+    name: string;
+    isActive: boolean;
+    description: string;
+    createdAt: string;
+    price: number;
+    category: ICategory;
+    stock: number;
+}
+
+export interface ICategory {
+    id: number;
+    title: string;
+    isActive: boolean;
+}
+
+export type TTab = {
+    id: number;
+    label: string;
+    content: JSX.Element;
+};
