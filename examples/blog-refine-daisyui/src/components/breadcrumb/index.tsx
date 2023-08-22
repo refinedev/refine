b72dbd4f93cc24799236c8a03d1f1458963d6c04
Abdullah Numan <anewman15@hotmail.com>
2023-08-22 22:07:33 +0600

    feat(blog): blog-refine-daisyui - update breadcrumbs with daisyUI

--- examples/blog-refine-daisyui/src/components/breadcrumb/index.tsx
+++ examples/blog-refine-daisyui/src/components/breadcrumb/index.tsx
@@ -5,18 +5,23 @@ export const Breadcrumb = () => {
     const { breadcrumbs } = useBreadcrumb();
 
     return (
-        <ul className="breadcrumb">
-            {breadcrumbs.map((breadcrumb) => {
-                return (
-                    <li key={`breadcrumb-${breadcrumb.label}`}>
-                        {breadcrumb.href ? (
-                            <Link to={breadcrumb.href}>{breadcrumb.label}</Link>
-                        ) : (
-                            <span>{breadcrumb.label}</span>
-                        )}
-                    </li>
-                );
-            })}
-        </ul>
+        <div className="text-sm breadcrumbs">
+            <ul>
+                {breadcrumbs?.map((breadcrumb) => {
+                    return (
+                        <li key={`breadcrumb-${breadcrumb?.label}`}>
+                            <span className="mx-2">{breadcrumb?.icon}</span>
+                            {breadcrumb?.href ? (
+                                <Link to={breadcrumb?.href}>
+                                    {breadcrumb?.label}
+                                </Link>
+                            ) : (
+                                <span>{breadcrumb?.label}</span>
+                            )}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
     );
 };
