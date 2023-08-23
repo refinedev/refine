db41b12ab38b5b02d57d6f859aef7b5e479ba216
Abdullah Numan <anewman15@hotmail.com>
2023-08-23 15:42:42 +0600

    feat(blog): blog-refine-daisyui - add more icons

--- examples/blog-refine-daisyui/src/components/icons/index.tsx
+++ examples/blog-refine-daisyui/src/components/icons/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+type TIconProps = {
+    size: number;
+    colors: {
+        stroke: string;
+        fill: string;
+    };
+};
+
 export const AscIcon = () => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -158,14 +166,14 @@ export const FilterIcon = () => (
     </svg>
 );
 
-export const ProductIcon = () => (
+export const ProductIcon = ({ size, colors }: TIconProps) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-4 h-4"
+        stroke={colors?.fill}
+        className={`w-${size} h-${size}`}
     >
         <path
             strokeLinecap="round"
@@ -175,6 +183,44 @@ export const ProductIcon = () => (
     </svg>
 );
 
+export const DollarIcon = ({ size, colors }: TIconProps) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke={`${colors?.stroke}`}
+            className={`w-${size} h-${size}`}
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+        </svg>
+    );
+};
+
+export const UserGroupIcon = ({ size, colors }: TIconProps) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke={`${colors?.stroke}`}
+            className={`w-${size} h-${size}`}
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+            />
+        </svg>
+    );
+};
+
 export const RefreshIcon = () => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -182,7 +228,7 @@ export const RefreshIcon = () => (
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="w-4 h-4"
+        className="w-6 h-6"
     >
         <path
             strokeLinecap="round"
