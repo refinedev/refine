b26040ef835a5099a5c9b3bdb2f67f8391b97c34
Abdullah Numan <anewman15@hotmail.com>
2023-08-24 14:29:16 +0600

    fix(blog): blog-refine-daisyui - update breadcrumbs comp

--- examples/blog-refine-daisyui/src/components/breadcrumb/index.tsx
+++ examples/blog-refine-daisyui/src/components/breadcrumb/index.tsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 export const Breadcrumb = () => {
     const { breadcrumbs } = useBreadcrumb();
 
+    if (breadcrumbs.length == 1) return null;
+
     return (
         <div className="text-sm breadcrumbs">
-            <ul>
+            <ul className="my-2">
                 {breadcrumbs?.map((breadcrumb) => {
                     return (
                         <li key={`breadcrumb-${breadcrumb?.label}`}>
