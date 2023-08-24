62734444f1171632ac34983cd3d7e408222c59bf
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-08-24 14:03:07 +0300

    fix(app-crm): antd.ts duplicate key

--- examples/app-crm/src/providers/antd.ts
+++ examples/app-crm/src/providers/antd.ts
@@ -36,15 +36,6 @@ export const themeConfig: ThemeConfig = {
         Calendar: {
             colorBgContainer: "#FFFFFF",
         },
-        Table: {
-            colorBgContainer: "#fff",
-        },
-        Input: {
-            colorBgContainer: "#fff",
-        },
-        InputNumber: {
-            colorBgContainer: "#fff",
-        },
         Radio: {
             colorBgContainer: "#fff",
         },
