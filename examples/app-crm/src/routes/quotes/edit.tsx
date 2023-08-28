7f1b2b5c18350f6d5f7438a9ac63dbf5d7709715
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-08-28 23:34:00 +0300

    fix(app-crm): typo

--- examples/app-crm/src/routes/quotes/edit.tsx
+++ examples/app-crm/src/routes/quotes/edit.tsx
@@ -4,7 +4,8 @@ import { QuotesFormModal } from "../../components";
 export const QuotesEditPage: FC<PropsWithChildren> = ({ children }) => {
     return (
         <>
-            <QuotesFormModal action="edit" />;{children}
+            <QuotesFormModal action="edit" />
+            {children}
         </>
     );
 };
