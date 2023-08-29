472d2568c23eccf559f41f9386b80bb06391ca16
Salih <salihozdemir94@gmail.com>
2023-08-29 13:14:56 +0300

    fix: contact modal drawer style issues

--- examples/app-crm/src/components/index.ts
+++ examples/app-crm/src/components/index.ts
@@ -4,3 +4,5 @@ export * from "./fullscreen-loading";
 export * from "./deal-kanban-card";
 export * from "./quotes";
 export * from "./custom-avatar";
+export * from "./single-element-form";
+export * from "./select-option-with-avatar";
