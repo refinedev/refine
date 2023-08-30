906d621b717fdd964d39b9f44f3b121906ce1011
Necati Özmen <necatiozmn@gmail.com>
2023-08-30 22:45:34 +0300

    docs(blog): add markdown article (#4876)
    * docs(blog): add markdown post
    
    * chore: add markdown example
    
    * chore: update package json
    
    * chore: update package json
    
    * chore: remove old project
    
    * chore: add new project
    
    * docs(blog): update post
    
    * chore: fix package json
    
    * chore: fix example package json

---
+++ examples/blog-refine-markdown/src/App.tsx
@@ -0,0 +1,100 @@
+import { ErrorComponent, GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { ThemedLayoutV2, notificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import routerBindings, {
+    DocumentTitleHandler,
+    NavigateToResource,
+    UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+import { PostCreate, PostEdit, PostList, PostShow } from "./pages/posts";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <RefineKbarProvider>
+                <ColorModeContextProvider>
+                    <Refine
+                        dataProvider={dataProvider(
+                            "https://api.fake-rest.refine.dev",
+                        )}
+                        notificationProvider={notificationProvider}
+                        routerProvider={routerBindings}
+                        resources={[
+                            {
+                                name: "blog_posts",
+                                list: "/blog-posts",
+                                create: "/blog-posts/create",
+                                edit: "/blog-posts/edit/:id",
+                                show: "/blog-posts/show/:id",
+                                meta: {
+                                    canDelete: true,
+                                },
+                            },
+                            {
+                                name: "categories",
+                                list: "/categories",
+                                create: "/categories/create",
+                                edit: "/categories/edit/:id",
+                                show: "/categories/show/:id",
+                                meta: {
+                                    canDelete: true,
+                                },
+                            },
+                        ]}
+                        options={{
+                            syncWithLocation: true,
+                            warnWhenUnsavedChanges: true,
+                        }}
+                    >
+                        <Routes>
+                            <Route
+                                element={
+                                    <ThemedLayoutV2>
+                                        <Outlet />
+                                    </ThemedLayoutV2>
+                                }
+                            >
+                                <Route
+                                    index
+                                    element={
+                                        <NavigateToResource resource="blog_posts" />
+                                    }
+                                />
+                                <Route path="/blog-posts">
+                                    <Route index element={<PostList />} />
+                                    <Route
+                                        path="create"
+                                        element={<PostCreate />}
+                                    />
+                                    <Route
+                                        path="edit/:id"
+                                        element={<PostEdit />}
+                                    />
+                                    <Route
+                                        path="show/:id"
+                                        element={<PostShow />}
+                                    />
+                                </Route>
+
+                                <Route path="*" element={<ErrorComponent />} />
+                            </Route>
+                        </Routes>
+
+                        <RefineKbar />
+                        <UnsavedChangesNotifier />
+                    </Refine>
+                </ColorModeContextProvider>
+            </RefineKbarProvider>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ examples/blog-refine-markdown/src/contexts/color-mode/index.tsx
@@ -0,0 +1,60 @@
+import { RefineThemes } from "@refinedev/antd";
+import { ConfigProvider, theme } from "antd";
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
+
+type ColorModeContextType = {
+    mode: string;
+    setMode: (mode: string) => void;
+};
+
+export const ColorModeContext = createContext<ColorModeContextType>(
+    {} as ColorModeContextType,
+);
+
+export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
+    children,
+}) => {
+    const colorModeFromLocalStorage = localStorage.getItem("colorMode");
+    const isSystemPreferenceDark = window?.matchMedia(
+        "(prefers-color-scheme: dark)",
+    ).matches;
+
+    const systemPreference = isSystemPreferenceDark ? "dark" : "light";
+    const [mode, setMode] = useState(
+        colorModeFromLocalStorage || systemPreference,
+    );
+
+    useEffect(() => {
+        window.localStorage.setItem("colorMode", mode);
+    }, [mode]);
+
+    const setColorMode = () => {
+        if (mode === "light") {
+            setMode("dark");
+        } else {
+            setMode("light");
+        }
+    };
+
+    const { darkAlgorithm, defaultAlgorithm } = theme;
+
+    return (
+        <ColorModeContext.Provider
+            value={{
+                setMode: setColorMode,
+                mode,
+            }}
+        >
+            <ConfigProvider
+                // you can change the theme colors here. example: ...RefineThemes.Magenta,
+                theme={{
+                    ...RefineThemes.Blue,
+                    algorithm:
+                        mode === "light" ? defaultAlgorithm : darkAlgorithm,
+                }}
+            >
+                {children}
+            </ConfigProvider>
+        </ColorModeContext.Provider>
+    );
+};
