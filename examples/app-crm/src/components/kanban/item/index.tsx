5f1dc6262c520302f4b8e23b6254d33dba408539
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-08-31 14:21:11 +0300

    feat(app-crm): overflow:scroll added to kanban (#4884)
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>

--- examples/app-crm/src/components/kanban/item/index.tsx
+++ examples/app-crm/src/components/kanban/item/index.tsx
@@ -19,43 +19,40 @@ export const KanbanItem: FC<PropsWithChildren<Props>> = ({
         id,
         data,
     });
+
     return (
-        <>
+        <div
+            style={{
+                position: "relative",
+            }}
+        >
             <div
+                ref={setNodeRef}
+                {...listeners}
+                {...attributes}
                 style={{
+                    opacity: active ? (active.id === id ? 1 : 0.5) : 1,
+                    borderRadius: "8px",
                     position: "relative",
+                    cursor: "grab",
                 }}
             >
-                <div
-                    ref={setNodeRef}
-                    {...listeners}
-                    {...attributes}
-                    style={{
-                        opacity: active ? (active.id === id ? 1 : 0.5) : 1,
-                        borderRadius: "8px",
-                        position: "relative",
-                        cursor: "grab",
-                    }}
-                >
-                    {children}
-                </div>
+                {children}
             </div>
-            {
+            {active?.id === id && (
                 <DragOverlay>
-                    {active?.id === id && (
-                        <div
-                            style={{
-                                borderRadius: "8px",
-                                boxShadow:
-                                    "0px 9px 28px 8px rgba(0, 0, 0, 0.05), 0px 3px 6px -4px rgba(0, 0, 0, 0.12), 0px 6px 16px 0px rgba(0, 0, 0, 0.08)",
-                                cursor: "grabbing",
-                            }}
-                        >
-                            {children}
-                        </div>
-                    )}
+                    <div
+                        style={{
+                            borderRadius: "8px",
+                            boxShadow:
+                                "0px 9px 28px 8px rgba(0, 0, 0, 0.05), 0px 3px 6px -4px rgba(0, 0, 0, 0.12), 0px 6px 16px 0px rgba(0, 0, 0, 0.08)",
+                            cursor: "grabbing",
+                        }}
+                    >
+                        {children}
+                    </div>
                 </DragOverlay>
-            }
-        </>
+            )}
+        </div>
     );
 };
