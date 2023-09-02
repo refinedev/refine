44646333e602b5163f765e9946d7715cf00e317f
Abdullah Numan <anewman15@hotmail.com>
2023-09-02 13:27:54 +0600

    chore(blog): blog-refine-daisyui - refactor KPI cards in Stats comp

--- examples/blog-refine-daisyui/src/pages/dashboard/index.tsx
+++ examples/blog-refine-daisyui/src/pages/dashboard/index.tsx
@@ -1,18 +1,12 @@
 import React, { useMemo } from "react";
 import { CrudFilter, useList } from "@refinedev/core";
 import dayjs from "dayjs";
-import { KpiCard } from "../../components/dashboard/kpiCard";
+import Stats from "../../components/dashboard/Stats";
 import { ResponsiveAreaChart } from "../../components/dashboard/ResponsiveAreaChart";
 import { ResponsiveBarChart } from "../../components/dashboard/ResponsiveBarChart";
 import { TabView } from "../../components/dashboard/TabView";
 import { RecentSales } from "../../components/dashboard/RecentSales";
 import { IChartDatum, TTab } from "../../interfaces";
-
-import {
-    CurrencyDollarIcon,
-    ShoppingCartIcon,
-    UserGroupIcon,
-} from "@heroicons/react/24/outline";
 
 const filters: CrudFilter[] = [
     {
@@ -107,42 +101,11 @@ export const Dashboard: React.FC = () => {
 
     return (
         <>
-            <div className="w-full mx-auto mb-4 flex flex-col justify-center items-stretch md:flex-row md:justify-between drop-shadow-md">
-                <div className="w-full mx-auto md:flex-1 md:mr-2">
-                    <KpiCard
-                        title="Weekly Revenue"
-                        data={dailyRevenue}
-                        formatTotal={(value: number | string) => `$ ${value}`}
-                        icon={<CurrencyDollarIcon className="h-32 w-32" />}
-                        colors={{
-                            stroke: "rgb(54, 162, 235)",
-                            fill: "rgba(54, 162, 235, 0.2)",
-                        }}
-                    />
-                </div>
-                <div className="w-full mx-auto md:flex-1">
-                    <KpiCard
-                        title="Weekly Orders"
-                        data={dailyOrders}
-                        icon={<ShoppingCartIcon className="h-32 w-32" />}
-                        colors={{
-                            stroke: "rgb(255, 159, 64)",
-                            fill: "rgba(255, 159, 64, 0.2)",
-                        }}
-                    />
-                </div>
-                <div className="w-full mx-auto md:flex-1 md:ml-2">
-                    <KpiCard
-                        title="New Customers"
-                        data={newCustomers}
-                        icon={<UserGroupIcon className="h-32 w-32" />}
-                        colors={{
-                            stroke: "rgb(76, 175, 80)",
-                            fill: "rgba(76, 175, 80, 0.2)",
-                        }}
-                    />
-                </div>
-            </div>
+            <Stats
+                dailyRevenue={dailyRevenue}
+                dailyOrders={dailyOrders}
+                newCustomers={newCustomers}
+            />
             <TabView tabs={tabs} />
             <RecentSales />
         </>
