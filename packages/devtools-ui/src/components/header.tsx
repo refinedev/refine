96d41faf035d307e0f2ad4b0a7b8a90e281ab484
Ali Emir Şen <senaliemir@gmail.com>
2023-09-06 00:49:18 +0300

    chore(devtools-ui): show connected app

--- packages/devtools-ui/src/components/header.tsx
+++ packages/devtools-ui/src/components/header.tsx
@@ -4,8 +4,55 @@ import clsx from "clsx";
 import { LogoIcon } from "./icons/logo";
 import { ContactIcon } from "./icons/contact";
 import { NotificationIcon } from "./icons/notification";
+import {
+    DevToolsContext,
+    DevtoolsEvent,
+    receive,
+} from "@refinedev/devtools-shared";
 
 export const Header = () => {
+    const [connectedApp, setConnectedApp] = React.useState<string | null>(null);
+    const { ws } = React.useContext(DevToolsContext);
+
+    const fetchConnectedApp = React.useCallback(async () => {
+        try {
+            const response = await fetch("api/connected-app");
+            const data = (await response.json()) as { url: string | null };
+            setConnectedApp(data.url);
+        } catch (error) {
+            //
+        }
+    }, []);
+
+    React.useEffect(() => {
+        fetchConnectedApp();
+    }, [fetchConnectedApp]);
+
+    React.useEffect(() => {
+        if (!ws) return () => 0;
+
+        const unsubscribeConnected = receive(
+            ws,
+            DevtoolsEvent.DEVTOOLS_CONNECTED_APP,
+            (data) => {
+                setConnectedApp(data.url);
+            },
+        );
+
+        const unsubscribeDisconnected = receive(
+            ws,
+            DevtoolsEvent.DEVTOOLS_DISCONNECTED_APP,
+            () => {
+                setConnectedApp(null);
+            },
+        );
+
+        return () => {
+            unsubscribeConnected();
+            unsubscribeDisconnected();
+        };
+    }, [ws]);
+
     return (
         <div
             className={clsx(
@@ -22,8 +69,41 @@ export const Header = () => {
                 "re-h-[65px]",
             )}
         >
-            <div>
+            <div className="re-flex-shrink-0">
                 <LogoIcon />
+            </div>
+            <div
+                className={clsx(
+                    "re-bg-gray-900",
+                    "re-flex-1",
+                    "re-border-gray-700",
+                    "re-py-2",
+                    "re-px-4",
+                    "re-rounded-lg",
+                    "re-text-sm",
+                    "re-leading-6",
+                    "re-flex",
+                    "re-items-center",
+                    "re-justify-center",
+                    "re-text-gray-300",
+                )}
+            >
+                <span
+                    className={clsx(
+                        "re-block",
+                        "re-h-2",
+                        "re-w-2",
+                        "re-rounded-full",
+                        connectedApp && "re-bg-alt-green",
+                        !connectedApp && "re-bg-alt-red",
+                        "re-flex-shrink-0",
+                        "re-mr-2",
+                    )}
+                />
+
+                <span className="re-font-mono">
+                    {connectedApp ?? "No App Connected"}
+                </span>
             </div>
             <div
                 className={clsx(
@@ -31,6 +111,7 @@ export const Header = () => {
                     "re-items-center",
                     "re-justify-center",
                     "re-gap-2",
+                    "re-flex-shrink-0",
                 )}
             >
                 <div
