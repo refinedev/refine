a910ddf56947fefbb96f73888e9dd332e6735533
Ali Emir Şen <senaliemir@gmail.com>
2023-09-05 23:29:04 +0300

    chore(devtools-shared): send app url to devtools server

--- packages/devtools-shared/src/context.tsx
+++ packages/devtools-shared/src/context.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { DevtoolsEvent } from "./event-types";
+import { send } from "./send";
+import { receive } from "./receive";
 
 type DevToolsContextValue = {
+    __devtools: boolean;
     port: number;
     url: string;
     secure: boolean;
     ws: WebSocket | null;
+    devtoolsUrl?: string;
 };
 
 export const DevToolsContext = React.createContext<DevToolsContextValue>({
+    __devtools: false,
     port: 5002,
     url: "localhost",
     secure: false,
@@ -16,31 +21,57 @@ export const DevToolsContext = React.createContext<DevToolsContextValue>({
 });
 
 type Props = React.PropsWithChildren<
-    Partial<Pick<DevToolsContextValue, "port">>
+    Partial<Pick<DevToolsContextValue, "port" | "__devtools">>
 >;
 
 export const DevToolsContextProvider: React.FC<Props> = ({
+    __devtools,
     port,
     children,
 }) => {
-    const [values] = React.useState<DevToolsContextValue>({
+    const [values, setValues] = React.useState<DevToolsContextValue>({
+        __devtools: __devtools ?? false,
         port: port ?? 5002,
         url: "localhost",
         secure: false,
         ws: null,
     });
 
-    const [ws] = React.useState<WebSocket | null>(() => {
-        const ws = new WebSocket(
+    const [ws, setWs] = React.useState<WebSocket | null>(null);
+
+    React.useEffect(() => {
+        const wsInstance = new WebSocket(
             `${values.secure ? "wss" : "ws"}://localhost:${values.port}`,
         );
 
-        ws.addEventListener("open", () => {
-            ws.send("hello");
+        const unsubscribe = receive(
+            wsInstance,
+            DevtoolsEvent.DEVTOOLS_HANDSHAKE,
+            (data) => {
+                setValues((v) => ({
+                    ...v,
+                    devtoolsUrl: data.url,
+                }));
+                unsubscribe();
+            },
+        );
+
+        wsInstance.addEventListener("open", () => {
+            if (!values.__devtools) {
+                send(wsInstance, DevtoolsEvent.DEVTOOLS_INIT, {
+                    url: window.location.origin,
+                });
+            }
         });
 
-        return ws;
-    });
+        setWs(wsInstance);
+
+        return () => {
+            unsubscribe();
+
+            wsInstance.close(1000, window.location.origin);
+        };
+    }, []);
 
     const contextValues = React.useMemo<DevToolsContextValue>(
         () => ({
