1121bef63487f9535b6559000a45016307e7d7cc
Ali Emir Şen <senaliemir@gmail.com>
2023-09-06 16:06:24 +0300

    chore(devtools-ui): remove dummy part

--- packages/devtools-ui/src/overview.tsx
+++ packages/devtools-ui/src/overview.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { SidebarActiveIcon } from "./components/icons/sidebar-active";
 
 export const Overview = () => {
     const [count, setCount] = React.useState(0);
 
     return (
-        <div>
-            <h2 className="re-text-pink-500 re-font-bold re-text-2xl">
+        <div className="re-text-gray-0">
+            <h2 className="re-text-pink-900 re-font-bold re-text-2xl">
                 Dev Tools Overview
             </h2>
-            <SidebarActiveIcon />
             <p>Count: {count}</p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
