42f6a76babe2bca01a7dcd7a44383842e4a595c6
Salih <salihozdemir94@gmail.com>
2023-09-15 12:34:28 +0300

    chore: add digital-ocean blog post example

---
+++ examples/blog-refine-digital-ocean/src/components/dealChart/index.tsx
@@ -0,0 +1,135 @@
+import React, { useMemo } from "react";
+import { useCustom } from "@refinedev/core";
+import { Card, Typography } from "antd";
+import { Area, AreaConfig } from "@ant-design/plots";
+import { DollarOutlined } from "@ant-design/icons";
+import dayjs from "dayjs";
+
+type YearlyDealGroupedResponse = {
+    yearlyDealGrouped: {
+        nodes: {
+            title: string;
+            dealsAggregate: {
+                groupBy: {
+                    closeDateMonth: number;
+                    closeDateYear: number;
+                };
+                sum: {
+                    value: number;
+                };
+            }[];
+        }[];
+    };
+};
+
+export const DealChart: React.FC = () => {
+    const { data } = useCustom<YearlyDealGroupedResponse>({
+        method: "post",
+        url: "/graphql",
+        meta: {
+            rawQuery: `query {
+                yearlyDealGrouped: dealStages(filter: { title: { in: ["WON", "LOST"] } }) {
+                    nodes {
+                      title
+                      dealsAggregate {
+                        groupBy {
+                          closeDateMonth
+                          closeDateYear
+                        }
+                        sum {
+                          value
+                        }
+                      }
+                    }
+                  }
+              }              
+            `,
+        },
+    });
+
+    const dealData = useMemo(() => {
+        const won = data?.data.yearlyDealGrouped.nodes
+            .find((node) => node.title === "WON")
+            ?.dealsAggregate.map((item) => {
+                const { closeDateMonth, closeDateYear } = item.groupBy;
+                const date = dayjs(`${closeDateYear}-${closeDateMonth}-01`);
+                return {
+                    timeUnix: date.unix(),
+                    timeText: date.format("MMM YYYY"),
+                    value: item.sum.value,
+                    state: "Won",
+                };
+            });
+
+        const lost = data?.data.yearlyDealGrouped.nodes
+            .find((node) => node.title === "LOST")
+            ?.dealsAggregate.map((item) => {
+                const { closeDateMonth, closeDateYear } = item.groupBy;
+                const date = dayjs(`${closeDateYear}-${closeDateMonth}-01`);
+                return {
+                    timeUnix: date.unix(),
+                    timeText: date.format("MMM YYYY"),
+                    value: item.sum.value,
+                    state: "Lost",
+                };
+            });
+
+        return [...(won || []), ...(lost || [])].sort(
+            (a, b) => a.timeUnix - b.timeUnix,
+        );
+    }, [data]);
+
+    const config: AreaConfig = {
+        isStack: false,
+        data: dealData,
+        xField: "timeText",
+        yField: "value",
+        seriesField: "state",
+        animation: true,
+        startOnZero: false,
+        smooth: true,
+        legend: { offsetY: -6 },
+        yAxis: {
+            tickCount: 4,
+            label: {
+                formatter: (v) => `$${Number(v) / 1000}k`,
+            },
+        },
+        tooltip: {
+            formatter: (data) => ({
+                name: data.state,
+                value: `$${Number(data.value) / 1000}k`,
+            }),
+        },
+        areaStyle: (datum) => {
+            const won = "l(270) 0:#ffffff 0.5:#b7eb8f 1:#52c41a";
+            const lost = "l(270) 0:#ffffff 0.5:#f3b7c2 1:#ff4d4f";
+            return { fill: datum.state === "Won" ? won : lost };
+        },
+        color: (datum) => (datum.state === "Won" ? "#52C41A" : "#F5222D"),
+    };
+
+    return (
+        <Card
+            style={{ height: "432px" }}
+            headStyle={{ padding: "8px 16px" }}
+            bodyStyle={{ padding: "24px 24px 0px 24px" }}
+            title={
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "8px",
+                    }}
+                >
+                    <DollarOutlined />
+                    <Typography.Text style={{ marginLeft: ".5rem" }}>
+                        Deals
+                    </Typography.Text>
+                </div>
+            }
+        >
+            <Area {...config} height={325} />
+        </Card>
+    );
+};
---
+++ examples/blog-refine-digital-ocean/src/pages/contacts/show.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { IResourceComponentsProps, useShow } from "@refinedev/core";
+import { Show, NumberField, TextField, EmailField } from "@refinedev/antd";
+import { Typography } from "antd";
+
+const { Title } = Typography;
+
+export const ContactShow: React.FC<IResourceComponentsProps> = () => {
+    const { queryResult } = useShow({
+        meta: {
+            fields: [
+                "id",
+                "name",
+                "email",
+                { company: ["id", "name"] },
+                "jobTitle",
+                "phone",
+                "status",
+            ],
+        },
+    });
+    const { data, isLoading } = queryResult;
+    const record = data?.data;
+
+    return (
+        <Show isLoading={isLoading}>
+            <Title level={5}>Id</Title>
+            <NumberField value={record?.id ?? ""} />
+            <Title level={5}>Name</Title>
+            <TextField value={record?.name} />
+            <Title level={5}>Email</Title>
+            <EmailField value={record?.email} />
+            <Title level={5}>Company</Title>
+            <TextField value={record?.company?.name} />
+            <Title level={5}>Job Title</Title>
+            <TextField value={record?.jobTitle} />
+            <Title level={5}>Phone</Title>
+            <TextField value={record?.phone} />
+            <Title level={5}>Status</Title>
+            <TextField value={record?.status} />
+        </Show>
+    );
+};
