605454a82eca347884927f642f9c4e04af3eb678
Ali Emir Şen <senaliemir@gmail.com>
2023-09-18 18:14:41 +0300

    chore(devtools-shared): separated latest version and available package types

--- packages/devtools-shared/src/index.ts
+++ packages/devtools-shared/src/index.ts
@@ -1,7 +1,11 @@
 export { DevtoolsEvent, DevtoolsEventPayloads } from "./event-types";
 export { TraceType } from "./trace";
 export { Feed, FeedSection } from "./feed";
-export { PackageType } from "./package";
+export {
+    PackageType,
+    PackageLatestVersionType,
+    AvailablePackageType,
+} from "./package";
 export { RefineHook, Scopes, hooksByScope, scopes } from "./scopes";
 
 export { DevToolsContextProvider, DevToolsContext } from "./context";
