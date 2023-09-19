ed7391361dfd139fb0a8cb0fb192224c39fbea31
necatiozmen <necatiozmn@gmail.com>
2023-09-19 11:18:33 +0300

    chore: add hericons example

---
+++ examples/blog-heroicons-example/src/components/icons/Avatar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Avatar = () => {
+    return (
+        <a href="/">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="avatar"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+            </svg>
+        </a>
+    );
+};
