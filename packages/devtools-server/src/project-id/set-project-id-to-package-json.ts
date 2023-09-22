afd4992e40e08634fb3a8ded7c81771100150968
Ali Emir Şen <senaliemir@gmail.com>
2023-09-22 16:24:54 +0300

    chore(devtools-server): add project-id actions

---
+++ packages/devtools-server/src/project-id/set-project-id-to-package-json.ts
@@ -0,0 +1,15 @@
+import execa from "execa";
+
+export const setProjectIdToPackageJson = async (
+    projectId: string,
+    projectPath = process.cwd(),
+) => {
+    try {
+        execa.sync("npm", ["pkg", "set", `refine.projectId=${projectId}`], {
+            cwd: projectPath,
+        });
+        return true;
+    } catch (e) {
+        return null;
+    }
+};
