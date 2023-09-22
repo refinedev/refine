9844522e885d5a8c81b85b84a0d8d147a9f526dd
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-09-22 17:10:00 +0300

    feat: split antd into manual chunk

--- examples/app-crm/vite.config.ts
+++ examples/app-crm/vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tsconfigPaths(), react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    antd: ["antd"],
+                },
+            },
+        },
+    },
 });
