a3aab443c003bcc5564fa934e35d9a72aa3ea245
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-09-25 17:18:36 +0300

    chore(devtools-ui): persistently store monitor filters in local storage (#5005)
    * chore(devtools-ui): only show clear filter button if there are filters
    
    * fix(devtools-ui): add highlight value to parent filters
    
    * feat(devtools-ui): add persistent filters
    
    * chore(devtools-ui): add changeset
    
    ---------
    
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>

---
+++ packages/devtools-ui/src/utils/local-storage.ts
@@ -0,0 +1,22 @@
+export const getLocalStorage = <T>(name: string, defaultValue: T): T => {
+    if (typeof window === "undefined") {
+        return defaultValue;
+    }
+
+    try {
+        const value = window.localStorage.getItem(name);
+        return value ? JSON.parse(value) : defaultValue;
+    } catch (error) {
+        return defaultValue;
+    }
+};
+
+export const setLocalStorage = <T>(name: string, newValue: T) => {
+    if (typeof window === "undefined") {
+        return;
+    }
+
+    try {
+        window.localStorage.setItem(name, JSON.stringify(newValue));
+    } catch (error) {}
+};
