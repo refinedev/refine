031400caaed37e98ca6d6a01e6c4e6be29b0c77a
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-09-25 18:28:14 +0300

    feat(devtools): add feature slide to login and onboarding page (#4985)
    * feat(devtools): add feature slide to login and onboarding page
    
    * feat(devtools): add auto slide change
    
    * feat(devtools): ui improvements
    
    * chore(devtools): add changeset
    
    * chore(devtools-ui): update responsiveness in average screens
    
    * chore(devtools-ui): update active item background url
    
    ---------
    
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>

--- packages/devtools-ui/src/components/sidebar.tsx
+++ packages/devtools-ui/src/components/sidebar.tsx
@@ -15,31 +15,7 @@ import { ChatbotIcon } from "./icons/chatbot";
 
 import { NavLink, useLocation } from "react-router-dom";
 import { HiddenItemsBgIcon } from "./icons/hidden-items-bg";
-
-const ActiveItemBackground = ({ active }: { active?: boolean }) => {
-    return (
-        <div
-            className={clsx(
-                "re-absolute",
-                "re-left-0",
-                "re-top-0",
-                "re-h-full",
-                "re-w-full",
-                active ? "re-scale-100" : "re-scale-0",
-                "re-transition-all",
-                "re-duration-300",
-                "re-ease-[cubic-bezier(.25,.75,.5,1.25)]",
-            )}
-            style={{
-                background:
-                    "url(https://refine.ams3.cdn.digitaloceanspaces.com/devtools/active-background-icon.png)",
-                backgroundSize: "36px 36px",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-            }}
-        />
-    );
-};
+import { ActiveItemBackground } from "./active-item-background";
 
 const SidebarItem = ({
     item: { icon, path, soon, label: itemLabel },
