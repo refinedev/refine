62f35cafa15f1cb8da4d957f9e1ea5370b00f2ea
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-09-29 12:57:15 +0300

    feat(core): add resource parameter to useGo hook

---
+++ .changeset/wicked-wolves-hunt.md
@@ -0,0 +1,42 @@
+---
+"@refinedev/core": minor
+---
+
+Feature: The `go` function returned by `useGo` now accepts a `resource` object as the `to` parameter.
+From now now, you can provide either a string or a resource object to the `go` function. When a resource object is passed, it will be transformed into the path defined within the resources array of the `<Refine />` component.
+
+`to` accepts an object with the following shape to navigate to a resource:
+
+| Name     | Type                                                        | Description                                                 |
+| -------- | ----------------------------------------------------------- | ----------------------------------------------------------- |
+| resource | `string`                                                    | resource name or identifier.                                |
+| id       | [`BaseKey`][basekey]                                        | required when `action` is `"edit"`, `"show"`, or `"clone"`. |
+| action   | `"list"` \| `"create"` \| `"edit"` \| `"show"` \| `"clone"` | action name.                                                |
+
+```tsx
+import { useGo } from "@refinedev/core";
+
+const MyComponent = () => {
+    const go = useGo();
+
+    return (
+        <Button
+            onClick={() => {
+                go({
+                    to:  {
+                        resource: "posts", // resource name or identifier
+                        action: "edit",
+                        id: "1",
+                    }
+                    query: {
+                         foo: "bar",
+                    },
+                    type: "push",
+                });
+            }}
+        >
+            Go Posts With Default Filters
+        </Button>
+    );
+};
+```
--- packages/core/src/hooks/router/use-go/index.tsx
+++ packages/core/src/hooks/router/use-go/index.tsx
@@ -1,15 +1,102 @@
 import { RouterBindingsContext } from "@contexts/router";
+import { useResource } from "@hooks/resource";
 import React, { useContext } from "react";
+import type {
+    Action,
+    BaseKey,
+    GoConfig as GoConfigBase,
+    IResourceItem,
+} from "../../../interfaces";
+
+type ResourceWithoutId = {
+    /**
+     *  The name or identifier of the resource.
+     */
+    resource: string;
+    action: Extract<Action, "create" | "list">;
+    id?: never;
+};
+
+type ResourceWithId = {
+    /**
+     *  The name or identifier of the resource.
+     */
+    resource: string;
+    action: Extract<Action, "edit" | "show" | "clone">;
+    id: BaseKey;
+};
+
+export type Resource = ResourceWithoutId | ResourceWithId;
+
+export type GoConfigWithResource = Omit<GoConfigBase, "to"> & {
+    to?: GoConfigBase["to"] | Resource;
+};
 
 export const useGo = () => {
     const bindings = useContext(RouterBindingsContext);
+    const { select: resourceSelect } = useResource();
 
     const useGo = React.useMemo(
         () => bindings?.go ?? (() => () => undefined),
         [bindings?.go],
     );
 
-    const go = useGo();
+    const goFromRouter = useGo();
+
+    const go = (config: GoConfigWithResource | GoConfigBase) => {
+        if (typeof config.to !== "object") {
+            return goFromRouter({ ...config, to: config.to });
+        }
+
+        // when config.to is an object, it means that we want to go to a resource.
+        // so we need to find the path for the resource.
+        const { resource } = resourceSelect(config.to.resource);
+        const newTo = findToPathFromResource(config.to, resource);
+
+        return goFromRouter({
+            ...config,
+            to: newTo,
+        });
+    };
 
     return go;
+};
+
+/**
+ * Find the path for a resource.
+ * @internal
+ */
+export const findToPathFromResource = (
+    to: Resource,
+    resource: IResourceItem,
+) => {
+    if (!to?.action || !to?.resource) {
+        throw new Error(`[useGo]: "action" or "resource" is required.`);
+    }
+
+    if (["edit", "show", "clone"].includes(to?.action) && !to.id) {
+        throw new Error(
+            `[useGo]: [action: ${to.action}] requires an "id" for resource [resource: ${to.resource}]`,
+        );
+    }
+
+    const actionUrl = resource[to.action];
+    if (!actionUrl) {
+        throw new Error(
+            `[useGo]: [action: ${to.action}] is not defined for [resource: ${to.resource}]`,
+        );
+    }
+
+    let newTo = "";
+    if (typeof actionUrl === "string") {
+        newTo = actionUrl;
+    }
+    if (typeof actionUrl === "object") {
+        newTo = actionUrl.path;
+    }
+    if (typeof to.id !== "undefined") {
+        newTo = newTo.replace(":id", to.id.toString());
+    }
+
+    return newTo;
 };
