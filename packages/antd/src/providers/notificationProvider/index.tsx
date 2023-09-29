cc4e3b5030e6050a534b9fda338df3deb32f1a2b
Mahir <mahdimahir45@gmail.com>
2023-09-29 10:27:00 +0600

    fix: antd notificationProvider could not access theme context with static method

--- packages/antd/src/providers/notificationProvider/index.tsx
+++ packages/antd/src/providers/notificationProvider/index.tsx
@@ -1,41 +1,53 @@
-import React from "react";
 import { NotificationProvider } from "@refinedev/core";
-import { notification } from "antd";
+import { App, notification as staticNotification } from "antd";
+import React from "react";
 
 import { UndoableNotification } from "@components/undoableNotification";
 
-export const notificationProvider: NotificationProvider = {
-    open: ({
-        key,
-        message,
-        description,
-        type,
-        cancelMutation,
-        undoableTimeout,
-    }) => {
-        if (type === "progress") {
-            notification.open({
-                key,
-                description: (
-                    <UndoableNotification
-                        notificationKey={key}
-                        message={message}
-                        cancelMutation={cancelMutation}
-                        undoableTimeout={undoableTimeout}
-                    />
-                ),
-                message: null,
-                duration: 0,
-                closeIcon: <></>,
-            });
-        } else {
-            notification.open({
-                key,
-                description: message,
-                message: description ?? null,
-                type,
-            });
-        }
-    },
-    close: (key) => notification.destroy(key),
+const { useApp } = App;
+
+export const notificationProvider = () => {
+    const { notification: notificationFromContext } = useApp();
+    const notification =
+        "open" in notificationFromContext
+            ? notificationFromContext
+            : staticNotification;
+
+    const notificationProvider: NotificationProvider = {
+        open: ({
+            key,
+            message,
+            description,
+            type,
+            cancelMutation,
+            undoableTimeout,
+        }) => {
+            if (type === "progress") {
+                notification.open({
+                    key,
+                    description: (
+                        <UndoableNotification
+                            notificationKey={key}
+                            message={message}
+                            cancelMutation={cancelMutation}
+                            undoableTimeout={undoableTimeout}
+                        />
+                    ),
+                    message: null,
+                    duration: 0,
+                    closeIcon: <></>,
+                });
+            } else {
+                notification.open({
+                    key,
+                    description: message,
+                    message: description ?? null,
+                    type,
+                });
+            }
+        },
+        close: (key) => notification.destroy(key),
+    };
+
+    return notificationProvider;
 };
