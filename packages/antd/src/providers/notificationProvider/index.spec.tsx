a4f413f9bc9f30cf9530bba42f4a11c0ddcdf67c
Mahir <mahdimahir45@gmail.com>
2023-10-02 19:59:58 +0600

    fix: added deprecated notification provider tests and updated current notification provider tests

--- packages/antd/src/providers/notificationProvider/index.spec.tsx
+++ packages/antd/src/providers/notificationProvider/index.spec.tsx
@@ -4,7 +4,7 @@ import { renderHook, waitFor } from "@testing-library/react";
 import { notification, App } from "antd";
 import { UndoableNotification } from "@components/undoableNotification";
 import { act } from "@test";
-import { notificationProvider } from ".";
+import { notificationProvider, useNotificationProvider } from ".";
 
 const mockNotification: OpenNotificationParams = {
     key: "test-notification",
@@ -15,6 +15,87 @@ const mockNotification: OpenNotificationParams = {
 const cancelMutation = jest.fn();
 
 describe("Antd notificationProvider", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    const notificationOpenSpy = jest.spyOn(notification, "open");
+    const notificationCloseSpy = jest.spyOn(notification, "destroy");
+
+    it("should render notification type succes notification", async () => {
+        notificationProvider.open?.(mockNotification);
+
+        expect(notificationOpenSpy).toBeCalledTimes(1);
+        expect(notificationOpenSpy).toBeCalledWith({
+            ...mockNotification,
+            message: null,
+            description: mockNotification.message,
+        });
+    });
+
+    it("should render notification type error notification", async () => {
+        notificationProvider.open?.({
+            ...mockNotification,
+            type: "error",
+        });
+
+        expect(notificationOpenSpy).toBeCalledTimes(1);
+        expect(notificationOpenSpy).toBeCalledWith({
+            ...mockNotification,
+            message: null,
+            description: mockNotification.message,
+            type: "error",
+        });
+    });
+
+    it("should render notification with description", async () => {
+        notificationProvider.open?.({
+            ...mockNotification,
+            description: "Notification Description",
+        });
+
+        expect(notificationOpenSpy).toBeCalledTimes(1);
+        expect(notificationOpenSpy).toBeCalledWith({
+            ...mockNotification,
+            message: "Notification Description",
+            description: "Test Notification Message",
+        });
+    });
+
+    it("should render notification type error notification", async () => {
+        notificationProvider.open?.({
+            ...mockNotification,
+            type: "progress",
+            cancelMutation,
+            undoableTimeout: 5,
+        });
+
+        expect(notificationOpenSpy).toBeCalledTimes(1);
+        expect(notificationOpenSpy).toBeCalledWith({
+            key: "test-notification",
+            message: null,
+            closeIcon: <React.Fragment />,
+            description: (
+                <UndoableNotification
+                    message="Test Notification Message"
+                    notificationKey="test-notification"
+                    cancelMutation={cancelMutation}
+                    undoableTimeout={5}
+                />
+            ),
+            duration: 0,
+        });
+    });
+
+    it("should close notification", async () => {
+        notificationProvider.close?.("notification-key");
+
+        expect(notificationCloseSpy).toBeCalledTimes(1);
+        expect(notificationCloseSpy).toBeCalledWith("notification-key");
+    });
+});
+
+describe("Antd useNotificationProvider", () => {
     describe("using without Ant design's App component", () => {
         afterEach(() => {
             jest.clearAllMocks();
@@ -24,7 +105,7 @@ describe("Antd notificationProvider", () => {
         const notificationCloseSpy = jest.spyOn(notification, "destroy");
 
         it("should render notification type succes notification", async () => {
-            const { result } = renderHook(() => notificationProvider(), {});
+            const { result } = renderHook(() => useNotificationProvider(), {});
 
             result.current.open?.(mockNotification);
 
@@ -37,7 +118,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should render notification type error notification", async () => {
-            const { result } = renderHook(() => notificationProvider(), {});
+            const { result } = renderHook(() => useNotificationProvider(), {});
 
             result.current.open?.({
                 ...mockNotification,
@@ -54,7 +135,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should render notification with description", async () => {
-            const { result } = renderHook(() => notificationProvider(), {});
+            const { result } = renderHook(() => useNotificationProvider(), {});
 
             result.current.open?.({
                 ...mockNotification,
@@ -70,7 +151,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should render notification type error notification", async () => {
-            const { result } = renderHook(() => notificationProvider(), {});
+            const { result } = renderHook(() => useNotificationProvider(), {});
 
             result.current.open?.({
                 ...mockNotification,
@@ -97,7 +178,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should close notification", async () => {
-            const { result } = renderHook(() => notificationProvider(), {});
+            const { result } = renderHook(() => useNotificationProvider(), {});
 
             result.current.close?.("notification-key");
 
@@ -124,7 +205,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should render notification type succes notification", async () => {
-            const { result } = renderHook(() => notificationProvider());
+            const { result } = renderHook(() => useNotificationProvider());
 
             act(() => {
                 result.current.open?.(mockNotification);
@@ -141,7 +222,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should render notification type error notification", async () => {
-            const { result } = renderHook(() => notificationProvider());
+            const { result } = renderHook(() => useNotificationProvider());
 
             act(() => {
                 result.current.open?.({
@@ -162,7 +243,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should render notification with description", async () => {
-            const { result } = renderHook(() => notificationProvider());
+            const { result } = renderHook(() => useNotificationProvider());
 
             act(() => {
                 result.current.open?.({
@@ -182,7 +263,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should render notification type error notification", async () => {
-            const { result } = renderHook(() => notificationProvider());
+            const { result } = renderHook(() => useNotificationProvider());
 
             act(() => {
                 result.current.open?.({
@@ -213,7 +294,7 @@ describe("Antd notificationProvider", () => {
         });
 
         it("should close notification", async () => {
-            const { result } = renderHook(() => notificationProvider());
+            const { result } = renderHook(() => useNotificationProvider());
 
             act(() => {
                 result.current.close?.("notification-key");
