f76fa288c2b01614e9f99b5ff5bdc3d11674bab0
Ali Emir Şen <senaliemir@gmail.com>
2023-10-04 11:56:16 +0300

    fix(devtools-server): use port constant for consistency

--- packages/devtools-server/src/setup-server.ts
+++ packages/devtools-server/src/setup-server.ts
@@ -1,9 +1,10 @@
 import type { Express } from "express";
+import { SERVER_PORT } from "./constants";
 
 export const setupServer = (app: Express) => {
-    const server = app.listen(5001, () => {
+    const server = app.listen(SERVER_PORT, () => {
         if (__DEVELOPMENT__) {
-            console.log("Server started on PORT 5001");
+            console.log(`Server started on PORT ${SERVER_PORT}`);
         }
     });
 
