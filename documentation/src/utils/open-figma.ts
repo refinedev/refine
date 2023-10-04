8e002f0b680f921c896733b3b4b8007206f5d732
Omer Aplak <omer@pankod.com>
2023-10-04 15:43:31 +0300

    chore: typo fix

--- documentation/src/utils/open-figma.ts
+++ documentation/src/utils/open-figma.ts
@@ -1,7 +1,7 @@
 export const openFigma = () => {
     return window
         .open(
-            "https://www.figma.com/file/y8lhTyumAN0zYI5nKKLoLN/refine.dev-brand-assets",
+            "https://s.refine.dev/logo",
             "_blank",
         )
         ?.focus();
