0f3177bf6cf2e0d09b60991f6e82b50356d5c229
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-10-05 16:37:16 +0300

    feat(docs): update top announcement banner (#5068)
    * feat(docs): update top announcement banner
    
    * feat(docs): responsive ui improvements
    
    * feat(docs): update banner link

--- documentation/src/refine-theme/top-announcement.tsx
+++ documentation/src/refine-theme/top-announcement.tsx
@@ -1,43 +1,201 @@
 import clsx from "clsx";
-import React from "react";
+import React, { SVGProps } from "react";
+import { RefineLogoCyan } from "./icons/refine-logo-cyan";
 
 export const TopAnnouncement = () => {
     return (
+        <div className={clsx("w-full h-12", "relative", "bg-top-announcement")}>
+            <div
+                className={clsx(
+                    "hidden lg:flex",
+                    "w-full h-full",
+                    "border-b border-solid border-[#47ebeb26]",
+                    "top-announcement-mask",
+                )}
+            >
+                <div
+                    className={clsx(
+                        "w-[1280px] h-full",
+                        "mx-auto",
+                        "flex",
+                        "justify-between",
+                    )}
+                >
+                    <div
+                        className={clsx(
+                            "w-[calc(50%-300px)] h-full",
+                            "relative",
+                        )}
+                    >
+                        <GlowSmall
+                            style={{
+                                animationDelay: "1.5s",
+                            }}
+                            className={clsx(
+                                "absolute",
+                                "top-[2px]",
+                                "right-[220px]",
+                            )}
+                            id={"1"}
+                        />
+                        <GlowSmall
+                            style={{
+                                animationDelay: "1s",
+                            }}
+                            className={clsx(
+                                "absolute",
+                                "rotate-180",
+                                "top-[8px] right-[100px]",
+                            )}
+                            id={"2"}
+                        />
+                        <GlowBig
+                            className={clsx("absolute", "right-[10px]")}
+                            id={"3"}
+                        />
+                    </div>
+
+                    <div
+                        className={clsx(
+                            "w-[calc(50%-300px)] h-full",
+                            "relative",
+                        )}
+                    >
+                        <GlowSmall
+                            style={{
+                                animationDelay: "2s",
+                            }}
+                            className={clsx(
+                                "absolute",
+                                "rotate-180",
+                                "top-[6px] right-[180px]",
+                            )}
+                            id={"4"}
+                        />
+                        <GlowSmall
+                            style={{
+                                animationDelay: "0.5s",
+                            }}
+                            className={clsx(
+                                "delay-[1300]",
+                                "absolute",
+                                "top-[2px]",
+                                "right-[40px]",
+                            )}
+                            id={"5"}
+                        />
+
+                        <GlowBig
+                            className={clsx("absolute", "right-[-70px]")}
+                            id={"6"}
+                        />
+                    </div>
+                </div>
+            </div>
+            <Text />
+        </div>
+    );
+};
+
+const Text = () => {
+    return (
         <a
-            href="https://github.com/refinedev/refine"
+            href="https://s.refine.dev/devtools-beta"
             target="_blank"
             rel="noreferrer"
             className={clsx(
-                "block",
-                "w-full",
-                "p-2",
-                "header-md:p-4",
-                "bg-refine-blue",
-                // "bg-opacity-[0.15]",
-                "text-base",
-                "text-gray-900",
+                "relative lg:absolute",
+                "px-2 lg:px-0",
+                "top-0",
+                "left-[50%]",
+                "translate-x-[-50%]",
+                "bg-top-announcement-text",
+                "h-full w-full lg:w-[680px]",
+                "flex items-center justify-center",
+                "text-white",
+                "text-xs",
                 "text-center",
                 "no-underline",
                 "hover:no-underline",
-                "hover:text-gray-900",
             )}
-            style={{
-                borderBottom: "1px solid rgba(0, 128, 255, 0.15)",
-                background:
-                    "linear-gradient(180deg, rgba(0, 128, 255, 0.05) 0%, rgba(110, 179, 247, 0.00) 100%), #F6FAFE",
-            }}
         >
-            🧙‍♂️ refine grants your wishes! Please give us a ⭐️ on{" "}
-            <span
-                className={clsx(
-                    "font-bold",
-                    "text-refine-blue",
-                    "hover:text-refine-blue-dark",
-                )}
-            >
-                GitHub
-            </span>{" "}
-            to keep the magic going.
+            <RefineLogoCyan className="flex-shrink-0" />
+            <div className={clsx("ml-2")}>
+                Unlock the power of{" "}
+                <span className={clsx("font-semibold")}>refine Devtools</span>,
+                your intelligent companion for development.
+                <span
+                    className={clsx("text-[#47EBEB]", "font-semibold", "ml-2")}
+                >
+                    Learn More
+                </span>
+            </div>
         </a>
     );
 };
+
+const GlowSmall = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={80}
+        height={40}
+        fill="none"
+        {...props}
+        className={clsx(
+            "animate-top-announcement-glow",
+            "opacity-1",
+            props.className,
+        )}
+    >
+        <circle cx={40} r={40} fill={`url(#${props.id}-a)`} fillOpacity={0.5} />
+        <defs>
+            <radialGradient
+                id={`${props.id}-a`}
+                cx={0}
+                cy={0}
+                r={1}
+                gradientTransform="matrix(0 40 -40 0 40 0)"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#47EBEB" />
+                <stop offset={1} stopColor="#47EBEB" stopOpacity={0} />
+            </radialGradient>
+        </defs>
+    </svg>
+);
+
+const GlowBig = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={120}
+        height={48}
+        fill="none"
+        {...props}
+        className={clsx(
+            "animate-top-announcement-glow",
+            "opacity-1",
+            props.className,
+        )}
+    >
+        <circle
+            cx={60}
+            cy={24}
+            r={60}
+            fill={`url(#${props.id}-a)`}
+            fillOpacity={0.5}
+        />
+        <defs>
+            <radialGradient
+                id={`${props.id}-a`}
+                cx={0}
+                cy={0}
+                r={1}
+                gradientTransform="matrix(0 60 -60 0 60 24)"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#47EBEB" />
+                <stop offset={1} stopColor="#47EBEB" stopOpacity={0} />
+            </radialGradient>
+        </defs>
+    </svg>
+);
