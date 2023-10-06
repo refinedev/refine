128497d014a0ae1216e4401f28b2c1b6e6ab6265
Necati Özmen <necatiozmn@gmail.com>
2023-10-06 17:56:50 +0300

    docs(blog): add react hot toast post (#5072)
    * docs(blog): add react hot toast post
    
    * chore: add react hot toast example
    
    * chore: update package
    
    * chore: update package json
    
    * docs(blog): update social
    
    ---------
    
    Co-authored-by: Batuhan Wilhelm <batuhanwilhelm@gmail.com>

---
+++ examples/blog-react-hot-toast/src/pages/categories/list.tsx
@@ -0,0 +1,6 @@
+import { IResourceComponentsProps } from "@refinedev/core";
+import { HeadlessListInferencer } from "@refinedev/inferencer/headless";
+
+export const CategoryList: React.FC<IResourceComponentsProps> = () => {
+    return <HeadlessListInferencer />;
+};
