46072c686bae4e14e6a7f0c024a68be9d7f03788
Ali Emir Şen <senaliemir@gmail.com>
2023-10-09 17:47:03 +0300

    test(devtools-ui): add tests for clean file path util

---
+++ packages/devtools-ui/src/utils/clean-file-path/index.test.ts
@@ -0,0 +1,33 @@
+import { cleanFilePath } from ".";
+
+describe("cleanFilePath", () => {
+    it("should remove the hostname from the path", () => {
+        expect(
+            cleanFilePath("http://localhost:3000/src/pages/posts/list.tsx"),
+        ).toEqual("src/pages/posts/list.tsx");
+    });
+
+    it("should remove the cache busting query string", () => {
+        expect(
+            cleanFilePath(
+                "http://localhost:3000/src/pages/posts/list.tsx?ts=1234567890",
+            ),
+        ).toEqual("src/pages/posts/list.tsx");
+    });
+
+    it("should remove webpack-internal:///", () => {
+        expect(
+            cleanFilePath("webpack-internal:///src/pages/posts/list.tsx"),
+        ).toEqual("src/pages/posts/list.tsx");
+    });
+
+    it("should return the original path if it doesn't match any custom path", () => {
+        expect(cleanFilePath("src/pages/posts/list.tsx")).toEqual(
+            "src/pages/posts/list.tsx",
+        );
+    });
+
+    it("should return undefined if the path is undefined", () => {
+        expect(cleanFilePath(undefined)).toEqual(undefined);
+    });
+});
