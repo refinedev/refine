9faf048ce872f721fca868e000f8f50fe26b40ff
Ali Emir Şen <senaliemir@gmail.com>
2023-10-09 15:48:03 +0300

    feat(devtools-server): add open in vscode endpoint

---
+++ packages/devtools-server/src/serve-open-in-editor.ts
@@ -0,0 +1,16 @@
+import { Express } from "express";
+import path from "path";
+
+export const serveOpenInEditor = (app: Express, basePath: string) => {
+    app.get("/open-in-editor/*", (req, res) => {
+        const { line, column } = req.query;
+
+        const filePath = req.path.replace("/open-in-editor", "");
+
+        const vscodeUrl = `vscode://file/${path.join(basePath, filePath)}?${
+            line ? `line=${line}` : ""
+        }${column ? `&column=${column}` : ""}`;
+
+        res.redirect(vscodeUrl);
+    });
+};
