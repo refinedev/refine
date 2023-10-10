b838412f0d0b790ba95f3c07a899a021d3cd2c84
Mahir Mahdi <66949598+MahirMahdi@users.noreply.github.com>
2023-10-10 13:42:57 +0600

    fix: antd notificationProvider bug (#4945)
    * fix: antd notificationProvider could not access theme context with static method
    
    * fix: antd notificationProvider issue
    
    * test(antd): fix notification provider tests
    
    * chore(changeset): add usage warning and diff
    
    * fix: notification provider is deprecated because it could not consume theme context
    
    * fix: added deprecated notification provider tests and updated current notification provider tests
    
    * docs: updated notification provider section
    
    * Update documentation/docs/api-reference/antd/theming.md
    
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>
    
    * fix: updated changeset
    
    * chore: refactor code examples
    
    * fix: typo on jsdoc
    
    * chore(docs): highlight code examples
    
    ---------
    
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>
    Co-authored-by: Batuhan Wilhelm <batuhanwilhelm@gmail.com>
    Co-authored-by: Alican Erdurmaz <alicanerdurmaz@gmail.com>

--- packages/antd/src/providers/notificationProvider/index.spec.tsx
+++ packages/antd/src/providers/notificationProvider/index.spec.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { OpenNotificationParams } from "@refinedev/core";
-import { notification } from "antd";
+import { renderHook, waitFor } from "@testing-library/react";
+import { notification, App } from "antd";
 import { UndoableNotification } from "@components/undoableNotification";
-
-import { notificationProvider } from ".";
+import { act } from "@test";
+import { notificationProvider, useNotificationProvider } from ".";
 
 const mockNotification: OpenNotificationParams = {
     key: "test-notification",
@@ -91,5 +92,218 @@ describe("Antd notificationProvider", () => {
 
         expect(notificationCloseSpy).toBeCalledTimes(1);
         expect(notificationCloseSpy).toBeCalledWith("notification-key");
+    });
+});
+
+describe("Antd useNotificationProvider", () => {
+    describe("using without Ant design's App component", () => {
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        const notificationOpenSpy = jest.spyOn(notification, "open");
+        const notificationCloseSpy = jest.spyOn(notification, "destroy");
+
+        it("should render notification type succes notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider(), {});
+
+            result.current.open?.(mockNotification);
+
+            expect(notificationOpenSpy).toBeCalledTimes(1);
+            expect(notificationOpenSpy).toBeCalledWith({
+                ...mockNotification,
+                message: null,
+                description: mockNotification.message,
+            });
+        });
+
+        it("should render notification type error notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider(), {});
+
+            result.current.open?.({
+                ...mockNotification,
+                type: "error",
+            });
+
+            expect(notificationOpenSpy).toBeCalledTimes(1);
+            expect(notificationOpenSpy).toBeCalledWith({
+                ...mockNotification,
+                message: null,
+                description: mockNotification.message,
+                type: "error",
+            });
+        });
+
+        it("should render notification with description", async () => {
+            const { result } = renderHook(() => useNotificationProvider(), {});
+
+            result.current.open?.({
+                ...mockNotification,
+                description: "Notification Description",
+            });
+
+            expect(notificationOpenSpy).toBeCalledTimes(1);
+            expect(notificationOpenSpy).toBeCalledWith({
+                ...mockNotification,
+                message: "Notification Description",
+                description: "Test Notification Message",
+            });
+        });
+
+        it("should render notification type error notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider(), {});
+
+            result.current.open?.({
+                ...mockNotification,
+                type: "progress",
+                cancelMutation,
+                undoableTimeout: 5,
+            });
+
+            expect(notificationOpenSpy).toBeCalledTimes(1);
+            expect(notificationOpenSpy).toBeCalledWith({
+                key: "test-notification",
+                message: null,
+                closeIcon: <React.Fragment />,
+                description: (
+                    <UndoableNotification
+                        message="Test Notification Message"
+                        notificationKey="test-notification"
+                        cancelMutation={cancelMutation}
+                        undoableTimeout={5}
+                    />
+                ),
+                duration: 0,
+            });
+        });
+
+        it("should close notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider(), {});
+
+            result.current.close?.("notification-key");
+
+            expect(notificationCloseSpy).toBeCalledTimes(1);
+            expect(notificationCloseSpy).toBeCalledWith("notification-key");
+        });
+    });
+
+    describe("using with Ant design's App component", () => {
+        const openFn = jest.fn();
+        const destroyFn = jest.fn();
+
+        beforeAll(() => {
+            jest.spyOn(App, "useApp").mockReturnValue({
+                notification: {
+                    open: openFn,
+                    destroy: destroyFn,
+                },
+            } as unknown as ReturnType<typeof App.useApp>);
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it("should render notification type succes notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider());
+
+            act(() => {
+                result.current.open?.(mockNotification);
+            });
+
+            await waitFor(() => {
+                expect(openFn).toBeCalledTimes(1);
+                expect(openFn).toBeCalledWith({
+                    ...mockNotification,
+                    message: null,
+                    description: mockNotification.message,
+                });
+            });
+        });
+
+        it("should render notification type error notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider());
+
+            act(() => {
+                result.current.open?.({
+                    ...mockNotification,
+                    type: "error",
+                });
+            });
+
+            await waitFor(() => {
+                expect(openFn).toBeCalledTimes(1);
+                expect(openFn).toBeCalledWith({
+                    ...mockNotification,
+                    message: null,
+                    description: mockNotification.message,
+                    type: "error",
+                });
+            });
+        });
+
+        it("should render notification with description", async () => {
+            const { result } = renderHook(() => useNotificationProvider());
+
+            act(() => {
+                result.current.open?.({
+                    ...mockNotification,
+                    description: "Notification Description",
+                });
+            });
+
+            await waitFor(() => {
+                expect(openFn).toBeCalledTimes(1);
+                expect(openFn).toBeCalledWith({
+                    ...mockNotification,
+                    message: "Notification Description",
+                    description: "Test Notification Message",
+                });
+            });
+        });
+
+        it("should render notification type error notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider());
+
+            act(() => {
+                result.current.open?.({
+                    ...mockNotification,
+                    type: "progress",
+                    cancelMutation,
+                    undoableTimeout: 5,
+                });
+            });
+
+            await waitFor(() => {
+                expect(openFn).toBeCalledTimes(1);
+                expect(openFn).toBeCalledWith({
+                    key: "test-notification",
+                    message: null,
+                    closeIcon: <React.Fragment />,
+                    description: (
+                        <UndoableNotification
+                            message="Test Notification Message"
+                            notificationKey="test-notification"
+                            cancelMutation={cancelMutation}
+                            undoableTimeout={5}
+                        />
+                    ),
+                    duration: 0,
+                });
+            });
+        });
+
+        it("should close notification", async () => {
+            const { result } = renderHook(() => useNotificationProvider());
+
+            act(() => {
+                result.current.close?.("notification-key");
+            });
+
+            await waitFor(() => {
+                expect(destroyFn).toBeCalledTimes(1);
+                expect(destroyFn).toBeCalledWith("notification-key");
+            });
+        });
     });
 });
