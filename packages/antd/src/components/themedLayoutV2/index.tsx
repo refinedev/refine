f97f03af346e426432e0e565d512aa0f580259d0
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-10-19 15:45:18 +0300

    fix(antd): use memoize

--- packages/antd/src/components/themedLayoutV2/index.tsx
+++ packages/antd/src/components/themedLayoutV2/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Grid, Layout as AntdLayout } from "antd";
 
 import { ThemedSiderV2 as DefaultSider } from "./sider";
@@ -17,15 +17,18 @@ export const ThemedLayoutV2: React.FC<RefineThemedLayoutV2Props> = ({
 }) => {
     const breakpoint = Grid.useBreakpoint();
     const SiderToRender = Sider ?? DefaultSider;
-    const hasSider = SiderToRender({ Title });
     const HeaderToRender = Header ?? DefaultHeader;
     const isSmall = typeof breakpoint.sm === "undefined" ? true : breakpoint.sm;
+
+    const hasSider = useMemo(() => {
+        return !!SiderToRender({ Title });
+    }, [SiderToRender, Title]);
 
     return (
         <ThemedLayoutContextProvider
             initialSiderCollapsed={initialSiderCollapsed}
         >
-            <AntdLayout style={{ minHeight: "100vh" }} hasSider={!!hasSider}>
+            <AntdLayout style={{ minHeight: "100vh" }} hasSider={hasSider}>
                 <SiderToRender Title={Title} />
                 <AntdLayout>
                     <HeaderToRender />
