b7f0af3e90e74120c2e1598625ce7759a7d14c0e
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-10-23 11:48:25 +0300

    feat(examples): add data-provider-sanity

---
+++ examples/data-provider-sanity/src/interfaces/index.d.ts
@@ -0,0 +1,27 @@
+export interface ICategory {
+    _updatedAt: string;
+    _createdAt: string;
+    _rev: string;
+    _type: string;
+    description: string;
+    _id: string;
+    title: string;
+}
+
+export interface IPost {
+    _createdAt: string;
+    content: string;
+    slug: {
+        current: string;
+    };
+    title: string;
+    _id: string;
+    categories: {
+        _ref: string;
+        _type: string;
+        _key: string;
+    }[];
+    _type: string;
+    _updatedAt: string;
+    _rev: string;
+}
---
+++ examples/data-provider-sanity/src/pages/posts/show.tsx
@@ -0,0 +1,43 @@
+import { useShow, IResourceComponentsProps, useMany } from "@refinedev/core";
+import { Show, MarkdownField } from "@refinedev/antd";
+
+import { Space, Tag, Typography } from "antd";
+import { ICategory, IPost } from "../../interfaces";
+
+const { Title, Text } = Typography;
+
+export const PostShow: React.FC<IResourceComponentsProps> = () => {
+    const { queryResult } = useShow<IPost>();
+    const { data, isLoading } = queryResult;
+    const record = data?.data;
+
+    const categoryIds =
+        record?.categories?.map((category) => category._ref) || [];
+    const { data: dataCategories } = useMany<ICategory>({
+        resource: "category",
+        ids: categoryIds,
+    });
+
+    return (
+        <Show isLoading={isLoading}>
+            <Title level={5}>Id</Title>
+            <Text>{record?._id}</Text>
+
+            <Title level={5}>Title</Title>
+            <Text>{record?.title}</Text>
+
+            <Title level={5}>Slug</Title>
+            <Text>{record?.slug?.current}</Text>
+
+            <Title level={5}>Content</Title>
+            <MarkdownField value={record?.content} />
+
+            <Title level={5}>Categories</Title>
+            <Space>
+                {dataCategories?.data?.map((category) => (
+                    <Tag key={category._id}>{category.title}</Tag>
+                ))}
+            </Space>
+        </Show>
+    );
+};
