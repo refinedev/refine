66c27b49de24242d77acbec20467cd6144e4b3ee
Ömer Faruk APLAK <omer@pankod.com>
2023-10-30 11:03:59 +0300

    chore: update team

--- documentation/src/assets/team.ts
+++ documentation/src/assets/team.ts
@@ -25,17 +25,7 @@ export const team = [
     {
         name: "Ali Emir Şen",
         avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/ali.png",
-        role1: "Frontend Team Lead",
-    },
-    {
-        name: "Yıldıray Ünlü",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/yildiray.png",
-        role1: "Backend Team Lead",
-    },
-    {
-        name: "Salih Özdemir",
-        avatar: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/about/images/team-avatars/salih.png",
-        role1: "Software Developer",
+        role1: "Senior Software Developer",
     },
     {
         name: "Alican Erdurmaz",
