8d360925a2946d36687bb977626d7e347b3986b0
Necati Özmen <necatiozmn@gmail.com>
2023-11-02 13:16:06 +0300

    chore: add bit post example (#5213)
    * chore: add bit post example
    
    * chore: update package json
    
    * chore: update example package
    
    * chore: update example import
    
    * chore: update type

---
+++ examples/blog-refine-mui/src/components/charts/AreaGraph.tsx
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
+
+import { IAreaGraphProps } from "../../interfaces";
+
+export const formatDate = new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    year: "numeric",
+    day: "numeric",
+});
+
+export const AreaGraph: React.FC<IAreaGraphProps> = ({
+    data,
+    stroke,
+    fill,
+}) => {
+    const transformedData = data.map(({ date, value }) => ({
+        date: formatDate.format(new Date(date)),
+        value,
+    }));
+
+    return (
+        <ResponsiveContainer width="99%" aspect={3}>
+            <AreaChart data={transformedData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" />
+                <YAxis dataKey="value" />
+                <Tooltip label="Daily Revenue" />
+                <Area
+                    type="monotone"
+                    dataKey="value"
+                    stroke={stroke}
+                    fill={fill}
+                />
+            </AreaChart>
+        </ResponsiveContainer>
+    );
+};
---
+++ examples/blog-refine-mui/src/pages/products/show.tsx
@@ -0,0 +1,73 @@
+import {
+    useShow,
+    IResourceComponentsProps,
+    useTranslate,
+    useOne,
+} from "@refinedev/core";
+import {
+    Show,
+    NumberField,
+    TextFieldComponent as TextField,
+    BooleanField,
+    MarkdownField,
+    DateField,
+} from "@refinedev/mui";
+
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+
+export const ProductShow: React.FC<IResourceComponentsProps> = () => {
+    const translate = useTranslate();
+    const { queryResult } = useShow();
+    const { data, isLoading } = queryResult;
+
+    const record = data?.data;
+
+    const { data: categoryData, isLoading: categoryIsLoading } = useOne({
+        resource: "categories",
+        id: record?.category?.id || "",
+        queryOptions: {
+            enabled: !!record,
+        },
+    });
+
+    return (
+        <Show isLoading={isLoading}>
+            <Stack gap={1}>
+                <Typography variant="body1" fontWeight="bold">
+                    {translate("id")}
+                </Typography>
+                <NumberField value={record?.id ?? ""} />
+                <Typography variant="body1" fontWeight="bold">
+                    {translate("Name")}
+                </Typography>
+                <TextField value={record?.name} />
+                <Typography variant="body1" fontWeight="bold">
+                    {translate("IsActive")}
+                </Typography>
+                <BooleanField value={record?.isActive} />
+                <Typography variant="body1" fontWeight="bold">
+                    {translate("Description")}
+                </Typography>
+                <MarkdownField value={record?.description} />
+                <Typography variant="body1" fontWeight="bold">
+                    {translate("CreatedAt")}
+                </Typography>
+                <DateField value={record?.createdAt} />
+                <Typography variant="body1" fontWeight="bold">
+                    {translate("Price")}
+                </Typography>
+                <NumberField value={record?.price ?? ""} />
+                <Typography variant="body1" fontWeight="bold">
+                    {translate("Category")}
+                </Typography>
+
+                {categoryIsLoading ? (
+                    <>Loading...</>
+                ) : (
+                    <>{categoryData?.data?.title}</>
+                )}
+            </Stack>
+        </Show>
+    );
+};
