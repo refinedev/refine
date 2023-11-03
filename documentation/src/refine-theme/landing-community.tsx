42cfc9bd0d67c5245ab4c05a10d406242fb8822b
Ali Emir Şen <senaliemir@gmail.com>
2023-11-03 16:53:17 +0300

    docs(landing): fix aspect ratio

--- documentation/src/refine-theme/landing-community.tsx
+++ documentation/src/refine-theme/landing-community.tsx
@@ -140,6 +140,9 @@ export const LandingCommunity: FC<Props> = ({ className }) => {
                         src={`https://refine.ams3.cdn.digitaloceanspaces.com/website/static/assets/investors${
                             colorMode === "dark" ? "-dark" : ""
                         }.png`}
+                        style={{
+                            aspectRatio: "908/544",
+                        }}
                         alt="investors"
                         loading="lazy"
                     />
