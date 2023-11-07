c4efe6da470531d58462c8acb6e891a3473188d1
Ali Emir Şen <senaliemir@gmail.com>
2023-11-07 10:35:09 +0300

    docs(landing): update footer

--- documentation/src/refine-theme/landing-footer.tsx
+++ documentation/src/refine-theme/landing-footer.tsx
@@ -1,15 +1,8 @@
 import clsx from "clsx";
 import React from "react";
 import { openFigma } from "../utils/open-figma";
-import {
-    footerDescription,
-    menuItems,
-    secondaryMenuItems,
-    socialLinks,
-} from "./footer-data";
+import { menuItems, secondaryMenuItems, socialLinks } from "./footer-data";
 import { HeartOutlinedIcon } from "./icons/heart-outlined";
-import { RefineLogoIcon } from "./icons/refine-logo";
-import { ProductHuntLogo } from "./product-hunt-logo";
 import Link from "@docusaurus/Link";
 import { RefineLogoSingleIcon } from "./icons/refine-logo-single";
 import { PHBadgeIcon } from "./icons/ph-badge";
@@ -145,6 +138,7 @@ export const LandingFooter = () => {
                     >
                         <Link
                             to="/"
+                            onContextMenu={openFigma}
                             className={clsx(
                                 "hover:no-underline",
                                 "text-gray-900 dark:text-gray-0",
@@ -153,7 +147,7 @@ export const LandingFooter = () => {
                             <RefineLogoSingleIcon />
                         </Link>
                         <a
-                            href="#"
+                            href="https://www.producthunt.com/posts/refine-3?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-refine&#0045;3"
                             target="_blank"
                             rel="noreferrer"
                             className={clsx("hover:no-underline")}
@@ -329,228 +323,5 @@ export const LandingFooter = () => {
                 </div>
             </div>
         </footer>
-    );
-    return (
-        <>
-            <div
-                className={clsx(
-                    "border-t",
-                    "border-t-refine-landing-footer-border",
-                    "bg-refine-landing-footer-bg",
-                    "w-full",
-                    "py-6",
-                    "landing-sm:py-8",
-                    "landing-md:py-11",
-                    "px-4",
-                    "landing-sm:px-8",
-                    "landing-md:px-8",
-                    "landing-lg:px-12",
-                    "landing-xl:px-20",
-                )}
-            >
-                <div
-                    className={clsx(
-                        "max-w-screen-landing-footer",
-                        "w-full",
-                        "flex flex-col",
-                        "gap-6",
-                        "mx-auto",
-                    )}
-                >
-                    <div className={clsx("flex items-center justify-between")}>
-                        <RefineLogoIcon
-                            className="text-gray-0"
-                            onContextMenu={openFigma}
-                        />
-                        <ProductHuntLogo className="landing-md:block hidden" />
-                    </div>
-                    <div
-                        className={clsx(
-                            "flex",
-                            "flex-col",
-                            "landing-xl:flex-row",
-                            "items-start",
-                            "justify-between",
-                            "landing-lg:gap-8",
-                        )}
-                    >
-                        <div
-                            className={clsx(
-                                "text-xs",
-                                "opacity-75",
-                                "landing-lg:opacity-100",
-                                "landing-lg:text-base",
-                                "text-gray-0",
-                                "landing-md:max-w-[304px] w-full",
-                                "w-full",
-                                "flex-shrink-0",
-                                "mb-6",
-                                "landing-xl:mb-0",
-                            )}
-                        >
-                            {footerDescription}
-                        </div>
-                        <ProductHuntLogo
-                            className={clsx(
-                                "landing-md:hidden block",
-                                "mx-auto mb-10",
-                            )}
-                        />
-                        <div
-                            className={clsx(
-                                "w-full",
-                                "landing-lg:w-auto",
-                                "landing-md:grid-cols-3 landing-lg:grid-cols-5 grid grid-cols-2",
-                                "landing-md:gap-4 gap-10",
-                            )}
-                        >
-                            {menuItems.map((menu) => (
-                                <div
-                                    className={clsx(
-                                        "flex min-w-[144px] max-w-[304px] flex-col gap-4",
-                                    )}
-                                    key={menu.label}
-                                >
-                                    <div className="text-gray-0 text-base font-semibold">
-                                        {menu.label}
-                                    </div>
-                                    <div className="flex flex-col gap-2">
-                                        {menu.items.map((item) => (
-                                            <a
-                                                href={item.href}
-                                                key={item.label}
-                                                className={clsx(
-                                                    "text-base",
-                                                    "text-gray-0",
-                                                    "opacity-75",
-                                                    "hover:opacity-100",
-                                                    "hover:no-underline",
-                                                    "hover:text-gray-0",
-                                                )}
-                                            >
-                                                {item.label}
-                                            </a>
-                                        ))}
-                                    </div>
-                                </div>
-                            ))}
-                            <div
-                                className={clsx(
-                                    "landing-md:col-span-3 landing-lg:col-span-2 w-full landing-content:max-w-[304px]",
-                                )}
-                            >
-                                <div className={clsx("flex flex-col gap-4")}>
-                                    <div className="text-gray-0 text-base font-semibold">
-                                        Contact
-                                    </div>
-                                    <div
-                                        className={clsx(
-                                            "text-base",
-                                            "text-gray-0",
-                                        )}
-                                    >
-                                        <div className={clsx("opacity-75")}>
-                                            Refine Development Inc.{" "}
-                                            <br className="landing-md:hidden landing-xl:inline" />
-                                            256 Chapman Road STE 105-4{" "}
-                                            <br className="landing-md:hidden landing-xl:inline" />
-                                            Newark, DE 19702
-                                            <br className="landing-md:hidden landing-xl:inline" />
-                                            <br className="landing-md:hidden landing-xl:inline" />
-                                        </div>
-                                        <a
-                                            href="mailto:info@refine.dev"
-                                            className={clsx(
-                                                "text-gray-0",
-                                                "opacity-75",
-                                                "hover:opacity-100",
-                                                "hover:no-underline",
-                                                "hover:text-gray-0",
-                                            )}
-                                        >
-                                            info@refine.dev
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div
-                className={clsx(
-                    "border-t",
-                    "border-t-refine-landing-footer-border",
-                    "bg-refine-landing-footer-bg",
-                    "w-full",
-                    "px-4",
-                    "landing-sm:px-8",
-                    "landing-md:px-8",
-                    "landing-lg:px-12",
-                    "landing-xl:px-20",
-                    "py-6",
-                )}
-            >
-                <div
-                    className={clsx(
-                        "mx-auto",
-                        "max-w-screen-landing-footer",
-                        "w-full",
-                        "landing-lg:gap-2 gap-6",
-                        "flex items-center justify-between",
-                        "landing-lg:flex-row flex-col-reverse",
-                    )}
-                >
-                    <div className={clsx("text-base", "text-gray-0")}>
-                        © 2023, refine from San Francisco to wherever
-                        you&apos;re with{" "}
-                        <HeartOutlinedIcon className="text-refine-red ml-2 inline leading-6" />
-                    </div>
-                    <div
-                        className={clsx(
-                            "flex items-center",
-                            "gap-4",
-                            "landing-lg:flex-row flex-col",
-                            "landing-lg:max-w-none max-w-[382px]",
-                        )}
-                    >
-                        <span
-                            className={clsx(
-                                "text-gray-0",
-                                "landing-lg:text-left text-center opacity-75",
-                            )}
-                        >
-                            Join us on
-                        </span>
-                        <div
-                            className={clsx(
-                                "flex",
-                                "flex-row",
-                                "items-center",
-                                "gap-4",
-                                "justify-between",
-                            )}
-                        >
-                            {socialLinks.map(({ href, icon: Icon }) => (
-                                <a
-                                    href={href}
-                                    key={href}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className={clsx(
-                                        "text-gray-0",
-                                        "opacity-75",
-                                        "no-underline",
-                                        "hover:text-gray-0 hover:no-underline",
-                                    )}
-                                >
-                                    <Icon className="landing-lg:w-6 landing-lg:h-6 h-9 w-9" />
-                                </a>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
     );
 };
