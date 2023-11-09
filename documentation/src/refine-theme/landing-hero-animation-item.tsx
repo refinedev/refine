dc5eec21d8626f65e478842de8258ae327e61d88
Ali Emir Şen <senaliemir@gmail.com>
2023-11-09 10:41:26 +0300

    docs(landing): fix animation item glow in hero

--- documentation/src/refine-theme/landing-hero-animation-item.tsx
+++ documentation/src/refine-theme/landing-hero-animation-item.tsx
@@ -10,10 +10,16 @@ type ItemProps = {
     vertical: "top" | "bottom";
     horizontal: "left" | "right";
     previousName?: string;
-    icon: React.ReactNode;
+    icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
     section: string;
     name: string;
     color: string;
+    rayClassName?: string;
+};
+
+const hexToAlpha = (hex: string, alpha: number) => {
+    const alphaHex = Math.round(alpha * 255).toString(16);
+    return hex + alphaHex;
 };
 
 export const LandingHeroAnimationItem = React.memo(
@@ -21,10 +27,11 @@ export const LandingHeroAnimationItem = React.memo(
         vertical,
         horizontal,
         previousName,
-        icon,
+        icon: Icon,
         section,
         name,
         color,
+        rayClassName,
     }: ItemProps) {
         const ref = React.useRef<ChangingTextElementRef>(null);
 
@@ -63,7 +70,17 @@ export const LandingHeroAnimationItem = React.memo(
                     )}
                     key={name}
                 >
-                    {icon}
+                    <Icon
+                        style={{
+                            filter: `drop-shadow(0px 0px 20px ${hexToAlpha(
+                                color,
+                                0.75,
+                            )}) drop-shadow(0px 0px 30px ${hexToAlpha(
+                                color,
+                                0.5,
+                            )})`,
+                        }}
+                    />
                 </div>
                 <div
                     className={clsx(
@@ -153,6 +170,7 @@ export const LandingHeroAnimationItem = React.memo(
                             "animate-beam-spin",
                             "will-change-transform",
                             "bg-landing-hero-beam-bg",
+                            rayClassName,
                         )}
                         style={{
                             color,
