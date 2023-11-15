55127aee51279da4ffcdc759420a289c5b48bff5
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-15 15:03:17 +0300

    feat(docs): add clarity (#5263)

---
+++ documentation/plugins/clarity.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+
+async function microsoftClarity() {
+    return {
+        name: "docusaurus-plugin-refine-clarity",
+        injectHtmlTags() {
+            return {
+                headTags: [
+                    {
+                        tagName: "link",
+                        attributes: {
+                            rel: "preconnect",
+                            href: "https://www.clarity.ms",
+                        },
+                    },
+                    {
+                        tagName: "script",
+                        innerHTML: `
+                      (function(c,l,a,r,i,t,y){
+                          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                       })(window, document, "clarity", "script", "jquujqps85");`,
+                    },
+                ],
+            };
+        },
+    };
+}
+exports.default = microsoftClarity;
