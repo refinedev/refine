6c9276beb839cd6c69765e93bc1ef9eecbcf1c0d
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-20 17:22:29 +0300

    fix(core): get svg from cdn

--- packages/core/src/components/gh-banner/styles.ts
+++ packages/core/src/components/gh-banner/styles.ts
@@ -12,8 +12,8 @@ export const CSSRules = [
     `,
     `
     .top-announcement-mask {
-        mask-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMTYiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgY2xpcC1wYXRoPSJ1cmwoI2EpIj48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGNsaXAtcnVsZT0iZXZlbm9kZCIgZD0iTS41IDB2Mi43MTdsNC41IDIuNyA0LjUtMi43VjBoMXYyLjcxN2w0LjUgMi43IDQuNS0yLjdWMGguNXYzLjU4M2wtNC41IDIuN3Y0LjQzNGw0LjUgMi43VjE2aC0uNXYtMS43MTdsLTQuNS0yLjctNC41IDIuN1YxNmgtMXYtMS43MTdsLTQuNS0yLjctNC41IDIuN1YxNkgwdi0yLjU4M2w0LjUtMi43VjYuMjgzTDAgMy41ODNWMGguNVptNSA2LjI4MyA0LjUtMi43IDQuNSAyLjd2NC40MzRsLTQuNSAyLjctNC41LTIuN1Y2LjI4M1oiIGZpbGw9IiNmZmYiLz48L2c+PGRlZnM+PGNsaXBQYXRoIGlkPSJhIj48cGF0aCBmaWxsPSIjZmZmIiBkPSJNMCAwaDIwdjE2SDB6Ii8+PC9jbGlwUGF0aD48L2RlZnM+PC9zdmc+);
-        -webkit-mask-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMTYiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgY2xpcC1wYXRoPSJ1cmwoI2EpIj48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGNsaXAtcnVsZT0iZXZlbm9kZCIgZD0iTS41IDB2Mi43MTdsNC41IDIuNyA0LjUtMi43VjBoMXYyLjcxN2w0LjUgMi43IDQuNS0yLjdWMGguNXYzLjU4M2wtNC41IDIuN3Y0LjQzNGw0LjUgMi43VjE2aC0uNXYtMS43MTdsLTQuNS0yLjctNC41IDIuN1YxNmgtMXYtMS43MTdsLTQuNS0yLjctNC41IDIuN1YxNkgwdi0yLjU4M2w0LjUtMi43VjYuMjgzTDAgMy41ODNWMGguNVptNSA2LjI4MyA0LjUtMi43IDQuNSAyLjd2NC40MzRsLTQuNSAyLjctNC41LTIuN1Y2LjI4M1oiIGZpbGw9IiNmZmYiLz48L2c+PGRlZnM+PGNsaXBQYXRoIGlkPSJhIj48cGF0aCBmaWxsPSIjZmZmIiBkPSJNMCAwaDIwdjE2SDB6Ii8+PC9jbGlwUGF0aD48L2RlZnM+PC9zdmc+);
+        mask-image: url(https://refine.ams3.cdn.digitaloceanspaces.com/website/static/assets/hexagon.svg);
+        -webkit-mask-image: url(https://refine.ams3.cdn.digitaloceanspaces.com/website/static/assets/hexagon.svg);
         mask-repeat: repeat;
         -webkit-mask-repeat: repeat;
         background: rgba(71, 235, 235, 0.25);
