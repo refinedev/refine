d976adf2fb88edf49545219747d81413ccf4466d
Omer Aplak <omer@pankod.com>
2023-11-21 09:19:01 +0300

    chore: typo fix
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>

--- packages/core/src/components/gh-banner/index.tsx
+++ packages/core/src/components/gh-banner/index.tsx
@@ -11,7 +11,7 @@ export const GitHubBanner = () => {
     }, []);
 
     const texts = [
-        "Be a part of our journey. Star Refine on GitHub!",
+        "Be a part of our journey by starring Refine on GitHub.",
         "Join our community. Star Refine on GitHub!",
         "Let's elevate Refine together. Star us on GitHub!",
         "Enhance Refine's reach. Give us a star on GitHub!",
