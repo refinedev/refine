0d1e269c02837bea4c1df3f4a56dfad52974bd7a
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-21 12:08:48 +0300

    feat(core): update github banner styles (#5289)
    * feat(core): update github banner styles
    
    * fix(core): get svg from cdn
    
    * fix(core): remove commented code
    
    * chore: typo fix
    
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>
    
    * chore: typo fix
    
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>
    
    * chore: typo fix
    
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>
    
    * chore: typo fix
    
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>
    
    * fix(core): github banner hydration-error
    
    * feat(core): update github banner texts
    
    * chore(core): update changeset
    
    Co-authored-by: Omer Aplak <omer@pankod.com>
    
    * feat(core): update github banner text
    
    * fix(core): remove unnecessary condition on github banner
    
    ---------
    
    Co-authored-by: Omer Aplak <omer@pankod.com>
    Co-authored-by: Necati Özmen <necatiozmn@gmail.com>

---
+++ .changeset/yellow-suns-smile.md
@@ -0,0 +1,7 @@
+---
+"@refinedev/core": patch
+---
+
+feat: `<GitHubBanner />` styles updated.
+
+fix: `<GitHubBanner />` hydration error. #5295
--- packages/core/src/components/gh-banner/styles.ts
+++ packages/core/src/components/gh-banner/styles.ts
@@ -1,5 +1,25 @@
 export const CSSRules = [
     `
+    .bg-top-announcement {
+        border-bottom: 1px solid rgba(71, 235, 235, 0.15);
+        background: radial-gradient(
+                218.19% 111.8% at 0% 0%,
+                rgba(71, 235, 235, 0.1) 0%,
+                rgba(71, 235, 235, 0.2) 100%
+            ),
+            #14141f;
+    }
+    `,
+    `
+    .top-announcement-mask {
+        mask-image: url(https://refine.ams3.cdn.digitaloceanspaces.com/website/static/assets/hexagon.svg);
+        -webkit-mask-image: url(https://refine.ams3.cdn.digitaloceanspaces.com/website/static/assets/hexagon.svg);
+        mask-repeat: repeat;
+        -webkit-mask-repeat: repeat;
+        background: rgba(71, 235, 235, 0.25);
+    }
+    `,
+    `
     .banner {
         display: flex;
         @media (max-width: 1000px) {
@@ -7,60 +27,20 @@ export const CSSRules = [
         }
     }`,
     `
-    .banner::before,
-    .banner::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-    `,
-    `
-    .banner::before {
-        background-image: linear-gradient(
-            270deg,
-            rgba(255, 76, 77, 0.35) 0%,
-            rgba(255, 153, 51, 0.35) 12.5%,
-            rgba(255, 191, 0, 0.35) 25%,
-            rgba(38, 217, 127, 0.35) 37.5%,
-            rgba(71, 235, 235, 0.35) 50%,
-            rgba(0, 128, 255, 0.35) 62.5%,
-            rgba(51, 51, 255, 0.35) 75%,
-            rgba(128, 0, 255, 0.35) 87.5%,
-            rgba(237, 94, 201, 0.35) 100%
-        );
-        background-position: 0 0;
-        background-size: 200% 100%;
-        background-repeat: repeat-x;
-        animation: bgpos 4s linear infinite;
-      }
-    `,
-    `
-    .banner::after {
-        background: linear-gradient(
-          180deg,
-          rgba(13, 13, 13, 0.85) 0%,
-          rgba(13, 13, 13, 0) 100%
-        );
-    }
-    `,
-    `
-    @keyframes bgpos {
-        0% {
-            background-position: 0 0;
-        }
-
-        100% {
-            background-position: -200% 0;
-        }
-    }
-    `,
-    `
     .gh-link, .gh-link:hover, .gh-link:active, .gh-link:visited, .gh-link:focus {
         text-decoration: none;
         z-index: 9;
+    }
+    `,
+    `
+    @keyframes top-announcement-glow {
+        0% {
+            opacity: 1;
+        }
+
+        100% {
+            opacity: 0;
+        }
     }
     `,
 ];
