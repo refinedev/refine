5353740b14d2c7f24e6adc42ec690c8cb227f2a6
Ali Emir Şen <senaliemir@gmail.com>
2023-11-24 11:38:51 +0300

    docs(antd): update layout section in introduction

--- documentation/docs/api-reference/antd/introduction/layout-remix.tsx
+++ documentation/docs/api-reference/antd/introduction/layout-remix.tsx
@@ -6,7 +6,6 @@ export default function LayoutRemix() {
     <Sandpack
       showNavigator
       hidePreview
-      showFiles
       dependencies={{
         "@refinedev/antd": "latest",
         "@refinedev/core": "latest",
@@ -19,68 +18,18 @@ export default function LayoutRemix() {
         "/app/root.tsx": {
           code: RootTsxCode,
         },
-        "/app/routes/_protected.tsx": {
+        "/app/routes/_layout.tsx": {
           code: ProtectedTsxCode,
           active: true,
         },
-        "/app/routes/_protected.products._index.tsx": {
+        "/app/routes/_layout.products._index.tsx": {
           code: ListTsxCode,
-        },
-        "/app/routes/_protected.products.$id.tsx": {
-          code: ShowTsxCode,
-        },
-        "/app/routes/_protected.products.$id.edit.tsx": {
-          code: EditTsxCode,
-        },
-        "/app/routes/_protected.products.create.tsx": {
-          code: CreateTsxCode,
-        },
-        "/app/routes/_auth.tsx": {
-          code: AuthTsxCode,
-        },
-        "/app/routes/_auth.login.tsx": {
-          code: LoginTsxCode,
-        },
-        "/app/auth-provider.tsx": {
-          code: AuthProviderTsxCode,
           hidden: true,
         },
       }}
     />
   );
 }
-
-const AuthProviderTsxCode = /* jsx */ `
-const authProvider = {
-    login: async ({ username, password }) => {
-      (window as any).authenticated = true;
-      return { success: true };
-    },
-    check: async () => {
-      // auto login at first time
-      if (typeof (window as any).authenticated === "undefined") {
-        (window as any).authenticated = true;
-      }
-      return { authenticated: Boolean((window as any).authenticated) };
-    },
-    logout: async () => {
-      (window as any).authenticated = false;
-      return { success: true };
-    },
-    register: async () => {
-      return { success: true };
-    },
-    forgotPassword: async () => {
-      return { success: true };
-    },
-    resetPassword: async () => {
-      return { success: true };
-    },
-    getIdentity: async () => ({ id: 1, name: "John Doe", avatar: "https://i.pravatar.cc/300"})
-};
-
-export default authProvider;
-`.trim();
 
 const RootTsxCode = /* jsx */ `
 import React from "react";
@@ -103,8 +52,6 @@ import { ConfigProvider, App as AntdApp } from "antd";
 
 import resetStyle from "@refinedev/antd/dist/reset.css";
 
-import authProvider from "./auth-provider";
-
 export default function App() {
   return (
     <html lang="en">
@@ -118,18 +65,13 @@ export default function App() {
             <Refine
               routerProvider={routerProvider}
               dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-              authProvider={authProvider}
               notificationProvider={useNotificationProvider}
               resources={[
                 {
                   name: "products",
                   list: "/products",
-                  show: "/products/:id",
-                  edit: "/products/:id/edit",
-                  create: "/products/create",
                 },
               ]}
-              options={{ syncWithLocation: true }}
             >
               <Outlet />
             </Refine>
@@ -149,72 +91,21 @@ import { ThemedLayoutV2 } from "@refinedev/antd";
 import { Outlet } from "@remix-run/react";
 import { LoaderArgs, redirect } from "@remix-run/node";
 
-import authProvider from "../auth-provider";
-
-export default function AuthenticatedLayout() {
-    // \`<ThemedLayoutV2>\` is only applied to the authenticated users
+/**
+ * Routes starting with \`_layout\` will have their children rendered inside the layout.
+ */
+export default function Layout() {
     return (
         <ThemedLayoutV2>
             <Outlet />
         </ThemedLayoutV2>
     );
 }
-
-/**
- * We're checking if the current session is authenticated.
- * If not, we're redirecting the user to the login page.
- * This is applied for all routes that are nested under this layout (_protected).
- */
-export async function loader({ request }: LoaderArgs) {
-    const { authenticated, redirectTo } = await authProvider.check(request);
-
-    if (!authenticated) {
-        throw redirect(redirectTo ?? "/login");
-    }
-
-    return {};
-}
-`.trim();
-
-const AuthTsxCode = /* jsx */ `
-import { Outlet } from "@remix-run/react";
-import { LoaderArgs, redirect } from "@remix-run/node";
-
-import { authProvider } from "~/authProvider";
-
-export default function AuthLayout() {
-    // no layout is applied for the auth routes
-    return <Outlet />;
-}
-
-/**
- * If the current session is authenticated, we're redirecting the user to the home page.
- * Alternatively, we could also use the \`Authenticated\` component inside the \`AuthLayout\` to handle the redirect.
- * But, server-side redirects are more performant.
- */
-export async function loader({ request }: LoaderArgs) {
-    const { authenticated, redirectTo } = await authProvider.check(request);
-
-    if (authenticated) {
-        throw redirect(redirectTo ?? "/");
-    }
-
-    return {};
-}
-`.trim();
-
-const LoginTsxCode = /* jsx */ `
-import { AuthPage } from "@refinedev/antd";
-
-export default function LoginPage() {
-  return <AuthPage type="login" />;
-}
-
 `.trim();
 
 const ListTsxCode = /* jsx */ `
 import React from "react";
-import { List, ShowButton, EditButton, useTable } from "@refinedev/antd";
+import { List, useTable } from "@refinedev/antd";
 import { Space, Table } from "antd";
 
 export default function ProductList() {
@@ -226,177 +117,8 @@ export default function ProductList() {
         <Table.Column dataIndex="id" title="Id" />
         <Table.Column dataIndex="name" title="Name" />
         <Table.Column dataIndex="price" title="Price" />
-        <Table.Column
-          title="Actions"
-          dataIndex="actions"
-          render={(_, record: BaseRecord) => (
-            <Space>
-              <ShowButton hideText size="small" recordItemId={record.id} />
-              <EditButton hideText size="small" recordItemId={record.id} />
-            </Space>
-          )}
-        />
       </Table>
     </List>
-  );
-};
-`.trim();
-
-const ShowTsxCode = /* jsx */ `
-import React from "react";
-import { useShow } from "@refinedev/core";
-import { MarkdownField, NumberField, Show, TextField } from "@refinedev/antd";
-import { Typography } from "antd";
-
-const { Title } = Typography;
-
-export default function ProductShow() {
-  const { queryResult } = useShow();
-  const { data, isLoading } = queryResult;
-
-  const record = data?.data;
-
-  return (
-    <Show isLoading={isLoading}>
-      <Title level={5}>Id</Title>
-      <NumberField value={record?.id ?? ""} />
-      <Title level={5}>Name</Title>
-      <TextField value={record?.name} />
-      <Title level={5}>Material</Title>
-      <TextField value={record?.material} />
-      <Title level={5}>Description</Title>
-      <MarkdownField value={record?.description} />
-      <Title level={5}>Price</Title>
-      <NumberField value={record?.price ?? ""} />
-    </Show>
-  );
-};
-`.trim();
-
-const EditTsxCode = /* jsx */ `
-import React from "react";
-import { Typography, Form, Input, InputNumber } from "antd";
-import { Edit, useForm } from "@refinedev/antd";
-
-const { Title } = Typography;
-const { TextArea } = Input;
-
-export default function ProductEdit() {
-  const { formProps, saveButtonProps, formLoading } = useForm();
-
-  return (
-    <Edit saveButtonProps={saveButtonProps} isLoading={formLoading}>
-      <Form {...formProps} layout="vertical">
-          <Form.Item
-              label="Name"
-              name="name"
-              rules={[
-                  {
-                      required: true,
-                  },
-              ]}
-          >
-              <Input />
-          </Form.Item>
-          <Form.Item
-              label="Material"
-              name="material"
-              rules={[
-                  {
-                      required: true,
-                  },
-              ]}
-          >
-              <Input />
-          </Form.Item>
-          <Form.Item
-              label="Description"
-              name="description"
-              rules={[
-                  {
-                      required: true,
-                  },
-              ]}
-          >
-              <TextArea rows={4} />
-          </Form.Item>
-          <Form.Item
-              label="Price"
-              name="price"
-              rules={[
-                  {
-                      required: true,
-                  },
-              ]}
-          >
-              <InputNumber />
-          </Form.Item>
-      </Form>
-  </Edit>
-  );
-};
-`.trim();
-
-const CreateTsxCode = /* jsx */ `
-import React from "react";
-import { Typography, Form, Input, InputNumber } from "antd";
-import { Create, useForm } from "@refinedev/antd";
-
-const { Title } = Typography;
-const { TextArea } = Input;
-
-export default function ProductCreate() {
-  const { formProps, saveButtonProps, formLoading } = useForm();
-
-  return (
-    <Create saveButtonProps={saveButtonProps} isLoading={formLoading}>
-      <Form {...formProps} layout="vertical">
-        <Form.Item
-            label="Name"
-            name="name"
-            rules={[
-                {
-                    required: true,
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-        <Form.Item
-            label="Material"
-            name="material"
-            rules={[
-                {
-                    required: true,
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-        <Form.Item
-            label="Description"
-            name="description"
-            rules={[
-                {
-                    required: true,
-                },
-            ]}
-        >
-            <TextArea rows={4} />
-        </Form.Item>
-        <Form.Item
-            label="Price"
-            name="price"
-            rules={[
-                {
-                    required: true,
-                },
-            ]}
-        >
-            <InputNumber />
-        </Form.Item>
-      </Form>
-    </Create>
   );
 };
 `.trim();
