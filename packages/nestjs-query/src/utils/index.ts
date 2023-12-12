de032a571c0e5e4935f0cbb5d11eef6e5a9fe05c
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-12-12 16:54:42 +0300

    feat: add isMutation helper

--- packages/nestjs-query/src/utils/index.ts
+++ packages/nestjs-query/src/utils/index.ts
@@ -728,3 +728,19 @@ export function fieldsToString(documentNode: DocumentNode) {
     // Join all field lines into a single string
     return fieldLines.join("\n").trim();
 }
+
+export const isMutation = (documentNode: DocumentNode) => {
+    let isMutation = false;
+
+    visit(documentNode, {
+        OperationDefinition: {
+            enter(node) {
+                if (node.operation === "mutation") {
+                    isMutation = true;
+                }
+            },
+        },
+    });
+
+    return isMutation;
+};
