ec300aa9ae42c9c58afc461f762c20948133b00f
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-12-19 16:29:59 +0300

    feat(nestjs-query): specs for update

--- packages/nestjs-query/test/create/index.spec.ts
+++ packages/nestjs-query/test/create/index.spec.ts
@@ -6,7 +6,7 @@ import "./index.mock";
 
 describe("create", () => {
     describe("gql", () => {
-        it("correct response with meta", async () => {
+        it("correct response with mutation", async () => {
             const { data } = await dataProvider(client).create({
                 resource: "blog_posts",
                 variables: {
@@ -18,6 +18,41 @@ describe("create", () => {
                 },
                 meta: {
                     gqlMutation: gql`
+                        mutation CreateOneBlogPost(
+                            $input: CreateOneBlogPostInput!
+                        ) {
+                            createOneBlogPost(input: $input) {
+                                id
+                                title
+                                content
+                                status
+                                category {
+                                    id
+                                }
+                            }
+                        }
+                    `,
+                },
+            });
+
+            expect(data["title"]).toEqual("foo");
+            expect(data["content"]).toEqual("bar");
+            expect(data["category"].id).toEqual("1");
+            expect(data["status"]).toEqual("DRAFT");
+        });
+
+        it("correct response with query", async () => {
+            const { data } = await dataProvider(client).create({
+                resource: "blog_posts",
+                variables: {
+                    title: "foo",
+                    content: "bar",
+                    categoryId: "1",
+                    status: "DRAFT",
+                    createdAt: "2023-08-08T11:40:35.779Z",
+                },
+                meta: {
+                    gqlQuery: gql`
                         mutation CreateOneBlogPost(
                             $input: CreateOneBlogPostInput!
                         ) {
--- packages/nestjs-query/test/getOne/index.spec.ts
+++ packages/nestjs-query/test/getOne/index.spec.ts
@@ -6,65 +6,61 @@ import "./index.mock";
 
 describe("useOne", () => {
     describe("gql", () => {
-        describe("query", () => {
-            it("correct response with meta", async () => {
-                const { data } = await dataProvider(client).getOne({
-                    resource: "blog_posts",
-                    id: "1",
-                    meta: {
-                        gqlQuery: gql`
-                            query GetOneBlogPost($id: ID!) {
-                                blogPost(id: $id) {
+        it("correct response with query", async () => {
+            const { data } = await dataProvider(client).getOne({
+                resource: "blog_posts",
+                id: "1",
+                meta: {
+                    gqlQuery: gql`
+                        query GetOneBlogPost($id: ID!) {
+                            blogPost(id: $id) {
+                                id
+                                title
+                                content
+                                status
+                                category {
                                     id
-                                    title
-                                    content
-                                    status
-                                    category {
-                                        id
-                                    }
                                 }
                             }
-                        `,
-                    },
-                });
-
-                expect(data["id"]).toBeDefined();
-                expect(data["title"]).toBeDefined();
-                expect(data["content"]).toBeDefined();
-                expect(data["category"].id).toBeDefined();
+                        }
+                    `,
+                },
             });
-        });
 
-        describe("mutation", () => {
-            it("correct response with meta", async () => {
-                const { data } = await dataProvider(client).getOne({
-                    resource: "blog_posts",
-                    id: "1",
-                    meta: {
-                        gqlMutation: gql`
-                            mutation UpdateOneBlogPost(
-                                $input: UpdateOneBlogPostInput!
-                            ) {
-                                updateOneBlogPost(input: $input) {
-                                    id
-                                    title
-                                    content
-                                    status
-                                    category {
-                                        id
-                                    }
-                                }
+            expect(data["id"]).toBeDefined();
+            expect(data["title"]).toBeDefined();
+            expect(data["content"]).toBeDefined();
+            expect(data["category"].id).toBeDefined();
+        });
+    });
+
+    it("correct response with mutation", async () => {
+        const { data } = await dataProvider(client).getOne({
+            resource: "blog_posts",
+            id: "1",
+            meta: {
+                gqlMutation: gql`
+                    mutation UpdateOneBlogPost(
+                        $input: UpdateOneBlogPostInput!
+                    ) {
+                        updateOneBlogPost(input: $input) {
+                            id
+                            title
+                            content
+                            status
+                            category {
+                                id
                             }
-                        `,
-                    },
-                });
-
-                expect(data["id"]).toBeDefined();
-                expect(data["title"]).toBeDefined();
-                expect(data["content"]).toBeDefined();
-                expect(data["category"].id).toBeDefined();
-            });
+                        }
+                    }
+                `,
+            },
         });
+
+        expect(data["id"]).toBeDefined();
+        expect(data["title"]).toBeDefined();
+        expect(data["content"]).toBeDefined();
+        expect(data["category"].id).toBeDefined();
     });
 
     describe("fields (legacy)", () => {
