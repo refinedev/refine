70eaccea0a5af634ca90552e4a3909e8dd44369c
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-12-19 16:05:15 +0300

    feat(nestjs-query): specs for create, createMany, custom

--- packages/nestjs-query/test/create/index.spec.ts
+++ packages/nestjs-query/test/create/index.spec.ts
@@ -1,48 +1,88 @@
+import gql from "graphql-tag";
 import dataProvider from "../../src/index";
 import client from "../gqlClient";
 
 import "./index.mock";
 
 describe("create", () => {
-    it("correct response with meta", async () => {
-        const { data } = await dataProvider(client).create({
-            resource: "blog_posts",
-            variables: {
-                title: "foo",
-                content: "bar",
-                categoryId: "1",
-                status: "DRAFT",
-                createdAt: "2023-08-08T11:40:35.779Z",
-            },
-            meta: {
-                fields: [
-                    "id",
-                    "title",
-                    "content",
-                    "status",
-                    { category: ["id"] },
-                ],
-            },
-        });
+    describe("gql", () => {
+        it("correct response with meta", async () => {
+            const { data } = await dataProvider(client).create({
+                resource: "blog_posts",
+                variables: {
+                    title: "foo",
+                    content: "bar",
+                    categoryId: "1",
+                    status: "DRAFT",
+                    createdAt: "2023-08-08T11:40:35.779Z",
+                },
+                meta: {
+                    gqlMutation: gql`
+                        mutation CreateOneBlogPost(
+                            $input: CreateOneBlogPostInput!
+                        ) {
+                            createOneBlogPost(input: $input) {
+                                id
+                                title
+                                content
+                                status
+                                category {
+                                    id
+                                }
+                            }
+                        }
+                    `,
+                },
+            });
 
-        expect(data["title"]).toEqual("foo");
-        expect(data["content"]).toEqual("bar");
-        expect(data["category"].id).toEqual("1");
-        expect(data["status"]).toEqual("DRAFT");
+            expect(data["title"]).toEqual("foo");
+            expect(data["content"]).toEqual("bar");
+            expect(data["category"].id).toEqual("1");
+            expect(data["status"]).toEqual("DRAFT");
+        });
     });
 
-    it("correct response without meta", async () => {
-        const { data } = await dataProvider(client).create({
-            resource: "blog_posts",
-            variables: {
-                title: "foo",
-                content: "bar",
-                categoryId: 1,
-                status: "DRAFT",
-                createdAt: "2023-08-08T11:40:35.803Z",
-            },
+    describe("fields (legacy)", () => {
+        it("correct response with meta", async () => {
+            const { data } = await dataProvider(client).create({
+                resource: "blog_posts",
+                variables: {
+                    title: "foo",
+                    content: "bar",
+                    categoryId: "1",
+                    status: "DRAFT",
+                    createdAt: "2023-08-08T11:40:35.779Z",
+                },
+                meta: {
+                    fields: [
+                        "id",
+                        "title",
+                        "content",
+                        "status",
+                        { category: ["id"] },
+                    ],
+                },
+            });
+
+            expect(data["title"]).toEqual("foo");
+            expect(data["content"]).toEqual("bar");
+            expect(data["category"].id).toEqual("1");
+            expect(data["status"]).toEqual("DRAFT");
         });
 
-        expect(data["id"]).toBeDefined();
+        it("correct response without meta", async () => {
+            const { data } = await dataProvider(client).create({
+                resource: "blog_posts",
+                variables: {
+                    title: "foo",
+                    content: "bar",
+                    categoryId: 1,
+                    status: "DRAFT",
+                    createdAt: "2023-08-08T11:40:35.803Z",
+                },
+            });
+
+            expect(data["id"]).toBeDefined();
+        });
     });
 });
--- packages/nestjs-query/test/createMany/index.spec.ts
+++ packages/nestjs-query/test/createMany/index.spec.ts
@@ -52,62 +52,64 @@ describe("createMany", () => {
         });
     });
 
-    it("correct response with meta", async () => {
-        const { data } = await dataProvider(client).createMany({
-            resource: "blog_posts",
-            variables: [
-                {
-                    title: "foo",
-                    content: "bar",
-                    categoryId: "2",
-                    status: "DRAFT",
-                    createdAt: "2023-08-08T11:40:36.177Z",
+    describe("fields (legacy)", () => {
+        it("correct response with meta", async () => {
+            const { data } = await dataProvider(client).createMany({
+                resource: "blog_posts",
+                variables: [
+                    {
+                        title: "foo",
+                        content: "bar",
+                        categoryId: "2",
+                        status: "DRAFT",
+                        createdAt: "2023-08-08T11:40:36.177Z",
+                    },
+                    {
+                        title: "foo-2",
+                        content: "bar-2",
+                        categoryId: "3",
+                        status: "DRAFT",
+                        createdAt: "2023-08-08T11:40:36.177Z",
+                    },
+                ],
+                meta: {
+                    fields: ["id", "title", "content", { category: ["id"] }],
                 },
-                {
-                    title: "foo-2",
-                    content: "bar-2",
-                    categoryId: "3",
-                    status: "DRAFT",
-                    createdAt: "2023-08-08T11:40:36.177Z",
-                },
-            ],
-            meta: {
-                fields: ["id", "title", "content", { category: ["id"] }],
-            },
+            });
+
+            expect(data[0]["title"]).toEqual("foo");
+            expect(data[0]["content"]).toEqual("bar");
+            expect(data[0]["category"].id).toEqual("2");
+
+            expect(data[1]["title"]).toEqual("foo-2");
+            expect(data[1]["content"]).toEqual("bar-2");
+            expect(data[1]["category"].id).toEqual("3");
         });
 
-        expect(data[0]["title"]).toEqual("foo");
-        expect(data[0]["content"]).toEqual("bar");
-        expect(data[0]["category"].id).toEqual("2");
+        it("correct response without meta", async () => {
+            const { data } = await dataProvider(client).createMany({
+                resource: "blog_posts",
+                variables: [
+                    {
+                        title: "foo",
+                        content: "bar",
+                        categoryId: "2",
+                        status: "DRAFT",
+                        createdAt: "2023-08-08T11:40:36.197Z",
+                    },
+                    {
+                        title: "foo-2",
+                        content: "bar-2",
+                        categoryId: "3",
+                        status: "DRAFT",
+                        createdAt: "2023-08-08T11:40:36.197Z",
+                    },
+                ],
+            });
 
-        expect(data[1]["title"]).toEqual("foo-2");
-        expect(data[1]["content"]).toEqual("bar-2");
-        expect(data[1]["category"].id).toEqual("3");
-    });
+            expect(data[0]["id"]).toBeDefined();
 
-    it("correct response without meta", async () => {
-        const { data } = await dataProvider(client).createMany({
-            resource: "blog_posts",
-            variables: [
-                {
-                    title: "foo",
-                    content: "bar",
-                    categoryId: "2",
-                    status: "DRAFT",
-                    createdAt: "2023-08-08T11:40:36.197Z",
-                },
-                {
-                    title: "foo-2",
-                    content: "bar-2",
-                    categoryId: "3",
-                    status: "DRAFT",
-                    createdAt: "2023-08-08T11:40:36.197Z",
-                },
-            ],
+            expect(data[1]["id"]).toBeDefined();
         });
-
-        expect(data[0]["id"]).toBeDefined();
-
-        expect(data[1]["id"]).toBeDefined();
     });
 });
