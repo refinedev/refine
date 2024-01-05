ffbf6e8f629c189747c13bae5df8412500c4c618
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-01-05 16:32:56 +0300

    fix: add missing VITE prefix to env variables

--- examples/app-crm/src/providers/search-client.tsx
+++ examples/app-crm/src/providers/search-client.tsx
@@ -1,8 +1,8 @@
 import algoliasearch from "algoliasearch/lite";
 
-const ALGOLIA_APP_ID = import.meta.env.ALGOLIA_APP_ID;
+const ALGOLIA_APP_ID = import.meta.env.VITE_ALGOLIA_APP_ID;
 
-const ALGOLIA_API_KEY = import.meta.env.ALGOLIA_API_KEY;
+const ALGOLIA_API_KEY = import.meta.env.VITE_ALGOLIA_API_KEY;
 
 const isAlgoliaEnabled = ALGOLIA_APP_ID && ALGOLIA_API_KEY;
 
