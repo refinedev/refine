466a793298bc155eeaffa3d06087b95474a2d910
EgeAytin <ege.aytin@gmail.com>
2024-01-05 18:53:26 +0300

    feat: add permify access control example

---
+++ examples/access-control-permify/src/pages/categories/create.tsx
@@ -0,0 +1,29 @@
+import { IResourceComponentsProps } from "@refinedev/core";
+
+import { Create, useForm } from "@refinedev/antd";
+
+import { Form, Input } from "antd";
+
+import { ICategory } from "../../interfaces";
+
+export const CategoryCreate: React.FC<IResourceComponentsProps> = () => {
+    const { formProps, saveButtonProps } = useForm<ICategory>();
+
+    return (
+        <Create saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Create>
+    );
+};
