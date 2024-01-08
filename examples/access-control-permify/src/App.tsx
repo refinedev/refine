bfd2eb4347a496506d46cfe87167064b0e235e4d
Ege Aytın <ege.aytin@gmail.com>
2024-01-08 17:48:04 +0300

    feat: add permify access control example (#5487)
    * feat: add permify access control example
    
    * chore: bump core dependencies
    
    ---------
    
    Co-authored-by: Omer Aplak <omer@refine.dev>

---
+++ examples/access-control-permify/src/App.tsx
@@ -0,0 +1,157 @@
+import { CanAccess, GitHubBanner, Refine } from "@refinedev/core";
+import {
+  useNotificationProvider,
+  ThemedLayoutV2,
+  ErrorComponent,
+  RefineThemes,
+} from "@refinedev/antd";
+import dataProvider from "@refinedev/simple-rest";
+import routerProvider, {
+  NavigateToResource,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { ConfigProvider, App as AntdApp } from "antd";
+
+import "@refinedev/antd/dist/reset.css";
+
+import { Header } from "./components/header";
+import { PostList, PostCreate, PostEdit, PostShow } from "./pages/posts";
+import { UserList, UserCreate, UserEdit, UserShow } from "./pages/users";
+import {
+  CategoryList,
+  CategoryCreate,
+  CategoryEdit,
+  CategoryShow,
+} from "./pages/categories";
+import { PermifyClient } from "./authz/permifyClient";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+// Create an instance of Permify Client
+const instance = "http://localhost:3476";
+const permify = new PermifyClient(instance);
+
+//sample users data
+import users from "./data/users.json";
+
+const App: React.FC = () => {
+  const role = localStorage.getItem("role") ?? "admin";
+  const user =
+    users.find((user) => user.roles[0].guard_name === role) ?? users[0];
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <ConfigProvider theme={RefineThemes.Blue}>
+        <AntdApp>
+          <Refine
+            dataProvider={dataProvider(API_URL)}
+            accessControlProvider={{
+              can: async ({ action, params, resource }) => {
+                console.log("action", action);
+                console.log("resourceAPP", resource);
+                console.log("params", params);
+
+              if(action) {
+                  //post specific access checks - show, edit and delete a post
+                  if (
+                    action === "show" ||
+                    action === "edit" ||
+                    action === "delete"
+                  ) {
+                    const result = await permify.isAuthorized(
+                      user.id,
+                      resource!,
+                      action,
+                      params?.id
+                    );
+                    return Promise.resolve({
+                      can: result,
+                    });
+                  }
+
+                  //organization specific access checks - listing posts & creating posts
+                  const result = await permify.isAuthorized(
+                    user.id,
+                    "organization",
+                    action,
+                    user.organization_id
+                  );
+                  return Promise.resolve({
+                    can: result,
+                  });
+              }
+              return Promise.resolve({
+                can: true,
+              });
+               
+              },
+            }}
+            resources={[
+              {
+                name: "posts",
+                list: "/posts",
+                show: "/posts/show/:id",
+                create: "/posts/create",
+                edit: "/posts/edit/:id",
+                meta: {
+                  canDelete: true,
+                },
+              },
+            ]}
+            notificationProvider={useNotificationProvider}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+            }}
+          >
+            <Routes>
+              <Route
+                element={
+                  <ThemedLayoutV2 Header={() => <Header role={role} />}>
+                    <CanAccess>
+                      <Outlet />
+                    </CanAccess>
+                  </ThemedLayoutV2>
+                }
+              >
+                <Route
+                  index
+                  element={<NavigateToResource resource="posts" />}
+                />
+
+                <Route path="/posts">
+                  <Route index element={<PostList />} />
+                  <Route path="create" element={<PostCreate />} />
+                  <Route path="edit/:id" element={<PostEdit />} />
+                  <Route path="show/:id" element={<PostShow />} />
+                </Route>
+
+                <Route path="/users">
+                  <Route index element={<UserList />} />
+                  <Route path="create" element={<UserCreate />} />
+                  <Route path="edit/:id" element={<UserEdit />} />
+                  <Route path="show/:id" element={<UserShow />} />
+                </Route>
+
+                <Route path="/categories">
+                  <Route index element={<CategoryList />} />
+                  <Route path="create" element={<CategoryCreate />} />
+                  <Route path="edit/:id" element={<CategoryEdit />} />
+                  <Route path="show/:id" element={<CategoryShow />} />
+                </Route>
+
+                <Route path="*" element={<ErrorComponent />} />
+              </Route>
+            </Routes>
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+        </AntdApp>
+      </ConfigProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ examples/access-control-permify/src/pages/users/show.tsx
@@ -0,0 +1,43 @@
+import { useShow, IResourceComponentsProps } from "@refinedev/core";
+
+import { Show, EmailField, ImageField } from "@refinedev/antd";
+
+import { Typography, Space } from "antd";
+
+import { IUser } from "../../interfaces";
+
+const { Title, Text } = Typography;
+
+export const UserShow: React.FC<IResourceComponentsProps> = () => {
+    const { queryResult } = useShow<IUser>();
+    const { data, isLoading } = queryResult;
+    const record = data?.data;
+
+    return (
+        <Show isLoading={isLoading}>
+            <Title level={5}>Id</Title>
+            <Text>{record?.id}</Text>
+
+            <Title level={5}>First Name</Title>
+            <Text>{record?.firstName}</Text>
+
+            <Title level={5}>Last Name</Title>
+            <Text>{record?.lastName}</Text>
+
+            <Title level={5}>Email</Title>
+            <EmailField value={record?.email} />
+
+            <Title level={5}>Images</Title>
+            <Space wrap>
+                {record?.avatar.map((img) => (
+                    <ImageField
+                        key={img.name}
+                        value={img.url}
+                        title={img.name}
+                        width={200}
+                    />
+                ))}
+            </Space>
+        </Show>
+    );
+};
