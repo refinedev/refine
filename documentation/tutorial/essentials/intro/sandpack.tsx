fbb3ad7aa1b24463eab0681b65236c39d95a9276
Ali Emir Şen <senaliemir@gmail.com>
2024-01-12 02:30:32 +0300

    docs(tutorial-0): add welcome page

--- documentation/tutorial/essentials/intro/sandpack.tsx
+++ documentation/tutorial/essentials/intro/sandpack.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import dedent from "dedent";
 import { TutorialSandpack } from "@site/src/refine-theme/tutorial-sandpack";
-import { useSandpack } from "@codesandbox/sandpack-react";
+
+const dependencies = {
+    "@refinedev/core": "latest",
+    "@refinedev/simple-rest": "latest",
+};
 
 export const Sandpack = ({ children }: { children: React.ReactNode }) => {
     return (
         <TutorialSandpack
+            previewOnly
+            dependencies={dependencies}
             files={{
                 "App.tsx": {
-                    code: `
-export default function App(): JSX.Element {
-    return <h1>Hello world!</h1>
-}
-            `.trim(),
+                    code: AppTsxCode,
                 },
             }}
         >
@@ -21,88 +22,18 @@ export default function App(): JSX.Element {
     );
 };
 
-export const ChangeAppTsxSpan = ({
-    children,
-}: {
-    children: React.ReactNode;
-}) => {
-    const { sandpack } = useSandpack();
+const AppTsxCode = /* jsx */ `
+import React from "react";
+import { Refine, WelcomePage } from "@refinedev/core";
+import dataProvider from "@refinedev/simple-rest";
 
-    const onClick = () => {
-        sandpack.updateFile(
-            "App.tsx",
-            `
-export default function App(): JSX.Element {
-    return <h1>Hello alisko!</h1>
-}
-`.trim(),
-        );
-    };
-
+export default function App() {
     return (
-        <span
-            onClick={onClick}
-            className="text-refine-react-light-link dark:text-refine-react-dark-link cursor-pointer"
+        <Refine
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
         >
-            {children}
-        </span>
-    );
-};
-
-export const AddFileSpan = ({ children }: { children: React.ReactNode }) => {
-    const { sandpack } = useSandpack();
-
-    const onClick = () => {
-        // sandpack.
-        sandpack.addFile({
-            "/my-component.tsx": {
-                hidden: true,
-                code: `
-export default function MyComponent(): JSX.Element {
-    return <h1>This is my component!</h1>
+            <WelcomePage />
+        </Refine>
+    )
 }
-                    `.trim(),
-            },
-        });
-    };
-
-    return (
-        <span
-            onClick={onClick}
-            className="text-refine-react-light-link dark:text-refine-react-dark-link cursor-pointer [&>p]:!text-red-500"
-        >
-            {children}
-        </span>
-    );
-};
-
-export const UpdateFileSpan = ({ children }: { children: React.ReactNode }) => {
-    const { sandpack } = useSandpack();
-
-    const onClick = () => {
-        // sandpack.
-        sandpack.updateFile({
-            "/my-component.tsx": {
-                active: true,
-                hidden: false,
-                code: dedent`
-export default function MyComponent(): JSX.Element {
-    return <h1>This is my component!</h1>
-}
-                    `.trim(),
-            },
-        });
-        sandpack.visibleFiles;
-        console.log(sandpack.visibleFiles);
-        sandpack.openFile("/my-component.tsx");
-    };
-
-    return (
-        <span
-            onClick={onClick}
-            className="text-refine-react-light-link dark:text-refine-react-dark-link cursor-pointer [&>p]:!text-red-500"
-        >
-            {children}
-        </span>
-    );
-};
+`.trim();
