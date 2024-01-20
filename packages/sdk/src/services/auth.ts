8da8cdc8b17cc3d2acf66448ad9cd81b47af8388
Grant <149294029+gtsp233@users.noreply.github.com>
2024-01-20 00:27:32 -0500

    block javascript url to prevent xss

--- packages/sdk/src/services/auth.ts
+++ packages/sdk/src/services/auth.ts
@@ -1,6 +1,7 @@
 import { Client } from "../client";
 import { ISession, IUser } from "../interfaces";
 
+
 class Auth {
     private client: Client;
 
@@ -30,6 +31,11 @@ class Auth {
             const baseUrl = this.client.getBaseUrl();
             const clientId = this.client.getClientId();
 
+            const isJavaScriptProtocol = /^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i
+
+            if (isJavaScriptProtocol.test(baseUrl)) {
+                return;
+            }
             return window.location.replace(
                 `${baseUrl}/oauth/${provider}?applicationClientId=${clientId}`,
             );
