5a3ad1dee1799d99b3eb90839fb2ebd4bcc59978
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-01-22 10:10:16 +0300

    chore: remove sdk folder

--- packages/sdk/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    rootDir: "./",
-    displayName: "sdk",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-    testEnvironment: "jsdom",
-};
--- packages/sdk/src/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./api/log";
-export * from "./auth";
-export * from "./log";
-export * from "./draft-resource";
-export * from "./config";
-export * from "./connect-query";
-export * from "./storage";
