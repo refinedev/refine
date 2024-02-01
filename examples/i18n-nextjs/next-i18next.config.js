6967cf3c7fe098693c1acb2e9461c69a37616b52
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-02-01 13:44:18 +0300

    feat(examples): update i18n-next to appdir

--- examples/i18n-nextjs/next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en", "de"],
-        localePath: path.resolve("./public/locales"),
-    },
-};
