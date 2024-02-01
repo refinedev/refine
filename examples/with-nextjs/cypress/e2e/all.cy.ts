7f4d3b9b577946118c5f978f7283c022e6a93c4c
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-02-01 15:03:04 +0300

    fix: add missing e2e specs back

--- examples/with-nextjs/cypress/e2e/all.cy.ts
+++ examples/with-nextjs/cypress/e2e/all.cy.ts
@@ -4,6 +4,39 @@
 describe("with-nextjs", () => {
     const BASE_URL = "http://localhost:3000";
 
+    const fillForm = () => {
+        cy.fixture("mock-post").then((mockPost) => {
+            cy.get("#title").clear();
+            cy.get("#title").type(mockPost.title);
+            cy.setAntdDropdown({ id: "category_id", selectIndex: 0 });
+            cy.setAntdSelect({ id: "status", value: mockPost.status });
+        });
+    };
+
+    const assertSuccessResponse = (response: any) => {
+        const body = response?.body;
+
+        expect(response?.statusCode).to.eq(200);
+        expect(body).to.have.property("id");
+        expect(body).to.have.property("category");
+
+        cy.fixture("mock-post").then((mockPost) => {
+            expect(body?.title).to.eq(mockPost.title);
+            // expect(body?.content).to.eq(mockPost.content);
+            expect(body?.status?.toLowerCase()).to.eq(
+                mockPost?.status?.toLowerCase(),
+            );
+        });
+
+        cy.location().should((loc) => {
+            expect(loc.pathname).to.eq("/posts");
+        });
+    };
+
+    const submitForm = () => {
+        return cy.getSaveButton().click();
+    };
+
     const submitAuthForm = () => {
         return cy.get("button[type=submit]").click();
     };
@@ -19,6 +52,12 @@ describe("with-nextjs", () => {
     };
 
     beforeEach(() => {
+        cy.interceptGETPost();
+        cy.interceptPOSTPost();
+        cy.interceptPATCHPost();
+        cy.interceptDELETEPost();
+        cy.interceptGETPosts();
+        cy.interceptGETCategories();
         cy.clearAllCookies();
         cy.clearAllLocalStorage();
         cy.clearAllSessionStorage();
@@ -153,6 +192,142 @@ describe("with-nextjs", () => {
             cy.reload();
             cy.get(".ant-typography").contains(/jane doe/i);
             cy.get(".ant-avatar > img").should("have.attr", "src");
+        });
+    });
+
+    describe("crud", () => {
+        beforeEach(() => {
+            login();
+        });
+
+        it("should create record", () => {
+            cy.getCreateButton().click();
+            cy.wait("@getCategories");
+            cy.location("pathname").should("eq", "/posts/create");
+
+            cy.assertDocumentTitle("Post", "create");
+
+            fillForm();
+            submitForm();
+
+            cy.wait("@postPost").then((interception) => {
+                const response = interception?.response;
+                assertSuccessResponse(response);
+            });
+        });
+
+        it("should edit record", () => {
+            // wait loading state and render to be finished
+            cy.wait("@getPosts");
+            cy.getAntdLoadingOverlay().should("not.exist");
+
+            cy.getEditButton().last().click();
+            cy.wait("@getPost");
+            cy.wait("@getCategories");
+
+            cy.assertDocumentTitle("Post", "edit");
+
+            fillForm();
+            submitForm();
+
+            cy.wait("@patchPost").then((interception) => {
+                const response = interception?.response;
+
+                assertSuccessResponse(response);
+            });
+        });
+
+        it("should delete record", () => {
+            cy.wait("@getPosts");
+            cy.getAntdLoadingOverlay().should("not.exist");
+
+            cy.getEditButton().last().click();
+
+            // wait loading state and render to be finished
+            cy.wait("@getPost");
+            cy.getAntdLoadingOverlay().should("not.exist");
+            cy.getSaveButton().should("not.be.disabled");
+
+            cy.getDeleteButton().last().click();
+            cy.getAntdPopoverDeleteButton().click();
+
+            cy.wait("@deletePost").then((interception) => {
+                const response = interception?.response;
+
+                expect(response?.statusCode).to.eq(200);
+                cy.getAntdNotification().should("contain", "Success");
+                cy.location().should((loc) => {
+                    expect(loc.pathname).to.eq("/posts");
+                });
+            });
+        });
+
+        it("should create form render errors", () => {
+            cy.getCreateButton().click();
+
+            submitForm();
+            cy.getAntdFormItemError({ id: "title" }).contains(
+                /please enter title/gi,
+            );
+            cy.getAntdFormItemError({ id: "category_id" }).contains(
+                /please enter category/gi,
+            );
+            cy.getAntdFormItemError({ id: "status" }).contains(
+                /please enter status/gi,
+            );
+
+            fillForm();
+
+            cy.getAntdFormItemError({ id: "title" }).should("not.exist");
+            cy.getAntdFormItemError({ id: "category_id" }).should("not.exist");
+            cy.getAntdFormItemError({ id: "status" }).should("not.exist");
+        });
+
+        it("should edit form render errors", () => {
+            cy.getEditButton().last().click();
+
+            // wait loading state and render to be finished
+            cy.wait("@getPost");
+            cy.getSaveButton().should("not.be.disabled");
+            cy.getAntdLoadingOverlay().should("not.exist");
+
+            cy.get("#title").should("not.have.value", "");
+            cy.get("#title").clear();
+
+            cy.getAntdFormItemError({ id: "title" }).contains(
+                /please enter title/gi,
+            );
+
+            fillForm();
+
+            cy.getAntdFormItemError({ id: "title" }).should("not.exist");
+        });
+
+        it("should create form warn when unsaved changes", () => {
+            cy.wait("@getPosts");
+            cy.getCreateButton().click();
+            cy.get("#title").type("any value");
+            cy.get(".ant-page-header-back-button > .ant-btn").click();
+            cy.on("window:confirm", (str) => {
+                expect(str).to.includes("You have unsaved changes");
+            });
+        });
+
+        it("should edit form warn when unsaved changes", () => {
+            cy.wait("@getPosts");
+            cy.getEditButton().last().click();
+
+            // wait loading state and render to be finished
+            cy.wait("@getPost");
+            cy.getSaveButton().should("not.be.disabled");
+            cy.getAntdLoadingOverlay().should("not.exist");
+
+            cy.get("#title").clear();
+            cy.get("#title").type("any value");
+            cy.get(".ant-page-header-back-button > .ant-btn").click();
+            cy.on("window:confirm", (str) => {
+                expect(str).to.includes("You have unsaved changes");
+            });
         });
     });
 });
