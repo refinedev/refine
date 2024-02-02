69b5097a94c3916c663e19109e025af605d3ef8f
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-02-02 09:15:03 +0300

    fix(docs): nextjs import path

--- examples/with-nextjs/src/providers/data-provider/index.ts
+++ examples/with-nextjs/src/providers/data-provider/index.ts
@@ -2,6 +2,6 @@
 
 import dataProviderSimpleRest from "@refinedev/simple-rest";
 
-const API_URL = "https://api.fake-rest.refine.dev";
+export const API_URL = "https://api.fake-rest.refine.dev";
 
 export const dataProvider = dataProviderSimpleRest(API_URL);
