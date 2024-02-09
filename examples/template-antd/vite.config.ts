a077e170a12e78b0f6fd6ec741c35fb09c529e9e
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-02-09 11:31:34 +0300

    chore: update template-antd example

--- examples/template-antd/vite.config.ts
+++ examples/template-antd/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
 });
