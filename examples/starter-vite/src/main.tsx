7dcfc902606b2793a880cb176533409681d0b9ed
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-02-09 11:24:32 +0300

    chore: update starter-vite example

--- examples/starter-vite/src/main.tsx
+++ examples/starter-vite/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
