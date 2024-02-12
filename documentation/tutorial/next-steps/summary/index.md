8be0b5e6eb14fad04697ea997ea62c34b550a4b5
Ali Emir Şen <senaliemir@gmail.com>
2024-02-12 10:13:23 +0300

    docs(tutorial): add next-steps unit summary step

--- documentation/tutorial/next-steps/summary/index.md
+++ documentation/tutorial/next-steps/summary/index.md
@@ -6,6 +6,19 @@ import { Sandpack } from "./sandpack.tsx";
 
 <Sandpack>
 
-Your content here.
+Contgratulations! You've completed the tutorial. You've learned how to create a CRUD application from scratch by Refine and learned about the basic concepts of Refine. You can now use Refine to create your own applications without any limitations.
+
+We've covered the following topics in this tutorial:
+
+- How to setup a Refine project,
+- Essentials of data fetching and mutations,
+- Handling authentication and applying it to your data providers,
+- Handling routes, navigation and resources,
+- Using UI libraries and Refine's built-in components,
+- Using Refine's DX tools to improve your development experience.
+
+Refine's features are not limited to these topics and you can find more information about all the features and capabilities of Refine in the [documentation](/docs).
+
+If you encounter any issues or have any questions, feel free to ask in the [Refine Discord](https://discord.gg/refine) or [GitHub Discussions](https://github.com/refinedev/refine/discussions).
 
 </Sandpack>
--- documentation/tutorial/next-steps/summary/sandpack.tsx
+++ documentation/tutorial/next-steps/summary/sandpack.tsx
@@ -1,20 +1,53 @@
 import React from "react";
+import { Fireworks } from "@fireworks-js/react";
+import type { FireworksHandlers } from "@fireworks-js/react";
+
 import { TutorialSandpack } from "@site/src/refine-theme/tutorial-sandpack";
 
+import { finalFiles as initialFiles } from "@site/tutorial/next-steps/devtools/react-router/sandpack";
+import { dependencies } from "@site/tutorial/next-steps/intro/ant-design/sandpack";
+
 export const Sandpack = ({ children }: { children: React.ReactNode }) => {
+    const ref = React.useRef<FireworksHandlers>(null);
+
+    React.useEffect(() => {
+        const timeout = setTimeout(() => {
+            ref.current?.waitStop();
+        }, 8000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
-        <TutorialSandpack
-            files={{
-                "App.tsx": {
-                    code: `
-export default function App(): JSX.Element {
-    return <h1>Hello world!</h1>
-}
-            `.trim(),
-                },
-            }}
-        >
-            {children}
-        </TutorialSandpack>
+        <>
+            <TutorialSandpack
+                showNavigator
+                dependencies={dependencies}
+                files={initialFiles}
+                finalFiles={finalFiles}
+                previewOnly
+            >
+                {children}
+            </TutorialSandpack>
+            <Fireworks
+                ref={ref}
+                autostart={true}
+                options={{
+                    intensity: 38,
+                    explosion: 8,
+                }}
+                style={{
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    position: "fixed",
+                    zIndex: 99999,
+                    pointerEvents: "none",
+                }}
+            />
+        </>
     );
 };
+
+export const finalFiles = initialFiles;
