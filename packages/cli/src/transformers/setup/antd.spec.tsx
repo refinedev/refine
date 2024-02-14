43c9a529e6aa796a488ac53efc91964449ed1325
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-02-14 15:16:42 +0300

    feat: add tests for existing setup

--- packages/cli/src/transformers/setup/antd.spec.tsx
+++ packages/cli/src/transformers/setup/antd.spec.tsx
@@ -4,9 +4,14 @@ import { VITE_STARTER_SOURCE } from "./fixtures/vite-starter";
 
 import jscodeshift from "jscodeshift";
 import { BASE_HEADLESS_SOURCE } from "./fixtures/antd/base-headless";
+import { WITH_EXISTING_ANTD_SETUP_SOURCE } from "./fixtures/antd/with-existing-antd-setup";
 
 describe("antd transformer", () => {
-    const cases = [VITE_STARTER_SOURCE, BASE_HEADLESS_SOURCE];
+    const cases = [
+        VITE_STARTER_SOURCE,
+        BASE_HEADLESS_SOURCE,
+        WITH_EXISTING_ANTD_SETUP_SOURCE,
+    ];
 
     it.each(cases)(
         "should transform the file\n----------------------------------------------------\n%s\n----------------------------------------------------",
