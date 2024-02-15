cdbe733c4532c69992540b7f6c9a95e1fe4db0b7
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-02-15 15:18:43 +0300

    fix: failing spec

--- packages/cli/src/commands/add/index.spec.ts
+++ packages/cli/src/commands/add/index.spec.ts
@@ -1,6 +1,5 @@
 import * as utilsProject from "../../utils/project/index";
 import * as utilsResource from "../../utils/resource/index";
-import { getGroupedArgs } from ".";
 import {
     getDefaultPath,
     getProviderOptions,
@@ -10,42 +9,6 @@ import {
 import { ProjectTypes } from "@definitions/projectTypes";
 
 describe("add", () => {
-    it.each([
-        {
-            args: ["auth", "live", "resource", "user", "post", "data"],
-            expected: {
-                providers: ["auth", "live"],
-                resources: ["user", "post", "data"],
-            },
-        },
-        {
-            args: ["resource", "auth", "live", "user", "post", "data"],
-            expected: {
-                providers: [],
-                resources: ["auth", "live", "user", "post", "data"],
-            },
-        },
-        {
-            args: ["auth", "live", "data", "resource"],
-            expected: {
-                providers: ["auth", "live", "data"],
-                resources: [],
-            },
-        },
-        {
-            args: ["auth", "live", "data", "not-provider"],
-            expected: {
-                providers: ["auth", "live", "data"],
-                resources: [],
-            },
-        },
-    ])("should group by args", ({ args, expected }) => {
-        const { providers, resources } = getGroupedArgs(args);
-
-        expect(providers).toEqual(expected.providers);
-        expect(resources).toEqual(expected.resources);
-    });
-
     it.each(providerArgs)(
         "should return proivder options for %s",
         (provider) => {
