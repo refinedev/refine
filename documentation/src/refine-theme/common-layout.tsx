2d8d476f2bd39dc03670ea1f253cb298fc325744
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-02-16 16:55:20 +0300

    feat(docs): update logo

--- documentation/src/refine-theme/common-layout.tsx
+++ documentation/src/refine-theme/common-layout.tsx
@@ -66,9 +66,10 @@ export const CommonLayout = (props: any) => {
                     async={true}
                     src="https://widget.kapa.ai/kapa-widget.bundle.js"
                     data-website-id="fa91d75a-5c82-4272-a893-a21d92245578"
-                    data-project-logo="https://refine.ams3.cdn.digitaloceanspaces.com/assets/refine-white-icon.png"
+                    data-project-logo="https://refine.ams3.cdn.digitaloceanspaces.com/assets/refine-ai-bot-logo.png"
                     data-project-name="Refine"
                     data-project-color="#303450"
+                    data-modal-image="https://refine.ams3.cdn.digitaloceanspaces.com/assets/refine-white-icon.png"
                     data-modal-header-bg-color="#303450"
                     data-modal-title-color="#ffffff"
                     data-button-border-radius="100%"
@@ -78,8 +79,8 @@ export const CommonLayout = (props: any) => {
                     data-button-text=""
                     data-button-box-shadow="none"
                     data-button-height="66px"
-                    data-button-image-height="64px"
-                    data-button-image-width="64px"
+                    data-button-image-height="60px"
+                    data-button-image-width="60px"
                     data-modal-title=""
                 />
             </Head>
