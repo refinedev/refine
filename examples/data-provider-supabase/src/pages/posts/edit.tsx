e9e9052f68c98aad10f04c60f172478fa290f8c7
Mahmoud Mousa Hamad <34195384+MahmoudMousaHamad@users.noreply.github.com>
2024-02-26 11:42:26 -0600

    chore: add status form item to edit page

--- examples/data-provider-supabase/src/pages/posts/edit.tsx
+++ examples/data-provider-supabase/src/pages/posts/edit.tsx
@@ -87,6 +87,32 @@ export const PostEdit: React.FC<IResourceComponentsProps> = () => {
         >
           <Select {...categorySelectProps} />
         </Form.Item>
+         <Form.Item
+          label="Status"
+          name="status"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Select
+            options={[
+              {
+                label: "Published",
+                value: "published",
+              },
+              {
+                label: "Draft",
+                value: "draft",
+              },
+              {
+                label: "Rejected",
+                value: "rejected",
+              },
+            ]}
+          />
+        </Form.Item>
         <Form.Item
           label="Content"
           name="content"
