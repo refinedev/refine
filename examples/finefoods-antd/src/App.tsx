9d1be80d613a6c86627bd04dc2f712663072ee9a
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-03-04 16:44:47 +0300

    fix(finefoods-antd): courier list path

--- examples/finefoods-antd/src/App.tsx
+++ examples/finefoods-antd/src/App.tsx
@@ -211,6 +211,7 @@ const App: React.FC = () => {
 
                 <Route path="/couriers">
                   <Route
+                    path=""
                     element={
                       <CourierList>
                         <Outlet />
