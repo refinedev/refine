8cd860b2248486c5bfa1facf73770784a5e48f08
Ali Emir Şen <senaliemir@gmail.com>
2024-03-06 15:56:01 +0300

    test(core): add missing cases for async-debounce

--- packages/core/src/definitions/helpers/async-debounce/index.spec.ts
+++ packages/core/src/definitions/helpers/async-debounce/index.spec.ts
@@ -111,4 +111,78 @@ describe("asyncDebounce", () => {
     await waitFor(() => expect(catcher).toHaveBeenCalledTimes(2));
     await waitFor(() => expect(catcher).toHaveBeenCalledWith("canceled"));
   });
+
+  it("should call the correct callback in long awaits", async () => {
+    const resolvedMock = jest.fn();
+    const fn = jest.fn(
+      (num: number) =>
+        new Promise((res) => {
+          setTimeout(() => {
+            resolvedMock(num);
+            res(num);
+          }, 2000);
+        }),
+    );
+    const catcher = jest.fn();
+    const resolver = jest.fn();
+    const nextResolver = jest.fn();
+    const debounced = asyncDebounce(fn, 1000, "canceled");
+
+    const options = { timeout: 3000 };
+
+    debounced(1).catch(catcher);
+    debounced(2).catch(catcher);
+    debounced(3).then(resolver).catch(catcher);
+
+    await waitFor(() => expect(catcher).toHaveBeenCalledTimes(2));
+
+    // wait for function to be triggered
+    await waitFor(() => expect(fn).toHaveBeenCalledTimes(1));
+    // but not resolved yet
+    await waitFor(() => expect(resolvedMock).not.toBeCalled());
+
+    // call the debounced again
+    debounced(4).then(nextResolver).catch(catcher);
+
+    // next call should not interrupt the previous call and successfully resolve with `resolver`
+    await waitFor(() => expect(resolver).toBeCalledTimes(1), options);
+    await waitFor(() => expect(resolvedMock).toBeCalledWith(3));
+    // next call should not be resolved
+    await waitFor(() => expect(nextResolver).not.toBeCalled());
+
+    // wait for second call to be made
+    await waitFor(() => expect(fn).toHaveBeenCalledTimes(2), options);
+    // wait for it to be resolved
+    await waitFor(() => expect(nextResolver).toBeCalledTimes(1), options);
+    await waitFor(() => expect(resolvedMock).toBeCalledWith(4));
+    // fourth call should not reject the third because it's already resolved
+    await waitFor(() => expect(catcher).toHaveBeenCalledTimes(2));
+    // third call should not be resolved again
+    await waitFor(() => expect(resolver).toHaveBeenCalledTimes(1));
+  });
+
+  it("should cancel previous and reject last if errored", async () => {
+    const fn = jest.fn((num: number) => {
+      if (num === 3) {
+        return Promise.reject("error");
+      }
+      return Promise.resolve(num);
+    });
+    const catcher = jest.fn();
+    const debounced = asyncDebounce(fn, 1000, "canceled");
+
+    debounced(1).catch(catcher);
+    debounced(2).catch(catcher);
+
+    await waitFor(() => expect(catcher).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(catcher).toHaveBeenCalledWith("canceled"));
+
+    debounced(3).catch(catcher);
+
+    await waitFor(() => expect(catcher).toHaveBeenCalledTimes(2));
+    await waitFor(() => expect(catcher).toHaveBeenCalledWith("canceled"));
+
+    await waitFor(() => expect(catcher).toHaveBeenCalledTimes(3));
+    await waitFor(() => expect(catcher).toHaveBeenCalledWith("error"));
+  });
 });
