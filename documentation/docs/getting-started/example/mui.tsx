f3a6a0a16474d61122c1182969d8938fc0290fe6
Ali Emir Şen <senaliemir@gmail.com>
2024-03-08 17:37:46 +0300

    docs(quickstart): add rounded corners

--- documentation/docs/getting-started/example/mui.tsx
+++ documentation/docs/getting-started/example/mui.tsx
@@ -4,23 +4,40 @@ import clsx from "clsx";
 import { PREVIEW_URL } from "./preview-url";
 
 export const MUIExample = () => (
-  <div className={clsx("pb-6", "h-[656px]")}>
-    <iframe
-      title="MUI Example"
+  <div className={clsx("pb-6")}>
+    <div
       className={clsx(
-        "absolute",
-        "left-0",
-        "right-0",
+        "h-[600px]",
+        "mx-auto",
+        "max-w-screen-xl",
         "w-full",
         "px-2",
         "md:px-4",
         "xl:px-6",
-        "max-w-screen-xl",
-        "mx-auto",
-        "min-h-max",
-        "h-[656px]",
+        "absolute",
+        "left-0",
+        "right-0",
       )}
-      src={PREVIEW_URL}
-    />
+    >
+      <div
+        className={clsx(
+          "border border-gray-200 dark:border-gray-600",
+          "relative",
+          "overflow-hidden",
+          "rounded-xl",
+          "landing-xl:rounded-2xl",
+          "overflow-hidden",
+          "w-full",
+          "h-full",
+        )}
+      >
+        <iframe
+          title="MUI Example"
+          className={clsx("h-full", "w-full")}
+          src={PREVIEW_URL}
+        />
+      </div>
+    </div>
+    <div className={clsx("h-[600px]")} />
   </div>
 );
