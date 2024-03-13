af196fb680ac4b50a6c0f3444092d1ab6529e673
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-03-13 09:31:44 +0300

    feat(graphql): add gqlMutaton and gqlQuery support for graphql data provider

--- packages/graphql/test/updateMany/index.spec.ts
+++ packages/graphql/test/updateMany/index.spec.ts
@@ -1,3 +1,4 @@
+import gql from "graphql-tag";
 import dataProvider from "../../src/index";
 import client from "../gqlClient";
 import "./index.mock";
@@ -24,15 +25,15 @@ describe("updateMany", () => {
       },
     });
 
-    expect(data[0]["id"]).toEqual("24");
-    expect(data[0]["title"]).toEqual("updated-foo");
-    expect(data[0]["content"]).toEqual("updated-bar");
-    expect(data[0]["category"].id).toEqual("2");
+    expect(data[0].id).toEqual("24");
+    expect(data[0].title).toEqual("updated-foo");
+    expect(data[0].content).toEqual("updated-bar");
+    expect(data[0].category.id).toEqual("2");
 
-    expect(data[1]["id"]).toEqual("25");
-    expect(data[1]["title"]).toEqual("updated-foo");
-    expect(data[1]["content"]).toEqual("updated-bar");
-    expect(data[1]["category"].id).toEqual("2");
+    expect(data[1].id).toEqual("25");
+    expect(data[1].title).toEqual("updated-foo");
+    expect(data[1].content).toEqual("updated-bar");
+    expect(data[1].category.id).toEqual("2");
   });
 
   it("correct response without meta", async () => {
@@ -46,7 +47,40 @@ describe("updateMany", () => {
       },
     });
 
-    expect(data[0]["id"]).toEqual("24");
-    expect(data[1]["id"]).toEqual("25");
+    expect(data[0].id).toEqual("24");
+    expect(data[1].id).toEqual("25");
+  });
+});
+
+describe("updateMany gql", () => {
+  it("correct response", async () => {
+    const { data } = await dataProvider(client).updateMany!({
+      resource: "posts",
+      ids: ["2121", "6223"],
+      variables: {
+        title: "updated-test",
+        content: "updated-test",
+        category: "19",
+      },
+      meta: {
+        gqlQuery: gql`
+          mutation ($ids: [ID!], $data: editPostInput!) {
+            updatePost (input: { where: { id_in: $ids }, data: $data }) {
+              post {
+                id
+                title
+                content
+                category {
+                  id
+                }
+              }
+            }
+          }
+        `,
+      },
+    });
+
+    expect(data[0].id).toEqual("2121");
+    expect(data[1].id).toEqual("6223");
   });
 });
