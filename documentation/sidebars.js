9e83144992c770cfb2e07326477db97778f0707f
Ali Emir Şen <senaliemir@gmail.com>
2024-04-02 17:47:30 +0300

    docs(core): add use resource params docs

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -227,6 +227,7 @@ module.exports = {
           collapsed: false,
           label: "Hooks",
           items: [
+            "routing/hooks/use-resource-params/index",
             "routing/hooks/use-resource/index",
             "routing/hooks/use-go/index",
             "routing/hooks/use-back/index",
@@ -441,6 +442,7 @@ module.exports = {
             "core/hooks/utilities/use-breadcrumb/index",
             "core/hooks/utilities/use-import/index",
             "core/hooks/utilities/use-export/index",
+            "core/hooks/utilities/buttons/index",
           ],
         },
       ],
