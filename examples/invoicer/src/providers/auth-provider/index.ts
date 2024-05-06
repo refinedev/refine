49fcd61fa1775a696359ae548465bf6a965c1ad9
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-05-06 16:56:40 +0300

    fix(invoicer): use axios interceptors to populate header

--- examples/invoicer/src/providers/auth-provider/index.ts
+++ examples/invoicer/src/providers/auth-provider/index.ts
@@ -1,6 +1,5 @@
 import { AuthProvider } from "@refinedev/core";
 import { AuthHelper } from "@refinedev/strapi-v4";
-import { axiosInstance } from "../axios";
 import { API_URL, TOKEN_KEY } from "../../utils/constants";
 
 export const strapiAuthHelper = AuthHelper(`${API_URL}/api`);
@@ -11,9 +10,6 @@ export const authProvider: AuthProvider = {
       const { data, status } = await strapiAuthHelper.login(email, password);
       if (status === 200) {
         localStorage.setItem(TOKEN_KEY, data.jwt);
-
-        // set header axios instance
-        axiosInstance.defaults.headers.common.Authorization = `Bearer ${data.jwt}`;
 
         return {
           success: true,
@@ -58,7 +54,6 @@ export const authProvider: AuthProvider = {
   check: async () => {
     const token = localStorage.getItem(TOKEN_KEY);
     if (token) {
-      axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
       return {
         authenticated: true,
       };
