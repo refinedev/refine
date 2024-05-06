3aacc29bb69085dc412af3e4788b353f897f02b5
Ali Emir Şen <senaliemir@gmail.com>
2024-05-06 22:47:39 +0300

    chore(cli): add bin fix dummy executable for pnpm

---
+++ packages/cli/dist/cli.cjs
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+// This file will be replaced by the "build" script.
+// It's only a placeholder to resolve chicken-egg problem in installation process of the monorepo.
+// Related: https://github.com/pnpm/pnpm/issues/1801
