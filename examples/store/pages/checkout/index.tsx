1fb2525d106070f9aeccf6074bc48efd9ab10b43
Ali Emir Şen <senaliemir@gmail.com>
2024-05-22 11:39:12 +0300

    fix(store): redirect to home on empty checkout

--- examples/store/pages/checkout/index.tsx
+++ examples/store/pages/checkout/index.tsx
@@ -1,7 +1,23 @@
 import { CheckoutTemplate } from "@components/checkout";
 import { SEO } from "@components/common";
+import { useCartContext } from "@lib/context";
+import { useRouter } from "next/router";
+import React from "react";
 
 const CheckoutPage: React.FC = () => {
+  const { cart, cartIsFetching } = useCartContext();
+  const { replace } = useRouter();
+  const navigated = React.useRef(false);
+
+  React.useEffect(() => {
+    if (cart && !cartIsFetching && cart.items.length === 0) {
+      if (!navigated.current) {
+        replace("/");
+        navigated.current = true;
+      }
+    }
+  }, [cart, cartIsFetching, replace]);
+
   return (
     <>
       <SEO title="Checkout" />
