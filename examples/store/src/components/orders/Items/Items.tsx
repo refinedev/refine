bd8d2d61d53087d6ce5fdc7226498007308587c8
Ali Emir Şen <senaliemir@gmail.com>
2024-05-23 09:46:17 +0300

    chore(store): remove comment

--- examples/store/src/components/orders/Items/Items.tsx
+++ examples/store/src/components/orders/Items/Items.tsx
@@ -23,43 +23,7 @@ export const Items: React.FC<ItemsProps> = ({ cartId, items, region }) => {
     >
       {enrichedItems?.length
         ? enrichedItems.map((item) => {
-            return (
-              <CheckoutCartItem item={item as LineItem} />
-              // <div className="grid grid-cols-[122px_1fr] gap-x-4" key={item.id}>
-              //   <div className="w-[122px]">
-              //     <Thumbnail thumbnail={item.thumbnail} size="full" />
-              //   </div>
-              //   <div className="flex flex-1 flex-col justify-between">
-              //     <div className="text-small-regular flex flex-1 flex-col">
-              //       <div className="flex items-start justify-between">
-              //         <div>
-              //           <h3 className="text-base-regular mr-4 overflow-hidden overflow-ellipsis whitespace-nowrap">
-              //             <Link
-              //               href={{
-              //                 pathname: "/product/[handle]",
-              //                 query: {
-              //                   handle: item.variant.product.handle,
-              //                 },
-              //               }}
-              //             >
-              //               {item.title}
-              //             </Link>
-              //           </h3>
-              //           <LineItemOptions variant={item.variant} />
-              //           <span>Quantity: {item.quantity}</span>
-              //         </div>
-              //         <div className="flex justify-end">
-              //           <LineItemPrice
-              //             quantity={item.quantity}
-              //             region={region}
-              //             variant={item.variant as CalculatedVariant}
-              //           />
-              //         </div>
-              //       </div>
-              //     </div>
-              //   </div>
-              // </div>
-            );
+            return <CheckoutCartItem item={item as LineItem} />;
           })
         : Array.from(Array(items.length).keys()).map((i) => {
             return <SkeletonLineItem key={i} />;
