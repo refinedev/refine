a3cd6af6cf9000d5c688b4ec1bd41635b854307d
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-05-27 23:13:34 +0300

    fix(with-nextjs-supabase): dynamic api was called outside request

--- examples/with-nextjs-supabase/src/providers/auth-provider/auth-provider.server.ts
+++ examples/with-nextjs-supabase/src/providers/auth-provider/auth-provider.server.ts
@@ -1,9 +1,10 @@
 import type { AuthProvider } from "@refinedev/core";
-import { supabaseServerClient } from "@utils/supabase/server";
+import { createSupabaseServerClient } from "@utils/supabase/server";
 
 export const authProviderServer: Pick<AuthProvider, "check"> = {
   check: async () => {
-    const { data, error } = await supabaseServerClient.auth.getSession();
+    const { data, error } =
+      await createSupabaseServerClient().auth.getSession();
     const { session } = data;
 
     if (error) {
--- examples/with-nextjs-supabase/src/utils/supabase/server.ts
+++ examples/with-nextjs-supabase/src/utils/supabase/server.ts
@@ -2,17 +2,17 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { SUPABASE_KEY, SUPABASE_URL } from "./contants";
 
-export const supabaseServerClient = createServerClient(
-  SUPABASE_URL,
-  SUPABASE_KEY,
-  {
+export const createSupabaseServerClient = () => {
+  const cookieStore = cookies();
+
+  return createServerClient(SUPABASE_URL, SUPABASE_KEY, {
     cookies: {
       get(name: string) {
-        return cookies().get(name)?.value;
+        return cookieStore.get(name)?.value;
       },
       set(name: string, value: string, options: CookieOptions) {
         try {
-          cookies().set({ name, value, ...options });
+          cookieStore.set({ name, value, ...options });
         } catch (error) {
           // The `set` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
@@ -21,7 +21,7 @@ export const supabaseServerClient = createServerClient(
       },
       remove(name: string, options: CookieOptions) {
         try {
-          cookies().set({ name, value: "", ...options });
+          cookieStore.set({ name, value: "", ...options });
         } catch (error) {
           // The `delete` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
@@ -29,5 +29,5 @@ export const supabaseServerClient = createServerClient(
         }
       },
     },
-  },
-);
+  });
+};
