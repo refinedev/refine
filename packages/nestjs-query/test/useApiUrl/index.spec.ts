64cbcd6c669a1947453c857093e096af4aa9125c
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-05-28 12:13:23 +0300

    fix(nestjs-query): test description

--- packages/nestjs-query/test/useApiUrl/index.spec.ts
+++ packages/nestjs-query/test/useApiUrl/index.spec.ts
@@ -3,7 +3,7 @@ import client, { API_URL } from "../gqlClient";
 
 describe("getApiUrl", () => {
   describe("should return API URL from client", () => {
-    it("correct response with query", async () => {
+    it("correct response with getApiUrl", async () => {
       const apiURL = dataProvider(client).getApiUrl();
 
       expect(apiURL).toEqual(API_URL);
