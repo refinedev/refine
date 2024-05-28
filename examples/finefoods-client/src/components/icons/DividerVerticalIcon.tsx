aae03a0bff246df91850bf646d70f27b6f5ce1b5
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-05-28 11:14:05 +0300

    fix: biome errors

--- examples/finefoods-client/src/components/icons/DividerVerticalIcon.tsx
+++ examples/finefoods-client/src/components/icons/DividerVerticalIcon.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 
-import { SVGProps } from "react";
+import type { SVGProps } from "react";
 
 const DividerVerticalIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
