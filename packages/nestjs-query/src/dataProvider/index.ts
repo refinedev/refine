64941de00b3cb42659c6ce1e7f8d1a4698d9e80c
Igor Donin <igordonin@gmail.com>
2024-07-17 07:27:42 -0300

    fix(nestjs-query): add missing dot to obj destructuring

--- packages/nestjs-query/src/dataProvider/index.ts
+++ packages/nestjs-query/src/dataProvider/index.ts
@@ -431,7 +431,7 @@ const dataProvider = (client: GraphQLClient): Required<DataProvider> => {
       const _client = new GraphQLClient(requestUrl, {
         ...client.requestConfig,
         method: validMethod,
-        headers: { ..client.requestConfig.headers, ...headers },
+        headers: { ...client.requestConfig.headers, ...headers },
       });
 
       const gqlOperation = meta?.gqlMutation ?? meta?.gqlQuery;
