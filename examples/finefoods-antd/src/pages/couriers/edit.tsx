82684cb818aaa333ca5523ea8148acfebe7778e8
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-07-22 11:34:39 +0300

    fix(examples): not found error

--- examples/finefoods-antd/src/pages/couriers/edit.tsx
+++ examples/finefoods-antd/src/pages/couriers/edit.tsx
@@ -135,7 +135,7 @@ export const CourierEdit = () => {
                 }}
               >
                 <CourierStatus
-                  isLoading={queryResult?.isLoading}
+                  isLoading={query?.isLoading}
                   value={
                     courier?.status || {
                       id: 3,
