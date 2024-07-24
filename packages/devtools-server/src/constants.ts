adac05f695bb448c6cc865a49af0f271a5b070bd
Ali Emir Şen <senaliemir@gmail.com>
2024-07-24 16:55:57 +0300

    feat(devtools-server): ability to change ports via REFINE_DEVTOOLS_PORT env var

--- packages/devtools-server/src/constants.ts
+++ packages/devtools-server/src/constants.ts
@@ -1,5 +1,13 @@
+import dotenv from "dotenv";
+
+const refineEnv: Record<string, string> = {};
+dotenv.config({ processEnv: refineEnv });
+
+const RAW_ENV_REFINE_DEVTOOLS_PORT =
+  process.env.REFINE_DEVTOOLS_PORT || refineEnv.REFINE_DEVTOOLS_PORT;
 export const DEFAULT_SERVER_PORT = 5001;
-export const SERVER_PORT = DEFAULT_SERVER_PORT;
+export const SERVER_PORT =
+  Number(RAW_ENV_REFINE_DEVTOOLS_PORT) || DEFAULT_SERVER_PORT;
 
 export const AUTH_SERVER_URL = __DEVELOPMENT__
   ? "https://develop.auth.refine.dev"
