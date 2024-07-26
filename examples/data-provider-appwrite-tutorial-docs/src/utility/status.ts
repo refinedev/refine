d44de34f2399992ca2ee7fd4cc4003f1467d3199
Ali Emir Şen <senaliemir@gmail.com>
2024-07-26 09:25:03 +0300

    chore(appwrite): update appwrite examples with cloud (#6168)

---
+++ examples/data-provider-appwrite-tutorial-docs/src/utility/status.ts
@@ -0,0 +1,14 @@
+export const statuses = [
+  {
+    label: "Draft",
+    value: "draft",
+  },
+  {
+    label: "Published",
+    value: "published",
+  },
+  {
+    label: "Rejected",
+    value: "rejected",
+  },
+];
