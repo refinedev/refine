2ac75eaad07dfc7ab72ef45faa12466b7a4c5b56
Ali Emir Şen <senaliemir@gmail.com>
2024-08-05 10:30:03 +0300

    test(cli): fix linting issues

--- packages/cli/src/commands/add/sub-commands/resource/index.test.ts
+++ packages/cli/src/commands/add/sub-commands/resource/index.test.ts
@@ -9,7 +9,7 @@ describe("add", () => {
   beforeAll(() => {
     // usefull for speed up the tests.
     jest.spyOn(console, "log").mockImplementation();
-    
+
     jest.spyOn(testTargetModule, "installInferencer").mockImplementation();
   });
 
