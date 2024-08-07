c3c3d2acbf3f907a25bbd61f9f15b5eeca59c07b
Andrew Aliaj <andyaliaj@gmail.com>
2024-08-07 11:42:29 -0400

    docs: add guidelines for linting, formatting, and commit conventions

--- packages/antd/src/hooks/modal/useModal/index.spec.ts
+++ packages/antd/src/hooks/modal/useModal/index.spec.ts
@@ -1,5 +1,5 @@
 import { renderHook } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { act } from "@testing-library/react";
 
 import { TestWrapper } from "@test";
 
