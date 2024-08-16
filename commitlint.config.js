e0e5fdcfd77613ef77285be3958f072b4962c933
Ali Emir Şen <senaliemir@gmail.com>
2024-08-16 11:09:14 +0300

    chore(commitlint): update header max length

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "header-max-length": [1, "always", 160],
     "body-max-line-length": [1, "always", 160],
   },
   helpUrl:
