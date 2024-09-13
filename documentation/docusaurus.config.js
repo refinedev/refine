352074fa36280b0a162d4e04ff383ededcd9f985
Archil Gogia <arshileg@gmail.com>
2024-09-13 22:21:54 +0400

    adds test

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -16,7 +16,7 @@ const siteConfig = {
   title: "Refine",
   tagline: "Build your React-based CRUD applications, without constraints",
   url: "https://refine.dev",
-  baseUrl: "/",
+  baseUrl: "/test",
   projectName: "refine",
   organizationName: "refinedev",
   trailingSlash: true,
