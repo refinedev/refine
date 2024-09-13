c787c2a3e97a5b4fd49b4cbbe244651d632b5eca
Archil Gogia <arshileg@gmail.com>
2024-09-13 23:32:31 +0400

    adds test

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -15,7 +15,7 @@ const thumbsUpDownFeedbackWidget = require("./plugins/thumbs-up-down-feedback-wi
 const siteConfig = {
   title: "Refine",
   tagline: "Build your React-based CRUD applications, without constraints",
-  url: "https://excelnode.com/blog",
+  url: "https://excelnode.com/test",
   baseUrl: "/",
   projectName: "refine",
   organizationName: "refinedev",
