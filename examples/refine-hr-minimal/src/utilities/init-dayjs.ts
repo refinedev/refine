71562aec10f00040ff27a51c437f3f13536a97fb
Ali Emir Şen <senaliemir@gmail.com>
2024-09-23 09:46:26 +0300

    chore: remove unused dayjs plugins

--- examples/refine-hr-minimal/src/utilities/init-dayjs.ts
+++ examples/refine-hr-minimal/src/utilities/init-dayjs.ts
@@ -1,14 +1,8 @@
 import dayjs from "dayjs";
-import isBetween from "dayjs/plugin/isBetween";
 import duration from "dayjs/plugin/duration";
 import relativeTime from "dayjs/plugin/relativeTime";
 import updateLocale from "dayjs/plugin/updateLocale";
-import minMax from "dayjs/plugin/minMax";
-import isoWeek from "dayjs/plugin/isoWeek";
 
 dayjs.extend(relativeTime);
 dayjs.extend(duration);
 dayjs.extend(updateLocale);
-dayjs.extend(isBetween);
-dayjs.extend(minMax);
-dayjs.extend(isoWeek);
