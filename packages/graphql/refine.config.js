c4f26c23784f36b6cdfeee3aeab1e8df2832fcc6
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-10-07 17:32:18 +0300

    feat(graphql): update index exports and swizzle config

--- packages/graphql/refine.config.js
+++ packages/graphql/refine.config.js
@@ -8,8 +8,6 @@ module.exports = {
         label: "GraphQL",
         requiredPackages: [
           "graphql-ws@5.9.1",
-          "graphql-request@5.2.0",
-          "gql-query-builder@3.5.5",
           "camelcase@6.2.0",
           "graphql@15.6.1",
           "pluralize@8.0.0",
@@ -24,32 +22,28 @@ module.exports = {
             dest: "./providers/graphql/dataProvider/index.ts",
           },
           {
+            src: "./src/dataProvider/options.ts",
+            dest: "./providers/graphql/dataProvider/options.ts",
+          },
+          {
             src: "./src/liveProvider/index.ts",
             dest: "./providers/graphql/liveProvider/index.ts",
+          },
+          {
+            src: "./src/liveProvider/helpers.ts",
+            dest: "./providers/graphql/liveProvider/helpers.ts",
           },
           {
             src: "./src/utils/index.ts",
             dest: "./providers/graphql/utils/index.ts",
           },
           {
-            src: "./src/utils/generateFilter.ts",
-            dest: "./providers/graphql/utils/generateFilter.ts",
+            src: "./src/utils/graphql.ts",
+            dest: "./providers/graphql/utils/graphql.ts",
           },
           {
-            src: "./src/utils/generateSort.ts",
-            dest: "./providers/graphql/utils/generateSort.ts",
-          },
-          {
-            src: "./src/utils/generateUseListSubscription.ts",
-            dest: "./providers/graphql/utils/generateUseListSubscription.ts",
-          },
-          {
-            src: "./src/utils/generateUseManySubscription.ts",
-            dest: "./providers/graphql/utils/generateUseManySubscription.ts",
-          },
-          {
-            src: "./src/utils/generateUseOneSubscription.ts",
-            dest: "./providers/graphql/utils/generateUseOneSubscription.ts",
+            src: "./src/utils/getListHelpers.ts",
+            dest: "./providers/graphql/utils/getListHelpers.ts",
           },
         ],
         message: `
@@ -58,12 +52,21 @@ module.exports = {
               \`\`\`
               // title: App.tsx
               import dataProvider, { liveProvider } from "providers/graphql";
+              import { Client, fetchExchange } from "@urql/core";
+              import { createClient } from "graphql-ws";
+
+              const API_URL = "https://api.nestjs-query.refine.dev/graphql";
+              const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
+
+              const client = new Client({ url: API_URL, exchanges: [fetchExchange] });
+
+              const wsClient = createClient({url: WS_URL})
 
               const App = () => {
                   return (
                       <Refine
-                          dataProvider={dataProvider}
-                          liveProvider={liveProvider}
+                          dataProvider={dataProvider(client)}
+                          liveProvider={liveProvider(wsClient)}
                           /* ... */
                       />
                   );
--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,10 +1,10 @@
 import dataProvider from "./dataProvider/index.js";
-export default dataProvider;
 
 export * from "./dataProvider/index.js";
+export * from "./dataProvider/options.js";
+
 export * from "./liveProvider/index.js";
 
-export * as qqlQueryBuilder from "gql-query-builder";
 export * as graphqlWS from "graphql-ws";
 
-export * from "./utils/index.js";
+export default dataProvider;
