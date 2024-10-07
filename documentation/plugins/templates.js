0b67bf5e683db161010b1f5b0a76b8a21e60c795
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-10-07 09:57:11 +0300

    feat(docs): add "Enterprise Edition" to templates (#6382)

--- documentation/plugins/templates.js
+++ documentation/plugins/templates.js
@@ -40,9 +40,10 @@ const templates = [
     images: [
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-refine-crm.png",
     ],
-    runOnYourLocalPath: "app-crm",
+    runOnYourLocalPath: null,
+    edition: "Enterprise",
     liveDemo: "https://example.crm.refine.dev/",
-    github: "https://github.com/refinedev/refine/tree/master/examples/app-crm",
+    github: null,
     reactPlatform: "Vite",
     uiFramework: "Ant Design",
     dataProvider: "Nestjs-query",
@@ -97,6 +98,7 @@ This CRM app template can be used in for various app requirements like B2B appli
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-storefront.png",
     ],
     runOnYourLocalPath: "finefoods-client",
+    edition: "Community",
     liveDemo: "https://example.refine.dev/",
     github:
       "https://github.com/refinedev/refine/tree/master/examples/finefoods-client",
@@ -105,6 +107,7 @@ This CRM app template can be used in for various app requirements like B2B appli
     dataProvider: "Rest API",
     authProvider: "Custom",
     description: `
+
 This is a template that can serve as an example for building React-based storefronts, admin panels, or internal tools using Refine. Implemented popular tools like Tailwind CSS and Next.js, which are highly demanded by the community.
 
 ### Key Features:
@@ -122,6 +125,7 @@ The source code is also open-source; feel free to use or inspect it to discover 
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-finefoods-material-ui.png",
     ],
     runOnYourLocalPath: "finefoods-material-ui",
+    edition: "Community",
     liveDemo: "https://example.mui.admin.refine.dev",
     github:
       "https://github.com/refinedev/refine/tree/master/examples/finefoods-material-ui",
@@ -172,6 +176,7 @@ This admin panel template can be used in for various app requirements like B2B a
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-finefoods-ant-design.png",
     ],
     runOnYourLocalPath: "finefoods-antd",
+    edition: "Community",
     liveDemo: "https://example.admin.refine.dev",
     github:
       "https://github.com/refinedev/refine/tree/master/examples/finefoods-antd",
@@ -224,6 +229,7 @@ This admin panel template can be used in for various app requirements like B2B a
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-store.png",
     ],
     runOnYourLocalPath: "store",
+    edition: "Community",
     liveDemo: "https://store.refine.dev/",
     github: "https://github.com/refinedev/refine/tree/master/examples/store",
     reactPlatform: "Next.js",
@@ -257,6 +263,7 @@ With its production-ready status, this template offers a solid foundation for bu
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-pixels.png",
     ],
     runOnYourLocalPath: "pixels",
+    edition: "Community",
     liveDemo: "https://pixels.refine.dev/",
     github: "https://github.com/refinedev/refine/tree/master/examples/pixels",
     tutorial: "https://refine.dev/week-of-refine-supabase/",
@@ -287,6 +294,7 @@ We built this template to demonstrate how the Refine framework simplifies and sp
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-invoice-generator.png",
     ],
     runOnYourLocalPath: "invoicer",
+    edition: "Community",
     liveDemo: "https://refine-invoicer-8mk7d.ondigitalocean.app/",
     github: "https://github.com/refinedev/refine/tree/master/examples/invoicer",
     reactPlatform: "Vite",
@@ -316,6 +324,7 @@ We built this template to showcase the efficiency and ease of using the Refine f
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/video-club-win95.png",
     ],
     runOnYourLocalPath: "win95",
+    edition: "Community",
     liveDemo: "https://videoclub.refine.dev",
     github: "https://github.com/refinedev/refine/tree/master/examples/win95",
     reactPlatform: "Vite",
@@ -335,6 +344,7 @@ The source code of the CRUD app is also open-source; feel free to use or inspect
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-realworld.png",
     ],
     runOnYourLocalPath: "real-world-example",
+    edition: "Community",
     liveDemo: "https://refine-real-world.netlify.app/",
     github: "https://github.com/refinedev/real-world-example",
     reactPlatform: "Vite",
@@ -368,6 +378,7 @@ Since the source code of this RealWorld app is open-source, you have the freedom
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-multitenancy-strapi.png",
     ],
     runOnYourLocalPath: "multi-tenancy-strapi",
+    edition: "Community",
     liveDemo: "https://multi-tenancy-strapi.refine.dev",
     github:
       "https://github.com/refinedev/refine/tree/master/examples/multi-tenancy-strapi",
@@ -391,6 +402,7 @@ The source code of this multitenancy app is open-source, allowing you to use or 
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-ant-design-template.png",
     ],
     runOnYourLocalPath: "auth-antd",
+    edition: "Community",
     liveDemo:
       "https://codesandbox.io/p/sandbox/github/refinedev/refine/tree/master/examples/auth-antd",
     github:
@@ -411,6 +423,7 @@ Complete internal tool template built with Material UI. Features authentication 
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-material-ui-template.png",
     ],
     runOnYourLocalPath: "auth-material-ui",
+    edition: "Community",
     liveDemo:
       "https://codesandbox.io/embed/github/refinedev/refine/tree/master/examples/auth-material-ui",
     github:
@@ -431,6 +444,7 @@ Complete internal tool template built with Material UI. Features authentication 
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-mantine-template.png",
     ],
     runOnYourLocalPath: "auth-mantine",
+    edition: "Community",
     liveDemo:
       "https://codesandbox.io/embed/github/refinedev/refine/tree/master/examples/auth-mantine",
     github:
@@ -451,6 +465,7 @@ Complete internal tool template built with Mantine. Features authentication and 
       "https://refine.ams3.cdn.digitaloceanspaces.com/templates/detail-chakra-ui-template.png",
     ],
     runOnYourLocalPath: "auth-chakra-ui",
+    edition: "Community",
     liveDemo:
       "https://codesandbox.io/embed/github/refinedev/refine/tree/master/examples/auth-chakra-ui",
     github:
--- documentation/src/refine-theme/templates-filters.tsx
+++ documentation/src/refine-theme/templates-filters.tsx
@@ -1,14 +1,27 @@
 import clsx from "clsx";
-import React, { type FC, type SVGProps } from "react";
+import React, {
+  type PropsWithChildren,
+  type FC,
+  type SVGProps,
+  type ReactNode,
+} from "react";
+import type { TemplateEdition } from "../types/integrations";
+
+export type Filters = {
+  edition: TemplateEdition;
+  uiFramework: string[];
+  backend: string[];
+};
 
 type Props = {
   svgId?: string;
   className?: string;
-  selected: {
-    uiFramework: string[];
-    backend: string[];
-  };
+  selected: Filters;
   data: {
+    editions: {
+      label: string;
+      icon: null;
+    }[];
     uiFrameworks: {
       label: string;
       icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
@@ -18,8 +31,9 @@ type Props = {
       icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
     }[];
   };
-  onBackendChange: (backend: string) => void;
-  onUIFrameworkChange: (framework: string) => void;
+  onEditionChange: (edition: string) => void;
+  onBackendChange: (backend: Filters["backend"][number]) => void;
+  onUIFrameworkChange: (framework: Filters["uiFramework"][number]) => void;
 };
 
 export const TemplatesFilters: FC<Props> = ({
@@ -27,97 +41,122 @@ export const TemplatesFilters: FC<Props> = ({
   className,
   data,
   selected,
+  onEditionChange,
   onBackendChange,
   onUIFrameworkChange,
 }) => {
   return (
     <div className={clsx("flex", "flex-col", "not-prose", className)}>
-      <h4
-        className={clsx("pl-4", "text-sm", "dark:text-gray-500 text-gray-700")}
-      >
-        UI Frameworks
-      </h4>
-      <div className={clsx("flex", "flex-col", "items-start", "gap-3", "mt-4")}>
+      <List label="Edition">
+        {data.editions.map((item) => {
+          const isSelected = selected.edition === item.label;
+
+          return (
+            <ListItem
+              key={item.label}
+              label={item.label}
+              isSelected={isSelected}
+              onClick={() => onEditionChange(item.label)}
+            />
+          );
+        })}
+      </List>
+      <List label="UI Frameworks" className={clsx("mt-10")}>
         {data.uiFrameworks.map((item) => {
           const isSelected = selected.uiFramework.includes(item.label);
           const Icon = item.icon;
 
           return (
-            <div
+            <ListItem
               key={item.label}
+              icon={<Icon id={svgId} />}
+              label={item.label}
+              isSelected={isSelected}
               onClick={() => onUIFrameworkChange(item.label)}
-              role="button"
-              className={clsx(
-                "flex",
-                "items-center",
-                "py-2 pl-2 pr-4",
-                "gap-2",
-                "rounded-full",
-                "cursor-pointer",
-                "border dark:border-gray-700 border-gray-200",
-                isSelected && "dark:bg-gray-700 bg-gray-50",
-                "transition-colors duration-200 ease-in-out",
-              )}
-            >
-              <Icon id={svgId} />
-              <span
-                className={clsx(
-                  "text-sm",
-                  !isSelected && "dark:text-gray-400 text-gray-600",
-                  isSelected && "dark:text-gray-0 text-gray-900",
-                )}
-              >
-                {item.label}
-              </span>
-            </div>
+            />
           );
         })}
-      </div>
-      <h4
-        className={clsx(
-          "pl-4",
-          "text-sm",
-          "dark:text-gray-500 text-gray-700",
-          "mt-10",
-        )}
-      >
-        Backends
-      </h4>
-      <div className={clsx("flex", "flex-col", "items-start", "gap-4", "mt-3")}>
+      </List>
+      <List label="Backends" className={clsx("mt-10")}>
         {data.backends.map((item) => {
           const isSelected = selected.backend.includes(item.label);
           const Icon = item.icon;
 
           return (
-            <div
+            <ListItem
               key={item.label}
+              icon={<Icon id={svgId} />}
+              label={item.label}
+              isSelected={isSelected}
               onClick={() => onBackendChange(item.label)}
-              role="button"
-              className={clsx(
-                "flex",
-                "items-center",
-                "py-2 pl-2 pr-4",
-                "gap-2",
-                "rounded-full",
-                "cursor-pointer",
-                "border dark:border-gray-700 border-gray-200",
-                isSelected && "dark:bg-gray-700 bg-gray-50",
-              )}
-            >
-              <Icon id={svgId} />
-              <span
-                className={clsx(
-                  "text-sm",
-                  !isSelected && "dark:text-gray-400 text-gray-600",
-                  isSelected && "dark:text-gray-0 text-gray-900",
-                )}
-              >
-                {item.label}
-              </span>
-            </div>
+            />
           );
         })}
-      </div>
+      </List>
     </div>
+  );
+};
+
+const List = (
+  props: PropsWithChildren<{
+    label: string;
+    className?: string;
+  }>,
+) => {
+  return (
+    <>
+      <h4
+        className={clsx(
+          "pl-4",
+          "text-sm",
+          "dark:text-gray-500 text-gray-700",
+          props.className,
+        )}
+      >
+        {props.label}
+      </h4>
+      <div className={clsx("flex", "flex-col", "items-start", "gap-4", "mt-3")}>
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+const ListItem = (props: {
+  icon?: ReactNode;
+  label: string;
+  isSelected: boolean;
+  onClick: () => void;
+}) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className={clsx(
+        "appearance-none",
+        "flex",
+        "items-center",
+        "h-10",
+        "pr-4",
+        props.icon && "pl-2",
+        !props.icon && "pl-4",
+        "gap-2",
+        "rounded-full",
+        "cursor-pointer",
+        "border dark:border-gray-700 border-gray-200",
+        props.isSelected && "dark:bg-gray-700 bg-gray-50",
+        "transition-colors duration-200 ease-in-out",
+      )}
+    >
+      {props.icon}
+      <span
+        className={clsx(
+          "text-sm",
+          !props.isSelected && "dark:text-gray-400 text-gray-600",
+          props.isSelected && "dark:text-gray-0 text-gray-900",
+        )}
+      >
+        {props.label}
+      </span>
+    </button>
   );
 };
