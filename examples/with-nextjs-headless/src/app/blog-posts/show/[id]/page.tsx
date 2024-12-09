6b8016a9bbb7422255711bf67f8247eb44db78a9
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-12-09 13:31:06 +0300

    feat(nextjs-router): add Next.js 15 support (#6551) (resolves #6430)
    Co-authored-by: Batuhan Wilhelm <batuhanwilhelm@gmail.com>

---
+++ examples/with-nextjs-headless/src/app/blog-posts/show/[id]/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Link, useOne, useShow } from "@refinedev/core";
+import type { BlogPost, Category } from "@types";
+
+export default function BlogPostShow() {
+  const { query } = useShow<BlogPost>();
+  const { data, isLoading } = query;
+  const record = data?.data;
+
+  const { data: categoryData, isLoading: categoryIsLoading } = useOne<Category>(
+    {
+      resource: "categories",
+      id: record?.category?.id || "",
+      queryOptions: {
+        enabled: !!record,
+      },
+    },
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <br />
+      <Link go={{ to: { resource: "blog_posts", action: "list" } }}>
+        ← Go to list
+      </Link>
+      <br />
+
+      <div>
+        <h5>ID</h5>
+        <div>{record?.id}</div>
+      </div>
+
+      <div>
+        <h5>Title</h5>
+        <div>{record?.title}</div>
+      </div>
+
+      <div>
+        <h5>Content</h5>
+        <div>{record?.content}</div>
+      </div>
+
+      <div>
+        <h5>Category</h5>
+        <div>
+          {categoryIsLoading ? "Loading..." : categoryData?.data?.title}
+        </div>
+      </div>
+
+      <div>
+        <h5>Status</h5>
+        <div>{record?.status}</div>
+      </div>
+
+      <div>
+        <h5>Created At</h5>
+        <div>
+          {record?.createdAt && new Date(record.createdAt).toLocaleDateString()}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ examples/with-nextjs-headless/src/app/register/page.tsx
@@ -0,0 +1,5 @@
+import { AuthPage } from "@components/auth-page";
+
+export default async function Register() {
+  return <AuthPage type="register" />;
+}
