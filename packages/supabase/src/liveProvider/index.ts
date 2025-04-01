7756236886b821d025473679b167bba0d42fb316
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2025-04-01 11:57:36 +0300

    fix: update warnOnce usage
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>

--- packages/supabase/src/liveProvider/index.ts
+++ packages/supabase/src/liveProvider/index.ts
@@ -74,12 +74,10 @@ export const liveProvider = (
           return;
         }
 
-        if (filters.length > 1 && !meta?.realtimeFilter) {
-          warnOnce(
-            true,
-            `Warning: Multiple filters detected for resource "${resource}". Supabase Realtime currently supports only a single filter. The first filter will be applied. To customize this behavior, use the 'meta.realtimeFilter' property.`,
-          );
-        }
+        warnOnce(
+          filters.length > 1 && !meta?.realtimeFilter,
+          `Warning: Multiple filters detected for resource "${resource}". Supabase Realtime currently supports only a single filter. The first filter will be applied. To customize this behavior, use the 'meta.realtimeFilter' property.`,
+        );
 
         const effectiveFilter = meta?.realtimeFilter
           ? [meta.realtimeFilter]
