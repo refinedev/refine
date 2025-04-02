e5caa3313ae1153e247636189a5d4124aa38e806
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2025-04-02 10:46:17 +0300

    fix(hasura): type import error

--- packages/hasura/src/utils/generateUseListSubscription.ts
+++ packages/hasura/src/utils/generateUseListSubscription.ts
@@ -4,7 +4,7 @@ import camelcase from "camelcase";
 import { type HasuraCrudFilters, generateFilters } from "./generateFilters";
 import { generateSorting } from "./generateSorting";
 import { getOperationFields } from "./graphql";
-import { NamingConvention } from "../dataProvider";
+import type { NamingConvention } from "../dataProvider";
 
 type GenerateUseListSubscriptionParams = {
   resource: string;
