16d9fa1d5a1d265ee6ba942c46473456883b4400
Nabil Alamin <aminonimisi2@gmail.com>
2025-05-02 12:33:42 +0100

    fix(chakra-ui): add meta to ShowButton Props (#6740)

---
+++ .changeset/proud-socks-yawn.md
@@ -0,0 +1,7 @@
+---
+"@refinedev/chakra-ui": minor
+---
+
+- Ensure `meta` prop is passed to the `ShowButton` component of Charka-UI
+
+[Resolves #6707](https://github.com/refinedev/refine/issues/6707)
---
+++ examples/inferencer-antd/src/lib/init-dayjs.ts
@@ -0,0 +1,14 @@
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import localeData from "dayjs/plugin/localeData";
+import weekday from "dayjs/plugin/weekday";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import weekYear from "dayjs/plugin/weekYear";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.extend(weekOfYear);
+dayjs.extend(weekYear);
