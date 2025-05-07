3edcc46783e26e6d412ef5ced67bff6a9d06fc06
thanaParis <thana.paris@s44.team>
2025-05-07 15:21:13 -0400

    fix(hasura): meta.gqlVariables now passed to createMany query for Hasura data provider

--- packages/hasura/test/createMany/index.spec.ts
+++ packages/hasura/test/createMany/index.spec.ts
@@ -172,4 +172,111 @@ describe("with gqlMutation", () => {
 
     expect(data[1]["title"]).toEqual("Etiam tincidunt ex ut auctor faucibus");
   });
+
+  it("correctly passes variables from meta.gqlVariables to the query", async () => {
+    const client = createClient("hasura-default");
+    const { data } = await dataProvider(client, {
+      namingConvention: "hasura-default",
+    }).createMany!({
+      resource: "posts",
+      variables: [
+        {
+          content: "Vestibulum vulputate sapien arcu.",
+          title: "Aenean ultricies non libero sit amet pellentesque",
+          category_id: "e27156c3-9998-434f-bd5b-2b078283ff26",
+        },
+        {
+          content: "Aliquam nibh erat.",
+          title: "Etiam tincidunt ex ut auctor faucibus",
+          category_id: "e27156c3-9998-434f-bd5b-2b078283ff26",
+        },
+      ],
+      meta: {
+        gqlMutation: gql`
+          mutation CreateManyPosts(
+            $objects: [posts_insert_input!]!,
+            $includeCategory: Boolean = false
+          ) {
+            insert_posts(objects: $objects) {
+              returning {
+                id
+                title
+                content
+                category @include(if: $includeCategory) {
+                  id
+                }
+              }
+            }
+          }
+        `,
+        gqlVariables: {
+          includeCategory: true,
+        },
+      },
+    });
+
+    expect(data[0]["title"]).toEqual(
+      "Aenean ultricies non libero sit amet pellentesque",
+    );
+    expect(data[0]["content"]).toEqual("Vestibulum vulputate sapien arcu.");
+    expect(data[0]["category"].id).toEqual(
+      "e27156c3-9998-434f-bd5b-2b078283ff26",
+    );
+
+    expect(data[1]["title"]).toEqual("Etiam tincidunt ex ut auctor faucibus");
+    expect(data[1]["content"]).toEqual("Aliquam nibh erat.");
+    expect(data[1]["category"].id).toEqual(
+      "e27156c3-9998-434f-bd5b-2b078283ff26",
+    );
+  });
+
+  it("doesn't pass extra variables to the query without meta.gqlVariables", async () => {
+    const client = createClient("hasura-default");
+    const { data } = await dataProvider(client, {
+      namingConvention: "hasura-default",
+    }).createMany!({
+      resource: "posts",
+      variables: [
+        {
+          content: "Vestibulum vulputate sapien arcu.",
+          title: "Aenean ultricies non libero sit amet pellentesque",
+          category_id: "e27156c3-9998-434f-bd5b-2b078283ff26",
+        },
+        {
+          content: "Aliquam nibh erat.",
+          title: "Etiam tincidunt ex ut auctor faucibus",
+          category_id: "e27156c3-9998-434f-bd5b-2b078283ff26",
+        },
+      ],
+      meta: {
+        gqlMutation: gql`
+          mutation CreateManyPosts(
+            $objects: [posts_insert_input!]!,
+            $includeCategory: Boolean = false
+          ) {
+            insert_posts(objects: $objects) {
+              returning {
+                id
+                title
+                content
+                category @include(if: $includeCategory) {
+                  id
+                }
+              }
+            }
+          }
+        `,
+      },
+    });
+
+    expect(data[0]["title"]).toEqual(
+      "Aenean ultricies non libero sit amet pellentesque",
+    );
+    expect(data[0]["content"]).toEqual("Vestibulum vulputate sapien arcu.");
+    expect(data[0]["category"]).toBeUndefined();
+
+    expect(data[1]["title"]).toEqual("Etiam tincidunt ex ut auctor faucibus");
+    expect(data[1]["content"]).toEqual("Aliquam nibh erat.");
+    expect(data[1]["category"]).toBeUndefined();
+  });
 });
