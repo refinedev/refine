cbfaa99ddfd0bf85a9712184081231fbd8e02c86
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2025-05-12 10:18:33 +0300

    Revert "fix(hasura): meta.gqlVariables passed to deleteMany Hasura data provi…"
    This reverts commit 135dfb729279c80ed438eaa782d9332ed27021f1.

--- packages/hasura/src/dataProvider/index.ts
+++ packages/hasura/src/dataProvider/index.ts
@@ -509,26 +509,13 @@ const dataProvider = (
         : camelcase(`delete_${operation}`);
 
       if (meta?.gqlMutation) {
-        const hasuraFilters = mergeHasuraFilters(
-          {
+        const response = await client.request<BaseRecord>(meta?.gqlMutation, {
+          where: {
             id: {
               _in: ids,
             },
           },
-          meta?.gqlVariables?.where,
-        );
-
-        const variables = {
-          ...(meta.gqlVariables && meta.gqlVariables),
-          ...(hasuraFilters && {
-            where: hasuraFilters,
-          }),
-        };
-
-        const response = await client.request<BaseRecord>(
-          meta?.gqlMutation,
-          variables,
-        );
+        });
 
         return {
           data: response[deleteOperation]["returning"],
