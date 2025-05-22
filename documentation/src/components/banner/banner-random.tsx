88363ece2adca2cf18e4918935bf03fde65f8849
DoguhanOzgurAkca <doguhanozgurakca@gmail.com>
2025-05-22 16:32:39 +0300

    docs(blog): removed an old version of the banners

--- documentation/src/components/banner/banner-random.tsx
+++ documentation/src/components/banner/banner-random.tsx
@@ -18,20 +18,6 @@ const data = [
     },
     bannerName: "banner-refine-ai-beta",
   },
-  /*   {
-    description:
-      "The next-gen approach to build enterprise-ready React-based internal tools, admin panels, dashboards & B2B apps with the power of GenAI.",
-    image: {
-      src: "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/banners/banner-refine-ai.png",
-      alt: "Refine AI Image",
-      href: "https://s.refine.dev/banner-with-image?ref=refine-ai-banner",
-    },
-    button: {
-      text: "Join the waitlist",
-      href: "https://s.refine.dev/banner-join-waitlist?ref=banner-refine-ai-banner",
-    },
-    bannerName: "banner-refine-ai-beta",
-  }, */
   /*    {
         title: "Save developer hours!",
         description:
