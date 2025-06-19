b11ccec54e4765219129f7cf62d4e489e055d70f
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2025-06-19 10:02:30 +0300

    chore: update link

--- documentation/src/refine-theme/top-announcement.tsx
+++ documentation/src/refine-theme/top-announcement.tsx
@@ -5,7 +5,7 @@ export const TopAnnouncement = () => {
   return (
     <div className="not-prose">
       <a
-        href="https://www.producthunt.com/products/refine-2?launch=refineai-2"
+        href="https://dub.sh/refine-ai-launch"
         target="_blank"
         rel="noopener noreferrer"
         className={clsx(
