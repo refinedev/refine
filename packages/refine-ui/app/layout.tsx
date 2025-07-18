d9a88c1553110513c52e6c62c84f1781fe29539d
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2025-07-18 10:57:50 +0300

    chore: rename refinedev-ui to refinedev-refine-ui

--- packages/refine-ui/app/layout.tsx
+++ packages/refine-ui/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../app/global.css";
+import "./global.css";
 
 const inter = Inter({
   variable: "--font-inter",
