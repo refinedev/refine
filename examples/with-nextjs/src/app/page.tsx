a6bdf158cd32dded1c7501c5037f6e7616f71e8d
Dhruv Bajaj <155525469+dhruvbajaj13@users.noreply.github.com>
2025-07-23 15:03:42 +0530

    Update page.tsx

--- examples/with-nextjs/src/app/page.tsx
+++ examples/with-nextjs/src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { Suspense } from "react";
-
 import { NavigateToResource } from "@refinedev/nextjs-router";
 import { Authenticated } from "@refinedev/core";
+import { Box, Text, Heading } from "@chakra-ui/react";
 
 export default function IndexPage() {
   return (
     <Suspense>
       <Authenticated key="home-page">
+        <Box p={8}>
+          <Heading mb={2}>Welcome to the Refine + Next.js Example! </Heading>
+          <Text mb={6}>You’ll be redirected to the default resource shortly.</Text>
+        </Box>
         <NavigateToResource />
       </Authenticated>
     </Suspense>
