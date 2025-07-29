553b0a3b4283478b42db0dce50ff35ebf4884b04
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2025-07-29 10:10:55 +0300

    feat(react-hook-form): remove deprecated api

--- packages/react-hook-form/test/jest.setup.ts
+++ packages/react-hook-form/test/jest.setup.ts
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/react";
 import { configure } from "@testing-library/dom";
 import * as util from "util";
 
