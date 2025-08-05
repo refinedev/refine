c73a66cf384b2cccc1e7f884243e001e66e63c16
arndom <aminonimisi2@gmail.com>
2025-08-05 18:06:18 +0100

    fix(core): fix type mismatch in `useGetLocale`
    - edit test case

--- packages/core/src/hooks/i18n/useGetLocale.ts
+++ packages/core/src/hooks/i18n/useGetLocale.ts
@@ -2,7 +2,7 @@ import { useCallback, useContext } from "react";
 
 import { I18nContext } from "@contexts/i18n";
 
-export type UseGetLocaleType = () => () => string | undefined;
+export type UseGetLocaleType = () => () => string;
 
 /**
  * If you need to know the current locale, refine provides the `useGetLocale` hook.
@@ -13,5 +13,11 @@ export type UseGetLocaleType = () => () => string | undefined;
 export const useGetLocale: UseGetLocaleType = () => {
   const { i18nProvider } = useContext(I18nContext);
 
-  return useCallback(() => i18nProvider?.getLocale(), []);
+  if (!i18nProvider) {
+    throw new Error(
+      "useGetLocale cannot be called without i18n provider being defined.",
+    );
+  }
+
+  return useCallback(() => i18nProvider.getLocale(), []);
 };
