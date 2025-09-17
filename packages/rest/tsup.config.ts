50a2b1f96f1802e641826500680d021d81c8616d
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2025-09-17 18:07:17 +0300

    fix(rest): add entry files in the dist root

--- packages/rest/tsup.config.ts
+++ packages/rest/tsup.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "tsup";
 import { NodeResolvePlugin } from "@esbuild-plugins/node-resolve";
 
 export default defineConfig((options) => ({
-  entry: ["src/index.ts"],
+  entry: {
+    index: "src/index.ts",
+    "nestjsx-crud": "src/data-providers/nestjsx-crud/index.ts",
+    "simple-rest": "src/data-providers/simple-rest/index.ts",
+    "strapi-v4": "src/data-providers/strapi-v4/index.ts",
+  },
   splitting: false,
   sourcemap: true,
   clean: false,
