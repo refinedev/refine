7a5b034df5419bbd7d2ae6181271b87a222e4948
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2026-01-14 00:58:41 +0100

    fix(examples): handle MUI X v8 CSS imports in Remix SSR

--- examples/with-remix-material-ui/remix.config.js
+++ examples/with-remix-material-ui/remix.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverModuleFormat: "cjs",
+  // Bundle MUI X DataGrid for server to handle CSS imports in v8
+  serverDependenciesToBundle: [/@mui\/x-data-grid/],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
