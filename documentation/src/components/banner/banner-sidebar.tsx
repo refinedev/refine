47d7e631215e12fe1f99a02844af2f83dfcaac0d
erenkurnaz <h.erenkurnaz@icloud.com>
2026-01-14 18:14:38 +0300

    chore: update sidebar banner image

--- documentation/src/components/banner/banner-sidebar.tsx
+++ documentation/src/components/banner/banner-sidebar.tsx
@@ -1,6 +1,4 @@
 import Link from "@docusaurus/Link";
-import { ArrowRightIcon } from "@site/src/refine-theme/icons/arrow-right";
-import { LandingRainbowButton } from "@site/src/refine-theme/landing-rainbow-button";
 import clsx from "clsx";
 import React from "react";
 
@@ -8,7 +6,7 @@ const text = "Explore Refine AI";
 const description =
   "The next-gen approach to build enterprise-ready React-based internal tools with the power of GenAI.";
 const image =
-  "https://refine.ams3.cdn.digitaloceanspaces.com/website/static/banners/banner-sidebar-refine-ai.png";
+  "https://refine.ams3.cdn.digitaloceanspaces.com/blog-banners/blog-side-banner.webp";
 
 export const BannerSidebar = ({ shouldShowBanner }) => {
   React.useEffect(() => {
@@ -27,48 +25,13 @@ export const BannerSidebar = ({ shouldShowBanner }) => {
   }, [shouldShowBanner]);
 
   return (
-    <div
-      className={clsx(
-        "flex",
-        "flex-col",
-        "gap-6",
-        "py-6",
-        "px-4",
-        "rounded-2xl",
-        "bg-banner-examples-sider-purple",
-        "not-prose",
-      )}
+    <Link
+      to={"https://s.refine.dev/blog-sidebar-ai?ref=banner-sidebar"}
+      target="_blank"
+      rel="noopener"
+      className={clsx("flex", "w-full", "rounded-md", "overflow-hidden")}
     >
-      <Link
-        to={"https://s.refine.dev/blog-sidebar-ai?ref=banner-sidebar"}
-        target="_blank"
-        rel="noopener"
-        className={clsx(
-          "flex",
-          "w-full h-auto xl:h-[152px]",
-          "flex-shrink-0",
-          "rounded-md",
-          "overflow-hidden",
-        )}
-      >
-        <img src={image} alt={"refine App screenshot"} loading="lazy" />
-      </Link>
-
-      <h2 className={clsx("text-2xl font-semibold", "text-gray-0")}>{text}</h2>
-      <p className={clsx("text-base", "text-gray-100")}>{description}</p>
-
-      <LandingRainbowButton
-        className={clsx("w-max")}
-        buttonClassname={clsx("!px-4", "!py-2")}
-        href={"https://s.refine.dev/blog-sidebar-ai?ref=banner-sidebar"}
-        target="_blank"
-        rel="noopener"
-      >
-        <div className={clsx("text-gray-900", "text-base", "font-bold")}>
-          Learn more
-        </div>
-        <ArrowRightIcon className={clsx("ml-2", "w-4", "h-4")} />
-      </LandingRainbowButton>
-    </div>
+      <img src={image} alt={"refine App screenshot"} loading="lazy" />
+    </Link>
   );
 };
