2e438a827789c76b1a332bfd54ebb8130cd9f988
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2026-01-22 15:09:14 +0300

    feat: replace slugify function with toSlug for breadcrumb generation

--- documentation/src/components/templates-filtered-page/index.tsx
+++ documentation/src/components/templates-filtered-page/index.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { TemplatesPage } from "@site/src/pages/core/templates";
 import type { BreadcrumbItem } from "@site/src/components/breadcrumbs";
+import { toSlug } from "../../../plugins/templates";
 
 type FiltersPayload = {
   uiFramework?: string;
   backend?: string;
-};
-
-const slugify = (value: string) => {
-  return value
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/(^-|-$)/g, "");
 };
 
 const buildBreadcrumbs = (filters: FiltersPayload): BreadcrumbItem[] => {
@@ -21,7 +15,7 @@ const buildBreadcrumbs = (filters: FiltersPayload): BreadcrumbItem[] => {
   ];
 
   if (filters.uiFramework) {
-    const uiSlug = slugify(filters.uiFramework);
+    const uiSlug = toSlug(filters.uiFramework);
     items.push({
       label: filters.uiFramework,
       href: filters.backend ? `/core/templates/${uiSlug}/` : undefined,
