1064508d42da0a9f37ffa6c6dcd221bb649a9a04
阿泥豆 <1243352777@qq.com>
2026-05-11 20:29:27 +0800

    fix(react-hook-form): useFieldArray fields empty on second visit

--- packages/react-hook-form/src/useForm/index.ts
+++ packages/react-hook-form/src/useForm/index.ts
@@ -129,6 +129,7 @@ export const useForm = <
     watch,
     setValue,
     getValues,
+    reset,
     handleSubmit: handleSubmitReactHookForm,
     setError,
     formState: { dirtyFields },
@@ -268,7 +269,7 @@ export const useForm = <
     const applyQueryValues = () => {
       if (!isActive) return;
 
-      applyValuesToFields(getRegisteredFields(), data, false);
+      reset(data, { keepDirtyValues: true });
     };
 
     queryDataRef.current = data;
